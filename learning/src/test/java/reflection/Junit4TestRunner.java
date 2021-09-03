77ba2ea0b2017c9dbc6474ae21aa61a214cd4cd4
jaeseongDev <qkrwotjd1445@naver.com>
2021-09-03 18:10:45 +0900

    test: Junit4 Test, Reflections Test 추가

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,30 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
 
 class Junit4TestRunner {
 
     @Test
+    @DisplayName("Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            Annotation[] annotations = method.getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType() == MyTest.class) {
+                    method.invoke(junit4Test);
+                }
+            }
+        }
     }
 }
