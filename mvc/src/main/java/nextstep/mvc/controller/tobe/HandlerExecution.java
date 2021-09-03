767ca47910f6869ffb487d1bc99bbe9937132f89
Minsung Son <sns2831@gmail.com>
2021-09-03 18:09:55 +0900

    feat: AnnotationHandlerMapping 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,33 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.view.ModelAndView;
 
-public class HandlerExecution {
+public class HandlerExecution implements Controller{
+
+    private final Object controller;
+    private final Method handler;
+
+    public HandlerExecution(Object controller, Method handler) {
+        this.controller = controller;
+        this.handler = handler;
+    }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+
+        ModelAndView modelAndView = new ModelAndView((model, request1, response1) -> {
+        });
+
+        Object id = request.getAttribute("id");
+        modelAndView.addObject("id", id);
+
+        return modelAndView;
+    }
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        return (String) handler.invoke(controller, req, res);
     }
 }
