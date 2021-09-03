12171c6d44dad2336f6454eb438fd1064cf8ce43
jaeseongDev <qkrwotjd1445@naver.com>
2021-09-03 19:33:31 +0900

    feat: AnnotationHandlerMapping 클래스 구현 완료

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,21 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +30,43 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            // TODO : basePackage에서 @Controller 어노테이션 붙은 클래스 전부 불러오기
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+            // TODO : @Controller 붙은 클래스에서 @RequestMapping 붙은 메서드들 전부 추출
+            for (Class<?> controllerClass : controllerClasses) {
+                List<Method> methods = Arrays.stream(controllerClass.getMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toList());
+
+                // TODO : @RequestMapping의 value와 method 값을 얻어서 HandlerKey 객체 만들기
+                for (Method method : methods) {
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    String requestUri = requestMapping.value();
+                    RequestMethod[] requestMethods = requestMapping.method();
+                    for (RequestMethod requestMethod : requestMethods) {
+                        HandlerKey handlerKey = new HandlerKey(requestUri, requestMethod);
+                        // TODO : Reflection을 활용해서 클래스의 instance와 method를 생성
+                        Object instance = controllerClass.getConstructor().newInstance();
+
+                        // TODO : HandlerExecution 만들기
+                        HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+
+                        // TODO : handlerExecutions Map에 넣기
+                        handlerExecutions.put(handlerKey, handlerExecution);
+                    }
+                }
+            }
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException();
+        }
     }
 
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        String requestUri = request.getRequestURI();
+        RequestMethod method = RequestMethod.valueOf(request.getMethod().toUpperCase());
+        return handlerExecutions.get(new HandlerKey(requestUri, method));
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,9 +2,18 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
+
+    private Object instance;
+    private Method method;
+
+    public HandlerExecution(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
         return null;
