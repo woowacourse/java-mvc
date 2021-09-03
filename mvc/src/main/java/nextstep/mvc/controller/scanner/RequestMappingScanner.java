9801b1d74e66f392bdadf7cb63e9c6a6ba86f2b2
Minsung Son <sns2831@gmail.com>
2021-09-03 17:06:13 +0900

    feat: RequestMappingScanner 구현

--- mvc/src/main/java/nextstep/mvc/controller/scanner/RequestMappingScanner.java
+++ mvc/src/main/java/nextstep/mvc/controller/scanner/RequestMappingScanner.java
@@ -5,6 +5,7 @@ import static java.util.stream.Collectors.toSet;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
+import nextstep.mvc.controller.tobe.HandlerKey;
 import nextstep.web.annotation.RequestMapping;
 
 public class RequestMappingScanner {
@@ -12,6 +13,19 @@ public class RequestMappingScanner {
     public static Set<Method> getRequestMappedMethod(Class<?> controller) {
         return Arrays.stream(controller.getDeclaredMethods())
             .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .collect(toSet());
+    }
+
+    public static Set<HandlerKey> extractHandlerKeys(Method requestMappedMethod) {
+        if(!requestMappedMethod.isAnnotationPresent(RequestMapping.class)) {
+            throw new IllegalArgumentException("This method is not RequestMapped");
+        }
+
+        RequestMapping requestMapping = requestMappedMethod.getAnnotation(RequestMapping.class);
+        String url = requestMapping.value();
+
+        return Arrays.stream(requestMapping.method())
+            .map(method -> new HandlerKey(url, method))
             .collect(toSet());
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/scanner/test_classes/Test1.java
+++ mvc/src/test/java/nextstep/mvc/controller/scanner/test_classes/Test1.java
@@ -1,16 +1,20 @@
 package nextstep.mvc.controller.scanner.test_classes;
 
+import static nextstep.web.support.RequestMethod.GET;
+import static nextstep.web.support.RequestMethod.POST;
+
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 
 @Controller
 public class Test1 {
 
-    @RequestMapping
+    @RequestMapping(value = "/test1", method = GET)
     public void test1() {
     }
 
-    @RequestMapping
+
+    @RequestMapping(value = "/test2", method = {GET, POST})
     public void test2() {
     }
 
