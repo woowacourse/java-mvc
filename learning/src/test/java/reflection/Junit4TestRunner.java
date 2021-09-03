ee4788d9f6a989bed13d4314c6e311c6c0c56f21
jaeseongDev <qkrwotjd1445@naver.com>
2021-09-03 19:21:04 +0900

    test: 어노테이션 찾는 테스트 코드 수정

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,11 @@
 package reflection;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
 
 class Junit4TestRunner {
 
@@ -16,15 +14,11 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
         Junit4Test junit4Test = clazz.getConstructor().newInstance();
-
-        Method[] methods = clazz.getDeclaredMethods();
+        List<Method> methods = Arrays.stream(clazz.getMethods())
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .collect(Collectors.toList());
         for (Method method : methods) {
-            Annotation[] annotations = method.getAnnotations();
-            for (Annotation annotation : annotations) {
-                if (annotation.annotationType() == MyTest.class) {
-                    method.invoke(junit4Test);
-                }
-            }
+            method.invoke(junit4Test);
         }
     }
 }
