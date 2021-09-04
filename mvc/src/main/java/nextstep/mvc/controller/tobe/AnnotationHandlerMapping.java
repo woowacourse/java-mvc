a1bac242283ac70c0e11414f3cf2979f3d690b44
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-04 20:11:48 +0900

    feat: HandlerExecution.java 에 핸들러 인스턴스 멤버변수 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -9,6 +9,7 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,8 +35,19 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     private void putHandlerExecutionsOfHandlerClasses(Set<Class<?>> handlerClasses) {
         for (Class<?> handlerClass : handlerClasses) {
+            log.info("Annotation Controller 등록 : {}", handlerClass.getName());
             final Method[] methods = handlerClass.getDeclaredMethods();
-            putHandlerExecutionsOfHandlerMethods(methods);
+            final Object handlerInstance = createHandlerInstance(handlerClass);
+            putHandlerExecutionsOfHandlerMethods(handlerInstance, methods);
+        }
+    }
+
+    private Object createHandlerInstance(Class<?> handlerClass) {
+        try {
+            return handlerClass.getConstructor().newInstance();
+        } catch (Exception e) {
+            log.error("핸들러 클래스 {} 의 인스턴스 생성 중 에러가 발생했습니다.", handlerClass.getName());
+            throw new IllegalStateException(String.format("핸들러 클래스 %s 의 인스턴스 생성 중 에러가 발생했습니다.", handlerClass.getName()));
         }
     }
 
@@ -44,15 +56,16 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         return reflections.getTypesAnnotatedWith(Controller.class);
     }
 
-    private void putHandlerExecutionsOfHandlerMethods(Method[] methods) {
+    private void putHandlerExecutionsOfHandlerMethods(Object handlerInstance, Method[] methods) {
         for (Method method : methods) {
+            log.info("Annotation Controller {} 의 method {} 등록", handlerInstance.getClass().getName(), method.getName());
             final RequestMapping requestMappingAnnotation = method.getAnnotation(RequestMapping.class);
 
             final String requestUri = requestMappingAnnotation.value();
             final RequestMethod requestMethod = requestMappingAnnotation.method()[0];
 
             final HandlerKey handlerKey = new HandlerKey(requestUri, requestMethod);
-            final HandlerExecution handlerExecution = new HandlerExecution(method);
+            final HandlerExecution handlerExecution = new HandlerExecution(handlerInstance, method);
             handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -8,15 +8,15 @@ import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Object handler;
     private final Method method;
 
-    public HandlerExecution(Method method) {
+    public HandlerExecution(Object handler, Method method) {
+        this.handler = handler;
         this.method = method;
     }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        final Class<?> declaringClass = method.getDeclaringClass();
-        final Object handler = declaringClass.getConstructor().newInstance();
         return (ModelAndView) method.invoke(handler, request, response);
     }
 }
