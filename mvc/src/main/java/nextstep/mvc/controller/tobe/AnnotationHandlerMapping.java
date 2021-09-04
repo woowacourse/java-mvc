416991e42645b0efc106df86bdd64a09b3e57052
Kimun Kim <kkm97351@gmail.com>
2021-09-05 03:52:49 +0900

    test: AnnotationHandlerMappingTest 테스트 통과하도록 작성

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,29 +2,68 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.mvc.controller.exception.HandlerMappingInitiationException;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
+        log.info("Start Initialing AnnotationHandlerMapping!");
+
+        for (Object basePackage : basePackages) {
+            log.info("Register HandlerExecution from package: {}", basePackage);
+            registerHandlerMapping(basePackage);
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void registerHandlerMapping(Object packageName) {
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> handlerExecutionClasses = reflections.getTypesAnnotatedWith(RequestMapping.class);
+
+        for (Class<?> handlerExecutionClass : handlerExecutionClasses) {
+            RequestMapping annotation = handlerExecutionClass.getAnnotation(RequestMapping.class);
+
+            String url = annotation.value();
+            RequestMethod[] methods = annotation.method();
+
+            try {
+                HandlerExecution handlerExecution = (HandlerExecution) handlerExecutionClass.getDeclaredConstructor().newInstance();
+
+                for (RequestMethod method : methods) {
+                    HandlerKey handlerKey = new HandlerKey(url, method);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                    log.info("[{}] {} : {} has been registered.", method, url, handlerExecution);
+                }
+
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException exception) {
+                throw new HandlerMappingInitiationException(exception);
+            }
+        }
+    }
+
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
---
+++ mvc/src/test/java/samples/handlerExecution/GetTestHandlerExecution.java
@@ -0,0 +1,23 @@
+package samples.handlerExecution;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+@RequestMapping(value = "/get-test", method = RequestMethod.GET)
+public class GetTestHandlerExecution extends HandlerExecution {
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String attributeName = "id";
+        Object id = request.getAttribute(attributeName);
+
+        ModelAndView modelAndView = new ModelAndView(null);
+
+        modelAndView.addObject(attributeName, id);
+
+        return modelAndView;
+    }
+}
