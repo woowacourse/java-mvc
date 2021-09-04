b38963c375156b3f6baaba4e3cf2679d9c1150af
cjy17 <cjy1705@naver.com>
2021-09-04 16:55:31 +0900

    feat: 학습테스트 진행

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +8,14 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Object obj = clazz.getConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method test1 = clazz.getMethod("test1");
+        Method test2 = clazz.getMethod("test2");
+        Method three = clazz.getMethod("three");
+
+        test1.invoke(obj);
+        test2.invoke(obj);
+        three.invoke(obj);
     }
 }
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,12 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,5 +21,17 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        classes.addAll(controllers);
+        classes.addAll(services);
+        classes.addAll(repositories);
+
+        for (Class<?> clazz : classes) {
+            System.out.println(clazz.toString());
+        }
     }
 }
