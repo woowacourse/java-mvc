63128bc9f24477e39f499d9b52160f604d826a97
ecsimsw <ecsimsw@gmail.com>
2021-09-05 08:24:14 +0900

    feat : reflections test

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -12,6 +15,21 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
+
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .stream()
+                .map(Class::getName)
+                .forEach(log::info);
+
+        reflections.getTypesAnnotatedWith(Service.class)
+                .stream()
+                .map(Class::getName)
+                .forEach(log::info);
+
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .stream()
+                .map(Class::getName)
+                .forEach(log::info);
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
