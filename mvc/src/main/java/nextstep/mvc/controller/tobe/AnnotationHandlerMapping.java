2fd70868779c8235aa2375403e8acb8377b6670e
NewWisdom <wlgp2500@gmail.com>
2021-09-05 23:03:20 +0900

    feat(annotation-handler-mapping): initial 메서드 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,15 +2,20 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -21,7 +26,42 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+            for (Class<?> controllerClass : controllerClasses) {
+                addHandlerExecutions(controllerClass);
+            }
+        } catch (Exception e) {
+            LOG.error("Annotation Handler Mapping Fail!", e);
+        }
+        LOG.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private void addHandlerExecutions(Class<?> controllerClass) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
+        List<Method> methods = getMethodsWithRequestMapping(controllerClass);
+        for (Method method : methods) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String url = requestMapping.value();
+            RequestMethod[] requestMethods = requestMapping.method();
+            for (RequestMethod requestMethod : requestMethods) {
+                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                HandlerExecution handlerExecution = getHandlerExecution(controllerClass, method);
+                this.handlerExecutions.put(handlerKey, handlerExecution);
+                LOG.info("Add Handler Execution Key : {}, Value : {} !", handlerKey, handlerExecution);
+            }
+        }
+    }
+
+    private List<Method> getMethodsWithRequestMapping(Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private HandlerExecution getHandlerExecution(Class<?> controllerClass, Method method) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
+        Object handler = controllerClass.getConstructor().newInstance();
+        return new HandlerExecution(handler, method);
     }
 
     public Object getHandler(HttpServletRequest request) {
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -4,9 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
+
+    private final Object handler;
+    private final Method method;
+
+    public HandlerExecution(Object handler, Method method) {
+        this.handler = handler;
+        this.method = method;
+    }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "HandlerExecution{" +
+                "handler=" + handler +
+                ", method=" + method +
+                '}';
     }
 }
