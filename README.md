001de4dc73aaa08b48e47022ee9726fb64647367
xrabcde <xrabcde@gmail.com>
2021-09-05 13:59:14 +0900

    test: Junit4 테스트 작성

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # MVC 프레임워크 구현하기
 
 ## 1단계 기능 요구사항
-- [ ] 학습 테스트
+- [x] 학습 테스트
   - [x] Reflection 테스트
   - [x] Reflections 테스트
   - [x] Junit3 테스트
-  - [ ] Junit4 테스트
+  - [x] Junit4 테스트
 - [ ] AnnotationHandlerMapping 구현하기
 - [ ] 기존 코드와 호환되도록 리팩터링
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,25 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class Junit4TestRunner {
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+class Junit4TestRunner {
     @Test
+    @DisplayName("Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        List<Method> methods = Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = new Junit4Test();
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
     }
 }
