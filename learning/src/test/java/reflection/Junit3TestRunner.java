d4ae1e5fde343f27c6588418ad4b41fdf9784a49
Gump <xowjd41@naver.com>
2021-09-05 23:05:37 +0900

    feat: 리플렉션 테스트 추가 구현

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
+        //given
         Class<Junit3Test> clazz = Junit3Test.class;
-
+        //when
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        final Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+        //then
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.getName().startsWith("test")) {
+                declaredMethod.invoke(junit3Test);
+            }
+        }
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,26 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
-
+        //given
+        final Junit4Test junit4Test = new Junit4Test();
+        final Reflections reflections = new Reflections("reflection", new MethodAnnotationsScanner(), new SubTypesScanner());
+        //when
+        final Set<Method> methodsAnnotatedWith = reflections.getMethodsAnnotatedWith(MyTest.class);
+        //then
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : methodsAnnotatedWith) {
+            method.invoke(junit4Test);
+        }
     }
 }
