1fb7d91a357a8310d6cd42df13fead03e09c179a
NewWisdom <wlgp2500@gmail.com>
2021-09-05 19:08:12 +0900

    test(junit): 학습 테스트 구현

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,27 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Class<MyTest> myTestClazz = MyTest.class;
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> Objects.nonNull(m.getAnnotation(myTestClazz)))
+                .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
     }
 }
