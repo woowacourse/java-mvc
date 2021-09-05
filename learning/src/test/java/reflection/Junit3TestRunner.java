3eef2d1eaa051dc6eb7480eca60714b7a905ee39
Byung Wook Park <bbwwpark@naver.com>
2021-09-05 17:31:44 +0900

    feat: 메서드 이름으로 메서드 찾아서 실행시키는 방법 학습

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,28 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
+    @DisplayName("Junit3Test에서 test로 시작하는 메소드 실행한다.")
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] methods = clazz.getMethods();
+        List<Method> testMethods = Arrays.stream(methods)
+            .filter(it -> it.getName().startsWith("test"))
+            .collect(Collectors.toList());
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = new Junit3Test();
+
+        for (Method method : testMethods) {
+            method.invoke(junit3Test);
+        }
     }
+
 }
