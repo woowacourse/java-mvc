9cd183deb94c353030e71e03f097e2f6592dd605
Byung Wook Park <bbwwpark@naver.com>
2021-09-05 17:59:10 +0900

    feat: 특정 애노테이션이 붙어있는 모든 메서드 를 찾아서 실행시키는 방법 학습

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -7,12 +12,21 @@ import org.slf4j.LoggerFactory;
 
 class ReflectionsTest {
 
-    private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ReflectionsTest.class);
+    private static final Reflections REFLECTIONS = new Reflections("examples");
 
+    @DisplayName("클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.")
     @Test
-    void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("examples");
-
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+    void showAnnotationClass() {
+        loggingWithAnotatedClass(REFLECTIONS.getTypesAnnotatedWith(Controller.class), Controller.class);
+        loggingWithAnotatedClass(REFLECTIONS.getTypesAnnotatedWith(Service.class), Service.class);
+        loggingWithAnotatedClass(REFLECTIONS.getTypesAnnotatedWith(Repository.class), Repository.class);
     }
+
+    private void loggingWithAnotatedClass(Set<Class<?>> controllerAnnotatedWith, Class<?> annotation) {
+        for (Class<?> aClass : controllerAnnotatedWith) {
+            LOG.debug("애노테이션 = {}, 클래스 = {}", annotation.getName(), aClass.getName());
+        }
+    }
+
 }
