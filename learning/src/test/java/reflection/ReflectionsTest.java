523fca30b1fd462800437ac0c789ac74981ca366
Hyeon9mak <jinha3507@gmail.com>
2021-09-07 07:18:00 +0900

    test: ReflectionsTest 작성

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -10,9 +14,21 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> classesAnnotatedWithController = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> classesAnnotatedWithService = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> classesAnnotatedWithRepository = reflections.getTypesAnnotatedWith(Repository.class);
+
+        loggingClasses(Controller.class, classesAnnotatedWithController);
+        loggingClasses(Service.class, classesAnnotatedWithService);
+        loggingClasses(Repository.class, classesAnnotatedWithRepository);
+    }
+
+    private void loggingClasses(Class<?> annotationClass, Set<Class<?>> annotatedClass) {
+        for (Class<?> clazz : annotatedClass) {
+            log.debug("@{} annotated class: {}", annotationClass.getSimpleName(), clazz.getName());
+        }
     }
 }
