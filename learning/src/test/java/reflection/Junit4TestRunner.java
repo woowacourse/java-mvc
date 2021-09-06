48081300759a75ba7ceb041a48b63346b0f71483
Eugine Park <pkeugine@gmail.com>
2021-09-06 22:33:01 +0900

    test: Junit4TestRunner 완성

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,39 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
+import java.lang.reflect.Method;
 
 class Junit4TestRunner {
 
     @Test
-    void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+    void runWithReflections() throws Exception {
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
+        final Reflections reflections = new Reflections(
+                new ConfigurationBuilder()
+                        .setUrls(ClasspathHelper.forClass(Junit4Test.class))
+                        .setScanners(new MethodAnnotationsScanner())
+        );
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : reflections.getMethodsAnnotatedWith(MyTest.class)) {
+            method.invoke(junit4Test);
+        }
+    }
+
+    @Test
+    void runWithoutReflections() throws Exception {
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
+
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
