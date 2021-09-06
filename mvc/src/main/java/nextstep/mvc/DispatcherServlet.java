544809d7d290078e1e7924d72b5b1d72343ca2ed
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-07 05:23:21 +0900

    refactor: HandlerAdapter 적용, move 메서드 삭제 및 JspView 활용

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,19 +1,19 @@
 package nextstep.mvc;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.controller.tobe.HandlerExecution;
-import nextstep.mvc.view.JspView;
+import nextstep.mvc.handleradapter.AnnotationHandlerAdapter;
+import nextstep.mvc.handleradapter.HandlerAdapter;
+import nextstep.mvc.handleradapter.LegacyHandlerAdapter;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.mvc.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,14 +23,20 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private final List<HandlerMapping> handlerMappings;
+    private final List<HandlerAdapter> handlerAdapters;
 
     public DispatcherServlet() {
         this.handlerMappings = new ArrayList<>();
+        this.handlerAdapters = new ArrayList<>();
     }
 
     @Override
     public void init() {
         handlerMappings.forEach(HandlerMapping::initialize);
+        handlerAdapters.addAll(Arrays.asList(
+                new LegacyHandlerAdapter(),
+                new AnnotationHandlerAdapter()
+        ));
     }
 
     public void addHandlerMapping(HandlerMapping handlerMapping) {
@@ -43,11 +49,10 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             final Object handler = getHandler(request);
-            if (handler instanceof Controller) {
-                handleController(request, response, (Controller) handler);
-                return;
-            }
-            handleHandler(request, response, (HandlerExecution) handler);
+            final HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
+            final ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+            final View view = modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
@@ -62,24 +67,10 @@ public class DispatcherServlet extends HttpServlet {
                 .orElseThrow();
     }
 
-    private void handleController(HttpServletRequest request, HttpServletResponse response, Controller controller) throws Exception {
-        final String viewName = controller.execute(request, response);
-        move(viewName, request, response);
-    }
-
-    private void handleHandler(HttpServletRequest request, HttpServletResponse response, HandlerExecution handlerExecution) throws Exception {
-        final ModelAndView modelAndView = handlerExecution.handle(request, response);
-        final View view = modelAndView.getView();
-        view.render(modelAndView.getModel(), request, response);
-    }
-
-    private void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+    private HandlerAdapter getHandlerAdapter(Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.supports(handler))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("handler에 해당하는 handlerAdapter를 찾을 수 없습니다."));
     }
 }
--- mvc/src/main/java/nextstep/mvc/handleradapter/HandlerAdapter.java
+++ mvc/src/main/java/nextstep/mvc/handleradapter/HandlerAdapter.java
@@ -1,4 +1,4 @@
-package nextstep.mvc;
+package nextstep.mvc.handleradapter;
 
 import nextstep.mvc.view.ModelAndView;
 
