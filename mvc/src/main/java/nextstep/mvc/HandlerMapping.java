73b7686afb5d4263fc4145428e22867525eecd51
Byung Wook Park <bbwwpark@naver.com>
2021-09-06 23:34:26 +0900

    feat: 애노테이션 기반 핸들러(컨트롤러), 어댑터 구현 ControllerScanner 구현 - 애노테이션이 붙어있는 컨트롤러를 찾아서 인스턴스를 생성하는 역할 부여

--- mvc/src/main/java/nextstep/mvc/HandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/HandlerMapping.java
@@ -1,10 +1,12 @@
 package nextstep.mvc;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import nextstep.mvc.controller.tobe.HandlerExecution;
 
 public interface HandlerMapping {
 
-    void initialize();
+    void initialize() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException;
 
-    Object getHandler(HttpServletRequest request);
+    HandlerExecution getHandler(HttpServletRequest request);
 }
---
+++ mvc/src/main/java/nextstep/mvc/view/JsonViewResolver.java
@@ -0,0 +1,14 @@
+package nextstep.mvc.view;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class JsonViewResolver implements ViewResolver {
+
+    @Override
+    public View resolveViewName(String viewName, HttpServletRequest httpServletRequest) throws Exception {
+        if (viewName == null || viewName.isEmpty() || viewName.isBlank()) {
+            return new JsonView();
+        }
+        return null;
+    }
+}
