8dc1bffcd5ab968e3671d365d0e598c3dd31fd80
NewWisdom <wlgp2500@gmail.com>
2021-09-06 22:07:50 +0900

    refactor(annotation-handler-mapping): initialize 메서드 리팩토링

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -25,34 +26,40 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Map<Class<?>, Object> controllers = ControllerScanner.getControllers(basePackage);
+        initializeHandlerExecutions(controllers);
+    }
+
+    private void initializeHandlerExecutions(Map<Class<?>, Object> controllers) {
         try {
-            Map<Class<?>, Object> controllers = ControllerScanner.getControllers(basePackage);
-            for (Class<?> controllerClass : controllers.keySet()) {
+            for (Map.Entry<Class<?>, Object> controllerEntry : controllers.entrySet()) {
+                Class<?> controllerClass = controllerEntry.getKey();
                 Set<Method> methods = ReflectionUtils.getAllMethods(controllerClass, ReflectionUtils.withAnnotation(RequestMapping.class));
-                addHandlerExecutions(controllerClass, methods);
+                addHandlerExecutions(controllers, controllerClass, methods);
             }
         } catch (Exception e) {
             log.error("Annotation Handler Mapping Fail!", e);
         }
     }
 
-    private void addHandlerExecutions(Class<?> controllerClass, Set<Method> methods) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
+    private void addHandlerExecutions(Map<Class<?>, Object> controllers, Class<?> controllerClass, Set<Method> methods) {
         for (Method method : methods) {
             RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
             String url = requestMapping.value();
             RequestMethod[] requestMethods = requestMapping.method();
-            for (RequestMethod requestMethod : requestMethods) {
-                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-                HandlerExecution handlerExecution = createHandlerExecution(controllerClass, method);
+            List<HandlerKey> handlerKeys = getHandlerKeys(url, requestMethods);
+            HandlerExecution handlerExecution = new HandlerExecution(controllers.get(controllerClass), method);
+            for (HandlerKey handlerKey : handlerKeys) {
                 this.handlerExecutions.put(handlerKey, handlerExecution);
-                log.info("Path : {}, Controller : {}", url, controllerClass.getName());
+                log.info("Key : {}, Value : {}", handlerKey, controllerClass);
             }
         }
     }
 
-    private HandlerExecution createHandlerExecution(Class<?> controllerClass, Method method) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
-        Object handler = controllerClass.getConstructor().newInstance();
-        return new HandlerExecution(handler, method);
+    private List<HandlerKey> getHandlerKeys(String url, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
+                .collect(Collectors.toList());
     }
 
     public Object getHandler(HttpServletRequest request) {
