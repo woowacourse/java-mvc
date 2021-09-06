ff3a152a1b150ebb80858ffbffc23a086943f12e
Eugine Park <pkeugine@gmail.com>
2021-09-06 22:32:42 +0900

    test: Junit3TestRunner 완성

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,20 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test test = clazz.getConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            final String methodName = method.getName();
+            if (methodName.startsWith("test")) {
+                method.invoke(test);
+            }
+        }
     }
 }
