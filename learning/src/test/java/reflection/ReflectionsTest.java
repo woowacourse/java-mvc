47b7ebb9370760c492303ac61c087b4bb7a56383
Eugine Park <pkeugine@gmail.com>
2021-09-06 22:31:37 +0900

    test: ReflectionsTest 통과

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,45 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final List<Class<? extends Annotation>> classes = Arrays.asList(
+                Controller.class,
+                Service.class,
+                Repository.class
+        );
+        final List<String> clazz = classes.stream()
+                .map(reflections::getTypesAnnotatedWith)
+                .flatMap(Collection::stream)
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
+
+        assertThat(clazz).contains(
+                "QnaController",
+                "MyQnaService",
+                "JdbcUserRepository",
+                "JdbcQuestionRepository"
+        );
     }
 }
