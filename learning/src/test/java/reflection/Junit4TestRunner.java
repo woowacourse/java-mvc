eec3345ff30ae475bc51a8761ffa0908c9f4425f
sakjung <ssangyu123@gmail.com>
2021-09-07 01:05:27 +0900

    test: reflection 학습테스트

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,12 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Set;
 
 class Junit4TestRunner {
 
@@ -9,5 +15,14 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Constructor<Junit4Test> declaredConstructor = clazz.getDeclaredConstructor();
+        final Junit4Test junit4Test = declaredConstructor.newInstance();
+
+        final Reflections reflections = new Reflections(clazz, new MethodAnnotationsScanner());
+        final Set<Method> methodsAnnotatedWithMyTest = reflections.getMethodsAnnotatedWith(MyTest.class);
+
+        for (Method method : methodsAnnotatedWithMyTest) {
+            method.invoke(junit4Test);
+        }
     }
 }
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,17 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class ReflectionsTest {
 
@@ -14,5 +22,18 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> classesWithControllerAnnotation = reflections.getTypesAnnotatedWith(Controller.class);
+
+        final Set<Class<?>> classesWithServiceAnnotation = reflections.getTypesAnnotatedWith(Service.class);
+
+        final Set<Class<?>> classesWithRepositoryAnnotation = reflections.getTypesAnnotatedWith(Repository.class);
+
+        final Set<Class<?>> allClassesWithAnnotation = Stream.of(classesWithControllerAnnotation, classesWithServiceAnnotation, classesWithRepositoryAnnotation)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+
+        for (final Class<?> clazz : allClassesWithAnnotation) {
+            log.info("class name: " + clazz.getSimpleName());
+        }
     }
 }
