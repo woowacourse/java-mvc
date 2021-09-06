a1f35109931acf081ece73bb4536092d9e82d59a
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-06 20:43:40 +0900

    feat: 메소드 불러오기 테스트

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,30 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
+    void runOneMethod() throws Exception {
+        Class<Junit3Test> clazz = Junit3Test.class;
+        Method method = clazz.getMethod("test1");
+
+        method.invoke(clazz.getConstructor().newInstance());
+        // TODO 하나의 메소드를 타겟하여 실행
+    }
+
+    @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] methods = clazz.getMethods();
 
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                ;
+                method.invoke(clazz.getConstructor().newInstance());
+            }
+        }
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
     }
 }
