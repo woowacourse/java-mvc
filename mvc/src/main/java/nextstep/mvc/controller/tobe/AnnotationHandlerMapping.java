f5d6132474d887176d5a62ed6b7716a7aa650d50
ROOT <junroot0909@gmail.com>
2021-09-07 15:18:05 +0900

    feat: 어노테이션 핸들러매핑 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,7 +1,17 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.checkerframework.checker.units.qual.A;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +21,7 @@ import java.util.Map;
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final Class<RequestMapping> REQUEST_MAPPING_CLASS = RequestMapping.class;
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -21,10 +32,41 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (final Class<?> controllerType : controllerTypes) {
+            initializeFromControllerType(controllerType);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void initializeFromControllerType(final Class<?> controllerType) {
+        try {
+            final Object controller = controllerType.getDeclaredConstructor().newInstance();
+            for (final Method method : controllerType.getDeclaredMethods()) {
+                if (!containsRequestMappingAnnotation(method)) {
+                    continue;
+                }
+                initializeFromMethod(method, controller);
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalStateException(String.format("컨트롤러를 인스턴스화 할 수 없습니다.(%s)", controllerType.getName()));
+        }
+    }
+
+    private boolean containsRequestMappingAnnotation(final Method method) {
+        return Arrays.stream(method.getDeclaredAnnotations())
+            .anyMatch(annotation -> annotation.annotationType().equals(REQUEST_MAPPING_CLASS));
+    }
+
+    private void initializeFromMethod(final Method method, final Object controller) {
+        final RequestMapping annotation = method.getAnnotation(REQUEST_MAPPING_CLASS);
+        for (RequestMethod requestMethod : annotation.method()) {
+            handlerExecutions.put(new HandlerKey(annotation.value(), requestMethod), new HandlerExecution(controller, method));
+        }
+    }
+
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
     }
 }
