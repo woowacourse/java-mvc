e61fb63e0bc5854be6e6484ad7b81a6fa711eba6
ROOT <junroot0909@gmail.com>
2021-09-07 14:18:14 +0900

    test: 리플렉션 학습 테스트 완료

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +9,14 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+            .filter(method -> method.getName().startsWith("test"))
+            .forEach(method -> {
+                try {
+                    method.invoke(new Junit3Test());
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            });
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +11,18 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getMethods()).filter(method -> hasAnnotation(method, MyTest.class))
+        .forEach(method -> {
+            try {
+                method.invoke(new Junit4Test());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private boolean hasAnnotation(final Method method, final Class<?> annotationType) {
+        return Arrays.stream(method.getAnnotations())
+            .anyMatch(annotation -> annotation.annotationType().equals(annotationType));
     }
 }
