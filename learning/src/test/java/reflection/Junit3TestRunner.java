25f9db63a7e238777f1034ffca888e2f7ee4eca3
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-09-07 22:56:27 +0900

    test: 학습테스트 구현

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,21 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test junit3Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            String targetName = "test";
+            if (method.getName().contains(targetName)) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,22 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Objects;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            final MyTest myTestAnnotation = method.getAnnotation(MyTest.class);
+            if (!Objects.isNull(myTestAnnotation)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
