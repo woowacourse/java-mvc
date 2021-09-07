89ff433304e21e54e69957d0313a66dda12168fa
seovalue <seominjeong.dev@gmail.com>
2021-09-08 00:45:48 +0900

    test: reflection learning-test 완료

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +9,11 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method: methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -10,9 +14,22 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
+        Set<Class<?>> classesWithController = reflections.getTypesAnnotatedWith(Controller.class);
+        classesWithController.forEach(
+            c -> log.info(">>> @Controller at {}", c.getSimpleName())
+        );
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> classesWithService = reflections.getTypesAnnotatedWith(Service.class);
+        classesWithService.forEach(
+            c -> log.info(">>> @Service at {}", c.getSimpleName())
+        );
+
+        Set<Class<?>> classesWithRepository = reflections.getTypesAnnotatedWith(Repository.class);
+        classesWithRepository.forEach(
+            c -> log.info(">>> @Repository at {}", c.getSimpleName())
+        );
+
     }
 }
