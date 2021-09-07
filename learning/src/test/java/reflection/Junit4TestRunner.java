07a99326876464eb73d885f21631c5ba66cf1a25
Wannte <T.wannte@gmail.com>
2021-09-08 00:02:03 +0900

    feat: JSPView todo 구현

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,5 @@
 package reflection;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +17,6 @@ class Junit4TestRunner {
                 method.invoke(junit4Test);
             }
         }
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
     }
 }
