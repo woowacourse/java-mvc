90d08c43e206f79e8ae4af7cf0068132dd10ed68
ROOT <junroot0909@gmail.com>
2021-09-07 23:29:33 +0900

    refactor: 어노테이션 스캔 로직 리팩토링

---
+++ mvc/src/main/java/nextstep/mvc/ClassScanner.java
@@ -0,0 +1,44 @@
+package nextstep.mvc;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.xml.transform.Source;
+import org.reflections.Reflections;
+
+public class ClassScanner {
+
+    private final Reflections reflections;
+
+    public ClassScanner(final Object... basePackage) {
+        reflections = new Reflections(basePackage);
+    }
+
+    public Set<Object> scanAllObjectsWithAnnotation(final Class<? extends Annotation> annotationType) {
+        final Set<Class<?>> objectTypes = reflections.getTypesAnnotatedWith(annotationType);
+
+        return objectTypes.stream().map(type -> {
+            try {
+                return type.getDeclaredConstructor().newInstance();
+            } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                throw new IllegalStateException(String.format("컨트롤러를 인스턴스화 할 수 없습니다.(%s)", type.getName()));
+            }
+        }).collect(Collectors.toSet());
+    }
+
+    public Set<Method> scanAllMethodsWithAnnotation(final Object object, final Class<? extends Annotation> annotationType) {
+        final Method[] methods = object.getClass().getDeclaredMethods();
+
+        return Arrays.stream(methods)
+            .filter(method -> containsAnnotation(method, annotationType))
+            .collect(Collectors.toSet());
+    }
+
+    private boolean containsAnnotation(final Method method, final Class<? extends Annotation> annotationType) {
+        return Arrays.stream(method.getDeclaredAnnotations())
+            .anyMatch(annotation -> annotation.annotationType().equals(annotationType));
+    }
+}
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,65 +1,54 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import nextstep.mvc.ClassScanner;
 import nextstep.mvc.HandlerMapping;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final Class<Controller> CONTROLLER_CLASS = Controller.class;
     private static final Class<RequestMapping> REQUEST_MAPPING_CLASS = RequestMapping.class;
 
-    private final Object[] basePackage;
+    private final ClassScanner classScanner;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(Object... basePackage) {
-        this.basePackage = basePackage;
+        this.classScanner = new ClassScanner(basePackage);
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
-        final Reflections reflections = new Reflections(basePackage);
-        final Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
-        for (final Class<?> controllerType : controllerTypes) {
-            initializeFromControllerType(controllerType);
+        final Set<Object> controllers = classScanner.scanAllObjectsWithAnnotation(CONTROLLER_CLASS);
+
+        for (final Object controller : controllers) {
+            final Set<Method> methods = classScanner.scanAllMethodsWithAnnotation(controller, REQUEST_MAPPING_CLASS);
+            initializeFromMethods(methods, controller);
         }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void initializeFromControllerType(final Class<?> controllerType) {
-        try {
-            final Object controller = controllerType.getDeclaredConstructor().newInstance();
-            for (final Method method : controllerType.getDeclaredMethods()) {
-                if (!containsRequestMappingAnnotation(method)) {
-                    continue;
-                }
-                initializeFromMethod(method, controller);
-            }
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new IllegalStateException(String.format("컨트롤러를 인스턴스화 할 수 없습니다.(%s)", controllerType.getName()));
+    private void initializeFromMethods(final Set<Method> methods, final Object controller) {
+        for (final Method method : methods) {
+            initializeFromMethod(method, controller);
         }
-    }
-
-    private boolean containsRequestMappingAnnotation(final Method method) {
-        return Arrays.stream(method.getDeclaredAnnotations())
-            .anyMatch(annotation -> annotation.annotationType().equals(REQUEST_MAPPING_CLASS));
     }
 
     private void initializeFromMethod(final Method method, final Object controller) {
         final RequestMapping annotation = method.getAnnotation(REQUEST_MAPPING_CLASS);
-        for (RequestMethod requestMethod : annotation.method()) {
-            handlerExecutions.put(new HandlerKey(annotation.value(), requestMethod), new HandlerExecution(controller, method));
+        final String url = annotation.value();
+
+        for (final RequestMethod requestMethod : annotation.method()) {
+            handlerExecutions.put(new HandlerKey(url, requestMethod), new HandlerExecution(controller, method));
         }
     }
 
