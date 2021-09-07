f15b3c298c49fd33cbc3103debf1ad692cba86cf
Hyeon9mak <jinha3507@gmail.com>
2021-09-08 08:19:27 +0900

    feat: Annotation Based MVC 동작 구현
    AnnotationHandlerMappingTest 통과

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MVC 프레임워크 구현하기
-- [ ] `AnnotationHandlerMapping.initialize` 메서드 완성
+- [x] `AnnotationHandlerMapping.initialize` 메서드 완성
+- [x] `AnnotationHandlerMappingTest` 테스트 통과 시키기
+- [ ] `AnnotationHandlerMapping` 리팩토링
+- [ ] `DispatcherServlet` 리팩토링
 - [ ] `AnnotationHandlerAdapter` 클래스 완성
-- [ ] `AnnotationHandlerMappingTest` 테스트 통과 시키기
 - [ ] Legacy MVC도 Annotation Based MVC와 함께 동작하도록 하기
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Map.Entry;
 import java.util.Set;
 import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.controller.ControllerScanner;
 import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +34,22 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         try {
             ControllerScanner controllerScanner = new ControllerScanner(new Reflections(basePackage));
             Map<Class<?>, Object> controllers = controllerScanner.getControllers();
-            
+
+            // TODO: 일급 컬렉션 분리
+            for (Class<?> aClass : controllers.keySet()) {
+                Set<Method> allMethods = ReflectionUtils.getAllMethods(aClass, ReflectionUtils.withAnnotation(RequestMapping.class));
+                for (Method method : allMethods) {
+
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    for (RequestMethod httpMethod : requestMapping.method()) {
+                        HandlerKey handlerKey = new HandlerKey(requestMapping.value(), httpMethod);
+                        HandlerExecution handlerExecution = new HandlerExecution(method, controllers.get(aClass));
+                        handlerExecutions.put(handlerKey, handlerExecution);
+                    }
+
+                }
+            }
+
             LOG.info("Initialized AnnotationHandlerMapping!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,6 +58,10 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     @Override
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        String uri = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
