89f3a2b9ef01942d55f021c720474fdd14aa833d
seovalue <seominjeong.dev@gmail.com>
2021-09-08 06:36:10 +0900

    feat: AnnotationHandlerMapping#initialize, getHandler 메서드 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,11 +2,16 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +26,42 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Object> controllers = new HashSet<>();
+        for (Class<?> controllerType : controllerTypes) {
+            try {
+                controllers.add(controllerType.getDeclaredConstructor().newInstance());
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                log.info("AnnotationHandlerMapping#initialize, 인스턴스화를 할 수 없습니다.");
+            }
+        }
+
+        for (Object controller : controllers) {
+            Set<Method> methods = new HashSet<>();
+            Method[] declaredMethods = controller.getClass().getDeclaredMethods();
+
+            // controller에 선언되어있는 method들을 methods에 담는다.
+            for (Method method : declaredMethods) {
+                if (Arrays.stream(method.getDeclaredAnnotations()).anyMatch(annotation -> annotation.annotationType().equals(RequestMapping.class))){
+                    methods.add(method);
+                }
+            }
+
+            // RequestMapping 어노테이션이 붙은 annotation의 uri와 value를 가져와서 handlerExecutions에 담는다.
+            for (Method method: methods) {
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                String uri = annotation.value();
+
+                for (RequestMethod requestMethod: annotation.method()) {
+                    handlerExecutions.put(new HandlerKey(uri, requestMethod), new HandlerExecution(controller, method));
+                }
+            }
+        }
     }
 
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,22 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.view.ModelAndView;
+
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Object controller;
+    private final Method method;
+
+    public HandlerExecution(Object controller, Method method) {
+        this.controller = controller;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
