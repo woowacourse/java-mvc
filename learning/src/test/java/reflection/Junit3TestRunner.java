882fb71ea1d609a1c46c7b4c7913c665bab9d703
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-08 11:44:42 +0900

    refactor: Junit3TestRunner 리팩토링

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,7 +2,6 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
@@ -13,16 +12,17 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         final Class<Junit3Test> clazz = Junit3Test.class;
-        final Constructor<Junit3Test> declaredConstructor = clazz.getDeclaredConstructor();
-        final Junit3Test junit3TestInstance = declaredConstructor.newInstance();
+
         final Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            invokeMethodIfNameStartsWith( "test", clazz, method);
+        }
+    }
 
-        final List<Method> testMethods = Arrays.stream(methods)
-                .filter(method -> method.getName().startsWith("test"))
-                .collect(Collectors.toList());
-
-        for (Method testMethod : testMethods) {
-            testMethod.invoke(junit3TestInstance);
+    private void invokeMethodIfNameStartsWith(String methodNamePrefix, Class<?> clazz, Method method) throws Exception {
+        final Object instance = clazz.getDeclaredConstructor().newInstance();
+        if (method.getName().startsWith(methodNamePrefix)) {
+            method.invoke(instance);
         }
     }
 }
