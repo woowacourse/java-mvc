531e5d19d0b4f39b08c6af78a26742e9fd7423c7
Seed <hsik0225@gmail.com>
2021-09-08 21:19:30 +0900

    test: Junit3, 4 학습 테스트 작성

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,38 @@
 package reflection;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // given
+        final ByteArrayOutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        Class<Junit3Test> clazz = Junit3Test.class;
+        final List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                                                    .filter(method -> method.getName().startsWith("test"))
+                                                    .collect(Collectors.toList());
+
+        final Junit3Test junit3Test = clazz.getConstructor().newInstance();
+
+        // when
+        for (Method method : methods) {
+            method.invoke(junit3Test);
+        }
+
+        // then
+        assertThat(captor.toString().trim()).isEqualTo("Running Test1\nRunning Test2");
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,38 @@
 package reflection;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // given
+        final ByteArrayOutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
+        Class<Junit4Test> clazz = Junit4Test.class;
+        final List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                                           .filter(method -> method.isAnnotationPresent(MyTest.class))
+                                           .collect(Collectors.toList());
+
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
+
+        // when
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
+
+        // then
+        assertThat(captor.toString().trim()).isEqualTo("Running Test1\nRunning Test2");
     }
 }
