68d2416479b8156b6457a412ac7909affd7cee4a
Hyeon9mak <jinha3507@gmail.com>
2021-09-08 15:22:14 +0900

    refactor: DispatcherServlet 책임 분리
    HandlerAdapters, HandlerMappings 일급 컬렉션 도입
    ModelAndView가 렌더링을 진행하도록 변경

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 - [x] `AnnotationHandlerMapping.initialize` 메서드 완성
 - [x] `AnnotationHandlerMappingTest` 테스트 통과 시키기
 - [ ] `AnnotationHandlerMapping` 리팩토링
-- [ ] `DispatcherServlet` 리팩토링
-- [ ] `AnnotationHandlerAdapter` 클래스 완성
+- [x] `DispatcherServlet` 리팩토링
+- [x] `AnnotationHandlerAdapter` 클래스 완성
 - [ ] Legacy MVC도 Annotation Based MVC와 함께 동작하도록 하기
+
+- DispatcherServlet에선 Controller가 redirect 해야할 경로를 줬건 말건 알바 아니지 않을까?
+  - 그건 View의 '정보'를 넘겨 받은 View 단에서 알아서 Model 참고해서 렌더링 해야지?
--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,34 +1,23 @@
 package nextstep.mvc;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DispatcherServlet extends HttpServlet {
 
-    private static final long SERIAL_VERSION_UID = 1L;
     private static final Logger LOG = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> handlerMappings;
-    private final List<HandlerAdapter> handlerAdapters;
+    private final HandlerMappings handlerMappings;
+    private final HandlerAdapters handlerAdapters;
 
     public DispatcherServlet() {
-        this.handlerMappings = new ArrayList<>();
-        this.handlerAdapters = new ArrayList<>();
-    }
-
-    @Override
-    public void init() {
-        handlerMappings.forEach(HandlerMapping::initialize);
+        this.handlerMappings = new HandlerMappings();
+        this.handlerAdapters = new HandlerAdapters();
     }
 
     public void addHandlerMapping(HandlerMapping handlerMapping) {
@@ -40,34 +29,23 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
+    public void init() {
+        handlerMappings.initialize();
+    }
+
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         LOG.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
-        // TODO: 일급 컬렉션, HandlerMapping과 HandlerAdapter 조회 동작 균형 맞추기, move 메서드 제거
         try {
-            Object handler = getHandler(request);
-            HandlerAdapter handlerAdapter = getAdapter(handler);
+            Object handler = handlerMappings.getHandler(request);
+            HandlerAdapter handlerAdapter = handlerAdapters.getAdapter(handler);
 
             ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
-            modelAndView.getView().render(modelAndView.getModel(), request, response);
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             LOG.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private Object getHandler(HttpServletRequest request) {
-        return handlerMappings.stream()
-            .map(handlerMapping -> handlerMapping.getHandler(request))
-            .filter(Objects::nonNull)
-            .findFirst()
-            .orElseThrow();
-    }
-
-    private HandlerAdapter getAdapter(Object handler) {
-        return handlerAdapters.stream()
-            .filter(handlerAdapter -> handlerAdapter.supports(handler))
-            .findFirst()
-            .orElseThrow();
     }
 }
