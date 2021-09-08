34c971e90f5fa70b7b0c98c51b22545943373c23
Seed <hsik0225@gmail.com>
2021-09-08 21:29:03 +0900

    test: 어노테이션이 붙은 컨트롤러 검색 테스트 코드 작성

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,19 @@
 package reflection;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
+
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReflectionsTest {
 
@@ -11,8 +21,23 @@ class ReflectionsTest {
 
     @Test
     void showAnnotationClass() throws Exception {
+
+        // given
+        final ByteArrayOutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        // when
+        Set<Class<?>> controllerAnnotated = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> serviceAnnotated = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositoryAnnotated = reflections.getTypesAnnotatedWith(Repository.class);
+
+        controllerAnnotated.forEach(clazz -> log.info("Class annotated controller : {}", clazz.getSimpleName()));
+        serviceAnnotated.forEach(clazz -> log.info("Class annotated service : {}", clazz.getSimpleName()));
+        repositoryAnnotated.forEach(clazz -> log.info("Class annotated repository : {}", clazz.getSimpleName()));
+
+        // then
+        assertThat(captor.toString().trim()).contains("QnaController", "MyQnaService", "JdbcUserRepository", "JdbcQuestionRepository");
     }
 }
