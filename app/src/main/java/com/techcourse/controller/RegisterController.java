4ab8cb32f4180b9636c5aa5e340d39b474d8c6f1
seovalue <seominjeong.dev@gmail.com>
2021-09-08 09:35:34 +0900

    refactor: RegisterController#save session 등록하도록 수정

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -4,6 +4,7 @@ import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
@@ -19,20 +20,23 @@ public class RegisterController {
     private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
-        log.info("Method: POST, Request URI: {}", req.getRequestURI());
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Method: POST, Request URI: {}", request.getRequestURI());
         final User user = new User(2,
-                req.getParameter("account"),
-                req.getParameter("password"),
-                req.getParameter("email"));
+                request.getParameter("account"),
+                request.getParameter("password"),
+                request.getParameter("email"));
         InMemoryUserRepository.save(user);
+
+        final HttpSession session = request.getSession();
+        session.setAttribute(UserSession.SESSION_KEY, user);
 
         return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) {
-        log.info("Method: GET, Request URI: {}", req.getRequestURI());
+    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Method: GET, Request URI: {}", request.getRequestURI());
         return new ModelAndView(new JspView("/register.jsp"));
     }
 }
