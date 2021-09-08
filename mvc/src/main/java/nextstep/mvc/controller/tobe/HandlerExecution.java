96f79ce2116d126203449f8cfa0331c0f6d44472
Jinho <jh8579@gmail.com>
2021-09-08 15:12:17 +0900

    feat: HandlerExecution 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -1,12 +1,21 @@
 package nextstep.mvc.controller.tobe;
 
+import java.lang.reflect.Method;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
+    private final Object declaredObject;
+    private final Method method;
+
+    public HandlerExecution(Object declaredObject, Method method) {
+        this.declaredObject = declaredObject;
+        this.method = method;
+    }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(declaredObject, request, response);
     }
 }
