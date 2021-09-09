3f10975ba4aea2467ed08a95c75557d79486dce7
sjpark-dev <sjpark.dev@gmail.com>
2021-09-09 11:40:41 +0900

    feat: ControllerScanner 테스트 코드 작성

---
+++ mvc/src/test/java/samples/TestController3.java
@@ -0,0 +1,33 @@
+package samples;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class TestController3 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController3.class);
+
+    @RequestMapping(value = "/get-test", method = RequestMethod.GET)
+    public ModelAndView findUserId(HttpServletRequest request, HttpServletResponse response) {
+        LOG.info("test controller get method");
+        final ModelAndView modelAndView = new ModelAndView(new JspView(""));
+        modelAndView.addObject("id", request.getAttribute("id"));
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/post-test", method = RequestMethod.POST)
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        LOG.info("test controller post method");
+        final ModelAndView modelAndView = new ModelAndView(new JspView(""));
+        modelAndView.addObject("id", request.getAttribute("id"));
+        return modelAndView;
+    }
+}
