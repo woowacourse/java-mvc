adb7c09401de42d55df90389bbdd1ec0d0fbefc9
unluckyjung <ybook2012@gmail.com>
2021-09-10 02:11:19 +0900

    feat: 어노테이션 기반의 MVC 프레임워크 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,20 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(final Object instance, final Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
