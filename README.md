90dfe2c69e9a45f5703be9973976c0aadf88c1c3
RinSabbit <celena1004@naver.com>
2021-09-09 22:13:20 +0900

    refactor: LoginController를 어노테이션 기반 컨트롤러로 변경

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MVC 프레임워크 구현하기
 
-- [ ] Annotation Based MVC Framework 구현
+- [X] Annotation Based MVC Framework 구현
   - [X] HTTP 메서드와 URL을 매핑 조건에 포함
   - [X] AnnotationHandlerMapping 테스트 통과
-  - [ ] JspView Todo (DispatcherServlet 참고)
-- [ ] Legacy MVC와 Annotation Based MVC 통합
+  - [X] JspView Todo (DispatcherServlet 참고)
+- [X] Legacy MVC와 Annotation Based MVC 통합
--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,14 +1,12 @@
 package com.techcourse;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-import nextstep.mvc.controller.tobe.HandlerMapping;
 import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.controller.asis.ForwardController;
+import nextstep.mvc.controller.tobe.HandlerMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +18,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
 
         log.info("Initialized Handler Mapping!");
