795574f518af1a2eb74a0cc137a08815ed796648
unluckyjung <ybook2012@gmail.com>
2021-09-10 01:08:08 +0900

    feat: 리플랙션을 이용해 컨트롤러 어노테이션이 붙은 클래스를 찾는 기능구현

---
+++ mvc/src/test/java/nextstep/mvc/controller/ControllerScannerTest.java
@@ -0,0 +1,29 @@
+package nextstep.mvc.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ControllerScannerTest {
+
+    @DisplayName("패키지 경로를주면, @Controller 어노테이션이 붙은 클래스들을 찾아서 보관한다.")
+    @Test
+    void controllerScannerTest() {
+        ControllerScanner controllerScanner = new ControllerScanner("samples");
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        annotationClassesCheck(controllers.keySet(), List.of("TestController"));
+    }
+
+    private void annotationClassesCheck(final Set<Class<?>> typesAnnotatedWithController, final List<String> classNames) {
+        List<String> controllerNames = typesAnnotatedWithController.stream()
+            .map(Class::getSimpleName)
+            .collect(Collectors.toList());
+
+        assertThat(controllerNames).hasSameSizeAs(classNames).hasSameElementsAs(classNames);
+    }
+}
