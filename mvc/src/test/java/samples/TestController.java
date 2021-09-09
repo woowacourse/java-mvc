8e059b81387ea854d3182319fbd38b15e5a32ff1
unluckyjung <ybook2012@gmail.com>
2021-09-10 02:58:21 +0900

    feat: jspView 구현

--- mvc/src/test/java/samples/TestController.java
+++ mvc/src/test/java/samples/TestController.java
@@ -13,20 +13,20 @@ import org.slf4j.LoggerFactory;
 @Controller
 public class TestController {
 
-    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping(value = "/get-test", method = RequestMethod.GET)
-    public ModelAndView findUserId(HttpServletRequest request, HttpServletResponse response) {
-        log.info("test controller get method");
-        final ModelAndView modelAndView = new ModelAndView(new JspView(""));
+    public ModelAndView findUserId(final HttpServletRequest request, final HttpServletResponse response) {
+        LOGGER.info("test controller get method");
+        ModelAndView modelAndView = new ModelAndView(new JspView("/post-test.jsp"));
         modelAndView.addObject("id", request.getAttribute("id"));
         return modelAndView;
     }
 
     @RequestMapping(value = "/post-test", method = RequestMethod.POST)
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
-        log.info("test controller post method");
-        final ModelAndView modelAndView = new ModelAndView(new JspView(""));
+    public ModelAndView save(final HttpServletRequest request, final HttpServletResponse response) {
+        LOGGER.info("test controller post method");
+        ModelAndView modelAndView = new ModelAndView(new JspView("/post-test.jsp"));
         modelAndView.addObject("id", request.getAttribute("id"));
         return modelAndView;
     }
