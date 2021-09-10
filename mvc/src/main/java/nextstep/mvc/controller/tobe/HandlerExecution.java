c95f64e967beb61303bc10154514792432dc0deb
Aaron <whilter08@gmail.com>
2021-09-10 14:43:25 +0900

    refactor: 컨트롤러 인스턴스 캐싱 후 재사용하도록 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -8,13 +8,22 @@ import nextstep.mvc.view.ModelAndView;
 public class HandlerExecution {
 
     private final Method method;
+    private final Object controller;
 
     public HandlerExecution(Method method) {
         this.method = method;
+        this.controller = getController(method);
     }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Object controller = this.method.getDeclaringClass().getConstructor().newInstance();
-        return (ModelAndView) this.method.invoke(controller, request, response);
+        return (ModelAndView) method.invoke(controller, request, response);
+    }
+
+    private Object getController(Method method) {
+        try {
+            return method.getDeclaringClass().getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Method로부터 Controller를 찾을 수 없습니다.");
+        }
     }
 }
