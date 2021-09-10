b51495c9d142d52c914373164772ab69744dc0ad
rok93 <goodboy302@naver.com>
2021-09-10 20:28:23 +0900

    test: Junit4Test RunningTest 추가

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -11,9 +11,9 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
         for (Method method : clazz.getMethods()) {
             String methodName = method.getName();
+            Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
             if (methodName.startsWith("test")) {
                 System.out.println("실행한 메서드: " + clazz.getSimpleName() + "#" + methodName);
-                Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
                 method.invoke(junit3Test);
             }
         }
