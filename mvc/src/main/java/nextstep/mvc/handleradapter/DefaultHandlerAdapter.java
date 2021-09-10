5719d3f3f19e22804aff11745177415f952cd826
ROOT <junroot0909@gmail.com>
2021-09-10 12:37:15 +0900

    refactor: 메소드 분리

--- mvc/src/main/java/nextstep/mvc/handleradapter/DefaultHandlerAdapter.java
+++ mvc/src/main/java/nextstep/mvc/handleradapter/DefaultHandlerAdapter.java
@@ -22,15 +22,17 @@ public class DefaultHandlerAdapter implements HandlerAdapter {
 
     @Override
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        final HandlerAdapter handlerAdapter = findHandlerAdapter(handler);
+        try {
+            return handlerAdapter.handle(request, response, handler);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("핸들러 처리 중에 예외가 발생했습니다.", exception);
+        }
+    }
+
+    private HandlerAdapter findHandlerAdapter(final Object handler) {
         return DEFAULT_ADAPTERS.stream()
             .filter(adapter -> adapter.supports(handler))
-            .map(adapter -> {
-                try {
-                    return adapter.handle(request, response, handler);
-                } catch (Exception exception) {
-                    throw new IllegalArgumentException("핸들러 처리 중에 예외가 발생했습니다.", exception);
-                }
-            })
             .findAny()
             .orElseThrow(() -> new IllegalStateException(String.format("지원하지 않는 핸들러입니다.(%s)", handler.getClass())));
     }
