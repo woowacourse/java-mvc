cb1e408b95996d857f73d30e3d1b8c011426b4c9
binghe819 <binghe819@gmail.com>
2021-09-10 15:50:44 +0900

    test: 학습 테스트 작성

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,12 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +15,20 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // 생성자를 통한 객체 생성
+        Constructor<?> constructorOfClazz = clazz.getConstructor();
+        Junit4Test junit4Test = (Junit4Test) constructorOfClazz.newInstance();
+
+        // @MyTest 애노테이션을 가진 메서드 추출
+        List<Method> methods = Arrays.stream(clazz.getMethods())
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .collect(Collectors.toList());
+
+        assertThat(methods).hasSize(2);
+
+        // 메서드 실행
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
     }
 }
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,11 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -13,6 +19,22 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        controllers.forEach(controller -> log.info(controller.getCanonicalName()));
+        services.forEach(service -> log.info(service.getCanonicalName()));
+        repositories.forEach(repository -> log.info(repository.getCanonicalName()));
+
+        assertThat(controllers)
+            .extracting("name")
+            .containsExactly("examples.QnaController");
+        assertThat(services)
+            .extracting("name")
+            .containsExactly("examples.MyQnaService");
+        assertThat(repositories)
+            .extracting("name")
+            .contains("examples.JdbcQuestionRepository", "examples.JdbcUserRepository");
     }
 }
