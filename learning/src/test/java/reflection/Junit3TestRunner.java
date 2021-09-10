c8f4e511451dcf36e197bcccdafed00e42498911
rok93 <goodboy302@naver.com>
2021-09-10 17:10:48 +0900

    test: Junit3 RunningTest 추가

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,20 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            String methodName = method.getName();
+            if (methodName.startsWith("test")) {
+                System.out.println("실행한 메서드: " + clazz.getSimpleName() + "#" + methodName);
+                Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
