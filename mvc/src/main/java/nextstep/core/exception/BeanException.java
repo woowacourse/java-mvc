f08d8e91bbe77d01e261449c6a57714ba158998f
bomin <bomin_93@naver.com>
2021-09-10 19:39:44 +0900

    refactor: 필요없는 변수 삭제

--- mvc/src/main/java/nextstep/core/exception/BeanException.java
+++ mvc/src/main/java/nextstep/core/exception/BeanException.java
@@ -2,8 +2,6 @@ package nextstep.core.exception;
 
 public class BeanException extends RuntimeException{
 
-    private static final String MESSAGE = "해당하는 빈을 찾을 수 없습니다.";
-
     public BeanException(BeanErrorMessage message) {
         super(message.getMessage());
     }
