325a468ae1c8a2e4f2c983b8024bc97f8f6e6f0f
youngbin <thisisyoungbin@gmail.com>
2021-09-10 18:50:43 +0900

    test: Junit3, 4 테스트 구현

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getMethods()).filter(method -> method.isAnnotationPresent(MyTest.class)).forEach(method -> {
+            try {
+                method.invoke(junit4Test);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
