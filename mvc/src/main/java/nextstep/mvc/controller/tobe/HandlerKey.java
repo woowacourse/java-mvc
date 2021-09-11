bd3a045c842afe44a47feb95af82eba4bfe66251
rok93 <goodboy302@naver.com>
2021-09-11 22:26:31 +0900

    feat: AnnotationHandlerMappingTest 테스트 통과하도록 AnnotationHandlerMapping의 initilize 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerKey.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerKey.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.controller.tobe;
 
+import jakarta.servlet.http.HttpServletRequest;
 import nextstep.web.support.RequestMethod;
 
 import java.util.Objects;
@@ -12,6 +13,11 @@ public class HandlerKey {
     public HandlerKey(String url, RequestMethod requestMethod) {
         this.url = url;
         this.requestMethod = requestMethod;
+    }
+
+    public static HandlerKey of(HttpServletRequest request) {
+        //todo: request.getMethod() 의 결과가 GET, POST와 같이 들어오는지 확인할 것!
+        return new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
     }
 
     @Override
