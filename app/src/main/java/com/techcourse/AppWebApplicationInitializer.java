cc38407b7ac600564c2d29ef5cb3613701a297e9
김주원 <kjw11077@naver.com>
2021-09-12 01:30:56 +0900

    feat: RegisterController 어노테이션 기반 컨트롤러로 변경

--- app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
+++ app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
@@ -14,11 +14,13 @@ public class AppWebApplicationInitializer implements WebApplicationInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(AppWebApplicationInitializer.class);
 
+    private static final String BASE_PACKAGE = "com.techcourse";
+
     @Override
     public void onStartup(ServletContext servletContext) {
         final DispatcherServlet dispatcherServlet = new DispatcherServlet();
         dispatcherServlet.addHandlerMapping(new ManualHandlerMapping());
-        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping());
+        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping(BASE_PACKAGE));
         dispatcherServlet.addHandlerAdapters(new ControllerHandlerAdaptor());
         dispatcherServlet.addHandlerAdapters(new AnnotationHandlerAdaptor());
 
