e26536e26d437fd1644eafa541b6c629e6f0552b
pomo <bosl95@naver.com>
2021-09-13 07:53:27 +0900

    chore: 불필요한 출력문 제거

--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -15,18 +15,15 @@ class ReflectionsTest {
     private static final Logger LOG = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
-        System.out.println("********* CONTROLLER ***********");
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
         controllers.forEach(controller -> LOG.info(String.format("%s : %s", controller.getName(), controller.getAnnotation(Controller.class))));
 
-        System.out.println("********* SERVICE ***********");
         Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
         services.forEach(service -> LOG.info(String.format("%s : %s", service.getName(), service.getAnnotation(Service.class))));
 
-        System.out.println("********* REPOSITORY ***********");
         Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
         repositories.forEach(repository -> LOG.info(String.format("%s : %s", repository.getName(), repository.getAnnotation(Repository.class))));
     }
