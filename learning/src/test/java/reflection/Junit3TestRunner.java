c503017bed1d46672524dc65d487020e3e062ae3
seunghyun <ggyool@naver.com>
2021-09-12 14:50:37 +0900

    test: 학습 테스트 수행

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,26 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class Junit3TestRunner {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
+class Junit3TestRunner {
+    
+    @DisplayName("Junit3Test에서 test로 시작하는 메소드 실행")
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Constructor<Junit3Test> constructor = clazz.getConstructor();
+        Junit3Test junit3Test = constructor.newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,25 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 class Junit4TestRunner {
 
+    @DisplayName("Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Constructor<Junit4Test> constructor = clazz.getConstructor();
+        Junit4Test junit4Test = constructor.newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
