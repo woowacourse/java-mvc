f7c55e624a282a21a1518796c84995f356cc809e
younghoonkwon <younghoon.kwon91@gmail.com>
2021-09-12 22:03:31 +0900

    feat: annotation 기반 컨트롤러 구현

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -1,4 +1,75 @@
 package com.techcourse.controller;
 
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest req, HttpServletResponse res) {
+        if (UserSession.isLoggedIn(req.getSession())) {
+            return "redirect:/index.jsp";
+        }
+
+        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+                .map(user -> {
+                    log.info("User : {}", user);
+                    return login(req, user);
+                })
+                .orElse("redirect:/401.jsp");
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest req, HttpServletResponse res) {
+        final HttpSession session = req.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public String loginView(HttpServletRequest req, HttpServletResponse res) {
+        return UserSession.getUserFrom(req.getSession())
+                .map(user -> {
+                    log.info("logged in {}", user.getAccount());
+                    return "redirect:/index.jsp";
+                })
+                .orElse("/login.jsp");
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(HttpServletRequest req, HttpServletResponse res) {
+        final User user = new User(2,
+                req.getParameter("account"),
+                req.getParameter("password"),
+                req.getParameter("email"));
+        InMemoryUserRepository.save(user);
+        return "redirect:/index.jsp";
+    }
+
+    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
+    public String registerView(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        return "/register.jsp";
+    }
+
+
+    private String login(HttpServletRequest request, User user) {
+        if (user.checkPassword(request.getParameter("password"))) {
+            final HttpSession session = request.getSession();
+            session.setAttribute(UserSession.SESSION_KEY, user);
+            return "redirect:/index.jsp";
+        }
+
+        return "redirect:/401.jsp";
+    }
 }
