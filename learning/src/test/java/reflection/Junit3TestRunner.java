bdbab8174cba3c1bcf7489a4c27a908e7e05ed11
younghoonkwon <younghoon.kwon91@gmail.com>
2021-09-12 18:51:09 +0900

    feat: 학습테스트 완료

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,26 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+
+        List<Method> testMethods = Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        for (Method testMethod : testMethods) {
+            testMethod.invoke(junit3Test);
+        }
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,25 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit4TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run1() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
+        Method[] methods = clazz.getMethods();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> testMethods = Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        for (Method testMethod : testMethods) {
+            testMethod.invoke(junit4Test);
+        }
     }
 }
