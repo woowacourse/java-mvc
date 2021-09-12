9db61da907dc64e5eb0593a161bf8f701d9b806d
seunghyun <ggyool@naver.com>
2021-09-12 15:51:52 +0900

    feat: HandlerExecutionAdapter 구현

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # 1단계 - MVC 프레임워크 구현하기
 
+## MVC 패키지
+
 - [x] **AnnotationHandlerMapping 구현하기**
-  - **handler 등록 과정**    
-  - 클래스의 @Contoller 와 @RequestMapping 을 찾는다.
-  - 두 정보를 참고하여 default url 을 구한다.
-  - 클래스 안에서 @RequestMapping 가 붙은 메서드를 찾는다.
-  - 어노테이션의 value 를 참고하여 url 를 만들고 method 를 참고하여 HandlerExecution 을 등록한다. 
-  
+    - **handler 등록 과정**
+    - 클래스의 @Contoller 와 @RequestMapping 을 찾는다.
+    - 두 정보를 참고하여 default url 을 구한다.
+    - 클래스 안에서 @RequestMapping 가 붙은 메서드를 찾는다.
+    - 어노테이션의 value 를 참고하여 url 를 만들고 method 를 참고하여 HandlerExecution 을 등록한다.
+- [x] **HandlerExecutionAdapter 구현하기**
+- [ ] **DispatcherServlet 에서 HandlerExecution 과 Controller 를 찾는 분기 만들기**
+
+## APP 패키지
+
 - [ ] DispatcherServlet 에 AnnotationHandlerMapping 등록하기
+
---
+++ mvc/src/test/java/nextstep/mvc/adapter/HandlerExecutionAdapterTest.java
@@ -0,0 +1,44 @@
+package nextstep.mvc.adapter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+class HandlerExecutionAdapterTest {
+
+    @DisplayName("HandlerExecutionAdapter 는 handlerExecution 을 지원한다.")
+    @Test
+    void supports() {
+        // given
+        HandlerExecution handlerExecution = mock(HandlerExecution.class);
+
+        // when, then
+        HandlerExecutionAdapter handlerAdapter = new HandlerExecutionAdapter();
+        assertThat(handlerAdapter.supports(handlerExecution)).isTrue();
+    }
+
+    @DisplayName("HandlerExecutionAdapter 로 handlerExecution 를 실행한다.")
+    @Test
+    void handle() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HandlerExecution handlerExecution = mock(HandlerExecution.class);
+
+        HandlerExecutionAdapter handlerAdapter = new HandlerExecutionAdapter();
+
+        // when
+        handlerAdapter.handle(request, response, handlerExecution);
+
+        // then
+        then(handlerExecution).should(times(1))
+                .handle(request, response);
+    }
+}
