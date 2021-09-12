3a559a0f3733db9cf8722ddac6df55d092d71732
younghoonkwon <younghoon.kwon91@gmail.com>
2021-09-12 21:43:17 +0900

    feat: annotation 기반 동작 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,9 +2,14 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +27,19 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        try {
+            Reflections reflections = new Reflections(basePackage, new MethodAnnotationsScanner());
+            for (Method method : reflections.getMethodsAnnotatedWith(RequestMapping.class)) {
+                final RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                final HandlerKey handlerKey = new HandlerKey(annotation.value(), RequestMethod.valueOf(annotation.method()[0].name()));
+                HandlerExecution handlerExecution = new HandlerExecution(method.getDeclaringClass().getConstructor().newInstance(), method);
+                handlerExecutions.put(handlerKey, handlerExecution);
+                log.debug("Initialize Key: {}, Execution: {}", handlerKey, handlerExecution);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Object getHandler(HttpServletRequest request) {
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -4,9 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
+    }
+
+    @Override
+    public String toString() {
+        return "HandlerExecution{" +
+                "instance=" + instance +
+                ", method=" + method +
+                '}';
     }
 }
