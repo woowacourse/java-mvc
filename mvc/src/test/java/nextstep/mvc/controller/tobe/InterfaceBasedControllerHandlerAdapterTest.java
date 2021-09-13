b9b850c50c9c6a538cf96a832f0fc338d739bfcf
Kimun Kim <kkm97351@gmail.com>
2021-09-14 01:45:35 +0900

    test: HandlerAdapter 테스트 코드 작성

--- mvc/src/test/java/nextstep/mvc/controller/tobe/InterfaceBasedControllerHandlerAdapterTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/InterfaceBasedControllerHandlerAdapterTest.java
@@ -1,7 +1,55 @@
 package nextstep.mvc.controller.tobe;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 class InterfaceBasedControllerHandlerAdapterTest {
-    // todo 테스트 작성
+    @Test
+    @DisplayName("HandlerExecution 객체를 찾아 handle() 메소드를 실행할 수 있다.")
+    void getHandlerAndExecute() throws Exception {
+        // given
+        List<Object> handlers = new ArrayList<>();
+
+        Controller mockController = mock(Controller.class);
+        String expectedViewName = "success.jsp";
+        given(mockController.execute(any(HttpServletRequest.class), any(HttpServletResponse.class)))
+                .willReturn(expectedViewName);
+
+        HandlerExecution mockHandlerExecution = mock(HandlerExecution.class);
+        ;
+        given(mockHandlerExecution.handle(any(HttpServletRequest.class), any(HttpServletResponse.class)))
+                .willReturn(new ModelAndView(new JspView("fail")));
+
+        handlers.add(mockController);
+        handlers.add(mockHandlerExecution);
+
+        // when
+        HandlerAdapter handlerExecutionHandlerAdaptor = new InterfaceBasedControllerHandlerAdapter();
+        Object handler = handlers.stream()
+                .filter(handlerExecutionHandlerAdaptor::supports)
+                .findFirst()
+                .orElseThrow();
+
+        ModelAndView actual = handlerExecutionHandlerAdaptor.handle(mock(HttpServletRequest.class), mock(HttpServletResponse.class), handler);
+
+        // then
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringActualNullFields()
+                .isEqualTo(new ModelAndView(new JspView(expectedViewName)));
+    }
 }
