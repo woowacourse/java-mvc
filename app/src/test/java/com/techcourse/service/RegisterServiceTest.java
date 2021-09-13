71634a679f44659a9ef7e95618409a25a301ae8f
Hyeon9mak <jinha3507@gmail.com>
2021-09-13 11:59:13 +0900

    test: RegisterSercie 테스트 클래스 레벨 DisplayName 어노테이션 추가

--- app/src/test/java/com/techcourse/service/RegisterServiceTest.java
+++ app/src/test/java/com/techcourse/service/RegisterServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("RegisterService는")
 class RegisterServiceTest {
 
     private RegisterService registerService;
