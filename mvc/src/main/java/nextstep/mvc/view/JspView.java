6636d71e5d55622c6e9a5b57f50c731bcdd81f83
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-14 15:46:47 +0900

    refactor: DispatcherServlet 코드 및 JspView render 코드 리팩토링

--- mvc/src/main/java/nextstep/mvc/view/JspView.java
+++ mvc/src/main/java/nextstep/mvc/view/JspView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -12,19 +13,25 @@ public class JspView implements View {
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
     public static final String REDIRECT_PREFIX = "redirect:";
+    private final String viewName;
 
     public JspView(String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // todo
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
+            return;
+        }
 
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
--- mvc/src/main/java/nextstep/mvc/view/ModelAndView.java
+++ mvc/src/main/java/nextstep/mvc/view/ModelAndView.java
@@ -1,5 +1,8 @@
 package nextstep.mvc.view;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
