2967ed02666a48477b6f9834bdb246600874e13b
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-14 15:04:27 +0900

    feat: Annotation 기반 코드 추가

---
+++ app/src/main/java/com/techcourse/controller/AnnotationLogoutController.java
@@ -0,0 +1,21 @@
+package com.techcourse.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+@Controller
+public class AnnotationLogoutController {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
+        final HttpSession session = request.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return new ModelAndView(new JspView("/"));
+    }
+}
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,11 +2,19 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -22,9 +30,31 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections("controller");
+        Set<Class<?>> controllerClass = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : controllerClass) {
+            try {
+                Object clazz = aClass.getDeclaredConstructor().newInstance();
+                Set<Method> methods = ReflectionUtils.getAllMethods(aClass, ReflectionUtils.withAnnotation(RequestMapping.class));
+                for (Method method : methods) {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    handlerExecutions.put(
+                            new HandlerKey(annotation.value(), annotation.method()[0]),
+                            new HandlerExecution(clazz, method)
+                    );
+                }
+            } catch (InstantiationException | InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public Object getHandler(HttpServletRequest request) {
-        return null;
+        HandlerKey key = new HandlerKey(
+                request.getRequestURI(),
+                RequestMethod.requestMethod(request.getMethod())
+        );
+
+        return handlerExecutions.get(key);
     }
 }
