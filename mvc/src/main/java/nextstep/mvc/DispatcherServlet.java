e202c1f2c8009ffbe2059f9b2ed097dcbc4f527c
Youngbin Kim <67272922+thisisyoungbin@users.noreply.github.com>
2021-09-14 21:21:51 +0900

    [MVC 구현하기 - 1단계] 욘(김영빈) 미션 제출합니다.  (#55)
    * test: Reflection 테스트 구현
    
    * test: ReflectionsTest 구현
    
    * test: Junit3, 4 테스트 구현
    
    * feat: AnnotationHandlerMapping 내의 handlerExcutioins 초기화 기능 구현
    
    * feat: AnnotationHandlerMapping 테스트 통과하도록 handle 메서드 구현
    
    * feat: JspView구현
    
    * feat: Legacy MVC와 Annotation Based MVC 통합
    
    * refactor: TestController 클래스 추가
    
    * feat: test.jsp 파일 추가
    
    * feat: TestController 추가

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -6,7 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.HandlerExecution;
 import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.mvc.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,22 +42,30 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final Controller controller = getController(request);
-            final String viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            Object handler = getHandler(request);
+            if (handler instanceof Controller) {
+                Controller controller = (Controller) handler;
+                String viewName = controller.execute(request, response);
+                move(viewName, request, response);
+            }
+            if (handler instanceof HandlerExecution) {
+                HandlerExecution handlerExecution = (HandlerExecution) handler;
+                final ModelAndView modelAndView = handlerExecution.handle(request, response);
+                final View view = modelAndView.getView();
+                view.render(modelAndView.getModel(), request, response);
+            }
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private Controller getController(HttpServletRequest request) {
+    private Object getHandler(HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
                 .filter(Objects::nonNull)
-                .map(Controller.class::cast)
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(() -> new IllegalArgumentException("올바른 요청이 아닙니다."));
     }
 
     private void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws Exception {
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -4,9 +4,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
+    private final Method method;
+    private final Class<?> controller;
+
+    public HandlerExecution(Method method, Class<?> controller) {
+        this.method = method;
+        this.controller = controller;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        Object controllerInstance = controller.getDeclaredConstructor().newInstance();
+        method.setAccessible(true);
+        return (ModelAndView) method.invoke(controllerInstance, request, response);
     }
 }
