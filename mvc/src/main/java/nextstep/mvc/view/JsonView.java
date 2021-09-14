47a577170960b8fbd74ac74f00add5ce8545b39d
NewWisdom <wlgp2500@gmail.com>
2021-09-14 19:29:30 +0900

    feat(json-view): render 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,31 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Map;
 
+import static nextstep.web.support.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 public class JsonView implements View {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
+        String jsonValue = convertToJsonValue(model);
+        response.getWriter().write(objectMapper.writeValueAsString(jsonValue));
+    }
+
+    private String convertToJsonValue(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Map.Entry<String, ?> entry = model.entrySet().iterator().next();
+            String key = entry.getKey();
+            return objectMapper.writeValueAsString(model.get(key));
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
