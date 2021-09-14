60563b4a3148328206e9477234b830d5a11ab3cd
Jinho <jh8579@gmail.com>
2021-09-14 16:32:50 +0900

    feat: JsonView 구현

---
+++ mvc/src/main/java/nextstep/mvc/view/JsonParser.java
@@ -0,0 +1,11 @@
+package nextstep.mvc.view;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonParser {
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String writeValueAsString(Object object) throws Exception{
+        return objectMapper.writeValueAsString(object);
+    }
+}
