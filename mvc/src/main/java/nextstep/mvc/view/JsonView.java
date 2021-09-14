9995a8008dded6267309c7ae4f155cd94c041b95
Jinho <jh8579@gmail.com>
2021-09-14 22:23:37 +0900

    fix: model 객체가 1개인 경우 그대로 반환하도록 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -13,10 +13,19 @@ public class JsonView implements View {
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String bodyJson = JsonParser.writeValueAsString(model);
+        String bodyJson = responseToJson(model);
         response.setContentType(APPLICATION_JSON_UTF8_VALUE);
 
         ServletOutputStream outputStream = response.getOutputStream();
         outputStream.write(bodyJson.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String responseToJson(Map<String, ?> model) throws Exception {
+        if (model.size() == 1) {
+            for (Object value : model.values()) {
+                return JsonParser.writeValueAsString(value);
+            }
+        }
+        return JsonParser.writeValueAsString(model);
     }
 }
