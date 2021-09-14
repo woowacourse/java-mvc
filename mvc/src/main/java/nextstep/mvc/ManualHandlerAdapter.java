ce19a331ad5e8dcdcd49c046bea88184c0f2a10d
youngbin <thisisyoungbin@gmail.com>
2021-09-14 23:36:59 +0900

    feat: ManualHandlerAdapter & AnnotationHandlerAdapter 클래스 구현

---
+++ mvc/src/main/java/nextstep/mvc/ManualHandlerAdapter.java
@@ -0,0 +1,22 @@
+package nextstep.mvc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+
+public class ManualHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Controller controller = (Controller) handler;
+        String viewName = controller.execute(request, response);;
+        return new ModelAndView(new JspView(viewName));
+    }
+}
