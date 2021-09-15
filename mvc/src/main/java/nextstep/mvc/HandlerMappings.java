871935852a13811be4a66594a2d9e28f75fb4d4a
Hyeon9mak <jinha3507@gmail.com>
2021-09-15 13:56:06 +0900

    refactor: 사용되지 않는 import 제거

--- mvc/src/main/java/nextstep/mvc/HandlerMappings.java
+++ mvc/src/main/java/nextstep/mvc/HandlerMappings.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import nextstep.mvc.exception.HandlerAdapterNotFoundException;
 import nextstep.mvc.exception.HandlerNotFoundException;
 
 public class HandlerMappings {
