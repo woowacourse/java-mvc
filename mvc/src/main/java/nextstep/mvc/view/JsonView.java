b3e0043810d1d4b3e5c566eb6e16ab30a6eab0cd
dwl5 <ssop6403@gmail.com>
2021-09-15 17:21:54 +0900

    feat: JsonView 추가

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,37 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object value = getModelValue(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(objectMapper.writeValueAsBytes(value));
+    }
+
+
+    private Object getModelValue(Map<String, ?> model) {
+        if (model.size() == 1) {
+            return model.entrySet()
+                    .stream()
+                    .findAny()
+                    .orElseThrow(RuntimeException::new);
+        }
+
+        return model;
     }
 }
