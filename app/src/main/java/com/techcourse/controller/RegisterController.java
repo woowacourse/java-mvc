df127b8f25c799e6a618b35a8a720112315766c7
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-15 23:27:40 +0900

    feat: 로그인한 사용자가 "GET /register" 요청 시, "/index.jsp" 로 리다이렉트 하도록 수정

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -15,6 +15,8 @@ import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 import static com.techcourse.view.ViewName.*;
 
 @Controller
@@ -31,6 +33,12 @@ public class RegisterController {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
         LOG.info("RegisterController GET method");
+        final Optional<User> sessionUser = UserSession.getUserFrom(request.getSession());
+        if (sessionUser.isPresent()) {
+            final User user = sessionUser.get();
+            LOG.info("logged in {}", user.getAccount());
+            return new ModelAndView(new JspView(REDIRECT_PREFIX + INDEX_JSP_VIEW_NAME));
+        }
         return new ModelAndView(new JspView(REGISTER_JSP_VIEW_NAME));
     }
 
