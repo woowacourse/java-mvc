136ddd3944a9c4723e3b4511e3ff9cd31a14a7b3
Hyeon9mak <jinha3507@gmail.com>
2021-09-15 13:55:58 +0900

    refactor: InMemoryUserRepository에서 ConcurrentHashMap을 사용하도록 수정

--- app/src/test/java/com/techcourse/service/RegisterServiceTest.java
+++ app/src/test/java/com/techcourse/service/RegisterServiceTest.java
@@ -8,6 +8,7 @@ import com.techcourse.exception.DuplicateAccountException;
 import com.techcourse.repository.InMemoryUserRepository;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,7 +22,7 @@ class RegisterServiceTest {
 
     @BeforeEach
     void setUp() {
-        Map<String, User> database = new HashMap<>();
+        Map<String, User> database = new ConcurrentHashMap<>();
         userRepository = new InMemoryUserRepository(database, new AtomicLong(1));
 
         registerService = new RegisterService(userRepository);
