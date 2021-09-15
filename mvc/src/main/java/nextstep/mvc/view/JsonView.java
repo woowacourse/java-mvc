4a9750532f191f19522745f9ccedef6a40f8e345
TaewanKimmmm <ktww88@gmail.com>
2021-09-16 07:49:08 +0900

    feat: JsonView 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,29 @@
 package nextstep.mvc.view;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.web.support.MediaType;
+
 public class JsonView implements View {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.write(convertToJson(model));
+    }
+
+    private String convertToJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values().toArray()[0]);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
