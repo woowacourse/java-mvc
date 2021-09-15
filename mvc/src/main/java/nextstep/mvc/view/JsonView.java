fba20ff2c415bbbd2a979ccc4f1ecf09edd28ca0
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-09-15 13:49:12 +0900

    feat: JsonView 구현 및 UserController 추가

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,35 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.exception.NotExistException;
+import nextstep.web.support.MediaType;
 
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object value = getModelValue(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(objectMapper.writeValueAsBytes(value));
+        outputStream.flush();
     }
+
+    private Object getModelValue(Map<String, ?> model) {
+        if (model.size() == 1) {
+            return model.keySet().stream()
+                    .map(model::get)
+                    .findAny()
+                    .orElseThrow(NotExistException::new);
+        }
+        return model;
+    }
+
 }
