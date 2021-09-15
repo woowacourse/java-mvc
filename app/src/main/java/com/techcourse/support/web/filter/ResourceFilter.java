ffad1618d805e21ade6ec95669dd1e2bac5fadaa
Minsung Son <sns2831@gmail.com>
2021-09-15 22:14:07 +0900

    fix: resource prefix 제거

--- app/src/main/java/com/techcourse/support/web/filter/ResourceFilter.java
+++ app/src/main/java/com/techcourse/support/web/filter/ResourceFilter.java
@@ -25,7 +25,6 @@ public class ResourceFilter implements Filter {
 
     static {
         resourcePrefixs.addAll(Arrays.asList(
-                "/",
                 "/css",
                 "/js",
                 "/assets",
