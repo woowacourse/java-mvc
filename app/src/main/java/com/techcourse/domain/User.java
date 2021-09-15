5db6f1bfedcb82fe1a0c4e00e3658652e6b718c8
Daeun Lee <leede0418@likelion.org>
2021-09-16 02:21:39 +0900

    refactor: getter 추가

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -26,13 +26,21 @@ public class User {
         return account;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
-                ", account='" + account + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+            "id=" + id +
+            ", account='" + account + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            '}';
     }
 }
