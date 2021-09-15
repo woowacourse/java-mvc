45990592cd25c24bbab7161e4df8ebaf313fb699
y_woo :) <dusdn1702@naver.com>
2021-09-15 17:21:51 +0900

    refactor: json view 구현

---
+++ app/src/test/java/com/techcourse/controller/UserControllerTest.java
@@ -0,0 +1,50 @@
+package com.techcourse.controller;
+
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.ModelAndView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UserControllerTest {
+    private AnnotationHandlerMapping handlerMapping;
+
+    @BeforeEach
+    void setUp() {
+        handlerMapping = new AnnotationHandlerMapping("com.techcourse.controller");
+        try {
+            handlerMapping.initialize();
+        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void show() {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getParameter("account")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/api/user");
+        when(request.getMethod()).thenReturn("GET");
+
+        User user = new User(1L, "gugu", "1234", "gugu@gugu");
+        InMemoryUserRepository.save(user);
+
+        final HandlerExecution handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        final ModelAndView modelAndView = handlerExecution.handle(request, response);
+
+        assertThat(modelAndView.getObject("user")).usingRecursiveComparison()
+                .isEqualTo(user);
+    }
+}
--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,32 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
-
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        for (Map.Entry<String, ?> entry : model.entrySet()) {
+            makeResponse(response, entry.getValue());
+        }
+    }
+
+    private void makeResponse(HttpServletResponse response, Object value) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        PrintWriter out = response.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(value);
+        out.print(json);
+        out.flush();
     }
 }
