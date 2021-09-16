20e4753f8becb4f69709569e3807c4470f82511f
sjpark-dev <sjpark.dev@gmail.com>
2021-09-16 11:34:05 +0900

    refactor: JsonView 예외 핸들링 & ObjectMapper 상수화

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -3,7 +3,6 @@ package nextstep.mvc.view;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 import nextstep.web.support.MediaType;
@@ -13,6 +12,7 @@ import org.slf4j.LoggerFactory;
 public class JsonView implements View {
 
     private static final Logger LOG = LoggerFactory.getLogger(JsonView.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) {
@@ -32,12 +32,11 @@ public class JsonView implements View {
             PrintWriter writer = response.getWriter();
             response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
             response.setCharacterEncoding("UTF-8");
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonString = objectMapper.writeValueAsString(object);
+            String jsonString = OBJECT_MAPPER.writeValueAsString(object);
             writer.print(jsonString);
             writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new IllegalStateException();
         }
     }
 }
