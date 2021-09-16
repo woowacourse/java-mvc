2ebf7a73e226e33847e5c2b436e81e45051ef234
gomding <backchida@gmail.com>
2021-09-17 02:29:39 +0900

    refactor(Register): 회원 가입시 계정 이름 중복이면 409 페이지를 보여주도록 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.techcourse.controller;
 
 import com.techcourse.domain.User;
+import com.techcourse.exception.DuplicatedUserException;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,8 +15,12 @@ public class RegisterController implements Controller {
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
-        InMemoryUserRepository.save(user);
-
+        try {
+            InMemoryUserRepository.save(user);
+        } catch (DuplicatedUserException e) {
+            res.setStatus(409);
+            return "409.jsp";
+        }
         return "redirect:/index.jsp";
     }
 }
