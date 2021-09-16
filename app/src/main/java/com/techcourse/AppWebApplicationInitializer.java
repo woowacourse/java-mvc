72cb367a99698270b9b53e28cb4eee35712aeb2f
Kimun Kim <kkm97351@gmail.com>
2021-09-17 01:00:00 +0900

    refactor: 인터페이스 컨트롤러 기반의 로직 삭제

--- app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
+++ app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletRegistration;
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
 import nextstep.mvc.controller.tobe.HandlerExecutionHandlerAdaptor;
-import nextstep.mvc.controller.tobe.InterfaceBasedControllerHandlerAdapter;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,11 +16,8 @@ public class AppWebApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) {
         final DispatcherServlet dispatcherServlet = new DispatcherServlet();
-//        3단계 조건에 의한 deprecated
-//        dispatcherServlet.addHandlerMapping(new ManualHandlerMapping());
         dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping(getClass().getPackageName()));
 
-        dispatcherServlet.addHandlerAdaptor(new InterfaceBasedControllerHandlerAdapter());
         dispatcherServlet.addHandlerAdaptor(new HandlerExecutionHandlerAdaptor());
 
         final ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
