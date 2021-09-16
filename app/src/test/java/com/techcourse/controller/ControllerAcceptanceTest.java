de79e3bc1abc56d8cb7e392e9674e7b13a98bdc3
livenow14 <xowjd41@naver.com>
2021-09-16 13:35:44 +0900

    refactor: 코드 스멜 수정

--- app/src/test/java/com/techcourse/controller/ControllerAcceptanceTest.java
+++ app/src/test/java/com/techcourse/controller/ControllerAcceptanceTest.java
@@ -8,6 +8,8 @@ import org.apache.catalina.startup.Tomcat;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -20,7 +22,7 @@ import static nextstep.web.support.MediaType.APPLICATION_FORM;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("컨트롤러 인수 테스트")
-public class ControllerAcceptanceTest {
+class ControllerAcceptanceTest {
 
     @BeforeAll
     static void setUp() throws LifecycleException {
@@ -69,21 +71,12 @@ public class ControllerAcceptanceTest {
         assertThat(responseCode).isEqualTo(500);
     }
 
-    @Test
-    void login_view_test() throws IOException {
+    @ParameterizedTest
+    @ValueSource(strings = {"/login", "/login/view", "/logout" , "/register", "/register/view"})
+    void controller_get_method(String path) throws IOException {
         //given
         //when
-        final HttpURLConnection httpURLConnection = connectTomcat("/login");
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        //then
-        assertThat(bufferedReader.readLine()).isNotNull();
-    }
-
-    @Test
-    void login_view_test2() throws IOException {
-        //given
-        //when
-        final HttpURLConnection httpURLConnection = connectTomcat("/login/view");
+        final HttpURLConnection httpURLConnection = connectTomcat(path);
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
         //then
         assertThat(bufferedReader.readLine()).isNotNull();
@@ -99,42 +92,12 @@ public class ControllerAcceptanceTest {
         assertThat(bufferedReader.readLine()).isNotNull();
     }
 
-    @Test
-    void logout_test() throws IOException {
-        //given
-        //when
-        final HttpURLConnection httpURLConnection = connectTomcat("/logout");
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        //then
-        assertThat(bufferedReader.readLine()).isNotNull();
-    }
 
     @Test
     void register_test() throws IOException {
         //given
         //when
         final HttpURLConnection httpURLConnection = postConnectTomcat("/register?account=hihi&password=password&email=hi@hi.com", APPLICATION_FORM);
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        //then
-        assertThat(bufferedReader.readLine()).isNotNull();
-    }
-
-
-    @Test
-    void register_view_test() throws IOException {
-        //given
-        //when
-        final HttpURLConnection httpURLConnection = connectTomcat("/register");
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        //then
-        assertThat(bufferedReader.readLine()).isNotNull();
-    }
-
-    @Test
-    void register_view_test2() throws IOException {
-        //given
-        //when
-        final HttpURLConnection httpURLConnection = connectTomcat("/register/view");
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
         //then
         assertThat(bufferedReader.readLine()).isNotNull();
