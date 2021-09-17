0f07c8c2c386daf242fe98004c9c45eedbe4afbc
gomding <backchida@gmail.com>
2021-09-17 16:06:22 +0900

    fix(InMemoryUserRepository): 사용하지않는 import 제거

--- app/src/test/java/com/techcourse/repository/InMemoryUserRepositoryTest.java
+++ app/src/test/java/com/techcourse/repository/InMemoryUserRepositoryTest.java
@@ -2,7 +2,6 @@ package com.techcourse.repository;
 
 import com.techcourse.domain.User;
 import com.techcourse.exception.DuplicatedUserException;
-import javassist.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +10,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryUserRepositoryTest {
 
