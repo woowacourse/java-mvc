e05e3121b804f0d52f4c11b432573e6f0c247ba4
gomding <backchida@gmail.com>
2021-09-17 15:51:50 +0900

    refactor(UserController): User 조회 실패시 404페이지로 이동하도록 수정

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.techcourse.controller;
 
 import com.techcourse.domain.User;
-import com.techcourse.exception.NotFoundException;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.JsonView;
+import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -24,9 +26,11 @@ public class UserController {
         log.debug("user id : {}", account);
 
         final ModelAndView modelAndView = new ModelAndView(new JsonView());
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow(() -> new NotFoundException("유저를 조회하는데 실패했습니다."));
-
+        Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
+        if (optionalUser.isEmpty()) {
+            return new ModelAndView(new JspView("404.jsp"));
+        }
+        User user = optionalUser.get();
         modelAndView.addObject("user", user);
         return modelAndView;
     }
