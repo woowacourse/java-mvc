d2ce3668c943506a973bb803efb2aa5506ea66d1
Nasung <rkdrbgks22@ks.ac.kr>
2021-09-18 22:11:47 +0900

    [MVC 구현하기 - 2, 3단계] 와일더(강규한) 미션 제출합니다. (#92)
    * feat: UserController 추가
    
    * feat: JsonView 구현
    
    * feat: JsonView 구현
    
    * refactor: 어노테이션 기반 MVC 로 변경
    
    * refactor: 상수 final 추가

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,14 +1,10 @@
 package com.techcourse;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
-import com.techcourse.controller.LogoutController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.controller.asis.ForwardController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,11 +16,6 @@ public class ManualHandlerMapping implements HandlerMapping {
 
     @Override
     public void initialize() {
-        controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
-        controllers.put("/logout", new LogoutController());
-
         log.info("Initialized Handler Mapping!");
         controllers.keySet().forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
     }
--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,12 +1,38 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final String MEDIA_TYPE = MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+    private final int status;
+
+    public JsonView() {
+        this(HttpServletResponse.SC_OK);
+    }
+
+    public JsonView(int status) {
+        this.status = status;
+    }
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setStatus(status);
+        response.setContentType(MEDIA_TYPE);
+        response.getWriter().write(convertModelToJson(model));
+    }
+
+    public String convertModelToJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return OBJECT_MAPPER.writeValueAsString(model.values().toArray()[0]);
+        }
+        return OBJECT_MAPPER.writeValueAsString(model);
     }
 }
