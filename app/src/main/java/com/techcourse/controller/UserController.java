ae7da2099833a86b9babc00ad185c4a32a0266c0
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-09-18 12:41:06 +0900

    feat: JsonView에서 여러 파라미터의 요청에 대해 응답 받아볼 수 있는 컨트롤러 메서드 추가

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -13,13 +13,15 @@ import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 @Controller
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView showSingleUser(HttpServletRequest request, HttpServletResponse response) {
         final String account = request.getParameter("account");
         log.debug("user id : {}", account);
 
@@ -28,6 +30,34 @@ public class UserController {
                 .orElseThrow(() -> new ApplicationException("사용자를 찾을 수 없습니다."));
 
         modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/all-users", method = RequestMethod.GET)
+    public ModelAndView showAllUsers(HttpServletRequest request, HttpServletResponse response) {
+        final ModelAndView modelAndView = new ModelAndView(new JsonView());
+        final Map<String, User> allUsers = InMemoryUserRepository.findAllUsers();
+        for (Map.Entry<String, User> userEntry : allUsers.entrySet()) {
+            modelAndView.addObject(userEntry.getKey(), userEntry.getValue());
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/two-users", method = RequestMethod.GET)
+    public ModelAndView showTwoUsers(HttpServletRequest request, HttpServletResponse response) {
+        final String account1 = request.getParameter("account1");
+        final String account2 = request.getParameter("account2");
+        log.debug("user id : {}", account1);
+        log.debug("user id : {}", account2);
+
+        final ModelAndView modelAndView = new ModelAndView(new JsonView());
+        final User user1 = InMemoryUserRepository.findByAccount(account1)
+                .orElseThrow(() -> new ApplicationException("사용자를 찾을 수 없습니다."));
+        final User user2 = InMemoryUserRepository.findByAccount(account2)
+                .orElseThrow(() -> new ApplicationException("사용자를 찾을 수 없습니다."));
+
+        modelAndView.addObject("user1", user1);
+        modelAndView.addObject("user2", user2);
         return modelAndView;
     }
 }
