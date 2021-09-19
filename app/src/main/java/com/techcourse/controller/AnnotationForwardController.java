d7d16e4bca9de53816b43067b1f62bff5545cb4c
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-19 13:33:19 +0900

    feat: 어노테이션 기반 ForwardController 추가

---
+++ app/src/main/java/com/techcourse/controller/AnnotationForwardController.java
@@ -0,0 +1,19 @@
+package com.techcourse.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+@Controller
+public class AnnotationForwardController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView root(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView(new JspView(JspView.REDIRECT_PREFIX + "/index.jsp"));
+    }
+
+}
