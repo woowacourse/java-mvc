a0027b9a1a603168adcd90954ae3d07d266f8aa5
pkeugine <pkeugine@gmail.com>
2021-09-20 17:34:46 +0900

    feat: model 개수에 따라 json 출력 형식 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,22 +1,53 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.web.support.MediaType;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        String modelAsString = mapper.writeValueAsString(model);
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        PrintWriter writer = response.getWriter();
-        writer.print(modelAsString);
-        writer.flush();
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) {
+        String modelAsJson = processToJson(model);
+        printToResponse(response, modelAsJson);
+    }
+
+    private String processToJson(Map<String, ?> model) {
+        try {
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            if (model.size() == 1) {
+                return mapper.writeValueAsString(model.values().iterator().next());
+            }
+            return mapper.writeValueAsString(model);
+        } catch (JsonProcessingException exception) {
+            throw new MvcException(
+                    String.format("Exception when converting [%s] to json.", model),
+                    exception
+            );
+        }
+    }
+
+    private void printToResponse(HttpServletResponse response, String modelAsJson) {
+        try {
+            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            PrintWriter writer = response.getWriter();
+            writer.print(modelAsJson);
+            writer.flush();
+        } catch (IOException exception) {
+            throw new MvcException(
+                    String.format("Exception when printing \"%s\" as json to response", modelAsJson),
+                    exception
+            );
+        }
     }
 }
---
+++ mvc/src/main/java/nextstep/mvc/view/MvcException.java
@@ -0,0 +1,8 @@
+package nextstep.mvc.view;
+
+public class MvcException extends RuntimeException {
+
+    public MvcException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
