a847513c1eed81936b50829121a4319d1e13f9d7
unluckyjung <ybook2012@gmail.com>
2021-09-20 17:43:12 +0900

    refactor: 생성자 위치 변경

--- app/src/main/java/com/techcourse/domain/UserSession.java
+++ app/src/main/java/com/techcourse/domain/UserSession.java
@@ -7,6 +7,9 @@ public class UserSession {
 
     public static final String SESSION_KEY = "user";
 
+    private UserSession() {
+    }
+
     public static Optional<User> getUserFrom(HttpSession session) {
         final User user = (User) session.getAttribute(SESSION_KEY);
         return Optional.ofNullable(user);
@@ -14,8 +17,5 @@ public class UserSession {
 
     public static boolean isLoggedIn(HttpSession session) {
         return getUserFrom(session).isPresent();
-    }
-
-    private UserSession() {
     }
 }
