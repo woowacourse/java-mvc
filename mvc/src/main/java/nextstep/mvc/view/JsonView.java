47843bd043947607d71a7d477e38da87359deaf9
YeongBeom Heo <hybeom@gmail.com>
2021-09-20 15:55:34 +0900

    feat : JsonView 데이터 하나일 때 기능 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -2,36 +2,38 @@ package nextstep.mvc.view;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import nextstep.web.support.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
-
-    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
-
-    public JsonView() {
-    }
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request,
         HttpServletResponse response) throws Exception {
-        String valueAsString = extarctedValueFromModel(model);
+        String valueAsString;
+        valueAsString = extractedJsonDataFromModel(model);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         PrintWriter out = response.getWriter();
         out.print(valueAsString);
         out.flush();
     }
 
-    private String extarctedValueFromModel(Map<String, ?> model)
-        throws JsonProcessingException {
+    private String extractedJsonDataFromModel(Map<String, ?> model) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+
+        if (model.size() == 1) {
+            Set<String> rawKeys = model.keySet();
+            List<String> keys = new LinkedList<>(rawKeys);
+            String uniqueKey = keys.get(0);
+            return objectMapper.writeValueAsString(model.get(uniqueKey));
+        }
+
         return objectMapper.writeValueAsString(model);
     }
 }
