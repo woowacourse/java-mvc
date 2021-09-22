952b58157a2b25e821aac9bcb3624890be775a3e
TaewanKimmmm <ktww88@gmail.com>
2021-09-22 19:11:35 +0900

    test: ControllerScannerTest 수정

--- mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -4,23 +4,15 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import nextstep.web.support.RequestMethod;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ControllerScannerTest {
 
     @Test
     void getControllers() {
         ControllerScanner controllerScanner = new ControllerScanner("samples");
-        Map<HandlerKey, HandlerExecution> controllers = controllerScanner.getControllers();
-        assertEquals(controllers.size(), 2);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
 
-        HandlerKey handlerKeyGet = new HandlerKey("/get-test", RequestMethod.GET);
-        HandlerKey handlerKeyPost = new HandlerKey("/post-test", RequestMethod.POST);
-
-        assertTrue(controllers.containsKey(handlerKeyGet));
-        assertTrue(controllers.containsKey(handlerKeyPost));
+        assertEquals(1, controllers.size());
     }
 }
