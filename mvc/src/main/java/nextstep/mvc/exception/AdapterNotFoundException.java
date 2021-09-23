8937518fef248dfdba5a508b0fc37469f25fc71b
Eugine Park <pkeugine@gmail.com>
2021-09-23 19:17:48 +0900

    refactor: AdapterNotFoundException이 MvcException을 상속하도록 변경

--- mvc/src/main/java/nextstep/mvc/exception/AdapterNotFoundException.java
+++ mvc/src/main/java/nextstep/mvc/exception/AdapterNotFoundException.java
@@ -1,6 +1,6 @@
 package nextstep.mvc.exception;
 
-public class AdapterNotFoundException extends RuntimeException {
+public class AdapterNotFoundException extends MvcException {
 
     public AdapterNotFoundException(Object handler) {
         super(String.format("Unable to find handler adapter. [Handler]: %s", handler.toString()));
