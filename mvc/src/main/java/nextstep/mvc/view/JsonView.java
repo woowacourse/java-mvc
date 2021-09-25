5b88a1b748475f63706fc2636c4af56ebe627121
김주원 <kjw11077@naver.com>
2021-09-26 01:02:30 +0900

    refactor: json 렌더시 ObjectMapper -> ObjectWriter 사용하도록 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,6 +1,7 @@
 package nextstep.mvc.view;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -8,25 +9,21 @@ import java.util.Map;
 
 public class JsonView implements View {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectWriter OBJECT_WRITER = new ObjectMapper().writerWithDefaultPrettyPrinter();
 
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType("application/json");
 
         final Object modelObject = modelToObject(model);
-
-        final String jsonBody = objectMapper.writeValueAsString(modelObject);
+        final String jsonBody = OBJECT_WRITER.writeValueAsString(modelObject);
         response.getWriter().append(jsonBody);
     }
 
-    private Object modelToObject(Map<String, ?> model) {
+    private Object modelToObject(Map<String, Object> model) {
         if (model.size() == 1) {
-            return model.values().stream()
-                    .findFirst()
-                    .orElseThrow();
+            return model.values().iterator().next();
         }
-
         return model;
     }
 }
