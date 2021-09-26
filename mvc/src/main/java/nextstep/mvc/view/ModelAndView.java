b11e49ba3d665fe34328fa6d63cfb96d09562015
rok93 <goodboy302@naver.com>
2021-09-26 20:22:09 +0900

    refactor: 코드 가독성을 위해 메서드 순서 재배치

--- mvc/src/main/java/nextstep/mvc/view/ModelAndView.java
+++ mvc/src/main/java/nextstep/mvc/view/ModelAndView.java
@@ -26,15 +26,15 @@ public class ModelAndView {
         return model.get(attributeName);
     }
 
+    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        view.render(model, request, response);
+    }
+
     public Map<String, Object> getModel() {
         return Collections.unmodifiableMap(model);
     }
 
     public View getView() {
         return view;
-    }
-
-    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        view.render(model, request, response);
     }
 }
