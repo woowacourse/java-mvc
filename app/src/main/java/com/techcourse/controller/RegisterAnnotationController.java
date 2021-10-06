72125905c7bd13c249819b15c3d53d2a7095181c
Solji Choi <bssj9307@gmail.com>
2021-10-06 23:14:32 +0900

    feat: 어노테이션 기반 Controller 추가

---
+++ app/src/main/java/com/techcourse/controller/RegisterAnnotationController.java
@@ -0,0 +1,29 @@
+package com.techcourse.controller;
+
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+@Controller
+public class RegisterAnnotationController {
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String doGet(HttpServletRequest req, HttpServletResponse res) {
+        return "/register.jsp";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String doPost(HttpServletRequest req, HttpServletResponse res) {
+        final User user = new User(2,
+                req.getParameter("account"),
+                req.getParameter("password"),
+                req.getParameter("email"));
+        InMemoryUserRepository.save(user);
+
+        return "redirect:/index.jsp";
+    }
+}
