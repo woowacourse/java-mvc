577b95e0810309c7813108f8056eef65c30ff356
Solji Choi <bssj9307@gmail.com>
2021-10-13 22:36:26 +0900

    refactor: HandlerAdpater 리턴 타입 변경

--- mvc/src/main/java/nextstep/mvc/HandlerAdapter.java
+++ mvc/src/main/java/nextstep/mvc/HandlerAdapter.java
@@ -2,11 +2,12 @@ package nextstep.mvc;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.ModelAndView;
 
 public interface HandlerAdapter {
 
     boolean supports(Object handler);
 
-    Object handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception;
 }
