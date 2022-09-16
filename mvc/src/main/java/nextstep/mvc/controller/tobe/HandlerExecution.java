4f58b41d1fe85159e688bedadd3de76cd67d1293
sinb57 <sinb57@naver.com>
2022-09-16 18:06:46 +0900

    refactor: HandlerExecution 리팩토링

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -1,6 +1,8 @@
 package nextstep.mvc.controller.tobe;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,11 +17,22 @@ public class HandlerExecution {
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final Object controller = emptyConstructor(method).newInstance();
 
-        Object o = method.getDeclaringClass().getConstructor().newInstance();
+        return (ModelAndView) method.invoke(controller, request, response);
+    }
 
-        return (ModelAndView) method.invoke(o, request, response);
+    private Constructor<?> emptyConstructor(final Method method) {
+        final Class<?> declaringClass = method.getDeclaringClass();
+        final List<Constructor<?>> constructors = List.of(declaringClass.getConstructors());
 
+        return constructors.stream()
+                .filter(this::hasNoParameters)
+                .findAny()
+                .orElseThrow(RuntimeException::new);
+    }
 
+    private boolean hasNoParameters(final Constructor<?> constructor) {
+        return constructor.getParameterCount() == 0;
     }
 }
