20441b7bba866f3d3e43bb6ea36e3f7a3407f9cd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-16 21:05:13 +0900

    feat(HandlerExecution): invoke method with reflection

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,20 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Object controller;
+    private final Method handler;
+
+    public HandlerExecution(Object controller, Method handler) {
+        this.controller = controller;
+        this.handler = handler;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handler.invoke(controller, request, response);
     }
 }
