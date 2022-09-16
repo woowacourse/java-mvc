3f674cbdd7d602b47ebf628d40439df00de8fd30
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-16 17:09:51 +0900

    feat: AnnotationHandlerMappingTest 통과하도록 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,11 +2,19 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +29,48 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+
         log.info("Initialized AnnotationHandlerMapping!");
+        findHandlerFromPackage();
     }
 
     public Object getHandler(final HttpServletRequest request) {
+        String requestUrl = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestUrl, requestMethod);
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private void findHandlerFromPackage() {
+        for (Object eachPackage : basePackage) {
+            Reflections reflections = new Reflections(eachPackage);
+            Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+            findHandlerFromClass(classes);
+        }
+    }
+
+    private void findHandlerFromClass(Set<Class<?>> classes) {
+        for (Class<?> clazz : classes) {
+            Method[] methods = clazz.getMethods();
+            findHandlerFromMethod(methods);
+        }
+    }
+
+    private Method findHandlerFromMethod(Method[] methods) {
+        for (Method method: methods) {
+            addRequestMapping(method);
+        }
         return null;
+    }
+
+    private void addRequestMapping(Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            for (RequestMethod requestMethod : requestMapping.method()) {
+                HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+                HandlerExecution execution = new HandlerExecution(method);
+                handlerExecutions.put(handlerKey, execution);
+            }
+        }
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/AnnotationHandlerMappingTest.java
@@ -31,6 +31,8 @@ class AnnotationHandlerMappingTest {
         final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
         final var modelAndView = handlerExecution.handle(request, response);
 
+        System.out.println(modelAndView);
+
         assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
     }
 
