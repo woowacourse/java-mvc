11b991b527c1e599bc6b3d47424fc5ef347136d8
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-17 12:58:09 +0900

    feat(DispatcherServlet): handle and render both Controller and HandlerExecution

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -8,6 +8,7 @@ import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.controller.tobe.HandlerExecution;
 import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
+import nextstep.mvc.view.RedirectView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,17 +45,32 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             final var handler = getHandler(request);
-            if (handler instanceof Controller) {
-                String viewName = ((Controller) handler).execute(request, response);
-                move(viewName, request, response);
-            }
-            if (handler instanceof HandlerExecution) {
-                ModelAndView modelAndView = (ModelAndView) ((HandlerExecution) handler).handle(request, response);
-            }
+            ModelAndView modelAndView = getModelAndView(request, response, handler);
+            move(modelAndView, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private ModelAndView getModelAndView(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        ModelAndView modelAndView = null;
+        if (handler instanceof Controller) {
+            String viewName = ((Controller) handler).execute(request, response);
+            modelAndView = toModelAndView(viewName);
+        }
+        if (handler instanceof HandlerExecution) {
+            modelAndView = (ModelAndView) ((HandlerExecution) handler).handle(request, response);
+        }
+        return modelAndView;
+    }
+
+    private ModelAndView toModelAndView(String viewName) {
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            return new ModelAndView(new RedirectView(viewName));
+        }
+        return new ModelAndView(new JspView(viewName));
     }
 
     private Object getHandler(final HttpServletRequest request) {
@@ -65,14 +81,12 @@ public class DispatcherServlet extends HttpServlet {
                 .orElseThrow();
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response)
+    private void move(final ModelAndView modelAndView, final HttpServletRequest request,
+                      final HttpServletResponse response)
             throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+        if (modelAndView != null) {
+            final var view = modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
         }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
