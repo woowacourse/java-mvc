3295b9a69e9fe511ea54443c01221514785d9566
leokim <kunga077@gmail.com>
2022-09-18 00:13:36 +0900

    feat: request method 객체 생성 및 테스트

--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,15 @@
 package nextstep.web.support;
 
+import java.util.Arrays;
+import nextstep.web.support.exception.RequestMethodNotFoundException;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod from(String method) {
+        return Arrays.stream(values())
+                .filter(requestMethod -> method.equalsIgnoreCase(requestMethod.name()))
+                .findFirst()
+                .orElseThrow(RequestMethodNotFoundException::new);
+    }
 }
