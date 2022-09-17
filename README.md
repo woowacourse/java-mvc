e44b9141fa441e16e767bcfb9a3c7ca6f7500fb5
Gyuchool <balance0591@naver.com>
2022-09-17 15:52:17 +0900

    feat: implement AnnotationHandlerMapping

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## 기능 구현 사항
 
-- [ ] AnnotationHandlerMapping을 구현한다.
+- [x] AnnotationHandlerMapping을 구현한다.
 - [ ] DispatcherServlet에서 HandlerMapping 인터페이스를 활용하여 AnnotationHandlerMapping을 처리한다.
 - [ ] DispatcherServlet에서 HandlerMapping 인터페이스를 활용하여 ManualHandlerMapping을 처리할 수 있다.
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,7 +1,13 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,19 +18,46 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        for (var basePackage : basePackages) {
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+
+            for( var clazz : classes){
+                try {
+                    Object controller = clazz.getDeclaredConstructor().newInstance();
+                    for (var method : clazz.getMethods()){ // 클래스안의 메서드들
+                        if (method.isAnnotationPresent(RequestMapping.class)) {
+                            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                            String url = requestMapping.value();
+                            RequestMethod[] methods = requestMapping.method();
+                            for (RequestMethod requestMethod : methods) {
+                                log.info("SAVE >> url :{}, requestMethod:{}, method:{}", url, requestMethod, method);
+                                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                                handlerExecutions.put(handlerKey, new HandlerExecution(controller, method));
+                            }
+                        }
+                    }
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod httpMethod = RequestMethod.from(request.getMethod());
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), httpMethod));
     }
 }
