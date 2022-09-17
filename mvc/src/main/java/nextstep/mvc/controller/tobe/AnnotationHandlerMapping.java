8d0a475ea0daa23f887133f07606d34a92f5e506
beomWhale <cndqjacndqja@gmail.com>
2022-09-17 19:09:18 +0900

    feat: AnnotationHandlerMappingTest가 정상 동작한다.

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,20 @@
 package nextstep.mvc.controller.tobe;
 
-import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.reflections.scanners.Scanners.TypesAnnotated;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -22,9 +30,35 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.get(TypesAnnotated.with(Controller.class).asClass());
+
+        for (Class<?> aClass : classes) {
+            Method[] methods = aClass.getMethods();
+            for (Method method : methods) {
+                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                if (annotation != null) {
+                    String value = annotation.value();
+                    RequestMethod[] method1 = annotation.method();
+                    for (RequestMethod requestMethod : method1) {
+                        try{
+                            HandlerKey handlerKey = new HandlerKey(value, requestMethod);
+                            Constructor<?> constructor = aClass.getConstructor();
+                            HandlerExecution handlerExecution = new HandlerExecution(constructor.newInstance(), method);
+                            handlerExecutions.put(handlerKey, handlerExecution);
+                        }
+                        catch (Exception exception) {
+                            throw new IllegalArgumentException("reflection 중 문제가 발생했습니다.");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
