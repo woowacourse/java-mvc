910122444f80054695422bf7a0d30a99004ec024
seong-wooo <seongwoo0513@uos.ac.kr>
2022-09-18 01:38:06 +0900

    refactor: reformat code 실행

--- mvc/src/main/java/nextstep/mvc/view/View.java
+++ mvc/src/main/java/nextstep/mvc/view/View.java
@@ -3,7 +3,6 @@ package nextstep.mvc.view;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.util.Map;
 
