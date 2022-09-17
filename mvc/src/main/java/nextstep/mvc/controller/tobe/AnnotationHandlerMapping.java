d2753be94f15edbc1d5a6ab8419e433ac6cf01e0
hyeonic <dev.hyeonic@gmail.com>
2022-09-17 23:24:53 +0900

    feat: basePackage의 Controller 클래스에서 @RequestMapping이 달린 메서드의 핸들러 로직을 가질 수 있도록 기능 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,7 +1,16 @@
 package nextstep.mvc.controller.tobe;
 
+import static java.util.stream.Collectors.*;
+
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,11 +29,31 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
+    @Override
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerClazz : controllerClasses) {
+            handlerExecutions.putAll(toHandlerExecutions(controllerClazz));
+        }
     }
 
+    private Map<HandlerKey, HandlerExecution> toHandlerExecutions(final Class<?> controllerClazz) {
+        return Arrays.stream(controllerClazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(toMap(this::toHandlerKey, method -> new HandlerExecution(controllerClazz, method)));
+    }
+
+    private HandlerKey toHandlerKey(final Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        return new HandlerKey(requestMapping.value(), requestMapping.method()[0]);
+    }
+
+    @Override
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        String requestURI = request.getRequestURI();
+        return handlerExecutions.get(new HandlerKey(requestURI, requestMethod));
     }
 }
