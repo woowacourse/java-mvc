dbb363cfec8a7f4164a1a9862a4f14022b62d7aa
Chris <bjuuuu98@gmail.com>
2022-09-18 16:24:04 +0900

    feat: AnnotationHandlerMappingTes 통과하도록 AnnotaionHandlerMapping 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,16 +1,25 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private static final int HTTP_REQUEST_METHOD_INDEX = 0;
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -22,9 +31,43 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            initializeHandlerExecutions();
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeHandlerExecutions()
+            throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        for (Object basePackage : basePackage) {
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+            initializeHandlerExecutions(controllers);
+        }
+    }
+
+    private void initializeHandlerExecutions(Set<Class<?>> controllers)
+            throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        for (Class<?> controller : controllers) {
+            Object instance = controller.getDeclaredConstructor()
+                    .newInstance();
+            Arrays.stream(controller.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> insertIntoHandlerExecutions(instance, method));
+        }
+    }
+
+    private void insertIntoHandlerExecutions(Object controller, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        HandlerKey handlerKey = new HandlerKey(requestMapping.value(),
+                requestMapping.method()[HTTP_REQUEST_METHOD_INDEX]);
+        handlerExecutions.put(handlerKey, new HandlerExecution(controller, method));
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.from(request.getMethod());
+        return handlerExecutions.get(new HandlerKey(requestURI, requestMethod));
     }
 }
--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,14 @@
 package nextstep.web.support;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod from(String requestMethod) {
+        return Arrays.stream(values())
+                .filter(value -> value.name().equals(requestMethod))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
