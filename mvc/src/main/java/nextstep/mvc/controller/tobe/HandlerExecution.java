fd41bb62d3c8280b5ef7d0f3e43ed868de3f5e3e
Wishoon <heonga2@gmail.com>
2022-09-18 17:07:48 +0900

    feat: HandlerExecution에서 Controller 구현 제거

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -3,10 +3,9 @@ package nextstep.mvc.controller.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
-import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.view.ModelAndView;
 
-public class HandlerExecution implements Controller {
+public class HandlerExecution {
 
     private final Object controller;
     private final Method method;
@@ -14,12 +13,6 @@ public class HandlerExecution implements Controller {
     public HandlerExecution(final Object controller, final Method method) {
         this.controller = controller;
         this.method = method;
-    }
-
-    @Override
-    public String execute(final HttpServletRequest req,
-                          final HttpServletResponse res) throws Exception {
-        return null;
     }
 
     public ModelAndView handle(final HttpServletRequest request,
