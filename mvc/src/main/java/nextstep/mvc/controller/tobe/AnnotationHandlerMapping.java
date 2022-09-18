287c3044799be89891bca9963fcef48bcd01ff66
Hongdonggeon <ghd700@naver.com>
2022-09-18 16:39:42 +0900

    feat: 어노테이션 핸들러 매핑 초기화
    - @Controller 어노테이션이 붙은 클래스정보 리플렉션으로 찾아오기
    - @RequestMapping 어노테이션이 붙은 메서드 정보 리플렉션으로 찾아오기

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,20 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -22,9 +30,54 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        for (Object packageName : basePackage) {
+            initializeAnnotationHandlerMapping(packageName);
+        }
+    }
+
+    private void initializeAnnotationHandlerMapping(final Object packageName) {
+        final Reflections reflections = new Reflections(packageName);
+        final Set<Class<?>> classesWithController = extractClassesWithController(reflections);
+
+        for (Class<?> aClass : classesWithController) {
+            final List<Method> methodsWithRequestMapping = extractMethodsWithRequestMapping(aClass);
+            final List<RequestMapping> requestMappings = toRequestMapping(methodsWithRequestMapping);
+            initializeHandlerExecutions(requestMappings);
+        }
+    }
+
+    private Set<Class<?>> extractClassesWithController(final Reflections reflections) {
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private List<Method> extractMethodsWithRequestMapping(final Class<?> aClass) {
+        final Method[] methods = aClass.getMethods();
+        return Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private List<RequestMapping> toRequestMapping(final List<Method> methods) {
+        return methods.stream()
+                .map(method -> method.getAnnotation(RequestMapping.class))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void initializeHandlerExecutions(final List<RequestMapping> requestMappings) {
+        for (RequestMapping requestMapping : requestMappings) {
+            final String value = requestMapping.value();
+            final RequestMethod[] requestMethods = requestMapping.method();
+            for (RequestMethod requestMethod : requestMethods) {
+                final HandlerKey handlerKey = new HandlerKey(value, requestMethod);
+                handlerExecutions.put(handlerKey, new HandlerExecution());
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.of(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
