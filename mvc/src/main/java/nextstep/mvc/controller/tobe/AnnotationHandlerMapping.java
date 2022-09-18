20ab4b8c8363af9d70ccffb788b3d4981a446e54
syoun602 <syoun602@gmail.com>
2022-09-18 18:04:28 +0900

    feat: Annotation이 붙은 모든 클래스 메서드를 등록하는 initialize 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,21 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +30,52 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        controllers.forEach(this::addAllHandlerExecutions);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
     public Object getHandler(final HttpServletRequest request) {
         return null;
+    }
+
+    private void addAllHandlerExecutions(final Class<?> controller) {
+        final List<Method> declaredMethods = Arrays.stream(controller.getDeclaredMethods())
+                .filter(it -> isAnnotatedWith(it, RequestMapping.class))
+                .collect(Collectors.toList());
+        final Object handler = createInstance(controller);
+
+        for (final Method declaredMethod : declaredMethods) {
+            final RequestMapping annotation = declaredMethod.getAnnotation(RequestMapping.class);
+            addHandlerExecution(annotation, declaredMethod, handler);
+        }
+    }
+
+    private boolean isAnnotatedWith(final Method method, final Class<?> annotationClass) {
+        return Arrays.stream(method.getDeclaredAnnotations())
+                .anyMatch(annotation -> annotation.annotationType().equals(annotationClass));
+    }
+
+    private void addHandlerExecution(final RequestMapping annotation,
+                                     final Method declaredMethod,
+                                     final Object handler) {
+        for (final RequestMethod method : annotation.method()) {
+            final HandlerKey handlerKey = new HandlerKey(annotation.value(), method);
+            final HandlerExecution handlerExecution = new HandlerExecution(handler, declaredMethod);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException |
+                InvocationTargetException | NoSuchMethodException e) {
+            log.error("add execution exception : {}", e.getMessage(), e);
+            throw new RuntimeException();
+        }
     }
 }
