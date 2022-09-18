83ad43f0ef2a63eb398a6d4b00a711dcd90a8aee
brorae <jhr3239@gmail.com>
2022-09-19 01:56:49 +0900

    refactor: AnnotationHandlerMapping 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,7 +1,13 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Set;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +28,46 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        for (Object eachPackage : basePackage) {
+            Reflections reflections = new Reflections(eachPackage);
+            addHandlerExecutions(reflections);
+        }
+    }
+
+    private void addHandlerExecutions(Reflections reflections) {
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : classes) {
+            Method[] methods = clazz.getDeclaredMethods();
+            checkRequestMappingMethod(methods);
+        }
+    }
+
+    private void checkRequestMappingMethod(Method[] methods) {
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                putHandlerExecution(method);
+            }
+        }
+    }
+
+    private void putHandlerExecution(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] applyingMethods = requestMapping.method();
+        for (RequestMethod applyingMethod : applyingMethods) {
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), applyingMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        try {
+            return handlerExecutions.get(handlerKey);
+        } catch(NullPointerException e) {
+            log.error("Handler Not Found", e);
+            throw new IllegalStateException();
+        }
     }
 }
