53627bf1a6ef18d1c4be288167373acbcb5a5637
yeon-06 <rnjstldus2@gmail.com>
2022-09-18 18:44:36 +0900

    feat: getController() 제거 후 HandlerAdapter 활용

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -4,11 +4,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.view.JspView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -48,31 +43,36 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final var controller = getController(request);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            doProcess(request, response);
+
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private Controller getController(final HttpServletRequest request) {
+    private void doProcess(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final Object handler = getHandlerMapping(request);
+        final HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
+        final ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+        if (modelAndView != null) {
+            modelAndView.render(request, response);
+        }
+    }
+
+    private Object getHandlerMapping(final HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
                 .filter(Objects::nonNull)
-                .map(Controller.class::cast)
-                .findFirst()
+                .findAny()
                 .orElseThrow();
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+    private HandlerAdapter getHandlerAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(it -> it.supports(handler))
+                .findAny()
+                .orElseThrow();
     }
 }
