1326965ea3184f1f99cca05be7544945f990d71c
kth990303 <kth990303@naver.com>
2022-09-18 15:03:26 +0900

    feat: JSPView 렌더링 시 redirect 요청 처리하는 기능 추가

--- mvc/src/main/java/nextstep/mvc/view/JspView.java
+++ mvc/src/main/java/nextstep/mvc/view/JspView.java
@@ -1,11 +1,12 @@
 package nextstep.mvc.view;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 public class JspView implements View {
 
@@ -13,18 +14,33 @@ public class JspView implements View {
 
     public static final String REDIRECT_PREFIX = "redirect:";
 
+    private final String viewName;
+
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+        addModelToRequest(model, request);
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            redirectRender(response);
+            return;
+        }
+        final RequestDispatcher dispatcher = request.getRequestDispatcher(viewName);
+        dispatcher.forward(request, response);
+    }
 
+    private void addModelToRequest(final Map<String, ?> model, final HttpServletRequest request) {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
+    }
 
-        // todo
+    private void redirectRender(final HttpServletResponse response) throws IOException {
+        final String location = viewName.substring(REDIRECT_PREFIX.length());
+        response.sendRedirect(location);
     }
 }
