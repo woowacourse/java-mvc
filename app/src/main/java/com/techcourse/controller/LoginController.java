4641736dc931e20177cc4c1acc38bf1b909d1cc9
Juhyung990122 <harukawa99@naver.com>
2022-09-18 13:55:06 +0900

    feat: Adapter 구현 및 등록

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -5,6 +5,8 @@ import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.controller.asis.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +29,7 @@ public class LoginController implements Controller {
                 .orElse("redirect:/401.jsp");
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     private String login(final HttpServletRequest request, final User user) {
         if (user.checkPassword(request.getParameter("password"))) {
             final var session = request.getSession();
