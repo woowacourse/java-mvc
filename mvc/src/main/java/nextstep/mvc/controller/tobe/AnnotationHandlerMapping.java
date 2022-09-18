2163160dcbda3fa1a8e747463b26dd34d4364d91
yukong <kyb062666@gmail.com>
2022-09-18 21:52:28 +0900

    feat: @Controller 읽어와 핸들러 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,11 +2,21 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +31,43 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        extractRequestMappings(classes).forEach(this::addHandler);
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
+        if (!handlerExecutions.containsKey(handlerKey)) {
+            throw new IllegalArgumentException(String.format("요청한 핸들러가 존재하지 않습니다. [%s]", handlerKey));
+        }
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private List<RequestMapping> extractRequestMappings(final Set<Class<?>> classes) {
+        final List<RequestMapping> requestMappings = new ArrayList<>();
+        for (final Class<?> clazz : classes) {
+            requestMappings.addAll(getRequestMappings(clazz));
+        }
+        return requestMappings;
+    }
+
+    private List<RequestMapping> getRequestMappings(final Class<?> clazz) {
+        return Arrays.stream(clazz.getMethods())
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .map(method -> method.getAnnotation(RequestMapping.class))
+            .collect(Collectors.toList());
+    }
+
+    private void addHandler(final RequestMapping requestMapping) {
+        for (final RequestMethod method : requestMapping.method()) {
+            final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), method);
+            if (handlerExecutions.containsKey(handlerKey)) {
+                throw new IllegalArgumentException(String.format("중복적으로 매핑되었습니다. [%s]", handlerKey));
+            }
+            handlerExecutions.put(handlerKey, new HandlerExecution());
+        }
     }
 }
--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,16 @@
 package nextstep.web.support;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
+
     GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    ;
+
+    public static RequestMethod from(final String value) {
+        return Arrays.stream(values())
+            .filter(method -> method.name().equals(value.toUpperCase()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(String.format("정의되지 않은 메서드입니다. [%s]", value)));
+    }
 }
