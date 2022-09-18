56e3384cd87777123200ceb5fa1835e8b73f90a5
Ohzzi <clover7even@naver.com>
2022-09-18 16:41:51 +0900

    feat: JspView의 redirect와 forward 구현

--- mvc/src/main/java/nextstep/mvc/view/JspView.java
+++ mvc/src/main/java/nextstep/mvc/view/JspView.java
@@ -1,7 +1,10 @@
 package nextstep.mvc.view;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,18 +16,40 @@ public class JspView implements View {
 
     public static final String REDIRECT_PREFIX = "redirect:";
 
+    private final String viewName;
+
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+        if (isRedirect()) {
+            redirect(response);
+            return;
+        }
+        setAttribute(model, request);
+        forward(request, response);
+    }
 
+    private boolean isRedirect() {
+        return viewName.startsWith(JspView.REDIRECT_PREFIX);
+    }
+
+    private void redirect(final HttpServletResponse response) throws IOException {
+        response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+    }
+
+    private void setAttribute(final Map<String, ?> model, final HttpServletRequest request) {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
+    }
 
-        // todo
+    private void forward(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+        final RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
