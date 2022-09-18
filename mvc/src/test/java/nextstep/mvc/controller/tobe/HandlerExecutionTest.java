f51be15813079098a9de90a57676b079fe012381
kth990303 <kth990303@naver.com>
2022-09-18 14:51:25 +0900

    test: HandlerExecution 테스트 코드 추가

---
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/HandlerExecutionTest.java
@@ -0,0 +1,43 @@
+package nextstep.mvc.controller.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Map;
+import nextstep.mvc.view.ModelAndView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import samples.TestController;
+
+class HandlerExecutionTest {
+
+    private HandlerExecution handlerExecution;
+
+    @BeforeEach
+    void setUp() throws NoSuchMethodException {
+        final Method executionMethod = TestController.class.getDeclaredMethod(
+                "findUserId", HttpServletRequest.class, HttpServletResponse.class
+        );
+        handlerExecution = new HandlerExecution(executionMethod);
+    }
+
+    @Test
+    @DisplayName("handler가 주어지면 올바르게 handle한 결과를 반환한다.")
+    void handle() throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("kth990303");
+
+        final ModelAndView modelAndView = handlerExecution.handle(request, response);
+
+        assertThat(modelAndView)
+                .extracting("model")
+                .isEqualTo(Map.of("id", "kth990303"));
+    }
+}
