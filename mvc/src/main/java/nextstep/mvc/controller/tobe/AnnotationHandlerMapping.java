e51ebbd0f680f2e82ea57d3d6a440d7a580cdf0b
jurlring <78091011+jurlring@users.noreply.github.com>
2022-09-18 22:15:32 +0900

    feat: AnnotationHandlerMapping 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,19 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +28,40 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        initHandlerExecutions();
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void initHandlerExecutions() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerClass : controllerClasses) {
+            scanRequestMapping(controllerClass);
+        }
+    }
+
+    private void scanRequestMapping(Class<?> controllerClass) {
+        try {
+            Object controller = controllerClass.getDeclaredConstructor().newInstance();
+            Arrays.stream(controllerClass.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> putHandlerExecutions(controller, method));
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.fillInStackTrace();
+        }
+    }
+
+    private void putHandlerExecutions(Object controllerClass, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        String url = requestMapping.value();
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            handlerExecutions.put(new HandlerKey(url, requestMethod), new HandlerExecution(controllerClass, method));
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+        return handlerExecutions.get(new HandlerKey(uri, RequestMethod.valueOf(method)));
     }
 }
