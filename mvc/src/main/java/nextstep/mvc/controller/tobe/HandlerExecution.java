aed6fae4bf421d7b775df9e42b1037fdef5c463f
kth990303 <kth990303@naver.com>
2022-09-18 12:19:01 +0900

    feat: AnnotationHandlerMapping 및 HandlerExecution 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,31 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        final Object controller = findController();
+        return (ModelAndView) method.invoke(controller, request, response);
+    }
+
+    private Object findController()
+            throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        return Arrays.stream(method.getDeclaringClass()
+                        .getConstructors())
+                .filter(it -> it.getParameterCount() == 0)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchMethodException("기본 생성자가 존재하지 않습니다."))
+                .newInstance();
     }
 }
