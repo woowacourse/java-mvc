6558985a3ef44bede9a5b3457456c641f1ddce0f
hyewoncc <hyewoncc@gmail.com>
2022-09-18 16:42:39 +0900

    feat: 테스트 통과하도록 AnnotationHandlerMapping 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,19 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -22,9 +29,23 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        List<Method> methods = reflections.getTypesAnnotatedWith(Controller.class).stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
+        for (Method method : methods) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method()[0]);
+            handlerExecutions.put(handlerKey, new HandlerExecution(method));
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestMethod = request.getMethod();
+        String uri = request.getRequestURI();
+
+        HandlerKey handlerKey = new HandlerKey(uri, RequestMethod.from(requestMethod));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,17 @@
 package nextstep.web.support;
 
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE,
+    ;
+
+    public static RequestMethod from(final String method) {
+        return valueOf(method.toUpperCase());
+    }
 }
