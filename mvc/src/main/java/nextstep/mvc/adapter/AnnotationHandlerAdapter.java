0f727fe90b4e786fcaed3def0199008a35282b9c
yeon-06 <rnjstldus2@gmail.com>
2022-09-18 18:41:14 +0900

    feat: HandlerAdapter 구현체 생성

---
+++ mvc/src/main/java/nextstep/mvc/adapter/AnnotationHandlerAdapter.java
@@ -0,0 +1,22 @@
+package nextstep.mvc.adapter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.ModelAndView;
+
+public class AnnotationHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
+        HandlerExecution handlerExecution = (HandlerExecution) handler;
+        return handlerExecution.handle(request, response);
+    }
+}
---
+++ mvc/src/main/java/nextstep/mvc/adapter/ControllerHandlerAdapter.java
@@ -0,0 +1,34 @@
+package nextstep.mvc.adapter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+
+public class ControllerHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
+
+        final Controller controller = (Controller) handler;
+        final String viewName = controller.execute(request, response);
+
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return new ModelAndView(new JspView(viewName));
+        }
+
+        final RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
+        return null;
+    }
+}
