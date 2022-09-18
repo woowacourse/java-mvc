40389c4cee357011379e2f07067f476acc5a03c4
Wishoon <heonga2@gmail.com>
2022-09-18 13:20:47 +0900

    feat: @RequestMapping Method extractHandlerKeys 구현

--- mvc/src/main/java/nextstep/mvc/controller/scanner/RequestMappingScanner.java
+++ mvc/src/main/java/nextstep/mvc/controller/scanner/RequestMappingScanner.java
@@ -1,8 +1,10 @@
 package nextstep.mvc.controller.scanner;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
+import nextstep.mvc.controller.tobe.HandlerKey;
 import nextstep.web.annotation.RequestMapping;
 
 public class RequestMappingScanner {
@@ -16,9 +18,22 @@ public class RequestMappingScanner {
         return INSTANCE;
     }
 
-    public Set<?> getAllAnnotations(Class<?> controller) {
+    public Set<Method> getAllAnnotations(Class<?> controller) {
         return Arrays.stream(controller.getDeclaredMethods())
             .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .collect(Collectors.toSet());
+    }
+
+    public Set<HandlerKey> extractHandlerKeys(Method requestMappedMethod) {
+        if (!requestMappedMethod.isAnnotationPresent(RequestMapping.class)) {
+            throw new IllegalArgumentException("Method is not RequestMapped");
+        }
+
+        RequestMapping requestMapping = requestMappedMethod.getAnnotation(RequestMapping.class);
+        String url = requestMapping.value();
+
+        return Arrays.stream(requestMapping.method())
+            .map(method -> new HandlerKey(url, method))
             .collect(Collectors.toSet());
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/scanner/RequestMappingScannerTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/scanner/RequestMappingScannerTest.java
@@ -2,16 +2,23 @@ package nextstep.mvc.controller.scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.mvc.controller.tobe.HandlerKey;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("RequestMapping Scanner는 ")
 class RequestMappingScannerTest {
 
     @Test
-    @DisplayName("RequestMapping Scanner는 BasePackage를 기준으로 @RequestMapping이 사용된 모든 메서드를 찾는다.")
+    @DisplayName("BasePackage를 기준으로 @RequestMapping이 사용된 모든 메서드를 찾는다.")
     void findAllMethodsOfAnnotationRequestMapping() {
         // given & when
         Set<?> methods = RequestMappingScanner
@@ -19,14 +26,39 @@ class RequestMappingScannerTest {
             .getAllAnnotations(AnnotationController.class);
 
         // then
-        assertThat(methods).hasSize(1);
+        assertThat(methods).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("URL과 HttpMethod를 가지는 HandlerKey로 변환을 할 수 있다.")
+    void convertObjectToHavingURLAndHttpMethod() {
+        // given
+        RequestMappingScanner requestMappingScanner = RequestMappingScanner.getInstance();
+        Set<Method> methods = requestMappingScanner
+            .getAllAnnotations(AnnotationController.class);
+
+        // when
+        List<HandlerKey> handlerKeys = methods.stream()
+            .map(requestMappingScanner::extractHandlerKeys)
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+
+        // then
+        assertThat(handlerKeys).containsOnly(
+            new HandlerKey("/", RequestMethod.GET),
+            new HandlerKey("/", RequestMethod.POST));
     }
 
     @Controller
     private class AnnotationController {
 
-        @RequestMapping
-        public void requestMappingMethod() {
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        public void requestMappingMethodOfGet() {
+
+        }
+
+        @RequestMapping(value = "/", method = RequestMethod.POST)
+        public void requestMappingMethodOfPost() {
 
         }
 
