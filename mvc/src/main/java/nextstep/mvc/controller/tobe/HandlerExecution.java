3028110de3b460a74a3788cf44e701582d9cf54f
yangdongjue5510 <djwhy5510@gmail.com>
2022-09-18 17:32:03 +0900

    feat: AnnotationHandlerMappingTest가 정상 동작하도록 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,20 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+    private final Object instance;
+
+    public HandlerExecution(final Method method, final Object instance) {
+        this.method = method;
+        this.instance = instance;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
