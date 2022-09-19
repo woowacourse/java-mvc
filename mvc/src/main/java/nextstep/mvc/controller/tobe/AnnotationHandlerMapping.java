d6a1ca612c42de6e322ffce3d53cfac184036780
bcc0830 <bcc0830@bithub.com>
2022-09-19 22:39:47 +0900

    feat : AnnotationHandlerMappingTest를 통과하도록 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,11 +2,14 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -25,6 +28,17 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = addHandler(request, this.basePackage);
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private HandlerKey addHandler(HttpServletRequest request, Object[] basePackage) {
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        String url = request.getRequestURI();
+        HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+        if (!handlerExecutions.containsKey(handlerKey)) {
+            handlerExecutions.put(handlerKey, new HandlerExecution(basePackage));
+        }
+        return handlerKey;
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,50 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private Object[] basePackage;
+    public HandlerExecution(Object[] basePackage) {
+        this.basePackage = basePackage;
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Reflections reflections = new Reflections(basePackage);
+        List<Method> extractedMethods = reflections.getTypesAnnotatedWith(Controller.class).stream()
+                .flatMap(each -> Arrays.stream(each.getMethods())
+                        .filter(temp -> temp.isAnnotationPresent(RequestMapping.class)))
+                .collect(Collectors.toUnmodifiableList());
+        Method method = extractedMethods.stream()
+                .filter(eachMethod ->
+                        Arrays.stream(eachMethod.getAnnotation(RequestMapping.class).method())
+                                .anyMatch(each -> each == RequestMethod.valueOf(request.getMethod()))
+                                && eachMethod.getAnnotation(RequestMapping.class).value().equals(request.getRequestURI()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+        return invoke(method, request, response);
+    }
+
+    private ModelAndView invoke(Method method, HttpServletRequest request, HttpServletResponse response ) {
+        try {
+            Class<?> declaringClass = method.getDeclaringClass();
+            Object instance = declaringClass.getConstructor().newInstance();
+            return (ModelAndView) method.invoke(instance, request, response);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
