18b072dfd4f9b9620f285eb48ac4e7f8610de5b2
nailseong <nnis9857@gmail.com>
2022-09-19 22:05:12 +0900

    feat: Annotation기반으로 동작하는 Handler의 HandlerAdapter 구현

--- mvc/src/test/java/nextstep/mvc/DispatcherServletTest.java
+++ mvc/src/test/java/nextstep/mvc/DispatcherServletTest.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.mock;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.adapter.AnnotationHandlerAdapter;
 import nextstep.mvc.adapter.ManualHandlerAdapter;
+import nextstep.mvc.handlermapping.AnnotationHandlerMapping;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -34,6 +37,37 @@ class DispatcherServletTest {
             willReturn("/get-manual")
                     .given(request)
                     .getRequestURI();
+
+            // when & then
+            assertThatCode(() -> dispatcherServlet.service(request, response))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("GET /get-test 요청을 어노테이션 기반의 컨트롤러로 처리한다.")
+        void service_annotation_success() {
+            // given
+            final DispatcherServlet dispatcherServlet = new DispatcherServlet();
+            dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping("samples"));
+            dispatcherServlet.addHandlerAdapter(new AnnotationHandlerAdapter());
+            dispatcherServlet.init();
+
+            final HttpServletRequest request = mock(HttpServletRequest.class);
+            final HttpServletResponse response = mock(HttpServletResponse.class);
+            final RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+
+            willReturn("/get-test")
+                    .given(request)
+                    .getRequestURI();
+            willReturn("GET")
+                    .given(request)
+                    .getMethod();
+            willReturn("gugu")
+                    .given(request)
+                    .getAttribute("id");
+            willReturn(requestDispatcher)
+                    .given(request)
+                    .getRequestDispatcher("");
 
             // when & then
             assertThatCode(() -> dispatcherServlet.service(request, response))
