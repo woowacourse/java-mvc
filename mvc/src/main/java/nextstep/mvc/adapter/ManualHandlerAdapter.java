db906fb06e57b0b7b34c514a3d626b118526d512
nailseong <nnis9857@gmail.com>
2022-09-19 21:00:18 +0900

    feat: Controller를 실행시키는 ManualHandlerAdapter 구현

---
+++ mvc/src/main/java/nextstep/mvc/adapter/ManualHandlerAdapter.java
@@ -0,0 +1,24 @@
+package nextstep.mvc.adapter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+
+public class ManualHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
+        final Controller controller = (Controller) handler;
+        final String viewName = controller.execute(request, response);
+        final JspView jspView = new JspView(viewName);
+        return new ModelAndView(jspView);
+    }
+}
