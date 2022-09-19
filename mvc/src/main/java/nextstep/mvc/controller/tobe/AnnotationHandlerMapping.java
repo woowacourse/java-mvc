559cac3f1d4ae689f15e1989fe613d24ad33d4d9
BeomsooKim <kbsat@naver.com>
2022-09-20 01:47:17 +0900

    refactor: AnnotationHandlerMapping null 체크 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
@@ -78,6 +79,8 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
+        Objects.requireNonNull(request);
+
         final String requestURI = request.getRequestURI();
         final RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
         final HandlerKey key = new HandlerKey(requestURI, requestMethod);
