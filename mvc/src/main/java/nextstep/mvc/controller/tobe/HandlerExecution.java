7da9c2ef94a6bbce8f103ac6a7ae8373090b8853
woong7 <driedseas@gmail.com>
2022-09-19 17:54:42 +0900

    feat : 1단계 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -1,12 +1,22 @@
 package nextstep.mvc.controller.tobe;
 
+import java.lang.reflect.Method;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Object handler;
+    private final Method method;
+
+    public HandlerExecution(Object handler, Method method) {
+        this.handler = handler;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView)method.invoke(handler, request, response);
     }
 }
