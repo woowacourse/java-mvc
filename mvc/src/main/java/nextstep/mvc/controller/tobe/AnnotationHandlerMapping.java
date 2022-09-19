d06e3f759ec8a4c39f865a6c832e71df49652ef1
이한울 <dkfghg3@naver.com>
2022-09-19 22:23:54 +0900

    feat: AnnotationHandlerMapping 및 HandlerExecution 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,17 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +26,33 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        scanController(new Reflections(basePackage));
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void scanController(final Reflections reflections) {
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(this::scanMethod);
+    }
+
+    private void scanMethod(final Class<?> controller) {
+        Arrays.stream(controller.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> addHandler(controller, method));
+    }
+
+    private void addHandler(final Class<?> clazz, final Method method) {
+        final RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+        final String url = requestMapping.value();
+
+        Arrays.stream(requestMapping.method())
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
+                .forEach(handlerKey -> handlerExecutions.put(handlerKey, new HandlerExecution(clazz, method)));
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(),
+                RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
