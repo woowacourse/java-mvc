65d41ff51c6be2db313fe1c2b05d730b73a7b3c1
2yujeong <yjfflq4872@gmail.com>
2022-09-20 02:27:50 +0900

    feat: ManualHandlerAdaptor 추가

---
+++ app/src/main/java/com/techcourse/ManualHandlerAdaptor.java
@@ -0,0 +1,22 @@
+package com.techcourse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.ModelAndView;
+
+public class ManualHandlerAdaptor implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
+        final Controller controller = (Controller) handler;
+        return new ModelAndView(controller.execute(request, response));
+    }
+}
