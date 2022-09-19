7e663629325335efd1d85f8b5029285db6710252
SuyeonChoi <tndus970120@gmail.com>
2022-09-19 20:56:47 +0900

    feat: DispatcherServlet에서 AnnotationHandlerMapping를 처리하는 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,30 +1,72 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        for (final Object basePackage : basePackages) {
+            final Collection<URL> packagePath = ClasspathHelper.forPackage(basePackage.toString());
+            final Reflections reflections = new Reflections(packagePath);
+            final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+            mapControllerServiceForExecution(controllers);
+        }
+    }
+
+    private void mapControllerServiceForExecution(final Set<Class<?>> controllers) {
+        for (final Class<?> controller : controllers) {
+            final List<Method> methods = Arrays.stream(controller.getDeclaredMethods())
+                    .filter(it -> it.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toList());
+
+            mapMethodForExecution(methods, controller);
+        }
+    }
+
+    private void mapMethodForExecution(final List<Method> methods, final Class<?> controller) {
+        for (final Method method : methods) {
+            final RequestMapping requestMappingAnnotation = method.getAnnotation(RequestMapping.class);
+            final String path = requestMappingAnnotation.value();
+            final RequestMethod requestMethod = requestMappingAnnotation.method()[0];
+
+            handlerExecutions.put(new HandlerKey(path, requestMethod), new HandlerExecution(controller, method));
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String url = request.getRequestURI();
+        final RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
+        return handlerExecutions.get(new HandlerKey(url, requestMethod));
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,19 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
+    private final Class<?> handlerClass;
+    private final Method handlerMethod;
+
+    public HandlerExecution(final Class<?> handlerClass, final Method handlerMethod) {
+        this.handlerClass = handlerClass;
+        this.handlerMethod = handlerMethod;
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handlerMethod.invoke(handlerClass.newInstance(), request, response);
     }
 }
