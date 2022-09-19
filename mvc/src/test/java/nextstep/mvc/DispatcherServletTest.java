4496759be82548009fa451b7da340fa13cea8842
shindong96 <2006peter@naver.com>
2022-09-19 15:55:39 +0900

    feat: DispatcherServlet에서 AnnotationHandlerMapping을 이용할 수 있게 구현

---
+++ mvc/src/test/java/nextstep/mvc/DispatcherServletTest.java
@@ -0,0 +1,41 @@
+package nextstep.mvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DispatcherServletTest {
+
+    @DisplayName("어노테이션 기반 handler를 사용할 수 있는지 테스트")
+    @Test
+    void service() throws ServletException, IOException {
+        // given
+        final DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping("samples"));
+        dispatcherServlet.init();
+
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+        final RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getRequestDispatcher("/get-test")).thenReturn(requestDispatcher);
+
+        // when
+        dispatcherServlet.service(request, response);
+
+        // then
+        verify(request, times(1)).getRequestDispatcher("/get-test");
+    }
+}
