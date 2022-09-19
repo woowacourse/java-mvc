98fa22dd32c4c3d128e2a561371e248831d08404
hyunrrr <whgusrms96@gmail.com>
2022-09-19 19:49:11 +0900

    feat: 핸들러와 핸들러의 특정 메서드를 실행할 수 있도록 리플랙션을 사용한 코드 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,20 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private Class<?> handler;
+    private Method method;
+
+    public HandlerExecution(Class<?> aClass, Method method) {
+        this.handler = aClass;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(handler, request, response);
     }
 }
