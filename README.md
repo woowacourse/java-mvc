fdd82d340694ea21150031ec2ae611c092eb830b
Donggyu <a29661498@gmail.com>
2022-09-19 10:58:07 +0900

    feat: AnnotationHandlerMapping 구현

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ## 1단계 구현 내용 정리
 
-- [ ] AnnotationHandlerMappingTest가 정상 동작한다.
+- [x] AnnotationHandlerMappingTest가 정상 동작한다.
+  - [x] 어노테이션 기반으로 HTTP 메서드와 URL에 따라 컨트롤러를 매핑해줄 수 있다.
 - [ ] DispatcherServlet에서 HandlerMapping 인터페이스를 활용하여 AnnotationHandlerMapping과 ManualHandlerMapping 둘다 처리할 수 있다.
 
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,18 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -25,6 +31,30 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        for (Object o : basePackage) {
+            final Reflections reflections = new Reflections(o);
+            final Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+            log.info("{}", typesAnnotatedWith);
+
+            for (Class<?> aClass : typesAnnotatedWith) {
+                final Method[] declaredMethods = aClass.getDeclaredMethods();
+                for (Method declaredMethod : declaredMethods) {
+                    final RequestMapping requestMapping = declaredMethod.getAnnotation(RequestMapping.class);
+                    if (requestMapping != null) {
+                        final HandlerKey handlerKey = new HandlerKey(requestMapping.value(),
+                                RequestMethod.valueOf(requestMapping.method()[0].name()));
+                        final Object handler;
+                        try {
+                            handler = declaredMethod.getDeclaringClass().getConstructor().newInstance();
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                            throw new RuntimeException("매핑 가능한 핸들러가 존재하지 않습니다.");
+                        }
+                        handlerExecutions.put(handlerKey, new HandlerExecution(handler, declaredMethod));
+                    }
+                }
+            }
+        }
+
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
     }
 }
