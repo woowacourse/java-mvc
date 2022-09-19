0c53eddec7080424d63e8752d175fc0ac4f1986d
김찬 <gmelddl514@gmail.com>
2022-09-19 19:30:46 +0900

    feat: handle 메서드 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/ManualHandlerAdapter.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ManualHandlerAdapter.java
@@ -2,7 +2,6 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Enumeration;
 import nextstep.mvc.HandlerAdapter;
 import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.view.JspView;
@@ -24,22 +23,6 @@ public class ManualHandlerAdapter implements HandlerAdapter {
                                final Object handler)
             throws Exception {
         final String viewName = ((Controller) handler).execute(request, response);
-        ModelAndView modelAndView = new ModelAndView(new JspView(viewName));
-        addAttributes(request, modelAndView);
         return new ModelAndView(new JspView(viewName));
-    }
-
-    private void addAttributes(final HttpServletRequest request, final ModelAndView modelAndView) {
-        final Enumeration<String> attributes = request.getAttributeNames();
-        if (attributes == null) {
-            return;
-        }
-
-        while (attributes.hasMoreElements()) {
-            final String key = attributes.nextElement();
-            final Object value = request.getAttribute(key);
-            modelAndView.addObject(key, value);
-            log.info("key = {}, value = {}", key, value);
-        }
     }
 }
