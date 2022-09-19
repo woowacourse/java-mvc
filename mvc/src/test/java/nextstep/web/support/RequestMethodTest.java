1d807d3cc4ce0d12834eb4d1c32411ded536e354
김찬 <gmelddl514@gmail.com>
2022-09-19 19:24:24 +0900

    refactor: RequestMethod 조회 로직 수정

--- mvc/src/test/java/nextstep/web/support/RequestMethodTest.java
+++ mvc/src/test/java/nextstep/web/support/RequestMethodTest.java
@@ -3,21 +3,19 @@ package nextstep.web.support;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 
 class RequestMethodTest {
 
     @Test
     void 해당하는_RequestMethod가_없으면_예외가_발생한다() {
-        assertThatThrownBy(() -> RequestMethod.from("None"))
-                .isInstanceOf(NoSuchElementException.class)
-                .hasMessage("해당하는 HttpMethod가 존재하지 않습니다.");
+        assertThatThrownBy(() -> RequestMethod.valueOf("None"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 해당하는_RequestMethod를_찾는다() {
-        final RequestMethod actual = RequestMethod.from("GET");
+        final RequestMethod actual = RequestMethod.valueOf("GET");
 
         assertThat(actual).isEqualTo(RequestMethod.GET);
     }
