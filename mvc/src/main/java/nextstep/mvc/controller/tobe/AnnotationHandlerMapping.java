3f9941e80bb11dec302cd3f85f12063d981936f5
bomi <bonita9931@gmail.com>
2022-09-19 16:30:17 +0900

    feat: AnnotationHandlerMapping에서 method reflection으로 가져오는 로직 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,16 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -20,7 +24,41 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
+    /*
+            Class<Junit4Test> clazz = Junit4Test.class;
+
+        Junit4Test instance = clazz.getConstructor()
+                .newInstance();
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> invoke(instance, method));
+     */
+
+
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+
+        final var classes = new HashSet<Class<?>>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+
+        for (Class<?> aClass : classes) {
+            try {
+                Object controller = aClass.getConstructor().newInstance();
+                Arrays.stream(aClass.getDeclaredMethods())
+                        .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                        .forEach(method -> {
+                            log.info(method.getName());
+                            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                            HandlerKey key = new HandlerKey(annotation.value(), annotation.method()[0]);
+                            HandlerExecution value = new HandlerExecution();
+                            handlerExecutions.put(key, value);
+                        });
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
