3b95bd7327fd351d4cb124bafb4784557ae0cc54
Hongdonggeon <ghd700@naver.com>
2022-09-19 13:25:27 +0900

    remove: 불필요한 메서드 제거

--- app/src/main/java/com/techcourse/controller/TestController.java
+++ app/src/main/java/com/techcourse/controller/TestController.java
@@ -18,15 +18,7 @@ public class TestController {
     @RequestMapping(value = "/get-test", method = RequestMethod.GET)
     public ModelAndView findUserId(final HttpServletRequest request, final HttpServletResponse response) {
         log.info("test controller get method");
-        final var modelAndView = new ModelAndView(new JspView(""));
-        modelAndView.addObject("id", request.getAttribute("id"));
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/post-test", method = RequestMethod.POST)
-    public ModelAndView save(final HttpServletRequest request, final HttpServletResponse response) {
-        log.info("test controller post method");
-        final var modelAndView = new ModelAndView(new JspView(""));
+        final var modelAndView = new ModelAndView(new JspView("index.jsp"));
         modelAndView.addObject("id", request.getAttribute("id"));
         return modelAndView;
     }
