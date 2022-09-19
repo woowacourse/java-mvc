7d35e9766ae026652e74a318c2a72581075297de
dwl21 <nggus5@naver.com>
2022-09-19 19:36:42 +0900

    test: DispatcherServlet 작성

---
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/DispatcherServletTest.java
@@ -0,0 +1,52 @@
+package nextstep.mvc.controller.tobe;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.DispatcherServlet;
+import nextstep.mvc.exception.NotFoundHandlerException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DispatcherServletTest {
+
+    private DispatcherServlet dispatcherServlet;
+
+    @BeforeEach
+    void setUp() {
+        dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping("samples"));
+        dispatcherServlet.init();
+    }
+
+    @Test
+    @DisplayName("uri, http method에 해당하는 핸들러를 실행한다.")
+    void success() {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        assertDoesNotThrow(() -> dispatcherServlet.service(request, response));
+    }
+
+    @Test
+    @DisplayName("해당하는 핸들러를 찾지 못하면 예외를 발생시킨다.")
+    void fail_not_found_handler() {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getRequestURI()).thenReturn("/invalid");
+        when(request.getMethod()).thenReturn("GET");
+
+        assertThatThrownBy(() -> dispatcherServlet.service(request, response))
+            .isInstanceOf(NotFoundHandlerException.class);
+    }
+}
