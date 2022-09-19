4ad2a43d3999c808c88be6080e183406a7b8177d
tonic523 <diddmlvkf@gmail.com>
2022-09-19 19:53:21 +0900

    feat: AnnotationHandlerMapping 구현

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # @MVC 구현하기
+
+## 🚀 1단계 - @MVC 프레임워크 구현하기
+
+- [X] AnnotationHandlerMappingTest가 정상 동작한다.
+- [ ] DispatcherServlet에서 HandlerMapping 인터페이스를 활용하여 AnnotationHandlerMapping과 ManualHandlerMapping 둘다 처리할 수 있다.
+
+### 구현한 기능
+- `AnnotationHandlerMapping`에서 `Map<HandlerKey, HandlerExecution>` 를 초기화한다.
--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,16 +1,17 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import nextstep.mvc.HandlerMapping;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
-
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -21,10 +22,41 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerClass : controllers) {
+            initHandlerExecutions(getInstance(controllerClass), controllerClass.getDeclaredMethods());
+        }
+    }
+
+    private void initHandlerExecutions(Object controllerInstance, Method[] methods) {
+        for (Method method : methods) {
+            putHandlerKeyByHandlerExecutor(controllerInstance, method);
+        }
+    }
+
+    private void putHandlerKeyByHandlerExecutor(Object controllerInstance, Method method) {
+        RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+        String value = requestMapping.value();
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            handlerExecutions.put(new HandlerKey(value, requestMethod),
+                    new HandlerExecution(controllerInstance, method));
+        }
+    }
+
+    private Object getInstance(Class<?> controller) {
+        try {
+            return controller.getConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(uri, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
