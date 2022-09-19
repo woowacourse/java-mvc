815d08d2669040fa10821f567330c5dad361b4e8
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-09-19 15:37:22 +0900

    feat: HandlerExecution 필드 추가

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,9 +2,18 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
+
+    private final Object object;
+    private final Method method;
+
+    public HandlerExecution(Object object, Method method) {
+        this.object = object;
+        this.method = method;
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         return null;
