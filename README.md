c23aca9f5f34ab5aef2348c3853f0bf35ae55819
youngwooyoo <fbduddn97@naver.com>
2022-09-20 11:46:45 +0900

    feat: HandlerExecution.handle() 구현 -> invoke

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # @MVC 구현하기
 
 ## step1 요구 사항 
-- [ ] AnnotationHandlerMappingTest가 정상 동작한다.
+- [x] AnnotationHandlerMappingTest가 정상 동작한다.
 - [ ] DispatcherServlet에서 HandlerMapping 인터페이스를 활용하여 AnnotationHandlerMapping과 ManualHandlerMapping 둘다 처리할 수 있다.
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,24 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HandlerExecution {
 
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(Object instance, Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerExecution.class);
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
