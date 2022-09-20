5707e203aec87f55a74e58dcc7f376dfb3e738a8
Richard JEON <ztzy1907@gmail.com>
2022-09-20 13:39:05 +0900

    test: 인터페이스 기반, 애너테이션 기반 support 테스트 구현

---
+++ mvc/src/test/java/nextstep/mvc/ControllerMappingHandlerAdapterTest.java
@@ -0,0 +1,55 @@
+package nextstep.mvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.stream.Stream;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ControllerMappingHandlerAdapterTest {
+
+    @MethodSource("methodSource")
+    @ParameterizedTest(name = " : {0}")
+    void controllerMappingHandlerAdapter_supports_controller_type(String description, Object handler,
+                                                                  boolean expected) {
+        // given
+        final ControllerMappingHandlerAdapter controllerMappingHandlerAdapter = new ControllerMappingHandlerAdapter();
+
+        // when
+        final boolean actual = controllerMappingHandlerAdapter.supports(handler);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> methodSource() {
+        return Stream.of(
+                Arguments.of(
+                        "ControllerMappingHandlerAdapter는 Controller 인터페이스 타입 외엔 support 하지 않는다",
+                        new HandlerExecution(null, null),
+                        false
+                ),
+                Arguments.of(
+                        "ControllerMappingHandlerAdapter는 Controller 인터페이스 타입만 support 한다",
+                        new Controller() {
+                            @Override
+                            public String execute(final HttpServletRequest req, final HttpServletResponse res)
+                                    throws Exception {
+                                return null;
+                            }
+                        },
+                        true
+                ),
+                Arguments.of(
+                        "ControllerMappingHandlerAdapter는 Controller 인터페이스 타입 외엔 support 하지 않는다",
+                        null,
+                        false
+                )
+        );
+    }
+}
---
+++ mvc/src/test/java/nextstep/mvc/RequestMappingHandlerAdapterTest.java
@@ -0,0 +1,55 @@
+package nextstep.mvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.stream.Stream;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class RequestMappingHandlerAdapterTest {
+
+    @MethodSource("methodSource")
+    @ParameterizedTest(name = " : {0}")
+    void requestMappingHandlerAdapter_supports_handlerExecution_type(String description, Object handler,
+                                                                     boolean expected) {
+        // given
+        final RequestMappingHandlerAdapter requestMappingHandlerAdapter = new RequestMappingHandlerAdapter();
+
+        // when
+        final boolean actual = requestMappingHandlerAdapter.supports(handler);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> methodSource() {
+        return Stream.of(
+                Arguments.of(
+                        "RequestMappingHandlerAdapter는 HandlerExecution을 support한다",
+                        new HandlerExecution(null, null),
+                        true
+                ),
+                Arguments.of(
+                        "RequestMappingHandlerAdapter는 HandlerExecution 외엔 support 하지 않는다",
+                        new Controller() {
+                            @Override
+                            public String execute(final HttpServletRequest req, final HttpServletResponse res)
+                                    throws Exception {
+                                return null;
+                            }
+                        },
+                        false
+                ),
+                Arguments.of(
+                        "RequestMappingHandlerAdapter는 HandlerExecution 외엔 support 하지 않는다",
+                        null,
+                        false
+                )
+        );
+    }
+}
