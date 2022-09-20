a0da391a2ee4d5d4008f1f2d337e9e811c2daa43
SuyeonChoi <tndus970120@gmail.com>
2022-09-20 12:02:19 +0900

    feat(AppWebApplicationInitializer): AnnotationHandlerMapping를 dispatcherServlet에서 처리할 수 있도록 추가

--- app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
+++ app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
@@ -2,6 +2,7 @@ package com.techcourse;
 
 import jakarta.servlet.ServletContext;
 import nextstep.mvc.DispatcherServlet;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +10,13 @@ import org.slf4j.LoggerFactory;
 public class AppWebApplicationInitializer implements WebApplicationInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(AppWebApplicationInitializer.class);
+    private static final String BASE_PACKAGE = "com.techcourse.controller";
 
     @Override
     public void onStartup(final ServletContext servletContext) {
         final var dispatcherServlet = new DispatcherServlet();
         dispatcherServlet.addHandlerMapping(new ManualHandlerMapping());
+        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping(BASE_PACKAGE));
 
         final var dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
         dispatcher.setLoadOnStartup(1);
