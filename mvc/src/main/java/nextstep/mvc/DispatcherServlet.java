9c867e5ac8a26a61f4c167487c52d5012112d1cf
EastHShin <xldk78@gmail.com>
2022-09-20 12:53:17 +0900

    feat: AnnotationHandlerMapping 도 작동하도록 구현

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.HandlerExecution;
 import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
 import nextstep.mvc.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,9 +42,8 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final var controller = getController(request);
-            final var viewName = controller.execute(request, response);
-            final View view = new JspView(viewName);
+            final Object handler = getController(request);
+            View view = getView(handler, request, response);
             view.render(Collections.emptyMap(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
@@ -50,12 +51,22 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
-    private Controller getController(final HttpServletRequest request) {
+    private View getView(Object handler, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (handler == Controller.class){
+            final Controller controller = (Controller) handler;
+            final String viewName = controller.execute(request, response);
+            return new JspView(viewName);
+        }
+        final HandlerExecution execution = (HandlerExecution) handler;
+        final ModelAndView modelAndView = execution.handle(request, response);
+        return modelAndView.getView();
+    }
+
+    private Object getController(final HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
                 .filter(Objects::nonNull)
-                .map(Controller.class::cast)
-                .findFirst()
+                .findAny()
                 .orElseThrow();
     }
 }
