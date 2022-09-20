daac902fd5482725ea3ec3d1114ae940fb5ec051
leokim <kunga077@gmail.com>
2022-09-21 03:13:13 +0900

    refactor: request url validation 추가

--- mvc/src/main/java/nextstep/web/support/RequestUrl.java
+++ mvc/src/main/java/nextstep/web/support/RequestUrl.java
@@ -1,11 +1,20 @@
 package nextstep.web.support;
 
+import nextstep.web.exception.InvalideRequestFormatException;
+
 public class RequestUrl {
 
     private final String value;
 
     public RequestUrl(final String value) {
+        validateRequestFormat(value);
         this.value = value;
+    }
+
+    private void validateRequestFormat(final String value) {
+        if (!value.startsWith("/")) {
+            throw new InvalideRequestFormatException();
+        }
     }
 
     @Override
