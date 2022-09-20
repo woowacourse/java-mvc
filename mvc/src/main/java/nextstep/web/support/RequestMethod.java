cd223260f4d2aa3690ce844f619beb9e4ce19b1f
Richard JEON <ztzy1907@gmail.com>
2022-09-20 12:32:30 +0900

    feat: RequestMethod 정적 팩터리 메서드 구현

--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,24 @@
 package nextstep.web.support;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE,
+    ;
+
+    public static RequestMethod from(String httpMethod) {
+        return Arrays.stream(values())
+                .filter(requestMethod -> requestMethod.name().equalsIgnoreCase(httpMethod))
+                .findAny()
+                .orElseThrow(NoSuchElementException::new);
+    }
 }
