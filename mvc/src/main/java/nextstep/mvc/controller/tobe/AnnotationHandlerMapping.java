07e20875e5881544163a13f4943cf51c80df2b8c
youngwooyoo <fbduddn97@naver.com>
2022-09-20 11:46:16 +0900

    feat: AnnotationHandlerMapping.initialize() 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,20 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -21,10 +29,36 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        HashSet<Class<?>> classes = new HashSet<>(reflections.getTypesAnnotatedWith(Controller.class));
+
+        for (Class<?> clazz : classes) {
+            try {
+                Object instance = clazz.getDeclaredConstructor().newInstance();
+                List<Method> methods = Stream.of(clazz.getDeclaredMethods())
+                        .filter(x -> x.isAnnotationPresent(RequestMapping.class))
+                        .collect(Collectors.toList());
+
+                for (Method method : methods) {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    String url = annotation.value();
+                    RequestMethod requestMethod = annotation.method()[0];
+                    HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                    HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod method = RequestMethod.find(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, method);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/nextstep/web/support/RequestMethod.java
+++ mvc/src/main/java/nextstep/web/support/RequestMethod.java
@@ -1,5 +1,14 @@
 package nextstep.web.support;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod find(String input) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(value -> value.name().equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 Request method가 없다."));
+    }
 }
