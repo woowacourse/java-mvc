4142ed4dd72bc5cc8f235388fa24307a47dff787
Richard JEON <ztzy1907@gmail.com>
2022-09-20 12:36:27 +0900

    feat: RequestMapping 기반 핸들러 어댑터 구현

---
+++ mvc/src/main/java/nextstep/mvc/RequestMappingHandlerAdapter.java
@@ -0,0 +1,23 @@
+package nextstep.mvc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.ModelAndView;
+
+public class RequestMappingHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
+        final var handlerExecution = (HandlerExecution) handler;
+        final var modelAndView = handlerExecution.handle(request, response);
+
+        return modelAndView;
+    }
+}
