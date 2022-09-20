23249d15ef1c063c3e0c9d4ef7fe483f39c9c6e1
이한울 <dkfghg3@naver.com>
2022-09-20 11:33:49 +0900

    refactor: 메서드 분리

--- mvc/src/main/java/nextstep/mvc/view/JspView.java
+++ mvc/src/main/java/nextstep/mvc/view/JspView.java
@@ -1,7 +1,9 @@
 package nextstep.mvc.view;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +12,6 @@ import java.util.Map;
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
-
     public static final String REDIRECT_PREFIX = "redirect:";
 
     private final String viewName;
@@ -22,15 +23,26 @@ public class JspView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            redirect(response);
             return;
         }
+        setAttribute(model, request);
+        forward(request, response);
+    }
 
+    private void redirect(final HttpServletResponse response) throws IOException {
+        response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+    }
+
+    private void setAttribute(final Map<String, ?> model, final HttpServletRequest request) {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
+    }
 
+    private void forward(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
         final var requestDispatcher = request.getRequestDispatcher(viewName);
         requestDispatcher.forward(request, response);
     }
