930748d2dd0502329f543324472a0f3685166f06
JoJo Green <82805588+jojogreen91@users.noreply.github.com>
2022-09-21 15:06:31 +0900

    [MVC 구현하기 - 1단계] 조조그린(조한석) 미션 제출합니다. (#175)
    * feat: View 구현
    
    * feat: Adapter 구현
    
    * refactor: 패키지 변경
    
    * feat: HandlerMapping 구현
    
    * feat: DispatcherServlet 구현
    
    * refactor: ManualHandlerMapping 수정

--- app/src/main/java/com/techcourse/controller/asis/UserSession.java
+++ app/src/main/java/com/techcourse/controller/asis/UserSession.java
@@ -1,8 +1,7 @@
-package com.techcourse.controller;
+package com.techcourse.controller.asis;
 
 import com.techcourse.domain.User;
 import jakarta.servlet.http.HttpSession;
-
 import java.util.Optional;
 
 public class UserSession {
