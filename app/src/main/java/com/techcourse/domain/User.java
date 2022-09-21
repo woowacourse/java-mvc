849532d322552d72e46f196e7918c3bd6d8d6b0e
Richard JEON <ztzy1907@gmail.com>
2022-09-21 15:00:18 +0900

    feat: User 객체 id할당 메서드 구현

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -1,5 +1,7 @@
 package com.techcourse.domain;
 
+import java.util.Objects;
+
 public class User {
 
     private final long id;
@@ -12,6 +14,22 @@ public class User {
         this.account = account;
         this.password = password;
         this.email = email;
+
+        validate();
+    }
+
+    private void validate() {
+        if (Objects.isNull(this.password) || doesNotHaveText()) {
+            throw new IllegalArgumentException(String.format("password를 확인해주세요 : %s", this.password));
+        }
+    }
+
+    private boolean doesNotHaveText() {
+        return password.strip().length() == 0;
+    }
+
+    public User(String account, String password, String email) {
+        this(0L, account, password, email);
     }
 
     public boolean checkPassword(String password) {
@@ -20,6 +38,10 @@ public class User {
 
     public String getAccount() {
         return account;
+    }
+
+    public User assignId(long id) {
+        return new User(id, this.account, this.password, this.email);
     }
 
     @Override
