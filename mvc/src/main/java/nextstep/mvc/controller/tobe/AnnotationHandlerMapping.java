714fec09c142fe7bbe8aeff6d82bc5b7be617d74
EastHShin <xldk78@gmail.com>
2022-09-21 13:15:48 +0900

    refactor(AnnotationHandlerMapping): requestMapping 이 없을 경우 handlerExecutions에 넣는 동작을 하지 않도록 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -3,9 +3,12 @@ package nextstep.mvc.controller.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
@@ -29,37 +32,28 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        Set<Class<?>> controllerClasses = getControllerClasses();
+        Set<Class<?>> handlerClasses = getHandlerClasses();
 
-        for (Class<?> controllerClass : controllerClasses) {
-            initializeHandlerExecutions(controllerClass);
+        for (Class<?> handlerClass : handlerClasses) {
+            initializeHandlerExecutions(handlerClass);
         }
 
         logPathAndController();
     }
 
-    private Set<Class<?>> getControllerClasses() {
+    private Set<Class<?>> getHandlerClasses() {
         final Reflections reflections = new Reflections(basePackages);
         return reflections.getTypesAnnotatedWith(Controller.class);
     }
 
     private void initializeHandlerExecutions(Class<?> controllerClass) {
         final Object controller = newInstanceOf(controllerClass);
-        final Method[] declaredMethods = controllerClass.getDeclaredMethods();
-        for (Method controllerMethod : declaredMethods) {
-            final HandlerExecution handlerExecution = new HandlerExecution(controller, controllerMethod);
+        final List<Method> handlerMethods = extractValidHandler(controllerClass);
 
-            putToHandlerExecutions(controllerMethod, handlerExecution);
-        }
-    }
+        for (Method handlerMethod : handlerMethods) {
+            final HandlerExecution handlerExecution = new HandlerExecution(controller, handlerMethod);
 
-    private void putToHandlerExecutions(Method controllerMethod, HandlerExecution handlerExecution) {
-        final RequestMapping requestMapping = controllerMethod.getAnnotation(RequestMapping.class);
-        final String url = requestMapping.value();
-        final RequestMethod[] requestMethods = requestMapping.method();
-        for (RequestMethod requestMethod : requestMethods) {
-            final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-            handlerExecutions.put(handlerKey, handlerExecution);
+            putToHandlerExecutions(handlerMethod, handlerExecution);
         }
     }
 
@@ -73,11 +67,43 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         }
     }
 
+    private List<Method> extractValidHandler(Class<?> controllerClass) {
+        final Method[] declaredMethods = controllerClass.getDeclaredMethods();
+        return Arrays.stream(declaredMethods)
+                .filter(this::isValidRequestMapping)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isValidRequestMapping(Method controllerMethod){
+        final RequestMapping requestMapping = controllerMethod.getAnnotation(RequestMapping.class);
+        if (requestMapping == null) {
+            return false;
+        }
+
+        if (requestMapping.value().isEmpty()){
+            return false;
+        }
+
+        return requestMapping.method().length > 0;
+    }
+
+    private void putToHandlerExecutions(Method controllerMethod, HandlerExecution handlerExecution) {
+        final RequestMapping requestMapping = controllerMethod.getAnnotation(RequestMapping.class);
+
+        final String url = requestMapping.value();
+        final RequestMethod[] requestMethods = requestMapping.method();
+
+        for (RequestMethod requestMethod : requestMethods) {
+            final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
     private void logPathAndController() {
         handlerExecutions.forEach(
                 (handlerKey, handlerExecution) ->
                         log.info("Path : " + handlerKey.getRequestMethod() + " " + handlerKey.getUrl() +
-                                "Controller : " + handlerExecution.getController().getClass()));
+                                ", Controller : " + handlerExecution.getController().getClass()));
     }
 
     @Override
