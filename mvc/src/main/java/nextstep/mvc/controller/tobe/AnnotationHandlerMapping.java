213ef2632b531661517b1ec994ad1ef3d3e937df
Kim Hyuntae <75936123+Hyunta@users.noreply.github.com>
2022-09-21 14:01:07 +0900

    [MVC 구현하기 - 1단계] 아서(김현태) 미션 제출합니다. (#149)
    * feat: AnnotationHandlerMapping이 초기화하는 기능 구현
    
    - Reflections를 통해 basePackage에서 @Controller 를 갖고있는 클래스를 가져온다.
    - @RequestMapping이 있는 메서드를 받아온다.
    - HttpUri와 HttpMethod를 key로 HandlerExecution을 value로 초기화한다.
    - HandlerExecution 은 클래스 정보와 해당 메서드를 이용해 실행된다.
    
    * refactor: @Override 애노테이션 추가
    
    * feat: DispatcherServlet에서 AnnotationHandlerMapping도 처리할 수 있도록 변경
    
    * feat: AnnotationMapping 확인용 TestController 추가
    
    * chore: logback.xml 추가
    
    * feat: getDeclaredMethods -> getMethods로 변경
    
    - public 접근 제어자의 메서드만 제공받기 위해 수정
    
    * feat: HandlerExecution 에서 clazz를 삭제
    
    * refactor: Reflections의 동적 매개변수 생성자를 사용
    
    - 불필요한 for문 삭제

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,19 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
@@ -20,11 +27,35 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
+    @Override
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        for (Class<?> controller : reflections.getTypesAnnotatedWith(Controller.class)) {
+            List<Method> methods = getRequestMappingMethods(controller);
+            methods.forEach(this::setUpHandlerExecutions);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private List<Method> getRequestMappingMethods(Class<?> controller) {
+        return Arrays.stream(controller.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private void setUpHandlerExecutions(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            HandlerKey key = new HandlerKey(requestMapping.value(), requestMethod);
+            HandlerExecution value = new HandlerExecution(method);
+            handlerExecutions.put(key, value);
+            log.info("Path : {} , Method : {}", requestMapping.value(), requestMethod);
+        }
+    }
+
+    @Override
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,19 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        Object instance = method.getDeclaringClass().getConstructor().newInstance();
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
