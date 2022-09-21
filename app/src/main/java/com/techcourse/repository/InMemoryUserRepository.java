5a4a20684520e1008a5958088378ec0464d7a0c9
Richard JEON <ztzy1907@gmail.com>
2022-09-21 15:00:49 +0900

    feat: InMemoryUserRepository User 저장 시 id 할당하도록 구현

--- app/src/main/java/com/techcourse/repository/InMemoryUserRepository.java
+++ app/src/main/java/com/techcourse/repository/InMemoryUserRepository.java
@@ -1,27 +1,34 @@
 package com.techcourse.repository;
 
 import com.techcourse.domain.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryUserRepository {
 
+    private static long ID_SEQUENCE = 1L;
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final var user = new User(1, "gugu", "password", "hkkang@woowahan.com");
+        final var user = new User(ID_SEQUENCE, "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
     public static void save(User user) {
-        database.put(user.getAccount(), user);
+        final var userIdAssigned = assignIdToUser(user);
+
+        database.put(userIdAssigned.getAccount(), userIdAssigned);
+    }
+
+    private synchronized static User assignIdToUser(User user) {
+        return user.assignId(++ID_SEQUENCE);
     }
 
     public static Optional<User> findByAccount(String account) {
         return Optional.ofNullable(database.get(account));
     }
 
-    private InMemoryUserRepository() {}
+    private InMemoryUserRepository() {
+    }
 }
