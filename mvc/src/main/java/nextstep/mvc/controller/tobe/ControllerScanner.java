b542e09bacd78a8207631951965ea3c6a745c80c
yeon-06 <rnjstldus2@gmail.com>
2022-09-22 16:54:45 +0900

    refactor: Controller 스캔하는 로직 ControllerScanner로 분리

---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
@@ -0,0 +1,36 @@
+package nextstep.mvc.controller.tobe;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+
+public class ControllerScanner {
+
+    private final Map<Class<?>, Object> controllers = new HashMap<>();
+
+    public ControllerScanner(final Object[] basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        initialize(controllers);
+    }
+
+    private void initialize(final Set<Class<?>> controllers) {
+        controllers.forEach(it -> this.controllers.put(it, newInstance(it)));
+    }
+
+    private Object newInstance(final Class<?> controller) {
+        try {
+            return controller.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        return controllers;
+    }
+}
