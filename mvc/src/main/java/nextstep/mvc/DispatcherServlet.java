c2c98e2962b6ea4d9f36ecc9cdea1f6a8ffeae5c
최진영 <jinyoungchoi95@gmail.com>
2022-09-22 13:08:34 +0900

    [MVC 구현하기 - 1단계] 오리(최진영) 미션 제출합니다. (#172)
    * feat: AnnotationHandlerMapping 구현
    
    * feat: DispatcherServlet이 handler 지원하도록 수정
    
    * test: DispatcherServlet 테스트 추가
    
    * refactor: 개행 수정
    
    * feat: handler not found 예외 추가 및 테스트
    
    * refactor: Reflections 생성 방식 변경
    
    * refactor: render 메소드 네이밍 수정

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -4,14 +4,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.view.JspView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DispatcherServlet extends HttpServlet {
 
@@ -38,31 +39,34 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final var controller = getController(request);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            final var handler = getHandler(request);
+            final var modelAndView = executeHandler(handler, request, response);
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private Controller getController(final HttpServletRequest request) {
+    private Object getHandler(final HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
                 .filter(Objects::nonNull)
-                .map(Controller.class::cast)
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(() -> new RuntimeException("요청을 처리할 수 없습니다."));
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+    private ModelAndView executeHandler(final Object handler, final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+        if (HandlerExecution.class.isAssignableFrom(handler.getClass())) {
+            return ((HandlerExecution) handler).handle(request, response);
+        }
+        if (Controller.class.isAssignableFrom(handler.getClass())) {
+            String viewName = ((Controller) handler).execute(request, response);
+            JspView jspView = new JspView(viewName);
+            return new ModelAndView(jspView);
         }
 
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+        throw new IllegalStateException("지원하는 handler가 존재하지 않습니다.");
     }
 }
