a15f252edeada9549df98e93092363413aba5e13
bcc0830 <bcc0830@bithub.com>
2022-09-22 14:29:36 +0900

    refactor : AnnotationHandlerMapping 초기화 시 메소드 분리

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -29,31 +29,46 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        List<Method> extractedMethods = searchAnnotatedMethods(reflections);
+        List<HandlerKey> handlerKeys = generateHandlerKeys(extractedMethods);
+        List<HandlerExecution> executions = generateHandlerExecutions(extractedMethods);
+        addHandlerExcutions(handlerKeys, executions);
         log.info("Initialized AnnotationHandlerMapping!");
-        preprocess();
     }
 
-    private void preprocess() {
-        Reflections reflections = new Reflections(basePackage);
-        List<Method> extractedMethods = reflections.getTypesAnnotatedWith(Controller.class).stream()
-                .flatMap(each -> Arrays.stream(each.getMethods())
-                        .filter(temp -> temp.isAnnotationPresent(RequestMapping.class)))
-                .collect(Collectors.toUnmodifiableList());
-        List<HandlerKey> handlerKeys = extractedMethods.stream()
-                .flatMap(each ->
-                        Arrays.stream(each.getAnnotation(RequestMapping.class).method())
-                                .map(requestMethod -> new HandlerKey(each.getAnnotation(RequestMapping.class).value(),
-                                        requestMethod)))
-                .collect(Collectors.toUnmodifiableList());
-        List<HandlerExecution> executions = extractedMethods.stream()
-                .map(HandlerExecution::new)
-                .collect(Collectors.toUnmodifiableList());
+    private void addHandlerExcutions(List<HandlerKey> handlerKeys, List<HandlerExecution> executions) {
         if (handlerKeys.size() != executions.size()) {
             throw new IllegalArgumentException("Handler Key의 갯수와 Handler Execution의 갯수가 동일하지 않습니다.");
         }
         for (int i = 0; i < handlerKeys.size(); i++) {
             this.handlerExecutions.put(handlerKeys.get(i), executions.get(i));
         }
+    }
+
+    private List<HandlerExecution> generateHandlerExecutions(List<Method> extractedMethods) {
+        List<HandlerExecution> executions = extractedMethods.stream()
+                .map(HandlerExecution::new)
+                .collect(Collectors.toUnmodifiableList());
+        return executions;
+    }
+
+    private List<HandlerKey> generateHandlerKeys(List<Method> extractedMethods) {
+        List<HandlerKey> handlerKeys = extractedMethods.stream()
+                .flatMap(each ->
+                        Arrays.stream(each.getAnnotation(RequestMapping.class).method())
+                                .map(requestMethod -> new HandlerKey(each.getAnnotation(RequestMapping.class).value(),
+                                        requestMethod)))
+                .collect(Collectors.toUnmodifiableList());
+        return handlerKeys;
+    }
+
+    private List<Method> searchAnnotatedMethods(Reflections reflections) {
+        List<Method> extractedMethods = reflections.getTypesAnnotatedWith(Controller.class).stream()
+                .flatMap(each -> Arrays.stream(each.getMethods())
+                        .filter(temp -> temp.isAnnotationPresent(RequestMapping.class)))
+                .collect(Collectors.toUnmodifiableList());
+        return extractedMethods;
     }
 
     public Object getHandler(HttpServletRequest request) {
