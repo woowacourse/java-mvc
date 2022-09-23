987857af039de664e638fb9fa1f91728d83d7106
hyunrrr <whgusrms96@gmail.com>
2022-09-23 15:16:14 +0900

    test: 어노테이션 기반 registerView 컨트롤러 테스트 구현

---
+++ app/src/test/java/com/techcourse/controller/annotation/RegisterViewControllerTest.java
@@ -0,0 +1,48 @@
+package com.techcourse.controller.annotation;
+
+import static nextstep.mvc.view.JspView.REDIRECT_PREFIX;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Field;
+import nextstep.mvc.HandlerMapping;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.mvc.view.View;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterViewControllerTest {
+
+    private HandlerMapping handlerMapping = new AnnotationHandlerMapping("com.techcourse");
+
+    @BeforeEach
+    void setUp() {
+        handlerMapping.initialize();
+    }
+
+    @Test
+    @DisplayName("회원가입 페이지 접근")
+    void execute() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getRequestURI()).thenReturn("/register/view");
+        when(request.getMethod()).thenReturn("GET");
+
+        HandlerExecution handler = (HandlerExecution) handlerMapping.getHandler(request);
+        ModelAndView modelAndView = handler.handle(request, response);
+
+        Class<? extends View> aClass = modelAndView.getView().getClass();
+        Field field = aClass.getDeclaredField("viewName");
+        field.setAccessible(true);
+
+        assertThat(field.get(modelAndView.getView())).isEqualTo("/register.jsp");
+    }
+}
