6333cd203f87111fcbefa54bfe70709ce70f83b6
brorae <jhr3239@gmail.com>
2022-09-23 14:03:30 +0900

    [MVC 구현하기 - 1단계] 레넌(조형래) 미션 제출합니다. (#170)
    * refactor: AnnotationHandlerMapping 수정
    
    * refactor: HandlerAdapter 추가
    
    * refactor: reflection 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -2,11 +2,19 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        final Object controller = method.getDeclaringClass().getDeclaredConstructor().newInstance();
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
