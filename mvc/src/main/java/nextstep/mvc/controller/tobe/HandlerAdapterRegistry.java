7d7832b65ed9217125d634fb32ec3dc1c0fd0d41
skullKim <kyk990328@gmail.com>
2022-09-24 17:47:41 +0900

    feat: HandlerAdapterRegistry, HandlerMappingRegistry 구현

---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerAdapterRegistry.java
@@ -0,0 +1,25 @@
+package nextstep.mvc.controller.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+import nextstep.mvc.HandlerAdapter;
+
+public class HandlerAdapterRegistry {
+
+    private final List<HandlerAdapter> handlerAdapters;
+
+    public HandlerAdapterRegistry() {
+        handlerAdapters = new ArrayList<>();
+    }
+
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.supports(handler))
+                .findFirst()
+                .orElseThrow();
+    }
+}
