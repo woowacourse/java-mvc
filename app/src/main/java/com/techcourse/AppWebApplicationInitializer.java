b4a8adc3e38a0a4c68cc868e56d7bce5c643cb42
summerlunaa <summerlunaa9@gmail.com>
2022-09-24 20:08:39 +0900

    refactor: 패키지 경로를 하드코딩하지 않고 메서드를 사용해서 가져오도록 변경

--- app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
+++ app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
@@ -3,8 +3,8 @@ package com.techcourse;
 import jakarta.servlet.ServletContext;
 import nextstep.mvc.DispatcherServlet;
 import nextstep.mvc.controller.tobe.handleradapter.AnnotationHandlerAdapter;
-import nextstep.mvc.controller.tobe.handlermapping.AnnotationHandlerMapping;
 import nextstep.mvc.controller.tobe.handleradapter.ManualHandlerAdapter;
+import nextstep.mvc.controller.tobe.handlermapping.AnnotationHandlerMapping;
 import nextstep.web.WebApplicationInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +17,7 @@ public class AppWebApplicationInitializer implements WebApplicationInitializer {
     public void onStartup(final ServletContext servletContext) {
         final var dispatcherServlet = new DispatcherServlet();
         dispatcherServlet.addHandlerMapping(new ManualHandlerMapping());
-        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
+        dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping(getClass().getPackageName()));
 
         dispatcherServlet.addHandlerAdapter(new ManualHandlerAdapter());
         dispatcherServlet.addHandlerAdapter(new AnnotationHandlerAdapter());
