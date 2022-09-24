a9aead107996594a9acefa3f34345273aa96e6f2
yhh1056 <yhh1056@naver.com>
2022-09-25 03:13:37 +0900

    refactor: 어노테이션 기반 컨트롤러로 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -21,10 +21,8 @@ public class ManualHandlerMapping implements HandlerMapping {
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
         controllers.put("/login", new LoginController());
-//        controllers.put("/login/view", new LoginViewController());  해당 요청은 annotation 기반으로 처리
+        controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
