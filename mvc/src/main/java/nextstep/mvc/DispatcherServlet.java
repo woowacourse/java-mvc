1412418ebdc0f9b7a6bcd5a3648f7070258e2712
EastHShin <xldk78@gmail.com>
2022-09-25 02:03:23 +0900

    refactor: HandlerExecutor 가 ModelAndView를 반환해 legacy 컨트롤러와 annotation 컨트롤러 동시 작동

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -4,14 +4,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Collections;
 import java.util.Optional;
 import nextstep.mvc.controller.HandlerMappingRegistry;
-import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.controller.tobe.HandlerExecution;
-import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
-import nextstep.mvc.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +16,11 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private final HandlerMappingRegistry handlerMappingRegistry;
+    private final HandlerExecutor handlerExecutor;
 
     public DispatcherServlet() {
         this.handlerMappingRegistry = new HandlerMappingRegistry();
+        this.handlerExecutor = new HandlerExecutor(new HandlerAdapterRegistry());
     }
 
     @Override
@@ -35,8 +32,13 @@ public class DispatcherServlet extends HttpServlet {
         handlerMappingRegistry.add(handlerMapping);
     }
 
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        handlerExecutor.addHandlerAdapter(handlerAdapter);
+    }
+
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
@@ -46,22 +48,11 @@ public class DispatcherServlet extends HttpServlet {
                 return;
             }
 
-            View view = getView(handler, request, response);
-            view.render(Collections.emptyMap(), request, response);
+            final ModelAndView modelAndView = handlerExecutor.handle(request, response, handler.get());
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private View getView(Object handler, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if (handler instanceof Controller){
-            final Controller controller = (Controller) handler;
-            final String viewName = controller.execute(request, response);
-            return new JspView(viewName);
-        }
-        final HandlerExecution execution = (HandlerExecution) handler;
-        final ModelAndView modelAndView = execution.handle(request, response);
-        return modelAndView.getView();
     }
 }
---
+++ mvc/src/main/java/nextstep/mvc/HandlerExecutor.java
@@ -0,0 +1,24 @@
+package nextstep.mvc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.ModelAndView;
+
+public class HandlerExecutor {
+
+    private final HandlerAdapterRegistry handlerAdapterRegistry;
+
+    public HandlerExecutor(HandlerAdapterRegistry handlerAdapterRegistry) {
+        this.handlerAdapterRegistry = handlerAdapterRegistry;
+    }
+
+    public void addHandlerAdapter(HandlerAdapter handlerAdapter) {
+        handlerAdapterRegistry.add(handlerAdapter);
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        final HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
+        return handlerAdapter.handle(request, response, handler);
+    }
+}
