e8f04b8c2bd99dabfeef09e50e36d827e59aceb2
Donggyu <a29661498@gmail.com>
2022-09-24 22:50:41 +0900

    feat: 핸들러를 찾지 못하는 경우 404 응답하도록 구현

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,9 +1,12 @@
 package nextstep.mvc;
 
+import static jakarta.servlet.http.HttpServletResponse.*;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import nextstep.mvc.view.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,20 +38,22 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response)
-            throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
-        final Object handler = handlerMappingRegistry.getHandler(request);
-        final HandlerAdapter adapter = handlerAdapters.getHandlerAdapter(handler);
-
-        handleRequest(request, response, handler, adapter);
+        final Optional<Object> handler = handlerMappingRegistry.getHandler(request);
+        if (handler.isEmpty()) {
+            response.setStatus(SC_NOT_FOUND);
+            return;
+        }
+        final HandlerAdapter handlerAdapter = handlerAdapters.getHandlerAdapter(handler);
+        handleRequest(request, response, handler, handlerAdapter);
     }
 
     private static void handleRequest(final HttpServletRequest request, final HttpServletResponse response,
-                                  final Object handler, final HandlerAdapter adapter) throws ServletException {
+                                  final Object handler, final HandlerAdapter handlerAdapter) throws ServletException {
         try {
-            final ModelAndView modelAndView = adapter.handle(request, response, handler);
+            final ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
             modelAndView.renderView(request, response);
         } catch (Exception e) {
             log.error("Exception : {}", e.getMessage(), e);
