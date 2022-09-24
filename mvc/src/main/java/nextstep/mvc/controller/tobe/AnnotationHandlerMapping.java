1c5fa9eb79e3cf91ff74c03e2d2eecce34ca4d20
yeongunheo <gjduddns@gmail.com>
2022-09-24 14:13:24 +0900

    refactor: AnnotationHandlerMapping 클래스에서 mapHandlerKeys 메서드 분리

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,9 +2,12 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
@@ -30,22 +33,32 @@ public class AnnotationHandlerMapping implements HandlerMapping {
         ControllerScanner controllerScanner = new ControllerScanner(basePackage);
         Map<Class<?>, Object> controllers = controllerScanner.getControllers();
         for (Class<?> clazz : controllers.keySet()) {
-            Set<Method> methods = ReflectionUtils.getAllMethods(clazz,
-                    ReflectionUtils.withAnnotation(RequestMapping.class));
+            Set<Method> methods = getRequestMappingMethod(clazz);
             addAllHandlerExecutions(clazz, methods);
         }
     }
 
+    private Set<Method> getRequestMappingMethod(Class<?> clazz) {
+        return ReflectionUtils.getAllMethods(clazz,
+                ReflectionUtils.withAnnotation(RequestMapping.class));
+    }
+
     private void addAllHandlerExecutions(Class<?> clazz, Set<Method> methods) {
         for (Method method : methods) {
-            if (method.isAnnotationPresent(RequestMapping.class)) {
-                RequestMapping annotation = method.getAnnotation(RequestMapping.class);
-                String url = annotation.value();
-                RequestMethod requestMethod = annotation.method()[0];
-                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            String url = annotation.value();
+            RequestMethod[] requestMethods = annotation.method();
+            List<HandlerKey> handlerKeys = mapHandlerKeys(url, requestMethods);
+            for (HandlerKey handlerKey : handlerKeys) {
                 handlerExecutions.put(handlerKey, new HandlerExecution(clazz, method));
             }
         }
+    }
+
+    private List<HandlerKey> mapHandlerKeys(String url, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public Object getHandler(final HttpServletRequest request) {
