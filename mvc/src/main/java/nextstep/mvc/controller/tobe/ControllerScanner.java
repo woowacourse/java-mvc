c2769aae8c0ff7ac28b845aec8ae8af79917f354
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-24 14:22:38 +0900

    feat: ControllerScanner controller 인스턴스 생성 클래스 추가

---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
@@ -0,0 +1,30 @@
+package nextstep.mvc.controller.tobe;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import nextstep.mvc.util.ReflectionUtils;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+
+public class ControllerScanner {
+
+    private final Reflections reflections;
+
+    private ControllerScanner(final Reflections reflections) {
+        this.reflections = reflections;
+    }
+
+    public static ControllerScanner from(final Object[] basePackages) {
+        return new ControllerScanner(new Reflections(basePackages));
+    }
+
+    public Map<Class<?>, Object> getControllers() throws Exception {
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        Map<Class<?>, Object> controllers = new HashMap<>();
+        for (Class<?> controllerClass : controllerClasses) {
+            controllers.put(controllerClass, ReflectionUtils.createNewInstance(controllerClass));
+        }
+        return controllers;
+    }
+}
