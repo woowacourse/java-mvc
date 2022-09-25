74294b779cdb6b794ccffbd100249541068e7fd5
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-25 20:11:19 +0900

    test: ControllerScannerTest 추가

---
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -0,0 +1,23 @@
+package nextstep.mvc.controller.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import samples.TestController;
+
+class ControllerScannerTest {
+
+    @DisplayName("Controller을 정상적으로 가져오는지를 테스트한다.")
+    @Test
+    void getController() {
+        final ControllerScanner controllerScanner = new ControllerScanner("samples");
+        final Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+
+        assertThat(controllers.get(TestController.class)).isNotNull();
+    }
+}
