f70ea61152e06229a2e189b0d4ac5730e3574a28
Yunki Kim <37966705+skullkim@users.noreply.github.com>
2022-09-25 22:01:03 +0900

    [MVC 구현하기 - 2단계] 스컬(김윤기)미션 제출합니다. (#207)
    * feat: 어노테이션 컨트롤러와 기존 컨트롤러를 동시에 사용하게 구현
    
    * feat: HandlerAdapterRegistry, HandlerMappingRegistry 구현
    
    * refactor: 필요 없는 코드 삭제
    
    * fix: 테스트를 통과할 수 있게 코드 수정
    
    * feat: ControllerScanner를 통해 컨트롤러를 스캔하는 기능 추가
    
    * chore: 코딩 컨벤션에 맞추어 스타일 수정
    
    * refactor: 컨트롤러 통합

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -4,18 +4,19 @@ import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
+import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String getLoginPage(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         if (UserSession.isLoggedIn(req.getSession())) {
             return "redirect:/index.jsp";
         }
@@ -23,12 +24,12 @@ public class LoginController implements Controller {
         return InMemoryUserRepository.findByAccount(req.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
-                    return login(req, user);
+                    return getRedirectPage(req, user);
                 })
                 .orElse("redirect:/401.jsp");
     }
 
-    private String login(final HttpServletRequest request, final User user) {
+    private String getRedirectPage(final HttpServletRequest request, final User user) {
         if (user.checkPassword(request.getParameter("password"))) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
@@ -36,5 +37,15 @@ public class LoginController implements Controller {
         } else {
             return "redirect:/401.jsp";
         }
+    }
+
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public String login(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return UserSession.getUserFrom(req.getSession())
+                .map(user -> {
+                    log.info("logged in {}", user.getAccount());
+                    return "redirect:/index.jsp";
+                })
+                .orElse("/login.jsp");
     }
 }
--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -3,6 +3,7 @@ package nextstep.mvc.controller.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 
 public class HandlerExecution {
@@ -16,6 +17,10 @@ public class HandlerExecution {
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(handler, request, response);
+        final Object view = method.invoke(handler, request, response);
+        if (view instanceof String) {
+            return new ModelAndView(new JspView((String) view));
+        }
+        return (ModelAndView) view;
     }
 }
