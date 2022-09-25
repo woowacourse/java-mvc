39e40d05d79a327945f73bec804cd89b65cdf1bf
Ohzzi <clover7even@naver.com>
2022-09-25 12:08:17 +0900

    feat: HandlerMappingRegistry 구현

--- mvc/src/test/java/nextstep/mvc/controller/tobe/DummyController.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/DummyController.java
@@ -1,7 +1,16 @@
 package nextstep.mvc.controller.tobe;
 
+import static nextstep.web.support.RequestMethod.GET;
+
+import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 
 @Controller
 public class DummyController {
+
+    @RequestMapping(value = "/dummy", method = {GET})
+    public ModelAndView dummy() {
+        return null;
+    }
 }
---
+++ mvc/src/test/java/nextstep/mvc/registry/HandlerMappingRegistryTest.java
@@ -0,0 +1,44 @@
+package nextstep.mvc.registry;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.exception.HandlerNotFoundException;
+import org.junit.jupiter.api.Test;
+
+class HandlerMappingRegistryTest {
+
+    @Test
+    void 핸들러를_레지스트리에_추가한다() {
+        HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        System.out.println(getClass().getPackage());
+        AnnotationHandlerMapping handlerMapping = new AnnotationHandlerMapping("nextstep.mvc");
+        handlerMapping.initialize();
+        handlerMappingRegistry.addHandlerMapping(handlerMapping);
+
+        when(request.getRequestURI()).thenReturn("/dummy");
+        when(request.getMethod()).thenReturn("GET");
+
+        Object handler = handlerMappingRegistry.getHandler(request);
+
+        assertThat(handler).isNotNull();
+    }
+
+    @Test
+    void 처리할_수_있는_핸들러가_없으면_예외를_반환한다() {
+        HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        when(request.getRequestURI()).thenReturn("/not-mapped");
+        when(request.getMethod()).thenReturn("GET");
+
+        assertThatThrownBy(() -> handlerMappingRegistry.getHandler(request))
+                .isExactlyInstanceOf(HandlerNotFoundException.class);
+    }
+}
