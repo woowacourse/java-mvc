8aa2a0c7ec61954ccef1ce68cae8d46b2f1983b0
sure-why-not <pkse1213@gmail.com>
2022-09-26 00:08:13 +0900

    refactor: LogoutController 를 @Controller 기반으로 변경

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -2,14 +2,20 @@ package com.techcourse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 
-public class LogoutController implements Controller {
+@nextstep.web.annotation.Controller
+public class LogoutController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var session = req.getSession();
+    private static final String REDIRECT_ROOT_VIEW_NAME = "redirect:/";
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(final HttpServletRequest request, final HttpServletResponse response) {
+        final var session = request.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return new ModelAndView(new JspView(REDIRECT_ROOT_VIEW_NAME));
     }
 }
