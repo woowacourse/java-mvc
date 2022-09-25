2d7eba1397d2b3b73231f4abd4ed31578b1365a3
asebn1 <asebn121@gmail.com>
2022-09-26 03:20:27 +0900

    feat: handlerMappings의 일급컬렉션인 HandlerMappingRegistry를 구현한다.

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -7,34 +7,35 @@ import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.exception.FailMapHandler;
 import nextstep.mvc.handlerAdaptor.HandlerAdapter;
 import nextstep.mvc.handlerMapping.HandlerMapping;
+import nextstep.mvc.handlerMapping.HandlerMappingRegistry;
 import nextstep.mvc.view.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> handlerMappings;
+    private final HandlerMappingRegistry handlerMappingRegistry;
     private final List<HandlerAdapter> handlerAdapters;
 
+
     public DispatcherServlet() {
-        this.handlerMappings = new ArrayList<>();
+        this.handlerMappingRegistry = HandlerMappingRegistry.from();
         this.handlerAdapters = new ArrayList<>();
     }
 
     @Override
     public void init() {
-        handlerMappings.forEach(HandlerMapping::initialize);
+        handlerMappingRegistry.initialize();
     }
 
     public void addHandlerMapping(final HandlerMapping handlerMapping) {
-        handlerMappings.add(handlerMapping);
+        handlerMappingRegistry.addHandlerMapping(handlerMapping);
     }
 
     public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
@@ -57,11 +58,7 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     private Object getHandler(final HttpServletRequest request) {
-        return handlerMappings.stream()
-                .map(handlerMapping -> handlerMapping.getHandler(request))
-                .filter(Objects::nonNull)
-                .findFirst()
-                .orElseThrow(FailMapHandler::new);
+        return handlerMappingRegistry.getHandler(request);
     }
 
     private HandlerAdapter getHandlerAdapter(final Object handler) {
---
+++ mvc/src/main/java/nextstep/mvc/handlerMapping/HandlerMappingRegistry.java
@@ -0,0 +1,37 @@
+package nextstep.mvc.handlerMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import nextstep.mvc.exception.FailMapHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HandlerMappingRegistry {
+
+    private final List<HandlerMapping> handlerMappings;
+
+    private HandlerMappingRegistry(final List<HandlerMapping> handlerMappings) {
+        this.handlerMappings = handlerMappings;
+    }
+
+    public static HandlerMappingRegistry from() {
+        return new HandlerMappingRegistry(new ArrayList<>());
+    }
+
+    public void addHandlerMapping(final HandlerMapping handlerMapping) {
+        handlerMappings.add(handlerMapping);
+    }
+
+    public void initialize(){
+        handlerMappings.forEach(HandlerMapping::initialize);
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(FailMapHandler::new);
+    }
+}
