2e382d2398d66bda08007c2baabd8d1c3ba5ad7c
meatsby <meatsby@gmail.com>
2022-09-25 21:17:13 +0900

    feat: ControllerScanner 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerKey.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerKey.java
@@ -1,12 +1,7 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 
 public class HandlerKey {
@@ -24,15 +19,6 @@ public class HandlerKey {
         String method = request.getMethod();
         RequestMethod requestMethod = RequestMethod.valueOf(method);
         return new HandlerKey(requestURI, requestMethod);
-    }
-
-    public static List<HandlerKey> from(final Method handlerMethod) {
-        RequestMapping requestMapping = handlerMethod.getAnnotation(RequestMapping.class);
-        String requestURI = requestMapping.value();
-        RequestMethod[] requestMethods = requestMapping.method();
-        return Stream.of(requestMethods)
-                .map(requestMethod -> new HandlerKey(requestURI, requestMethod))
-                .collect(Collectors.toList());
     }
 
     @Override
---
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -0,0 +1,20 @@
+package nextstep.mvc.controller.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import samples.TestController;
+
+class ControllerScannerTest {
+
+    @Test
+    void getControllers() {
+        ControllerScanner controllerScanner = ControllerScanner.from("samples");
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+
+        Object controller = controllers.get(TestController.class);
+
+        assertThat(controller).isInstanceOf(TestController.class);
+    }
+}
