64fdb46d8bfc224ae8091aae1de8ce5a5a6f4cf2
sure-why-not <pkse1213@gmail.com>
2022-09-26 00:17:47 +0900

    refactor: RegisterViewController 를 @Controller 기반으로 변경

--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -2,12 +2,19 @@ package com.techcourse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 
-public class RegisterViewController implements Controller {
+@Controller
+public class RegisterViewController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        return "/register.jsp";
+    private static final String REGISTER_JSP_VIEW_NAME = "/register.jsp";
+
+    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
+    public ModelAndView registerView(final HttpServletRequest request, final HttpServletResponse response) {
+        return new ModelAndView(new JspView(REGISTER_JSP_VIEW_NAME));
     }
 }
