162efeb1d0cb32be03b2af42310068767be8c1a8
Ohzzi <clover7even@naver.com>
2022-09-25 13:13:28 +0900

    feat: HandlerAdapterRegistry 구현

---
+++ mvc/src/main/java/nextstep/mvc/registry/HandlerAdapterRegistry.java
@@ -0,0 +1,26 @@
+package nextstep.mvc.registry;
+
+import java.util.ArrayList;
+import java.util.List;
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.exception.HandlerAdapterNotFoundException;
+
+public class HandlerAdapterRegistry {
+
+    private final List<HandlerAdapter> handlerAdapters;
+
+    public HandlerAdapterRegistry() {
+        this.handlerAdapters = new ArrayList<>();
+    }
+
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.supports(handler))
+                .findAny()
+                .orElseThrow(HandlerAdapterNotFoundException::new);
+    }
+}
---
+++ mvc/src/test/java/nextstep/mvc/registry/HandlerAdapterRegistryTest.java
@@ -0,0 +1,35 @@
+package nextstep.mvc.registry;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import nextstep.mvc.HandlerAdapter;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.controller.tobe.HandlerExecutionHandlerAdapter;
+import nextstep.mvc.exception.HandlerAdapterNotFoundException;
+import org.junit.jupiter.api.Test;
+
+class HandlerAdapterRegistryTest {
+
+    @Test
+    void 어댑터를_레지스트리에_추가한다() {
+        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
+        HandlerExecution handlerExecution = mock(HandlerExecution.class);
+
+        HandlerAdapter handlerExecutionHandlerAdapter = new HandlerExecutionHandlerAdapter();
+        handlerAdapterRegistry.addHandlerAdapter(handlerExecutionHandlerAdapter);
+
+        assertThat(handlerAdapterRegistry.getHandlerAdapter(handlerExecution))
+                .isEqualTo(handlerExecutionHandlerAdapter);
+    }
+
+    @Test
+    void 처리할_수_있는_어댑터가_없으면_예외를_반환한다() {
+        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
+        HandlerExecution handlerExecution = mock(HandlerExecution.class);
+
+        assertThatThrownBy(() -> handlerAdapterRegistry.getHandlerAdapter(handlerExecution))
+                .isInstanceOf(HandlerAdapterNotFoundException.class);
+    }
+}
