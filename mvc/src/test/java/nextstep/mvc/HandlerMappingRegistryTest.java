2f88375c79f85810fd3d938fb6bc9cd604a747e1
yeongunheo <gjduddns@gmail.com>
2022-09-25 10:33:36 +0900

    test: 테스트 코드 추가

---
+++ mvc/src/test/java/nextstep/mvc/HandlerMappingRegistryTest.java
@@ -0,0 +1,59 @@
+package nextstep.mvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import common.FakeManualHandlerMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.controller.asis.Controller;
+import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
+import nextstep.mvc.controller.tobe.HandlerExecution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HandlerMappingRegistryTest {
+
+    private final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+
+    @BeforeEach
+    void setUp() {
+        handlerMappingRegistry.addHandlerMapping(new FakeManualHandlerMapping());
+        handlerMappingRegistry.addHandlerMapping(new AnnotationHandlerMapping("samples"));
+        handlerMappingRegistry.init();
+    }
+
+    @Test
+    @DisplayName("애노테이션 기반의 핸들러를 찾아온다.")
+    void getHandlerWithAnnotation() {
+        // given
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        // when
+        Object handler = handlerMappingRegistry.getHandler(request);
+
+        // then
+        assertThat(handler).isInstanceOf(HandlerExecution.class);
+    }
+
+    @Test
+    @DisplayName("인터페이스 기반의 핸들러를 찾아온다.")
+    void getHandlerWithInterface() {
+        // given
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+        when(request.getRequestURI()).thenReturn("/");
+        when(request.getMethod()).thenReturn("GET");
+
+        // when
+        Object handler = handlerMappingRegistry.getHandler(request);
+
+        // then
+        assertThat(handler).isInstanceOf(Controller.class);
+    }
+}
