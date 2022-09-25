851e4ba9f74e5f3de02e6f1fc135d1bf6625945c
sinb57 <sinb57@naver.com>
2022-09-25 15:13:39 +0900

    refactor: HandlerMappingRegistry 클래스 분리

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -1,10 +1,5 @@
 package nextstep.mvc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,21 +14,20 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> handlerMappings;
+    private final HandlerMappingRegistry handlerMappingRegistry;
     private final HandlerAdapterRegistry handlerAdapterRegistry;
 
     public DispatcherServlet() {
-        this.handlerMappings = new ArrayList<>();
+        handlerMappingRegistry = new HandlerMappingRegistry();
         handlerAdapterRegistry = new HandlerAdapterRegistry();
     }
 
     @Override
     public void init() {
-        handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     public void addHandlerMapping(final HandlerMapping handlerMapping) {
-        handlerMappings.add(handlerMapping);
+        handlerMappingRegistry.add(handlerMapping);
     }
 
     public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
@@ -57,17 +51,9 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     private ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        final var handler = getHandler(request);
+        final var handler = handlerMappingRegistry.getHandler(request);
         final var handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
 
         return handlerAdapter.handle(request, response, handler);
-    }
-
-    private Object getHandler(final HttpServletRequest request) {
-        return handlerMappings.stream()
-                .map(handlerMapping -> handlerMapping.getHandler(request))
-                .filter(Objects::nonNull)
-                .findAny()
-                .orElseThrow(() -> new NoSuchElementException("해당하는 HandlerMapping을 찾을 수 없습니다."));
     }
 }
---
+++ mvc/src/main/java/nextstep/mvc/HandlerMappingRegistry.java
@@ -0,0 +1,30 @@
+package nextstep.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HandlerMappingRegistry {
+
+    private final List<HandlerMapping> handlerMappings;
+
+    public HandlerMappingRegistry() {
+        this.handlerMappings = new ArrayList<>();
+    }
+
+    public void add(final HandlerMapping handlerMapping) {
+        handlerMapping.initialize();
+        handlerMappings.add(handlerMapping);
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("해당하는 HandlerMapping을 찾을 수 없습니다."));
+    }
+}
