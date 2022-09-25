f39f42a5712a51d31e18a28068b854b1fbd158d9
yoon <duddbs774@gmail.com>
2022-09-25 20:06:08 +0900

    feat: @Controller 클래스를 찾아서 객체 생성 역할 분리

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -1,16 +1,15 @@
 package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import nextstep.mvc.HandlerMapping;
-import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 import org.reflections.ReflectionUtils;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,37 +17,29 @@ public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final ControllerScanner controllerScanner;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+        this.controllerScanner = new ControllerScanner(basePackage);
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
-        final var reflections = new Reflections(basePackage);
-        final var controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
-        controllerClasses.forEach(this::init);
+        final List<Object> controllers = controllerScanner.scan();
+        controllers.forEach(this::init);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void init(final Class<?> clazz) {
-        final Object handler;
-        try {
-            handler = clazz.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        final var annotatedMethods = ReflectionUtils.getAllMethods(clazz,
+    private void init(final Object controller) {
+        final Class<?> clazz = controller.getClass();
+        final Set<Method> methods = ReflectionUtils.getAllMethods(clazz,
                 ReflectionUtils.withAnnotation(RequestMapping.class));
-        for (final Method method : annotatedMethods) {
+        for (final Method method : methods) {
             final var requestMapping = method.getAnnotation(RequestMapping.class);
             handlerExecutions.put(
                     new HandlerKey(requestMapping.value(), requestMapping.method()[0]),
-                    new HandlerExecution(handler, method)
-            );
+                    new HandlerExecution(controller, method));
         }
     }
 
---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
@@ -0,0 +1,31 @@
+package nextstep.mvc.controller.tobe;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+
+public class ControllerScanner {
+
+    private final Reflections reflections;
+
+    public ControllerScanner(final Object[] basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
+
+    public List<Object> scan() {
+        final Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        return classes.stream()
+                .map(it -> {
+                    try {
+                        return it.getDeclaredConstructor().newInstance();
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                             NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+}
