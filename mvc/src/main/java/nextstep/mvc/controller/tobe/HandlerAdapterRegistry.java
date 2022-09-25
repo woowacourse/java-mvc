17e6818b1930cbf0655fbf7c8012cbe53cc55c1c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-26 00:15:26 +0900

    feat(HandlerAdapterRegistry): implement class for initializing and finding HandlerAdapters

---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerAdapterRegistry.java
@@ -0,0 +1,21 @@
+package nextstep.mvc.controller.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+import nextstep.mvc.HandlerAdapter;
+
+public class HandlerAdapterRegistry {
+
+    private final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.supports(handler))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("처리할 수 없는 요청입니다."));
+    }
+}
