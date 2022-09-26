9798179df018894ef0815a93a67fbb3a9f91e7a3
asebn1 <asebn121@gmail.com>
2022-09-27 03:38:19 +0900

    refactor: objectMapper를 클래스 상수로 등록

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -8,8 +8,10 @@ import nextstep.web.support.MediaType;
 import java.util.Map;
 
 public class JsonView implements View {
+
     private static final int SINGLE_MODEL_SIZE = 1;
     private static final int SINGLE_MODEL_INDEX = 0;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response)
@@ -20,9 +22,8 @@ public class JsonView implements View {
     }
 
     private String convertJsonToString(final Map<String, ?> model) throws Exception {
-        final ObjectMapper objectMapper = new ObjectMapper();
         if (model.size() == SINGLE_MODEL_SIZE) {
-            return objectMapper.writeValueAsString(model.values().toArray()[SINGLE_MODEL_INDEX]);
+            return model.values().toArray()[SINGLE_MODEL_INDEX].toString();
         }
         return objectMapper.writeValueAsString(model);
     }
