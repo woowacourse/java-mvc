08121ca6903cd5d2444289bb1ead09dcd758e818
DOES <ldk980130@gmail.com>
2022-09-26 19:28:02 +0900

    feat: JsonView를 구현한다.

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,37 @@
 package nextstep.mvc.view;
 
+import java.io.PrintWriter;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
-    @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Override
+	public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+		PrintWriter writer = response.getWriter();
+		writer.write(convertToJson(model));
+	}
+
+	private String convertToJson(Map<String, ?> model) throws JsonProcessingException {
+		if (model.size() == 1) {
+            return objectMapper.writeValueAsString(getFirstValue(model));
+        }
+        return objectMapper.writeValueAsString(model);
     }
+
+	private Object getFirstValue(Map<String, ?> model) {
+		return model.keySet().stream()
+			.map(model::get)
+			.findFirst()
+			.orElseThrow();
+	}
 }
