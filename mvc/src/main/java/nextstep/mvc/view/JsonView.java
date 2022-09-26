cc65ad1bbff44f201cc727c35e8f889abbe16ed8
chawani <chawani@naver.com>
2022-09-26 23:11:00 +0900

    feat: json 형태로 응답을 반환하는 JsonView 클래스 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,33 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        PrintWriter responseWriter = response.getWriter();
+        responseWriter.write(toJsonFormat(model));
+    }
+
+    private String toJsonFormat(Map<String, ?> model) throws JsonProcessingException {
+        ObjectWriter objectWriter = mapper.writerWithDefaultPrettyPrinter();
+
+        if (model.size() == 1) {
+            return objectWriter.writeValueAsString(model.values().toArray()[0]);
+        }
+        return objectWriter.writeValueAsString(model);
     }
 }
