6134879a3b6af2b28dccdc7c2c7864556d6f1438
cjlee38 <cjlee38@github.com>
2022-09-26 12:33:00 +0900

    refactor: serialize value if model has only one entry

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -4,22 +4,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Map.Entry;
 import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
     private static final ObjectMapper mapper = new ObjectMapper();
+    private static final int EMPTY = 0;
+    private static final int ONLY_ONE = 1;
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        Object value = decideModelToSerialize(model);
         try {
-            String serialized = mapper.writeValueAsString(model);
+            String serialized = mapper.writeValueAsString(value);
             response.getWriter().write(serialized);
         } catch (IOException e) {
             throw new RuntimeException("Fails on write model as json", e);
         }
+    }
+
+    private Object decideModelToSerialize(Map<String, ?> model) {
+        if (model.size() == EMPTY) {
+            return "";
+        }
+        if (model.size() == ONLY_ONE) {
+            return model.values().toArray()[0];
+        }
+        return model;
     }
 
     @Override
--- mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -1,11 +1,15 @@
 package nextstep.mvc.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +18,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JsonViewTest {
+
+    public static class MyWriter extends PrintWriter {
+
+        private StringBuilder cache;
+
+        public MyWriter(OutputStream out) {
+            super(out);
+            this.cache = new StringBuilder();
+        }
+
+        @Override
+        public void write(String s) {
+            cache.append(s);
+            super.write(s);
+        }
+
+        public String getCache() {
+            return cache.toString();
+        }
+    }
+
+
     @Test
     @DisplayName("json view를 렌더링한다")
     void render() throws IOException {
@@ -23,12 +49,49 @@ class JsonViewTest {
         model.put("hello", "world");
 
         HttpServletResponse response = HttpServletFixtures.createResponse();
-        PrintWriter writer = mock(PrintWriter.class);
+        MyWriter writer = new MyWriter(new BufferedOutputStream(new ByteArrayOutputStream()));
         when(response.getWriter()).thenReturn(writer);
 
         // when & then
         assertThatNoException().isThrownBy(
                 () -> view.render(model, null, response)
         );
+    }
+
+    @Test
+    @DisplayName("model 값이 하나라면 해당 값의 내부를 직렬화한다")
+    void renderInside() throws IOException {
+        // given
+        View view = new JsonView();
+        Map<String, Object> model = new HashMap<>();
+        model.put("hello", "world");
+
+        HttpServletResponse response = HttpServletFixtures.createResponse();
+        MyWriter writer = new MyWriter(new BufferedOutputStream(new ByteArrayOutputStream()));
+        when(response.getWriter()).thenReturn(writer);
+        // when
+        view.render(model, null, response);
+
+        // then
+        assertThat(writer.getCache()).isEqualTo("world");
+    }
+
+    @Test
+    @DisplayName("model 값이 여러개라면 모델 자체를 직렬화한다")
+    void renderAll() throws IOException {
+        // given
+        View view = new JsonView();
+        Map<String, Object> model = new HashMap<>();
+        model.put("hello", "world");
+        model.put("name", "chalee");
+
+        HttpServletResponse response = HttpServletFixtures.createResponse();
+        MyWriter writer = new MyWriter(new BufferedOutputStream(new ByteArrayOutputStream()));
+        when(response.getWriter()).thenReturn(writer);
+        // when
+        view.render(model, null, response);
+
+        // then
+        assertThat(writer.getCache()).isEqualTo("{\"name\":\"chalee\",\"hello\":\"world\"}");
     }
 }
