443b3bc433f374df7cf8e28e02ba60f0c32d4e7e
corinne <yoo77hyeon@gmail.com>
2022-09-26 22:27:42 +0900

    refactor: 상수 사용 방법 변경

--- app/src/main/java/com/techcourse/controller/WelcomeController.java
+++ app/src/main/java/com/techcourse/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.techcourse.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
@@ -10,7 +12,7 @@ import nextstep.web.support.RequestMethod;
 public class WelcomeController {
 
     @RequestMapping(value = "/", method = {RequestMethod.GET})
-    public ModelAndView welcome() {
+    public ModelAndView welcome(final HttpServletRequest request, final HttpServletResponse response) {
         return new ModelAndView(new JspView("/index.jsp"));
     }
 }
