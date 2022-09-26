ddcb8ed5b573c2235a7e5b5286838a319fffcefa
devHudi <devhudi@gmail.com>
2022-09-26 16:42:30 +0900

    refactor: HandlerMappingRegistry 구현

--- mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
+++ mvc/src/main/java/nextstep/mvc/DispatcherServlet.java
@@ -4,9 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import nextstep.mvc.controller.tobe.exception.ControllerNotFoundException;
 import nextstep.mvc.view.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,16 +15,15 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private final HandlerAdapterRegistry handlerAdapterRegistry;
-    private final List<HandlerMapping> handlerMappings;
+    private final HandlerMappingRegistry handlerMappingRegistry;
 
     public DispatcherServlet() {
         this.handlerAdapterRegistry = new HandlerAdapterRegistry();
-        this.handlerMappings = new ArrayList<>();
+        this.handlerMappingRegistry = new HandlerMappingRegistry();
     }
 
     @Override
     public void init() {
-        handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
@@ -34,7 +31,7 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     public void addHandlerMapping(final HandlerMapping handlerMapping) {
-        handlerMappings.add(handlerMapping);
+        handlerMappingRegistry.addHandlerMapping(handlerMapping);
     }
 
     @Override
@@ -43,7 +40,9 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final Object controller = getController(request);
+            Object controller = handlerMappingRegistry.getHandler(request)
+                    .orElseThrow(ControllerNotFoundException::new);
+
             HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(controller);
             ModelAndView modelAndView = handlerAdapter.handle(request, response, controller);
             modelAndView.render(request, response);
@@ -51,13 +50,5 @@ public class DispatcherServlet extends HttpServlet {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private Object getController(final HttpServletRequest request) {
-        return handlerMappings.stream()
-                .map(handlerMapping -> handlerMapping.getHandler(request))
-                .filter(Objects::nonNull)
-                .findFirst()
-                .orElseThrow();
     }
 }
