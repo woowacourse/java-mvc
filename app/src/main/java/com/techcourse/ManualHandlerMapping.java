aa4774f5c3376de9a8b10e93857e12f5abbc635c
EastHShin <xldk78@gmail.com>
2022-09-26 21:42:34 +0900

    refactor: ForwardController를 어노테이션 기반 MVC로 교체

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.controller.asis.Controller;
-import nextstep.mvc.controller.asis.ForwardController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +16,7 @@ public class ManualHandlerMapping implements HandlerMapping {
 
     @Override
     public void initialize() {
-        controllers.put("/", new ForwardController("/index.jsp"));
+//        controllers.put("/", new ForwardController("/index.jsp"));
 //        controllers.put("/login", new LoginController());
 //        controllers.put("/login/view", new LoginViewController());
 //        controllers.put("/logout", new LogoutController());
