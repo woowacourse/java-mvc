9c41eb9bf80c3fc56707b4b074b221704a3b5e3c
jurlring <78091011+jurlring@users.noreply.github.com>
2022-09-27 08:57:15 +0900

    feat: Json View 구현

--- README.md
+++ README.md
@@ -7,8 +7,9 @@
 - [x] HandlerAdapterRegistry 클래스에서 HandlerAdapter를 처리하도록 구현했다.
 
 ## 3단계 요구 사항
-- [ ] 힌트에서 제공한 UserController 컨트롤러가 json 형태로 응답을 반환한다.
-  - [ ] JspView 클래스를 구현
-  - [ ] JsonView 클래스를 구현
+
+- [x] 힌트에서 제공한 UserController 컨트롤러가 json 형태로 응답을 반환한다.
+    - [x] JspView 클래스를 구현
+    - [x] JsonView 클래스를 구현
 - [ ] 레거시 코드를 삭제하고 서버를 띄워도 정상 동작한다.
 
--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,29 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        String body = objectMapper.writeValueAsString(getModel(model));
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
+    }
+
+    private Object getModel(Map<String, ?> model) {
+        if (model.size() != 1) {
+            return model;
+        }
+        return model.values().stream()
+                .findFirst()
+                .orElseThrow();
     }
 }
