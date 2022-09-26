78b009a8e162184809af30ab7bc77ea403bed55a
yeongunheo <gjduddns@gmail.com>
2022-09-26 21:20:05 +0900

    refactor: JsonView가 파라미터로 받은 Map을 Json으로 리턴하도록 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -5,22 +5,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.util.Map;
-import java.util.Set;
 import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
+
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        Set<String> keySet = model.keySet();
-        for (String key : keySet) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Object value = model.get(key);
-            String result = objectMapper.writeValueAsString(value);
-            PrintWriter writer = response.getWriter();
-            writer.write(result);
-        }
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        model.keySet().forEach(key -> {
+            log.debug("attribute name : {}, value : {}", key, model.get(key));
+            request.setAttribute(key, model.get(key));
+        });
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String result = objectMapper.writeValueAsString(model);
+        PrintWriter writer = response.getWriter();
+        writer.write(result);
     }
 }
