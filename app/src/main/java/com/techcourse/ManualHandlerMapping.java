3710e3a63415c998c39838fbe66e1efaefc07d50
EastHShin <xldk78@gmail.com>
2022-09-26 21:36:38 +0900

    refactor: LogoutController를 어노테이션 기반 MVC로 교체

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -22,7 +22,7 @@ public class ManualHandlerMapping implements HandlerMapping {
         controllers.put("/", new ForwardController("/index.jsp"));
 //        controllers.put("/login", new LoginController());
 //        controllers.put("/login/view", new LoginViewController());
-        controllers.put("/logout", new LogoutController());
+//        controllers.put("/logout", new LogoutController());
         controllers.put("/register/view", new RegisterViewController());
 //        controllers.put("/register", new RegisterController());
 
