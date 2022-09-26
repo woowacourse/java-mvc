7c10330c6b1924e5e1b3e77f2418078d678a5733
bomi <bonita9931@gmail.com>
2022-09-26 13:54:21 +0900

    feat: ControllerScanner 클래스 추가

---
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
@@ -0,0 +1,40 @@
+package nextstep.mvc.controller.tobe;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.web.annotation.Controller;
+import org.reflections.Reflections;
+
+public class ControllerScanner {
+
+    private final Reflections reflections;
+
+
+    public ControllerScanner(final Reflections reflections) {
+        this.reflections = reflections;
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>(reflections.getTypesAnnotatedWith(Controller.class));
+
+        return instantiateControllers(classes);
+    }
+
+    private Map<Class<?>, Object> instantiateControllers(final Set<Class<?>> classes) {
+
+        return classes.stream()
+                .collect(Collectors.toMap(clazz -> clazz, this::createInstance));
+    }
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
