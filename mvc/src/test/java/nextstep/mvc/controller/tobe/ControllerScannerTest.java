f6562919414f9f52252867c5a39adc2f2057f500
yh20studio(tiki) <yh20studio@gmail.com>
2022-09-26 13:47:52 +0900

    refactor: ControllerScanner 생성

---
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -0,0 +1,22 @@
+package nextstep.mvc.controller.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+
+class ControllerScannerTest {
+
+    @Test
+    @DisplayName("컨트롤러를 찾는다")
+    void getController() {
+        final Reflections reflections = new Reflections("samples");
+        final ControllerScanner controllerScanner = new ControllerScanner(reflections);
+
+        final Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+
+        assertThat(controllers.size()).isEqualTo(1);
+    }
+}
