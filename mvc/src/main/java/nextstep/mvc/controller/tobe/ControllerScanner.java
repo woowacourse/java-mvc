f219c39c6558a6f09d205c385de9486024370677
dwl21 <nggus5@naver.com>
2022-09-26 14:57:32 +0900

    feat: RequestMapping 어노테이션이 붙은 메서드를 찾는 기능 구현

--- mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/ControllerScanner.java
@@ -1,7 +1,11 @@
 package nextstep.mvc.controller.tobe;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
 import org.reflections.Reflections;
 
 public class ControllerScanner {
@@ -18,5 +22,15 @@ public class ControllerScanner {
 
     public Set<Class<?>> findAllControllers() {
         return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    public List<Method> findMethodsWithRequestMapping(Class<?> controller) {
+        return filterRequestMapping(List.of(controller.getMethods()));
+    }
+
+    private List<Method> filterRequestMapping(List<Method> methods) {
+        return methods.stream()
+            .filter(it -> it.isAnnotationPresent(RequestMapping.class))
+            .collect(Collectors.toList());
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -3,10 +3,13 @@ package nextstep.mvc.controller.tobe;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Set;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import samples.TestController;
 
 class ControllerScannerTest {
 
@@ -18,5 +21,16 @@ class ControllerScannerTest {
         Set<Class<?>> controllers = controllerScanner.findAllControllers();
 
         assertThat(controllers).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("RequestMapping 어노테이션이 붙은 메서드를 찾을 수 있다.")
+    void findMethodsWithRequestMapping() {
+        ControllerScanner controllerScanner = ControllerScanner.from("samples");
+
+        List<Method> methods = controllerScanner.findMethodsWithRequestMapping(
+            TestController.class);
+
+        assertThat(methods).hasSize(2);
     }
 }
