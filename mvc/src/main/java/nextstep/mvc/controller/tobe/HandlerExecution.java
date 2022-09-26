b0093c1a0ba8507831d335816d2279647a3d9fef
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-26 14:23:44 +0900

    feat(HandlerExecution): handle methods with String return type

--- mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/HandlerExecution.java
@@ -3,7 +3,9 @@ package nextstep.mvc.controller.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
+import nextstep.mvc.view.RedirectView;
 
 public class HandlerExecution {
 
@@ -16,6 +18,21 @@ public class HandlerExecution {
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(declaredObject, request, response);
+        final var returnType = method.getReturnType();
+        if (String.class.equals(returnType)) {
+            final var viewName = (String) method.invoke(declaredObject, request, response);
+            return toModelAndView(viewName);
+        }
+        if (ModelAndView.class.equals(returnType)) {
+            return (ModelAndView) method.invoke(declaredObject, request, response);
+        }
+        throw new IllegalArgumentException("처리될 수 없는 로직입니다.");
+    }
+
+    private ModelAndView toModelAndView(String viewName) {
+        if (viewName.startsWith(RedirectView.REDIRECT_PREFIX)) {
+            return new ModelAndView(new RedirectView(viewName));
+        }
+        return new ModelAndView(new JspView(viewName));
     }
 }
