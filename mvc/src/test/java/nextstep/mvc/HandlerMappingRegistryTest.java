519a70eb915a22324ab92432e31faecb938126b4
Chris <bjuuuu98@gmail.com>
2022-09-27 10:24:53 +0900

    test: 적절한 Handler가 없는 경우에 대한 테스트 추가

--- mvc/src/test/java/nextstep/mvc/HandlerMappingRegistryTest.java
+++ mvc/src/test/java/nextstep/mvc/HandlerMappingRegistryTest.java
@@ -1,10 +1,12 @@
 package nextstep.mvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import jakarta.servlet.http.HttpServletRequest;
 import nextstep.mvc.controller.tobe.AnnotationHandlerMapping;
 import nextstep.mvc.controller.tobe.HandlerExecution;
+import nextstep.mvc.exception.HandlerAdapterNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -25,5 +27,20 @@ class HandlerMappingRegistryTest {
         Object handler = handlerMappingRegistry.getHandler(request);
 
         assertThat(handler).isInstanceOf(HandlerExecution.class);
+    }
+
+    @DisplayName("적절한 Handler가 없을 경우 예외를 발생시킨다.")
+    @Test
+    void getHandler_HandlerNotFoundException() {
+        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
+        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter());
+        HttpServletRequest invalidRequest = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(invalidRequest.getRequestURI())
+                .thenReturn("/invalid-uri");
+        Mockito.when(invalidRequest.getMethod())
+                .thenReturn("TRACE");
+
+        assertThatThrownBy(() -> handlerAdapterRegistry.getHandlerAdapter(invalidRequest))
+                .isInstanceOf(HandlerAdapterNotFoundException.class);
     }
 }
