40dc22d93880e75eda05a97d7ff407b828ef5415
dwl21 <nggus5@naver.com>
2022-09-27 13:47:46 +0900

    refactor: RegisterViewController 어노테이션 기반 수정

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -19,7 +19,6 @@ public class ManualHandlerMapping implements HandlerMapping {
 
     @Override
     public void initialize() {
-        controllers.put("/register/view", new RegisterViewController());
         controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
