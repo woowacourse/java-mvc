fb21a178274c7bd2b26f7af0b5a85e4cbc48a3fc
progress0407 <progress0407@gmail.com>
2022-09-27 10:26:40 +0900

    feat: jsnoView 기능 추가

--- app/src/main/java/com/techcourse/controller/v2/LoginV2Controller.java
+++ app/src/main/java/com/techcourse/controller/v2/LoginV2Controller.java
@@ -11,7 +11,6 @@ import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
-import nextstep.web.support.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,43 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
+import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model,
+                       final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        logModelEntry(model);
+        String body = convertModelToJsonString(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
+    }
+
+    private String convertModelToJsonString(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return firstModelValue(model);
+        }
+
+        return objectMapper.writeValueAsString(model);
+    }
+
+    private String firstModelValue(final Map<String, ?> model) {
+        return model.values().toArray()[0].toString();
+    }
+
+    private void logModelEntry(final Map<String, ?> model) {
+        model.keySet()
+                .forEach(key -> log.debug("attribute name : {}, value : {}", key, model.get(key)));
     }
 }
