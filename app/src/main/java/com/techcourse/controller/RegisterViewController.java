1db5ab901965fc4ca83c6246d48bd75084a4e33a
sinb57 <sinb57@naver.com>
2022-09-27 09:34:48 +0900

    refactor: RegisterViewController RequestMapping 명시

--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -6,11 +6,12 @@ import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 
 @Controller
 public class RegisterViewController {
 
-    @RequestMapping
+    @RequestMapping(value = "/register/view", method= RequestMethod.GET)
     public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) {
         return new ModelAndView(new JspView("/register.jsp"));
     }
