897a00a631c4e5402de249e7b28adacee0ef0d67
Juhyung990122 <harukawa99@naver.com>
2022-09-27 15:22:39 +0900

    feat: JsonView 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,37 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.mvc.exception.EmptyModelException;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String EMPTY = "";
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final PrintWriter printWriter = response.getWriter();
+        try{
+            printWriter.print(objectMapper.writeValueAsString(getAttribute(model)));
+        }catch (EmptyModelException e){
+            printWriter.print(EMPTY);
+        }
+    }
+
+    private Object getAttribute(Map<String, ?> model) {
+        if(model.size() > 1){
+            return model;
+        }
+        return model.values()
+                .stream()
+                .findAny()
+                .orElseThrow(EmptyModelException::new);
     }
 }
