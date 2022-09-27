714a6ad938eeaff3f52e863d3984081741fdb669
dwl21 <nggus5@naver.com>
2022-09-27 13:31:22 +0900

    refactor: 랜딩 페이지 어노테이션 기반 수정

---
+++ app/src/main/java/com/techcourse/controller/LandingViewController.java
@@ -0,0 +1,19 @@
+package com.techcourse.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
+
+@Controller
+public class LandingViewController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView showLandingPage(final HttpServletRequest request, final HttpServletResponse response) {
+        JspView jspView = new JspView("/index.jsp");
+        return new ModelAndView(jspView);
+    }
+}
