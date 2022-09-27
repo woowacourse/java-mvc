21dcfb5c19d66f734c1133d7da22fdc18d5ca00f
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-27 11:46:00 +0900

    test: AnnotationHandlerMappingTest 추가

--- mvc/src/test/java/nextstep/mvc/controller/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/AnnotationHandlerMappingTest.java
@@ -4,9 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.controller.AnnotationHandlerMapping;
 import nextstep.mvc.controller.HandlerExecution;
+import nextstep.mvc.mapping.HandlerMapping;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -50,5 +53,33 @@ class AnnotationHandlerMappingTest {
         final var modelAndView = handlerExecution.handle(request, response);
 
         assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+
+    @DisplayName("Method와 RequestURI가 등록된 경우, 해당 핸들러를 가져온다.")
+    @Test
+    void getHandler() {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(request.getMethod()).thenReturn("GET");
+        Mockito.when(request.getRequestURI()).thenReturn("/get-test");
+
+        final HandlerMapping handlerMapping = new AnnotationHandlerMapping("samples");
+        handlerMapping.initialize();
+
+        Object handler = handlerMapping.getHandler(request);
+        assertThat(handler).isNotNull();
+    }
+
+    @DisplayName("Method와 RequestURI가 등록되지 않은 경우, 핸들러를 가져올 수 없다.")
+    @Test
+    void getHandlerNotContain() {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(request.getMethod()).thenReturn("GET");
+        Mockito.when(request.getRequestURI()).thenReturn("/thatsnono");
+
+        final HandlerMapping handlerMapping = new AnnotationHandlerMapping("samples");
+        handlerMapping.initialize();
+
+        Object handler = handlerMapping.getHandler(request);
+        assertThat(handler).isNull();
     }
 }
