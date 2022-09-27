2fce81594d6d2ee51bfccadcd48bcb9f2e8dae38
kth990303 <kth990303@naver.com>
2022-09-27 13:33:05 +0900

    refactor: objectMapper를 상태로 가지도록 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -9,12 +9,17 @@ import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper;
+
+    public JsonView() {
+        this.objectMapper = new ObjectMapper();
+        objectMapper.writerWithDefaultPrettyPrinter();
+    }
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final String value = objectMapper.writerWithDefaultPrettyPrinter()
-                .writeValueAsString(model);
+        final String value = objectMapper.writeValueAsString(model);
 
         final PrintWriter writer = response.getWriter();
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
