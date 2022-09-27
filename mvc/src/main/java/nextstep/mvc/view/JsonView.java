4fa511f4536b502080d25dce8b5ad62b62165b7c
김찬 <gmelddl514@gmail.com>
2022-09-27 17:21:12 +0900

    chore: 상수명 컨벤션에 맞게 변경

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -9,20 +9,20 @@ import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final int CRITERIA = 1;
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         final PrintWriter printWriter = response.getWriter();
         if (model.size() == CRITERIA) {
-            printWriter.write(objectMapper.writeValueAsString(findValue(model)));
+            printWriter.write(OBJECT_MAPPER.writeValueAsString(findValue(model)));
             return;
         }
 
-        printWriter.write(objectMapper.writeValueAsString(model));
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        printWriter.write(OBJECT_MAPPER.writeValueAsString(model));
     }
 
     private Object findValue(Map<String, ?> model) {
