ed0c9cd5c4c1eeed00c24a73fdad75a68f599cca
Chris <bjuuuu98@gmail.com>
2022-09-27 13:27:36 +0900

    refactor: 테스트를 위한 Map.of를 LinkedHashMap으로 수정

--- mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -7,13 +7,14 @@ import static org.mockito.Mockito.when;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JsonViewTest {
 
-    @DisplayName("데이터가 하나이면 value만 반환한다.")
+    @DisplayName("데이터가 하나일 경우, value만 반환한다.")
     @Test
     void render_OneValue() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
@@ -28,16 +29,18 @@ class JsonViewTest {
         verify(response.getWriter()).write("\"value\"");
     }
 
-    @DisplayName("데이터가 여러개이면 key와 value의 쌍들을 반환한다.")
+    @DisplayName("데이터가 여러 개일 경우 key와 value의 쌍들을 반환한다.")
     @Test
     void render_MultiMap() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         PrintWriter writer = mock(PrintWriter.class);
         when(response.getWriter()).thenReturn(writer);
-
         JsonView jsonView = new JsonView();
-        Map<String, Object> model = Map.of("firstKey", "firstValue", "secondKey", "secondValue");
+        LinkedHashMap<String, Object> model = new LinkedHashMap<>();
+        model.put("firstKey", "firstValue");
+        model.put("secondKey", "secondValue");
+
         jsonView.render(model, request, response);
 
         verify(response.getWriter()).write("{\"firstKey\":\"firstValue\",\"secondKey\":\"secondValue\"}");
