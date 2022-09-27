85ffdaf6e05533ecc800974d9a2d97e4dc7273fc
yeon-06 <rnjstldus2@gmail.com>
2022-09-27 20:54:45 +0900

    feat: Map의 값이 1개 이하인 경우 고려

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,13 +9,32 @@ import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final String EMPTY_STRING = "";
+    private static final int MAP_MIN_SIZE = 1;
     private static final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        String messageBody = mapper.writeValueAsString(model);
+        String messageBody = toJson(model);
         response.getWriter().write(messageBody);
+    }
+
+    private String toJson(final Map<String, ?> model) throws JsonProcessingException {
+        if (model.isEmpty()) {
+            return EMPTY_STRING;
+        }
+        if (model.size() == MAP_MIN_SIZE) {
+            return mapper.writeValueAsString(findFirstModel(model));
+        }
+        return mapper.writeValueAsString(model);
+    }
+
+    private Object findFirstModel(final Map<String, ?> model) {
+        return model.values()
+                .stream()
+                .findAny()
+                .orElse(null);
     }
 }
