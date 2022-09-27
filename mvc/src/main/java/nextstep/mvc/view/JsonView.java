3a2d8f12cb9b7309f44081aae086b6fd9bab272d
Seongwon97 <osw970325@naver.com>
2022-09-27 10:04:56 +0900

    feat: JsonView 클래스를 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,40 @@
 package nextstep.mvc.view;
 
+import java.io.PrintWriter;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        model.keySet().forEach(key -> {
+            log.debug("attribute name : {}, value : {}", key, model.get(key));
+            request.setAttribute(key, model.get(key));
+        });
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(convertJson(model));
+    }
+
+    private String convertJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values());
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
