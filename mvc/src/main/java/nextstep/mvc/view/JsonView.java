b1288a7d608d6aabe53b4c434941fa3109e36ef2
yoon <duddbs774@gmail.com>
2022-09-27 17:32:22 +0900

    feat: model 의 데이터 개수에 따라 응답형식이 달라지도록 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -3,6 +3,7 @@ package nextstep.mvc.view;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 import nextstep.web.support.MediaType;
 
@@ -18,7 +19,18 @@ public class JsonView implements View {
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        final var jsonValue = objectMapper.writeValueAsString(model);
+        if (model.size() == 1) {
+            final var value = model.values().stream()
+                    .findFirst()
+                    .get();
+            writeResponse(response, value);
+            return;
+        }
+        writeResponse(response, model);
+    }
+
+    private void writeResponse(final HttpServletResponse response, final Object object) throws IOException {
+        final var jsonValue = objectMapper.writeValueAsString(object);
         final var writer = response.getWriter();
         writer.write(jsonValue);
     }
--- mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -7,19 +7,30 @@ import static org.mockito.Mockito.verify;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class JsonViewTest {
 
+    private JsonView jsonView;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private PrintWriter writer;
+
+    @BeforeEach
+    void setUp() {
+        this.jsonView = new JsonView();
+        this.request = mock(HttpServletRequest.class);
+        this.response = mock(HttpServletResponse.class);
+        this.writer = mock(PrintWriter.class);
+    }
+
     @Test
-    void 직렬화한_데이터를_Response에_작성한다() throws Exception {
+    void 직렬화할_데이터가_두_개_이상이면_Map_형태로_Response에_작성한다() throws Exception {
         // given
-        final var jsonView = new JsonView();
-        final var request = mock(HttpServletRequest.class);
-        final var response = mock(HttpServletResponse.class);
-        final var writer = mock(PrintWriter.class);
-        final Map<String, String> model = Map.of("user", "gugu");
+        final Map<String, ?> model = new LinkedHashMap<>(Map.of("name", "gugu", "age", 20));
         given(response.getWriter())
                 .willReturn(writer);
 
@@ -27,6 +38,20 @@ class JsonViewTest {
         jsonView.render(model, request, response);
 
         // then
-        verify(writer).write("{\"user\":\"gugu\"}");
+        verify(writer).write("{\"name\":\"gugu\",\"age\":20}");
+    }
+
+    @Test
+    void 직렬화할_데이터가_한_개라면_값을_그대로_Response에_작성한다() throws Exception {
+        // given
+        final Map<String, ?> model = Map.of("name", "gugu");
+        given(response.getWriter())
+                .willReturn(writer);
+
+        // when
+        jsonView.render(model, request, response);
+
+        // then
+        verify(writer).write("\"gugu\"");
     }
 }
