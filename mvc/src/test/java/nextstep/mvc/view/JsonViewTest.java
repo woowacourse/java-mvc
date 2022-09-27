73764ef32a9c06d080ef5b1067267f52420441e7
Nam Minjeong <nmj5012@naver.com>
2022-09-27 14:01:21 +0900

    refactor : 기존의 var을 구체적인 클래스로 변경

---
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -0,0 +1,17 @@
+package nextstep.mvc.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JsonViewTest {
+
+    @Test
+    @DisplayName("데이터를 json 방식으로 변환한다.")
+    void renderJson(){
+        JsonView jsonView = new JsonView();
+
+    }
+
+}
