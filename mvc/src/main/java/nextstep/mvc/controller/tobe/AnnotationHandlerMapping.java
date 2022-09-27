a5ba3e8d4841664236530c3086fe43e6155f8f62
yhh1056 <yhh1056@naver.com>
2022-09-27 10:01:48 +0900

    refactor: AnnotationHandlerMapping initialize 수정

--- mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/nextstep/mvc/controller/tobe/AnnotationHandlerMapping.java
@@ -2,12 +2,15 @@ package nextstep.mvc.controller.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import nextstep.mvc.HandlerMapping;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
+import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,33 +31,38 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     public void initialize() {
         ControllerScanner controllerScanner = new ControllerScanner(new Reflections(basePackage));
         Map<Class<?>, Object> controllers = controllerScanner.getControllers();
-        List<Object> values = Collections.singletonList(controllers.values());
-        for (Object controller : values) {
-            addHandlerExecutions(controller);
+        for (Method method : getRequestMappingMethod(controllers.keySet())) {
+            addHandlerExecutions(controllers, method, method.getAnnotation(RequestMapping.class));
         }
         logInitializedRequestPath();
     }
 
-    private void addHandlerExecutions(final Object controller) {
-        Method[] methods = controller.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(RequestMapping.class)) {
-                RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
-                addHandlerExecutions(controller, method, requestMapping);
-            }
+    private Set<Method> getRequestMappingMethod(final Set<Class<?>> classes) {
+        return classes.stream()
+                .flatMap(clazz -> ReflectionUtils.getAllMethods(clazz).stream())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toSet());
+    }
+
+    private void addHandlerExecutions(final Map<Class<?>, Object> controllers,
+                                      final Method method,
+                                      final RequestMapping requestMapping) {
+        HandlerExecution handlerExecution = getHandlerExecution(controllers, method);
+        List<HandlerKey> handlerKeys = getHandlerKeys(requestMapping.value(), requestMapping.method());
+        for (HandlerKey handlerKey : handlerKeys) {
+            handlerExecutions.add(handlerKey, handlerExecution);
         }
     }
 
-    private void addHandlerExecutions(final Object controller,
-                                      final Method method,
-                                      final RequestMapping requestMapping) {
-        String uri = requestMapping.value();
-        RequestMethod[] requestMethods = requestMapping.method();
-        for (RequestMethod requestMethod : requestMethods) {
-            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
-            HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
-            handlerExecutions.add(handlerKey, handlerExecution);
-        }
+    private HandlerExecution getHandlerExecution(final Map<Class<?>, Object> controllers, final Method method) {
+        Object controller = controllers.get(method.getDeclaringClass());
+        return new HandlerExecution(controller, method);
+    }
+
+    private List<HandlerKey> getHandlerKeys(final String uri, final RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(uri, requestMethod))
+                .collect(Collectors.toList());
     }
 
     private void logInitializedRequestPath() {
