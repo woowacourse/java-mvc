3742926e844184e62533f03fc2220a2a27f1b492
yeon-06 <rnjstldus2@gmail.com>
2022-09-27 20:33:20 +0900

    test: 테스트 검증 방식 변경

--- mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -1,10 +1,15 @@
 package nextstep.mvc.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Map;
 import nextstep.web.support.MediaType;
 import org.junit.jupiter.api.DisplayName;
@@ -17,15 +22,21 @@ class JsonViewTest {
     void jsonParsing() throws Exception {
         // given
         final JsonView jsonView = new JsonView();
-        final Map<String, String> model = Map.of("key", "value");
+        final Map<String, String> model = Map.of("key", "value", "key2", "value2");
         final HttpServletRequest request = mock(HttpServletRequest.class);
         final HttpServletResponse response = mock(HttpServletResponse.class);
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final StringWriter stringWriter = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        when(response.getWriter()).thenReturn(printWriter);
 
         // when
         jsonView.render(model, request, response);
 
         // then
         verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        verify(response).getWriter();
+        assertThat(stringWriter.toString()).isEqualTo(objectMapper.writeValueAsString(model));
     }
 }
