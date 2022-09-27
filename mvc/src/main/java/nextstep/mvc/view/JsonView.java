2c3a872905fd1f72346da2e4ab754185a8284415
shindong96 <2006peter@naver.com>
2022-09-27 13:38:25 +0900

    feat: JsonView 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,27 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        String jsonValue = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+
+        if (model.size() == 1) {
+            final Object key = model.keySet().toArray()[0];
+            final Object value = model.get(key);
+            jsonValue = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value);
+        }
+
+        response.getWriter().write(jsonValue);
     }
 }
---
+++ mvc/src/test/java/samples/TestUser.java
@@ -0,0 +1,15 @@
+package samples;
+
+public class TestUser {
+
+    public String name;
+    public int age;
+
+    public TestUser() {
+    }
+
+    public TestUser(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
