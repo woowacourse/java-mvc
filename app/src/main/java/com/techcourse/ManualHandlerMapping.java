5a8b776305e3f48bebb11fa9413ebab98a030d67
meatsby <meatsby@gmail.com>
2022-09-27 14:50:41 +0900

    refactor: LogoutController 수정

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,15 +1,15 @@
 package com.techcourse;
 
-import com.techcourse.controller.*;
+import com.techcourse.controller.RegisterController;
+import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 import nextstep.mvc.HandlerMapping;
 import nextstep.mvc.controller.asis.Controller;
 import nextstep.mvc.controller.asis.ForwardController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ManualHandlerMapping implements HandlerMapping {
 
@@ -20,7 +20,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/logout", new LogoutController());
         controllers.put("/register/view", new RegisterViewController());
         controllers.put("/register", new RegisterController());
 
--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -2,14 +2,18 @@ package com.techcourse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import nextstep.mvc.controller.asis.Controller;
+import jakarta.servlet.http.HttpSession;
+import nextstep.mvc.view.JspView;
+import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.RequestMapping;
+import nextstep.web.support.RequestMethod;
 
-public class LogoutController implements Controller {
+public class LogoutController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var session = req.getSession();
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public ModelAndView logout(final HttpServletRequest req, final HttpServletResponse res) {
+        HttpSession session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return new ModelAndView(JspView.from("redirect:/"));
     }
 }
