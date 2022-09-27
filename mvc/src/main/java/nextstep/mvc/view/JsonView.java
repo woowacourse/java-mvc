c6945f0486a2309b6786c238545be75753364cfd
Chris <bjuuuu98@gmail.com>
2022-09-27 12:52:44 +0900

    feat: JsonView 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,37 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.print(toJson(model));
+    }
+
+    private String toJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return OBJECT_MAPPER.writeValueAsString(getFirstValue(model));
+        }
+        return OBJECT_MAPPER.writeValueAsString(model);
+    }
+
+    private Object getFirstValue(Map<String, ?> model) {
+        return model.keySet()
+                .stream()
+                .map(model::get)
+                .findFirst()
+                .orElseThrow();
     }
 }
---
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -0,0 +1,45 @@
+package nextstep.mvc.view;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JsonViewTest {
+
+    @DisplayName("데이터가 하나이면 value만 반환한다.")
+    @Test
+    void render_OneValue() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView jsonView = new JsonView();
+        Map<String, Object> model = Map.of("key", "value");
+        jsonView.render(model, request, response);
+
+        verify(response.getWriter()).write("\"value\"");
+    }
+
+    @DisplayName("데이터가 여러개이면 key와 value의 쌍들을 반환한다.")
+    @Test
+    void render_MultiMap() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView jsonView = new JsonView();
+        Map<String, Object> model = Map.of("firstKey", "firstValue", "secondKey", "secondValue");
+        jsonView.render(model, request, response);
+
+        verify(response.getWriter()).write("{\"firstKey\":\"firstValue\",\"secondKey\":\"secondValue\"}");
+    }
+}
