a2251890163027c8b0b42e085a41281e7fd24ba1
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-09-27 22:49:00 +0900

    [MVC 구현하기 - 3단계] 호호(유현호) 미션 제출합니다. (#315)
    * refactor: 어노테이션 기반으로 수정
    
    * feat: json으로 응답 기능 추가
    
    * fix: 인덱스페이지 이동되도록 수정
    
    * fix: 로그아웃 컨트롤러 수정
    
    * refactor: JsonView 메서드 분리

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,33 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final int SINGLE_DATE = 1;
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        if (model.size() == SINGLE_DATE) {
+            writer.write(OBJECT_MAPPER.writeValueAsString(getSingleModel(model)));
+            return;
+        }
+        writer.write(OBJECT_MAPPER.writeValueAsString(model));
+    }
+
+    private Object getSingleModel(final Map<String, ?> model) {
+        return model.values().stream()
+                .findFirst()
+                .orElseThrow();
     }
 }
