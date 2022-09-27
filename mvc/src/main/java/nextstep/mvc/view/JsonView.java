963c1597149a247247dc4f18d11c75ab007859e1
leokim <kunga077@gmail.com>
2022-09-27 13:46:05 +0900

    feat: json view render 로직 구현

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,23 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final String CONTENT_TYPE = MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter writer = response.getWriter();
+        writer.write(objectMapper.writeValueAsString(model));
     }
 }
