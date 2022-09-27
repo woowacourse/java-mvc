2badd816f7faee6707b6e6f64e395ee68ae0ca35
summerlunaa <summerlunaa9@gmail.com>
2022-09-27 12:50:14 +0900

    feat: model에 데이터 개수에 따라 Json 형태가 바뀌도록 로직 추가

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -14,9 +14,19 @@ public class JsonView implements View {
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         String value = OBJECT_MAPPER.writerWithDefaultPrettyPrinter()
-                .writeValueAsString(model);
+                .writeValueAsString(getModelOrFirstValue(model));
 
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(value);
+    }
+
+    private Object getModelOrFirstValue(final Map<String, ?> model) {
+        if (model.size() != 1) {
+            return model;
+        }
+        return model.values()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("value 값이 존재하지 않습니다."));
     }
 }
