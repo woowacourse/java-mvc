c2e0b602d9f2b15864a507d571c4a6aab0e66d3e
yoon <duddbs774@gmail.com>
2022-09-27 10:11:07 +0900

    refactor: 기존 인터페이스 기반 Controller 를 어노테이션 기반으로 변경

--- app/src/main/java/com/techcourse/WelcomePageHandlerMapping.java
+++ app/src/main/java/com/techcourse/WelcomePageHandlerMapping.java
@@ -1,10 +1,5 @@
 package com.techcourse;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
-import com.techcourse.controller.LogoutController;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,20 +9,15 @@ import nextstep.mvc.controller.asis.ForwardController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ManualHandlerMapping implements HandlerMapping {
+public class WelcomePageHandlerMapping implements HandlerMapping {
 
-    private static final Logger log = LoggerFactory.getLogger(ManualHandlerMapping.class);
+    private static final Logger log = LoggerFactory.getLogger(WelcomePageHandlerMapping.class);
 
     private static final Map<String, Controller> controllers = new HashMap<>();
 
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
-        controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
