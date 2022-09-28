b6bd9ef1a6b011ad4ac6d374aaa31b9d76e69413
youngwooyoo <fbduddn97@naver.com>
2022-09-28 20:18:53 +0900

    refactor: objectMapper 상수로 관리

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -6,20 +6,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 import nextstep.web.support.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
-    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         try {
-            ObjectMapper mapper = new ObjectMapper();
             response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-            String result = mapper.writeValueAsString(model);
+            String result = OBJECT_MAPPER.writeValueAsString(model);
             response.getWriter().write(result);
         } catch (IOException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
