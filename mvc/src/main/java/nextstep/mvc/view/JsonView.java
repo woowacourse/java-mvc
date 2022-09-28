247a117c787c338d59be2df2da24edc0d41f3f37
injoon2019 <ison@sfu.ca>
2022-09-28 22:03:18 +0900

    feat: json 개수에 따른 분기 처리

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +14,17 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        String jsonResult = convertMap(model);
+
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(OBJECT_MAPPER.writeValueAsString(model));
+    }
+
+    private String convertMap(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() > 1) {
+            return OBJECT_MAPPER.writeValueAsString(model);
+        }
+
+        return OBJECT_MAPPER.writeValueAsString(model.values().toArray()[0]);
     }
 }
