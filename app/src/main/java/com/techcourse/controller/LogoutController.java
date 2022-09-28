0335476051945eadbc4fcd1c7d83d446a7517c8f
injoon2019 <ison@sfu.ca>
2022-09-28 13:59:46 +0900

    refactor: 컨트롤러 어노테이션 추가

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -4,9 +4,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
 
+@Controller
 public class LogoutController {
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
