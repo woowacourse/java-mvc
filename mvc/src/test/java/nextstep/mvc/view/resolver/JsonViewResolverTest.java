d491029011a97d1387947b1a88b2592f654bb357
Richard JEON <ztzy1907@gmail.com>
2022-09-28 12:51:08 +0900

    test: JsonViewResolverTest 시나리오 가독성 개선

--- mvc/src/test/java/nextstep/mvc/view/resolver/JsonViewResolverTest.java
+++ mvc/src/test/java/nextstep/mvc/view/resolver/JsonViewResolverTest.java
@@ -12,7 +12,6 @@ import support.User;
 
 class JsonViewResolverTest {
 
-
     @MethodSource("jsonViewResolver_resolve")
     @ParameterizedTest(name = "JsonViewResolver의 resolve메서드는 {0}")
     void sampleTest(String description, Object view, boolean expected) {
@@ -21,7 +20,7 @@ class JsonViewResolverTest {
 
         // when
         final View resolve = jsonViewResolver.resolve(view);
-        final boolean returnValueIsNull = Objects.isNull(resolve);
+        final boolean returnValueIsNull = Objects.nonNull(resolve);
 
         // then
         assertThat(returnValueIsNull).isEqualTo(expected);
@@ -29,11 +28,11 @@ class JsonViewResolverTest {
 
     private static Stream<Arguments> jsonViewResolver_resolve() {
         return Stream.of(
-                Arguments.of("view가 null일 경우 null을 반환한다", null, true),
-                Arguments.of("view가 .jsp로 끝나는 String일 경우 null을 반환한다", "index.jsp", true),
-                Arguments.of("view가 redirect:로 시작하는 String일 경우 null을 반환한다", "redirect:/index.jsp", true),
+                Arguments.of("view가 null일 경우 null을 반환한다", null, false),
+                Arguments.of("view가 .jsp로 끝나는 String일 경우 null을 반환한다", "index.jsp", false),
+                Arguments.of("view가 redirect:로 시작하는 String일 경우 null을 반환한다", "redirect:/index.jsp", false),
                 Arguments.of("view가 null이 아니고 redirect:로 시작하거나 .jsp로 끝나는 String이 아니면 JsonView를 반환한다",
-                        new User(1L, "gugu", "hkkang@woowahan.com"), false)
+                        new User(1L, "gugu", "hkkang@woowahan.com"), true)
         );
     }
 }
