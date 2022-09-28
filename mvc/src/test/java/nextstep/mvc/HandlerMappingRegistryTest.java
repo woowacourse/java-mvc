b10496976a8b6b01834b73c06c5f1ef5f2c83f67
yh20studio(tiki) <yh20studio@gmail.com>
2022-09-28 21:31:37 +0900

    test: 핸들러 매핑을 찾지 못했을 때 예외 테스트 추가

--- mvc/src/test/java/nextstep/mvc/HandlerMappingRegistryTest.java
+++ mvc/src/test/java/nextstep/mvc/HandlerMappingRegistryTest.java
@@ -1,6 +1,7 @@
 package nextstep.mvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,5 +26,37 @@ class HandlerMappingRegistryTest {
         handlerMappingRegistry.init();
 
         assertThat(handlerMappingRegistry.getHandlerMapping(request)).isInstanceOf(HandlerExecution.class);
+    }
+
+    @Test
+    @DisplayName("해당하는 uri가 없어서 핸들러 매핑을 찾지 못하면 예외가 발생한다.")
+    void getHandlerMappingUriException() {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/invalid");
+        when(request.getMethod()).thenReturn("GET");
+
+        final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+        handlerMappingRegistry.add(new AnnotationHandlerMapping("samples"));
+        handlerMappingRegistry.init();
+
+        assertThatThrownBy(() -> handlerMappingRegistry.getHandlerMapping(request))
+                .isExactlyInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("http method를 지원하지 않아서 핸들러 매핑을 찾지 못하면 예외가 발생한다.")
+    void getHandlerMappingMethodException() {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("POST");
+
+        final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+        handlerMappingRegistry.add(new AnnotationHandlerMapping("samples"));
+        handlerMappingRegistry.init();
+
+        assertThatThrownBy(() -> handlerMappingRegistry.getHandlerMapping(request))
+                .isExactlyInstanceOf(RuntimeException.class);
     }
 }
