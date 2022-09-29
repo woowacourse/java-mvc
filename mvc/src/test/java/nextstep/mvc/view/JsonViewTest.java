560aff333513ac29416835b2dcbf9e19e3da48bf
ihan-ul <dkfghg3@naver.com>
2022-09-29 15:42:47 +0900

    test: ViewJson model 데이터가 2개 이상일 경우, 테스트 코드 추가

--- mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
+++ mvc/src/test/java/nextstep/mvc/view/JsonViewTest.java
@@ -27,17 +27,40 @@ class JsonViewTest {
         view.render(model, request, response);
 
         verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        verify(printWriter).write("{\"id\":1,\"name\":\"slow\"}");
+        verify(printWriter).write("{\"id\":1,\"name\":\"slow\",\"email\":\"\"}");
+    }
+
+    @Test
+    void model_데이터가_2개_이상일_경우_Map_형태로_반환한다() throws Exception {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+        final PrintWriter printWriter = mock(PrintWriter.class);
+
+        when(response.getWriter()).thenReturn(printWriter);
+
+        final User user = new User(1L, "slow", "slow@naver.com");
+        final Map<String, ?> model = Map.of("user", user);
+        final View view = new JsonView();
+        view.render(model, request, response);
+
+        verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        verify(printWriter).write("{\"id\":1,\"name\":\"slow\",\"email\":\"slow@naver.com\"}");
     }
 
     private class User {
 
         private final long id;
         private final String name;
+        private final String email;
 
         public User(final long id, final String name) {
+            this(id, name, "");
+        }
+
+        public User(long id, String name, String email) {
             this.id = id;
             this.name = name;
+            this.email = email;
         }
 
         public long getId() {
@@ -46,6 +69,10 @@ class JsonViewTest {
 
         public String getName() {
             return name;
+        }
+
+        public String getEmail() {
+            return email;
         }
     }
 }
