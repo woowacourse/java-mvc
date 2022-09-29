891d01d4ab7cba0977af323f8c71bc400287b193
meatsby <meatsby@gmail.com>
2022-09-29 12:13:51 +0900

    refactor: model 데이터가 1개인 경우 json 반환 로직 추가

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,14 +10,25 @@ import nextstep.web.support.MediaType;
 public class JsonView implements View {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final int SINGLE_OBJECT_COUNT = 1;
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        String jsonValue = OBJECT_MAPPER.writeValueAsString(model);
-
+        String jsonValue = toJsonValue(model);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter()
                 .write(jsonValue);
+    }
+
+    private String toJsonValue(final Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == SINGLE_OBJECT_COUNT) {
+            Object object = model.values()
+                    .stream()
+                    .findFirst()
+                    .orElseThrow();
+            return OBJECT_MAPPER.writeValueAsString(object);
+        }
+        return OBJECT_MAPPER.writeValueAsString(model);
     }
 }
