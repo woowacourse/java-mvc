df8d35ca7f39621c1e375004e1094e984eaea7f6
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-29 13:02:51 +0900

    refactor: JsonView 모델 1개 처리 추가

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonView implements View {
@@ -13,9 +16,26 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        final String jsonData = objectMapper.writerWithDefaultPrettyPrinter()
-                .writeValueAsString(model);
+        final String jsonData = generateJsonData(model);
 
         response.getWriter().print(jsonData);
+    }
+
+    private String generateJsonData(Map<String, ?> model) throws JsonProcessingException {
+        if (hasOneElement(model)) {
+            final Entry<String, ?> entry = model.entrySet()
+                    .stream()
+                    .findAny()
+                    .get();
+
+            return objectMapper.writer().writeValueAsString(entry.getValue());
+        }
+
+         return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(model);
+    }
+
+    private boolean hasOneElement(Map<String, ?> model) {
+        return model.size() == 1;
     }
 }
