bb97c16d8e2e076301da95645de807c6f883c931
sinb57 <sinb57@naver.com>
2022-09-29 12:38:27 +0900

    refactor: /register/view uri 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -13,8 +13,13 @@ import nextstep.web.support.RequestMethod;
 @Controller
 public class RegisterController {
 
+    @RequestMapping(value = "/register", method= RequestMethod.GET)
+    public ModelAndView showView(final HttpServletRequest req, final HttpServletResponse res) {
+        return Page.REGISTER.getModelAndView();
+    }
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) {
+    public ModelAndView register(final HttpServletRequest req, final HttpServletResponse res) {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
