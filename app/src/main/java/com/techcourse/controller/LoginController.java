871d3ae8298a2d723a2eb79b04a67e01f6dd2e73
sinb57 <sinb57@naver.com>
2022-09-29 12:34:58 +0900

    refactor: LogoutController 제거

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -5,15 +5,13 @@ import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
@@ -46,7 +44,15 @@ public class LoginController {
 
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return Page.INDEX.getRedirectModelAndView();        });
+            return Page.INDEX.getRedirectModelAndView();
+        });
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(final HttpServletRequest req, final HttpServletResponse res) {
+        final var session = req.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return new ModelAndView(new JspView("redirect:/"));
     }
 
     private boolean isNullAnyParameter(final String... parameters) {
