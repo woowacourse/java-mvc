a83496a1c2fa85cf59ccf18adab58506c9cda503
shindong96 <2006peter@naver.com>
2022-09-29 17:36:23 +0900

    refactor: 메서드 분리

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,20 +9,25 @@ import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 
-        final ObjectMapper objectMapper = new ObjectMapper();
-        String jsonValue = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+        String jsonValue = getJsonValue(model);
 
         if (model.size() == 1) {
             final Object key = model.keySet().toArray()[0];
             final Object value = model.get(key);
-            jsonValue = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(value);
+            jsonValue = getJsonValue(value);
         }
 
         response.getWriter().write(jsonValue);
+    }
+
+    private String getJsonValue(Object model) throws JsonProcessingException {
+        return OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(model);
     }
 }
