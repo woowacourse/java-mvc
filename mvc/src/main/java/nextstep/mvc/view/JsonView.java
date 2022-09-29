d44d5d708b1061811a04db97cb3fa53ba3e328f0
토르 <injoon2019@gmail.com>
2022-09-29 13:07:21 +0900

    [MVC 구현하기 - 3단계] 토르(손인준) 미션 제출합니다. (#283)
    * feat: ControllerScanner 구현
    
    * feat: JsonView 구현
    
    * style: 불필요한 import 제거
    
    * refactor: 레거시 코드 제거
    
    * refactor: 컨트롤러 ModelAndView 반환
    
    * refactor: 컨트롤러 어노테이션 추가
    
    * feat: json 개수에 따른 분기 처리

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,30 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
+import nextstep.web.support.MediaType;
 
 public class JsonView implements View {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        String jsonResult = convertMap(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(OBJECT_MAPPER.writeValueAsString(model));
+    }
+
+    private String convertMap(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() > 1) {
+            return OBJECT_MAPPER.writeValueAsString(model);
+        }
+
+        return OBJECT_MAPPER.writeValueAsString(model.values().toArray()[0]);
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -1,7 +1,6 @@
 package nextstep.mvc.controller.tobe;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
