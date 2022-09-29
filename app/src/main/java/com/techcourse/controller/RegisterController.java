33ce4eabffab7e3464be7d92763c74ca04cf4b9e
sinb57 <sinb57@naver.com>
2022-09-29 12:42:49 +0900

    refactor: RegisterController 리팩토링

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package com.techcourse.controller;
 
+import java.util.Objects;
+import java.util.stream.Stream;
+
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 
@@ -20,12 +23,22 @@ public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView register(final HttpServletRequest req, final HttpServletResponse res) {
-        final var user = new User(2,
-                req.getParameter("account"),
-                req.getParameter("password"),
-                req.getParameter("email"));
+        final var account = req.getParameter("account");
+        final var password = req.getParameter("password");
+        final var email = req.getParameter("email");
+
+        if (isNullAnyParameter(account, password)) {
+            return Page.ERROR_404.getModelAndView();
+        }
+
+        final var user = new User(2, account, password, email);
         InMemoryUserRepository.save(user);
 
         return Page.INDEX.getRedirectModelAndView();
+    }
+
+    private boolean isNullAnyParameter(final String... parameters) {
+        return Stream.of(parameters)
+                .anyMatch(Objects::nonNull);
     }
 }
