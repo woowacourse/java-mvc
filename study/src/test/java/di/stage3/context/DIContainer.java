b16924ab0fd212f93d2108379f3b32f1fe102635
kang-hyungu <hkkang@woowahan.com>
2023-08-28 15:17:45 +0900

    라이브러리 버전 정리 및 실습 코드 추가

---
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -0,0 +1,20 @@
+package di.stage3.context;
+
+import java.util.Set;
+
+/**
+ * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
+ */
+class DIContainer {
+
+    private final Set<Object> beans;
+
+    public DIContainer(final Set<Class<?>> classes) {
+        this.beans = Set.of();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(final Class<T> aClass) {
+        return null;
+    }
+}
---
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -0,0 +1,24 @@
+package di.stage4.annotations;
+
+import java.util.Set;
+
+/**
+ * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
+ */
+class DIContainer {
+
+    private final Set<Object> beans;
+
+    public DIContainer(final Set<Class<?>> classes) {
+        this.beans = Set.of();
+    }
+
+    public static DIContainer createContainerForPackage(final String rootPackageName) {
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(final Class<T> aClass) {
+        return null;
+    }
+}
