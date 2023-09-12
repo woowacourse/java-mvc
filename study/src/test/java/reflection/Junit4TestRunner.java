448deb433e996234ee76245f32ca24416a5b7dbe
greeng00se <thegreengoose7@gmail.com>
2023-09-12 15:36:14 +0900

    feat: Reflection 학습 테스트

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,23 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Constructor<Junit4Test> constructor = clazz.getConstructor();
+        final Junit4Test junit4Test = constructor.newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.getDeclaredAnnotation(MyTest.class) != null) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,17 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +22,9 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Stream.of(Controller.class, Service.class, Repository.class)
+                .flatMap(clazz -> reflections.getTypesAnnotatedWith((Class<? extends Annotation>) clazz).stream())
+                .map(Class::getSimpleName)
+                .forEach(log::info);
     }
 }
