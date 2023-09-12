0e878c40694381c282892f10bce48e77f4f82488
Combi153 <jcm04@naver.com>
2023-09-12 15:42:12 +0900

    [학습 테스트] Reflection 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,6 +1,12 @@
 package reflection;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 class Junit3TestRunner {
 
@@ -9,5 +15,18 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        // constructor 흑마법
+        Constructor<Junit3Test> constructor = clazz.getDeclaredConstructor();
+        Junit3Test junit3Test = constructor.newInstance();
+
+        // method 흑마법
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().contains("test"))
+                .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            method.invoke(junit3Test);
+        }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,12 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +16,15 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Constructor<Junit4Test> constructor = clazz.getDeclaredConstructor();
+        Junit4Test junit4Test = constructor.newInstance();
+
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> Objects.nonNull(method.getAnnotation(MyTest.class)))
+                .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
     }
 }
