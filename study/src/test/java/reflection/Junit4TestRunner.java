0c6cf1c428eb5abfb1229341084ca89b23f19f8c
jjongwa <troas96@naver.com>
2023-09-12 19:59:16 +0900

    test: Junit4TestRunner 테스트 진행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +12,16 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        for (final Method method : declaredMethods) {
+            final MyTest methodAnnotation = method.getAnnotation(MyTest.class);
+            if (methodAnnotation != null) {
+                assertThat(methodAnnotation).isNotNull();
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
