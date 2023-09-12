d1184bc0cd35406476ec8b91781e1a4114256483
seokjin8678 <seokjin8678@gmail.com>
2023-09-12 20:29:44 +0900

    refactor: AnnotationHandlerMapping 메서드 리팩터링

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,9 +1,13 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import static java.util.stream.Collectors.toList;
+
 import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.reflections.Reflections;
@@ -29,20 +33,18 @@ public class AnnotationHandlerMapping {
         Reflections reflections = new Reflections(basePackage);
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
         for (Class<?> controller : controllers) {
-            Method[] methods = controller.getDeclaredMethods();
-            Object instance = getInstance(controller);
-            for (Method method : methods) {
-                if (method.isAnnotationPresent(RequestMapping.class)) {
-                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                    String path = requestMapping.value();
-                    RequestMethod[] httpMethods = requestMapping.method();
-                    for (RequestMethod httpMethod : httpMethods) {
-                        HandlerKey handlerKey = new HandlerKey(path, httpMethod);
-                        HandlerExecution handlerExecution = new HandlerExecution(instance, method);
-                        handlerExecutions.put(handlerKey, handlerExecution);
-                    }
-                }
-            }
+            initializeController(controller);
+        }
+    }
+
+    private void initializeController(Class<?> controller) {
+        Object instance = getInstance(controller);
+        List<Method> methods = getAnnotatedMethod(controller);
+        for (Method method : methods) {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            List<HandlerKey> handlerKeys = getHandlerKeys(annotation);
+            HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+            putHandlerExecution(handlerKeys, handlerExecution);
         }
     }
 
@@ -54,10 +56,31 @@ public class AnnotationHandlerMapping {
         }
     }
 
+    private List<Method> getAnnotatedMethod(Class<?> controller) {
+        Method[] methods = controller.getMethods();
+        return Arrays.stream(methods)
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .collect(toList());
+    }
+
+    private List<HandlerKey> getHandlerKeys(RequestMapping annotation) {
+        String url = annotation.value();
+        RequestMethod[] httpMethods = annotation.method();
+        return Arrays.stream(httpMethods)
+            .map(httpMethod -> new HandlerKey(url, httpMethod))
+            .collect(toList());
+    }
+
+    private void putHandlerExecution(List<HandlerKey> handlerKeys, HandlerExecution handlerExecution) {
+        for (HandlerKey handlerKey : handlerKeys) {
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        String method = request.getMethod();
         String requestURI = request.getRequestURI();
-        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
         return handlerExecutions.get(handlerKey);
     }
 }
