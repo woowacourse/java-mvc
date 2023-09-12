feee92b13379371898e2cb1b161063e0bd660ab2
thdwoqor <celebarte01@gmail.com>
2023-09-12 21:05:03 +0900

    chore: 학습 테스트 1단계

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+
+        Junit3Test junit3Test = clazz.getConstructor().newInstance();
+
+        for (Method method : methods) {
+            if(method.getName().startsWith("test")){
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
