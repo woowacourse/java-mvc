5bdbf4a780a166cce7b5e11cd3714d3a7e087637
kiarakim <kiarakim413@gmail.com>
2023-09-12 19:08:50 +0900

    feat: 1단계 - @MVC 프레임워크 구현하기

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +27,30 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        putHandlerExecutions();
+    }
+
+    private void putHandlerExecutions() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : typesAnnotatedWith) {
+            for (Method method : aClass.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    String url = annotation.value();
+                    RequestMethod requestMethod = annotation.method()[0];
+                    HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                    HandlerExecution handlerExecution = new HandlerExecution(method);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
