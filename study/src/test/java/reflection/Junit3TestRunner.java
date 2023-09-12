ba5e40bc23857b3761f327f375bbf6f4f183fa9a
jyeost <pig9619@naver.com>
2023-09-12 20:49:05 +0900

    study: 수업 미션 코드 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +11,13 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Junit3Test junit3Test = clazz.getConstructor().newInstance();
+
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
