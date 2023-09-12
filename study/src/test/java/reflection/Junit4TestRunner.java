0725da8b55456a2d01d3add29e3a422e2d0f2930
HubCreator <chocomilk8604@gmail.com>
2023-09-12 22:42:33 +0900

    feat: 학습 테스트 구현

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Constructor<Junit4Test> constructor = clazz.getDeclaredConstructor();
+        final Junit4Test junit4Test = constructor.newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
