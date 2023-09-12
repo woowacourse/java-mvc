9aede1645ef89d16eb3327af54802c3615b3b99e
LJW25 <dbjwlee@gmail.com>
2023-09-12 21:45:50 +0900

    test: Reflection 학습테스트 구현

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,20 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = new Junit4Test();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
