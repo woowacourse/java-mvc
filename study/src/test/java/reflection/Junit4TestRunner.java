e90343ea19c3f8ee862d73fec15133f06231781e
woosung1223 <dntjd991223@naver.com>
2023-09-12 23:00:35 +0900

    feat: Reflection 학습 테스트 구현

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,8 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 class Junit4TestRunner {
 
@@ -9,5 +11,18 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test instance = new Junit4Test();
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            Annotation[] annotations = method.getDeclaredAnnotations();
+
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType().equals(MyTest.class)) {
+                    method.invoke(instance);
+                }
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+import java.util.Set;
 
 class ReflectionsTest {
 
@@ -14,5 +18,20 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        controllers.stream()
+                .map(Class::getSimpleName)
+                .forEach(name -> log.info("controller = {}", name));
+
+        services.stream()
+                .map(Class::getSimpleName)
+                .forEach(name -> log.info("service = {}", name));
+
+        repositories.stream()
+                .map(Class::getSimpleName)
+                .forEach(name -> log.info("repository = {}", name));
     }
 }
