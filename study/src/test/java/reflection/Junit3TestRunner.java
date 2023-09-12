be2c6634b4da129ae37aba481212bd58c65a3f1e
JunDonghyuk <ehdgur4814@naver.com>
2023-09-12 15:31:41 +0900

    test: Junit4Test 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,11 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +14,11 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+        for (Method method : methods) {
+            method.invoke(new Junit3Test());
+        }
     }
 }
