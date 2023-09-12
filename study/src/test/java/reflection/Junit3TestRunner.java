ed21652e8cefa36155d614db2eb92de9eaf4297f
sosow0212 <sosow0212@naver.com>
2023-09-12 16:35:59 +0900

    feat: reflection 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,12 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +15,24 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        Junit3Test instance = clazz.newInstance();
+
+        Class<?> junit3Test = clazz.getClassLoader()
+                .loadClass("reflection.Junit3Test");
+
+        Method[] methods = junit3Test.getMethods();
+
+        List<Method> testMethod = Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        testMethod.forEach(method -> {
+            try {
+                method.invoke(instance);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,30 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test instance = clazz.newInstance();
+        Method[] methods = clazz.getMethods();
+
+        List<Method> methodsContainedAnnotation = Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        for (Method method : methodsContainedAnnotation) {
+            try {
+                method.invoke(instance);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
