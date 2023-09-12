f8dfd973b82861e7f01a3cf170e3a13d311d1257
apptie <appti@naver.com>
2023-09-12 15:37:46 +0900

    test: 리플렉션 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,19 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test junit3Test = new Junit3Test();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (final Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,36 @@
 package reflection;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+    void showAnnotationClass() {
+        final Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> classWithControllerAnnotation = reflections.getTypesAnnotatedWith(Controller.class);
+        final Set<Class<?>> classWithServiceAnnotation = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> classWithRepositoryAnnotation = reflections.getTypesAnnotatedWith(Repository.class);
+
+        for (final Class<?> clazz : classWithControllerAnnotation) {
+            log.info("{} 클래스는 @Controller가 명시되어 있습니다.", clazz);
+        }
+
+        for (final Class<?> clazz : classWithServiceAnnotation) {
+            log.info("{} 클래스는 @Service가 명시되어 있습니다.", clazz);
+        }
+
+        for (final Class<?> clazz : classWithRepositoryAnnotation) {
+            log.info("{} 클래스는 @Repository가 명시되어 있습니다.", clazz);
+        }
     }
 }
