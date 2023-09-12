9a8c67618f9be2eba056c3bfde13a1bdee58a382
odo27 <odo27@naver.com>
2023-09-12 21:13:52 +0900

    feat: 리플렉션을 활용해 어노테이션이 있는 클래스 찾기 학습테스트 구현

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,30 @@
 package reflection;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("reflection.examples");
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        logAnnotatedClass(controllerClasses);
+        logAnnotatedClass(serviceClasses);
+        logAnnotatedClass(repositoryClasses);
+    }
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+    private void logAnnotatedClass(Set<Class<?>> annotatedClasses) {
+        annotatedClasses.forEach(it -> log.info(it.getName()));
     }
 }
