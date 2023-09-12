f75221cabc6d23dbe896f5c30f238f5704099d23
greeng00se <thegreengoose7@gmail.com>
2023-09-12 22:20:42 +0900

    feat: Servlet과 Servlet Container 학습 테스트

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,5 @@
 package reflection;
 
-import java.lang.annotation.Annotation;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
@@ -23,7 +19,7 @@ class ReflectionsTest {
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
         Stream.of(Controller.class, Service.class, Repository.class)
-                .flatMap(clazz -> reflections.getTypesAnnotatedWith((Class<? extends Annotation>) clazz).stream())
+                .flatMap(clazz -> reflections.getTypesAnnotatedWith(clazz).stream())
                 .map(Class::getSimpleName)
                 .forEach(log::info);
     }
