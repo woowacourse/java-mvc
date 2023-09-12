58824d56c0ac5237c3676feb2bcc9326a3606aa9
gitchan <eunkeeeee@gmail.com>
2023-09-12 17:29:31 +0900

    feat: field, method 테스트 추가

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,13 +2,20 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        Class<?> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
         Object obj = new Junit3Test();
@@ -18,5 +25,28 @@ class Junit3TestRunner {
                 method.invoke(obj, null);
             }
         }
+    }
+
+    @Test
+    void fieldTest() {
+        Class<?> clazz = Junit3Test.class;
+        Field[] fields = clazz.getDeclaredFields();
+
+        assertThat(fields).isEmpty();
+    }
+
+    @Test
+    void methodTest() {
+        Class<?> clazz = Junit3Test.class;
+        Method[] methods = clazz.getDeclaredMethods();
+        List<String> methodNames = Arrays.stream(methods)
+                .map(Method::getName)
+                .collect(Collectors.toList());
+
+        assertAll(
+                () -> assertThat(methodNames.size()).isEqualTo(3),
+                () -> assertThat(methodNames)
+                        .containsAll(List.of("test1", "test2", "three"))
+        );
     }
 }
