13f48f283dd5c0a762be209929fb15ff46b91c47
Combi153 <jcm04@naver.com>
2023-09-12 19:10:30 +0900

    [학습 테스트] Servlet, Filter 학습 테스트

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -1,16 +1,23 @@
 package servlet.com.example;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
+import static org.eclipse.jdt.internal.compiler.util.Util.UTF_8;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding(UTF_8);
         chain.doFilter(request, response);
     }
 }
