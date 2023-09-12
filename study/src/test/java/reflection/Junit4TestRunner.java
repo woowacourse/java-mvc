651563f83d539ca5eba3fc4c4fb12f80e2c3c9aa
gitchan <eunkeeeee@gmail.com>
2023-09-12 22:41:09 +0900

    feat: Junit4Test에서 @MyTest 어노테이션이 있는 메소드 실행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,20 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // TODO Junit4Test에서 @MyTest 어노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.newInstance(), null);
+            }
+        }
     }
 }
