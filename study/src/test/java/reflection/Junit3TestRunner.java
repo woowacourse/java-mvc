ae9381a7502373ea4c6e8a4bdaa82c7284bf259b
bjk1649 <qhwns1649@naver.com>
2023-09-12 21:54:43 +0900

    test: 리플렉션 테스트 완료

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,24 @@
 package reflection;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final var clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+              .filter(method -> method.getName().startsWith("test"))
+              .forEach(method -> {
+                          try {
+                              method.invoke(clazz.getDeclaredConstructor().newInstance());
+                          } catch (Exception e) {
+                              throw new RuntimeException(e);
+                          }
+                      }
+              );
     }
 }
