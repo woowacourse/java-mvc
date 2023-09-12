0c7ffcb63d1ded87d0e11c6a9b52be325349c884
rawfishthelgh <rg970604@naver.com>
2023-09-12 17:35:46 +0900

    test: Reflection 실습

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
@@ -9,5 +11,11 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(new Junit4Test());
+            }
+        }
     }
 }
