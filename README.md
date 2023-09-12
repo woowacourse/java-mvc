dc1a228aab0ffb1b17392a89e5906cca957d3e53
greeng00se <thegreengoose7@gmail.com>
2023-09-12 23:48:04 +0900

    feat: AnnotationHandlerMappingTest 클래스의 실패하는 테스트를 통과시키기

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # @MVC 구현하기
+
+### 1 단계 - @MVC 프레임워크 구현하기
+
+[x] AnnotationHandlerMappingTest 클래스의 실패하는 테스트를 통과시키기
+
+### 2단계 - 점진적인 리팩터링
+
+
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +25,39 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize() throws Exception {
         log.info("Initialized AnnotationHandlerMapping!");
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> types = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> type : types) {
+            addHandlerExecutionsByType(type);
+        }
+    }
+
+    private void addHandlerExecutionsByType(final Class<?> type) throws Exception {
+        final Constructor<?>[] constructors = type.getConstructors();
+        final Object controller = constructors[0].newInstance();
+        for (final Method method : type.getDeclaredMethods()) {
+            final HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
+            }
+            final RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            addHandlerExecution(annotation, handlerExecution);
+        }
+    }
+
+    private void addHandlerExecution(final RequestMapping annotation, final HandlerExecution handlerExecution) {
+        for (final RequestMethod requestMethod : annotation.method()) {
+            final HandlerKey handlerKey = new HandlerKey(annotation.value(), requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
