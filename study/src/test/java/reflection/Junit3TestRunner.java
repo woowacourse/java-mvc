9942edea53bcab228be054b217047f69129f4316
songusika <sws981202@gmail.com>
2023-09-12 21:27:16 +0900

    test: 리플렉션으로 메서드를 실행하는 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,31 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
-class Junit3TestRunner {
+class Junit3TestRunner extends JunitTest {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        // given
+        final Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test junit3TestInstance = clazz.getDeclaredConstructor().newInstance();
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
 
+        // when
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (final Method declaredMethod : declaredMethods) {
+            declaredMethod.invoke(junit3TestInstance);
+        }
+
+        // then
+        assertThat(out.toString()).isEqualTo(String.join(System.lineSeparator(),
+                "Running Test1",
+                "Running Test2",
+                "Running Test3",
+                ""
+        ));
     }
 }
