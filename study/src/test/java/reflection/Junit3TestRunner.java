3d8dd3b8a8b7fefb76a612b0c186d9827bccf8b7
jjongwa <troas96@naver.com>
2023-09-12 19:40:02 +0900

    test: Junit3TestRunner 테스트 진행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,26 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        // 객체 생성 가능
+        final Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+
+        // 1. 객체에서 바로 메서드 호출
+        junit3Test.test1();
+        junit3Test.test2();
+
+        // 2. getDeclaredMethod로 메서드 찾아서 호출
+        final Method test1 = clazz.getDeclaredMethod("test1");
+        final Method test2 = clazz.getDeclaredMethod("test2");
+        test1.invoke(clazz.cast(junit3Test));
+        test2.invoke(clazz.cast(junit3Test));
+
+        // 3. getDeclaredMethods로 메서드 배열 가져온 후 for문 돌면서 호출
+        final Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.cast(junit3Test));
+            }
+        }
     }
 }
