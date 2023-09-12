524bed4236782d894cabf753fe76beb384135936
xxeol2 <jhj01177@naver.com>
2023-09-12 16:59:09 +0900

    study: reflection 학습 테스트 수행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,11 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,15 @@
 package reflection;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +20,16 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        /**
+         * getTypesAnnotatedWith -> 주어진 패키지나 클래스 경로에서 특정 애노테이션이 달린 클래스들을 검색하는 데 사용됨
+         * honorInherited: 상속된 어노테이션을 존중할지 여부
+         */
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Controller.class, true);
+        annotatedClasses.addAll(reflections.getTypesAnnotatedWith(Service.class, true));
+        annotatedClasses.addAll(reflections.getTypesAnnotatedWith(Repository.class, true));
+
+        for (final Class<?> clazz : annotatedClasses) {
+            log.info(clazz.getName());
+        }
     }
 }
