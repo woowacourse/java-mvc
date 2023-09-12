cf8b11f83442613a15de3d3a9789879518cd9e5f
JunDonghyuk <ehdgur4814@naver.com>
2023-09-13 00:11:22 +0900

    refactor: 미션 힌트에 따른 메서드 분리

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
-import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -8,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -29,7 +26,13 @@ public class AnnotationHandlerMapping {
     public void initialize() {
         log.info("Initializing AnnotationHandlerMapping starts!");
         for (Object base : basePackage) {
-            registerFromBasePackage((String) base);
+            ControllerScanner controllerScanner = new ControllerScanner(new Reflections(base));
+            Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+            Set<Method> methods = getRequestMappingMethods(controllers.keySet());
+            for (Method method : methods) {
+                RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                addHandlerExecutions(controllers, method, requestMapping);
+            }
         }
         log.info("Initializing AnnotationHandlerMapping succeeds!");
     }
@@ -42,45 +45,32 @@ public class AnnotationHandlerMapping {
             return handlerExecutions.get(handlerKey);
         } catch (IllegalArgumentException e) {
             log.error("Unsupported request method.");
-            throw e;
+            return null;
         }
     }
 
-    public void registerFromBasePackage(String basePackage) {
-        ControllerScanner controllerScanner = new ControllerScanner(new Reflections(basePackage));
-        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
-
-        for (Class<?> handler : controllers.keySet()) {
-            Object instance = controllers.get(handler);
-            List<Method> methods = getRequestMappingMethods(handler);
-            register(instance, methods);
+    private void addHandlerExecutions(Map<Class<?>, Object> controllers, Method method, RequestMapping requestMapping) {
+        Object instance = controllers.get(method.getDeclaringClass());
+        HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+        RequestMethod[] requestMethods = requestMapping.method();
+        List<HandlerKey> handlerKeys = mapHandlerKeys(requestMapping.value(), requestMethods);
+        for (HandlerKey handlerKey : handlerKeys) {
+            handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
 
-    private List<Method> getRequestMappingMethods(Class<?> handler) {
-        return Arrays.stream(handler.getMethods())
-                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+    private List<HandlerKey> mapHandlerKeys(String url, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
                 .collect(Collectors.toList());
     }
 
-    private void register(Object instance, List<Method> methods) {
-        for (Method method : methods) {
-            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-            String url = requestMapping.value();
-            RequestMethod[] requestMethods = requestMapping.method();
-            if (isInvalidRequestMapping(url, requestMethods)) {
-                continue;
-            }
-            for (RequestMethod requestMethod : requestMethods) {
-                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-                HandlerExecution handlerExecution = new HandlerExecution(instance, method);
-                handlerExecutions.put(handlerKey, handlerExecution);
-                log.info("uri = {}, method = {}, name = {} is mapped!", url, requestMethod.name(), method.getName());
-            }
-        }
-    }
-
-    private boolean isInvalidRequestMapping(String url, RequestMethod[] requestMethods) {
-        return url.isBlank() || requestMethods.length == 0;
+    private Set<Method> getRequestMappingMethods(Set<Class<?>> controllers) {
+        return controllers
+                .stream()
+                .map(Class::getMethods)
+                .flatMap(Arrays::stream)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toSet());
     }
 }
