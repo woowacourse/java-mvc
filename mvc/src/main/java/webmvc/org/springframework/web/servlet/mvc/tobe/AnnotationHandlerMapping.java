b91e2087546066e7de7f952f1a7b70444bccaa99
shin-mallang <huipulci1@naver.com>
2023-09-13 16:51:10 +0900

    1차 미션 완료

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +26,33 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : typesAnnotatedWith) {
+            try {
+                Object o = aClass.getConstructor().newInstance();
+                Method[] declaredMethods = o.getClass().getDeclaredMethods();
+                for (Method declaredMethod : declaredMethods) {
+                    Annotation[] declaredAnnotations = declaredMethod.getDeclaredAnnotations();
+                    for (Annotation declaredAnnotation : declaredAnnotations) {
+                        if (declaredAnnotation.annotationType().equals(RequestMapping.class)) {
+                            RequestMapping declaredAnnotation1 = (RequestMapping) declaredAnnotation;
+                            for (RequestMethod requestMethod : declaredAnnotation1.method()) {
+                                HandlerKey handlerKey = new HandlerKey(declaredAnnotation1.value(), requestMethod);
+                                HandlerExecution handlerExecution = new HandlerExecution(declaredMethod);
+                                handlerExecutions.put(handlerKey, handlerExecution);
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
