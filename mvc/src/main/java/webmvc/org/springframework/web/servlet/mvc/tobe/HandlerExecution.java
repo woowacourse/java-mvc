775beb4c4b93691c7ba93848e4df34fb8a923373
pilyang <pilyang.dev@gmail.com>
2023-09-13 21:56:50 +0900

    feat: AnnotaionHandlerMapping 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
+    private final Object controllerInstance;
+    private final Method handler;
+
+    public HandlerExecution(final Object controllerInstance, final Method handler) {
+        this.controllerInstance = controllerInstance;
+        this.handler = handler;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handler.invoke(controllerInstance, request, response);
     }
 }
