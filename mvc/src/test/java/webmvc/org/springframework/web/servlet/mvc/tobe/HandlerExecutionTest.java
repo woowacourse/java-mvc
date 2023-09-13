dd0bb047375de89e0d0cd47cbf82e3365a5aca84
greeng00se <thegreengoose7@gmail.com>
2023-09-14 02:09:38 +0900

    test: 테스트 빠진 부분 추가

---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecutionTest.java
@@ -0,0 +1,44 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import samples.TestController;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class HandlerExecutionTest {
+
+    @Test
+    void handle_메서드_실행() throws Exception {
+        // given
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/test/request");
+        when(request.getMethod()).thenReturn("GET");
+
+        final Method method = TestController.class.getDeclaredMethod(
+                "find",
+                HttpServletRequest.class,
+                HttpServletResponse.class
+        );
+
+        final HandlerExecution handlerExecution = new HandlerExecution(new TestController(), method);
+
+        // when
+        final ModelAndView modelAndView = handlerExecution.handle(request, response);
+
+        // then
+        assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+}
