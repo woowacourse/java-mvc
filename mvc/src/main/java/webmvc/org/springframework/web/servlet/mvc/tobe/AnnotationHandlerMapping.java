025481b58678ce9f9e8fd3b45d5ff86a1577ac5d
LJW25 <dbjwlee@gmail.com>
2023-09-13 23:52:21 +0900

    feat: MVC 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,61 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
-    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final Map<HandlerKey, HandlerExecution> handler;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackage = basePackage;
-        this.handlerExecutions = new HashMap<>();
+        this.handler = new HashMap<>();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+        controllerClasses.forEach(this::addMethodsOnHandler);
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void addMethodsOnHandler(final Class<?> controllerClass) {
+        final Method[] methods = controllerClass.getDeclaredMethods();
+
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> addMethodOnHandler(controllerClass, method));
+    }
+
+    private void addMethodOnHandler(final Class<?> controllerClass, final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String path = controllerClass.getAnnotation(Controller.class).path() + requestMapping.value();
+        final HandlerExecution handlerExecution = new HandlerExecution(controllerClass, method);
+
+        Arrays.stream(requestMapping.method())
+                .map(requestMethod -> new HandlerKey(path, requestMethod))
+                .forEach(handlerKey -> handler.put(handlerKey, handlerExecution));
+    }
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+
+        return handler.get(handlerKey);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,22 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Class<?> clazz;
+
+    private final Method method;
+
+    public HandlerExecution(final Class<?> clazz, final Method method) {
+        this.clazz = clazz;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        final Object obj = clazz.getConstructor().newInstance();
+        return (ModelAndView) method.invoke(obj, request, response);
     }
 }
