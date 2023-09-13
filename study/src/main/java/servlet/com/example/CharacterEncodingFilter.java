e533f0cefa502b0eac171f6129cc7ae6c34afbfd
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-13 17:42:58 +0900

    test: 서블릿 학습 테스트 구현

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -5,12 +5,16 @@ import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
 
+import static org.eclipse.jdt.internal.compiler.util.Util.UTF_8;
+
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding(UTF_8);
         chain.doFilter(request, response);
+        request.getServletContext().log("doFilter() 종료");
     }
 }
