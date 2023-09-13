6f08e66391d61c47433132746bb12a84666fee64
HubCreator <chocomilk8604@gmail.com>
2023-09-13 11:06:02 +0900

    feat: AnnotationHandlerMapping 초안

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +28,39 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize()  {
         log.info("Initialized AnnotationHandlerMapping!");
+        final Reflections reflections = new Reflections(basePackage);
+        registerFromBasePackage(reflections);
+    }
+
+    private void registerFromBasePackage(final Reflections reflections) {
+        final Set<Class<?>> clazzes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (final Class<?> clazz : clazzes) {
+            register(clazz);
+        }
+    }
+
+    private void register(final Class<?> clazz) {
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        final List<Method> methods = Arrays.stream(declaredMethods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+
+        for (final Method method : methods) {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            for (RequestMethod requestMethod : annotation.method()) {
+                final HandlerKey handlerKey = new HandlerKey(annotation.value(), requestMethod);
+                handlerExecutions.put(handlerKey, new HandlerExecution());
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey key = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+//        log.info("requestURI = {}", request.getRequestURI());
+//        log.info("method = {}", RequestMethod.valueOf(request.getMethod()));
+
+        return handlerExecutions.get(key);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -7,6 +7,8 @@ import webmvc.org.springframework.web.servlet.ModelAndView;
 public class HandlerExecution {
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        String id = (String) request.getAttribute("id");
+        final ModelAndView modelAndView = new ModelAndView(null);
+        return modelAndView.addObject("id", id);
     }
 }
