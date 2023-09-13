1ffea9c3fbaf0abdcccf97deda04b0d4d8a253b6
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-13 14:47:22 +0900

    feat: Annotation 기반 핸들러 매핑 구현

--- mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,15 @@
 package web.org.springframework.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
     GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    ;
+
+    public static RequestMethod from(final String request) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(method -> method.name().equalsIgnoreCase(request))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Not supported request method: " + request));
+    }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,24 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(getHandlerClass(), request, response);
+    }
+
+    private Object getHandlerClass()
+            throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        return method.getDeclaringClass().getDeclaredConstructor().newInstance();
     }
 }
