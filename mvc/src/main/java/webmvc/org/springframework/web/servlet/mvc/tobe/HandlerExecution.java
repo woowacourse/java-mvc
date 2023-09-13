69e7b244901b576d92d75bbbcb285dd875b0b9b3
jyeost <pig9619@naver.com>
2023-09-13 12:52:32 +0900

    feat: 1단계 테스트 통과 하도록 핸들러 매핑 클래스 작성

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method){
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(getHandlerInstance(), request, response);
+    }
+
+    private Object getHandlerInstance() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        return method.getDeclaringClass().getDeclaredConstructor().newInstance();
     }
 }
