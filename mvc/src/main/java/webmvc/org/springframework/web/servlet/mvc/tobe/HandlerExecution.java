2b99be4a09db890aff4ce41ed2c1e58d992e05d3
kokodak <kokodakadokok@gmail.co>
2023-09-13 22:31:02 +0900

    feat: HandlerExecution 에 대한 검증 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -3,6 +3,8 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
@@ -12,8 +14,26 @@ public class HandlerExecution {
 
     public HandlerExecution(final Object handler,
                             final Method handlerMethod) {
+        validateDeclare(handler, handlerMethod);
+        validateHandlerMethod(handlerMethod);
         this.handler = handler;
         this.handlerMethod = handlerMethod;
+    }
+
+    private void validateDeclare(final Object handler,
+                                 final Method handlerMethod) {
+        final Class<?> handlerClass = handler.getClass();
+        final Class<?> declaringClass = handlerMethod.getDeclaringClass();
+        if (!handlerClass.equals(declaringClass)) {
+            throw new IllegalArgumentException("Method not found in Handler");
+        }
+    }
+
+    private void validateHandlerMethod(final Method handlerMethod) {
+        final List<Class<?>> classes = Arrays.asList(handlerMethod.getParameterTypes());
+        if (!(classes.contains(HttpServletRequest.class) && classes.contains(HttpServletResponse.class))) {
+            throw new IllegalArgumentException("Invalid Parameters");
+        }
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
