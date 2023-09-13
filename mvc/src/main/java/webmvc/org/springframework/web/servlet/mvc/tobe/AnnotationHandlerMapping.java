02b07ffb16c9a4bfb756a6449fcf57fbd148c09f
Taeyeon <90550065+TaeyeonRoyce@users.noreply.github.com>
2023-09-13 17:43:24 +0900

    [MVC 미션 1단계] 로이스(원태연) 미션 제출합니다. (#363)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: Reflection 학습테스트
    
    * test: Servlet 학습 테스트 추가
    
    * feat: Annotation 기반 핸들러 매핑 구현
    
    * refactor: 메서드 분리
    
    * chore: sonar 설정 추가
    
    * test: 누락된 학습 테스트 추가
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,68 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        final Set<Class<?>> controllerClasses = new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+        for (final Class<?> controllerClass : controllerClasses) {
+            saveControllerAnnotatedClass(controllerClass);
+        }
+    }
+
+    private void saveControllerAnnotatedClass(final Class<?> controllerClass) {
+        final List<Method> methods = getDeclaredMethodsInControllerClass(controllerClass);
+        for (final Method method : methods) {
+            saveHandlerByDeclaredMethods(method);
+        }
+    }
+
+    private List<Method> getDeclaredMethodsInControllerClass(final Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private void saveHandlerByDeclaredMethods(final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String url = requestMapping.value();
+        final RequestMethod[] requestMethods = requestMapping.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution(method));
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        final String method = request.getMethod();
+        final String requestURI = request.getRequestURI();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.from(method));
+        return handlerExecutions.get(handlerKey);
+    }
+}
