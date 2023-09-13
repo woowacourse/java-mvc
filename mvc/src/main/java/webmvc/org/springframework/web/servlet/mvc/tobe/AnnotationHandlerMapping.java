b729ebd28f01ca86c21ea8d931c987e873b29da2
Donghun Shin <huipulci1@naver.com>
2023-09-13 19:47:32 +0900

    [MVC 구현하기 - 1단계] 말랑(신동훈) 미션 제출합니다.  (#371)
    * Reflection 학습 테스트 완료
    
    * 1차 미션 완료
    
    * Servlet 학습 테스트
    
    * Polishing

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,50 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static java.util.Arrays.stream;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        new Reflections(basePackage)
+                .getTypesAnnotatedWith(Controller.class)
+                .stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .flatMap(declaredMethod -> stream(declaredMethod.getDeclaredAnnotations())
+                        .filter(it -> it.annotationType().equals(RequestMapping.class))
+                        .map(RequestMapping.class::cast)
+                        .flatMap(declaredAnnotation -> stream(declaredAnnotation.method())
+                                .map(it -> Map.entry(
+                                        new HandlerKey(declaredAnnotation.value(), it),
+                                        new HandlerExecution(declaredMethod)))
+                        )
+                )
+                .forEach(it -> handlerExecutions.put(it.getKey(), it.getValue()));
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
+    }
+}
