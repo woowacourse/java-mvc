146561629157bc5c9f42b3458e6eecbff9c124db
HubCreator <chocomilk8604@gmail.com>
2023-09-13 10:21:50 +0900

    test: Reflections 테스트 작성

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -11,8 +14,14 @@ class ReflectionsTest {
 
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+        final Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(clazz -> log.info("controller = {}", clazz.getSimpleName()));
+        reflections.getTypesAnnotatedWith(Service.class)
+                .forEach(clazz -> log.info("controller = {}", clazz.getSimpleName()));
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .forEach(clazz -> log.info("controller = {}", clazz.getSimpleName()));
     }
 }
