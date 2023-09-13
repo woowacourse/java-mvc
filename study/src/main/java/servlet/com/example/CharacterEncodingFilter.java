fea74b3d93fef7e7d45f3caae7cce6f3d25e93a5
kdkdhoho <hkim4410@naver.com>
2023-09-13 18:58:41 +0900

    test: Servlet과 Servlet Container 테스트

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -1,8 +1,14 @@
 package servlet.com.example;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Locale.KOREA;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 @WebFilter("/*")
@@ -11,6 +17,10 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+
+        response.setCharacterEncoding(UTF_8.name());
+        response.setLocale(KOREA);
+
         chain.doFilter(request, response);
     }
 }
