e14158baf9c23a7c3b38ef4a884e588f1409c646
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-13 17:03:50 +0900

    feat: 어노테이션 기반의 MVC 프레임워크 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +29,55 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        controllers.stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Stream::of)
+                .filter(this::isRequestMappingAnnotation)
+                .forEach(this::addHandlerExecution);
     }
 
+    private boolean isRequestMappingAnnotation(Method method) {
+        return method.isAnnotationPresent(RequestMapping.class);
+    }
+
+    private void addHandlerExecution(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] httpMethods = requestMapping.method();
+        Object instance = getInstance(method.getDeclaringClass());
+
+        for (RequestMethod httpMethod : httpMethods) {
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), httpMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(method, instance);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private Object getInstance(Class<?> clazz) {
+        try {
+            Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+            return declaredConstructor.newInstance();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(String.format("해당 클래스(%s)에 기본 생성자가 없습니다 ", clazz.getName()), e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(String.format("해당 클래스(%s)의 인스턴스를 생성할 수 없습니다 ", clazz.getName()), e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(String.format("해당 클래스(%s)의 생성자에 접근할 수 없습니다 ", clazz.getName()), e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(String.format("해당 클래스(%s)의 생성자 호출 중 예외가 발생했습니다 ", clazz.getName()), e);
+        }
+    }
+
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(httpMethod));
+
+        return handlerExecutions.get(handlerKey);
     }
 }
