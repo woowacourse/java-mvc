e5d93ff85749ad7937cea7d29f48ea7c8ebd54d4
apptie <appti@naver.com>
2023-09-13 13:32:23 +0900

    refactor: JspView 생성자 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
@@ -2,30 +2,43 @@ package webmvc.org.springframework.web.servlet.view;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webmvc.org.springframework.web.servlet.View;
-
-import java.util.Map;
 
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
     public static final String REDIRECT_PREFIX = "redirect:";
+    private static final String JSP_SUFFIX = ".jsp";
+
+    private final String viewName;
 
     public JspView(final String viewName) {
+        if (viewName == null || viewName.isEmpty() || viewName.isBlank()) {
+            throw new IllegalArgumentException("이동할 페이지를 입력해주세요.");
+        }
+        if (!viewName.endsWith(JSP_SUFFIX)) {
+            throw new IllegalArgumentException("올바르지 않은 jsp 이름입니다.");
+        }
+
+        this.viewName = viewName.strip();
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+    public void render(
+            final Map<String, ?> model,
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
+        // TODO : 이후 미션 단계에서 구현 예정
 
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
     }
 }
---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/view/JspViewTest.java
@@ -0,0 +1,36 @@
+package webmvc.org.springframework.web.servlet.view;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class JspViewTest {
+
+    @Test
+    void 생성자는_유효한_값을_전달하면_JspView를_초기화한다() {
+        assertDoesNotThrow(() -> new JspView("/hello.jsp"));
+    }
+
+    @Test
+    void 생성자는_유효하지_않은_viewName을_전달하면_예외가_발생한다() {
+        final String invalidViewName = null;
+
+        assertThatThrownBy(() -> new JspView(invalidViewName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("이동할 페이지를 입력해주세요.");
+    }
+
+    @Test
+    void 생성자는_jsp_접미사가_없는_viewName을_전달하면_예외가_발생한다() {
+        final String notJspViewName = "hello.html";
+
+        assertThatThrownBy(() -> new JspView(notJspViewName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("올바르지 않은 jsp 이름입니다.");
+    }
+}
