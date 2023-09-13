80f13465f95a72ed4f004b10e5b4884d53d1729c
amaran-th <songsy405@naver.com>
2023-09-13 15:57:19 +0900

    feat: 학습 테스트 - 특정 단어로 시작하는 메서드 실행시키기

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+                .filter(method ->
+                        method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(new Junit3Test());
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
