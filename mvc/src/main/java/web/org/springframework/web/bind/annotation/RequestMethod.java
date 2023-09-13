815f2ddf5483f1317e2b7dc7f7f560a1ba031eb6
java-saeng <gusdn85554@naver.com>
2023-09-13 12:02:02 +0900

    feat : requestMethod 를 찾는 기능 구현

--- mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,22 @@
 package web.org.springframework.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+  GET,
+  HEAD,
+  POST,
+  PUT,
+  PATCH,
+  DELETE,
+  OPTIONS,
+  TRACE
+  ;
+
+  public static RequestMethod find(final String method) {
+    return Arrays.stream(values())
+        .filter(it -> it.name().equals(method.toUpperCase()))
+        .findAny()
+        .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 RequestMethod입니다."));
+  }
 }
