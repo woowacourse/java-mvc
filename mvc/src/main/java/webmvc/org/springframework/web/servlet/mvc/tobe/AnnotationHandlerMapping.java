4a0a522ab4843cea4bdf2fc58614f2c89d0fb773
aak2075 <lks971003@gmail.com>
2023-09-13 10:58:40 +0900

    feat: @MVC 프레임워크 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,15 +1,27 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.mvc.exception.CanNotInstanceHandlerException;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final Set<Class<?>> supportParameters = Set.of(HttpServletRequest.class,
+            HttpServletResponse.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -20,10 +32,53 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final var reflections = new Reflections(basePackage);
+        final var classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (final Class<?> clazz : classes) {
+            final var handler = getHandlerInstance(clazz);
+            final var methods = clazz.getMethods();
+
+            Arrays.stream(methods)
+                    .filter(this::supportParameters)
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> putHandlerExecutions(handler, method));
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private boolean supportParameters(final Method method) {
+        final var parameterTypes = Arrays.stream(method.getParameterTypes())
+                .collect(Collectors.toList());
+
+        return supportParameters.containsAll(parameterTypes) && parameterTypes.size() == 2;
+    }
+
+    private void putHandlerExecutions(final Object handler, final Method method) {
+        final var annotation = method.getDeclaredAnnotation(RequestMapping.class);
+        final var handlerExecution = new HandlerExecution(handler, method);
+
+        Arrays.stream(annotation.method())
+                .map(requestMethod -> new HandlerKey(annotation.value(), requestMethod))
+                .forEach(handlerKey -> handlerExecutions.put(handlerKey, handlerExecution));
+    }
+
+    private static Object getHandlerInstance(final Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (NoSuchMethodException |
+                IllegalAccessException |
+                InstantiationException |
+                 InvocationTargetException e
+        ) {
+            throw new CanNotInstanceHandlerException();
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
