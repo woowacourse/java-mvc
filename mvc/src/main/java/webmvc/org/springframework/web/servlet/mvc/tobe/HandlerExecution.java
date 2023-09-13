77cbbd74440aa8607b337e9a41e66ebb6ab433c7
dev_kong <ahdjd5@gmail.com>
2023-09-13 21:02:29 +0900

    feat: 어노테이션 기반의 MVC 프레임워크를 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,21 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import java.lang.reflect.Method;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
+    private final Method method;
+    private final Object controller;
+
+    public HandlerExecution(final Method method, final Object controller) {
+        this.method = method;
+        this.controller = controller;
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
