0da860cc93bb959c14cf44bd6595fcb49c94fc7f
Rosie <dev.kayoung@gmail.com>
2023-09-13 13:03:54 +0900

    refactor: HandlerExecution 필드 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -2,10 +2,10 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.reflections.Reflections;
@@ -16,66 +16,65 @@ import java.util.HashMap;
 import java.util.Map;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
-import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+        this.basePackages = basePackage;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
 
-        final var controllers = Arrays.stream(basePackage)
-                .flatMap(obj -> new Reflections(obj).getTypesAnnotatedWith(Controller.class).stream())
-                .map(this::getObject)
+        final var controllers = Arrays.stream(basePackages)
+                .flatMap(basePackage -> getControllerFrom(basePackage).stream())
+                .map(this::newInstance)
                 .collect(Collectors.toList());
-        for (Object controller: controllers) {
-            Map<HandlerKey, HandlerExecution> collect = Arrays.stream(
-                            controller.getClass().getDeclaredMethods())
-                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                    .flatMap(method -> getEntryStream(controller, method))
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-            handlerExecutions.putAll(collect);
+
+        for (Object controller : controllers) {
+            Map<HandlerKey, HandlerExecution> keyToExecutions = findHandlersFrom(controller);
+            handlerExecutions.putAll(keyToExecutions);
         }
     }
 
-    private Object getObject(final Class<?> clazz) {
+    private Set<Class<?>> getControllerFrom(final Object basePackage) {
+        return new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+    }
+
+    private Map<HandlerKey, HandlerExecution> findHandlersFrom(final Object controller) {
+        return Arrays.stream(
+                        controller.getClass().getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .flatMap(method -> getHandlerKeyToExecutions(controller, method))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Object newInstance(final Class<?> clazz) {
         try {
             return clazz.getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
     }
 
-    class OverridenHandlerExecution extends HandlerExecution {
-
-        private Object instance;
-        private Method handler;
-
-        public OverridenHandlerExecution(Object instance, Method handler) {
-            this.instance = instance;
-            this.handler = handler;
-        }
-
-        @Override
-        public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-            return (ModelAndView) handler.invoke(instance, request, response);
-        }
-    }
-
-    private Stream<Map.Entry<HandlerKey, HandlerExecution>> getEntryStream(final Object controller, final Method method) { // request mapping method
-        final var requestMapping = method.getAnnotation(RequestMapping.class);
-        RequestMethod[] methods = requestMapping.method();
-        return Stream.of(methods)
-                .map(m -> Map.entry(new HandlerKey(requestMapping.value(), m), new OverridenHandlerExecution(controller, method)));
+    private Stream<Map.Entry<HandlerKey, HandlerExecution>> getHandlerKeyToExecutions(final Object controller,
+                                                                                      final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final RequestMethod[] httpMethods = requestMapping.method();
+        return Stream.of(httpMethods)
+                .map(httpMethod -> {
+                    HandlerKey key = new HandlerKey(requestMapping.value(), httpMethod);
+                    HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+                    return Map.entry(key, handlerExecution);
+                });
     }
 
     public Object getHandler(final HttpServletRequest request) {
@@ -84,4 +83,5 @@ public class AnnotationHandlerMapping {
         final var handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
         return handlerExecutions.get(handlerKey);
     }
+
 }
