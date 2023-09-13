3c6a6f3479dacc64fca1a634bbd7cfd9b3c8cbef
greeng00se <thegreengoose7@gmail.com>
2023-09-13 14:04:25 +0900

    feat: Controller 애너테이션이 적용된 클래스를 반환하는 ControllerScanner 클래스 추가

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ### 1 단계 - @MVC 프레임워크 구현하기
 
-[x] AnnotationHandlerMappingTest 클래스의 실패하는 테스트를 통과시키기
+- [x] AnnotationHandlerMappingTest 클래스의 실패하는 테스트를 통과시키기
+- [x] Controller 애너테이션이 적용된 클래스를 전부 반환하는 기능 추가
 
 ### 2단계 - 점진적인 리팩터링
 
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,39 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+import context.org.springframework.stereotype.Controller;
+import java.lang.reflect.Constructor;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ControllerScanner {
+
+    private static final Logger log = LoggerFactory.getLogger(ControllerScanner.class);
+
+    private final Reflections reflections;
+
+    public ControllerScanner(final Object... basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        final Set<Class<?>> types = reflections.getTypesAnnotatedWith(Controller.class);
+        return types.stream()
+                .collect(toMap(identity(), this::instantiate));
+    }
+
+    private Object instantiate(final Class<?> type) {
+        try {
+            final Constructor<?> declaredConstructor = type.getDeclaredConstructor();
+            return declaredConstructor.newInstance();
+        } catch (final Exception e) {
+            log.error("Instantiate Failed!", e);
+            throw new IllegalArgumentException("Instantiate Failed");
+        }
+    }
+}
