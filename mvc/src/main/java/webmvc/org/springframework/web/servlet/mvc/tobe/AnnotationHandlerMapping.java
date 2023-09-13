92865fe7c8f84c2035ad42afc80dae20a40c5ee0
apptie <appti@naver.com>
2023-09-13 17:22:55 +0900

    feat: AnnotationHandlerMapping 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +26,54 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (final Class<?> controller : controllers) {
+            processRequestMappingMethod(controller);
+        }
+    }
+
+    private void processRequestMappingMethod(final Class<?> controller) {
+        final Method[] methods = controller.getDeclaredMethods();
+
+        for (final Method method : methods) {
+            processHandlerExecutors(controller, method);
+        }
+    }
+
+    private void processHandlerExecutors(final Class<?> controller, final Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method());
+            final HandlerExecution handlerExecution = calculateHandlerExecution(controller, method);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private HandlerExecution calculateHandlerExecution(final Class<?> controller, final Method method) {
+        try {
+            final Object handler = controller.getConstructor()
+                                             .newInstance();
+
+            return new HandlerExecution(handler, method);
+        } catch (final NoSuchMethodException |
+                InvocationTargetException |
+                InstantiationException |
+                IllegalAccessException e)
+        {
+            log.error("", e);
+        }
+
+        throw new IllegalArgumentException("해당 Handler의 Mapping 정보를 처리할 수 없습니다.");
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String url = request.getRequestURI();
+        final RequestMethod method = RequestMethod.from(request.getMethod());
+        final HandlerKey handlerKey = new HandlerKey(url, method);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
