2025c6cb289d9c5bc7c15a3752a6b7ed47a67547
ReO <89302528+Jaeyoung22@users.noreply.github.com>
2023-09-13 22:20:11 +0900

    [Spring MVC 구현] 리오(오영택) 1단계 미션 제출합니다. (#370)
    * test: 리플렉션 학습 테스트 작성
    
    * test: 서블릿 학습 테스트 작성
    
    * test: 필터 학습 테스트 작성
    
    * chore: 소나 리포트 설정 추가
    
    * feat: 어노테이션 기반으로 handler를 찾는 기능 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,6 +1,9 @@
 package reflection;
 
+import javassist.tools.reflect.Reflection;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
 
 class Junit3TestRunner {
 
@@ -9,5 +12,13 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
