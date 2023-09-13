8b0f7c8d88096a25c7bec6900e190a39cbe2de40
bjk1649 <qhwns1649@naver.com>
2023-09-13 14:34:48 +0900

    feat: 어노테이션 기반으로 HandlerMapping을 초기화해주는 기능 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +30,49 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        final var reflections = new Reflections(basePackage);
+        final var methodsPerClass = groupingMethodsByClass(reflections);
+
+        for (var classAndMethods : methodsPerClass.entrySet()) {
+            final var clazz = classAndMethods.getKey();
+            final var methods = classAndMethods.getValue();
+            try {
+                initializeEachClassExecutions(clazz, methods);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
+    private Map<? extends Class<?>, List<Method>> groupingMethodsByClass(final Reflections reflections) {
+        return reflections.getTypesAnnotatedWith(Controller.class).stream()
+                          .map(Class::getDeclaredMethods)
+                          .flatMap(Stream::of)
+                          .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                          .collect(Collectors.groupingBy(Method::getDeclaringClass));
+    }
+
+    private void initializeEachClassExecutions(
+            final Class<?> clazz,
+            final List<Method> methods
+    ) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        final var parentPath = clazz.getAnnotation(Controller.class).path();
+        final var target = clazz.getConstructor().newInstance();
+        for (var method : methods) {
+            initializeEachMethodExecution(method, parentPath, target);
+        }
+    }
+
+    private void initializeEachMethodExecution(final Method method, final String parentPath, final Object target) {
+        final var requestMapping = method.getAnnotation(RequestMapping.class);
+        final var path = parentPath + requestMapping.value();
+        Arrays.stream(requestMapping.method())
+              .map(requestMethod -> new HandlerKey(path, requestMethod))
+              .forEach(key -> handlerExecutions.put(key, new HandlerExecution(target, method)));
+    }
+
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod().toUpperCase())));
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -15,7 +15,7 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("reflection.examples");
 
         // 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어있는 모든 클래스 찾아 로그로 출력한다.
