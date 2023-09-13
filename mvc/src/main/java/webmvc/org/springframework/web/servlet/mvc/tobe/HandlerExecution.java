f2505f0d2d91a51fce9fa8ff0fb852cdef98b4c3
kokodak <kokodakadokok@gmail.co>
2023-09-13 22:00:12 +0900

    feat: 리플렉션을 활용하여 handle 메서드 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,21 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Object handler;
+    private final Method handlerMethod;
+
+    public HandlerExecution(final Object handler,
+                            final Method handlerMethod) {
+        this.handler = handler;
+        this.handlerMethod = handlerMethod;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handlerMethod.invoke(handler, request, response);
     }
 }
