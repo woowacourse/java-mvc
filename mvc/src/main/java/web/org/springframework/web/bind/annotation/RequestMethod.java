a2670933c637b83950bd6162a39688ce5d90f16d
apptie <appti@naver.com>
2023-09-13 16:15:13 +0900

    feat: 메서드 이름으로 RequestMethod를 찾는 기능 추가

--- mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,21 @@
 package web.org.springframework.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE;
+
+    public static RequestMethod from(final String methodName) {
+        return Arrays.stream(RequestMethod.values())
+                     .filter(requestMethod -> requestMethod.name().equalsIgnoreCase(methodName))
+                     .findAny()
+                     .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 HTTP Method 입니다."));
+    }
 }
