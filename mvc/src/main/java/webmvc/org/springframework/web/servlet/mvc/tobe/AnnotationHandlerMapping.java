dcd843a503654753c9f3b0360128f276b29d84b7
amaran-th <songsy405@naver.com>
2023-09-14 00:02:01 +0900

    feat: HandlerMapping 기능 구현
    - 어노테이션에 입력한 요청 메서드와 경로 정보에 따라 핸들러를 매핑하여 실행시키는 기능 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,9 +1,16 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,17 +20,55 @@ public class AnnotationHandlerMapping {
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private HandlerExecution defaultHandlerExecution;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize() throws NoSuchMethodException {
+        final Reflections reflections = new Reflections(basePackage);
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(this::putHandlerMethodInController);
+        final Method defaultHandler = this.getClass()
+                .getDeclaredMethod(
+                        "defaultHandler",
+                        HttpServletRequest.class,
+                        HttpServletResponse.class
+                );
+        defaultHandlerExecution = new HandlerExecution(this.getClass(), defaultHandler);
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void putHandlerMethodInController(final Class<?> clazz) {
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> putHandlerMethodPerRequestMethod(clazz, method));
+    }
+
+    private void putHandlerMethodPerRequestMethod(final Class<?> controller, final Method handler) {
+        final RequestMapping annotation = handler.getAnnotation(RequestMapping.class);
+        final RequestMethod[] requestMethods = annotation.method();
+        Arrays.stream(requestMethods)
+                .forEach(requestMethod ->
+                        handlerExecutions.put(
+                                new HandlerKey(annotation.value(), requestMethod),
+                                new HandlerExecution(controller, handler)
+                        )
+                );
+    }
+
     public Object getHandler(final HttpServletRequest request) {
+        final RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        final String requestURI = request.getRequestURI();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.getOrDefault(handlerKey, defaultHandlerExecution);
+    }
+
+    private Object defaultHandler(final HttpServletRequest request, final HttpServletResponse response) {
+        log.info("요청하신 HTTP 메서드와 경로에 매핑되는 핸들러가 존재하지 않습니다.");
         return null;
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Class<?> controller;
+    private final Method handler;
+
+    public HandlerExecution(final Class<?> controller, final Method handler) {
+        this.controller = controller;
+        this.handler = handler;
+    }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response)
+            throws InstantiationException, IllegalAccessException, InvocationTargetException {
+        return (ModelAndView) handler.invoke(controller.newInstance(), request, response);
     }
 }
