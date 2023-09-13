5a8cf955391bd74588f685a1064394882f4e5874
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-09-13 13:28:07 +0900

    feat: 어노테이션 기반의 MVC 프레임워크를 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.HashMap;
-import java.util.Map;
+import static java.util.stream.Collectors.toList;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +29,52 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        controllers.forEach(controller -> {
+            Object handler = createHandlerByConstructor(controller);
+            Arrays.stream(controller.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> parseMethod(handler, method));
+        });
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void parseMethod(Object handler, Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        System.out.println("annotation.value() = " + annotation.value());
+        System.out.println("annotation.method() = " + annotation.method());
+        RequestMethod[] httpMethods = annotation.method();
+        String path = annotation.value();
+        addHandlerExecution(handler, method, httpMethods, path);
     }
+
+    private void addHandlerExecution(Object handler, Method method, RequestMethod[] httpMethods, String path) {
+        List<HandlerKey> handlerKeys = Arrays.stream(httpMethods)
+                .map(httpMethod -> new HandlerKey(path, httpMethod))
+                .collect(toList());
+        HandlerExecution handlerExecution = new HandlerExecution(handler, method);
+        for (HandlerKey handlerKey : handlerKeys) {
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private static Object createHandlerByConstructor(Class<?> controller) {
+        try {
+            Object handler = controller.getConstructor().newInstance();
+            return handler;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "해당 컨트롤러의 생성자로 객체를 생성할 수 없습니다." + controller.getSimpleName() + "의 생성자를 다시 확인해주세요.");
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
+    }
+
 }
