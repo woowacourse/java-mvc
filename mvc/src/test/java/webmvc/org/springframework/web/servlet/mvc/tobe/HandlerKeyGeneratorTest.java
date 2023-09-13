045e557489d7f409de57376a2b797f6678657588
greeng00se <thegreengoose7@gmail.com>
2023-09-14 01:54:54 +0900

    feat: HandlerKey를 생성하는 클래스 추가

---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerKeyGeneratorTest.java
@@ -0,0 +1,36 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import samples.TestController;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class HandlerKeyGeneratorTest {
+
+    private final HandlerKeyGenerator handlerKeyGenerator = new HandlerKeyGenerator(new HttpMappingExtractor());
+
+    @Test
+    void prefix와_메서드를_입력받아_HandlerKey의_리스트를_반환한다() throws NoSuchMethodException {
+        // given
+        final Method method = TestController.class.getDeclaredMethod(
+                "find",
+                HttpServletRequest.class,
+                HttpServletResponse.class
+        );
+
+        // when
+        final List<HandlerKey> handlerKeys = handlerKeyGenerator.generate("/hello", method);
+
+        // then
+        assertThat(handlerKeys).containsExactly(new HandlerKey("/hello/get", RequestMethod.GET));
+    }
+}
