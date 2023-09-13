862d97cacc3238be634f39a8dca2d011f532f57c
kwonyj1022 <kwonyj1022@naver.com>
2023-09-14 08:34:20 +0900

    feat: HandlerExecution의 handle()에 예외 케이스 처리 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -3,7 +3,9 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.tobe.exception.InvalidClassException;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
@@ -16,8 +18,16 @@ public class HandlerExecution {
         this.method = method;
     }
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        final Object realObject = clazz.getDeclaredConstructor().newInstance();
-        return (ModelAndView) method.invoke(realObject, request, response);
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) {
+        final Object realObject;
+        try {
+            realObject = clazz.getDeclaredConstructor().newInstance();
+            return (ModelAndView) method.invoke(realObject, request, response);
+        } catch (InstantiationException |
+                 IllegalAccessException |
+                 InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new InvalidClassException("클래스의 인스턴스를 생성할 수 없습니다.");
+        }
     }
 }
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/exception/InvalidClassException.java
@@ -0,0 +1,8 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe.exception;
+
+public class InvalidClassException extends IllegalArgumentException {
+
+    public InvalidClassException(final String message) {
+        super(message);
+    }
+}
