411a22df03c27ff10eb2d390e378a0b9442d8dc5
ReO <yt930310@gmail.com>
2023-09-13 17:23:38 +0900

    feat: 어노테이션 기반으로 handler를 찾는 기능 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +29,52 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        try {
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void init() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerReflections = reflections.getTypesAnnotatedWith(context.org.springframework.stereotype.Controller.class);
+        for (Class<?> controllerReflection : controllerReflections) {
+            addHandlerWithTargetMethods(controllerReflection);
+        }
+    }
+
+    private void addHandlerWithTargetMethods(Class<?> controllerReflection) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        List<Method> methods = Arrays.stream(controllerReflection.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String value = requestMapping.value();
+            addKeyAndHandler(controllerReflection, requestMapping, value, method);
+        }
+    }
+
+    private void addKeyAndHandler(Class<?> controllerReflection, RequestMapping requestMapping, String value, Method method) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        RequestMethod[] requestMethods = requestMapping.method();
+        Object controller = controllerReflection.getDeclaredConstructor().newInstance();
+
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+            handlerExecutions.put(new HandlerKey(value, requestMethod), handlerExecution);
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        HandlerKey foundKey = handlerExecutions.keySet().stream()
+                .filter(key -> key.equals(handlerKey))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("요청에 해당하는 메서드가 없습니다."));
+
+        return handlerExecutions.get(foundKey);
     }
 }
