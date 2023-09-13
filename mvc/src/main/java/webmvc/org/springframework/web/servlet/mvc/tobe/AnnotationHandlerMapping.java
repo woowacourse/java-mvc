4dc9beb0669fcd3b33c51ec359ccf29583b34556
woosung1223 <dntjd991223@naver.com>
2023-09-13 13:45:54 +0900

    feat: 리플렉션을 통한 핸들러 매핑 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
+import core.org.springframework.util.ReflectionUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +29,48 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (Class<?> controller : controllers) {
+            try {
+                Constructor<?> constructor = ReflectionUtils.accessibleConstructor(controller);
+                ReflectionUtils.makeAccessible(constructor);
+                Object instance = constructor.newInstance(null);
+
+                Method[] methods = instance.getClass()
+                        .getDeclaredMethods();
+
+                for (Method method : methods) {
+
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+
+                        RequestMethod[] requestMethods = requestMapping.method();
+                        String requestURL = requestMapping.value();
+
+                        for (RequestMethod requestMethod : requestMethods) {
+                            HandlerKey handlerKey = new HandlerKey(requestURL, requestMethod);
+                            HandlerExecution handlerExecution = new HandlerExecution(instance);
+
+                            handlerExecutions.put(handlerKey, handlerExecution);
+                        }
+                    }
+                }
+
+            } catch (NoSuchMethodException | InstantiationException |
+                     IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,45 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import core.org.springframework.util.ReflectionUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Object instance;
+
+    public HandlerExecution(Object instance) {
+        this.instance = instance;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        Class<?> clazz = instance.getClass();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        String requestURI1 = request.getRequestURI();
+        String requestMethod = request.getMethod();
+
+        for (Method method : methods) {
+            RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+
+            String requestURI = requestMapping.value();
+            RequestMethod[] requestMethods = requestMapping.method();
+
+            for (RequestMethod each : requestMethods) {
+                if (requestURI1.equals(requestURI) && each.equals(RequestMethod.valueOf(requestMethod))) {
+                    Constructor<?> constructor = ReflectionUtils.accessibleConstructor(clazz);
+                    ReflectionUtils.makeAccessible(constructor);
+                    Object rawInstance = constructor.newInstance(null);
+
+                    return (ModelAndView) method.invoke(rawInstance, request, response);
+                }
+            }
+        }
+        throw new IllegalArgumentException();
     }
 }
