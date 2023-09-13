9bd1dd10ef84323ae98a763f4e59d1dca2b5c500
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-13 16:12:13 +0900

    feat: Annotation 기반 프레임워크를 지원하도록 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,92 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final List<Object> basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = List.of(basePackages);
         this.handlerExecutions = new HashMap<>();
+        makeHandlerExecutions(basePackages);
+    }
+
+    private void makeHandlerExecutions(final Object[] basePackages) {
+        for (final Object basePackage : basePackages) {
+            final Reflections reflections = new Reflections(basePackage);
+            final var classes = reflections.getTypesAnnotatedWith(Controller.class);
+            putHandlerExecutionsByClasses(classes);
+        }
+    }
+
+    private void putHandlerExecutionsByClasses(final Set<Class<?>> classes) {
+        for (final Class<?> clazz : classes) {
+            final Constructor<?> constructor = getConstructor(clazz);
+            final List<Method> methods = getMethodsWithAnnotation(clazz);
+            putHandlerExecutionsByMethods(constructor, methods);
+        }
+    }
+
+    private static Constructor<?> getConstructor(final Class<?> clazz) {
+        try {
+            return clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("기본 생성자가 존재하지 않습니다.");
+        }
+    }
+
+    private List<Method> getMethodsWithAnnotation(final Class<?> clazz) {
+        return Arrays.stream(clazz.getMethods())
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .collect(Collectors.toList());
+    }
+
+    private void putHandlerExecutionsByMethods(final Constructor<?> constructor, final List<Method> methods) {
+        for (final Method method : methods) {
+            putHandlerExecutionsByMethod(constructor, method);
+        }
+    }
+
+    private void putHandlerExecutionsByMethod(final Constructor<?> constructor, final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String requestURI = requestMapping.value();
+        final List<RequestMethod> requestMethods = List.of(requestMapping.method());
+        final Map<HandlerKey, HandlerExecution> handlerExecutionsByMethod = convertHandlerExecutions(constructor,
+            method,
+            requestMethods, requestURI);
+        handlerExecutions.putAll(handlerExecutionsByMethod);
+    }
+
+    private Map<HandlerKey, HandlerExecution> convertHandlerExecutions(
+        final Constructor<?> constructor,
+        final Method method,
+        final List<RequestMethod> requestMethods,
+        final String requestURI
+    ) {
+        return requestMethods.stream()
+            .map(requestMethod -> new HandlerKey(requestURI, requestMethod))
+            .collect(Collectors.toMap(
+                handlerKey -> handlerKey,
+                handlerKey -> new HandlerExecution(constructor, method)
+            ));
     }
 
     public void initialize() {
@@ -24,6 +94,14 @@ public class AnnotationHandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = makeHandlerKey(request);
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private HandlerKey makeHandlerKey(final HttpServletRequest request) {
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final RequestMethod requestMethod = RequestMethod.valueOf(method);
+        return new HandlerKey(requestURI, requestMethod);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,21 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Constructor<?> constructor;
+    private final Method method;
+
+    public HandlerExecution(final Constructor<?> constructor, final Method method) {
+        this.constructor = constructor;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(constructor.newInstance(), request, response);
     }
 }
