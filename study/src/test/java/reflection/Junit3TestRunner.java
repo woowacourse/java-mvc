b19151172bfe14ec251ddd6570c574700288236b
Miseong Kim <miseongk@naver.com>
2023-09-14 03:36:39 +0900

    test: Reflection 학습 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +8,15 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            final String methodName = declaredMethod.getName();
+            if (methodName.startsWith("test")) {
+                declaredMethod.invoke(junit3Test);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,13 @@
 package reflection;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +18,19 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> classes1 = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : classes1) {
+            log.info("Controller 어노테이션 - {}", clazz.getTypeName());
+        }
+
+        final Set<Class<?>> classes2 = reflections.getTypesAnnotatedWith(Service.class);
+        for (Class<?> clazz : classes2) {
+            log.info("Service 어노테이션 - {}", clazz.getTypeName());
+        }
+
+        final Set<Class<?>> classes3 = reflections.getTypesAnnotatedWith(Repository.class);
+        for (Class<?> clazz : classes3) {
+            log.info("Repository 어노테이션 - {}", clazz.getTypeName());
+        }
     }
 }
