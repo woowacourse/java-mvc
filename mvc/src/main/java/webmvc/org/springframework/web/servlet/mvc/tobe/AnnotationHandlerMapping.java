06f8f7c78f921f1d650c1797d653e5b8693f0adf
wugawuga <dnr3655@naver.com>
2023-09-14 01:40:41 +0900

    feat: AnnotationHandlerMapping 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +30,56 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (Class<?> controllerClass : controllerClasses) {
+            final List<Method> declaredMethods = getDeclaredRequestMapping(controllerClass);
+            addHandlerExecution(controllerClass, declaredMethods);
+        }
+    }
+
+    private List<Method> getDeclaredRequestMapping(final Class<?> controllerClass) {
+        final Method[] methods = controllerClass.getMethods();
+        return Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private void addHandlerExecution(final Class<?> controllerClass, final List<Method> declaredMethods) {
+        final String rootPath = getRootPath(controllerClass);
+        for (Method declaredMethod : declaredMethods) {
+            final List<HandlerKey> handlerKeys = getHandlerKeys(rootPath, declaredMethod);
+            final HandlerExecution handlerExecution = getHandlerExecution(controllerClass, declaredMethod);
+            handlerKeys.forEach(key -> handlerExecutions.put(key, handlerExecution));
+        }
+    }
+
+    private HandlerExecution getHandlerExecution(final Class<?> controllerClass, final Method declaredMethod) {
+        try {
+            return HandlerExecution.of(controllerClass, declaredMethod);
+        } catch (Exception e) {
+            throw new NoSuchElementException("Class 를 확인해주세요.");
+        }
+    }
+
+    private String getRootPath(final Class<?> controllerClass) {
+        return controllerClass.getAnnotation(Controller.class).path();
+    }
+
+    private List<HandlerKey> getHandlerKeys(final String rootPath, final Method declaredMethod) {
+        final RequestMapping requestMappingAnnotation = declaredMethod.getAnnotation(RequestMapping.class);
+
+        return Arrays.stream(requestMappingAnnotation.method())
+                .map(requestMethod -> new HandlerKey(rootPath + requestMappingAnnotation.value(), requestMethod))
+                .collect(Collectors.toList());
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKeyByRequest = new HandlerKey(request.getRequestURI(),
+                RequestMethod.valueOf(request.getMethod()));
+
+        return handlerExecutions.get(handlerKeyByRequest);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,24 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Object executor;
+    private final Method handlerMethod;
+
+    private HandlerExecution(final Object executor, final Method handlerMethod) {
+        this.executor = executor;
+        this.handlerMethod = handlerMethod;
+    }
+
+    public static HandlerExecution of(final Class<?> executor, final Method handlerMethod) throws Exception {
+        return new HandlerExecution(executor.getConstructor().newInstance(), handlerMethod);
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handlerMethod.invoke(executor, request, response);
     }
 }
