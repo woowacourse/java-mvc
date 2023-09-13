38ad9521dab7c982f8a1b2e635c6d71477a94fdf
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-13 17:30:53 +0900

    feat: reflection 학습 테스트 추가

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,6 +2,11 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 class Junit4TestRunner {
 
     @Test
@@ -9,5 +14,20 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            final int modifiers = method.getModifiers();
+            final Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
+
+            if (Modifier.isPublic(modifiers) && isContainMyTest(declaredAnnotations)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
+    }
+
+    private static boolean isContainMyTest(final Annotation[] declaredAnnotations) {
+        return Arrays.stream(declaredAnnotations)
+                     .anyMatch(annotation -> annotation instanceof MyTest);
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import java.util.Set;
 
 class ReflectionsTest {
 
@@ -14,5 +19,20 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.getTypesAnnotatedWith(Controller.class)
+                   .forEach(clazz -> {
+                       final String name = clazz.getSimpleName();
+                       log.info("Controller 클래스명 : {}", name);
+                   });
+        reflections.getTypesAnnotatedWith(Service.class)
+                   .forEach(clazz -> {
+                       final String name = clazz.getSimpleName();
+                       log.info("Service 클래스명 : {}", name);
+                   });
+        reflections.getTypesAnnotatedWith(Repository.class)
+                   .forEach(clazz -> {
+                       final String name = clazz.getSimpleName();
+                       log.info("Repository 클래스명 : {}", name);
+                   });
     }
 }
