f218719decbec56a6a919e259b342756f5e66edd
kim dae hee <akdlzmf1123@gmail.com>
2023-09-13 15:26:47 +0900

    test: 학습 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,25 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getMethods())
+              .map(Method::getName)
+              .filter(name -> name.startsWith("test"))
+              .forEach(name -> {
+                  try {
+                      clazz.getMethod(name).invoke(clazz.getDeclaredConstructor().newInstance());
+                  } catch (Exception e) {
+                      e.printStackTrace();
+                  }
+              });
     }
 }
