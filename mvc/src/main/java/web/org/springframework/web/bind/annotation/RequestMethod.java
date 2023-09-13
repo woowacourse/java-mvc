b6ae5aeadbe35e6fe1378006c9a95c44a34335f5
70825 <hello70825@gmail.com>
2023-09-13 22:55:45 +0900

    feat: 요청 메서드 종류를 식별하는 기능 추가

--- mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/web/org/springframework/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,23 @@
 package web.org.springframework.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    ;
+
+    public static RequestMethod find(final String methodName) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(method -> method.name().equalsIgnoreCase(methodName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 HTTP 요청 메서드입니다"));
+    }
 }
