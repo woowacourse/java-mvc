7f2f14132f6e223c00d99078f8623b2380eeca81
donghae-kim <donghae1999@gmail.com>
2023-09-13 10:24:20 +0900

    test: Reflection 학습 테스트 추가

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,9 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +12,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        final Junit3Test junit3Test = clazz.newInstance();
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        for (Method m:declaredMethods) {
+            if(m.getName().startsWith("test")){
+                m.invoke(junit3Test);
+            }
+        }
     }
 }
