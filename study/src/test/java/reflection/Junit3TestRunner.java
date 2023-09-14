95a4075764f3543269b3e8b94fc1bb24dcca1ac4
hgo641 <hgo641@gmail.com>
2023-09-14 16:58:29 +0900

    test: Reflection 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +11,15 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    } catch (final IllegalAccessException | InvocationTargetException | InstantiationException |
+                                   NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
