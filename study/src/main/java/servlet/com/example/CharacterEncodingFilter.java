fd5520da7b4d47dd5e34558e9c2f783e24592974
hgo641 <hgo641@gmail.com>
2023-09-14 16:59:17 +0900

    test: Servlet, Filter 테스트 작성

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -8,9 +8,13 @@ import java.io.IOException;
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
+    public static final String UTF_8 = "UTF-8";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        request.setCharacterEncoding(UTF_8);
+        response.setCharacterEncoding(UTF_8);
         chain.doFilter(request, response);
     }
 }
