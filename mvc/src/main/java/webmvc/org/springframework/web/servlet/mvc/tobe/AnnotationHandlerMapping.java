1a262a8df9583d8003a0aa837ef32af7f277ca71
ezzanzzan <cjsdmswjd821@naver.com>
2023-09-14 16:14:35 +0900

    refactor: 예외 처리 세분화

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -53,8 +54,15 @@ public class AnnotationHandlerMapping {
         try {
             final Object controllerInstance = controller.getConstructor().newInstance();
             handlerExecutions.put(new HandlerKey(value, requestMethod), new HandlerExecution(controllerInstance, method));
-        } catch (Exception e) {
-            log.error("{} 클래스를 생성할 수 없습니다.", controller.getSimpleName());
+        } catch (InstantiationException e) {
+            log.error("{} 클래스는 추상클래스이거나 인터페이스입니다.", controller.getSimpleName());
+        } catch (IllegalAccessException e) {
+            log.error("{} 클래스의 생성자에 접근할 수 없습니다.", controller.getSimpleName());
+        } catch (InvocationTargetException e) {
+            log.error("{} 클래스를 생성할 때 예외가 발생하였습니다.", controller.getSimpleName());
+            log.error("TargetException: {}", e.getTargetException().getMessage());
+        } catch (NoSuchMethodException e) {
+            log.error("{} 클래스의 기본 생성자를 찾을 수 없습니다.", controller.getSimpleName());
         }
     }
 
