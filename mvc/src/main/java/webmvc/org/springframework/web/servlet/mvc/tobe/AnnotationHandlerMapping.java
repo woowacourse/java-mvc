574c0887a35da7fe796c56e5bfd5a5e475bc99bf
제이미 <63184334+JJ503@users.noreply.github.com>
2023-09-14 16:18:11 +0900

    [MVC 구현하기 - 1단계] 제이미(임정수) 미션 제출합니다. (#411)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: reflection 학습 테스트 추가
    
    * feat: servlet 학습 테스트 추가
    
    * feat: 어노테이션을 통한 요청 매핑 기능 추가
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,69 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        controllerClasses.forEach(this::putHandlerExecutions);
+    }
+
+    private void putHandlerExecutions(final Class<?> clazz) {
+        final List<Method> methods = getMethods(clazz);
+
+        for (final Method method : methods) {
+            final List<HandlerKey> handlerKeys = calculateHandlerKeys(method);
+            handlerKeys.forEach(handlerKey -> handlerExecutions.put(handlerKey, new HandlerExecution(clazz, method)));
+        }
+    }
+
+    private static List<Method> getMethods(final Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getDeclaredMethods())
+                     .filter(clazz -> clazz.isAnnotationPresent(RequestMapping.class))
+                     .collect(Collectors.toList());
+    }
+
+    private List<HandlerKey> calculateHandlerKeys(final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String value = requestMapping.value();
+        return Arrays.stream(requestMapping.method())
+                     .map(requestMethod -> new HandlerKey(value, requestMethod))
+                     .collect(Collectors.toList());
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+
+        return handlerExecutions.get(handlerKey);
+    }
+}
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,9 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +12,18 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            final int modifiers = method.getModifiers();
+
+            if (isStartsWithTest(method) && Modifier.isPublic(modifiers)) {
+                method.invoke(clazz.newInstance());
+            }
+        }
+    }
+
+    private static boolean isStartsWithTest(final Method method) {
+        return method.getName().startsWith("test");
     }
 }
