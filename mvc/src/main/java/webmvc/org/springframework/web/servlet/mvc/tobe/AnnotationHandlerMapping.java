95d771086bf2040376d2786a620e83ff67184fc1
준팍(junpak) <112045553+junpakPark@users.noreply.github.com>
2023-09-14 10:30:01 +0900

    [MVC 구현하기 - 1단계] 준팍(박준현) 미션 제출합니다. (#369)
    * test: reflection 학습 테스트 완료
    
    * test: Servlet 학습 테스트 완료
    
    * chore: 소나 클라우드 설정 수정
    
    * feat: 어노테이션 기반의 MVC 프레임워크 구현
    
    * feat: 커스텀 예외 구현
    
    * style: 코드 포매팅
    
    * refactor: 메서드 분리 및 외부 변수 변경 forEach 제거
    
    * refactor: createHandlerEntry 메서드 inline
    
    * refactor: 메서드 네이밍 수정
    
    * style: 불필요 공백 제거

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,24 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.mvc.exception.ClassException;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +34,58 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        handlerExecutions.putAll(addHandlerExecution(controllers));
     }
 
+    private Map<HandlerKey, HandlerExecution> addHandlerExecution(Set<Class<?>> controllers) {
+        return controllers.stream().map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .filter(this::isRequestMappingAnnotation)
+                .flatMap(this::createHandlerExecution)
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+    }
+
+    private boolean isRequestMappingAnnotation(Method method) {
+        return method.isAnnotationPresent(RequestMapping.class);
+    }
+
+    private Stream<Entry<HandlerKey, HandlerExecution>> createHandlerExecution(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] httpMethods = requestMapping.method();
+        Object instance = getInstance(method.getDeclaringClass());
+
+        return Arrays.stream(httpMethods)
+                .map(httpMethod -> new SimpleEntry<>(
+                        new HandlerKey(requestMapping.value(), httpMethod),
+                        new HandlerExecution(method, instance))
+                );
+    }
+
+    private Object getInstance(Class<?> clazz) {
+        try {
+            Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+            return declaredConstructor.newInstance();
+        } catch (NoSuchMethodException e) {
+            throw new ClassException(String.format("해당 클래스(%s)에 기본 생성자가 없습니다 ", clazz.getName()), e);
+        } catch (InstantiationException e) {
+            throw new ClassException(String.format("해당 클래스(%s)의 인스턴스를 생성할 수 없습니다 ", clazz.getName()), e);
+        } catch (IllegalAccessException e) {
+            throw new ClassException(String.format("해당 클래스(%s)의 생성자에 접근할 수 없습니다 ", clazz.getName()), e);
+        } catch (InvocationTargetException e) {
+            throw new ClassException(String.format("해당 클래스(%s)의 생성자 호출 중 예외가 발생했습니다 ", clazz.getName()), e);
+        }
+    }
+
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String httpMethod = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(httpMethod));
+
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,30 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.exception.MethodException;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Method method;
+    private final Object instance;
+
+    public HandlerExecution(Method method, Object instance) {
+        this.method = method;
+        this.instance = instance;
     }
+
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) {
+        try {
+            return (ModelAndView) method.invoke(instance, request, response);
+        } catch (IllegalAccessException e) {
+            throw new MethodException(String.format("%s: 해당 메서드에 접근하지 못했습니다.", method.getName()), e);
+        } catch (InvocationTargetException e) {
+            throw new MethodException(String.format("%s: 메서드 호출 중 에러가 발생했습니다.", method.getName()), e);
+        }
+    }
+
 }
