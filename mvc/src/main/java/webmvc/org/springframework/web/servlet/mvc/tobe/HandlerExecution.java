a152f248c931bcce4f8243dac8393c5835b2fcfe
헙크 <chocomilk8604@gmail.com>
2023-09-14 19:50:38 +0900

    [MVC 구현하기 - 1단계] 헙크(정현승) 미션 제출합니다. (#384)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: 학습 테스트 구현
    
    * test: Reflections 테스트 작성
    
    * feat: AnnotationHandlerMapping 초안
    
    * refactor: AnnotationHandlerMapping 수정
    
    * test: Servlet 테스트 작성
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
+    private final Object instance;
+    private final Method method;
+
+    public HandlerExecution(final Object instance, final Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
