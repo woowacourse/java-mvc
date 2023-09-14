40eb5154f3b6de9d62d44e90e3a4019165ed7615
Go-Jaecheol <gojaech@naver.com>
2023-09-14 14:35:49 +0900

    feat: HandlerExecution 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        final Class<?> clazz = method.getDeclaringClass();
+        return (ModelAndView) method.invoke(clazz.getConstructor().newInstance(), request, response);
     }
 }
