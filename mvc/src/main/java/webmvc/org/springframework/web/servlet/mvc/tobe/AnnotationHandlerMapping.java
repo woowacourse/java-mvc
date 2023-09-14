6ce849f6e74bea97c8437fa79ffb237023565585
제이미 <63184334+JJ503@users.noreply.github.com>
2023-09-14 16:18:11 +0900

    [MVC 구현하기 - 1단계] 제이미(임정수) 미션 제출합니다. (#411)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: reflection 학습 테스트 추가
    
    * feat: servlet 학습 테스트 추가
    
    * feat: 어노테이션을 통한 요청 매핑 기능 추가
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +30,40 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        controllerClasses.forEach(this::putHandlerExecutions);
+    }
+
+    private void putHandlerExecutions(final Class<?> clazz) {
+        final List<Method> methods = getMethods(clazz);
+
+        for (final Method method : methods) {
+            final List<HandlerKey> handlerKeys = calculateHandlerKeys(method);
+            handlerKeys.forEach(handlerKey -> handlerExecutions.put(handlerKey, new HandlerExecution(clazz, method)));
+        }
+    }
+
+    private static List<Method> getMethods(final Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getDeclaredMethods())
+                     .filter(clazz -> clazz.isAnnotationPresent(RequestMapping.class))
+                     .collect(Collectors.toList());
+    }
+
+    private List<HandlerKey> calculateHandlerKeys(final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String value = requestMapping.value();
+        return Arrays.stream(requestMapping.method())
+                     .map(requestMethod -> new HandlerKey(value, requestMethod))
+                     .collect(Collectors.toList());
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+
+        return handlerExecutions.get(handlerKey);
     }
 }
