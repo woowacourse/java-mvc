33466ce5f1fef160d75956977130f91ccd2077c2
MODI <77962265+jaehee329@users.noreply.github.com>
2023-09-14 11:22:47 +0900

    [MVC 구현하기 - 1단계] 모디(전제희) 미션 제출합니다. #400
     - feat: AnnotationHandlerMapping 구현 (#400)

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -6,7 +6,13 @@ import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final HandlerMethod handlerMethod;
+
+    public HandlerExecution(HandlerMethod handlerMethod) {
+        this.handlerMethod = handlerMethod;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handlerMethod.getMethod().invoke(handlerMethod.getBean(), request, response);
     }
 }
