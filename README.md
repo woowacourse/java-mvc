b8bb274920a7bec52b366192509605a467be5732
waterricecake <91263263+waterricecake@users.noreply.github.com>
2023-09-14 22:54:00 +0900

    [MVC 구현하기 - 1단계] 달리(김동연) 미션 제출합니다.  (#407)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: study Reflection
    
    * test: study filter 설정
    
    * feat : AnnotationHandlerMapping 구현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # @MVC 구현하기
+
+
+## 기능 구현 목록
+
+- [x] AnnotationHandlerMapping 구현
+- [x] HandlerExecution 구현
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,10 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit3Test realClass = new Junit3Test();
+        for(Method method: methods){
+            method.invoke(realClass);
+        }
     }
 }
