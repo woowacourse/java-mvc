9db8e3bfd792a6215a1af64dcca26c9133202576
ChoiYoungHoon <dudgns2865@naver.com>
2023-09-14 12:30:35 +0900

    feat: HandlerExecution 핸들링 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,24 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import core.org.springframework.util.ReflectionUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(final Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        final Class<?> declaringClass = method.getDeclaringClass();
+        final Constructor<?> constructor = ReflectionUtils.accessibleConstructor(declaringClass);
+        final Object instance = constructor.newInstance();
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
