ad6b437b138197b1ec6e51403533060249308e03
hgo641 <hgo641@gmail.com>
2023-09-14 17:02:17 +0900

    feat: 핸들러 초기화 기능 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,67 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            final List<Method> requestMethods = getRequestMethods();
+            requestMethods.forEach(this::registerHandler);
+            log.info("Initialized AnnotationHandlerMapping!");
+        } catch (final Exception e) {
+            log.error("Fail To Initialize AnnotationHandlerMapping : {}", e.getMessage());
+        }
+    }
+
+    private List<Method> getRequestMethods() {
+        final Reflections reflections = new Reflections(basePackages);
+        return reflections.getTypesAnnotatedWith(Controller.class).stream()
+                .map(Class::getDeclaredMethods)
+                .flatMap(Arrays::stream)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    public void registerHandler(final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String requestURI = requestMapping.value();
+        Arrays.stream(requestMapping.method())
+                .forEach(requestMethod -> handlerExecutions.put(
+                        new HandlerKey(requestURI, requestMethod),
+                        new HandlerExecution(method)
+                ));
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        final HandlerExecution handlerExecution = handlerExecutions.get(new HandlerKey(requestURI, requestMethod));
+        if (handlerExecution == null) {
+            throw new IllegalArgumentException("요청과 매핑되는 핸들러가 존재하지 않습니다.");
+        }
+        return handlerExecution;
     }
 }
