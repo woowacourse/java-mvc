f0bb9c9834964bbeb0670c6fa85a23fc43459f9a
Ohjintaek <116645747+Ohjintaek@users.noreply.github.com>
2023-09-14 15:16:49 +0900

    [MVC 구현하기 - 1단계] 썬샷(오진택) 미션 제출합니다  (#415)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: Reflection 학습 테스트
    
    * feat: 컨트롤러 매핑 기능 구현
    
    * test: Servlet 학습 테스트
    
    * test: Filter 학습 테스트
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,72 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        try {
+            for (final Class<?> controller : controllers) {
+                final Constructor<?> constructor = controller.getConstructor();
+                final Object instance = constructor.newInstance();
+                mapRequestMappingAnnotatedMethod(controller, instance);
+            }
+            log.info("Initialized AnnotationHandlerMapping!");
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void mapRequestMappingAnnotatedMethod(final Class<?> clazz, final Object instance) {
+        final Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            if (requestMapping == null) {
+                continue;
+            }
+
+            final String url = requestMapping.value();
+            final RequestMethod[] requestMethods = requestMapping.method();
+            registerHandler(instance, method, url, requestMethods);
+        }
+    }
+
+    private void registerHandler(final Object instance, final Method method, final String url,
+                                 final RequestMethod[] requestMethods) {
+        for (final RequestMethod requestMethod : requestMethods) {
+            final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            final HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerExecutions.get(
+                new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
+    }
+}
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +11,13 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Constructor<Junit3Test> constructor = clazz.getConstructor();
+        final Object junit3Test = constructor.newInstance();
+        final Method[] methods = clazz.getDeclaredMethods();
+        for (final Method method : methods) {
+            if(method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
