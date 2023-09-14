246e55dc12271287400530507ba2105d4b59d5e6
yenawee <yaena0319@naver.com>
2023-09-14 10:42:18 +0900

    feat: HandlerExecution 에서 메서드 실행하도록 기능 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Method method;
+    private final Object controller;
+
+    public HandlerExecution(Method method, Object controller) {
+        this.method = method;
+        this.controller = controller;
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
