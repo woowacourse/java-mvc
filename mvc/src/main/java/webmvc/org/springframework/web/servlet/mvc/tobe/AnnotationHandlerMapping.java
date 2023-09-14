2c998b91cd711aff6fe46104437d48d95a6955b0
Hyun-Seo Oh / 오현서 <100915276+carsago@users.noreply.github.com>
2023-09-14 11:36:04 +0900

    [MVC 미션 1단계] 오리(오현서) 미션 제출합니다. (#355)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: annotation을 통한 Handler mapping
    
    * refactor: 핸들러 생성 과정 리팩토링
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,85 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static java.util.stream.Collectors.toList;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        for (Object packagePath : basePackage) {
+            initHandlerExecutions(packagePath);
+        }
+        log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private void initHandlerExecutions(Object packagePath) {
+        Set<Class<?>> clazz = getClazzByAnnotation(packagePath, Controller.class);
+        for (Class<?> aClass : clazz) {
+            List<Method> methods = getMethodsByAnnotation(aClass, RequestMapping.class);
+            Object instance = getInstance(aClass);
+            for (Method method : methods) {
+                HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+                putHandlerExecution(handlerExecution, method);
+            }
+        }
+    }
+
+    private <T extends Annotation> Set<Class<?>> getClazzByAnnotation(Object packagePath, Class<T> annotationClass) {
+        Reflections reflections = new Reflections(packagePath);
+        return reflections.getTypesAnnotatedWith(annotationClass);
+    }
+
+    private <T extends Annotation> List<Method> getMethodsByAnnotation(Class<?> aClass, Class<T> annotationClass) {
+        return Arrays.stream(aClass.getDeclaredMethods())
+            .filter(it -> it.isAnnotationPresent(annotationClass))
+            .collect(toList());
+    }
+
+    private Object getInstance(Class<?> aClass) {
+        try {
+            return aClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e
+        ) {
+            throw new HandlerMappingException();
+        }
+    }
+
+    private void putHandlerExecution(HandlerExecution handlerExecution, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
+    }
+}
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -0,0 +1,21 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+public class HandlerExecution {
+
+    private Object target;
+    private Method method;
+
+    public HandlerExecution(Object target, Method method) {
+        this.target = target;
+        this.method = method;
+    }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return (ModelAndView) method.invoke(target, request, response);
+    }
+}
