1ee465465926cc5c72e8ab2b611e1ee51ade46c6
This2sho <zx00018@naver.com>
2023-09-14 13:23:17 +0900

    feat: @Controller 지원하도록 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +29,57 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        for (Object subPackage : basePackage) {
+            intTargetPackage(subPackage);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void intTargetPackage(final Object base) {
+        final Reflections reflections = new Reflections(base);
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : controllers) {
+            initTargetController(clazz);
+        }
+    }
+
+    private void initTargetController(final Class<?> clazz) {
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            initTargetMethod(clazz, method);
+        }
+    }
+
+    private void initTargetMethod(final Class<?> clazz, final Method method) {
+        if (Objects.nonNull(method.getDeclaredAnnotation(RequestMapping.class))) {
+            final HandlerKey key = getHandlerKey(method);
+            final Object instance = getInstance(clazz);
+            final HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+            handlerExecutions.put(key, handlerExecution);
+        }
+    }
+
+    private HandlerKey getHandlerKey(final Method method) {
+        final RequestMapping requestMapping = method.getDeclaredAnnotation(RequestMapping.class);
+        final String uri = requestMapping.value();
+        final RequestMethod requestMethod = requestMapping.method()[0];
+        return new HandlerKey(uri, requestMethod);
+    }
+
+    private Object getInstance(final Class<?> clazz) {
+        try {
+            final Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final String method = request.getMethod();
+        final HandlerKey key = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(key);
     }
 }
