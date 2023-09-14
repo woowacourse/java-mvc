8b11e32610c3529665037d4379ce96ff32c12d78
Go-Jaecheol <gojaech@naver.com>
2023-09-14 14:35:29 +0900

    feat: AnnotationHandlerMapping의 initialize, getHandler 메서드 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +25,42 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final var reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for (final var clazz : controllerClasses) {
+            addHandlerExecutionsInClazz(clazz);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void addHandlerExecutionsInClazz(final Class<?> clazz) {
+        final List<Method> methods = findMethodsAnnotatedRequestMapping(clazz);
+        for (final var method : methods) {
+            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            addHandlerExecution(method, requestMapping);
+        }
+    }
+
+    private List<Method> findMethodsAnnotatedRequestMapping(final Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(it -> it.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
+    private void addHandlerExecution(final Method method, final RequestMapping requestMapping) {
+        final String url = requestMapping.value();
+        final RequestMethod[] requestMethods = requestMapping.method();
+        final var handlerExecution = new HandlerExecution(method);
+        for (final var requestMethod : requestMethods) {
+            final var handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final var requestMethod = RequestMethod.valueOf(request.getMethod());
+        final var handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
