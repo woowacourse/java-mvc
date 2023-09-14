5e17397134225d1a48b3006b58910ca5800e09ac
hum02 <ehdgma02@gmail.com>
2023-09-14 13:13:46 +0900

    [MVC 구현하기 - 1단계] 도치(김동흠) 미션 제출합니다.  (#380)
    * test:reflection 학습 테스트
    
    * test:servlet 학습 테스트
    
    * feat:어노테이션 기반 컨트롤러 등록 기능 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,29 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3TestInstance = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods).forEach(m -> startWithName(m, junit3TestInstance));
+    }
+
+    private void startWithName(Method method, Object obj) {
+        if (method.getName().startsWith("test")) {
+            try {
+                method.invoke(obj);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 class ReflectionsTest {
 
@@ -14,5 +20,10 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> classes = new HashSet<>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        classes.stream().forEach(c -> log.info(c.getSimpleName()));
     }
 }
