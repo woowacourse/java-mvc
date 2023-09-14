665d2726ae8aaceae44ec25c334ab89b639c7665
jaeyeon kim <jakind@naver.com>
2023-09-14 13:25:03 +0900

    feat : 요청으로 들어온 Request URI 와 Method 에 따라 그에 맞는 Handler 를 반환하는 기능 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,14 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.File;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +24,50 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        try {
+            for (Object base : basePackage) {
+                File[] javaClasses = new File(Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResource((String) base)
+                        .getPath()).listFiles();
+
+                for (File javaFile : javaClasses) {
+                    String fileName = javaFile.getName();
+
+                    if (fileName.contains(".")) {
+                        fileName = fileName.substring(0, fileName.lastIndexOf("."));
+                    }
+
+                    Class<?> clazz = Class.forName(base + "." + fileName);
+                    Method[] declaredMethods = clazz.getDeclaredMethods();
+
+                    for (Method method : declaredMethods) {
+                        if (!method.isAnnotationPresent(RequestMapping.class)) {
+                            continue;
+                        }
+
+                        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                        String value = annotation.value();
+                        RequestMethod[] requestMethods = annotation.method();
+
+                        for (RequestMethod requestMethod : requestMethods) {
+                            HandlerKey handlerKey = new HandlerKey(value, requestMethod);
+                            HandlerExecution handlerExecution = new HandlerExecution(clazz.getConstructor().newInstance(), method);
+                            handlerExecutions.put(handlerKey, handlerExecution);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey key = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+
+        return handlerExecutions.get(key);
     }
+
 }
