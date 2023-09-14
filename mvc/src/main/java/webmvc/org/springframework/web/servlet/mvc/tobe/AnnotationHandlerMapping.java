699ec07ac0dd76d04d073b31a761f837ff5c21b9
apptie <appti@naver.com>
2023-09-15 00:42:47 +0900

    refactor: AnnotationHandlerMapping이 ClassScanner를 사용하도록 변경

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -2,74 +2,52 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 import webmvc.org.springframework.web.servlet.mvc.HandlerMapping;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
 
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
-
-    private final Object[] basePackage;
+    private final ClassScanner classScanner;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+        this.classScanner = new ClassScanner(basePackage);
         this.handlerExecutions = new HashMap<>();
     }
 
     @Override
     public void initialize() {
-        final Reflections reflections = new Reflections(basePackage);
-        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        final List<Object> handlers = classScanner.findInstanceByAnnotation(Controller.class);
 
-        for (final Class<?> controller : controllers) {
-            processRequestMappingMethod(controller);
+        for (final Object handler : handlers) {
+            final Method[] methods = handler.getClass()
+                                            .getDeclaredMethods();
+
+            processHandlerExecutors(handler, methods);
         }
     }
 
-    private void processRequestMappingMethod(final Class<?> controller) {
-        final Method[] methods = controller.getDeclaredMethods();
-
+    private void processHandlerExecutors(final Object handler, final Method[] methods) {
         for (final Method method : methods) {
-            final Object handler = createInstanceOf(controller);
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
 
-            processHandlerExecutors(handler, method);
-        }
-    }
-
-    private Object createInstanceOf(final Class<?> controller) {
-        try {
-            return controller.getConstructor()
-                             .newInstance();
-        } catch (final NoSuchMethodException |
-                InvocationTargetException |
-                InstantiationException |
-                IllegalAccessException e) {
-            log.error("", e);
-        }
-
-        throw new IllegalArgumentException("해당 Handler를 생성할 수 없습니다.");
-    }
-
-    private void processHandlerExecutors(final Object handler, final Method method) {
-        if (method.isAnnotationPresent(RequestMapping.class)) {
-            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-
-            for (final RequestMethod requestMethod : requestMapping.method()) {
-                final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
-                final HandlerExecution handlerExecution = new HandlerExecution(handler, method);
-
-                handlerExecutions.put(handlerKey, handlerExecution);
+                putHandlerExecutors(handler, method, requestMapping);
             }
+        }
+    }
+
+    private void putHandlerExecutors(final Object handler, final Method method, final RequestMapping requestMapping) {
+        for (final RequestMethod requestMethod : requestMapping.method()) {
+            final HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+            final HandlerExecution handlerExecution = new HandlerExecution(handler, method);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
 
