9f0d3f7d5ea2799fdf0b2e454dec743530ed928a
헙크 <chocomilk8604@gmail.com>
2023-09-14 19:50:38 +0900

    [MVC 구현하기 - 1단계] 헙크(정현승) 미션 제출합니다. (#384)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: 학습 테스트 구현
    
    * test: Reflections 테스트 작성
    
    * feat: AnnotationHandlerMapping 초안
    
    * refactor: AnnotationHandlerMapping 수정
    
    * test: Servlet 테스트 작성
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +28,37 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize()  {
         log.info("Initialized AnnotationHandlerMapping!");
+        final Reflections reflections = new Reflections(basePackage);
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(this::register);
+    }
+
+    private void register(final Class<?> clazz) {
+        try {
+            final Object instance = clazz.getDeclaredConstructor().newInstance();
+            final List<Method> annotatedMethods = Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toList());
+
+            for (final Method annotatedMethod : annotatedMethods) {
+                final RequestMapping annotation = annotatedMethod.getAnnotation(RequestMapping.class);
+                for (RequestMethod requestMethod : annotation.method()) {
+                    final HandlerKey handlerKey = new HandlerKey(annotation.value(), requestMethod);
+                    handlerExecutions.put(handlerKey, new HandlerExecution(instance, annotatedMethod));
+                }
+            }
+        } catch (NoSuchMethodException |
+                 InvocationTargetException |
+                 InstantiationException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,21 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final Class<Junit3Test> clazz = Junit3Test.class;
+        final Constructor<Junit3Test> constructor = clazz.getDeclaredConstructor();
+        final Junit3Test junit3Test = constructor.newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (final Method declaredMethod : declaredMethods) {
+            declaredMethod.invoke(junit3Test);
+        }
     }
 }
