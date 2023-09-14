8308ffc49004fbe47fa95d9b56234d0670cbd6fd
최우창 <clllickme@naver.com>
2023-09-14 15:23:44 +0900

    [MVC 구현하기 - 1단계] 다즐(최우창) 미션 제출합니다. (#412)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: reflection 학습 테스트
    
    * test: filter, servlet 학습 테스트
    
    * feat: 컨트롤러 메서드를 실행하는 HandlerExecution 구현
    
    * feat: 어노테이션 기반 핸들러 매핑 기능 구현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,72 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : classes) {
+            addMethods(aClass);
+        }
+    }
+
+    private void addMethods(Class<?> aClass) {
+        for (Method method : aClass.getDeclaredMethods()) {
+            addHandler(aClass, method);
+        }
+    }
+
+    private void addHandler(Class<?> aClass, Method method) {
+        Controller controller = aClass.getAnnotation(Controller.class);
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (controller == null || requestMapping == null) {
+            return;
+        }
+        String rootUrl = controller.value();
+        String url = requestMapping.value();
+        RequestMethod[] requestMethods = requestMapping.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(rootUrl + url, requestMethod);
+            Object object = getInstance(aClass);
+            handlerExecutions.put(handlerKey, new HandlerExecution(object, method));
+        }
+    }
+
+    private Object getInstance(Class<?> aClass) {
+        try {
+            return aClass.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Object getHandler(HttpServletRequest request) {
+        HandlerKey handlerKey =
+                new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod().toUpperCase()));
+        return handlerExecutions.get(handlerKey);
+    }
+}
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,14 @@
 package reflection;
 
+import static org.reflections.scanners.Scanners.TypesAnnotated;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +19,7 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.get(TypesAnnotated.of(Controller.class, Service.class, Repository.class))
+                .forEach(clazz -> log.info("{}", clazz));
     }
 }
