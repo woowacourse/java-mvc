31c638cf8997bad0be05d86f864851db42f820de
SproutMJ <mjk54565@gmail.com>
2023-09-14 10:48:46 +0900

    [MVC 구현하기 - 1단계] 엔델(김민준) 미션 제출합니다.  (#383)
    * test: 리플렉션 학습 테스트
    
    * feat: 핸들러 매핑 구현을 통한 미션 1단계 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +28,46 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllerAnnotatedClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        controllerAnnotatedClasses.forEach(clazz -> {
+            if (clazz.isAnnotationPresent(RequestMapping.class)) {
+                final RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+                addHandlerExecution(requestMapping.value(), clazz);
+                return;
+            }
+            addHandlerExecution(clazz);
+        });
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void addHandlerExecution(final Class<?> clazz) {
+        addHandlerExecution("", clazz);
+    }
+
+    private void addHandlerExecution(final String prefixPath, final Class<?> clazz) {
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> {
+                    final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    final HandlerKey handlerKey = new HandlerKey(prefixPath + requestMapping.value(), requestMapping.method()[0]);
+                    final HandlerExecution handler = createHandler(clazz, method);
+                    handlerExecutions.put(handlerKey, handler);
+                });
+    }
+
+    private HandlerExecution createHandler(final Class<?> clazz, final Method method) {
+        try {
+            return new HandlerExecution(clazz.getConstructor().newInstance(), method);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            log.error("핸들러 생성 실패");
+            throw new RuntimeException("핸들러를 생성할 수 없습니다.");
+        }
+    }
+
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        return this.handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
@@ -9,5 +11,10 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (final Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(method.getDeclaringClass().getConstructor().newInstance());
+            }
+        }
     }
 }
