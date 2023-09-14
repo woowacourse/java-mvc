e603d66491df6f9e0e92d231d5d76e58fa8b0606
동해 <donghae1999@gmail.com>
2023-09-14 15:03:07 +0900

    [MVC 미션 1단계] 오션(김동해) 미션 제출합니다.  (#358)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: Reflection 학습 테스트 추가
    
    * test: 서블릿 학습 테스트 추가
    
    * refactor: 소나클라우드 수정
    
    * feat: MVC 프레임워크 구현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -0,0 +1,22 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Method;
+
+public class HandlerExecution {
+
+    private Object instance;
+    private Method method;
+
+    public HandlerExecution(final Object instance, final Method method) {
+        this.instance = instance;
+        this.method = method;
+    }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return (ModelAndView) method.invoke(instance,request,response);
+    }
+}
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 class ReflectionsTest {
 
@@ -14,5 +22,12 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> set = new HashSet<>();
+
+        set.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        set.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        set.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        set.stream().forEach(it -> log.info(it.getSimpleName()));
     }
 }
