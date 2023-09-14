0c7eb8c021a960752b4a6fa2dfb70d60d19afd5c
Hanuel Lee <91522259+hanueleee@users.noreply.github.com>
2023-09-14 17:30:22 +0900

    [MVC 구현하기 - 1단계] 오잉(이하늘) 미션 제출합니다.  (#419)
    * reflection 학습 테스트 완료
    
    * feat: @MVC 프레임워크 구현
    
    * refactor: AnnotationHandlerMapping의 initialize 메소드 리팩토링
    
    * chore: build.yml에 sonarcloud 관련 명령어 추가
    
    * test: reflection 학습테스트의 필요없는 기본 생성자 삭제

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +26,37 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize()
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            Object instance = controller.getDeclaredConstructor().newInstance();
+            Method[] methods = controller.getDeclaredMethods();
+
+            Arrays.stream(methods)
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> addHandlerExecutions(instance, method));
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void addHandlerExecutions(Object target, Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        HandlerExecution handlerExecution = new HandlerExecution(target, method);
+
+        RequestMethod[] requestMethods = annotation.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(annotation.value(), requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+
+        return handlerExecutions.get(new HandlerKey(requestURI, RequestMethod.valueOf(method)));
     }
 }
