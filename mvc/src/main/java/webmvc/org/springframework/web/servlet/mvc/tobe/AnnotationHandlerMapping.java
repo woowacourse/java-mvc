8a4ea2c633c45a60b991f1c3a1a879f2afd4cfa3
cpot5620 <cpot5620@gmail.com>
2023-09-14 12:41:33 +0900

    feat: AnnotationHandlerMapping 기능 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,20 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +29,47 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classesWithController = reflections.getTypesAnnotatedWith(Controller.class);
+        try {
+            initializeHandlerExecutions(classesWithController);
+        } catch (Exception e) {
+            log.error("ERROR : {}", e.getMessage());
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void initializeHandlerExecutions(Set<Class<?>> classesWithController)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        for (Class<?> classWithController : classesWithController) {
+            List<Method> methods = extractMethods(classWithController);
+
+            for (Method method : methods) {
+                RequestMapping requestMappingAnnotation = method.getAnnotation(RequestMapping.class);
+                String url = requestMappingAnnotation.value();
+                RequestMethod[] requestMethods = requestMappingAnnotation.method();
+
+                for (RequestMethod requestMethod : requestMethods) {
+                    HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                    HandlerExecution handlerExecution =
+                            new HandlerExecution(method, classWithController.getDeclaredConstructor().newInstance());
+
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+            }
+        }
+    }
+
+    private List<Method> extractMethods(Class<?> classWithController) {
+        return Arrays.stream(classWithController.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toList());
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String uri = request.getRequestURI();
+        RequestMethod method = RequestMethod.valueOf(request.getMethod());
+
+        return handlerExecutions.get(new HandlerKey(uri, method));
     }
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,11 +2,21 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Method handler;
+    private final Object object;
+
+    public HandlerExecution(Method handler, Object object) {
+        this.handler = handler;
+        this.object = object;
     }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return (ModelAndView) handler.invoke(object, request, response);
+    }
+
 }
