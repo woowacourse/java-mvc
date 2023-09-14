3e72b8fb27641f32109825c43c94515bf77ce8dd
이지우 <49433615+LJW25@users.noreply.github.com>
2023-09-15 01:02:15 +0900

    [MVC 구현하기 - 1단계] 이오(이지우) 미션 제출합니다. (#397)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: Reflection 학습테스트 구현
    
    * test: Servlet 학습테스트 구현
    
    * feat: MVC 구현
    
    * test: app 모듈 어플리케이션 실행 테스트 추가
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -0,0 +1,23 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+public class HandlerExecution {
+
+    private final Class<?> clazz;
+
+    private final Method method;
+
+    public HandlerExecution(final Class<?> clazz, final Method method) {
+        this.clazz = clazz;
+        this.method = method;
+    }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final Object obj = clazz.getConstructor().newInstance();
+        return (ModelAndView) method.invoke(obj, request, response);
+    }
+}
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,14 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -11,8 +16,16 @@ class ReflectionsTest {
 
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+        final Reflections reflections = new Reflections("reflection.examples");
+        final List<Class<? extends Annotation>> annotations = List.of(
+                Controller.class,
+                Service.class,
+                Repository.class
+        );
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        for (final Class<? extends Annotation> annotation : annotations) {
+            reflections.getTypesAnnotatedWith(annotation)
+                    .forEach(it -> log.info(it.getSimpleName()));
+        }
     }
 }
