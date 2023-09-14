98b718b0bd7a3adb192999b9449085b07155cf88
gray <68818952+Kim0914@users.noreply.github.com>
2023-09-14 16:55:29 +0900

    [MVC 미션 1단계] 그레이(김동욱) 미션 제출합니다. (#359)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: 리플렉션 테스트 작성
    
    * test: 서블릿, 필터 테스트 작성
    
    * feat: HandlerExecution object, method 필드 추가
    
    * feat: 어노테이션 핸들러 매핑 기능 추가
    
    * build: 소나 클라우드 설정 추가
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,70 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : classes) {
+            findAnnotatedMethod(aClass);
+        }
+    }
+
+    private void findAnnotatedMethod(Class<?> aClass) {
+        for (Method method : aClass.getDeclaredMethods()) {
+            addHandler(aClass, method);
+        }
+    }
+
+    private void addHandler(Class<?> aClass, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping == null) {
+            return;
+        }
+
+        String url = requestMapping.value();
+        RequestMethod[] methods = requestMapping.method();
+        for (RequestMethod requestMethod : methods) {
+            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution(getObject(aClass), method));
+        }
+    }
+
+    private Object getObject(Class<?> aClass) {
+        try {
+            return aClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
+    }
+}
