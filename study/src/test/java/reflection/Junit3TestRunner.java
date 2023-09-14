7d13951da53a96cfe7e6b44b62c9d4dfd32fb8f5
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-14 20:50:48 +0900

    [MVC 구현하기 - 1단계] 제리(권현재) 미션 제출합니다  (#414)
    * docs: 기능 요구사항 작성
    
    * test: 리플렉션 실습
    
    * test: 서블릿 학습 테스트 구현
    
    * feat: 핸들러 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+
+        for (final Method testMethod : testMethods) {
+            testMethod.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final List<Method> myTestMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        for (final Method myTestMethod : myTestMethods) {
+            myTestMethod.invoke(clazz.getDeclaredConstructor().newInstance());
+        }
     }
 }
