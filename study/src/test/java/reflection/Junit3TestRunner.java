69c80dd83b07cf0bc32c89efb23f661156d0a334
swonny <wonny921@gmail.com>
2023-09-14 14:13:39 +0900

    test: Reflection API 학습 테스트 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,33 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
+
+    private static void accept(Method method) {
+        try {
+            method.invoke(new Junit3Test());
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = clazz.getDeclaredMethods();
+
+        final Junit3Test junit3Test = new Junit3Test();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import static org.reflections.scanners.Scanners.TypesAnnotated;
 
 class ReflectionsTest {
 
@@ -14,5 +19,7 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.get(TypesAnnotated.of(Controller.class, Service.class, Repository.class))
+                   .forEach(classType -> log.info("class {}", classType));
     }
 }
