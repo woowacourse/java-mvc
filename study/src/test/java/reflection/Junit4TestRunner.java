3c6eee196b5e09e9132292a418c480c16e4cff9f
ezzanzzan <cjsdmswjd821@naver.com>
2023-09-14 12:44:17 +0900

    feat: reflection 학습 테스트

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,23 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
+        Method[] methods = clazz.getMethods();
+        for (final Method method : methods) {
+            Annotation[] annotations = method.getDeclaredAnnotations();
+            for (final Annotation annotation : annotations) {
+                if (annotation.annotationType().equals(MyTest.class)) {
+                    method.invoke(junit4Test);
+                }
+            }
+        }
     }
 }
