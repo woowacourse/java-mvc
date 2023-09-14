3ee8e441a6b2a0f6fbdd3d9d3c8330b542258b58
Rosie <dev.kayoung@gmail.com>
2023-09-14 15:35:30 +0900

    refactor: HandlerExecution 생성자 수정

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -10,12 +10,17 @@ public class HandlerExecution {
     private Object instance;
     private Method handler;
 
-    public HandlerExecution(final Object instance, final Method handler) {
-        this.instance = instance;
-        this.handler = handler;
+    public HandlerExecution(final Method handler) {
+        try {
+            this.instance = handler.getDeclaringClass().getConstructor().newInstance();
+            this.handler = handler;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         return (ModelAndView) handler.invoke(instance, request, response);
     }
+
 }
