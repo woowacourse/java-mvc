25e0d35fde88475ebbf76915f1bbc4dcef4be1b9
seokjin8678 <seokjin8678@gmail.com>
2023-09-14 09:26:51 +0900

    [MVC 구현하기 - 1단계] 글렌(전석진) 미션 제출합니다.  (#343)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: Reflection 테스트 작성
    
    * test: Servlet, Filter 테스트 작성
    
    * docs: 기능 요구 사항 추가
    
    * feat: 어노테이션 기반 핸들리 매핑 구현
    
    * docs: 기능 요구 사항 반영
    
    * refactor: AnnotationHandlerMapping 메서드 리팩터링
    
    * refactor: 변수, 메소드명 명확하게 변경
    
    * docs: 기능 요구 사항 반영
    
    * chore: 로그 추가
    
    * refactor: 파라미터, 변수명 수정
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # @MVC 구현하기
+
+## 기능 요구 사항 목록
+- [X] AnnotationHandlerMappingTest 통과 시키기
+- [X] 어노테이션 기반 MVC 프레임워크 구현
+- [ ] JspView 구현
+- [ ] Legacy MVC, @MVC 통합
+- [ ] ControllerScanner 추가
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,89 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackage;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackage) {
+        this.basePackage = basePackage;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : classes) {
+            initializeHandler(clazz);
+        }
+        log.info("Initialized AnnotationHandlerMapping!");
+        handlerExecutions.keySet()
+            .forEach(handlerKey -> log.info("Handler Key: {}", handlerKey));
+    }
+
+    private void initializeHandler(Class<?> clazz) {
+        Object instance = toInstance(clazz);
+        Set<Method> methods = getAnnotatedMethod(clazz);
+        for (Method method : methods) {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            List<HandlerKey> handlerKeys = getHandlerKeys(annotation);
+            HandlerExecution handlerExecution = new HandlerExecution(instance, method);
+            putHandlerExecution(handlerKeys, handlerExecution);
+        }
+    }
+
+    private Object toInstance(Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private Set<Method> getAnnotatedMethod(Class<?> clazz) {
+        Method[] methods = clazz.getDeclaredMethods();
+        return Arrays.stream(methods)
+            .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+            .collect(toSet());
+    }
+
+    private List<HandlerKey> getHandlerKeys(RequestMapping annotation) {
+        String url = annotation.value();
+        RequestMethod[] httpMethods = annotation.method();
+        return Arrays.stream(httpMethods)
+            .map(httpMethod -> new HandlerKey(url, httpMethod))
+            .collect(toList());
+    }
+
+    private void putHandlerExecution(List<HandlerKey> handlerKeys, HandlerExecution handlerExecution) {
+        for (HandlerKey handlerKey : handlerKeys) {
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
+    }
+}
