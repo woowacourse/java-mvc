fee01d72063430640a14bdee507a0b5669b7de40
ChoiYoungHoon <dudgns2865@naver.com>
2023-09-14 13:49:22 +0900

    test: 학습 테스트 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,24 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        final Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Junit3Test instance = clazz.getConstructor().newInstance();
+        final List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .collect(Collectors.toList());
+        for (final Method testMethod : testMethods) {
+            testMethod.invoke(instance);
+        }
     }
 }
