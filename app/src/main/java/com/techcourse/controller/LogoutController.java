f5c7c78c59f678f06912bb9cfd0bfba0dd8c4df2
sh111-coder <ohk9134@naver.com>
2023-09-15 23:29:54 +0900

    feat: Legacy MVC와 @MVC 통합을 위한 @MVC 메소드 추가

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -1,15 +1,28 @@
 package com.techcourse.controller;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.asis.MvcController;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController implements MvcController {
 
     @Override
     public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(final HttpServletRequest request, final HttpServletResponse response) {
+        final var session = request.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return new ModelAndView(new JspView("/index.jsp"));
     }
 }
