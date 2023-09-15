cebe57e4623bdefa5ae3255b24d0b275ae3cc637
sh111-coder <ohk9134@naver.com>
2023-09-15 22:47:12 +0900

    refactor: DispatcherServlet HandlerMapper, HandlerAdapters 사용해서 service 처리

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,38 +1,67 @@
 package com.techcourse;
 
+import com.techcourse.support.mvc.adapter.HandlerAdapters;
+import com.techcourse.support.mvc.adapter.ManualHandlerAdapter;
+import com.techcourse.support.mvc.handler.HandlerMapper;
+import com.techcourse.support.mvc.handler.ManualHandlerMapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.tobe.adapter.AnnotationHandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.adapter.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.handler.AnnotationHandlerMapping;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final String TECH_COURSE_BASE_PACKAGES = "com.techcourse";
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final HandlerMapper handlerMapper;
+    private final HandlerAdapters handlerAdapters;
 
     public DispatcherServlet() {
+        handlerMapper = new HandlerMapper();
+        handlerAdapters = new HandlerAdapters();
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        addInitHandlerMappings();
+        addInitHandlerAdapters();
+        try {
+            handlerMapper.init();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private void addInitHandlerMappings() {
+        handlerMapper.addHandlerMapping(new AnnotationHandlerMapping(TECH_COURSE_BASE_PACKAGES));
+        handlerMapper.addHandlerMapping(new ManualHandlerMapping());
+    }
+
+    private void addInitHandlerAdapters() {
+        handlerAdapters.addHandlerAdapter(new AnnotationHandlerAdapter());
+        handlerAdapters.addHandlerAdapter(new ManualHandlerAdapter());
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            Object handler = handlerMapper.getHandler(request);
+            HandlerAdapter handlerAdapter = handlerAdapters.getHandlerAdapter(handler);
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+            JspView view = (JspView) modelAndView.getView();
+            move(view.getViewName(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
