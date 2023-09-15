093c61c861b766cbb72516d2baef0f6a3b24c4fc
스플릿 <voicesplit@daum.net>
2023-09-15 15:13:04 +0900

    feat: @Controller, @RequestMapping 어노테이션을 사용하여 핸들러 맵핑 기능 구현 (#431)
    스프릿 안녕하세요~! 1단계 요구사항을 완료하셔서 머지시키겠습니다!
    남긴 리뷰도 봐주시고 다음 단계도 파이팅~!

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/exception/ReflectionInstantiationException.java
@@ -0,0 +1,8 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe.exception;
+
+public class ReflectionInstantiationException extends RuntimeException {
+
+    public ReflectionInstantiationException(final String message) {
+        super(message);
+    }
+}
