e07d886b3c970f266fe6bb9c1dd45245edc20b80
sosow0212 <sosow0212@naver.com>
2023-09-15 16:24:01 +0900

    refactor: 회원가입(GET & POST) 로직 모두 어노테이션 기반으로 작동하도록 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -2,27 +2,25 @@ package com.techcourse.controller;
 
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
-import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import web.org.springframework.web.bind.annotation.RequestMapping;
-import web.org.springframework.web.bind.annotation.RequestMethod;
-import webmvc.org.springframework.web.servlet.ModelAndView;
-import webmvc.org.springframework.web.servlet.view.JspView;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
 
-@Controller
-public class RegisterController {
+public class RegisterController implements Controller {
+
+    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
+    public ModelAndView findRegisterView(final HttpServletRequest req, final HttpServletResponse res) {
+        return new ModelAndView(new JspView("/register.jsp"));
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView register(final HttpServletRequest req, final HttpServletResponse res) {
         User user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
-                req.getParameter("email")
-        );
-
+                req.getParameter("email"));
         InMemoryUserRepository.save(user);
 
-        return new ModelAndView(new JspView("redirect:/index.jsp"));
+        return "redirect:/index.jsp";
     }
 }
