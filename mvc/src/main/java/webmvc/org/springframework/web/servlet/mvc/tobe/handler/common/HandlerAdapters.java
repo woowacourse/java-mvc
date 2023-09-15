af7c15bfc6437a12fcdd43989339d85187ccb52e
sosow0212 <sosow0212@naver.com>
2023-09-15 15:02:13 +0900

    feat: Mapper와 Adapter를 관리하는 기능 추가

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/handler/common/HandlerAdapters.java
@@ -0,0 +1,26 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe.handler.common;
+
+import webmvc.org.springframework.web.servlet.mvc.tobe.handler.adapter.HandlerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerAdapters {
+
+    private final List<HandlerAdapter> handlerAdapters;
+
+    public HandlerAdapters() {
+        this.handlerAdapters = new ArrayList<>();
+    }
+
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(final Object controller) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.isSupported(controller))
+                .findFirst()
+                .orElseThrow();
+    }
+}
