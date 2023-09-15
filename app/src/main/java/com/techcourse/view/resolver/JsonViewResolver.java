70655258dd5e9821408aa2099910c99da71134fe
apptie <appti@naver.com>
2023-09-15 17:33:18 +0900

    feat: JsonViewResolver 추가

---
+++ app/src/main/java/com/techcourse/view/resolver/JsonViewResolver.java
@@ -0,0 +1,24 @@
+package com.techcourse.view.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.view.JsonView;
+import webmvc.org.springframework.web.servlet.view.resolver.ViewResolver;
+
+public class JsonViewResolver implements ViewResolver {
+
+    private static final String ACCEPT_HEADER_KET = "Accept";
+    private static final String APPLICATION_JSON_VALUE = "application/json";
+
+    @Override
+    public boolean supports(final HttpServletRequest request, final String ignored) {
+        final String acceptHeaderValue = request.getHeader(ACCEPT_HEADER_KET);
+
+        return acceptHeaderValue != null && acceptHeaderValue.contains(APPLICATION_JSON_VALUE);
+    }
+
+    @Override
+    public View resolve(final String ignored) {
+        return new JsonView();
+    }
+}
