5501b7fb459b28cdd5a69e1271c609c4f12839fc
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-15 23:57:33 +0900

    refactor: 학습 테스트 구현 완료

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,7 @@
 package di.stage3.context;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,11 +12,32 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = new HashSet<>();
+
+        final Class<?> userDao = classes.stream()
+            .filter(clazz -> Arrays.asList(clazz.getInterfaces()).contains(UserDao.class))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("UserDao가 존재하지 않습니다."));
+
+        final Class<?> userService = classes.stream()
+            .filter(clazz -> clazz.isAssignableFrom(UserService.class))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("UserService가 존재하지 않습니다."));
+
+        try {
+            final UserDao userDaoInstance = (UserDao) userDao.getConstructor().newInstance();
+            beans.add(userDaoInstance);
+            beans.add(userService.getConstructor(UserDao.class).newInstance(userDaoInstance));
+        } catch (Exception e) {
+            throw new RuntimeException(e + " Bean 등록 시 예외가 발생했습니다.");
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+            .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("해당하는 클래스가 존재하지 않습니다."));
     }
 }
--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,17 @@
 package di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        final Reflections reflections = new Reflections(packageName);
+        final Set<Class<?>> response = new HashSet<>(reflections.getTypesAnnotatedWith(Inject.class));
+        response.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        response.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        return response;
     }
 }
