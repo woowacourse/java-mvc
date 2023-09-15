983444d541293e6b9d7fb889f99326f92779554f
odo27 <84677292+odo27@users.noreply.github.com>
2023-09-15 17:41:00 +0900

    [MVC 구현하기 - 1단계] 오도(문민혁) 미션 제출합니다. (#364)
    * feat: 리플렉션을 활용해 메서드 실행시키는 학습테스트 구현
    
    * feat: 리플렉션을 활용해 생성자와 필드에 접근하는 학습테스트 구현
    
    * feat: 리플렉션을 활용해 어노테이션이 있는 클래스 찾기 학습테스트 구현
    
    * feat: Servlet 학습테스트 구현
    
    * feat: Filter 에서 인코딩 설정하는 학습테스트 구현
    
    * feat: 빠르게 테스트 동작하게 만들기
    
    * feat: 패키지에서 컨트롤러 찾고 매핑해주는 기능 구현
    
    * feat: Controller 어노테이션 작동하도록 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecution;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
@@ -14,6 +18,7 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private ManualHandlerMapping manualHandlerMapping;
+    private AnnotationHandlerMapping annotationHandlerMapping;
 
     public DispatcherServlet() {
     }
@@ -22,16 +27,22 @@ public class DispatcherServlet extends HttpServlet {
     public void init() {
         manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+        annotationHandlerMapping = new AnnotationHandlerMapping("com.techcourse.controller");
+        annotationHandlerMapping.initialize();
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
-        final String requestURI = request.getRequestURI();
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
+            Controller controller = manualHandlerMapping.getHandler(requestURI);
+            if (controller == null) {
+                serviceByAnnotatedController(request, response);
+                return;
+            }
+            String viewName = controller.execute(request, response);
             move(viewName, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
@@ -39,7 +50,17 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+    private void serviceByAnnotatedController(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            HandlerExecution handlerExecution = (HandlerExecution) annotationHandlerMapping.getHandler(request);
+            ModelAndView modelAndView = handlerExecution.handle(request, response);
+            modelAndView.render(request, response);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
             return;
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +28,61 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            log.info("Initialized AnnotationHandlerMapping!");
+            String packageName = (String) basePackage[0];
+            Set<Class<?>> controllerClasses = findAllControllerClasses(packageName);
+            controllerClasses.forEach(this::addHandlerExecution);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private Set<Class<?>> findAllControllerClasses(String packageName) {
+        Reflections reflections = new Reflections(packageName);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void addHandlerExecution(Class<?> clazz) {
+        List<Method> methods = getMethods(clazz);
+        Object controller = createInstance(clazz);
+        methods.forEach(method -> addHandlerExecution(method, controller));
+    }
+
+    private List<Method> getMethods(Class<?> clazz) {
+        Method[] methods = clazz.getDeclaredMethods();
+        return Arrays.asList(methods);
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            return constructor.newInstance();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new IllegalArgumentException("컨트롤러에 예상치 못한 문제가 발생했습니다");
+        }
+    }
+
+    private void addHandlerExecution(Method method, Object controller) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+            String uri = annotation.value();
+            List<RequestMethod> requestMethods = Arrays.asList(annotation.method());
+            requestMethods.forEach(requestMethod -> addHandlerExecution(uri, requestMethod, controller, method));
+        }
+    }
+
+    private void addHandlerExecution(String uri, RequestMethod requestMethod, Object controller, Method method) {
+        HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+        handlerExecutions.put(handlerKey, handlerExecution);
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
     }
 }
