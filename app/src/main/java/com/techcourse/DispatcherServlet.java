f941033f92c37fbf72672f5d5dbd5aa426b7aa27
apptie <appti@naver.com>
2023-09-15 12:33:29 +0900

    feat: View render 기능 추가

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.View;
 import webmvc.org.springframework.web.servlet.mvc.HandlerAdapter;
 
 public class DispatcherServlet extends HttpServlet {
@@ -49,16 +50,18 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Legacy MVC + @MVC 통합 과정에서 Controller.execute()로 바로 viewName을 반환받던 코드에서 ModelAndView를 반환받도록 변경이 되었습니다.
-     * 그래서 View 관련 코드를 작성하다가 미션을 보니까 3단계...에서 하는 것 같더라고요...
-     * 그래서 일단 이렇게 방치를 하게 되었습니다...
-     */
     private void move(
             final ModelAndView modelAndView,
             final HttpServletRequest request,
             final HttpServletResponse response
     ) throws Exception {
-        // ignored
+        final View view = modelAndView.getView();
+
+        if (view == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return ;
+        }
+
+        view.render(modelAndView.getModel(), request, response);
     }
 }
