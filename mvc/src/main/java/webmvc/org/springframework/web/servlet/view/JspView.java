07d9d76b8df432ae26db74b4092d1c8c8e3ccc7a
HubCreator <chocomilk8604@gmail.com>
2023-09-16 22:32:36 +0900

    feat: JspView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
@@ -1,5 +1,6 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -9,23 +10,26 @@ import webmvc.org.springframework.web.servlet.View;
 import java.util.Map;
 
 public class JspView implements View {
-
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
-
     public static final String REDIRECT_PREFIX = "redirect:";
 
+    private final String viewName;
+
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
-
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return;
+        }
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
-
-        // todo
+        final RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
