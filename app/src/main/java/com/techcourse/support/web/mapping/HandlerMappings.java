f482bb9287927000ba13db077e78afd76e5203eb
HubCreator <chocomilk8604@gmail.com>
2023-09-16 23:40:54 +0900

    refactor: 패키지 구조 수정

--- app/src/main/java/com/techcourse/support/web/mapping/HandlerMappings.java
+++ app/src/main/java/com/techcourse/support/web/mapping/HandlerMappings.java
@@ -1,6 +1,5 @@
 package com.techcourse.support.web.mapping;
 
-import com.techcourse.ManualHandlerMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMapping;
