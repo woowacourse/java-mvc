b9341e95f537da90e6574f9b9a24334da50b9829
woosung1223 <dntjd991223@naver.com>
2023-09-16 18:47:21 +0900

    fix: Legacy Controller를 implements하지 않도록 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -5,19 +5,22 @@ import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import web.org.springframework.web.bind.annotation.PostMapping;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
 @context.org.springframework.stereotype.Controller
-public class RegisterController implements Controller {
+public class RegisterController {
 
     @PostMapping("/register")
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView save(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserRepository.save(user);
 
-        return "redirect:/index.jsp";
+        View view = new JspView("redirect:/index.jsp");
+        return new ModelAndView(view);
     }
 }
