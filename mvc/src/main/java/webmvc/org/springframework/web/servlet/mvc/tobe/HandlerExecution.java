b61efe4298c81689bec55dcb6e0ffc2033ba20d4
waterricecake <fruturum@nate.com>
2023-09-16 21:56:56 +0900

    refactor: 구현 controller 생성 시점 변경

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -7,15 +7,23 @@ import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
-    private final Class controller;
+    private final Object controller;
     private final Method method;
 
-    public HandlerExecution(Class controller, Method method) {
+    private HandlerExecution(Object controller, Method method) {
         this.controller = controller;
         this.method = method;
     }
 
+    public static HandlerExecution of(Class<?> controller, Method method) {
+        try {
+            return new HandlerExecution(controller.getConstructor().newInstance(), method);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(controller.getConstructor().newInstance(), request, response);
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
