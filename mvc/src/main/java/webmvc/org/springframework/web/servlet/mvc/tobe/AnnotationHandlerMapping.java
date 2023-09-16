8bbf985f59c910aac20b68cb97bb59424384ae20
lookh <103165859+aiaiaiai1@users.noreply.github.com>
2023-09-16 22:12:47 +0900

    [MVC 구현하기 - 1단계] 루쿠(백경환) 미션 제출합니다 (#437)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * test: 리플렉션 테스트 학습
    
    * test: 서블릿 테스트 학습
    
    * feat: 초기화 및 핸들러 실행 가져오기 기능 추가
    
    * feat: 핸들러 실행하는 기능 추가
    
    * refactor: 생성자 메서드 수정
    
    * refactor: 파이널 키워드 추가
    
    * refactor: 예외 세분화
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,73 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackages;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        for (Object basePackage : basePackages) {
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+            addHandlerExecutions(controllerClasses);
+        }
+    }
+
+    private void addHandlerExecutions(Set<Class<?>> classes) {
+        for (Class<?> clazz : classes) {
+            Method[] methods = clazz.getMethods();
+            Arrays.stream(methods)
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> addHandlerExecution(clazz, method));
+        }
+    }
+
+    private void addHandlerExecution(Class<?> clazz, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMapping.method()[0]);
+        HandlerExecution handlerExecution = new HandlerExecution(createInstance(clazz), method);
+        handlerExecutions.put(handlerKey, handlerExecution);
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (NoSuchMethodException e) {
+            throw new IllegalArgumentException("클래스에 기본 생성자가 없습니다.", e);
+        } catch (InstantiationException e) {
+            throw new IllegalArgumentException("클래스를 인스턴스화 할 수 없습니다.", e);
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException("클래스의 기본 생성자에 접근할 수 없습니다.", e);
+        } catch (InvocationTargetException e) {
+            throw new IllegalArgumentException("생성자 호출 중 예외가 발생했습니다.", e);
+        }
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
+    }
+
+}
