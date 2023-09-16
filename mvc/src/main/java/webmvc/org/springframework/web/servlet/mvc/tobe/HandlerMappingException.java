deeb53bcd524e66324865a787419e5d9d6b670d4
Miseong Kim <miseongk@naver.com>
2023-09-16 23:29:38 +0900

    refactor: 1단계 리뷰 반영

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerMappingException.java
@@ -0,0 +1,8 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+public class HandlerMappingException extends RuntimeException {
+
+    public HandlerMappingException(final String message) {
+        super(message);
+    }
+}
