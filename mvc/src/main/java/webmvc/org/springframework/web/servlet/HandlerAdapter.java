0baf614a265c552db0592a3897dbe97ed269f7bc
iamjooon2 <iamjooon2@gmail.com>
2023-09-18 01:36:16 +0900

    feat: HandlerMapping 추상화

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/HandlerAdapter.java
@@ -0,0 +1,12 @@
+package webmvc.org.springframework.web.servlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface HandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
+
+}
