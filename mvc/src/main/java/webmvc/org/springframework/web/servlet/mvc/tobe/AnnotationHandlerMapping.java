b1dd0b9eaf91aa3ab52850f3e43d1c82b0d20c57
jaehee329 <jjhs9803@gmail.com>
2023-09-17 19:45:48 +0900

    refactor: AnnotationHandlerMapping 클래스 메서드 분리

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -4,13 +4,12 @@ import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 import webmvc.org.springframework.util.ReflectionUtils;
@@ -29,16 +28,21 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> annotatedWithController = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class<?> clazz : annotatedWithController) {
-            try {
-                Object handler = instantiateHandler(clazz);
-                putAnnotatedHandler(clazz, handler);
-            } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
-                throw new SpringInitializationException("AnnotationHandlerMapping 초기화에 실패했습니다.");
-            }
+        Set<Class<?>> annotatedControllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : annotatedControllers) {
+            instantiateAndStore(clazz);
         }
         log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private void instantiateAndStore(Class<?> clazz) {
+        try {
+            Object handler = instantiateHandler(clazz);
+            storeAnnotatedHandlers(clazz, handler);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new SpringInitializationException("AnnotationHandlerMapping 초기화에 실패했습니다.");
+        }
     }
 
     private Object instantiateHandler(Class<?> clazz)
@@ -46,30 +50,31 @@ public class AnnotationHandlerMapping {
         return ReflectionUtils.accessibleConstructor(clazz).newInstance();
     }
 
-    private void putAnnotatedHandler(Class<?> clazz, Object handler) {
+    private void storeAnnotatedHandlers(Class<?> clazz, Object handler) {
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
-            putAnnotatedHandlerMethods(handler, method);
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                storeHandler(handler, method);
+            }
         }
     }
 
-    private void putAnnotatedHandlerMethods(Object handler, Method method) {
-        if (method.isAnnotationPresent(RequestMapping.class)) {
-            RequestMapping requestMapping = method.getDeclaredAnnotation(
-                    RequestMapping.class);
-            String url = requestMapping.value();
-            RequestMethod[] requestMethods = requestMapping.method();
-            for (RequestMethod requestMethod : requestMethods) {
-                HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-                HandlerMethod handlerMethod = new HandlerMethod(handler, method);
-                HandlerExecution handlerExecution = new HandlerExecution(handlerMethod);
-                handlerExecutions.put(handlerKey, handlerExecution);
-            }
+    private void storeHandler(Object handler, Method method) {
+        RequestMapping requestMapping = method.getDeclaredAnnotation(
+                RequestMapping.class);
+        String url = requestMapping.value();
+        RequestMethod[] requestMethods = requestMapping.method();
+        HandlerMethod handlerMethod = new HandlerMethod(handler, method);
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(handlerMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
 
     public Object getHandler(final HttpServletRequest request) {
         return handlerExecutions.get(
-                new HandlerKey(request.getRequestURI(), RequestMethod.resolve(request.getMethod())));
+                new HandlerKey(request.getRequestURI(),
+                        RequestMethod.resolve(request.getMethod())));
     }
 }
