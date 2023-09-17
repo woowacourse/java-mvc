bd5040fc378f950e7b6aae69671d8e3eec2e0f4f
JunH <tjdtls690@gmail.com>
2023-09-17 21:05:42 +0900

    :art: refactor: 인코딩 값 상수화

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/DefaultHandlerExecution.java
+++
@@ -1,9 +0,0 @@
-package webmvc.org.springframework.web.servlet.mvc.tobe;
-
-import java.lang.reflect.Method;
-
-public class DefaultHandlerExecution extends HandlerExecution {
-    public DefaultHandlerExecution(final Method method, final Object controller) {
-        super(method, controller);
-    }
-}
