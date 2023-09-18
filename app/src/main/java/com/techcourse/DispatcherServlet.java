60df5fa6ebb43df8d919d5cad1cd3c8a84e08d2b
kdkdhoho <hkim4410@naver.com>
2023-09-18 21:48:54 +0900

    feat: 2단계 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecution;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
@@ -13,31 +16,58 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final ManualHandlerMapping manualHandlerMapping;
+    private final AnnotationHandlerMapping annotationHandlerMapping;
 
-    public DispatcherServlet() {
+    public DispatcherServlet(ManualHandlerMapping manualHandlerMapping, AnnotationHandlerMapping annotationHandlerMapping) {
+        this.manualHandlerMapping = manualHandlerMapping;
+        this.annotationHandlerMapping = annotationHandlerMapping;
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+        annotationHandlerMapping.initialize();
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
+        String requestURI = request.getRequestURI();
+        String requestMethod = request.getMethod();
+        log.debug("Method : {}, Request URI : {}", requestMethod, requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
-        } catch (Throwable e) {
-            log.error("Exception : {}", e.getMessage(), e);
-            throw new ServletException(e.getMessage());
+            Controller controller = manualHandlerMapping.getHandler(requestURI);
+            HandlerExecution handler = annotationHandlerMapping.getHandler(request);
+
+            validateDuplicate(controller, handler, requestURI);
+            executeHandler(controller, handler, request, response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
+
+    private void validateDuplicate(Controller controller, HandlerExecution handler, String requestURI) {
+        if (controller != null && handler != null) {
+            log.debug("요청을 처리하는 URI가 중복되었습니다. Handler1={}, Handelr2={}, requestURI={}", controller, handler, requestURI);
+            throw new RuntimeException();
+        }
+    }
+
+    private void executeHandler(Controller controller, HandlerExecution handler,
+                                HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (controller == null && handler == null) {
+            move("redirect:404.jsp", request, response);
+            return;
+        }
+
+        if (controller != null) {
+            controller.execute(request, response);
+            return;
+        }
+        handler.handle(request, response);
+    }
+
 
     private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
