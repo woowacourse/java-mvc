aa50254bee79ac858fe646469d6f912bd57e8ca0
splitCoding <voicesplit@daum.net>
2023-09-18 21:07:19 +0900

    feat: HandlerExecution 생성시 클래스, 메서드 검증 로직 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -4,21 +4,41 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
     private final Object handler;
-    private final Method method;
+    private final Method handlerMethod;
 
-    public HandlerExecution(final Object handler, final Method method) {
+    public HandlerExecution(final Object handler, final Method handlerMethod) {
+        validateDeclare(handler, handlerMethod);
+        validateHandlerMethod(handlerMethod);
         this.handler = handler;
-        this.method = method;
+        this.handlerMethod = handlerMethod;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response)
         throws InvocationTargetException, IllegalAccessException {
 
-        return (ModelAndView) method.invoke(handler, request, response);
+        return (ModelAndView) handlerMethod.invoke(handler, request, response);
+    }
+
+    private void validateDeclare(final Object handler, final Method handlerMethod) {
+        final Class<?> handlerClass = handler.getClass();
+        final Class<?> declaringClass = handlerMethod.getDeclaringClass();
+        if (!handlerClass.equals(declaringClass)) {
+            throw new IllegalArgumentException("클래스에 해당 메서드가 없습니다.");
+        }
+    }
+
+    private void validateHandlerMethod(final Method handlerMethod) {
+        final List<Class<?>> parameterTypes = Arrays.asList(handlerMethod.getParameterTypes());
+        if (parameterTypes.contains(HttpServletRequest.class) &&
+            parameterTypes.contains(HttpServletResponse.class)) {
+            throw new IllegalArgumentException("메서드 인자가 적절하지 않습니다. parameters : " + parameterTypes);
+        }
     }
 }
