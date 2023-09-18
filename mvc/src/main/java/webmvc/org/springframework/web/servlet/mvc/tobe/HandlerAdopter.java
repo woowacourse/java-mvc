f68472eb0d793531ec986c5ea0a00b6ab8a26b71
dev_kong <ahdjd5@gmail.com>
2023-09-18 15:58:35 +0900

    feat: handlerAdapters 구현

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerAdopter.java
@@ -0,0 +1,15 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+public interface HandlerAdopter {
+    boolean isSupport(final Object handler);
+
+    ModelAndView handle(
+            final Object handler,
+            final HttpServletRequest request,
+            final HttpServletResponse response)
+            throws Exception;
+}
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerAdopters.java
@@ -0,0 +1,20 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerAdopters {
+
+    private final List<HandlerAdopter> handlerAdopters = new ArrayList<>();
+
+    public void add(final HandlerAdopter adopter) {
+        handlerAdopters.add(adopter);
+    }
+
+    public HandlerAdopter getAdopter(final Object handler) {
+        return handlerAdopters.stream()
+                .filter(adopter -> adopter.isSupport(handler))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("can't find suitable adopter))"));
+    }
+}
