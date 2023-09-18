9090225650bdb8626ab68c63cf7679196dcdfaeb
kiarakim <kiarakim413@gmail.com>
2023-09-18 13:50:59 +0900

    refactor: Handler를 찾아 handler 할 수 있도록 인터페이스 생성
    HandlerMapping
    HandlerAdapter

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
 import webmvc.org.springframework.web.servlet.mvc.asis.ForwardController;
-import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.mapping.HandlerMapping;
 
 import java.util.HashMap;
 import java.util.Map;
