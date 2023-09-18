b4f43ec9e4401978257bdf207821442ac53411b2
LJW25 <dbjwlee@gmail.com>
2023-09-18 23:38:21 +0900

    feat(DispatcherServlet): handler 적용 방식 변경

--- app/src/main/java/com/techcourse/servlet/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/servlet/DispatcherServlet.java
@@ -4,8 +4,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.mvc.handler.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.handler.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.handler.HandlerExecution;
+import webmvc.org.springframework.web.servlet.mvc.handler.HandlerExecutionHandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.handler.HandlerMapping;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
@@ -13,32 +21,55 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final List<HandlerMapping> handlerMappings = new ArrayList<>();
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        handlerMappings.add(new ManualHandlerMapping());
+        handlerMappings.add(new AnnotationHandlerMapping("com.techcourse"));
+        handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            final Object handler = mapHandler(request);
+            final HandlerAdapter handlerAdapter = mapHandlerAdapter(handler);
+            final Object view = handlerAdapter.execute(request, response);
+
+            move((String) view, request, response);
+            
         } catch (final Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
+
+    private Object mapHandler(final HttpServletRequest request) {
+        return handlerMappings.stream().filter(handlerMapping -> handlerMapping.getHandler(request) != null)
+                .findFirst()
+                .orElse(handlerMappings.get(0))
+                .getHandler(request);
+    }
+
+    private HandlerAdapter mapHandlerAdapter(final Object handler) {
+        if (handler instanceof Controller) {
+            log.info("this is controller");
+            return new ControllerHandlerAdapter(handler);
+        }
+        if (handler instanceof HandlerExecution) {
+            log.info("this is handlerExecution");
+            return new HandlerExecutionHandlerAdapter(handler);
+        }
+        throw new IllegalArgumentException();
+    }
+
 
     private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response)
             throws Exception {
