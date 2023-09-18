4cf2d5e8c8a64b0e06ed644d7b66560ffd40fe18
gitchan <eunkeeeee@gmail.com>
2023-09-18 20:58:12 +0900

    feat: 로그인 페이지 get, post 요청 분리

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.techcourse;
 
-import com.techcourse.controller.LoginController;
 import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
 import com.techcourse.controller.RegisterViewController;
@@ -24,7 +23,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
         controllers.put("/register/view", new RegisterViewController());
