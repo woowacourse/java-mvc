75a96f10a745d4379753a9cf0f4e4323ea64a741
cpot5620 <cpot5620@gmail.com>
2023-09-18 12:44:39 +0900

    feat: DispatcherServlet 서비스 기능 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -4,52 +4,79 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
-import webmvc.org.springframework.web.servlet.view.JspView;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdaptor;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMapping;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final List<HandlerMapping> handlerMappings = new ArrayList<>();
+    private final List<HandlerAdaptor> handlerAdaptors = new ArrayList<>();
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping();
         manualHandlerMapping.initialize();
+        annotationHandlerMapping.initialize();
+
+        handlerMappings.add(manualHandlerMapping);
+        handlerMappings.add(annotationHandlerMapping);
+
+        handlerAdaptors.add(new AnnotationHandlerAdaptor());
+        handlerAdaptors.add(new ManualHandlerAdaptor());
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final var controller = (Controller) manualHandlerMapping.getHandler(request);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            Object handler = getController(request);
+            HandlerAdaptor handlerAdaptor = getAdaptor(handler);
+            ModelAndView modelAndView = handlerAdaptor.handle(handler, request, response);
+
+            modelAndView.getView().render(Collections.emptyMap(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response)
-            throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+    private Object getController(HttpServletRequest request) {
+        for (HandlerMapping handlerMapping : handlerMappings) {
+            Object handler = handlerMapping.getHandler(request);
+
+            if (handler != null) {
+                return handler;
+            }
         }
 
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+        throw new NoSuchElementException("요청을 처리할 수 있는 컨트롤러가 존재하지 않습니다.");
     }
+
+    private HandlerAdaptor getAdaptor(Object handler) {
+        for (HandlerAdaptor handlerAdaptor : handlerAdaptors) {
+            if (handlerAdaptor.supports(handler)) {
+                return handlerAdaptor;
+            }
+        }
+
+        throw new NoSuchElementException("처리할 수 있는 핸들러가 존재하지 않습니다.");
+    }
+
 }
