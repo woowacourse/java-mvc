2b4150829139dbfab97bf35e95bf2c8971caa64c
This2sho <zx00018@naver.com>
2023-09-18 20:41:33 +0900

    refactor: 핸들러 생성하는 부분 추출

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -2,20 +2,32 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
-    private final Object object;
+    private final Object handler;
     private final Method method;
 
-    public HandlerExecution(final Object object, final Method method) {
-        this.object = object;
+    public HandlerExecution(final Method method) {
+        this.handler = createHandler(method);
         this.method = method;
     }
 
+    private Object createHandler(final Method method) {
+        try {
+            final Constructor<?> constructor = method.getDeclaringClass().getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e){
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(object, request, response);
+        return (ModelAndView) method.invoke(handler, request, response);
     }
 }
