7a0ffeab3c825a1f160d48209b9e05aaf952e310
kokodak <kokodakadokok@gmail.co>
2023-09-18 12:22:41 +0900

    feat: 기존 인터페이스 기반 컨트롤러에 어노테이션 추가

--- app/src/main/java/com/techcourse/controller/LoginViewController.java
+++ app/src/main/java/com/techcourse/controller/LoginViewController.java
@@ -4,19 +4,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
+@context.org.springframework.stereotype.Controller
 public class LoginViewController implements Controller {
 
     private static final Logger log = LoggerFactory.getLogger(LoginViewController.class);
 
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public String execute(final HttpServletRequest req,
+                          final HttpServletResponse res) throws Exception {
         return UserSession.getUserFrom(req.getSession())
-                .map(user -> {
-                    log.info("logged in {}", user.getAccount());
-                    return "redirect:/index.jsp";
-                })
-                .orElse("/login.jsp");
+                          .map(user -> {
+                              log.info("logged in {}", user.getAccount());
+                              return "redirect:/index.jsp";
+                          })
+                          .orElse("/login.jsp");
+    }
+
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public ModelAndView getLoginView(final HttpServletRequest request,
+                                     final HttpServletResponse response) throws Exception {
+        final String viewName = execute(request, response);
+        return new ModelAndView(new JspView(viewName));
     }
 }
