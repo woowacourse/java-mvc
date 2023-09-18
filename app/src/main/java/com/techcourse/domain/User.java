24e987da29cbc2beee0e7f5313cddce9ab48c8e3
splitCoding <voicesplit@daum.net>
2023-09-18 17:24:46 +0900

    refactor: 기존 유저, UserRepository 변경

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -2,16 +2,19 @@ package com.techcourse.domain;
 
 public class User {
 
-    private final long id;
+    private long id;
     private final String account;
     private final String password;
     private final String email;
 
-    public User(long id, String account, String password, String email) {
-        this.id = id;
+    public User(final String account, final String password, final String email) {
         this.account = account;
         this.password = password;
         this.email = email;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
     }
 
     public boolean checkPassword(String password) {
@@ -25,10 +28,10 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
-                ", account='" + account + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+            "id=" + id +
+            ", account='" + account + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            '}';
     }
 }
