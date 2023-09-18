142e5a3a563eb2260efa80555ce36e2d30cc16b3
hanueleee <ezzeezz@naver.com>
2023-09-18 23:03:30 +0900

    feat: DispatcherServlet에 List<HandlerAdapter> 추가

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -4,20 +4,35 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webmvc.org.springframework.web.servlet.view.JspView;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.mvc.AnnotationHandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.HandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
     private static final List<HandlerMapping> handlerMappings;
+    private static final List<HandlerAdapter> handlerAdapters;
 
     static {
         handlerMappings = new ArrayList<>();
         handlerMappings.add(new ManualHandlerMapping());
         handlerMappings.add(new AnnotationHandlerMapping());
+
+        handlerAdapters = new ArrayList<>();
+        handlerAdapters.add(new ManualHandlerAdapter());
+        handlerAdapters.add(new AnnotationHandlerAdapter());
     }
 
     public DispatcherServlet() {
