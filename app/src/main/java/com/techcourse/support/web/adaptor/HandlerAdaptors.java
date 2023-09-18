eaf9374efa31498aaf36f9ec3363ccb61787e21e
cpot5620 <cpot5620@gmail.com>
2023-09-18 13:03:43 +0900

    feat: HandlerAdaptor 일급 컬렉션 구현

---
+++ app/src/main/java/com/techcourse/support/web/adaptor/HandlerAdaptors.java
@@ -0,0 +1,27 @@
+package com.techcourse.support.web.adaptor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdaptor;
+
+public class HandlerAdaptors {
+
+    private final List<HandlerAdaptor> adaptors = new ArrayList<>();
+
+    public void initialize(){
+        adaptors.add(new AnnotationHandlerAdaptor());
+        adaptors.add(new ManualHandlerAdaptor());
+    }
+
+    public HandlerAdaptor getAdaptor(Object handler) {
+        for (HandlerAdaptor handlerAdaptor : adaptors) {
+            if (handlerAdaptor.supports(handler)) {
+                return handlerAdaptor;
+            }
+        }
+
+        throw new NoSuchElementException("처리할 수 있는 핸들러가 존재하지 않습니다.");
+    }
+
+}
