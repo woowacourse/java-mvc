a06c3af207426054e3b06db2f0a553f4608b3854
ingpyo <juninpyo726@naver.com>
2023-09-19 00:40:37 +0900

    feat: 찾은 controller의 메소드를 실행시켜주는 HandlerAdapter 정의

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -6,33 +6,54 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.RequestMappingHandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.SimpleControllerHandlerAdapter;
 import webmvc.org.springframework.web.servlet.view.JspView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private List<HandlerMapping> handlerMappings;
+    private List<HandlerAdapter> handlerAdapters;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        handlerMappings = new ArrayList<>();
+        handlerMappings.add(new ManualHandlerMapping());
+        handlerMappings.add(new AnnotationHandlerMapping());
+        handlerMappings.forEach(HandlerMapping::initialize);
+
+        handlerAdapters = new ArrayList<>();
+        handlerAdapters.add(new SimpleControllerHandlerAdapter());
+        handlerAdapters.add(new RequestMappingHandlerAdapter());
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
+        final Object handler = handlerMappings.stream()
+                .map(mapping -> mapping.getHandler(request))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않은 요정입니다."));
+        final HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                .filter(Adapter -> Adapter.supports(handler))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않은 요정입니다."));
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            final ModelAndView handle = handlerAdapter.handle(request, response, handler);
+            move(handle.getViewName(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
