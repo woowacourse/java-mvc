1ec558f75c844fbc381f49410436e50779f02564
hong-sile <gurwns9325@naver.com>
2023-09-18 13:19:54 +0900

    refactor: object를 인스턴스로 선언하도록 리팩터링

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -7,15 +7,22 @@ import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private static final String OBJECT_NOT_INITIALIZE_EXCEPTION_MESSAGE = "객체가 초기화되지 않습니다.";
+
     private final Method method;
+    private final Object object;
 
     public HandlerExecution(final Method method) {
         this.method = method;
+        try {
+            this.object = method.getDeclaringClass().getConstructor().newInstance();
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(OBJECT_NOT_INITIALIZE_EXCEPTION_MESSAGE);
+        }
     }
 
     final ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response)
         throws Exception {
-        final Object object = method.getDeclaringClass().getConstructor().newInstance();
         return (ModelAndView) method.invoke(object, request, response);
     }
 }
