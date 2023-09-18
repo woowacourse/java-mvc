493110484df4bbdc0d5bac3b4a1bf7b84148ce3a
yoondgu <doyoungwork@gmail.com>
2023-09-19 02:47:54 +0900

    feat: JspView 동작하도록 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
@@ -10,22 +10,27 @@ import java.util.Map;
 
 public class JspView implements View {
 
-    private static final Logger log = LoggerFactory.getLogger(JspView.class);
-
     public static final String REDIRECT_PREFIX = "redirect:";
+    private static final Logger log = LoggerFactory.getLogger(JspView.class);
+    private final String viewName;
 
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
-
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        final var requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
