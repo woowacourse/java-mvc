87aa6d0a869db5bf60c95cf5de0c21eb9ab361e6
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-18 20:35:55 +0900

    refactor: handler의 인스턴스를 주입 받도록 수정

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -3,19 +3,21 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    private final Class<?> clazz;
+    private final Object instance;
     private final Method method;
 
-    public HandlerExecution(final Class<?> clazz, final Method method) {
-        this.clazz = clazz;
+    public HandlerExecution(final Object instance, final Method method) {
+        this.instance = instance;
         this.method = method;
     }
 
-    public Object handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return method.invoke(clazz.newInstance(), request, response);
+    public Object handle(final HttpServletRequest request, final HttpServletResponse response)
+            throws InvocationTargetException, IllegalAccessException {
+        return method.invoke(instance, request, response);
     }
 }
