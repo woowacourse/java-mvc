8e5f62d4f27a461cb036c83d367a4eba701451fd
hong-sile <gurwns9325@naver.com>
2023-09-19 10:41:38 +0900

    feat: 404를 반환해주는 NotFoundHandler 추가

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/NotFoundHandler.java
@@ -0,0 +1,17 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
+
+public class NotFoundHandler implements Handler {
+
+    private static final String NOT_FOUND_VIEW_NAME = "redirect:/404.jsp";
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request,
+                               final HttpServletResponse response) {
+        return new ModelAndView(new JspView(NOT_FOUND_VIEW_NAME));
+    }
+}
