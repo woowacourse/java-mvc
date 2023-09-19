add0eaf53c10d5bb36375a812bcc62aa26ee99fe
apptie <appti@naver.com>
2023-09-20 00:45:34 +0900

    refactor: 필드 네이밍 변경

--- app/src/main/java/com/techcourse/support/web/mapping/ManualHandlerMappingWrapper.java
+++ app/src/main/java/com/techcourse/support/web/mapping/ManualHandlerMappingWrapper.java
@@ -5,15 +5,15 @@ import webmvc.org.springframework.web.servlet.mvc.HandlerMapping;
 
 public class ManualHandlerMappingWrapper implements HandlerMapping {
 
-    private final ManualHandlerMapping mappings = new ManualHandlerMapping();
+    private final ManualHandlerMapping mapping = new ManualHandlerMapping();
 
     @Override
     public void initialize() {
-        mappings.initialize();
+        mapping.initialize();
     }
 
     @Override
     public Object getHandler(final HttpServletRequest request) {
-        return mappings.getHandler(request.getRequestURI());
+        return mapping.getHandler(request.getRequestURI());
     }
 }
