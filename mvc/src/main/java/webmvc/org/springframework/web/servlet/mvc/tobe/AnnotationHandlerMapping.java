d0aa57f2ef92af404eab53246aff224682df11b5
rawfishthelgh <rg970604@naver.com>
2023-09-19 21:32:13 +0900

    refactor : 인스턴스화시 예외처리 런타임으로 변환

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +25,7 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public void initialize() {
         Reflections reflections = new Reflections(basePackage);
         Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
         for (Class<?> controller : controllers) {
@@ -36,7 +35,12 @@ public class AnnotationHandlerMapping {
                     RequestMapping annotation = method.getAnnotation(RequestMapping.class);
                     String url = annotation.value();
                     for (RequestMethod requestMethod : annotation.method()) {
-                        Object instance = controller.getDeclaredConstructor().newInstance();
+                        Object instance = null;
+                        try {
+                            instance = controller.getDeclaredConstructor().newInstance();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
                         HandlerKey handlerKey = new HandlerKey(url, requestMethod);
                         HandlerExecution handlerExecution = new HandlerExecution(instance, method);
                         handlerExecutions.put(handlerKey, handlerExecution);
