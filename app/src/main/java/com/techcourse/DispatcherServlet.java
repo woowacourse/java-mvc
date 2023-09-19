f5a5a90c6c6b2139fbf695e239c6e8389bcd154e
yenawee <yaena0319@naver.com>
2023-09-19 13:33:11 +0900

    refactor: HandlerAdapters, HandlerMappings 일급 컬렉션 생성

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -7,24 +7,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webmvc.org.springframework.web.servlet.mvc.handleradapter.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.handleradapter.HandlerAdapters;
 import webmvc.org.springframework.web.servlet.mvc.handlermapping.HandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.handlermapping.HandlerMappings;
 import webmvc.org.springframework.web.servlet.view.JspView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> handlerMappings;
-    private final List<HandlerAdapter> handlerAdapters;
+    private final HandlerMappings handlerMappings;
+    private final HandlerAdapters handlerAdapters;
 
     public DispatcherServlet() {
-        handlerMappings = new ArrayList<>();
-        handlerAdapters = new ArrayList<>();
+        handlerMappings = new HandlerMappings();
+        handlerAdapters = new HandlerAdapters();
     }
 
     @Override
@@ -32,7 +30,6 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     public void addHandlerMapping(HandlerMapping handlerMapping) {
-        handlerMapping.initialize();
         handlerMappings.add(handlerMapping);
     }
 
@@ -46,8 +43,8 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final Object handler = getHandler(request);
-            final HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
+            final Object handler = handlerMappings.getHandler(request);
+            final HandlerAdapter handlerAdapter = handlerAdapters.getHandlerAdapter(handler);
             final var modelAndView = handlerAdapter.handle(request, response, handler);
 
             move(modelAndView.getViewName(), request, response);
@@ -56,24 +53,6 @@ public class DispatcherServlet extends HttpServlet {
             throw new ServletException(e.getMessage());
         }
     }
-
-    private Object getHandler(HttpServletRequest request) {
-        log.info("requestURI = " + request.getRequestURI());
-
-        return handlerMappings.stream()
-                .map(handlerMapping -> handlerMapping.getHandler(request))
-                .filter(Objects::nonNull)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("요청에 맞는 Handler 를 찾지 못하였습니다"));
-    }
-
-    private HandlerAdapter getHandlerAdapter(Object handler) {
-        return handlerAdapters.stream()
-                .filter(handlerAdapter -> handlerAdapter.supports(handler))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("요청에 맞는 HandlerAdapter 를 찾지 못하였습니다"));
-    }
-
 
     private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/handlermapping/HandlerMappings.java
@@ -0,0 +1,35 @@
+package webmvc.org.springframework.web.servlet.mvc.handlermapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HandlerMappings {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerMappings.class);
+
+    private final List<HandlerMapping> handlerMappings;
+
+    public HandlerMappings() {
+        handlerMappings = new ArrayList<>();
+    }
+
+    public void add(HandlerMapping handlerMapping) {
+        handlerMapping.initialize();
+        handlerMappings.add(handlerMapping);
+    }
+
+    public Object getHandler(HttpServletRequest request) {
+        log.info("requestURI = " + request.getRequestURI());
+
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("요청에 맞는 Handler 를 찾지 못하였습니다"));
+    }
+}
