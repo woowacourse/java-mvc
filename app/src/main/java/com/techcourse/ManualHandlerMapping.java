bb266577bd429782ef54d894ff8191eaca9f0c41
BGuga <tmdwns9913@gmail.com>
2023-09-19 13:18:39 +0900

    feat: registerController 들을 annotaion 기반으로 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -3,8 +3,6 @@ package com.techcourse;
 import com.techcourse.controller.regacy.LoginController;
 import com.techcourse.controller.regacy.LoginViewController;
 import com.techcourse.controller.regacy.LogoutController;
-import com.techcourse.controller.regacy.RegisterController;
-import com.techcourse.controller.regacy.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,8 +25,6 @@ public class ManualHandlerMapping implements HandlerMapping {
         controllers.put("/login", new LoginController());
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
