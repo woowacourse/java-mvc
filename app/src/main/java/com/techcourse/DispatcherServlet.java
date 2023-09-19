5d0b1de37edad1bc13b83a308d32b27079da2f95
BGuga <tmdwns9913@gmail.com>
2023-09-19 12:48:09 +0900

    feat: handler, adapter registry 생성 및 DispathcerServlet 이 해당 객체를 사용하도록 변경

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -4,8 +4,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.asis.AnnotationControllerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.asis.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.asis.HandlerAdapterRegistry;
+import webmvc.org.springframework.web.servlet.mvc.asis.InterfaceControllerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMapping;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMappingRegistry;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
@@ -13,39 +23,67 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private transient HandlerMappingRegistry handlerMappingRegistry;
+    private transient HandlerAdapterRegistry handlerAdapterRegistry;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        this.handlerMappingRegistry = getHandlerMappingRegistry();
+        this.handlerAdapterRegistry = getHandlerAdapterRegistry();
+    }
+
+    private HandlerMappingRegistry getHandlerMappingRegistry() {
+        HandlerMappingRegistry registry = new HandlerMappingRegistry();
+        registry.addHandlerMapping(
+            getInitializedHandlerMapping(new ManualHandlerMapping()));
+        registry.addHandlerMapping(
+            getInitializedHandlerMapping(new AnnotationHandlerMapping("com")));
+        return registry;
+    }
+
+    private HandlerAdapterRegistry getHandlerAdapterRegistry() {
+        HandlerAdapterRegistry registry = new HandlerAdapterRegistry();
+        registry.addHandlerAdapter(new AnnotationControllerAdapter());
+        registry.addHandlerAdapter(new InterfaceControllerAdapter());
+        return registry;
+    }
+
+    private HandlerMapping getInitializedHandlerMapping(HandlerMapping handlerMapping) {
+        handlerMapping.initialize();
+        return handlerMapping;
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+        throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
-        } catch (Throwable e) {
+            Optional<Object> registryHandler = handlerMappingRegistry.getHandler(request);
+            if (registryHandler.isEmpty()) {
+                handlerNotFound(request, response);
+                return;
+            }
+            Object handler = registryHandler.get();
+            HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler)
+                .orElseThrow(() -> new IllegalStateException("핸드러를 처리할 어댑터가 없습니다."));
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+            modelAndView.getView().render(modelAndView.getModel(), request, response);
+        } catch (Exception e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+    private void handlerNotFound(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            JspView jspView = new JspView("/404.jsp");
+            jspView.render(Collections.emptyMap(), request, response);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("404 페이지 출력 실패", e);
         }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
