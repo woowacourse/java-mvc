2c1656203ca7e057dd81f4c69d6cef356d38e417
apptie <appti@naver.com>
2023-09-20 00:44:44 +0900

    fix: 요청의 Accept가 */*일 경우 JsonViewResolver가 처리할 수 있도록 변경

--- app/src/main/java/com/techcourse/support/web/resolver/JsonViewResolver.java
+++ app/src/main/java/com/techcourse/support/web/resolver/JsonViewResolver.java
@@ -9,12 +9,17 @@ public class JsonViewResolver implements ViewResolver {
 
     private static final String ACCEPT_HEADER_KET = "Accept";
     private static final String APPLICATION_JSON_VALUE = "application/json";
+    private static final String DEFAULT_ACCEPT_VALUE = "*/*";
 
     @Override
     public boolean supports(final HttpServletRequest request, final String ignored) {
         final String acceptHeaderValue = request.getHeader(ACCEPT_HEADER_KET);
 
-        return acceptHeaderValue != null && acceptHeaderValue.contains(APPLICATION_JSON_VALUE);
+        if (acceptHeaderValue == null) {
+            return false;
+        }
+
+        return DEFAULT_ACCEPT_VALUE.equals(acceptHeaderValue) || acceptHeaderValue.contains(APPLICATION_JSON_VALUE);
     }
 
     @Override
