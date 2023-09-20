02f12c15d4dd5c8097f37729505963f9cf7790bf
jjongwa <troas96@naver.com>
2023-09-21 00:56:22 +0900

    refactor: render 작업 view 쪽으로 이동

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -4,29 +4,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.View;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdapter;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdapters;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMappings;
-import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final HandlerMappings handlerMappings;
+    private final HandlerMappings handlerMappings = new HandlerMappings();
 
-    private final HandlerAdapters handlerAdapters;
-
-
-    public DispatcherServlet() {
-        this.handlerMappings = new HandlerMappings();
-        this.handlerAdapters = new HandlerAdapters();
-    }
+    private final HandlerAdapters handlerAdapters = new HandlerAdapters();
 
     @Override
     public void init() {
@@ -45,25 +40,13 @@ public class DispatcherServlet extends HttpServlet {
             final Object handler = handlerMappings.findHandler(request);
             final HandlerAdapter adapter = handlerAdapters.findAdapter(handler);
             final ModelAndView modelAndView = adapter.handle(handler, request, response);
-            move(modelAndView, request, response);
+
+            final View view = modelAndView.getView();
+            final Map<String, Object> model = modelAndView.getModel();
+            view.render(model, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
-    }
-
-    private void move(
-            final ModelAndView modelAndView,
-            final HttpServletRequest request,
-            final HttpServletResponse response
-    ) throws Exception {
-        final String viewName = modelAndView.getViewName();
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
