820bd9bffba880e370c977acc07ee258d8b6af19
rawfishthelgh <rg970604@naver.com>
2023-09-20 23:52:58 +0900

    feat : 로그인, 회원가입 컨트롤러 어노테이션 기반으로 처리

--- app/src/main/java/com/techcourse/handlerMapper/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/handlerMapper/ManualHandlerMapping.java
@@ -1,10 +1,6 @@
 package com.techcourse.handlerMapper;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,11 +19,7 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
