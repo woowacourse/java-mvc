17d3bc893c1301e844a7890cc80551912ec569a4
SproutMj <mjk54565@gmail.com>
2023-09-20 10:59:45 +0900

    feat: DispatcherServlet초기화

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -1,9 +1,12 @@
 package com.techcourse;
 
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecutionAdapter;
+import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 
 /**
  * Base class for {@link WebApplicationInitializer}
@@ -13,13 +16,20 @@ public class DispatcherServletInitializer implements WebApplicationInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServletInitializer.class);
 
+    private static final String BASE_PACKAGE = "com.techcourse";
     private static final String DEFAULT_SERVLET_NAME = "dispatcher";
 
     @Override
     public void onStartup(final ServletContext servletContext) {
-        final var dispatcherServlet = new DispatcherServlet();
+        final DispatcherServlet dispatcherServlet = new DispatcherServlet();
 
-        final var registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
+        dispatcherServlet.addHandlerMapper(new AnnotationHandlerMapping(BASE_PACKAGE));
+        dispatcherServlet.addHandlerMapper(new ManualHandlerMapping());
+        dispatcherServlet.addHandlerAdapter(new HandlerExecutionAdapter());
+        dispatcherServlet.addHandlerAdapter(new ManualHandlerAdapter());
+        dispatcherServlet.init();
+
+        final ServletRegistration.Dynamic registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
         if (registration == null) {
             throw new IllegalStateException("Failed to register servlet with name '" + DEFAULT_SERVLET_NAME + "'. " +
                     "Check if there is another servlet registered under the same name.");
