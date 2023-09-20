3f80ebd2495e351373fb8f1b26a6cd18cd4776e0
Combi153 <jcm04@naver.com>
2023-09-20 20:35:52 +0900

    [feat] ManualHandlerAdapter 기능 구현

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/ManualHandlerAdapter.java
@@ -0,0 +1,46 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.view.JspView;
+
+public class ManualHandlerAdapter implements HandlerAdapter {
+
+    private static final String HANDLER_METHOD = "execute";
+
+    @Override
+    public boolean supports(Object handler) {
+        return Controller.class.isAssignableFrom(handler.getClass());
+    }
+
+    @Override
+    public ModelAndView handle(Object handler, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Class<?> clazz = handler.getClass();
+            Object object = clazz.getDeclaredConstructor().newInstance();
+            Method method = clazz.getDeclaredMethod(
+                    HANDLER_METHOD,
+                    HttpServletRequest.class,
+                    HttpServletResponse.class
+            );
+            return invokeMethod(object, method, request, response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ModelAndView invokeMethod(
+            Object object,
+            Method method,
+            HttpServletRequest request,
+            HttpServletResponse response
+    )
+            throws Exception {
+        String viewName = (String) method.invoke(object, request, response);
+        JspView jspView = new JspView(viewName);
+        return new ModelAndView(jspView);
+    }
+}
