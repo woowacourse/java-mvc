0556589470efa07f3d40c55242c6b8f6f0f5ca37
헙크 <chocomilk8604@gmail.com>
2023-09-21 10:32:45 +0900

    [MVC 2단계] 헙크 미션 제출합니다 (#449)
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * [MVC 구현하기 - 1단계] 헙크(정현승) 미션 제출합니다. (#384)
    
    * 패키지 위치 변경 및 코드 정리
    
    * 서블릿 학습 테스트 코드 개선
    
    * feat: 학습 테스트 구현
    
    * test: Reflections 테스트 작성
    
    * feat: AnnotationHandlerMapping 초안
    
    * refactor: AnnotationHandlerMapping 수정
    
    * test: Servlet 테스트 작성
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>
    
    * feat: HandlerMapping 구현
    
    * feat: HandlerMappings와 Adapter 구현
    
    * feat: DispatcherServlet에 HandlerMappings, HandlerAdapters 적용
    
    * feat: JspView 구현
    
    * fix: HandlerExecution 수정
    
    * refactor: 패키지 구조 수정
    
    * refactor: 패키지 구조 수정
    
    * fix: DispatcherServlet 클래스의 ModelAndView의 model 반환 로직 수정
    
    * refactor: HandlerMappings 패키지 위치 수정
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerAdapters.java
@@ -0,0 +1,27 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class HandlerAdapters {
+    private final List<HandlerAdapter> adapters;
+
+    public HandlerAdapters() {
+        this.adapters = new ArrayList<>();
+    }
+
+    public void init() {
+        adapters.add(new AnnotationHandlerAdapter());
+        adapters.add(new ManualHandlerAdapter());
+    }
+
+    public Optional<HandlerAdapter> getAdapter(final Object handler) {
+        for (final HandlerAdapter handlerAdapter : adapters) {
+            if (handlerAdapter.supports(handler)) {
+                return Optional.of(handlerAdapter);
+            }
+        }
+        return Optional.empty();
+    }
+}
---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerMappings.java
@@ -0,0 +1,31 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class HandlerMappings {
+    private final List<HandlerMapping> mappings;
+
+    public HandlerMappings() {
+        this.mappings = new ArrayList<>();
+    }
+
+    public void init(final HandlerMapping... mappings) {
+        this.mappings.add(new AnnotationHandlerMapping());
+        this.mappings.addAll(List.of(mappings));
+    }
+
+    public Optional<Object> getHandler(final HttpServletRequest request) {
+        for (final HandlerMapping handlerMapping : mappings) {
+            final Object handler = handlerMapping.getHandler(request);
+            if (Objects.nonNull(handler)) {
+                return Optional.of(handler);
+            }
+        }
+        return Optional.empty();
+    }
+}
