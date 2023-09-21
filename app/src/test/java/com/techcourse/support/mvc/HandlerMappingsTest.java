aa41c4bb2f8511527ecccf552f1091f4233f916c
odo27 <odo27@naver.com>
2023-09-21 11:21:53 +0900

    test: 중복되는 given 로직 제거

--- app/src/test/java/com/techcourse/support/mvc/HandlerMappingsTest.java
+++ app/src/test/java/com/techcourse/support/mvc/HandlerMappingsTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
@@ -13,16 +14,23 @@ import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecution;
 
 class HandlerMappingsTest {
 
+    private HandlerMappings handlerMappings;
+    private HttpServletRequest request;
+
+    @BeforeEach
+    void setUp() {
+        handlerMappings = new HandlerMappings();
+        handlerMappings.addHandlerMapping(new ManualHandlerMapping());
+        handlerMappings.addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
+        handlerMappings.initialize();
+        request = mock(HttpServletRequest.class);
+    }
+
     @ParameterizedTest
     @CsvSource({"/logout", "/register/view", "/register"})
     void getHandlerFromManualHandlerMapping(String requestURI) {
         // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getRequestURI()).thenReturn(requestURI);
-        HandlerMappings handlerMappings = new HandlerMappings();
-        handlerMappings.addHandlerMapping(new ManualHandlerMapping());
-        handlerMappings.addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
-        handlerMappings.initialize();
 
         // when
         Object handler = handlerMappings.getHandler(request);
@@ -35,13 +43,8 @@ class HandlerMappingsTest {
     @CsvSource({"/login, POST", "/login/view, GET"})
     void getHandlerFromAnnotationHandlerMapping(String requestURI, String method) {
         // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getRequestURI()).thenReturn(requestURI);
         when(request.getMethod()).thenReturn(method);
-        HandlerMappings handlerMappings = new HandlerMappings();
-        handlerMappings.addHandlerMapping(new ManualHandlerMapping());
-        handlerMappings.addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
-        handlerMappings.initialize();
 
         // when
         Object handler = handlerMappings.getHandler(request);
