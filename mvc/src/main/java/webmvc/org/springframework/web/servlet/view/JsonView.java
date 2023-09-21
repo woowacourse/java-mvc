57be612e8f38f79e7dac41d08da302e91cf642b8
aak2075 <lks971003@gmail.com>
2023-09-22 02:21:13 +0900

    feat: JsonView 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.mvc.exception.CustomJsonProcessingException;
 
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final String body = getResponseBody(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
+    }
+
+    private String getResponseBody(final Map<String, ?> model) {
+        if (model.size() == 1) {
+            final var data = model.values().toArray()[0];
+            return String.valueOf(data);
+        }
+        try {
+            return objectMapper.writeValueAsString(model);
+        } catch (JsonProcessingException e) {
+            throw new CustomJsonProcessingException();
+        }
     }
 }
---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/view/JsonViewTest.java
@@ -0,0 +1,38 @@
+package webmvc.org.springframework.web.servlet.view;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+import web.org.springframework.http.MediaType;
+
+import java.io.PrintWriter;
+import java.net.http.HttpHeaders;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class JsonViewTest {
+
+    @Test
+    void render() throws Exception {
+        // given
+        final var jsonView = new JsonView();
+        final var model = Map.of("key", "value");
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+        final var writer = mock(PrintWriter.class);
+
+        when(response.getWriter()).thenReturn(writer);
+        doNothing().when(writer).write(anyString());
+
+        // when
+        jsonView.render(model, request, response);
+
+        // then
+        verify(response, times(1)).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        verify(writer, times(1)).write(anyString());
+    }
+}
