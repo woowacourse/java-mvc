c872df71813dec4f93f5acab900888a53bed3067
java-saeng <gusdn85554@naver.com>
2023-09-21 14:43:15 +0900

    feat : Json View 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,7 +1,9 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 import java.util.Map;
@@ -10,6 +12,18 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String body = objectMapper.writeValueAsString(model);
+
+        if (model.size() == 1) {
+            response.setContentType("text/html");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(body);
+            return;
+        }
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
     }
 
     @Override
