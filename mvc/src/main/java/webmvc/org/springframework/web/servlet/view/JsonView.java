f35963a908b78dccc19f74cfa3c6e04b76544971
최준호 <junho5336@gmail.com>
2023-09-21 20:55:49 +0900

    feat: JsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -7,14 +7,26 @@ import webmvc.org.springframework.web.servlet.View;
 
 import java.util.Map;
 
+import static web.org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 public class JsonView implements View {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
+        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
+
+        if (model == null || model.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        if (model.size() == 1) {
+            response.getWriter().write(model.values().iterator().next().toString());
+            return;
+        }
+
         response.getWriter().write(objectMapper.writeValueAsString(model));
     }
 }
