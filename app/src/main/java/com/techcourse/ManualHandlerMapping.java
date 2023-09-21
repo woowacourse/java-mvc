062535b2941012a96100c60294b90d5492507f9e
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-21 14:16:07 +0900

    refactor: App 모듈 내의 모든 Controller 어노테이션 기반으로 수정

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,8 +1,5 @@
 package com.techcourse;
 
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
-import com.techcourse.controller.LogoutController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,9 +18,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     @Override
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/login", new LoginController());
-        controllers.put("/login/view", new LoginViewController());
-        controllers.put("/logout", new LogoutController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -2,36 +2,69 @@ package com.techcourse.controller;
 
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+    public static final String INDEX_JSP = "redirect:/index.jsp";
+    public static final String REDIRECT_401_JSP = "redirect:/401.jsp";
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView showLoginPage(HttpServletRequest req, HttpServletResponse res) {
+
+        String viewName = UserSession.getUserFrom(req.getSession())
+                .map(user -> {
+                    log.info("logged in {}", user.getAccount());
+                    return INDEX_JSP;
+                })
+                .orElse("/login.jsp");
+
+        return new ModelAndView(new JspView(viewName));
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse res) {
+        if (UserSession.isLoggedIn(request.getSession())) {
+            return new ModelAndView(new JspView(INDEX_JSP));
         }
 
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+        String viewName = InMemoryUserRepository.findByAccount(request.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
-                    return login(req, user);
+                    return validatePassword(request, user);
                 })
-                .orElse("redirect:/401.jsp");
+                .orElse(REDIRECT_401_JSP);
+
+        return new ModelAndView(new JspView(viewName));
     }
 
-    private String login(final HttpServletRequest request, final User user) {
-        if (user.checkPassword(request.getParameter("password"))) {
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout(HttpServletRequest req, HttpServletResponse res) {
+        final var session = req.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+
+        return new ModelAndView(new JspView("redirect:/"));
+    }
+
+    private String validatePassword(final HttpServletRequest request, final User user) {
+        String password = request.getParameter("password");
+        if (user.checkPassword(password)) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return "redirect:/index.jsp";
+            return INDEX_JSP;
         }
-        return "redirect:/401.jsp";
+        return REDIRECT_401_JSP;
     }
+
 }
