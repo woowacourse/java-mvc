28ed976b9613415ac264e98115aef6eae5e89b3b
chaewon121 <cw121@naver.com>
2023-09-22 19:32:17 +0900

    feat: 어노테이션 함수 추가

--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -2,12 +2,22 @@ package com.techcourse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class RegisterViewController implements Controller {
 
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public String execute(final HttpServletRequest req, final HttpServletResponse res)  {
         return "/register.jsp";
+    }
+
+    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
+    public ModelAndView loginView(HttpServletRequest req, HttpServletResponse res) {
+        String path = execute(req, res);
+        return new ModelAndView(new JspView(path));
     }
 }
