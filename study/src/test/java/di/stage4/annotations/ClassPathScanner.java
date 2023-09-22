94d764ed4ac7eb8f7ac2fadd95530a087bbe9be5
kiarakim <kiarakim413@gmail.com>
2023-09-22 11:57:57 +0900

    feat(학습테스트 4단계):  @Inject를 붙인 필드만 필터하고 타입에 맞는 객체 찾아 대입 기능 구현

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,26 @@
 package di.stage4.annotations;
 
+import org.reflections.Reflections;
+
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassPathScanner {
 
+    private static Set<Class<?>> classes = new HashSet<>();
+
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        addServiceClasses(reflections);
+        addRepositoryClasses(reflections);
+        return classes;
+    }
+
+    private static void addServiceClasses(Reflections reflections) {
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+    }
+
+    private static void addRepositoryClasses(Reflections reflections) {
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
     }
 }
