435256ca3034777959f731e02d372427865931c9
chaewon121 <cw121@naver.com>
2023-09-22 19:40:36 +0900

    feat: 핸들러 어뎁터 인터페이스와 구현체 구현

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/asis/ControllerHandlerAdapter.java
@@ -0,0 +1,24 @@
+package webmvc.org.springframework.web.servlet.mvc.asis;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.mvc.HandlerAdapter;
+import webmvc.org.springframework.web.servlet.view.JspView;
+
+public class ControllerHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request,
+                               final HttpServletResponse response,
+                               final Object handler) throws Exception {
+        final Controller controller = (Controller) handler;
+        return new ModelAndView(new JspView(controller.execute(request, response)));
+    }
+
+}
