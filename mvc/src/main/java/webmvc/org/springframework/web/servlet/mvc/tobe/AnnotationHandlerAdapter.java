a58e79d5203dbb60bdd92319f91a801f34676238
aiaiaiai1 <cvc100326@naver.com>
2023-09-23 01:49:05 +0900

    feat: HandlerAdapter 추가

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerAdapter.java
@@ -0,0 +1,22 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+public class AnnotationHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler
+    ) throws Exception {
+        return ((HandlerExecution) handler).handle(request, response);
+    }
+}
