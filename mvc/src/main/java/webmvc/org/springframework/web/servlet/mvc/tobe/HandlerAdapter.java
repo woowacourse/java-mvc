984d539f4c32d1dfbf201b03f286b24b646e2c34
hgo641 <hgo641@gmail.com>
2023-09-22 19:23:00 +0900

    feat: 핸들러 어댑터 구현

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerAdapter.java
@@ -0,0 +1,13 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+
+public interface HandlerAdapter {
+
+    boolean isSupportable(final Object handler);
+
+    ModelAndView handle(final Object handler, final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception;
+}
