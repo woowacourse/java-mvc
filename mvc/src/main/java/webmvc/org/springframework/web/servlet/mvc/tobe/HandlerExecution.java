9ed926ce1e207b79cb9ff0fdc299e875b6a53667
BackFoxx <backfoxxx@gmail.com>
2023-09-22 15:00:55 +0900

    fix : 2단계 리팩토링 - getClass 대신 getDeclaringClass를 사용하여 정확한 클래스를 가져오도록 변경

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerExecution.java
@@ -3,7 +3,6 @@ package webmvc.org.springframework.web.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
-import webmvc.org.springframework.web.servlet.ModelAndView;
 
 public class HandlerExecution {
 
@@ -13,7 +12,7 @@ public class HandlerExecution {
         this.method = method;
     }
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(method.getClass().getDeclaredConstructor().newInstance(), request, response);
+    public Object handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return method.invoke(method.getDeclaringClass().getDeclaredConstructor().newInstance(), request, response);
     }
 }
