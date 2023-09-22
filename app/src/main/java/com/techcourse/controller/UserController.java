ec1c56bbcb392ae9e024e8c5af7417e26bf1a81e
ingpyo <juninpyo726@naver.com>
2023-09-22 17:00:36 +0900

    feat: JsonView render 구현

---
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.techcourse.controller;
+
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JsonView;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
+    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
+        final String account = request.getParameter("account");
+
+        final ModelAndView modelAndView = new ModelAndView(new JsonView());
+        final User user = InMemoryUserRepository.findByAccount(account)
+                .orElseThrow();
+
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+}
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,21 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import webmvc.org.springframework.web.servlet.View;
 
 import java.util.Map;
 
+import static web.org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 public class JsonView implements View {
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final String body = objectMapper.writeValueAsString(model);
+        response.getWriter().write(body);
+        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
     }
 }
