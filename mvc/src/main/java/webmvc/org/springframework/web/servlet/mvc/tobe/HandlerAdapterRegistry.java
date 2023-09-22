eecb1dae8b4349c53d0cf60a7491fd4968dc5745
hgo641 <hgo641@gmail.com>
2023-09-22 19:23:00 +0900

    feat: 핸들러 어댑터 구현

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/HandlerAdapterRegistry.java
@@ -0,0 +1,25 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerAdapterRegistry {
+
+    private final List<HandlerAdapter> handlerAdapters;
+
+    public HandlerAdapterRegistry() {
+        this.handlerAdapters = new ArrayList<>();
+        handlerAdapters.add(new AnnotationHandlerAdapter());
+    }
+
+    public void addHandlerAdapter(final HandlerAdapter handlerAdapter) {
+        this.handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(final Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.isSupportable(handler))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("핸들러를 처리할 수 있는 어댑터가 존재하지 않습니다."));
+    }
+}
