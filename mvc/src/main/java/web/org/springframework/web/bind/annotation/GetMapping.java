055186e52abb4eb7f29c077321147e8de8f4aa33
hyena <goatsfish@gmail.com>
2023-09-22 17:32:26 +0900

    feat: GetMapping, PostMapping 어노테이션 구현

--- mvc/src/main/java/web/org/springframework/web/bind/annotation/GetMapping.java
+++ mvc/src/main/java/web/org/springframework/web/bind/annotation/GetMapping.java
@@ -1,16 +1,13 @@
 package web.org.springframework.web.bind.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
-public @interface RequestMapping {
+@RequestMapping(method = RequestMethod.GET)
+public @interface GetMapping {
     String value() default "";
-
-    RequestMethod method();
 }
