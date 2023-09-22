0f5409f21733a49cebc295f37201454e21229b24
sosow0212 <sosow0212@naver.com>
2023-09-22 16:37:34 +0900

    refactor: ManualHandlerAdapter가 ModelAndView를 반환하도록 변경

--- mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/handler/FakeHandlerAdapter.java
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/handler/FakeHandlerAdapter.java
@@ -2,12 +2,13 @@ package webmvc.org.springframework.web.servlet.mvc.tobe.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.mvc.tobe.handler.adapter.HandlerAdapter;
 
 public class FakeHandlerAdapter implements HandlerAdapter {
 
     @Override
-    public Object execute(final HttpServletRequest req, final HttpServletResponse res, final Object handler) throws Exception {
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res, final Object handler) throws Exception {
         return null;
     }
 
