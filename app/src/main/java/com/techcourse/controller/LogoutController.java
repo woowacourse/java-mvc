a6fb942a8dae9774a906bc21e176c19a584539b1
seokjin8678 <seokjin8678@gmail.com>
2023-09-23 22:16:02 +0900

    refactor: Legacy MVC 제거

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -6,22 +6,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 import webmvc.org.springframework.web.servlet.ModelAndView;
-import webmvc.org.springframework.web.servlet.mvc.asis.MyController;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 @Controller
-public class LogoutController implements MyController {
-
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) {
-        final var session = req.getSession();
-        session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
-    }
+public class LogoutController {
 
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
     public ModelAndView logout(HttpServletRequest req, HttpServletResponse res) {
-        String path = execute(req, res);
-        return new ModelAndView(new JspView(path));
+        final var session = req.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return new ModelAndView(new JspView("redirect:/"));
     }
 }
--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -8,26 +8,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
 import webmvc.org.springframework.web.servlet.ModelAndView;
-import webmvc.org.springframework.web.servlet.mvc.asis.MyController;
+import webmvc.org.springframework.web.servlet.View;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 @Controller
-public class RegisterController implements MyController {
-
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) {
-        final var user = new User(2,
-                req.getParameter("account"),
-                req.getParameter("password"),
-                req.getParameter("email"));
-        InMemoryUserRepository.save(user);
-
-        return "redirect:/index.jsp";
-    }
+public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
-        String path = execute(req, res);
-        return new ModelAndView(new JspView(path));
+        return new ModelAndView(execute(req, res));
+    }
+
+    private View execute(HttpServletRequest req, HttpServletResponse res) {
+        User user = new User(2,
+            req.getParameter("account"),
+            req.getParameter("password"),
+            req.getParameter("email"));
+        InMemoryUserRepository.save(user);
+
+        return new JspView("redirect:/index.jsp");
     }
 }
