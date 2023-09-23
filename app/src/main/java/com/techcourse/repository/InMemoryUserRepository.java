f1edb185cabf60b66a99af27dceeb5165c102150
apptie <appti@naver.com>
2023-09-23 15:06:37 +0900

    refactor: User, UserRepository가 고유한 id를 가지도록 변경

--- app/src/main/java/com/techcourse/repository/InMemoryUserRepository.java
+++ app/src/main/java/com/techcourse/repository/InMemoryUserRepository.java
@@ -1,18 +1,21 @@
 package com.techcourse.repository;
 
 import com.techcourse.domain.User;
-
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryUserRepository {
 
+    private static final AtomicLong userId = new AtomicLong(0);
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final var user = new User(1, "gugu", "password", "hkkang@woowahan.com");
-        database.put(user.getAccount(), user);
+        final User registerUser = new User("gugu", "password", "hkkang@woowahan.com");
+        final User persistUser = new User(userId.incrementAndGet(), registerUser);
+
+        database.put(registerUser.getAccount(), persistUser);
     }
 
     public static void save(User user) {
@@ -23,5 +26,6 @@ public class InMemoryUserRepository {
         return Optional.ofNullable(database.get(account));
     }
 
-    private InMemoryUserRepository() {}
+    private InMemoryUserRepository() {
+    }
 }
