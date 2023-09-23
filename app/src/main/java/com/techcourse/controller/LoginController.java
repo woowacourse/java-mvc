59da5746a572d4582caa28ddd0b915452b266a31
sosow0212 <sosow0212@naver.com>
2023-09-23 17:12:07 +0900

    refactor: mav로 반환하도록 변경

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
 @Controller
 public class LoginController {
@@ -16,19 +18,19 @@ public class LoginController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(value = "/login/view", method = RequestMethod.GET)
-    public String getLoginView(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView getLoginView(final HttpServletRequest req, final HttpServletResponse res) {
         return UserSession.getUserFrom(req.getSession())
                 .map(user -> {
                     log.info("logged in {}", user.getAccount());
-                    return "redirect:/index.jsp";
+                    return new ModelAndView(new JspView("redirect:/index.jsp"));
                 })
-                .orElse("/login.jsp");
+                .orElse(new ModelAndView(new JspView("/login.jsp")));
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String processLogin(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView processLogin(final HttpServletRequest req, final HttpServletResponse res) {
         if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+            return new ModelAndView(new JspView("redirect:/index.jsp"));
         }
 
         return InMemoryUserRepository.findByAccount(req.getParameter("account"))
@@ -36,22 +38,23 @@ public class LoginController {
                     log.info("User : {}", user);
                     return login(req, user);
                 })
-                .orElse("redirect:/401.jsp");
+                .orElse(new ModelAndView(new JspView("redirect:/401.jsp")));
     }
 
-    private String login(final HttpServletRequest request, final User user) {
+    private ModelAndView login(final HttpServletRequest request, final User user) {
         if (user.checkPassword(request.getParameter("password"))) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return "redirect:/index.jsp";
+            return new ModelAndView(new JspView("redirect:/index.jsp"));
         }
-        return "redirect:/401.jsp";
+
+        return new ModelAndView(new JspView("redirect:/401.jsp"));
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logout(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView logout(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return new ModelAndView(new JspView("redirect:/"));
     }
 }
