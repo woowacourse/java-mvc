07f3419e46fb9ac39a3c08194523570fb09bdecf
yujamint <woawoa0513@gmail.com>
2023-09-23 21:05:26 +0900

    refactor: Controller.execute() 반환 타입 수정

--- app/src/main/java/com/techcourse/controller/legacy/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/legacy/LogoutController.java
@@ -3,14 +3,16 @@ package com.techcourse.controller.legacy;
 import com.techcourse.controller.UserSession;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class LogoutController implements Controller {
 
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return new ModelAndView(new JspView("redirect:/"));
     }
 }
--- app/src/main/java/com/techcourse/controller/legacy/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/legacy/RegisterViewController.java
@@ -2,12 +2,14 @@ package com.techcourse.controller.legacy;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
 public class RegisterViewController implements Controller {
 
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        return "/register.jsp";
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) {
+        return new ModelAndView(new JspView("/register.jsp"));
     }
 }
