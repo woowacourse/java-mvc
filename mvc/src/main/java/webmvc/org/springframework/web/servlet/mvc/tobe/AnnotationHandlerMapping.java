07d493330fca207f73696ff41a45f6667ce03a70
kwonyj1022 <kwonyj1022@naver.com>
2023-09-23 20:35:59 +0900

    feat: ControllerScanner 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,17 +1,17 @@
 package webmvc.org.springframework.web.servlet.mvc.tobe;
 
-import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
-import org.reflections.Reflections;
+import org.reflections.ReflectionUtils;
+import org.reflections.util.ReflectionUtilsPredicates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.bind.annotation.RequestMapping;
 import web.org.springframework.web.bind.annotation.RequestMethod;
-import webmvc.org.springframework.web.servlet.mvc.tobe.exception.InvalidClassException;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,57 +28,39 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        for (final Object base : basePackage) {
-            initializeHandlerInPackage(base);
+        final ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        final Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        final Set<Method> methods = findMethodsWithAnnotation(controllers.keySet(), RequestMapping.class);
+        for (final Method method : methods) {
+            initializeRequestMappingHandlerExecutions(controllers, method);
         }
 
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void initializeHandlerInPackage(final Object base) {
-        final Reflections reflections = new Reflections(base);
-        final Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
-        for (final Class<?> clazz : classes) {
-            initializeHandlerInClass(clazz);
-        }
-    }
-
-    private void initializeHandlerInClass(final Class<?> clazz) {
-        final Method[] methods = clazz.getDeclaredMethods();
-        for (final Method method : methods) {
-            initializeHandlerByMethod(clazz, method);
-        }
-    }
-
-    private void initializeHandlerByMethod(final Class<?> clazz, final Method method) {
-        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-        if (requestMapping != null) {
-            initializeRequestMappingHandler(clazz, method, requestMapping);
-        }
-    }
-
-    private void initializeRequestMappingHandler(
-            final Class<?> clazz,
-            final Method method,
-            final RequestMapping requestMapping
+    private Set<Method> findMethodsWithAnnotation(
+            final Set<Class<?>> classes,
+            final Class<? extends Annotation> annotation
     ) {
+        final Set<Method> methods = new HashSet<>();
+        for (final Class<?> clazz : classes) {
+            methods.addAll(ReflectionUtils.getAllMethods(clazz, ReflectionUtilsPredicates.withAnnotation(annotation)));
+        }
+
+        return methods;
+    }
+
+    private void initializeRequestMappingHandlerExecutions(final Map<Class<?>, Object> controllers, final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
         final String url = requestMapping.value();
         final RequestMethod[] requestMethods = requestMapping.method();
+
         for (final RequestMethod requestMethod : requestMethods) {
             final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-            final HandlerExecution handlerExecution = new HandlerExecution(getClassInstance(clazz), method);
-            handlerExecutions.put(handlerKey, handlerExecution);
-        }
-    }
+            final Object classInstance = controllers.get(method.getDeclaringClass());
+            final HandlerExecution handlerExecution = new HandlerExecution(classInstance, method);
 
-    private static Object getClassInstance(final Class<?> clazz) {
-        try {
-            return clazz.getConstructor().newInstance();
-        } catch (InstantiationException |
-                 IllegalAccessException |
-                 InvocationTargetException |
-                 NoSuchMethodException e) {
-            throw new InvalidClassException("클래스의 인스턴스를 생성할 수 없습니다.");
+            handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
 
