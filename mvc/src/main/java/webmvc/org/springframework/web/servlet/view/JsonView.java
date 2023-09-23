e4e0bbee50131d3484d8b13a6cda54f9cf1d52d0
rawfishthelgh <rg970604@naver.com>
2023-09-24 01:46:07 +0900

    feat: json타입 직렬화

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,22 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(model);
+        PrintWriter printWriter = response.getWriter();
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        printWriter.write(jsonString);
     }
 }
