b0f4e15e07af35afbb540203cdcbccd5ab49d1c2
thdwoqor <celebarte01@gmail.com>
2023-09-24 22:50:05 +0900

    refactor: DispatcherServletInitializer 위치 이동

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -1,9 +1,10 @@
-package webmvc.org.springframework.web.servlet.mvc;
+package com.techcourse;
 
 import jakarta.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
+import webmvc.org.springframework.web.servlet.mvc.DispatcherServlet;
 
 /**
  * Base class for {@link WebApplicationInitializer}
