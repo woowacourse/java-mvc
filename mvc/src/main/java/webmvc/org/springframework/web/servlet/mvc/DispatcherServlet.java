50677b6285195adac6632c2168bd7563b97d4e0f
Rosie <dev.kayoung@gmail.com>
2023-09-24 20:14:39 +0900

    refactor: final 로 변경

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/DispatcherServlet.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/DispatcherServlet.java
@@ -17,7 +17,7 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private List<HandlerMapping> handlerMappings;
+    private final List<HandlerMapping> handlerMappings;
 
     public DispatcherServlet(ServletContext servletContext) {
         handlerMappings = (List<HandlerMapping>) servletContext.getAttribute("handlerMappings");
