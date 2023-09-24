8d780275dfd8b551ec0ff5e08a70bd50773e60b4
mcodnjs <codnjs8989@gmail.com>
2023-09-24 18:07:03 +0900

    feat: JsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final int SINGLE_MODEL_SIZE = 1;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        if (model.size() == SINGLE_MODEL_SIZE) {
+            final Object oneValue = model.values().toArray()[0];
+            writeObjectAsJson(response, oneValue);
+            return;
+        }
+
+        writeObjectAsJson(response, model);
+    }
+
+    private void writeObjectAsJson(final HttpServletResponse response, final Object oneValue) throws IOException {
+        final String valueAsString = objectMapper.writeValueAsString(oneValue);
+        response.getWriter().write(valueAsString);
     }
 }
