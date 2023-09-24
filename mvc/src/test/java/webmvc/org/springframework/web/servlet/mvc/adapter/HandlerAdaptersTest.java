79b8705dacc9b1ab788b1ffdfe2d64fa9bbaed7a
somsom13 <somin1113@naver.com>
2023-09-24 18:12:28 +0900

    test: HandlerAdapter, Mapping 테스트

---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/adapter/HandlerAdaptersTest.java
@@ -0,0 +1,39 @@
+package webmvc.org.springframework.web.servlet.mvc.adapter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import webmvc.org.springframework.web.servlet.mvc.handlermapping.HandlerExecution;
+
+class HandlerAdaptersTest {
+
+    @DisplayName("Handler 객체가 HandlerExecution 이라면 어노테이션 기반의 adapter 가 반환된다.")
+    @Test
+    void getHandlerAdapter() throws Exception {
+        // given
+        final HandlerAdapters handlerAdapters = new HandlerAdapters();
+        final HandlerExecution handler = Mockito.mock(HandlerExecution.class);
+
+        // when
+        final HandlerAdapter handlerAdapter = handlerAdapters.getHandlerAdapter(handler);
+
+        // then
+        assertThat(handlerAdapter).isInstanceOf(AnnotationHandlerAdapter.class);
+    }
+
+    @DisplayName("지원되는 HandlerAdapter가 없다면 예외가 발생한다.")
+    @Test
+    void getHandlerAdapter_nothingSupported() throws Exception {
+        // given
+        final HandlerAdapters handlerAdapters = new HandlerAdapters();
+        final Object handler = new Object();
+
+        // when, then
+        assertThatThrownBy(() -> handlerAdapters.getHandlerAdapter(handler))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("적합한 핸들러 어댑터가 없습니다.");
+    }
+}
---
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/handlermapping/HandlerMappingsTest.java
@@ -0,0 +1,48 @@
+package webmvc.org.springframework.web.servlet.mvc.handlermapping;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HandlerMappingsTest {
+
+    @DisplayName("일치하는 URI, Method 의 handler 를 반환한다.")
+    @Test
+    void getHandler() {
+        // given
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HandlerMappings handlerMappings = new HandlerMappings("samples");
+        handlerMappings.initialize();
+
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        // when
+        final Object handler = handlerMappings.getHandler(request);
+
+        // then
+        assertThat(handler).isNotNull()
+            .isInstanceOf(HandlerExecution.class);
+    }
+
+    @DisplayName("매칭되는 Handler 가 없다면 예외가 발생한다.")
+    @Test
+    void getHandler_nothingMatched() {
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HandlerMappings handlerMappings = new HandlerMappings("samples");
+        handlerMappings.initialize();
+
+        when(request.getRequestURI()).thenReturn("/post-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        // when, then
+        assertThatThrownBy(() -> handlerMappings.getHandler(request))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("매칭되는 핸들러가 없습니다.");
+    }
+}
