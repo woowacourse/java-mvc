7d4c9439debd407c9970f8c2d681ee636b88ba48
dooboocookie <bkh3030@gmail.com>
2023-09-24 16:09:01 +0900

    feat: UserController 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -8,8 +8,6 @@ import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class JsonView implements View {
