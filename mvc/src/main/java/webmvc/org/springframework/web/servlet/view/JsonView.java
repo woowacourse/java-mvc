a93ebf2a3b56c1c5efdad370799a2fa47c90fc1f
splitCoding <voicesplit@daum.net>
2023-09-24 18:09:27 +0900

    feat: JsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,58 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final int SINGLE_ELEMENT_SIZE = 1;
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model,
+                       final HttpServletRequest request,
+                       final HttpServletResponse response) throws IOException {
+        setResponseHeaderForJsonView(response);
+        writeJsonStringInResponse(model, response);
+    }
+
+    private void setResponseHeaderForJsonView(final HttpServletResponse response) {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+    }
+
+    private void writeJsonStringInResponse(final Map<String, ?> model,
+                                           final HttpServletResponse response) throws IOException {
+        final String jsonString = getJsonString(model);
+
+        try (final PrintWriter writer = response.getWriter()) {
+            writer.write(jsonString);
+            writer.flush();
+        }
+    }
+
+    private String getJsonString(Map<String, ?> model) {
+        if (model.size() == SINGLE_ELEMENT_SIZE) {
+            final Object[] modelElements = model.values().toArray();
+            return writeValueAsString(modelElements[0]);
+        }
+        return writeValueAsString(model);
+    }
+
+    private String writeValueAsString(Object object) {
+        try {
+            return OBJECT_MAPPER.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
