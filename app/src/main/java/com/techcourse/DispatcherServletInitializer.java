72d054a5afbea23fab22a3615cd9d18303253cb7
pilyang <pilyang.dev@gmail.com>
2023-09-24 18:59:03 +0900

    refactor: legacy 컨트롤러 제거

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
 import webmvc.org.springframework.web.servlet.mvc.HandlerExecutionAdapter;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecution;
 
@@ -23,10 +22,8 @@ public class DispatcherServletInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(final ServletContext servletContext) {
         final var dispatcherServlet = new DispatcherServlet();
-        dispatcherServlet.addHandlerMapping(new ManualHandlerMapping());
         dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping(PACKAGE_TO_SEARCH_CONTROLLER));
         dispatcherServlet.addHandlerAdapterType(HandlerExecution.class, HandlerExecutionAdapter.class);
-        dispatcherServlet.addHandlerAdapterType(Controller.class, ManualControllerHandlerAdapter.class);
 
         final var registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
         if (registration == null) {
