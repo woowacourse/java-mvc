45bfa064cf59480b0b4c969bd443a05af42ed854
splitCoding <voicesplit@daum.net>
2023-09-24 17:26:31 +0900

    refactor: 컨트롤러 어노테이션 기반으로 변경

--- app/src/main/java/com/techcourse/controller/LoginViewController.java
+++ app/src/main/java/com/techcourse/controller/LoginViewController.java
@@ -1,22 +1,31 @@
 package com.techcourse.controller;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
-public class LoginViewController implements Controller {
+@Controller
+public class LoginViewController {
+
+    private static final View LOGIN_PAGE_VIEW = new JspView("redirect:/login.jsp");
+    private static final JspView REDIRECT_INDEX_PAGE_VIEW = new JspView("redirect:/index.jsp");
 
     private static final Logger log = LoggerFactory.getLogger(LoginViewController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public ModelAndView showLoginPage(final HttpServletRequest req, final HttpServletResponse res) {
         return UserSession.getUserFrom(req.getSession())
-                .map(user -> {
-                    log.info("logged in {}", user.getAccount());
-                    return "redirect:/index.jsp";
-                })
-                .orElse("/login.jsp");
+            .map(user -> {
+                log.info("logged in {}", user.getAccount());
+                return new ModelAndView(REDIRECT_INDEX_PAGE_VIEW);
+            })
+            .orElse(new ModelAndView(LOGIN_PAGE_VIEW));
     }
 }
--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -1,13 +1,20 @@
 package com.techcourse.controller;
 
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
-public class RegisterViewController implements Controller {
+@Controller
+public class RegisterViewController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        return "/register.jsp";
+    private static final JspView REGISTER_PAGE_VIEW = new JspView("/register.jsp");
+
+    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
+    public ModelAndView showRegisterPage(final HttpServletRequest req, final HttpServletResponse res) {
+        return new ModelAndView(REGISTER_PAGE_VIEW);
     }
 }
