91a9f68162fd0d3cc7214e7c69e886c183fcad70
hyena <goatsfish@gmail.com>
2023-09-24 09:16:41 +0900

    feat: Json 응답 뷰 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,39 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
+import webmvc.org.springframework.web.servlet.exception.ViewException;
 
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final String json = getResponseBody(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(json);
+    }
+
+    private String getResponseBody(final Map<String, ?> model) {
+        if (model.size() == 1) {
+            return String.valueOf(model.values().toArray()[0]);
+        }
+        try {
+            return objectMapper.writeValueAsString(model);
+        } catch (JsonProcessingException e) {
+            log.warn("Model\\[ {} \\] 을 Json으로 파싱하던 도중에 오류가 발생하였습니다.", model, e);
+            throw new ViewException("[ERROR] Model을 Json으로 파싱하던 도중에 오류가 발생하였습니다.");
+        }
     }
 }
