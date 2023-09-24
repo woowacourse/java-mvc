bb86f73f2dc65de9a69bf9d0b14e40d27a4773b8
thdwoqor <celebarte01@gmail.com>
2023-09-24 22:50:21 +0900

    feat: JsonView를 지원하도록 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,24 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.View;
-
 import java.util.Map;
+import web.org.springframework.http.MediaType;
+import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResult = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(jsonResult);
     }
 }
