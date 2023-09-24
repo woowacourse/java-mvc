8bb968ac1d6e7d11045c0a30bd1b27716dde1aec
dooboocookie <bkh3030@gmail.com>
2023-09-24 16:57:58 +0900

    refactor: 로그인 관련 컨트롤러 어노테이션 기반으로 변경

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -2,36 +2,64 @@ package com.techcourse.controller;
 
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
+import context.org.springframework.stereotype.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.mvc.handler.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
 
-public class LoginController implements Controller {
+import java.util.Optional;
+
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView viewLogin(final HttpServletRequest request,
+                                  final HttpServletResponse response) throws Exception {
+        final Optional<User> maybeUser = UserSession.getUserFrom(request.getSession());
+        if (maybeUser.isPresent()) {
+            log.info("user account={}", maybeUser.get().getAccount());
+            return new ModelAndView(new JspView("redirect:/"));
         }
-
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
-                .map(user -> {
-                    log.info("User : {}", user);
-                    return login(req, user);
-                })
-                .orElse("redirect:/401.jsp");
+        return new ModelAndView(new JspView("/login.jsp"));
     }
 
-    private String login(final HttpServletRequest request, final User user) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView handleLogin(final HttpServletRequest request, 
+                                    final HttpServletResponse response) throws Exception {
+        if (UserSession.isLoggedIn(request.getSession())) {
+            return new ModelAndView(new JspView("redirect:/"));
+        }
+
+        final String view = InMemoryUserRepository.findByAccount(request.getParameter("account"))
+                .map(user -> {
+                    log.info("User : {}", user);
+                    return login(request, user);
+                })
+                .orElse("redirect:/401.jsp");
+        return new ModelAndView(new JspView(view));
+    }
+
+    private String login(final HttpServletRequest request,
+                         final User user) {
         if (user.checkPassword(request.getParameter("password"))) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
             return "redirect:/index.jsp";
         }
         return "redirect:/401.jsp";
+    }
+
+    @RequestMapping(value = "/logout", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        final var session = req.getSession();
+        session.removeAttribute(UserSession.SESSION_KEY);
+        return new ModelAndView(new JspView("redirect:/"));
     }
 }
