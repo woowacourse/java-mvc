8e3a726ddee6d442e05999e27f41d2a19961f146
jyeost <pig9619@naver.com>
2023-09-24 17:11:05 +0900

    test: 핸들러 매핑 테스트 추가

---
+++ app/src/test/java/com/techcourse/HandlerMappingRegistryTest.java
@@ -0,0 +1,54 @@
+package com.techcourse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import webmvc.org.springframework.web.servlet.mvc.asis.Controller;
+import webmvc.org.springframework.web.servlet.mvc.exception.HandlerMappingNotFoundException;
+import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerExecution;
+import webmvc.org.springframework.web.servlet.mvc.tobe.Request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SuppressWarnings("NonAsciiCharacters")
+class HandlerMappingRegistryTest {
+
+    private final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+
+    @BeforeEach
+    void init() {
+        handlerMappingRegistry.initialize("com.techcourse");
+    }
+
+    @Test
+    void 어노테이션_핸들러매핑이_제대로_되어지는지_확인한다() {
+        final Request request = new Request("/register", "POST");
+        final Object handler = handlerMappingRegistry.getHandler(request);
+
+        assertThat(handler).isInstanceOf(HandlerExecution.class);
+    }
+
+    @Test
+    void 메뉴얼_핸들러_매핑이_제대로_되어지는지_확인한다() {
+        final Request request = new Request("/register/view", "GET");
+        final Object handler = handlerMappingRegistry.getHandler(request);
+
+        assertThat(handler).isInstanceOf(Controller.class);
+    }
+
+    @Test
+    void Uri_매핑이_중복된다면_어노테이션_매핑이_우선시된다() {
+        final Request request = new Request("/register", "GET");
+        final Object handler = handlerMappingRegistry.getHandler(request);
+
+        assertThat(handler).isInstanceOf(HandlerExecution.class);
+    }
+
+    @Test
+    void 매핑되지_않은_요청이라면_예외가_발생한다() {
+        final Request request = new Request("/registerrrr", "GET");
+
+        assertThatThrownBy(() -> handlerMappingRegistry.getHandler(request))
+                .isExactlyInstanceOf(HandlerMappingNotFoundException.class);
+    }
+}
