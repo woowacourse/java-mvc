eaf0ae5b3418c03d8bcca702547db286d9f6c603
seokhwan-an <asok369258@naver.com>
2023-09-24 16:26:15 +0900

    feat: JsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,13 +1,40 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
+    private static final String EMPTY = "";
+
+    private ObjectMapper objectMapper;
+
+    public JsonView() {
+        this.objectMapper = new ObjectMapper();
+    }
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final PrintWriter writer = response.getWriter();
+        final String body = makeBody(model);
+        writer.write(body);
+        writer.close();
+    }
+
+    private String makeBody(final Map<String, ?> model) throws JsonProcessingException {
+        if (model.isEmpty()) {
+            return EMPTY;
+        }
+        if (model.size() == 1) {
+            objectMapper.writeValueAsString(model.values().iterator().next());
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
