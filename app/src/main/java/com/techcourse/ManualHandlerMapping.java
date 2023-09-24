ee7e2ba369b1d7677050a7402c31cfa6b17bdf2a
hum02 <ehdgma02@gmail.com>
2023-09-24 12:55:22 +0900

    feat:jsonView 구현

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -3,7 +3,6 @@ package com.techcourse;
 import com.techcourse.controller.LoginController;
 import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
-import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -27,7 +26,6 @@ public class ManualHandlerMapping implements HandlerMapping {
         controllers.put("/login", new LoginController());
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,38 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final int SINGLE_SIZE = 1;
+    private final ObjectMapper mapper;
+
+    public JsonView() {
+        this.mapper = new ObjectMapper();
+    }
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final PrintWriter writer = response.getWriter();
+
+        try {
+            if (model.size() == SINGLE_SIZE) {
+                final Object onlyValue = model.values().toArray()[0];
+                mapper.writeValue(writer, onlyValue);
+            }
+            mapper.writeValue(writer, model);
+        } catch (IOException e) {
+            throw new RuntimeJsonMappingException("json을 값을 write할 수 없습니다.");
+        }
     }
 }
