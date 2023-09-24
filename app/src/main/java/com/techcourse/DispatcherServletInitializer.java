3ea4507f4d5ebd48099f4518694ebaa37c371c5b
yeonkkk <tjddus0302@gmail.com>
2023-09-24 13:44:37 +0900

    refactor: DispatcherServlet 패키지 변경

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
+import webmvc.org.springframework.web.servlet.DispatcherServlet;
 import webmvc.org.springframework.web.servlet.mvc.HandlerAdapters;
 import webmvc.org.springframework.web.servlet.mvc.HandlerMappings;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerAdapter;
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/DispatcherServlet.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/DispatcherServlet.java
@@ -1,4 +1,4 @@
-package com.techcourse;
+package webmvc.org.springframework.web.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -7,8 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webmvc.org.springframework.web.servlet.ModelAndView;
-import webmvc.org.springframework.web.servlet.View;
 import webmvc.org.springframework.web.servlet.mvc.HandlerAdapter;
 import webmvc.org.springframework.web.servlet.mvc.HandlerAdapters;
 import webmvc.org.springframework.web.servlet.mvc.HandlerMappings;
