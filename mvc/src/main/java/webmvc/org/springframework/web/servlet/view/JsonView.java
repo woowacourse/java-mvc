7b16e9bf13beafe56943ad27bc73403a3617741e
JunH <tjdtls690@gmail.com>
2023-09-25 02:03:54 +0900

    :sparkles: feat : JsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,26 +1,28 @@
 package webmvc.org.springframework.web.servlet.view;
 
 import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
-import static web.org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.View;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import web.org.springframework.http.MediaType;
+import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
         if (model == null || model.isEmpty()) {
             response.setStatus(SC_NOT_FOUND);
             return;
         }
-        response.setContentType(APPLICATION_JSON_UTF8_VALUE);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         final String body = new ObjectMapper().writeValueAsString(model);
-        response.getWriter().write(body);
+        final PrintWriter writer = response.getWriter();
+        writer.write(body);
     }
 
     @Override
