a3cfc777f7d1ef17cfe2a1fab55dfbdd09f30d93
songusika <sws981202@gmail.com>
2023-09-25 10:33:54 +0900

    refactor: DispatcherServletInitializer app 모듈로 이동

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -1,10 +1,13 @@
-package webmvc.org.springframework.web.servlet.mvc;
+package com.techcourse;
 
 import jakarta.servlet.ServletContext;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
+import webmvc.org.springframework.web.servlet.mvc.DispatcherServlet;
+import webmvc.org.springframework.web.servlet.mvc.HandlerAdapters;
+import webmvc.org.springframework.web.servlet.mvc.HandlerMappings;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerAdapter;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 
