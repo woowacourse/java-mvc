9fd39f2d90e154ae9c6337b2126a0fef419ea8dd
aiaiaiai1 <cvc100326@naver.com>
2023-09-25 16:45:13 +0900

    refactor: 레거시 컨트롤러 -> 어노테이션 기반 컨트롤러로 변경

--- app/src/main/java/com/techcourse/controller/ForwardController.java
+++ app/src/main/java/com/techcourse/controller/ForwardController.java
@@ -9,10 +9,10 @@ import webmvc.org.springframework.web.servlet.ModelAndView;
 import webmvc.org.springframework.web.servlet.view.JspView;
 
 @Controller
-public class RegisterViewController {
+public class ForwardController {
 
-    @RequestMapping(value = "/register/view", method = RequestMethod.GET)
-    public ModelAndView getRegisterView(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return new ModelAndView(new JspView("/register.jsp"));
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView execute(final HttpServletRequest request, final HttpServletResponse response) {
+        return new ModelAndView(new JspView("/index.jsp"));
     }
 }
