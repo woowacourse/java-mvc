18be38933d13d157acb074fa858165690f1a8f20
ezzanzzan <cjsdmswjd821@naver.com>
2023-09-25 17:23:18 +0900

    fix: getHandler 오류 해결

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/HandlerMappingRegistry.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/HandlerMappingRegistry.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HandlerMappingRegistry {
     private final List<HandlerMapping> handlerMappings = new ArrayList<>();
@@ -19,6 +20,7 @@ public class HandlerMappingRegistry {
     public Object getHandler(final HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
                 .findAny()
                 .orElseThrow(IllegalArgumentException::new);
     }
