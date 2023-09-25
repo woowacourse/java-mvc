813d5a7aeebaa561373290a7aeb78fd9bf7fe830
woo-chang <clllickme@naver.com>
2023-09-25 21:19:23 +0900

    refactor: Logout POST -> GET 메서드 수정

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -12,7 +12,7 @@ import webmvc.org.springframework.web.servlet.view.JspView;
 @Controller
 public class LogoutController {
 
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse res) {
         HttpSession session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
