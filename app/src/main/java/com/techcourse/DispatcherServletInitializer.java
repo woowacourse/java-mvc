8caebe14e968c21a7fc03e1bede7d671f96a9e02
This2sho <zx00018@naver.com>
2023-09-25 13:33:27 +0900

    refactor: legacy 코드 제거 및 DispatcherServlet 패키지 이동

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -4,15 +4,15 @@ import jakarta.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import web.org.springframework.web.WebApplicationInitializer;
-import webmvc.org.springframework.web.servlet.mvc.asis.ManualHandlerAdapter;
+import webmvc.org.springframework.web.servlet.mvc.DispatcherServlet;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerAdapter;
 import webmvc.org.springframework.web.servlet.mvc.tobe.AnnotationHandlerMapping;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerAdapters;
 import webmvc.org.springframework.web.servlet.mvc.tobe.HandlerMappings;
 
 /**
- * Base class for {@link WebApplicationInitializer}
- * implementations that register a {@link DispatcherServlet} in the servlet context.
+ * Base class for {@link WebApplicationInitializer} implementations that register a {@link DispatcherServlet} in the
+ * servlet context.
  */
 public class DispatcherServletInitializer implements WebApplicationInitializer {
 
@@ -23,11 +23,9 @@ public class DispatcherServletInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(final ServletContext servletContext) {
         final HandlerMappings handlerMappings = new HandlerMappings();
-        handlerMappings.add(new ManualHandlerMapping());
         handlerMappings.add(new AnnotationHandlerMapping("com.techcourse.controller"));
 
         final HandlerAdapters handlerAdapters = new HandlerAdapters();
-        handlerAdapters.add(new ManualHandlerAdapter());
         handlerAdapters.add(new AnnotationHandlerAdapter());
 
         final var dispatcherServlet = new DispatcherServlet(handlerMappings, handlerAdapters);
