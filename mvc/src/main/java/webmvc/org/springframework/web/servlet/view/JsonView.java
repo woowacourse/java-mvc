413093bd6c7872e0a3bb5fa2b8ef55c6edd823da
hong-sile <gurwns9325@naver.com>
2023-09-25 14:37:03 +0900

    feat: jsonView 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,15 +1,29 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request,
                        final HttpServletResponse response) throws Exception {
+        final String convertedModel = convertModel(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(convertedModel);
+    }
 
+    private String convertModel(final Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values().toArray()[0]);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
