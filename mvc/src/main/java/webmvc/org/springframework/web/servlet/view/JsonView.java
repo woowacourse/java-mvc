e5ccb8ec8b06fdfa87051b3be61dccddf28499b5
kwonyj1022 <kwonyj1022@naver.com>
2023-09-26 08:35:35 +0900

    feat: model에 데이터가 1개면 값을 그대로 반환하는 기능 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,5 +1,6 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,6 +8,7 @@ import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 import java.util.Map;
+import java.util.Set;
 
 public class JsonView implements View {
 
@@ -18,8 +20,19 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        final String body = objectMapper.writeValueAsString(model);
+        final String body = findBody(model);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(body);
+    }
+
+    private String findBody(final Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            final Set<String> keys = model.keySet();
+            final String firstKey = (String) keys.toArray()[0];
+
+            return objectMapper.writeValueAsString(model.get(firstKey));
+        }
+
+        return objectMapper.writeValueAsString(model);
     }
 }
