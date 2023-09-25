46bb8071ea762b93aa6a4e8e0a20251c1c28879c
iamjooon2 <iamjooon2@gmail.com>
2023-09-25 15:08:32 +0900

    chore: 개행 제거

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,22 +1,22 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import java.io.PrintWriter;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
-import java.io.PrintWriter;
-import java.util.Map;
-
 public class JsonView implements View {
-    private static final int SINGLE_JSON_SIZE = 1;
 
+    private static final int SINGLE_JSON_SIZE = 1;
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
 
     @Override
-    public void render(Map<String, ?> model,HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         PrintWriter printWriter = response.getWriter();
         String viewValue = objectMapper.writeValueAsString(createResult(model));
@@ -29,4 +29,5 @@ public class JsonView implements View {
         }
         return model;
     }
+
 }
