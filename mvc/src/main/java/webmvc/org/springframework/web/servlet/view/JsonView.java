167bce03041ee11ff8de85ed180ea21da4620e6b
zillionme <beatus96@naver.com>
2023-09-26 02:14:17 +0900

    chore: 사용하지 않는 필드 제거

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -3,8 +3,6 @@ package webmvc.org.springframework.web.servlet.view;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
@@ -12,8 +10,6 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
-
-    private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
