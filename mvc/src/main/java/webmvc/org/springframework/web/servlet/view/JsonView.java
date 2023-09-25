4b8637cdb5d75369e21d4b8c59ae9b7451ba9232
yenawee <yaena0319@naver.com>
2023-09-25 21:37:36 +0900

    feat: private field 도 직렬화되게 변경, model 데이터 1개일때는 값 그대로 반환하게 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,5 +1,6 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,9 +19,20 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String jsonValue = objectMapper.writeValueAsString(model);
+        String jsonValue;
+        if (model.size() == 1) {
+            jsonValue = makeResponseWhenModelHasOneData(model);
+        } else {
+            jsonValue = objectMapper.writeValueAsString(model);
+        }
 
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(jsonValue);
+    }
+
+    private String makeResponseWhenModelHasOneData(Map<String, ?> model) throws JsonProcessingException {
+        String key = model.keySet().iterator().next();
+        Object value = model.get(key);
+        return objectMapper.writeValueAsString(value);
     }
 }
