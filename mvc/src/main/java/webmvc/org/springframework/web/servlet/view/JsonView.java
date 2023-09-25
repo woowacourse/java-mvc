e69dab1bda871b9bd3753f2822abfa874744a935
sosow0212 <sosow0212@naver.com>
2023-09-25 17:26:16 +0900

    refactor: 단일 값인 경우 text/html 타입으로 주도록 변경

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -15,20 +15,22 @@ public class JsonView implements View {
     private static final int SINGLE_RESPONSE_VALUE = 1;
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TEXT_HTML_TYPE = "text/html";
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String body = getResponseBody(model);
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        String body = getResponseBody(model, response);
         response.getWriter().write(body);
     }
 
-    private String getResponseBody(final Map<String, ?> model) throws JsonProcessingException {
+    private String getResponseBody(final Map<String, ?> model, final HttpServletResponse response) throws JsonProcessingException {
         if (model.size() == SINGLE_RESPONSE_VALUE) {
             Object data = model.values().toArray()[FIRST_VALUE_INDEX];
+            response.setContentType(TEXT_HTML_TYPE);
             return String.valueOf(data);
         }
 
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         return objectMapper.writeValueAsString(model);
     }
 }
