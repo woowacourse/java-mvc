0666b4856e3ccc952137e9107063f405b5fe64a3
nuyh <hjaehyun25@gmail.com>
2023-09-25 14:54:45 +0900

    refactor: 어노테이션 기반 컨트롤러로 변경

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
         final String account = request.getParameter("account");
         log.debug("user id : {}", account);
-
+        
         final ModelAndView modelAndView = new ModelAndView(new JsonView());
         final User user = InMemoryUserRepository.findByAccount(account)
                 .orElseThrow();
