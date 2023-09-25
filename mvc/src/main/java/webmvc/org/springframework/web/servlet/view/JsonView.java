63c6471ad96517d2a43b812af51e8a8e523d7bf4
java-saeng <gusdn85554@naver.com>
2023-09-25 16:56:44 +0900

    refactor : I/O 작업 관련 Exception 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -3,12 +3,16 @@ package webmvc.org.springframework.web.servlet.view;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
+  private static final Logger log = LoggerFactory.getLogger(JsonView.class);
   private static final int SINGLE_VALUE_INDEX = 0;
 
   @Override
@@ -20,15 +24,20 @@ public class JsonView implements View {
     final ObjectMapper objectMapper = new ObjectMapper();
     final String body = objectMapper.writeValueAsString(model);
 
-    if (hasSingle(model)) {
-      response.setContentType("text/plain");
+    try {
+      if (hasSingle(model)) {
+        response.setContentType("text/plain");
 
-      response.getWriter().write(String.valueOf(model.values().toArray()[SINGLE_VALUE_INDEX]));
-      return;
+        response.getWriter().write(String.valueOf(model.values().toArray()[SINGLE_VALUE_INDEX]));
+        return;
+      }
+
+      response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+      response.getWriter().write(body);
+    } catch (IOException e) {
+      log.error("JSON IOException Error", e);
     }
 
-    response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-    response.getWriter().write(body);
   }
 
   private boolean hasSingle(final Map<String, ?> model) {
