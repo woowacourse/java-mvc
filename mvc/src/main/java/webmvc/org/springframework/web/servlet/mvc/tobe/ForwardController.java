9a033cdcd8499a5ad88a946793f3080479af4dac
hong-sile <gurwns9325@naver.com>
2023-09-25 16:08:52 +0900

    feat: forwardController 리팩터링

---
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/ForwardController.java
@@ -0,0 +1,21 @@
+package webmvc.org.springframework.web.servlet.mvc.tobe;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
+
+@Controller
+public class ForwardController {
+
+    private static final String ROOT_DIR = "/index.jsp";
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView show(final HttpServletRequest request,
+                             final HttpServletResponse response) {
+        return new ModelAndView(new JspView(ROOT_DIR));
+    }
+}
