bc9433f8a1f1141fa032e196c01ade9a9ee29329
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-25 13:08:08 +0900

    refactor: JsonView를 반환하는 UserController 추가

--- mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/DispatcherServletTest.java
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/mvc/tobe/DispatcherServletTest.java
@@ -9,6 +9,10 @@ import static org.mockito.Mockito.mock;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -66,5 +70,31 @@ class DispatcherServletTest {
 
         // when & then
         assertThatCode(() -> dispatcherServlet.service(request, response)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void user조회_테스트() throws IOException {
+        // given
+        final DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.init();
+
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+        final RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+        final StringWriter stringWriter = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        given(request.getRequestURI()).willReturn("/api/user");
+        given(request.getParameter("account")).willReturn("gugu");
+        given(request.getMethod()).willReturn("GET");
+        given(response.getWriter()).willReturn(printWriter);
+        given(request.getRequestDispatcher(any())).willReturn(requestDispatcher);
+
+        final Set<String> expected = Set.of("account='gugu'", "email='hkkang@woowahan.com'");
+
+        // when & then
+        assertThatCode(() -> dispatcherServlet.service(request, response)).doesNotThrowAnyException();
+        final String bufferedResponse = stringWriter.toString();
+        assertThat(bufferedResponse).contains(expected);
     }
 }
