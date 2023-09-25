a57bf7c1f374cfa45b5be10dd152fc46a63c212d
Ethan <kim76744dev@gmail.com>
2023-09-25 22:25:22 +0900

    refactor: Account가 없을 경우와 user가 없을 경우 예외처리

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -18,14 +18,23 @@ public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView show(final HttpServletRequest request, final HttpServletResponse response) {
         final String account = request.getParameter("account");
         log.debug("user id : {}", account);
+        if (account == null) {
+            return ModelAndView.redirectTo("/404.jsp");
+        }
+
+        User user = null;
+        try {
+            log.debug("userRepository.findByAccount() : {}", account);
+            user = InMemoryUserRepository.findByAccount(account)
+                    .orElseThrow(() -> new IllegalArgumentException("account에 해당하는 user를 찾을 수가 없습니다."));
+        } catch (final IllegalArgumentException e) {
+            return ModelAndView.redirectTo("/400.jsp");
+        }
 
         final ModelAndView modelAndView = new ModelAndView(new JsonView());
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow();
-
         modelAndView.addObject("user", user);
         return modelAndView;
     }
