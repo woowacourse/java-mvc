1b5976187d50dbe6921fde2c58571e76b90fffb9
woosung1223 <dntjd991223@naver.com>
2023-09-25 20:51:39 +0900

    test: Fake 객체가 아닌 verify를 사용하도록 변경

--- mvc/src/test/java/webmvc/org/springframework/web/servlet/view/JsonViewTest.java
+++ mvc/src/test/java/webmvc/org/springframework/web/servlet/view/JsonViewTest.java
@@ -1,11 +1,5 @@
 package webmvc.org.springframework.web.servlet.view;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletOutputStream;
-import jakarta.servlet.WriteListener;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -13,12 +7,12 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.io.BufferedWriter;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -28,39 +22,19 @@ class JsonViewTest {
     void Model_에_맞는_JSON_을_반환할_수_있다() throws Exception {
         // given
         Map<String, String> model = new LinkedHashMap<>();
-        model.put("name", "teo");
-        model.put("age", "25");
+        model.put("key", "value");
 
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-
-        FakeWriter writer = new FakeWriter();
-        when(response.getWriter()).thenReturn(writer);
+        PrintWriter printWriter = Mockito.mock(PrintWriter.class);
+        when(response.getWriter())
+                .thenReturn(printWriter);
 
         // when
         JsonView jsonView = new JsonView();
         jsonView.render(model, request, response);
 
         // then
-        Map<String, String> content = new ObjectMapper().readValue(writer.getContent(), HashMap.class);
-        assertThat(content).isEqualTo(model);
-    }
-
-    static class FakeWriter extends PrintWriter {
-
-        private String content;
-
-        public FakeWriter() {
-            super(nullWriter());
-        }
-
-        @Override
-        public void write(String s) {
-            this.content = s;
-        }
-
-        public String getContent() {
-            return content;
-        }
+        verify(printWriter).write("{\"key\":\"value\"}");
     }
 }
