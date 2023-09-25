5b901d3d9da80261190794f7a28e07c6512afb09
BackFoxx <backfoxxx@gmail.com>
2023-09-25 16:42:28 +0900

    feat : 3단계 - JsonView를 구현하고, DispatcherServlet에 연결

--- mvc/src/main/java/web/org/springframework/http/MediaType.java
+++ mvc/src/main/java/web/org/springframework/http/MediaType.java
@@ -1,5 +1,6 @@
 package web.org.springframework.http;
 
 public class MediaType {
+    public static final String TEXT_PLAIN_UTF8_VALUE = "text/plain;charset=UTF-8";
     public static final String APPLICATION_JSON_UTF8_VALUE = "application/json;charset=UTF-8";
 }
--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,28 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        if (model.size() == 1) {
+            response.setContentType(MediaType.TEXT_PLAIN_UTF8_VALUE);
+            final PrintWriter writer = response.getWriter();
+            writer.write(objectMapper.writeValueAsString(model.values().toArray()[0]));
+        }
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final PrintWriter writer = response.getWriter();
+        writer.write(objectMapper.writeValueAsString(model));
     }
 }
