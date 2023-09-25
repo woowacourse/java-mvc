368384958da4c18c64afe7b1cae963ad41bd4ed0
zillionme <beatus96@naver.com>
2023-09-25 15:16:42 +0900

    feat: json 뷰 및 UserController 구현

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,14 +1,33 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import web.org.springframework.http.MediaType;
 import webmvc.org.springframework.web.servlet.View;
 
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JspView.class);
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        if (model.size() == 1) {
+            Object one = model.values().toArray()[0];
+            response.getWriter().write(String.valueOf(one));
+            return;
+        }
+        String value = objectMapper.writeValueAsString(model);
+        writer.write(value);
+        writer.flush();
     }
 }
