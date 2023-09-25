45e328fb9c3f3fb511a4c3a2c394c4e73e85530d
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-25 10:35:35 +0900

    refactor: JsonView Render 구현 및 UserController 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,15 +1,35 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import webmvc.org.springframework.web.servlet.View;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import web.org.springframework.http.MediaType;
+import webmvc.org.springframework.web.servlet.View;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(
+            Map<String, ?> model,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        PrintWriter writer = response.getWriter();
+        String value = convertModelToJson(model);
+        writer.write(value);
+    }
+
+    private String convertModelToJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return String.valueOf(model.values().toArray()[0]);
+        }
+        return OBJECT_MAPPER.writeValueAsString(model);
     }
 
 }
