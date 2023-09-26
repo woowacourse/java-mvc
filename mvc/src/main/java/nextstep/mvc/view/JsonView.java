f2a9a896dc547a6cc5bbbbfe60c169f8cae440d0
gitchan <eunkeeeee@gmail.com>
2023-09-26 15:25:50 +0900

    refactor: size == 1 일 때에는 model 에서 value 를 빼서 json 으로 변환

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,5 +1,6 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +18,14 @@ public class JsonView implements View {
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(APPLICATION_JSON_UTF8_VALUE);
         final PrintWriter writer = response.getWriter();
-        writer.write(objectMapper.writeValueAsString(model));
+        writer.write(generateJson(model));
+    }
+
+    private String generateJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Object value = model.values().iterator().next();
+            return objectMapper.writeValueAsString(value);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
