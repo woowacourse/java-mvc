b65c405b032ef8e0282be993048c4d0ceb8e125e
songusika <sws981202@gmail.com>
2023-09-26 19:03:01 +0900

    refactor: ObjectWriter, ObjectReader 로 static 선언

--- mvc/src/main/java/web/org/springframework/util/HttpRequestBodyConverter.java
+++ mvc/src/main/java/web/org/springframework/util/HttpRequestBodyConverter.java
@@ -1,14 +1,25 @@
 package web.org.springframework.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class HttpRequestBodyConverter {
 
+    private static final ObjectReader reader;
+    private static final ObjectWriter writer;
+
+    static {
+        final ObjectMapper mapper = new ObjectMapper();
+        reader = mapper.reader();
+        writer = mapper.writer();
+    }
+
     public static Object serialize(final String json, final Class<?> target) throws Exception {
-        return new ObjectMapper().readValue(json, target);
+        return reader.readValue(json, target);
     }
 
     public static String deserialize(final Object object) throws Exception {
-        return new ObjectMapper().writeValueAsString(object);
+        return writer.writeValueAsString(object);
     }
 }
