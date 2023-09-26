869cd6b50562a57b3b610fc0f3b30fd554269148
rawfishthelgh <rg970604@naver.com>
2023-09-26 16:59:10 +0900

    refactor: 데이터 사이즈 1개일 때 값을 그대로 반환

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,5 +1,6 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,9 +15,17 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = objectMapper.writeValueAsString(model);
+        String jsonString = extractJsonString(model, objectMapper);
         PrintWriter printWriter = response.getWriter();
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         printWriter.write(jsonString);
+    }
+
+    private String extractJsonString(Map<String, ?> model, ObjectMapper objectMapper) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Object value = model.values().toArray()[0];
+            return objectMapper.writeValueAsString(value);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
