d36fd8a60560998af13e0dd50d7be4df56f1bfbc
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-09-26 22:56:40 +0900

    refactor: ObjectMapper 와 JsonView 통합

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JsonView.java
@@ -1,5 +1,7 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -7,11 +9,21 @@ import web.org.springframework.http.MediaType;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
         throws Exception {
-        final String body = JsonWriterObjectMapper.writeJson(model);
+        final String body = writeJson(model);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(body);
+    }
+
+    private String writeJson(final Map<String, ?> objects) {
+        try {
+            return objectMapper.writeValueAsString(objects);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Json 객체 변환 과정에서 예외가 발생했습니다.");
+        }
     }
 }
