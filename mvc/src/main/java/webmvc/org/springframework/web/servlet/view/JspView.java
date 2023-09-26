cfb31e91abad4242237d699fbaf81267873ade3e
Go-Jaecheol <gojaech@naver.com>
2023-09-26 21:42:31 +0900

    refactor: RedirectView 추상 클래스 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/view/JspView.java
@@ -1,32 +1,25 @@
 package webmvc.org.springframework.web.servlet.view;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webmvc.org.springframework.web.servlet.View;
 
+import java.io.IOException;
 import java.util.Map;
 
-public class JspView implements View {
+public class JspView extends RedirectView {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
-    public static final String REDIRECT_PREFIX = "redirect:";
-
-    private final String viewName;
-
     public JspView(final String viewName) {
-        this.viewName = viewName;
+        super(viewName);
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
+    protected void renderWithoutRedirect(final Map<String, ?> model, final HttpServletRequest request,
+                                         final HttpServletResponse response) throws ServletException, IOException {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
