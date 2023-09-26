aef4c783217a295b202df50ba0c1dd35cf0cf6ba
rawfishthelgh <rg970604@naver.com>
2023-09-26 16:52:09 +0900

    refactor: 인덱스 페이지 어노테이션 기반 출력

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/handlerMapper/ManualHandlerMapping.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/mvc/tobe/handlerMapper/ManualHandlerMapping.java
@@ -17,8 +17,6 @@ public class ManualHandlerMapping implements HandlerMapping {
 
     @Override
     public void initialize() {
-        controllers.put("/", new ForwardController("/index.jsp"));
-
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
                 .forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
