b2aece51f44bb9c685d609cbcfb783f735dbea44
hong-sile <gurwns9325@naver.com>
2023-09-26 17:40:24 +0900

    feat: DIContainer step 4 진행

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +15,67 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        setFields(beans);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        final Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
+    }
+
+    private void setFields(final Set<Object> beans) {
+        for (final Object bean : beans) {
+            final Field[] declaredFields = bean.getClass().getDeclaredFields();
+            injectBeans(declaredFields, bean);
+        }
+    }
+
+    private void injectBeans(final Field[] declaredFields, final Object bean) {
+        Arrays.stream(declaredFields)
+            .filter(field -> filterFieldIsNull(field, bean))
+            .filter(field -> field.isAnnotationPresent(Inject.class))
+            .forEach(field -> injectBean(field, bean));
+    }
+
+    private boolean filterFieldIsNull(final Field field, final Object bean) {
+        try {
+            field.setAccessible(true);
+            return Objects.isNull(field.get(bean));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void injectBean(final Field field, final Object object) {
+        try {
+            field.set(object, getBean(field.getType()));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+            .map(this::createDefaultObject)
+            .collect(Collectors.toSet());
+    }
+
+    private Object createDefaultObject(final Class<?> clazz) {
+        try {
+            final Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> clazz) {
+        return (T) beans.stream()
+            .filter(object -> clazz.isAssignableFrom(object.getClass()))
+            .findFirst()
+            .orElse(null);
     }
 }
