39f085277086ac1f08148494f48cdcace8b4307c
xxeol2 <jhj01177@naver.com>
2023-09-27 09:33:19 +0900

    feat: ForwardController 추가

---
+++ app/src/main/java/com/techcourse/controller/ForwardController.java
@@ -0,0 +1,20 @@
+package com.techcourse.controller;
+
+import context.org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import web.org.springframework.web.bind.annotation.RequestMapping;
+import web.org.springframework.web.bind.annotation.RequestMethod;
+import webmvc.org.springframework.web.servlet.ModelAndView;
+import webmvc.org.springframework.web.servlet.view.JspView;
+
+@Controller
+@RequestMapping("/")
+public class ForwardController {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getLoginView(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return new ModelAndView(new JspView("/index.jsp"));
+    }
+}
