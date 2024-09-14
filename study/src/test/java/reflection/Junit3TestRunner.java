2c72a414fc96e2a575fa1828f4eb2a3de4de119d
zangsu <zangsu_@naver.com>
2024-09-15 00:24:35 +0900

    study: Junit3TestRunner 완료

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +13,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+
+        List<Method> test = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+
+        Junit3Test obj = new Junit3Test();
+        for(var method : test) {
+            method.invoke(obj);
+        }
     }
 }
