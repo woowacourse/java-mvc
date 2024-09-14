e045a901f109b3d5081c41a6f48f53c924a55aa9
zangsu <zangsu_@naver.com>
2024-09-15 00:27:25 +0900

    study: Junit4TestRunner 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +13,13 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .toList();
+
+        var testInstance = clazz.getDeclaredConstructor().newInstance();
+        for(var method : methods) {
+            method.invoke(testInstance);
+        }
     }
 }
