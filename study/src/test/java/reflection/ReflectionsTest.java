42d2f4e3ab84f495d20649f1e26c7ced87031666
zangsu <zangsu_@naver.com>
2024-09-15 00:08:32 +0900

    docs: 오타 수정

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -13,6 +13,6 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정된 모든 클래스 찾아 로그로 출력한다.
     }
 }
