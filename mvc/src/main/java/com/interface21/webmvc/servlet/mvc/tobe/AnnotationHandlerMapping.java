19119fb5ba0f5d216cf590b557522922455a4e02
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-15 15:52:18 +0900

    feat: reflection을 통해 get, post 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,15 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 
 public class AnnotationHandlerMapping {
-
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -20,10 +22,32 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        getAnnotatedClasses().forEach(this::registerHandlers);
+    }
+
+    private Set<Class<?>> getAnnotatedClasses() {
+        return new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlers(Class<?> clazz) {
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> registerHandler(method, clazz));
+    }
+
+    private void registerHandler(Method method, Class<?> clazz) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        Arrays.stream(requestMapping.method())
+                .forEach(requestMethod -> putHandlerExecution(requestMapping.value(), requestMethod, method, clazz));
+    }
+
+    private void putHandlerExecution(String requestUri, RequestMethod requestMethod, Method method, Class<?> clazz) {
+        HandlerKey handlerKey = new HandlerKey(requestUri, requestMethod);
+        HandlerExecution handlerExecution = new HandlerExecution(method, clazz);
+        handlerExecutions.put(handlerKey, handlerExecution);
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(new HandlerKey(request.getRequestURI(), RequestMethod.of(request.getMethod())));
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Method method;
+    private final Class<?> controllerClass;
+
+    public HandlerExecution(Method method, Class<?> controllerClass) {
+        this.method = method;
+        this.controllerClass = controllerClass;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+        return (ModelAndView) method.invoke(controllerInstance, request, response);
     }
 }
