cdfbaed32be5040aa169e8ad2568b391021fa696
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-15 22:58:25 +0900

    test: Junit3 Reflection 학습 테스트 추가

--- study/src/test/java/reflection/Junit3Test.java
+++ study/src/test/java/reflection/Junit3Test.java
@@ -1,6 +1,7 @@
 package reflection;
 
 public class Junit3Test {
+
     public void test1() throws Exception {
         System.out.println("Running Test1");
     }
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,18 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+
+        Method[] methods = clazz.getMethods();
+
+        int count = 0;
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+                count++;
+            }
+        }
+
+        assertThat(count).isEqualTo(2);
     }
 }
