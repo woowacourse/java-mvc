bdda0ec37de656217d404a6e14f8ca1e0e06c49e
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-15 23:31:09 +0900

    feat: AnnotationHandlerMapping Entry 초기화 메서드 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,32 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HandlerExecution {
 
+    private static final Logger log = LoggerFactory.getLogger(HandlerExecution.class);
+
+    private final Object controller;
+    private final Method method;
+
+    public HandlerExecution(Object controller, Method method) {
+        this.controller = controller;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        log.info("Execute Handler -> Method: {}#{}, URI: {}, HTTP Method: {}",
+                method.getDeclaringClass().getSimpleName(),
+                method.getName(),
+                request.getRequestURI(),
+                request.getMethod()
+        );
+
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
@@ -1,7 +1,8 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.web.bind.annotation.RequestMethod;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class HandlerKey {
@@ -9,9 +10,23 @@ public class HandlerKey {
     private final String url;
     private final RequestMethod requestMethod;
 
-    public HandlerKey(final String url, final RequestMethod requestMethod) {
+    public HandlerKey(String url, RequestMethod requestMethod) {
         this.url = url;
         this.requestMethod = requestMethod;
+    }
+
+    public static List<HandlerKey> of(String url, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
+                .toList();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public RequestMethod getRequestMethod() {
+        return requestMethod;
     }
 
     @Override
@@ -24,9 +39,12 @@ public class HandlerKey {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof HandlerKey)) return false;
-        HandlerKey that = (HandlerKey) o;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof HandlerKey that)) {
+            return false;
+        }
         return Objects.equals(url, that.url) && requestMethod == that.requestMethod;
     }
 
