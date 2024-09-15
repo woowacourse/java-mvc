070816d47db41fd6a4b57f9df75a0ed4336d08ca
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-15 22:58:38 +0900

    test: Junit4 Reflection 학습 테스트 추가

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +12,18 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        Method[] methods = clazz.getMethods();
+
+        int count = 0;
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+                count++;
+            }
+        }
+
+        assertThat(count).isEqualTo(2);
     }
 }
