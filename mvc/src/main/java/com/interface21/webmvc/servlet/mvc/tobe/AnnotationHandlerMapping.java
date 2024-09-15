ace0a6bc00e9081d4d12ad4019a5137373d36740
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-15 19:18:21 +0900

    feat: 리플렉션을 통해 컨트롤러 매핑 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.mvc.tobe.exception.HandlerMappingException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +28,52 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> controllerTypeClass = reflections.getTypesAnnotatedWith(Controller.class);
+        log.info("Controller type class count : {}", controllerTypeClass.size());
+
+        try {
+            mapControllerToHandler(controllerTypeClass);
+        } catch (Throwable e) {
+            log.error("Exception : {}", e.getMessage(), e);
+            throw new HandlerMappingException(e.getMessage());
+        }
+    }
+
+    private void mapControllerToHandler(Set<Class<?>> controllerTypeClass)
+            throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        for (Class<?> controllerClass : controllerTypeClass) {
+            Object controller = controllerClass.getDeclaredConstructor().newInstance();
+            mapControllerExecutionMethod(controller);
+        }
+    }
+
+    private void mapControllerExecutionMethod(Object controller) {
+        for (Method method : controller.getClass().getMethods()) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                registerHandlerExecution(method, controller);
+            }
+        }
+    }
+
+    private void registerHandlerExecution(Method method, Object controller) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] requestMethods = requestMapping.method();
+        if (requestMethods.length == 0) {
+            requestMethods = RequestMethod.values();
+        }
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/exception/HandlerMappingException.java
@@ -0,0 +1,7 @@
+package com.interface21.webmvc.servlet.mvc.tobe.exception;
+
+public class HandlerMappingException extends RuntimeException {
+    public HandlerMappingException(String message) {
+        super(message);
+    }
+}
