e73b3d565e4b4b8fe74a04142ede03b7445a9236
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-15 16:41:43 +0900

    feat: HandlerExecution 작성

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private final Method method;
+
+    public HandlerExecution(Method method) {
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        Object instance = method.getDeclaringClass().getDeclaredConstructor().newInstance();
+
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
