92a1e8b8159da7146f6e70e5a13df36f73299c84
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-15 11:51:09 +0900

    test: ReflectionsTest 학습 테스트 작성

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -13,6 +16,11 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(clazz -> log.info("Controller: {}", clazz.getName()));
+        reflections.getTypesAnnotatedWith(Service.class)
+                .forEach(clazz -> log.info("Service: {}", clazz.getName()));
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .forEach(clazz -> log.info("Repository: {}", clazz.getName()));
     }
 }
