ff2013ef4d9e84a407cfda6efa13b00a45edc74f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-15 16:03:03 +0900

    test: 학습 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,12 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
