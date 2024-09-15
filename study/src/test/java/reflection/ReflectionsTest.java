7f9afed89d2b0546863b25a9bd0a295a5fd478f0
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-15 15:25:11 +0900

    study: reflections Test 추가

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,13 @@
 package reflection;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -13,6 +17,23 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        logClassesWithAnnotation(controllerClasses, "@Controller");
+
+        Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        logClassesWithAnnotation(serviceClasses, "@Service");
+
+        Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        logClassesWithAnnotation(repositoryClasses, "@Repository");
+    }
+
+    private void logClassesWithAnnotation(Set<Class<?>> classes, String annotationName) {
+        if (classes.isEmpty()) {
+            log.info("No classes found with annotation {}", annotationName);
+        } else {
+            for (Class<?> clazz : classes) {
+                log.info("Class {} is annotated with {}", clazz.getName(), annotationName);
+            }
+        }
     }
 }
