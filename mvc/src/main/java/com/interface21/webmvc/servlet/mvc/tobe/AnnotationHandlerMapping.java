5d64fabcf7665af66a52993a8e6d70cb7adc5c21
zangsu <zangsu_@naver.com>
2024-09-16 17:42:20 +0900

    feat: 어노테이션 기반의 컨트롤러를 지원하는 HandlerMapping 을 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +32,42 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage,
+                Scanners.TypesAnnotated, Scanners.MethodsAnnotated);
+        for (var controller : reflections.getTypesAnnotatedWith(Controller.class)) {
+            putController(controller);
+        }
+    }
+
+    private void putController(Class<?> controllerClass) {
+        List<Method> handlerMethods = Arrays.stream(controllerClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+        for (Method method : handlerMethods) {
+            putHandlerExecution(controllerClass, method);
+        }
+    }
+
+    private void putHandlerExecution(Class<?> controllerClass, Method handlerMethod) {
+        RequestMapping requestMapping = handlerMethod.getAnnotation(RequestMapping.class);
+        if (requestMapping.method().length == 0) {
+            mappingTo(controllerClass, handlerMethod, requestMapping.value(), RequestMethod.values());
+            return;
+        }
+
+        mappingTo(controllerClass, handlerMethod, requestMapping.value(), requestMapping.method());
+    }
+
+    private void mappingTo(Class<?> controllerClass, Method method, String value, RequestMethod... requestMethods) {
+        for (RequestMethod requestMethod : requestMethods) {
+            handlerExecutions.put(new HandlerKey(value, requestMethod), new HandlerExecution(controllerClass, method));
+            log.info("map method {}() to {} {}", method.getName(), requestMethod.name(), value);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return Optional.ofNullable(handlerExecutions.get(handlerKey))
+                .orElseThrow(() -> new NoMatchedHandlerException(request));
     }
 }
