c1f1c7183f355e906d566ffa4958b68f4149eab3
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-16 15:08:04 +0900

    test: Reflection 학습 테스트 완료

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,15 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                // 메소드가 private일 경우 접근 가능하도록 설정
+                method.setAccessible(true);
+                // Junit3Test의 인스턴스를 생성
+                Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+                // 메소드 실행
+                method.invoke(instance);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +10,12 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getDeclaredMethods()) {
+            MyTest annotation = method.getAnnotation(MyTest.class);
+            if(annotation != null) {
+                Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
+                method.invoke(instance);
+            }
+        }
     }
 }
