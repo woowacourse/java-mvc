013aca3bc13564726addd253c3934346abf5ff0d
fromitive <fromitive@gmail.com>
2024-09-16 14:31:00 +0900

    test: MyTest annotation 붙여진 메서드 실행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,35 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        // given
+        Junit4Test obj = new Junit4Test();
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // when
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(obj);
+            }
+        }
+        String actual = outputStream.toString();
+
+        // then
+        assertThat(actual).contains("Running Test1", "Running Test2");
     }
 }
