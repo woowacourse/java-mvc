3dfe169ea171231ff2240f492d6d09bacdea09b6
kunsanglee <gosmdochee2@gmail.com>
2024-09-17 00:35:24 +0900

    feat: AnnotationHandlerMappingTest 성공

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +26,53 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        // handlerExecutions reflection 이용해서 초기화한다.
+        for (Object o : basePackage) { // basePackage 를 전부 뒤져서 Controller 를 등록한다.
+            String path = (String) o;
+            Reflections reflections = new Reflections(path); // 해당 경로를 뒤질 리플랙션을 생성한다.
+            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class); // Controller 애너테이션이 붙은 컨트롤러들을 전부 꺼낸다.
+            for (Class<?> controller : controllers) { // 컨트롤러들을 전부 순회한다.
+                log.debug("Controller = {}\n", controller);
+                Method[] methods = controller.getDeclaredMethods(); // 컨트롤러에 선언된 모든 메서드들을 꺼낸다.
+                for (Method method : methods) { // 컨트롤러의 모든 메서드들을 순회한다.
+                    log.debug("Method = {}", method);
+                    Annotation[] annotations = method.getAnnotations(); // 컨트롤러의 메서드에 선언된 모든 애너테이션을 꺼낸다.
+                    for (Annotation annotation : annotations) { // 컨트롤러의 메서드에 선언된 모든 애너테이션을 순회한다.
+                        if (annotation instanceof RequestMapping requestMapping) { // 애너테이션이 RequestMapping 이면 형변환한다.
+                            if (requestMapping.method().length == 0) {
+                                for (RequestMethod requestMethod : RequestMethod.values()) {
+                                    createHandlerKeyAndExecution(requestMapping, requestMethod, controller, method);
+                                }
+                                return;
+                            }
+                            RequestMethod requestMethod = requestMapping.method()[0]; // RequestMapping 의 method 를 꺼낸다.
+                            createHandlerKeyAndExecution(requestMapping, requestMethod, controller, method);
+                        }
+                    }
+                }
+            }
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void createHandlerKeyAndExecution(RequestMapping requestMapping, RequestMethod requestMethod,
+                                              Class<?> controller, Method method) {
+        String url = requestMapping.value(); // RequestMapping 의 url 을 꺼낸다.
+        HandlerKey handlerKey = new HandlerKey(url, requestMethod); // url, method 를 HandlerKey 로 만든다.
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method); // HandlerExecution 을 만든다.
+
+        log.debug("requestMapping = {}", requestMapping);
+        log.debug("handlerKey = {}\n", handlerKey);
+
+        handlerExecutions.put(handlerKey, handlerExecution); // HandlerKey 에 해당하는 HandlerExecution 을 맵에 저장한다.
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        // request 에 따라서 HandlerExecution 을 반환해야함.
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,24 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Class<?> controller;
+    private final Method method;
+
+    public HandlerExecution(Class<?> controller, Method method) {
+        this.controller = controller;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        // request 의 uri, method 를 처리하는 TestController 메서드 실행
+        Object controllerInstance = controller.getDeclaredConstructor().newInstance();
+
+        return (ModelAndView) method.invoke(controllerInstance, request, response);
     }
 }
