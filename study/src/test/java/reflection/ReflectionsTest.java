505d9c090f53a2385284854c6ba1024626e6aa47
reddevilmidzy <midzy0228@gmail.com>
2024-09-17 02:16:37 +0900

    test: @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,22 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+import reflection.examples.JdbcQuestionRepository;
+import reflection.examples.JdbcUserRepository;
+import reflection.examples.MyQnaService;
+import reflection.examples.QnaController;
 
 class ReflectionsTest {
 
@@ -11,8 +24,23 @@ class ReflectionsTest {
 
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+        final Reflections reflections = new Reflections("reflection.examples");
+        final Set<Class<?>> expected = Set.of(
+                JdbcQuestionRepository.class,
+                QnaController.class,
+                MyQnaService.class,
+                JdbcUserRepository.class
+        );
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        for (final Class<?> aClass : classes) {
+            log.info(aClass.getName());
+        }
+
+        assertThat(classes).isEqualTo(expected);
     }
 }
