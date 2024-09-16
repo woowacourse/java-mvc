5337cc251d2f0a15dfe3dd4451fc31cb9a41d35e
unifolio0 <boho82ha@gmail.com>
2024-09-16 15:52:59 +0900

    refactor: 불필요한 변수 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKeyExtractor.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKeyExtractor.java
@@ -3,22 +3,20 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class HandlerKeyExtractor {
 
     public static List<HandlerKey> extract(Method method) {
-        List<HandlerKey> handlerKeys = new ArrayList<>();
         RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
         String value = requestMapping.value();
         RequestMethod[] requestMethods = requestMapping.method();
         if (requestMethods.length == 0) {
             requestMethods = RequestMethod.values();
         }
-        Arrays.stream(requestMethods)
-                .forEach(requestMethod -> handlerKeys.add(new HandlerKey(value, requestMethod)));
-        return handlerKeys;
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(value, requestMethod))
+                .toList();
     }
 }
