362af87b2b91a1f6655d3a8005b49b8eb2cc685b
Arachneee <jhg2819@naver.com>
2024-09-16 14:08:16 +0900

    style: 변수 명 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -7,14 +7,14 @@ import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    private final Method method;
+    private final Method handler;
 
-    public HandlerExecution(Method method) {
-        this.method = method;
+    public HandlerExecution(Method handler) {
+        this.handler = handler;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        Object clazz = method.getDeclaringClass().getConstructor().newInstance();
-        return (ModelAndView) method.invoke(clazz, request, response);
+        Object controller = handler.getDeclaringClass().getConstructor().newInstance();
+        return (ModelAndView) handler.invoke(controller, request, response);
     }
 }
