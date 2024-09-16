2a4827ff90b4ee243f0d698971aea7ec4f5c1261
3juhwan <13selfesteem91@naver.com>
2024-09-17 00:57:19 +0900

    feat: 핸들러 객체 생성

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/Handler.java
@@ -0,0 +1,34 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+public class Handler {
+
+    private final Method handler;
+    private final Object instance;
+
+    public Handler(Method handler, Object instance) {
+        this.handler = handler;
+        this.instance = instance;
+    }
+
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response)
+        throws Exception {
+        return (ModelAndView) handler.invoke(instance, request, response);
+    }
+
+    public RequestMethod[] getRequestMethods() {
+        RequestMapping requestMapping = handler.getDeclaredAnnotation(RequestMapping.class);
+        return requestMapping.method();
+    }
+
+    public String getUri() {
+        RequestMapping requestMapping = handler.getDeclaredAnnotation(RequestMapping.class);
+        return requestMapping.value();
+    }
+}
