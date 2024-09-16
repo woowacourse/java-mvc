039b88263162dafc42a3561f691dc0f22ab6415e
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-09-16 22:58:46 +0900

    study: Junit4TestRunner 학습 테스트 진행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,19 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
-
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Class clazz = Junit4Test.class;
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
