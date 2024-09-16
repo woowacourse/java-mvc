46a3705cb7f6533e4a423383e839ad65638ec1f8
haiseong <wjdgotjd9908@gmail.com>
2024-09-16 16:08:20 +0900

    feat: HandlerExecution 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -3,10 +3,66 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class HandlerExecution {
 
+    public static final int VALID_PARAMETER_LENGTH = 2;
+    public static final int REQUEST_PARAMETER_INDEX = 0;
+    public static final int RESPONSE_PARAMETER_INDEX = 1;
+    private final Object handler;
+    private final Method method;
+
+    public HandlerExecution(Object handler, Method method) {
+        validateParameters(method.getParameters());
+        validateReturnType(method.getReturnType());
+
+        this.handler = handler;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        Object invokeResult = method.invoke(handler, request, response);
+        validateInvokeResult(invokeResult);
+
+        return (ModelAndView) invokeResult;
+    }
+
+
+    private void validateParameters(Parameter[] parameters) {
+        validateParametersLength(parameters);
+        validateRequestParameter(parameters[REQUEST_PARAMETER_INDEX]);
+        validateResponseParameter(parameters[RESPONSE_PARAMETER_INDEX]);
+    }
+
+    private void validateParametersLength(Parameter[] parameters) {
+        if (parameters.length != VALID_PARAMETER_LENGTH) {
+            throw new IllegalArgumentException("핸들러 메소드의 인자수가 잘못되었습니다.");
+        }
+    }
+
+    private void validateRequestParameter(Parameter parameter) {
+        if (parameter.getType() != HttpServletRequest.class) {
+            throw new IllegalArgumentException("핸들러 메소드의 첫번째 인자가 HttpServletRequest 타입이 아닙니다.");
+        }
+    }
+
+    private void validateResponseParameter(Parameter parameter) {
+        if (parameter.getType() != HttpServletResponse.class) {
+            throw new IllegalArgumentException("핸들러 메소드의 두번째 인자가 HttpServletResponse 타입이 아닙니다.");
+        }
+    }
+
+    private void validateReturnType(Class<?> returnType) {
+        if (returnType != ModelAndView.class) {
+            throw new IllegalArgumentException("핸들러 메소드의 반환 타입이 ModelAndView 타입이 아닙니다.");
+        }
+    }
+
+    private void validateInvokeResult(Object invokeResult) {
+        if (!(invokeResult instanceof ModelAndView)) {
+            throw new IllegalStateException("핸들러 메소드가 ModelAndView 타입을 반환하지 않았습니다.");
+        }
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionTest.java
@@ -0,0 +1,119 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.Test;
+
+class HandlerExecutionTest {
+
+    private final TestController controller = new TestController();
+
+    @Test
+    void handle() throws Exception {
+        Method method = TestController.class.getMethod(
+                "validParametersAndReturn",
+                HttpServletRequest.class,
+                HttpServletResponse.class
+        );
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+
+        ModelAndView result = handlerExecution.handle(mock(HttpServletRequest.class), mock(HttpServletResponse.class));
+
+        assertThat(result.getView()).isInstanceOf(View.class);
+    }
+
+    @Test
+    void createHandlerExecutionWhenInvalidParameterCount() throws NoSuchMethodException {
+        Method method = TestController.class.getMethod("invalidParameterCount");
+
+        assertThatThrownBy(() -> new HandlerExecution(controller, method))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("핸들러 메소드의 인자수가 잘못되었습니다.");
+    }
+
+    @Test
+    void createHandlerExecutionWhenInvalidRequestParameterType() throws NoSuchMethodException {
+        Method method = TestController.class.getMethod(
+                "invalidRequestParameterType",
+                String.class,
+                HttpServletResponse.class
+        );
+
+        assertThatThrownBy(() -> new HandlerExecution(controller, method))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("핸들러 메소드의 첫번째 인자가 HttpServletRequest 타입이 아닙니다.");
+    }
+
+    @Test
+    void createHandlerExecutionWhenInvalidResponseParameterType() throws NoSuchMethodException {
+        Method method = TestController.class.getMethod(
+                "invalidResponseParameterType",
+                HttpServletRequest.class,
+                String.class
+        );
+
+        assertThatThrownBy(() -> new HandlerExecution(controller, method))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("핸들러 메소드의 두번째 인자가 HttpServletResponse 타입이 아닙니다.");
+    }
+
+    @Test
+    void createHandlerExecutionWhenInvalidReturnType() throws NoSuchMethodException {
+        Method method = TestController.class.getMethod(
+                "invalidReturnType",
+                HttpServletRequest.class,
+                HttpServletResponse.class
+        );
+
+        assertThatThrownBy(() -> new HandlerExecution(controller, method))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("핸들러 메소드의 반환 타입이 ModelAndView 타입이 아닙니다.");
+    }
+
+    @Test
+    void handleWhenInvalidInvokeResult() throws Exception {
+        Method method = TestController.class.getMethod(
+                "invalidInvokeResult",
+                HttpServletRequest.class,
+                HttpServletResponse.class
+        );
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+
+        assertThatThrownBy(
+                () -> handlerExecution.handle(mock(HttpServletRequest.class), mock(HttpServletResponse.class)))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("핸들러 메소드가 ModelAndView 타입을 반환하지 않았습니다.");
+    }
+
+
+    static class TestController {
+
+        public ModelAndView validParametersAndReturn(HttpServletRequest request, HttpServletResponse response) {
+            return new ModelAndView(mock(View.class));
+        }
+
+        public void invalidParameterCount() {
+        }
+
+        public void invalidRequestParameterType(String request, HttpServletResponse response) {
+        }
+
+        public void invalidResponseParameterType(HttpServletRequest request, String response) {
+        }
+
+        public String invalidReturnType(HttpServletRequest request, HttpServletResponse response) {
+            return "invalid";
+        }
+
+        public ModelAndView invalidInvokeResult(HttpServletRequest request, HttpServletResponse response) {
+            return null;
+        }
+    }
+}
