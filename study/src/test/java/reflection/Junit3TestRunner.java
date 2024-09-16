8478b2bc98f37ff3abd1bc674d02d162d32cd064
임수빈 <robin980108@naver.com>
2024-09-16 20:34:02 +0900

    학습 테스트 추가

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance(); // 클래스로부터 생성자를 가져오고, 객체를 생성한다.
+        Method[] methods = clazz.getMethods(); // 객체의 메서드를 가져온다. 상속된 메서드도 포함한다.
+        List<Method> testMethods = Arrays.stream(methods)
+                .filter(method -> method.getName().contains("test"))
+                .toList();
+
+        for (Method method : testMethods) {
+            method.invoke(junit3Test); // 메서드를 실행한다. 첫번째 매개변수는 메서드를 실행할 객체를 넘겨준다.
+        }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +12,14 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> myTestAnnotationedMethods = Arrays.stream(clazz.getDeclaredMethods()) // 상속된 메서드를 제외하고
+                .filter(method -> method.isAnnotationPresent(MyTest.class)) // 메서드에 어노테이션이 달린 것을 찾는다.
+                .toList();
+
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method myTestAnnotationedMethod : myTestAnnotationedMethods) {
+            myTestAnnotationedMethod.invoke(junit4Test);
+        }
     }
 }
