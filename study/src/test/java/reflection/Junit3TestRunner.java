d7388ee126374515346dea8139e85f853588f7ad
Gyeongho Yang <chchch1213@naver.com>
2024-09-16 18:29:25 +0900

    test: do basic reflection tests junit 3 and 4

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,13 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+        String prefix = "test";
+        for (Method method : methods) {
+            if (method.getName().startsWith(prefix)) {
+                method.invoke(instance);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +11,16 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
+        for (Method method : methods) {
+            Annotation[] annotations = method.getDeclaredAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType() == MyTest.class) {
+                    method.invoke(instance);
+                    break;
+                }
+            }
+        }
     }
 }
