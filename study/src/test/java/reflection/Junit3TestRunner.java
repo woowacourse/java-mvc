0d3b4916aec6567bfff210935dd91f837bf5962e
3juhwan <13selfesteem91@naver.com>
2024-09-16 13:19:44 +0900

    feat: Reflection 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +10,17 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = new Junit3Test();
+
+        int count = 0;
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+                count++;
+            }
+        }
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        assertThat(count).isEqualTo(2);
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +10,17 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = new Junit4Test();
+
+        int count = 0;
+        for (Method method : clazz.getMethods()) {
+            if (method.getDeclaredAnnotation(MyTest.class) != null) {
+                method.invoke(junit4Test);
+                count++;
+            }
+        }
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        assertThat(count).isEqualTo(2);
     }
 }
