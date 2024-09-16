b60a4c74556f0aeba7c23cc6f6b45cb808b4a52a
fromitive <fromitive@gmail.com>
2024-09-16 14:23:24 +0900

    test: junit3Test에서 test로 시작하는 메서드 실행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,36 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // given
+        Junit3Test obj = new Junit3Test();
+        Class<Junit3Test> classObj = Junit3Test.class;
+
+        // when
+        Method test1 = classObj.getDeclaredMethod("test1");
+        Method test2 = classObj.getDeclaredMethod("test2");
+
+        test1.invoke(obj); // obj.test1();
+        test2.invoke(obj); // obj.test2();
+
+        String actual = outputStream.toString();
+
+        // then
+        assertThat(actual).contains("Running Test1", "Running Test2");
     }
 }
