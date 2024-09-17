3fab83be227d6ed9bbc8a129e8dad62399586334
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-18 00:25:58 +0900

    feat: JspView 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -11,21 +11,29 @@ import java.util.Map;
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
+    private static final String REDIRECT_PREFIX = "redirect:";
 
-    public static final String REDIRECT_PREFIX = "redirect:";
+    private final String viewName;
 
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return;
+        }
+        setModelAttribute(model, request);
+        request.getRequestDispatcher(viewName).forward(request, response);
+    }
 
-        model.keySet().forEach(key -> {
-            log.debug("attribute name : {}, value : {}", key, model.get(key));
-            request.setAttribute(key, model.get(key));
-        });
-
-        // todo
+    private void setModelAttribute(final Map<String, ?> model, final HttpServletRequest request) {
+        model.keySet()
+                .forEach(key -> {
+                    log.debug("attribute name : {}, value : {}", key, model.get(key));
+                    request.setAttribute(key, model.get(key));
+                });
     }
 }
