b110fb4ba97d105f4f45098c33e640f3ce603ab6
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-17 22:47:53 +0900

    test: 리플렉션 러닝테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,28 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
+    @DisplayName("Junit3Test에서 test로 시작하는 메소드 실행")
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        //public getMethod -> public 만 가져옴
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(testMethod-> {
+                    try {
+                        testMethod.invoke(instance);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,25 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
+    @DisplayName("Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행")
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
-
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Class<Junit3Test> clazz = Junit3Test.class;
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(testMethod-> {
+                    try {
+                        testMethod.invoke(instance);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
