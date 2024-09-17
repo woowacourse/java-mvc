d16145a8c7e46edb65abd5d0c9f16ad417f725ec
hyeonjilee <skylar12200@gmail.com>
2024-09-18 08:42:16 +0900

    feat: View 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -11,21 +11,31 @@ import java.util.Map;
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
+    private static final String REDIRECT_PREFIX = "redirect:";
 
-    public static final String REDIRECT_PREFIX = "redirect:";
+    private final String viewName;
 
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
-
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        move(request, response);
+    }
+
+    private void move(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        final var requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
