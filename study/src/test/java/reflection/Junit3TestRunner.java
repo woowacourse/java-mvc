d64b5ad760af87e61ffdaa4b079e0eab25b2acf1
gyungchan Jo <namejgc@naver.com>
2024-09-17 17:59:45 +0900

    test: reflection 학습 테스트 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,8 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +11,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.startsWith("test")) {
+                Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+                method.invoke(instance);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import java.util.Set;
 
 class ReflectionsTest {
 
@@ -14,5 +19,12 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+
+        log.info("controller classes : {}", controllerClasses);
+        log.info("service classes : {}", serviceClasses);
+        log.info("repository classes : {}", repositoryClasses);
     }
 }
