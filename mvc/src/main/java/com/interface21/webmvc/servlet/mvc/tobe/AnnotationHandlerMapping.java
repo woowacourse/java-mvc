d56aab14e4badf31143f77f78d013f3205da34b8
fromitive <fromitive@gmail.com>
2024-09-17 23:02:50 +0900

    feat: AnnotationHandlerMapping 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,10 +31,65 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for(Class<?> controller : controllerClasses) {
+            searchHandlerExceptions(controller);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void searchHandlerExceptions(Class<?> controller){
+        List<Method> mappedMethods = Arrays.stream(controller.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+        handlerExecutions.putAll(createHandlerExecutions(controller, mappedMethods));
+    }
+
+    private Map<HandlerKey, HandlerExecution> createHandlerExecutions(Class<?> controller, List<Method> mappedMethods) {
+        return mappedMethods.stream()
+                .flatMap(method -> createHandlerKeys(method).stream()
+                        .map(mapper -> Map.entry(mapper, method)))
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        entry -> createHandlerExecution(controller, entry.getValue())));
+    }
+
+    private List<HandlerKey> createHandlerKeys(Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        String uri = annotation.value();
+        RequestMethod[] requestMethods = annotation.method();
+        if(requestMethods.length == 0) {
+            return generateFromAllRequestMethods(uri);
+        }
+        return generateFromRequestMethod(uri,requestMethods);
+    }
+
+    private HandlerExecution createHandlerExecution(Class<?> controller, Method targetMethod) {
+        try {
+            Constructor<?> firstConstructor = controller.getDeclaredConstructor();
+            Object executionTarget = firstConstructor.newInstance();
+            return new HandlerExecution(executionTarget,targetMethod);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("메서드 타입이 다른 controller로 초기화 했습니다.");
+        }
+    }
+
+    private List<HandlerKey> generateFromAllRequestMethods(String uri) {
+        return Arrays.stream(RequestMethod.values())
+                .map(requestMethod -> new HandlerKey(uri, requestMethod))
+                .toList();
+    }
+
+    private List<HandlerKey> generateFromRequestMethod(String uri, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(uri,requestMethod))
+                .toList();
+    }
+
+    public Object getHandler(HttpServletRequest request) {
+        HandlerKey key = new HandlerKey(request.getRequestURI().toString(),
+                RequestMethod.findMethod(request.getMethod()));
+        return handlerExecutions.get(key);
     }
 }
