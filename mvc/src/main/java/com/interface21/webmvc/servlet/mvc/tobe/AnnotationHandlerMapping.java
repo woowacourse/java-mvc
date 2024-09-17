4dbef2ef22aa2b34c4e271f3a475187ffe20ba98
hyeonjilee <skylar12200@gmail.com>
2024-09-17 17:52:03 +0900

    test: handlerMapping 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import javassist.NotFoundException;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -23,7 +30,27 @@ public class AnnotationHandlerMapping {
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    public Object getHandler(final HttpServletRequest request) throws NotFoundException {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder
+                .setUrls(ClasspathHelper.forPackage("samples"))
+                .setScanners(new MethodAnnotationsScanner()); // TODO: deprecated
+
+        Reflections reflections = new Reflections(configurationBuilder);
+        Set<Method> methods = reflections.getMethodsAnnotatedWith(RequestMapping.class);
+        Method targetMethod = methods.stream()
+                .filter(method -> method.getAnnotation(RequestMapping.class).value().equals(request.getRequestURI()))
+                .filter(method -> method.getAnnotation(RequestMapping.class).method()[0].name()
+                        .equals(request.getMethod()))
+                .findFirst() // TODO: 상수화
+                .orElseThrow(() -> new NotFoundException("No resource found"));
+
+        Object controller;
+        try {
+            controller = targetMethod.getDeclaringClass().getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Internal error: Failed to create controller instance", e);
+        }
+        return new HandlerExecution(controller, targetMethod);
     }
 }
