750339266ab80cca8d75369d8fe6ba68823f330c
libienz <leekh10195@gmail.com>
2024-09-18 02:45:04 +0900

    test: Junit4TestRunner 학습 테스트 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +8,13 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test testInstance = clazz.getDeclaredConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.setAccessible(true);
+                method.invoke(testInstance);
+            }
+        }
     }
 }
