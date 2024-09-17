6bb434352ef51849c909610de89aaa6c6cea2a62
libienz <leekh10195@gmail.com>
2024-09-18 02:43:26 +0900

    test: Junit3TestRunner 학습 테스트 완료

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +8,13 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test testInstance = clazz.getDeclaredConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.setAccessible(true);
+                method.invoke(testInstance);
+            }
+        }
     }
 }
