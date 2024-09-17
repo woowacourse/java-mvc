51d2a61be2a358878751f23770169e498405bd63
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-18 01:13:43 +0900

    test: 학습 테스트 완료

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -1,75 +1,103 @@
 package reflection;
 
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class ReflectionTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionTest.class);
 
+    @DisplayName("클래스 객체로부터 SimpleName, Name, CanonicalName을 알 수 있다")
     @Test
     void givenObject_whenGetsClassName_thenCorrect() {
         final Class<Question> clazz = Question.class;
 
-        assertThat(clazz.getSimpleName()).isEqualTo("");
-        assertThat(clazz.getName()).isEqualTo("");
-        assertThat(clazz.getCanonicalName()).isEqualTo("");
+        String expectedSimpleName = "Question";  // 느슨하게 식별(unique 보장 x)
+        String expectedName = "reflection.Question"; // ClassLoader scope에서 동적 로드 시 사용가능한 name unique 보장
+        String expectedCanonicalName = "reflection.Question"; // import문 포함
+
+        assertAll(
+                () -> assertThat(clazz.getSimpleName()).isEqualTo(expectedSimpleName),
+                () -> assertThat(clazz.getName()).isEqualTo(expectedName),
+                () -> assertThat(clazz.getCanonicalName()).isEqualTo(expectedCanonicalName)
+        );
     }
 
+    @DisplayName("Class.forName : 클래스명 기반 클래스 객체를 찾음")
     @Test
-    void givenClassName_whenCreatesObject_thenCorrect() throws ClassNotFoundException {
+    void givenClassName_whenCreatesObject_thenCorrect()
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         final Class<?> clazz = Class.forName("reflection.Question");
 
-        assertThat(clazz.getSimpleName()).isEqualTo("");
-        assertThat(clazz.getName()).isEqualTo("");
-        assertThat(clazz.getCanonicalName()).isEqualTo("");
+        Object object = clazz.newInstance();
+
+        String expectedSimpleName = object.getClass().getSimpleName();
+        String expectedName = object.getClass().getName();
+        String expectedCanonicalName = object.getClass().getCanonicalName();
+
+        assertAll(
+                () -> assertThat(clazz.getSimpleName()).isEqualTo(expectedSimpleName),
+                () -> assertThat(clazz.getName()).isEqualTo(expectedName),
+                () -> assertThat(clazz.getCanonicalName()).isEqualTo(expectedCanonicalName)
+        );
     }
 
+    @DisplayName("Class.getDeclaredFields : 클래스가 가진 필드 정보를 알 수 있다")
     @Test
     void givenObject_whenGetsFieldNamesAtRuntime_thenCorrect() {
         final Object student = new Student();
-        final Field[] fields = null;
-        final List<String> actualFieldNames = null;
+        final Field[] fields = student.getClass().getDeclaredFields();
+        final List<String> actualFieldNames = Stream.of(fields)
+                .map(Field::getName)
+                .toList();
 
         assertThat(actualFieldNames).contains("name", "age");
     }
 
+    @DisplayName("Class.getDeclaredMethods : 클래스가 가진 메서드 정보를 알 수 있다")
     @Test
     void givenClass_whenGetsMethods_thenCorrect() {
         final Class<?> animalClass = Student.class;
-        final Method[] methods = null;
-        final List<String> actualMethods = null;
+        final Method[] methods = animalClass.getDeclaredMethods();
+        final List<String> actualMethods = Stream.of(methods)
+                .map(Method::getName)
+                .toList();
 
         assertThat(actualMethods)
                 .hasSize(3)
                 .contains("getAge", "toString", "getName");
     }
 
+    @DisplayName("Class.getDeclaredConstructor : 클래스가 가진 생성자 정보를 알 수 있다")
     @Test
     void givenClass_whenGetsAllConstructors_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Constructor<?>[] constructors = null;
+        final Constructor<?>[] constructors = questionClass.getDeclaredConstructors();
 
         assertThat(constructors).hasSize(2);
     }
 
+    @DisplayName("생성자 객체를 기반으로 런타임에 동적인 객체 생성이 가능하다")
     @Test
     void givenClass_whenInstantiatesObjectsAtRuntime_thenCorrect() throws Exception {
         final Class<?> questionClass = Question.class;
 
-        final Constructor<?> firstConstructor = null;
-        final Constructor<?> secondConstructor = null;
+        final Constructor<?> firstConstructor = questionClass.getDeclaredConstructors()[0];
+        final Constructor<?> secondConstructor = questionClass.getConstructors()[1];
 
-        final Question firstQuestion = null;
-        final Question secondQuestion = null;
+        final Question firstQuestion = (Question) firstConstructor.newInstance("gugu", "제목1", "내용1");
+        final Question secondQuestion = (Question) secondConstructor.newInstance(1, "gugu", "제목2", "내용2", new Date(), 0);
 
         assertThat(firstQuestion.getWriter()).isEqualTo("gugu");
         assertThat(firstQuestion.getTitle()).isEqualTo("제목1");
@@ -79,51 +107,58 @@ class ReflectionTest {
         assertThat(secondQuestion.getContents()).isEqualTo("내용2");
     }
 
+    @DisplayName("getFields : public 필드만 가져온다. / getDeclaredFields : 접근제어자 무관")
     @Test
     void givenClass_whenGetsPublicFields_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Field[] fields = null;
+        final Field[] fields = questionClass.getFields();
 
         assertThat(fields).hasSize(0);
     }
 
+    @DisplayName("getDeclaredFields : 접근제어자 무관")
     @Test
     void givenClass_whenGetsDeclaredFields_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Field[] fields = null;
+        final Field[] fields = questionClass.getDeclaredFields();
 
         assertThat(fields).hasSize(6);
         assertThat(fields[0].getName()).isEqualTo("questionId");
     }
 
+    @DisplayName("getDeclaredField(String fieldName) 으로 필드를 가져올 수 있다")
     @Test
     void givenClass_whenGetsFieldsByName_thenCorrect() throws Exception {
+        final String fieldName = "questionId";
         final Class<?> questionClass = Question.class;
-        final Field field = null;
+        final Field field = questionClass.getDeclaredField(fieldName);
 
-        assertThat(field.getName()).isEqualTo("questionId");
+        assertThat(field.getName()).isEqualTo(fieldName);
     }
 
+    @DisplayName("Field.getType : 필드 클래스 타입를 가져온다")
     @Test
     void givenClassField_whenGetsType_thenCorrect() throws Exception {
         final Field field = Question.class.getDeclaredField("questionId");
-        final Class<?> fieldClass = null;
+        final Class<?> fieldClass = field.getType();
 
         assertThat(fieldClass.getSimpleName()).isEqualTo("long");
     }
 
+    @DisplayName("setAccessible : private 필드 접근을 허용한다")
     @Test
     void givenClassField_whenSetsAndGetsValue_thenCorrect() throws Exception {
         final Class<?> studentClass = Student.class;
-        final Student student = null;
-        final Field field = null;
+        final Student student = (Student) studentClass.newInstance();
+        final Field field = studentClass.getDeclaredField("age");
 
         // todo field에 접근 할 수 있도록 만든다.
+        field.setAccessible(true);
 
         assertThat(field.getInt(student)).isZero();
         assertThat(student.getAge()).isZero();
 
-        field.set(null, null);
+        field.set(student, 99);
 
         assertThat(field.getInt(student)).isEqualTo(99);
         assertThat(student.getAge()).isEqualTo(99);
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,46 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+import reflection.examples.JdbcQuestionRepository;
+import reflection.examples.JdbcUserRepository;
+import reflection.examples.MyQnaService;
+import reflection.examples.QnaController;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
+    @DisplayName("Reflections.getTypesAnnotatedWith : 어노테이션이 붙은 클래스 set을 알 수 있다.")
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> classes = new HashSet<>();
+
+        classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        classes.forEach(classInfo -> log.info("className : {} ", classInfo.getSimpleName()));
+
+        assertThat(classes).containsOnly(
+                JdbcQuestionRepository.class,
+                JdbcUserRepository.class,
+                MyQnaService.class,
+                QnaController.class
+        );
+
+
     }
 }
