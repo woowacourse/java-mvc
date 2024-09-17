c8b9922a1fe35df30e7097d332af9f85be2fc1c5
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-18 05:24:26 +0900

    refactor: AnnotationHandlerMapping 책임 HandlerExectutions에 위임

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,8 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.context.stereotype.Controller;
-import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final Class<Controller> CONTROLLER_ANNOTATION = Controller.class;
 
     private final Object[] basePackage;
     private final HandlerExecutions handlerExecutions;
@@ -27,33 +25,15 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         Reflections reflections = new Reflections(basePackage);
-        List<Object> controllers = reflections.getTypesAnnotatedWith(Controller.class).stream()
+        List<Object> controllers = reflections.getTypesAnnotatedWith(CONTROLLER_ANNOTATION).stream()
                 .map(instancePool::getInstance)
                 .toList();
-        controllers.forEach(this::setHandlerExecutions);
+        controllers.forEach(handlerExecutions::setHandlerExecutions);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void setHandlerExecutions(Object controller) {
-        Arrays.stream(controller.getClass().getMethods())
-                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                .toList()
-                .forEach(method -> setHandlerExecutions(controller, method));
-    }
-
-    private void setHandlerExecutions(Object controller, Method method) {
-        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-        String url = requestMapping.value();
-        RequestMethod[] methods = requestMapping.method();
-        for (RequestMethod requestMethod : methods) {
-            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
-            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
-            handlerExecutions.save(handlerKey, handlerExecution);
-        }
-    }
 
     public Object getHandler(final HttpServletRequest request) {
-        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
-        return handlerExecutions.find(handlerKey);
+        return handlerExecutions.findByUrlAndMethod(request.getRequestURI(), RequestMethod.from(request.getMethod()));
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutions.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutions.java
@@ -1,18 +1,40 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class HandlerExecutions {
 
+    private static final Class<RequestMapping> HANDLER_ANNOTATION = RequestMapping.class;
+
     private final Map<HandlerKey, HandlerExecution> handlerExecutions = new HashMap<>();
 
-    public void save(HandlerKey handlerKey, HandlerExecution handlerExecution) {
-        handlerExecutions.put(handlerKey, handlerExecution);
+
+    public void setHandlerExecutions(Object controller) {
+        Arrays.stream(controller.getClass().getMethods())
+                .filter(method -> method.isAnnotationPresent(HANDLER_ANNOTATION))
+                .toList()
+                .forEach(method -> setHandlerExecutions(controller, method));
     }
 
-    public HandlerExecution find(HandlerKey handlerKey) {
+    private void setHandlerExecutions(Object controller, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(HANDLER_ANNOTATION);
+        String url = requestMapping.value();
+        RequestMethod[] methods = requestMapping.method();
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+        for (RequestMethod requestMethod : methods) {
+            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    public HandlerExecution findByUrlAndMethod(String url, RequestMethod method) {
+        HandlerKey handlerKey = new HandlerKey(url, method);
         if (handlerExecutions.containsKey(handlerKey)) {
             return handlerExecutions.get(handlerKey);
         }
