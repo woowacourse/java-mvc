b16e0ea6d255e195eb4cc29e7aa11ea6ff537fec
eunjungL <dldmswjd322@gmail.com>
2024-09-18 02:15:15 +0900

    test: Junit3, Junit4, Reflection, Reflections 학습 테스트 풀이

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,21 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() throws IllegalAccessException, InvocationTargetException {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] methods = clazz.getMethods();
+        Junit3Test junit3Test = new Junit3Test();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
