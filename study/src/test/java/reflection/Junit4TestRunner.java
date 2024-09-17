91ab357e93c631a11b970b5e9783aa55fa627b1f
Eunhee Baek <78892355+ehBeak@users.noreply.github.com>
2024-09-17 20:10:20 +0900

    test(Junit4TestRunner): 어노테이션이 선언된 메소드 실행 테스트

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +13,16 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        List<String> methodNames = new ArrayList<>();
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                methodNames.add(method.getName());
+                method.invoke(junit4Test);
+            }
+        }
+
+        Assertions.assertThat(methodNames).containsExactlyInAnyOrder("one", "two");
     }
 }
