0ea9574f8ec403a5c2a92cc7a33bb91747bb37ba
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-18 16:37:33 +0900

    test: Junit3TestRunner 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +9,16 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Constructor<Junit3Test> constructor = clazz.getConstructor();
+        Junit3Test junit3Test = constructor.newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
+
