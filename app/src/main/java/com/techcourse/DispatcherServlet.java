bac8826f891685ef94858e85289fd11ea2c73a3d
Arachneee <jhg2819@naver.com>
2024-09-18 16:10:11 +0900

    feat: Legacy MVC와 @MVC 통합기능 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,6 +2,9 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -17,6 +20,7 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private ManualHandlerMapping manualHandlerMapping;
+    private AnnotationHandlerMapping annotationHandlerMapping;
 
     public DispatcherServlet() {
     }
@@ -25,27 +29,50 @@ public class DispatcherServlet extends HttpServlet {
     public void init() {
         manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+
+        String packageName = getClass().getPackageName();
+        annotationHandlerMapping = new AnnotationHandlerMapping(packageName);
+        annotationHandlerMapping.initialize();
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            Object handler = getHandler(request);
+            ModelAndView modelAndView = handle(handler, request, response);
+            move(modelAndView, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(String viewName, HttpServletRequest request, HttpServletResponse response)
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        try {
+            return annotationHandlerMapping.getHandler(request);
+        } catch (IllegalArgumentException e) {
+            log.info("annotationHandlerMapping에 존재하지 않는 요청 : {} {}", request.getMethod(), requestURI);
+        }
+        return manualHandlerMapping.getHandler(requestURI);
+    }
+
+    private ModelAndView handle(Object handler, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        ModelAndView modelAndView = new ModelAndView(new JspView(viewName));
+        if (handler instanceof HandlerExecution handlerExecution) {
+            return handlerExecution.handle(request, response);
+        }
+        if (handler instanceof Controller controller) {
+            String viewName = controller.execute(request, response);
+            return new ModelAndView(new JspView(viewName));
+        }
+        throw new IllegalArgumentException("지원하지 않는 handler 입니다.");
+    }
+
+    private void move(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
         Map<String, Object> model = modelAndView.getModel();
         View view = modelAndView.getView();
         view.render(model, request, response);
