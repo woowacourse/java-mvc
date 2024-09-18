6cf979d971d1185cfd39741a3ae7a033917768bf
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-19 01:25:04 +0900

    feat: jspView의 view로직 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,31 +1,58 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 public class JspView implements View {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
     public static final String REDIRECT_PREFIX = "redirect:";
+    private static final String NOT_FOUND_VIEW = "404.jsp";
+
+    private final String viewName;
 
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return;
+        }
 
+        setModelAttribute(model, request);
+        forwardRequest(request, response);
+    }
+
+    private void setModelAttribute(Map<String, ?> model, HttpServletRequest request) {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
+    }
 
-        // todo
+    private void forwardRequest(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+        // RequestDispatcher : 이전 요청의 파라미터 정보를 보존하여 요청
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+
+        if (requestDispatcher == null) {
+            response.sendRedirect(NOT_FOUND_VIEW);
+            return;
+        }
+        requestDispatcher.forward(request, response);
     }
 }
