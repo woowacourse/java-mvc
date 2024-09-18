686834f0b9d38a51e370e8d948c3af8227be181c
hyxrxn <gpfls981220@gmail.com>
2024-09-18 18:00:44 +0900

    test: Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +13,15 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        Stream<Method> tests = Arrays.stream(methods).filter(method -> method.isAnnotationPresent(MyTest.class));
+
+        tests.forEach(test -> {
+            try {
+                test.invoke(new Junit4Test());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
