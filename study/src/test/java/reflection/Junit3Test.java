16cde12693edfe680bed6c4518d08ec88a32a53c
le2sky <leehaneul990623@gmail.com>
2024-09-18 14:02:43 +0900

    test: junit3 학습 테스트 구현

--- study/src/test/java/reflection/Junit3Test.java
+++ study/src/test/java/reflection/Junit3Test.java
@@ -1,6 +1,13 @@
 package reflection;
 
 public class Junit3Test {
+
+    protected Junit3Test() {
+    }
+
+    public Junit3Test(String string1, String string2) {
+    }
+
     public void test1() throws Exception {
         System.out.println("Running Test1");
     }
--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +12,21 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method declaredMethod : clazz.getDeclaredMethods()) {
+            String methodName = declaredMethod.getName();
+            if (methodName.startsWith("test")) {
+                declaredMethod.invoke(junit3Test);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("declared constructor는 주어진 클래스 파라미터에 해당하는 생성자를 반환한다.")
+    void declaredConstructor() throws Exception {
+        Class<Junit3Test> clazz = Junit3Test.class;
+        Constructor<Junit3Test> constructor = clazz.getConstructor(String.class, String.class);
+        Junit3Test junit3Test = constructor.newInstance("hello", "world");
     }
 }
