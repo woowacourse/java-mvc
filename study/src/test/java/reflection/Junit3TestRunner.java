f45219783c5ddaad8990e1af67b70347e72e7682
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-18 16:42:55 +0900

    test(Junit3TestRunner): 1단계 학습 테스트 완료

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -7,7 +7,6 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        clazz.getMethod("test1").invoke(clazz.getDeclaredConstructor().newInstance());
     }
 }
