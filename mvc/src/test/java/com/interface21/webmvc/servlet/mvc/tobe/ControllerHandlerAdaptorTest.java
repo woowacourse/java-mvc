262ce72792d496411b43d47b45583f5fe3a73af8
Arachneee <jhg2819@naver.com>
2024-09-18 17:33:08 +0900

    test: 어뎁터 테스트 추가

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerHandlerAdaptorTest.java
@@ -0,0 +1,38 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+class ControllerHandlerAdaptorTest {
+
+    @DisplayName("Controller의 반환값 String을 이름으로 하는  JspView로 변환시킨다.")
+    @Test
+    void test() throws Exception {
+        HandlerAdaptor adaptor = new ControllerHandlerAdaptor();
+        Controller controller = (request, response) -> "aaa";
+
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpServletResponse response = new MockHttpServletResponse();
+
+        ModelAndView modelAndView = adaptor.handle(request, response, controller);
+
+        View view = modelAndView.getView();
+        assertAll(
+                () -> assertThat(view).isInstanceOf(JspView.class),
+                () -> assertThat(((JspView) view)).extracting("viewName").isEqualTo("aaa")
+        );
+
+    }
+
+}
