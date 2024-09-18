b2f7d24a95e162b3c58da82db3ca35f2ed3c91ad
coli-geonwoo <coli-geonwoo@gmail.com>
2024-09-19 00:26:14 +0900

    feat: handleExecution 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,26 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private final Method method;
+    private final Object handler;
+
+    public HandlerExecution(Method method) {
+        try {
+            this.method = method;
+            this.handler = method.getDeclaringClass()
+                    .getDeclaredConstructor()
+                    .newInstance();
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("핸들러를 초기화할 수 없습니다.", exception);
+        }
+    }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(handler, request, response);
     }
 }
