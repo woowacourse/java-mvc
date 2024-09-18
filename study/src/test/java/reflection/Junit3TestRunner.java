8aff47bee7429c66a30c13a3fc3d891830efa8d3
tkdgur0906 <tkdgur0906@naver.com>
2024-09-18 18:13:02 +0900

    test: Junit3TestRunner 학습 테스트 작성

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,18 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(new Junit3Test());
+            }
+        }
     }
 }
