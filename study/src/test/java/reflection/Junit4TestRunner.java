e9f18d3c04c430b1a6532b6cbad74f0927f7750e
le2sky <leehaneul990623@gmail.com>
2024-09-18 14:14:01 +0900

    test: junit4 test isAnnotationPresent 실습

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.stream;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -16,6 +17,22 @@ class Junit4TestRunner {
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
         List<Method> methods = stream(clazz.getDeclaredMethods())
                 .filter(it -> stream(it.getDeclaredAnnotations()).anyMatch(an -> an instanceof MyTest))
+                .toList();
+
+        for (Method it : methods) {
+            it.invoke(junit4Test);
+        }
+    }
+
+    @Test
+    @DisplayName("isAnnotationPresent")
+    void isAnnotationPresent() throws Exception {
+        Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        List<Method> methods = stream(clazz.getDeclaredMethods())
+                .filter(it -> it.isAnnotationPresent(MyTest.class))
                 .toList();
 
         for (Method it : methods) {
