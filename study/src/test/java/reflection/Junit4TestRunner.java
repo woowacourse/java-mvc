a2bd228d104bc115657d93a8014c420296523994
fram1998 <koust6u@pusan.ac.kr>
2024-09-18 22:52:07 +0900

    test: 학습 테스트 검증 테스트 추가

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.stream.Stream;
@@ -9,11 +13,26 @@ import org.junit.jupiter.api.Test;
 class Junit4TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() {
         final Class<Junit4Test> clazz = Junit4Test.class;
         Stream.of(clazz.getMethods())
                 .filter(this::hasAnnotation)
-                .forEach(method -> invokeMethod(clazz,method));
+                .forEach(method -> invokeMethod(clazz, method));
+    }
+
+    @Test
+    void assert_junit4_runner() {
+        // given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        // when
+        run();
+
+        // then
+        assertThat(outputStream)
+                .hasToString("Running Test1" + System.lineSeparator() + "Running Test2" + System.lineSeparator());
     }
 
     private boolean hasAnnotation(final Method method) {
@@ -24,7 +43,8 @@ class Junit4TestRunner {
         try {
             final Object instance = clazz.getDeclaredConstructor().newInstance();
             method.invoke(instance);
-        } catch (final NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+        } catch (final NoSuchMethodException | InvocationTargetException | InstantiationException |
+                       IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
