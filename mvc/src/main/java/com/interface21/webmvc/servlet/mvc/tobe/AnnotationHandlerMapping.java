eac4297bc5c07609da0b75f54056d04df1999f23
H <99153215+hyeon0208@users.noreply.github.com>
2024-09-19 01:09:00 +0900

    [MVC 구현하기 - 1단계] 카키(서현준) 미션 제출합니다. (#644)
    * test: 리플렉션, 서블릿 학습 테스트 추가
    
    * feat: 리플렉션을 사용해 핸들러 매핑 기능 추가
    
    * feat: JspView 클래스를 구현 및 View 반환 로직 수정
    
    * refactor: 패키지 스캔 로직 분리
    
    * refactor: handler 조회시 null 예외 처리
    
    * refactor: 상수 접근제어자 private로 변경
    
    * refactor: method.invoke() 첫 번째 인자를 Object 타입으로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +28,52 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        scanRequestMappings();
+    }
+
+    private void scanRequestMappings() {
+        Arrays.stream(basePackage)
+                .forEach(this::scanControllerBy);
+    }
+
+    private void scanControllerBy(Object packageName) {
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        controllerClasses.forEach(this::scanRequestMappingMethod);
+    }
+
+    private void scanRequestMappingMethod(Class<?> clazz) {
+        try {
+            Object controller = clazz.getDeclaredConstructor().newInstance();
+            Arrays.stream(clazz.getMethods())
+                    .filter(this::isRequestMappingMethod)
+                    .forEach(method -> addHandlerExecution(controller, method));
+        } catch (Exception e) {
+            log.error(clazz.getName() + " 컨트롤러를 생성하지 못했습니다. ", e);
+        }
+    }
+
+    private boolean isRequestMappingMethod(Method method) {
+        return method.isAnnotationPresent(RequestMapping.class);
+    }
+
+    private void addHandlerExecution(Object controller, Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        RequestMethod[] requestMethods = annotation.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(annotation.value(), requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution(controller, method));
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerExecution handler = handlerExecutions.get(new HandlerKey(requestURI, requestMethod));
+        if (handler == null) {
+            String invalidRequestInfo = String.join(" ", requestMethod.name(), requestURI);
+            throw new IllegalArgumentException("해당 요청을 처리할 수 있는 핸들러가 존재하지 않습니다. : " + invalidRequestInfo);
+        }
+        return handler;
     }
 }
