4c206777367fb74e3c4fd565fb4cf2a28f88531c
hyxrxn <gpfls981220@gmail.com>
2024-09-18 18:03:43 +0900

    test: 요구하는 클래스 로그로 출력
    - 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어있는 클래스

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,13 @@
 package reflection;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +18,13 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        controllers.forEach(controller -> log.info(controller.toString()));
+
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        services.forEach(service -> log.info(service.toString()));
+
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+        repositories.forEach(repository -> log.info(repository.toString()));
     }
 }
