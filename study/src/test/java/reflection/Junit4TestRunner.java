7606c6f81333ca25d537e83ead117bcd768998e5
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-18 16:43:19 +0900

    test(Junit4TestRunner): 2단계 학습 테스트 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +10,18 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        Arrays.stream(declaredMethods)
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> invoke(method, junit4Test));
+    }
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+    private void invoke(Method method, Junit4Test junit4Test) {
+        try {
+            method.invoke(junit4Test);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        };
     }
 }
