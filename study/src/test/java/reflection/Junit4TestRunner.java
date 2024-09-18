c66554de39cf49069087d25681a4d1f839bb21ca
jhon3242 <rldotnr2@gmail.com>
2024-09-18 11:22:37 +0900

    test: Junit4 학습 테스트 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +9,15 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Constructor<Junit4Test> constructor = clazz.getConstructor();
+        Junit4Test junit4Test = constructor.newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            MyTest annotation = declaredMethod.getAnnotation(MyTest.class);
+            if (annotation != null) {
+                declaredMethod.invoke(junit4Test);
+            }
+        }
     }
 }
