d885ad47e6f17d75a0c836aa691f14a9c141c13e
le2sky <leehaneul990623@gmail.com>
2024-09-18 15:18:04 +0900

    fix: 불필요한 type scanner 제거

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -2,7 +2,6 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reflection.annotation.Controller;
@@ -20,7 +19,7 @@ class ReflectionsTest {
     //  implementation 'org.reflections:reflections:0.10.2' 이곳에 있는 걸로 보임
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples", Scanners.TypesAnnotated);
+        Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
 
