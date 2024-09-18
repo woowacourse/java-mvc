d7936a4901be25b5e1aef9eab7d3bab236b4a989
libienz <leekh10195@gmail.com>
2024-09-19 05:09:41 +0900

    feat: 문자열에 해당하는 RequestMethod 찾는 기능 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod from(String method) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(value -> value.name().equals(method.toUpperCase()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 RequestMethod를 찾을 수 없습니다"));
+    }
 }
