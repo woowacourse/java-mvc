3ae1b9a97a3a5086c2706a022c65c19a262b3f16
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-18 17:33:19 +0900

    test: ReflectionsTest 테스트 작성

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,15 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -11,8 +17,17 @@ class ReflectionsTest {
 
     @Test
     void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
-
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Reflections reflections = new Reflections("reflection.examples");
+        Map<Class<? extends Annotation>, String> classes = Map.of(
+                Controller.class, "@Controller",
+                Service.class, "@Service",
+                Repository.class, "@Repository"
+        );
+
+        classes.forEach((annotationClass, annotationName) -> {
+            Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(annotationClass);
+            annotatedClasses.forEach(clazz -> log.info("{} : {}", annotationName, clazz.getName()));
+        });
     }
 }
