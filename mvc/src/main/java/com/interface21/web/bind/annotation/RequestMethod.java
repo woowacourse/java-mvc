3f4c4dc10b3cff7e64e1312f1bd696ae8d37b689
BurningFalls <sherlocky31415@gmail.com>
2024-09-18 18:47:58 +0900

    feature: 문자열과 일치하는 RequestMethod를 반환하는 기능 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod of(String method) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(requestMethod -> requestMethod.name().equals(method))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 HTTP Method 입니다."));
+    }
 }
---
+++ mvc/src/test/java/com/interface21/web/bind/annotation/RequestMethodTest.java
@@ -0,0 +1,38 @@
+package com.interface21.web.bind.annotation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class RequestMethodTest {
+
+    @DisplayName("문자열과 일치하는 RequestMethod를 반환한다.")
+    @EnumSource(RequestMethod.class)
+    @ParameterizedTest
+    void ofTest(RequestMethod expected) {
+        // given
+        String methodName = expected.name();
+
+        // when
+        RequestMethod actual = RequestMethod.of(methodName);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("문자열과 일치하는 RequestMethod가 없으면 IllegalArgumentException을 던진다.")
+    @Test
+    void ofTestThrowExceptionWhenNotFound() {
+        // given
+        String methodName = "EXAMPLE";
+
+        // when & then
+        assertThatThrownBy(() -> RequestMethod.of(methodName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지원하지 않는 HTTP Method 입니다.");
+    }
+}
