3f5ce11fbbb71ce833703040a86521e110720d92
yechop <channetattoo@gmail.com>
2024-09-19 13:40:38 +0900

    test: 학습테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +9,10 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.newInstance());
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,17 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -13,6 +21,12 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        final Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        assertThat(controllers.size()).isEqualTo(1);
+        assertThat(services.size()).isEqualTo(1);
+        assertThat(repositories.size()).isEqualTo(2);
     }
 }
