44a4f650fdf5e92f47e978d744d339c4c37602c8
hoyeonyy <highoption@naver.com>
2024-09-19 23:16:51 +0900

    test: ReflectionsTest 학습테스트 구현

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,31 @@
 package reflection;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
 class ReflectionsTest {
 
-    private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
+	private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
-    @Test
-    void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+	@Test
+	void showAnnotationClass() throws Exception {
+		Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
-    }
+		// TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+		Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+		Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+		Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+
+		controllerClasses.forEach(clazz -> log.info(clazz.getName()));
+		serviceClasses.forEach(clazz -> log.info(clazz.getName()));
+		repositoryClasses.forEach(clazz -> log.info(clazz.getName()));
+	}
 }
