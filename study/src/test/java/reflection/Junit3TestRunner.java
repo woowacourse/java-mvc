0340976ebc556b3fceaadc4cf58cae284e2ca0a9
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-19 11:28:39 +0900

    test(Junit3TestRunner): 학습테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +9,12 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method test1 = clazz.getMethod("test1");
+        Method test2 = clazz.getMethod("test2");
+        Method three = clazz.getMethod("three");
+
+        test1.invoke(new Junit3Test());
+        test2.invoke(new Junit3Test());
+        three.invoke(new Junit3Test());
     }
 }
