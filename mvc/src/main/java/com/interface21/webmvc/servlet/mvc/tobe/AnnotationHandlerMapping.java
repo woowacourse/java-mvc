98bb5187aa4655bb7ffcc091e1b0a543294dcb08
Gyeongho Yang <chchch1213@naver.com>
2024-09-19 15:34:09 +0900

    feat: inject handler execution with reflection

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,15 +1,23 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final String DELIMITER_PATH = "/";
+    private static final String REGEX_MANY_PATH_DELIMITER = "/{2,}";
+    private static final String DEFAULT_PATH = "";
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -21,9 +29,56 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        reflections.getTypesAnnotatedWith(Controller.class).forEach(this::assignHandlerByClass);
+    }
+
+    private void assignHandlerByClass(Class<?> clazz) {
+        RequestMapping annotation = clazz.getAnnotation(RequestMapping.class);
+        String path = Optional.ofNullable(annotation)
+                .map(RequestMapping::value)
+                .orElse(DEFAULT_PATH);
+
+        for (Method method : clazz.getMethods()) {
+            assignHandlerByMethod(clazz, method, path);
+        }
+    }
+
+    private void assignHandlerByMethod(Class<?> clazz, Method method, String basePath) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+        if (annotation == null) {
+            return;
+        }
+        String subPath = Optional.of(annotation.value()).orElse(DEFAULT_PATH);
+        String path = String.join(DELIMITER_PATH, DELIMITER_PATH, basePath, subPath)
+                .replaceAll(REGEX_MANY_PATH_DELIMITER, DELIMITER_PATH);
+
+        for (RequestMethod requestMethod : annotation.method()) {
+            HandlerKey handlerKey = new HandlerKey(path, requestMethod);
+            HandlerExecution handlerExecution = findHandlerExecution(clazz, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private HandlerExecution findHandlerExecution(Class<?> clazz, Method method) {
+        try {
+            return new HandlerExecution(method, clazz);
+        } catch (Exception e) {
+            log.error("Failed to find HandlerExecution for class {}", clazz, e);
+            return null;
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod requestMethod;
+        try {
+            requestMethod = RequestMethod.valueOf(request.getMethod());
+        } catch (IllegalArgumentException e) {
+            log.error("Failed to get HandlerExecution");
+            return null;
+        }
+
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,23 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Method method;
+    private final Object controllerInstance;
+
+    public HandlerExecution(Method method, Class<?> controllerClass)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        this.method = method;
+        this.controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(controllerInstance, request, response);
     }
 }
