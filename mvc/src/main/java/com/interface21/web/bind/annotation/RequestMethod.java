7215fe4a93e7e1aeee43f66d1b9871cc1932b1eb
jinwoo22 <jenny010925@naver.com>
2024-09-19 13:40:21 +0900

    feat: AnnotationHandlerMapping과 HandlerExecution 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod from(String method) {
+        return Arrays.stream(values())
+                .filter(requestMethod -> requestMethod.name().equals(method))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("메서드가 존재하지 않습니다."));
+    }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +29,44 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        registerHandlers(getControllers());
     }
 
+    private Set<Class<?>> getControllers() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlers(Set<Class<?>> controllers) {
+        for (Class<?> controller : controllers) {
+            for (Method method : getControllerMethods(controller)) {
+                registerHandler(controller, method);
+            }
+        }
+    }
+
+    private List<Method> getControllerMethods(Class<?> controller) {
+        return Arrays.stream(controller.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void registerHandler(Class<?> controller, Method method) {
+        RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+
+        String value = annotation.value();
+        RequestMethod[] requestMethods = annotation.method();
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+
+        for (RequestMethod requestMethod : requestMethods) {
+            handlerExecutions.put(new HandlerKey(value, requestMethod), handlerExecution);
+        }
+    }
+
+    // TODO method 설정이 되어 있지 않으면 모든 HTTP Method를 지원한다.
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
