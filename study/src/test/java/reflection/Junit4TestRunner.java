d920354ed20dcf12bf0676c22146cdfe75462cbd
hoyeonyy <highoption@naver.com>
2024-09-19 16:37:05 +0900

    test: Junit4TestRunner 학습테스트 구현

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +10,15 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // 클래스의 모든 선언된 메소드를 가져옵니다 (private 메소드 포함)
+        Method[] methods = clazz.getDeclaredMethods();
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(instance);
+            }
+        }
     }
 }
