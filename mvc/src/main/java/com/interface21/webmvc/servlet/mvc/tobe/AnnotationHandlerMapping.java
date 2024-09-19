d4a85de8f028f4f5a849f0a6faa18c50c660f97f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-19 16:36:29 +0900

    feat(AnnotationHandlerMapping): 핸들러 매핑의 초기화 작업

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
@@ -21,6 +32,43 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        Set<Class<?>> controllerClasses = findControllerClasses();
+        for (Class<?> controllerClass : controllerClasses) {
+            registerHandlerMethods(controllerClass);
+        }
+    }
+
+    private Set<Class<?>> findControllerClasses() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlerMethods(Class<?> controllerClass) {
+        Object controllerInstance = createControllerInstance(controllerClass);
+
+        Map<HandlerKey, HandlerExecution> newHandlerExecutions = Arrays.stream(controllerClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toMap(
+                        this::createHandlerKey,
+                        method -> new HandlerExecution(controllerInstance, method)
+                ));
+        handlerExecutions.putAll(newHandlerExecutions);
+    }
+
+    private Object createControllerInstance(Class<?> controllerClass) {
+        try {
+            return controllerClass.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException |
+                 InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalStateException(
+                    "Failed to instantiate controller: " + controllerClass.getName(), e);
+        }
+    }
+
+    private HandlerKey createHandlerKey(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        return new HandlerKey(requestMapping.value(), requestMapping.method());
     }
 
     public Object getHandler(final HttpServletRequest request) {
