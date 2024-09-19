5a2dd99cf0c7368eb0e3b575bfb58a14abfce464
3juhwan <13selfesteem91@naver.com>
2024-09-19 23:45:46 +0900

    feat: 디스패처 서블릿에 핸들러 어뎁터 등록

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,12 +2,14 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
-import com.interface21.webmvc.servlet.view.JspView;
+import com.interface21.webmvc.servlet.mvc.AnnotationHandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,26 +19,30 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private List<HandlerAdapter> handlerAdapters;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping();
+        annotationHandlerMapping.initialize();
+        handlerAdapters = List.of(new ManualHandlerAdapter(manualHandlerMapping),
+            new AnnotationHandlerAdapter(annotationHandlerMapping));
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
         throws ServletException {
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
+        String requestURI = request.getRequestURI();
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            Controller controller = manualHandlerMapping.getHandler(request);
-            String viewName = controller.execute(request, response);
-            ModelAndView modelAndView = new ModelAndView(new JspView(viewName));
+            HandlerAdapter handlerAdapter = getHandlerAdapter(request);
+            ModelAndView modelAndView = handlerAdapter.handle(request, response);
             move(modelAndView, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
@@ -44,9 +50,16 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
+    private HandlerAdapter getHandlerAdapter(HttpServletRequest request) {
+        HandlerAdapter handlerAdapter = handlerAdapters.stream()
+            .filter(adapter -> adapter.isSupports(request))
+            .findFirst()
+            .orElseThrow(() -> new UnsupportedOperationException("지원하지 않는 엔드포인트입니다."));
+        return handlerAdapter;
+    }
+
     private void move(ModelAndView modelAndView, HttpServletRequest request,
-        HttpServletResponse response)
-        throws Exception {
+        HttpServletResponse response) throws Exception {
         Map<String, Object> model = modelAndView.getModel();
         View view = modelAndView.getView();
         view.render(model, request, response);
