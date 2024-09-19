05f13519100298fadb3c32165c260fcb5cb23776
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-19 17:09:33 +0900

    fix: 레거시 MVC를 지원하도록 코드 복원

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,8 +1,6 @@
 package com.techcourse;
 
-import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,15 +13,15 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private AnnotationHandlerMapping annotationHandlerMapping;
+    private ManualHandlerMapping manualHandlerMapping;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        annotationHandlerMapping = new AnnotationHandlerMapping("com.techcourse.controller");
-        annotationHandlerMapping.initialize();
+        manualHandlerMapping = new ManualHandlerMapping();
+        manualHandlerMapping.initialize();
     }
 
     @Override
@@ -32,12 +30,28 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            HandlerExecution handler = annotationHandlerMapping.getHandler(request);
-            ModelAndView modelAndView = handler.handle(request, response);
-            modelAndView.render(request, response);
+//            추후 @MVC를 지원할 때 사용할 코드
+//            HandlerExecution handler = annotationHandlerMapping.getHandler(request);
+//            ModelAndView modelAndView = handler.handle(request, response);
+//            modelAndView.render(request, response);
+
+            final var controller = manualHandlerMapping.getHandler(requestURI);
+            final var viewName = controller.execute(request, response);
+            move(viewName, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    // 추후 @MVC를 지원할 때 삭제
+    private void move(String viewName, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+            return;
+        }
+
+        final var requestDispatcher = request.getRequestDispatcher(viewName);
+        requestDispatcher.forward(request, response);
     }
 }
