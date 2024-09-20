99b13fd0397411c45d4471eea498c7821f253be5
nhlee98 <e5slnh9876@gmail.com>
2024-09-21 01:06:27 +0900

    feat: 어노테이션을 기반으로 핸들러를 찾아 등록하는 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
@@ -1,7 +1,8 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.web.bind.annotation.RequestMethod;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class HandlerKey {
@@ -14,6 +15,12 @@ public class HandlerKey {
         this.requestMethod = requestMethod;
     }
 
+    public static List<HandlerKey> createHandlerKeys(String url, RequestMethod[] requestMethods) {
+        return Arrays.stream(requestMethods)
+                .map(requestMethod -> new HandlerKey(url, requestMethod))
+                .toList();
+    }
+
     @Override
     public String toString() {
         return "HandlerKey{" +
@@ -24,8 +31,12 @@ public class HandlerKey {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof HandlerKey)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof HandlerKey)) {
+            return false;
+        }
         HandlerKey that = (HandlerKey) o;
         return Objects.equals(url, that.url) && requestMethod == that.requestMethod;
     }
@@ -33,5 +44,13 @@ public class HandlerKey {
     @Override
     public int hashCode() {
         return Objects.hash(url, requestMethod);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public RequestMethod getRequestMethod() {
+        return requestMethod;
     }
 }
--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,13 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class AnnotationHandlerMappingTest {
 
@@ -15,12 +20,13 @@ class AnnotationHandlerMappingTest {
 
     @BeforeEach
     void setUp() {
-        handlerMapping = new AnnotationHandlerMapping("samples");
+        handlerMapping = new AnnotationHandlerMapping("samples.success");
         handlerMapping.initialize();
     }
 
     @Test
-    void get() throws Exception {
+    @DisplayName("GET 메소드에 대한 요청 핸들러를 찾아서 처리할 수 있다.")
+    void getHandlerTest() throws Exception {
         final var request = mock(HttpServletRequest.class);
         final var response = mock(HttpServletResponse.class);
 
@@ -35,7 +41,8 @@ class AnnotationHandlerMappingTest {
     }
 
     @Test
-    void post() throws Exception {
+    @DisplayName("POST 메소드에 대한 요청 핸들러를 찾아서 처리할 수 있다.")
+    void postHandlerTest() throws Exception {
         final var request = mock(HttpServletRequest.class);
         final var response = mock(HttpServletResponse.class);
 
@@ -47,5 +54,45 @@ class AnnotationHandlerMappingTest {
         final var modelAndView = handlerExecution.handle(request, response);
 
         assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = RequestMethod.class)
+    @DisplayName("RequestMapping의 method가 설정되어 있지 않은 경우 모든 HTTP method를 지원해야 한다.")
+    void requestMappingWithoutMethodSettingTest(RequestMethod method) throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/all-method-test");
+        when(request.getMethod()).thenReturn(method.name());
+
+        final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        final var modelAndView = handlerExecution.handle(request, response);
+
+        assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+
+    @Test
+    @DisplayName("같은 url, method에 대한 핸들러를 중복으로 등록하는 경우 예외가 발생한다.")
+    void duplicateHandlerKeyExceptionTest() {
+        AnnotationHandlerMapping handlerMapping = new AnnotationHandlerMapping("samples.exception.duplicate");
+        assertThatThrownBy(() -> handlerMapping.initialize())
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("GET /get-test에 대한 핸들러가 중복됩니다.");
+    }
+
+    @Test
+    @DisplayName("url, method를 처리할 수 있는 핸들러가 존재하지 않는 경우 예외가 발생한다.")
+    void getHandlerAbsenceExceptionTest() {
+        final var request = mock(HttpServletRequest.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/absence-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        assertThatThrownBy(() -> handlerMapping.getHandler(request))
+                .isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("GET /absence-test를 처리할 수 있는 핸들러가 존재하지 않습니다.");
     }
 }
