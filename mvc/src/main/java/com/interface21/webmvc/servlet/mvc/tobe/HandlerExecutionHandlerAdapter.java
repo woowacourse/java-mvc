5829d87190842ce7b0aa5dbced3f75562ba643f3
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-21 05:01:41 +0900

    refactor: 공통 HandlerAdapter 인터페이스와 구현체 정의

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionHandlerAdapter.java
@@ -0,0 +1,20 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HandlerExecutionHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HandlerExecution handlerExecution = (HandlerExecution) handler;
+        return handlerExecution.handle(request, response);
+    }
+}
