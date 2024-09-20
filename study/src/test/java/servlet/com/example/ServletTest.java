add7eea950d3d62a0d076298899d6eb98428c3f8
hoyeonyy <highoption@naver.com>
2024-09-20 14:00:04 +0900

    test: Servlet 학습테스트 구현

--- study/src/test/java/servlet/com/example/ServletTest.java
+++ study/src/test/java/servlet/com/example/ServletTest.java
@@ -1,55 +1,56 @@
 package servlet.com.example;
 
-import org.junit.jupiter.api.Test;
-import support.HttpUtils;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import support.HttpUtils;
 
 class ServletTest {
 
-    private final String WEBAPP_DIR_LOCATION = "src/main/webapp/";
+	private final String WEBAPP_DIR_LOCATION = "src/main/webapp/";
 
-    @Test
-    void testSharedCounter() {
-        // 톰캣 서버 시작
-        final var tomcatStarter = new TomcatStarter(WEBAPP_DIR_LOCATION);
-        tomcatStarter.start();
+	@Test
+	void testSharedCounter() {
+		// 톰캣 서버 시작
+		final var tomcatStarter = new TomcatStarter(WEBAPP_DIR_LOCATION);
+		tomcatStarter.start();
 
-        // shared-counter 페이지를 3번 호출한다.
-        final var PATH = "/shared-counter";
-        HttpUtils.send(PATH);
-        HttpUtils.send(PATH);
-        final var response = HttpUtils.send(PATH);
+		// shared-counter 페이지를 3번 호출한다.
+		final var PATH = "/shared-counter";
+		HttpUtils.send(PATH);
+		HttpUtils.send(PATH);
+		final var response = HttpUtils.send(PATH);
 
-        // 톰캣 서버 종료
-        tomcatStarter.stop();
+		// 톰캣 서버 종료
+		tomcatStarter.stop();
 
-        assertThat(response.statusCode()).isEqualTo(200);
+		assertThat(response.statusCode()).isEqualTo(200);
 
-        // expected를 0이 아닌 올바른 값으로 바꿔보자.
-        // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
-    }
+		// expected를 0이 아닌 올바른 값으로 바꿔보자.
+		// 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
+		assertThat(Integer.parseInt(response.body())).isEqualTo(3);
+	}
 
-    @Test
-    void testLocalCounter() {
-        // 톰캣 서버 시작
-        final var tomcatStarter = new TomcatStarter(WEBAPP_DIR_LOCATION);
-        tomcatStarter.start();
+	@Test
+	void testLocalCounter() {
+		// 톰캣 서버 시작
+		final var tomcatStarter = new TomcatStarter(WEBAPP_DIR_LOCATION);
+		tomcatStarter.start();
 
-        // local-counter 페이지를 3번 호출한다.
-        final var PATH = "/local-counter";
-        HttpUtils.send(PATH);
-        HttpUtils.send(PATH);
-        final var response = HttpUtils.send(PATH);
+		// local-counter 페이지를 3번 호출한다.
+		final var PATH = "/local-counter";
+		HttpUtils.send(PATH);
+		HttpUtils.send(PATH);
+		final var response = HttpUtils.send(PATH);
 
-        // 톰캣 서버 종료
-        tomcatStarter.stop();
+		// 톰캣 서버 종료
+		tomcatStarter.stop();
 
-        assertThat(response.statusCode()).isEqualTo(200);
+		assertThat(response.statusCode()).isEqualTo(200);
 
-        // expected를 0이 아닌 올바른 값으로 바꿔보자.
-        // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
-    }
+		// expected를 0이 아닌 올바른 값으로 바꿔보자.
+		// 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
+		assertThat(Integer.parseInt(response.body())).isEqualTo(1);
+	}
 }
