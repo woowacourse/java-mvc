a8b8ba6c41cabf38be73b103132341fd92975df0
kelly6bf <kelly6bf@gmail.com>
2024-09-21 03:06:29 +0900

    study: Junit3TestRunner 실험

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -8,10 +8,13 @@ class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit3Test> clazz = Junit3Test.class;
+        /**
+         * 구체 클래스를 명시하지 않아도 실행할 수 있다.
+         */
+        Class<?> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
-        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+        final Object instance = clazz.getDeclaredConstructor().newInstance();
         Method[] methods = clazz.getDeclaredMethods();
 
         // "test"로 시작하는 메소드 실행
