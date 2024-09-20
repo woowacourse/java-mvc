78e66b36315d652c510cf93a9e87c1bff815f787
reddevilmidzy <midzy0228@gmail.com>
2024-09-20 11:00:43 +0900

    refactor: enum에 인스턴스 변수 제거 후 name() 활용

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -6,32 +6,23 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum RequestMethod {
-    GET("GET"),
-    HEAD("HEAD"),
-    POST("POST"),
-    PUT("PUT"),
-    PATCH("PATCH"),
-    DELETE("DELETE"),
-    OPTIONS("OPTIONS"),
-    TRACE("TRACE");
+
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE;
 
     private static final Map<String, RequestMethod> SUIT_CASE = Arrays.stream(RequestMethod.values())
-            .collect(Collectors.toMap(RequestMethod::getValue, Function.identity()));
-
-    private final String value;
-
-    RequestMethod(String value) {
-        this.value = value;
-    }
+            .collect(Collectors.toMap(RequestMethod::name, Function.identity()));
 
     public static RequestMethod getByValue(String value) {
         if (SUIT_CASE.containsKey(value)) {
             return SUIT_CASE.get(value);
         }
         throw new IllegalArgumentException(String.format("%s는 지원하지 않는 메서드입니다.", value));
-    }
-
-    private String getValue() {
-        return value;
     }
 }
