1710f56170e16c1fefa4aeeefd57babb60db8d35
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-20 18:16:57 +0900

    test: RequestMethod 내 테스트 추가

--- mvc/src/test/java/com/interface21/web/bind/annotation/RequestMethodTest.java
+++ mvc/src/test/java/com/interface21/web/bind/annotation/RequestMethodTest.java
@@ -9,8 +9,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class RequestMethodTest {
 
     @Test
+    @DisplayName("문자열로 RequestMethod 객체를 가져온다.")
+    void request_method_get_with_string(){
+        final RequestMethod requestMethod = RequestMethod.from("POST");
+        assertThat(RequestMethod.POST).isEqualTo(requestMethod);
+    }
+
+
+    @Test
     @DisplayName("메소드 명은 대소문자를 구분하지 않는다.")
-    void some() {
+    void method_name_is_case_sensitive() {
         final RequestMethod method = RequestMethod.GET;
         assertThat(RequestMethod.from("get")).isEqualTo(method);
         assertThat(RequestMethod.from("GET")).isEqualTo(method);
@@ -18,7 +26,7 @@ class RequestMethodTest {
 
     @Test
     @DisplayName("없는 메소드 명은 예외를 발생한다.")
-    void some1() {
+    void throw_exception_when_not_exist_method_name() {
         assertThatThrownBy(() -> RequestMethod.from("SOME"))
                 .isInstanceOf(NotSupportMethodException.class);
     }
