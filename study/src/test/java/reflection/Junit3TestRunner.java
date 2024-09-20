6a3304a760ed40441d390646b77c14f0347f390c
yoonseo choi <choiyoonseo02@gmail.com>
2024-09-20 10:12:13 +0900

    test: 학습테스트 요구사항에 맞게 수정

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -9,12 +9,12 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        Method test1 = clazz.getMethod("test1");
-        Method test2 = clazz.getMethod("test2");
-        Method three = clazz.getMethod("three");
+        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
 
-        test1.invoke(new Junit3Test());
-        test2.invoke(new Junit3Test());
-        three.invoke(new Junit3Test());
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(instance);
+            }
+        }
     }
 }
