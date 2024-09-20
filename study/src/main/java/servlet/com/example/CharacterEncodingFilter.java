c72d81e8d251dc9dc54c9d0cdc419f2fe0f1c96f
eunjungL <dldmswjd322@gmail.com>
2024-09-20 12:16:56 +0900

    test: ServletTest, FilterTest 학습 내용 추가

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -11,6 +11,7 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding("UTF-8"); // writer에 쓰기 전에 encoding을 설정하기 위해 doFilter 전에 실행
         chain.doFilter(request, response);
     }
 }
