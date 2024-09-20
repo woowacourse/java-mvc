cfc3ec6eb280bcb2e63511b9872caf1030266b37
kelly6bf <kelly6bf@gmail.com>
2024-09-20 22:06:18 +0900

    study: Junit3TestRunner 풀이

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +11,14 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+        Method[] methods = clazz.getDeclaredMethods();
+
+        // "test"로 시작하는 메소드 실행
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(instance);  // 메소드 실행
+            }
+        }
     }
 }
