a83cf89c82cee0c514af8c5d27a0994fcbd71739
eunjungL <dldmswjd322@gmail.com>
2024-09-20 21:29:52 +0900

    test: 모든 메서드 맵핑 테스트 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -5,8 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -63,5 +68,23 @@ class AnnotationHandlerMappingTest {
         assertThatThrownBy(() -> handlerMapping.getHandler(request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("처리할 수 없는 요청입니다.");
+    }
+
+    @DisplayName("RequestMapping에 아무런 메서드가 지정되지 않으면 모든 메서드가 맵핑된다.")
+    @Test
+    void testNoMethod() throws NoSuchFieldException, IllegalAccessException {
+        // given
+        Set<HandlerKey> handlerKeys = new HashSet<>();
+        for (RequestMethod method : RequestMethod.values()) {
+            handlerKeys.add(new HandlerKey("/no-method-test", method));
+        }
+
+        // when
+        Field field = handlerMapping.getClass().getDeclaredField("handlerExecutions");
+        field.setAccessible(true);
+
+        // then
+        Map<HandlerKey, HandlerExecution> handlerExecutions = (Map<HandlerKey, HandlerExecution>) field.get(handlerMapping);
+       assertThat(handlerExecutions.keySet()).containsAnyElementsOf(handlerKeys);
     }
 }
