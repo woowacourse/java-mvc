5af70000073ca057e7cd9208c4f56f6131ef254e
linirini <2001yerin@naver.com>
2024-09-20 13:04:38 +0900

    test: Servlet 학습 테스트 수행

--- study/src/test/java/servlet/com/example/ServletTest.java
+++ study/src/test/java/servlet/com/example/ServletTest.java
@@ -1,9 +1,10 @@
 package servlet.com.example;
 
-import org.junit.jupiter.api.Test;
-import support.HttpUtils;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import support.HttpUtils;
 
 class ServletTest {
 
@@ -27,8 +28,14 @@ class ServletTest {
         assertThat(response.statusCode()).isEqualTo(200);
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
-        // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        /**
+         * 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
+         *
+         * 서블릿의 인스턴스 변수는 다른 스레드와 공유된다.
+         * 서버는 여러 스레드에서 접근 가능하므로 서블릿에서 비즈니스 로직을 처리할 때 인스턴스 변수는 사용하지 않는다.
+         * 해당 서블릿이 3개의 스레드에서 호출되었으므로 body의 결과는 3이 된다.
+         */
+        assertThat(Integer.parseInt(response.body())).isEqualTo(3);
     }
 
     @Test
@@ -49,7 +56,12 @@ class ServletTest {
         assertThat(response.statusCode()).isEqualTo(200);
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
-        // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        /**
+         * 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
+         *
+         * 서블릿의 인스턴스 변수는 다른 스레드와 공유되지만, 메서드 내 로컬 변수는 다른 스레드와 공유되지 않는다.
+         * 따라서, 비즈니스 로직 처리는 로컬 변수를 사용한다.
+         */
+        assertThat(Integer.parseInt(response.body())).isEqualTo(1);
     }
 }
