433dcee6844db26ba4fd35c9e09970d59ac49f5f
le2sky <leehaneul990623@gmail.com>
2024-09-20 20:40:22 +0900

    feat: annotaion handler mapping 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -14,16 +22,61 @@ public class AnnotationHandlerMapping {
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
+    public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
+        try {
+            processControllers();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void processControllers() throws Exception {
+        for (Class<?> controller : findControllers()) {
+            processController(controller);
+        }
+    }
+
+    private Set<Class<?>> findControllers() {
+        Reflections reflections = new Reflections(basePackage);
+
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void processController(Class<?> controller) throws Exception {
+        Object runnerInstance = controller.getDeclaredConstructor().newInstance();
+
+        for (Method method : findHandlerMethods(controller)) {
+            appendRequestMapping(method, runnerInstance);
+        }
+    }
+
+    private List<Method> findHandlerMethods(Class<?> controller) {
+        return Arrays.stream(controller.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void appendRequestMapping(Method method, Object runnerInstance) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(runnerInstance, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    public Object getHandler(HttpServletRequest request) {
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
