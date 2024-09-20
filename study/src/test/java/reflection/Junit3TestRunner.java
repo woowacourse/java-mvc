1b08ba2d49dcefde062c33fcc6f6fa87e6dcea42
seunghye218 <seunghye@student.42seoul.kr>
2024-09-20 10:17:43 +0900

    test: Reflection API 학습 테스트 통과

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,34 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        List<Method> methods = Arrays.stream(clazz.getMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+        List<String> methodNames = methods.stream()
+                .map(Method::getName)
+                .toList();
+
+        methods.forEach((method -> {
+            try {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }));
+
+        assertThat(methodNames).containsExactly("test1", "test2");
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +10,17 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        int myTestCount = 0;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+                myTestCount++;
+            }
+        }
+
+        assertThat(myTestCount).isEqualTo(2);
     }
 }
