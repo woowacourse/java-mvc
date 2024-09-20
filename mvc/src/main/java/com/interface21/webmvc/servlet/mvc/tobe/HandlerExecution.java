5bf902e154cea52f046a21ab282d6f2f777f2ce7
jinwoo22 <jenny010925@naver.com>
2024-09-20 14:53:36 +0900

    feat: 타입 명시

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -8,16 +8,16 @@ import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    private final Class controller;
+    private final Class<?> controller;
     private final Method method;
 
-    public HandlerExecution(Class controller, Method method) {
+    public HandlerExecution(Class<?> controller, Method method) {
         this.controller = controller;
         this.method = method;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        Constructor constructor = controller.getConstructor();
+        Constructor<?> constructor = controller.getConstructor();
         Object instance = constructor.newInstance();
 
         return (ModelAndView) method.invoke(instance, request, response);
