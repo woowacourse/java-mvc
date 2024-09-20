7e62ddbc059c6e84d35036cde901a651cbcddd56
reddevilmidzy <midzy0228@gmail.com>
2024-09-20 13:34:44 +0900

    refactor: enum 값 매핑하는 상수 이름 SUIT_CASE에서 METHOD_LOOKUP으로 변경

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -16,12 +16,12 @@ public enum RequestMethod {
     OPTIONS,
     TRACE;
 
-    private static final Map<String, RequestMethod> SUIT_CASE = Arrays.stream(RequestMethod.values())
+    private static final Map<String, RequestMethod> METHOD_LOOKUP = Arrays.stream(RequestMethod.values())
             .collect(Collectors.toMap(RequestMethod::name, Function.identity()));
 
     public static RequestMethod getByValue(String value) {
-        if (SUIT_CASE.containsKey(value)) {
-            return SUIT_CASE.get(value);
+        if (METHOD_LOOKUP.containsKey(value)) {
+            return METHOD_LOOKUP.get(value);
         }
         throw new IllegalArgumentException(String.format("%s는 지원하지 않는 메서드입니다.", value));
     }
