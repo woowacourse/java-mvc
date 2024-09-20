c1c2e12d89d917d1aeae32aa81696588a0bfbc2d
김형호 <140397285+HyungHoKim00@users.noreply.github.com>
2024-09-20 19:29:32 +0900

    [MVC 구현하기 - 1단계] 명오(김형호) 미션 제출합니다. (#648)
    * test: 학습 테스트 통과
    
    * feat: AnnotationHandlerMapping 구현
    
    * feat: JspView 구현
    
    * refactor: 메서드 분리 및 메서드명 수정
    
    * test: 불필요한 캐스팅 제거
    
    * test: JspView 테스트 추가
    
    * style: 코드 포맷팅
    
    * test: stream() 사용
    
    * refactor: DispatcherServlet에서 JspView 사용
    
    * style: 메서드 위치 변경
    
    * refactor: NPE 방지를 위해 initialize에서 filter를 사용하도록 변경, 에러메시지 변경
    
    * refactor: controller를 중복 생성하지 않도록 변경
    
    * test: 학습 테스트 완료
    
    * test: 실패하는 테스트 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,21 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping {
 
@@ -13,17 +23,80 @@ public class AnnotationHandlerMapping {
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final Set<Object> controllers;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
+        this.controllers = new HashSet<>();
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
+        controllers.addAll(createControllerInstances(controllerTypes));
+
+        controllerTypes.stream()
+                .flatMap(type -> Arrays.stream(type.getDeclaredMethods()))
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(this::addHandler);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private Set<Object> createControllerInstances(Set<Class<?>> controllerTypes) {
+        return controllerTypes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toSet());
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new HandlingException("해당 컨트롤러의 기본 생성자로 인스턴스를 생성할 수 없습니다.");
+        }
+    }
+
+    private void addHandler(final Method handler) {
+        RequestMapping annotation = handler.getAnnotation(RequestMapping.class);
+        String uri = annotation.value();
+        List<RequestMethod> methods = Arrays.asList(annotation.method());
+        if (methods.isEmpty()) {
+            addHandlerForAllRequestMethods(handler, uri);
+        }
+        methods.forEach(method -> addHandlerForRequestMethod(handler, method, uri));
+    }
+
+    private void addHandlerForAllRequestMethods(final Method handler, final String uri) {
+        Arrays.stream(RequestMethod.values())
+                .forEach(method -> addHandlerForRequestMethod(handler, method, uri));
+    }
+
+    private void addHandlerForRequestMethod(final Method handler, final RequestMethod method, final String uri) {
+        HandlerKey handlerKey = new HandlerKey(uri, method);
+        Object controller = findDeclaringController(handler);
+        if (handlerExecutions.containsKey(handlerKey)) {
+            throw new HandlingException("같은 요청을 처리할 메서드가 두 개 이상 존재합니다.");
+        }
+        handlerExecutions.put(handlerKey, new HandlerExecution(controller, handler));
+    }
+
+    private Object findDeclaringController(Method handler) {
+        return controllers.stream()
+                .filter(controller -> Objects.equals(controller.getClass(), handler.getDeclaringClass()))
+                .findAny()
+                .orElseThrow(() -> new HandlingException("해당 메서드를 실행할 컨트롤러가 존재하지 않습니다."));
+    }
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+
+        HandlerExecution handlerExecution = handlerExecutions.get(handlerKey);
+        if (handlerExecution == null) {
+            throw new HandlingException("요청을 처리할 핸들러가 존재하지 않습니다.");
+        }
+        return handlerExecution;
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,14 @@
 package reflection;
 
+import java.util.Collection;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -12,7 +17,9 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
-
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Stream.of(Controller.class, Service.class, Repository.class)
+                .map(reflections::getTypesAnnotatedWith)
+                .flatMap(Collection::stream)
+                .forEach(type -> log.info(type.toString()));
     }
 }
