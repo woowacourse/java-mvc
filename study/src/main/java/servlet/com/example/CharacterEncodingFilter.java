0cc5ac2fe4c4523a3e31f15ac1d6e5c5931ffc55
임수빈 <robin980108@naver.com>
2024-09-20 12:10:02 +0900

    학습 테스트 추가

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -1,16 +1,21 @@
 package servlet.com.example;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
-
 import java.io.IOException;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setCharacterEncoding("UTF-8"); // KoreanServlet 내부 구현에서 response의 출력 스트림을 건드려서 전처리에 인코딩 처리를 해야 한다.
         chain.doFilter(request, response);
     }
 }
