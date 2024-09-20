52caa077313d099e1282a617cb2b3f5c0f206c98
kelly6bf <kelly6bf@gmail.com>
2024-09-20 22:07:57 +0900

    study: Junit4TestRunner 풀이

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +11,17 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test 인스턴스 생성
+        Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
+
+        // 클래스의 모든 메소드 가져오기
+        Method[] methods = clazz.getDeclaredMethods();
+
+        // @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(instance);  // 메소드 실행
+            }
+        }
     }
 }
