97a9232b2fde1f5ec2845d8bb0f18e4a59efc314
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-21 22:42:01 +0900

    refactor: 스트림 적용 및 메서드 분리

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,6 +1,7 @@
 package reflection;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,14 +9,23 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        Junit3Test junit3Test = new Junit3Test();
+        Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.getName().startsWith("test")) {
-                method.invoke(junit3Test);
-            }
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> isMethodNameStartingWith(method, "test"))
+                .forEach(method -> invokeWithExceptionHandling(method, junit3Test));
+    }
+
+    private boolean isMethodNameStartingWith(Method method, String prefix) {
+        return method.getName().startsWith(prefix);
+    }
+
+    private void invokeWithExceptionHandling(Method method, Object instance) {
+        try {
+            method.invoke(instance);
+        } catch (Exception e) {
+            throw new RuntimeException("메소드 실행 중에 예외가 발생하였습니다: " + method.getName(), e);
         }
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,7 @@
 package reflection;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,14 +9,19 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        Junit4Test junit4Test = new Junit4Test();
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(MyTest.class)) {
-                method.invoke(junit4Test);
-            }
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .forEach(method -> invokeWithExceptionHandling(method, junit4Test));
+    }
+
+    private void invokeWithExceptionHandling(Method method, Object instance) {
+        try {
+            method.invoke(instance);
+        } catch (Exception e) {
+            throw new RuntimeException("메소드 실행 중에 예외가 발생하였습니다: " + method.getName(), e);
         }
     }
 }
