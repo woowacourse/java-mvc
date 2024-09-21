436551221c02472a684e0dc28a6534348b1978ee
3juhwan <13selfesteem91@naver.com>
2024-09-21 19:27:47 +0900

    refactor: view 렌더링을 클래스 내부로 이동

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,7 +1,6 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.HandlerMapping;
@@ -13,7 +12,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import org.slf4j.Logger;
@@ -56,7 +54,7 @@ public class DispatcherServlet extends HttpServlet {
             Object handler = getHandler(request);
             HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
             ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
-            move(modelAndView, request, response);
+            modelAndView.render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
@@ -76,12 +74,5 @@ public class DispatcherServlet extends HttpServlet {
             .filter(adapter -> adapter.isSupports(handler))
             .findFirst()
             .orElseThrow(() -> new NoSuchElementException("처리할 수 있는 핸들러 어댑터가 없습니다."));
-    }
-
-    private void move(ModelAndView modelAndView, HttpServletRequest request,
-        HttpServletResponse response) throws Exception {
-        Map<String, Object> model = modelAndView.getModel();
-        View view = modelAndView.getView();
-        view.render(model, request, response);
     }
 }
