4cfa80d2d90405f49218d91498f8d954cfe6b487
3juhwan <13selfesteem91@naver.com>
2024-09-21 17:40:12 +0900

    test: AnnotationHandlerAdapter 테스트 수정

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/AnnotationHandlerAdapterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/AnnotationHandlerAdapterTest.java
@@ -7,67 +7,59 @@ import static org.mockito.Mockito.when;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.Handler;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AnnotationHandlerAdapterTest {
 
-    private AnnotationHandlerMapping handlerMapping;
-
-    @BeforeEach
-    void setUp() {
-        handlerMapping = new AnnotationHandlerMapping("samples");
-        handlerMapping.initialize();
-    }
-
-    @DisplayName("지원하는 핸들러가 있다.")
+    @DisplayName("HandlerExecution 타입을 지원한다.")
     @Test
     void isSupportsTest() {
         // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-
-        when(request.getRequestURI()).thenReturn("/get-test");
-        when(request.getMethod()).thenReturn("GET");
-
-        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter(handlerMapping);
+        Object handler = mock(HandlerExecution.class);
 
         // when, then
-        assertThat(handlerAdapter.isSupports(request)).isTrue();
+        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter();
+        assertThat(handlerAdapter.isSupports(handler)).isTrue();
     }
 
-    @DisplayName("지원하는 핸들러가 없다.")
-    @Test
-    void isSupportsTest1() {
+    @DisplayName("HandlerExecution 타입 외에는 지원하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(classes = {Handler.class, HandlerAdapter.class, HandlerMapping.class})
+    void isSupportsTest1(Class<?> clazz) {
         // given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-
-        when(request.getRequestURI()).thenReturn("/get-test");
-        when(request.getMethod()).thenReturn("DELETE");
-
-        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter(handlerMapping);
+        Object handler = mock(clazz);
 
         // when, then
-        assertThat(handlerAdapter.isSupports(request)).isFalse();
+        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter();
+        assertThat(handlerAdapter.isSupports(handler)).isFalse();
     }
 
     @DisplayName("어뎁터가 핸들러를 실행하고 ModelAndView를 얻는다.")
     @Test
     void handleTest() throws Exception {
         // given
+        AnnotationHandlerMapping handlerMapping = new AnnotationHandlerMapping("samples");
+        handlerMapping.initialize();
+
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
         when(request.getRequestURI()).thenReturn("/get-test");
         when(request.getMethod()).thenReturn("GET");
 
-        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter(handlerMapping);
+        HandlerExecution handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
 
         // when
-        ModelAndView modelAndView = handlerAdapter.handle(request, response);
+        AnnotationHandlerAdapter handlerAdapter = new AnnotationHandlerAdapter();
+        ModelAndView modelAndView = handlerAdapter.handle(request, response, handlerExecution);
 
         // then
         assertAll(
