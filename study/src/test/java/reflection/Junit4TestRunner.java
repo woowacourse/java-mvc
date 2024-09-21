e9ba215b74d4d5f9d549e8e052f14101713c38e1
김민겸 <mingyum119@naver.com>
2024-09-21 12:14:12 +0900

    test: Reflection API 학습 테스트 완료

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -9,5 +11,11 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                Junit4Test junit4Test = new Junit4Test();
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
