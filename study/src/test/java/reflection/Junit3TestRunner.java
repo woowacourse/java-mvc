348662878160a50eee39204434f7838c1e681309
tsulocalize <tsulocalizing@gmail.com>
2024-09-21 15:31:19 +0900

    test: Reflection 학습 테스트 수행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,20 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = clazz.getConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(junit3Test, (Object[]) null);
+            }
+        }
     }
 }
