6c585a68368b3611c5c6c0625a0834afadee7e21
tackyu <tack970913@gmail.com>
2024-09-22 01:51:55 +0900

    chore: reflection 학습 테스트 진행

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,15 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +20,20 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        List<Class<? extends Annotation>> annotations = List.of(
+                Controller.class,
+                Service.class,
+                Repository.class
+        );
+        logAnnotatedClass(reflections, annotations);
+    }
+
+    private void logAnnotatedClass(Reflections reflections, List<Class<? extends Annotation>> annotations) {
+        for (Class<? extends Annotation> annotation : annotations) {
+            Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(annotation);
+            for (Class<?> annotatedClass : typesAnnotatedWith) {
+                log.info("\r\nAnnotated Class: {}\r\nAnnotation Name: {}", annotatedClass.toString(),annotation.getName());
+            }
+        }
     }
 }
