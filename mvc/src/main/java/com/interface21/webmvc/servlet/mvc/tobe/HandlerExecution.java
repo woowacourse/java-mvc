8a6a90c209f750edf3968452c9b3712a25255fc9
tsulocalize <tsulocalizing@gmail.com>
2024-09-21 16:45:04 +0900

    refactor: HandlerExecution 인터페이스로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -4,9 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
 
-public class HandlerExecution {
+public interface HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
-    }
+    ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response);
 }
