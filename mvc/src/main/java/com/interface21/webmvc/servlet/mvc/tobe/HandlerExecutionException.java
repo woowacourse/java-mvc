5805f7b8bc60b8ceb769d3b1716011e26d77fafc
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-21 23:27:13 +0900

    test(HandlerExecutionTest): HandlerExecution 테스트 추가

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionException.java
@@ -0,0 +1,8 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+public class HandlerExecutionException extends RuntimeException {
+
+    public HandlerExecutionException(String message) {
+        super(message);
+    }
+}
