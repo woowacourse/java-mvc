15b67f989924c5b9418cbacdfa6efd169c9966df
Eugene Jang <kyunellroll@gmail.com>
2024-09-21 22:35:54 +0900

    test(...): 학습 테스트 추가

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,22 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
 
 class Junit4TestRunner {
 
     @Test
+    @DisplayName("Mytest라는 어노테이션이 붙은 메서드만 실행시킨다.")
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(new Junit4Test());
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,31 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+
+import java.util.Set;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    @DisplayName("@Controller, @Service, @Repository 가 있는 클래스들을 로그로 찍는다.")
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        log.info(controllers.toString());
+        log.info(services.toString());
+        log.info(repositories.toString());
     }
 }
