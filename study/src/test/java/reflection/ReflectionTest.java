967ebd1f00289425f8546271fa42a5a5a7125228
eun-byeol <joeunbyeol98@gmail.com>
2024-09-22 22:08:57 +0900

    test: 클래스의 생성자 가져오는 로직 수정

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -71,8 +71,9 @@ class ReflectionTest {
     void givenClass_whenInstantiatesObjectsAtRuntime_thenCorrect() throws Exception {
         final Class<?> questionClass = Question.class;
 
-        final Constructor<?> firstConstructor = questionClass.getDeclaredConstructors()[0];
-        final Constructor<?> secondConstructor = questionClass.getDeclaredConstructors()[1];
+        final Constructor<?> firstConstructor = questionClass.getConstructor(String.class, String.class, String.class);
+        final Constructor<?> secondConstructor = questionClass.getConstructor(long.class, String.class, String.class,
+                String.class, Date.class, int.class);
 
         final Question firstQuestion = (Question) firstConstructor.newInstance("gugu", "제목1", "내용1");
         final Question secondQuestion = (Question) secondConstructor.newInstance(1, "gugu", "제목2", "내용2", Date.from(
