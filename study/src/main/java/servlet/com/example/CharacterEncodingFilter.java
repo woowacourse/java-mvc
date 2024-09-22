6142f0103a7c6bb655da859f7db61ed26e0c331c
Eugene Jang <kyunellroll@gmail.com>
2024-09-22 23:55:56 +0900

    test(...): 학습 테스트 추가

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -8,9 +8,14 @@ import java.io.IOException;
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
+    private static final String DEFAULT_ENCODING = "UTF-8";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+
+        response.setCharacterEncoding(DEFAULT_ENCODING);
+
         chain.doFilter(request, response);
     }
 }
