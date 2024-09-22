2da096a8c86d66bcbae9f25517981ba3e4649b73
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-09-22 19:18:03 +0900

    feat(HandlerExecution): HandlerExecution 클래스 기본 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,29 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Class<?> controllerClass;
+    private final Method method;
+
+    public HandlerExecution(Class<?> controllerClass, Method method) {
+        this.controllerClass = controllerClass;
+        this.method = method;
+    }
+
+    public ModelAndView handle(
+            final HttpServletRequest request, final HttpServletResponse response
+    ) throws IllegalArgumentException, ReflectiveOperationException {
+        Object controller = controllerClass.getDeclaredConstructor().newInstance();
+        try {
+            return (ModelAndView) method.invoke(controller, request, response);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new IllegalArgumentException("Failed to invoke controller method", e);
+        }
     }
 }
