0fcdc81b26f2621d0c241490b944f257066009f4
haiseong <wjdgotjd9908@gmail.com>
2024-09-23 05:09:36 +0900

    feat: DispatcherServlet에 handlerMappings 추가

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -5,12 +5,16 @@ import static com.interface21.webmvc.servlet.view.JspView.REDIRECT_PREFIX;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,15 +23,17 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final List<HandlerMapping> handlerMappings;
 
     public DispatcherServlet() {
+        this.handlerMappings = new CopyOnWriteArrayList<>();
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping(new ConcurrentHashMap<>());
-        manualHandlerMapping.initialize();
+        handlerMappings.add(new ManualHandlerMapping(new ConcurrentHashMap<>()));
+        handlerMappings.add(new AnnotationHandlerMapping(new ConcurrentHashMap<>()));
+        handlerMappings.forEach(HandlerMapping::initialize);
     }
 
     @Override
@@ -37,13 +43,21 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = (Controller) manualHandlerMapping.getHandler(request);
-            final var viewName = controller.execute(request, response);
+            Controller handler = getHandler(request);
+            final var viewName = handler.execute(request, response);
             move(viewName, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private Controller getHandler(HttpServletRequest request) {
+        return (Controller) handlerMappings.stream()
+                .filter(handlerMapping -> handlerMapping.canHandle(request))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Not found handler mapping"))
+                .getHandler(request);
     }
 
     private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response)
