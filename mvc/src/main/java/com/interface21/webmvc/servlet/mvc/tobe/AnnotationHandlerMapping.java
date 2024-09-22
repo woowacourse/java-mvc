58d8a8f674a179a89ddb9d8e8c245ea516dc89ba
sunghyun-0 <94783621+tsulocalize@users.noreply.github.com>
2024-09-23 00:06:59 +0900

    [MVC 구현하기 - 1단계] 우주(김성현) 미션 제출합니다. (#702)
    * test: 학습 테스트 수행
    
    * test: Reflection 학습 테스트 수행
    
    * refactor: HandlerExecution 인터페이스로 변경
    
    * feat: 어노테이션 기반 핸들러 매핑 기능 구현
    
    * feat: JspView 클래스 구현
    
    * refactor: 메서드명 명확하게 변경
    
    * refactor: 인덴트 줄이기
    
    * refactor: 예외 메시지 명확하게 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,23 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +32,69 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            Object instance = createInstance(controller);
+            List<Method> methods = getRequestMethods(controller);
+            addHandlers(instance, methods);
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private Object createInstance(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Can't create instance of class : " + clazz.getName(), e);
+        }
+    }
+
+    private List<Method> getRequestMethods(Class<?> clazz) {
+        return Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void addHandlers(Object instance, List<Method> methods) {
+        for (Method method : methods) {
+            addHandler(instance, method);
+        }
+    }
+
+    private void addHandler(Object instance, Method method) {
+        HandlerExecution handlerExecution = (req, res) -> {
+            try {
+                return (ModelAndView) method.invoke(instance, req, res);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        List<HandlerKey> keys = getKeys(method);
+        for (HandlerKey key : keys) {
+            handlerExecutions.put(key, handlerExecution);
+        }
+    }
+
+    private List<HandlerKey> getKeys(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping.method().length == 0) {
+            return Arrays.stream(RequestMethod.values()).map(
+                    requestMethod -> new HandlerKey(requestMapping.value(), requestMethod))
+                    .toList();
+        }
+        List<HandlerKey> handlerKeys = new ArrayList<>();
+        for (RequestMethod requestMethod : requestMapping.method()) {
+            handlerKeys.add(new HandlerKey(requestMapping.value(), requestMethod));
+        }
+        return handlerKeys;
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,22 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import java.lang.reflect.Method;
+import java.util.Set;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Reflections reflections = new Reflections("reflection");
+        Set<Method> methodsAnnotatedWith = reflections.getMethodsAnnotatedWith(MyTest.class);
+        for (Method method : methodsAnnotatedWith) {
+            method.invoke(junit4Test, (Object[]) null);
+        }
     }
 }
