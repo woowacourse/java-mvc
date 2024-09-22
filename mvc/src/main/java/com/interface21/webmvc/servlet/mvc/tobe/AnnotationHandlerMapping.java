876f35462a12e3db1af0082d0a8bd00d73794ffe
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-09-22 19:20:35 +0900

    feat(AnnotationHandlerMapping): RequestMapping annotation handling method initializing 및 mapping된 handler get 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,69 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final String[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
-        this.handlerExecutions = new HashMap<>();
+    public AnnotationHandlerMapping(final String... basePackages) {
+        this.basePackages = basePackages;
+        this.handlerExecutions = new ConcurrentHashMap<>();
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        for (String basePackage : basePackages) {
+            scanControllers(basePackage);
+        }
+        log.info("Initialized AnnotationHandlerMapping: {} handlers", handlerExecutions.size());
+    }
+
+    private void scanControllers(String basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (Class<?> controllerClass : controllers) {
+            Method[] methods = controllerClass.getDeclaredMethods();
+            scanControllerMethods(controllerClass, methods);
+        }
+    }
+
+    private void scanControllerMethods(Class<?> controllerClass, Method[] methods) {
+        for (Method method : methods) {
+            registerRequestMappingMethod(controllerClass, method);
+        }
+    }
+
+    private void registerRequestMappingMethod(Class<?> controllerClass, Method method) {
+        if (method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String url = requestMapping.value();
+            RequestMethod[] methodsArray = requestMapping.method();
+            RequestMethod requestMethod = methodsArray[0];
+            HandlerKey key = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(key, new HandlerExecution(controllerClass, method));
+            log.info("Annotation Mapping Initialized: {} - {}", key, method.getName());
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String uri = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
+        HandlerKey key = new HandlerKey(uri, requestMethod);
+        return handlerExecutions.get(key);
     }
 }
