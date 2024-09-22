9b3c1cc939c9c1d99c7ac1a002885d4d2dbb3345
HoeSeong123 <ghltjd3@gmail.com>
2024-09-22 23:15:17 +0900

    fix: 실패하는 테스트 코드 수정

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -14,7 +14,7 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
-        response.setLocale(Locale.KOREAN);
+        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 }
