7839c2ce00ba4637307425d2754f808ebfa2b7a4
Choo <choo000407@naver.com>
2024-09-23 04:45:25 +0900

    feat: URL과 HTTP 메서드로 컨트롤러 매핑할 수 있도록 handlerExecutions 초기화

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,7 +25,30 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        reflections.getTypesAnnotatedWith(Controller.class).stream()
+                .flatMap(clazz -> Arrays.stream(clazz.getMethods()))
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(this::addHandlers);
         log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private void addHandlers(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        String url = requestMapping.value();
+        RequestMethod[] requestMethods = requestMapping.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            addHandler(method, url, requestMethod);
+        }
+    }
+
+    private void addHandler(Method method, String url, RequestMethod requestMethod) {
+        HandlerKey key = new HandlerKey(url, requestMethod);
+        if (handlerExecutions.containsKey(key)) {
+            throw new IllegalArgumentException("이미 등록된 URL과 HTTP 메서드 조합입니다: " + requestMethod + " " + url);
+        }
+        HandlerExecution execution = new HandlerExecution(method.getDeclaringClass(), method);
+        handlerExecutions.put(key, execution);
     }
 
     public Object getHandler(final HttpServletRequest request) {
