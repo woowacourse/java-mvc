64fe9e7b52cf0d85ddd0d8c1e7fa257f0c859dd6
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-09-22 16:56:57 +0900

    study: servlet 학습 테스트 실시

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -11,6 +11,9 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+
+        // CharacterEncodingFilter에서 response에 UTF-8 설정을 넣어줌.
+        response.setContentType("text/html;charset=utf-8");
         chain.doFilter(request, response);
     }
 }
