e2108b2021198d4a182e65405d72ee1303144d60
slimsha2dy <chy2581@naver.com>
2024-09-23 00:59:14 +0900

    feat: 학습 테스트 검증 로직 추가

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -8,9 +13,9 @@ import org.junit.jupiter.api.Test;
 class Junit3TestRunner {
 
     @Test
-    void run() {
+    void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        Junit3Test junit3Test = new Junit3Test();
+        Junit3Test junit3Test = mock(Junit3Test.class);
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
         List<Method> methods = List.of(clazz.getDeclaredMethods());
@@ -23,5 +28,11 @@ class Junit3TestRunner {
                         throw new RuntimeException(e);
                     }
                 });
+
+        assertAll(
+                () -> verify(junit3Test).test1(),
+                () -> verify(junit3Test).test2(),
+                () -> verify(junit3Test, never()).three()
+        );
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,10 @@
 package reflection;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -10,7 +15,7 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        Junit4Test junit4Test = new Junit4Test();
+        Junit4Test junit4Test = mock(Junit4Test.class);
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
         List<Method> methods = List.of(clazz.getDeclaredMethods());
@@ -23,5 +28,11 @@ class Junit4TestRunner {
                         throw new RuntimeException(e);
                     }
                 });
+
+        assertAll(
+                () -> verify(junit4Test).one(),
+                () -> verify(junit4Test).two(),
+                () -> verify(junit4Test, never()).testThree()
+        );
     }
 }
