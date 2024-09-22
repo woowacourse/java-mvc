907024e8aaa3b11ca7267cc51be9c8ded7b3d3c6
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-23 01:55:09 +0900

    refactor: 불필요한 반복 로직 for문 밖으로 배치

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -8,12 +8,12 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
         for (Method method : clazz.getDeclaredMethods()) {
             MyTest annotation = method.getAnnotation(MyTest.class);
             if(annotation != null) {
-                Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
                 method.invoke(instance);
             }
         }
