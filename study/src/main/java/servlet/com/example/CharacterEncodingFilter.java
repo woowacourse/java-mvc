76560569e253720b375cdfd2b9af667cfc3800e6
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-09-22 19:21:20 +0900

    test(study): 학습테스트 완성

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -10,7 +10,9 @@ public class CharacterEncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
         request.getServletContext().log("doFilter() 호출");
+
         chain.doFilter(request, response);
     }
 }
