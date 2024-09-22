5646f8696df7edd21375d3e7d55be811a83434c3
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-09-23 02:00:16 +0900

    refactor: 불필요한 반복 로직 for문 밖으로 배치

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -8,14 +8,12 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        // Junit3Test의 인스턴스를 생성
+        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
         for (Method method : clazz.getDeclaredMethods()) {
             if (method.getName().startsWith("test")) {
-                // 메소드가 private일 경우 접근 가능하도록 설정
-                method.setAccessible(true);
-                // Junit3Test의 인스턴스를 생성
-                Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
                 // 메소드 실행
                 method.invoke(instance);
             }
