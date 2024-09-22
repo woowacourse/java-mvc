6b2b4f0e5fce7092b04685ddfc197669576f105b
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-22 12:47:10 +0900

    feat: JsonView 구현 및 테스트

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,31 @@
 package com.interface21.webmvc.servlet.view;
 
+import java.util.Map;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.Map;
-
 public class JsonView implements View {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String json = createJson(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(json);
+    }
+
+    private String createJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Object value = model.values().iterator().next();
+            return objectMapper.writeValueAsString(value);
+        }
+
+        return objectMapper.writeValueAsString(model);
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -0,0 +1,69 @@
+package com.interface21.webmvc.servlet.view;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.interface21.web.http.MediaType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class JsonViewTest {
+
+    @Test
+    @DisplayName("model에 데이터가 1개면 값을 그대로 반환한다.")
+    void returnElementWhenModelSizeIsOne() throws Exception {
+        // given
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView jsonView = new JsonView();
+        Map<String, Object> model = new LinkedHashMap<>();
+        model.put("user", new User("gooreum", 25));
+
+        // when
+        jsonView.render(model, request, response);
+
+        // then
+        assertAll(
+                () -> verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE),
+                () -> verify(response.getWriter()).write("{\"name\":\"gooreum\",\"age\":25}")
+        );
+    }
+
+    @Test
+    @DisplayName("model에 데이터가 2개 이상이면 Map 형태 그대로 반환한다.")
+    void returnMapWhenModelHasMoreThanOneElement() throws Exception {
+        // given
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView jsonView = new JsonView();
+        Map<String, Object> model = new LinkedHashMap<>();
+        model.put("user1", new User("gooreum1", 1));
+        model.put("user2", new User("gooreum2", 2));
+
+        // when
+        jsonView.render(model, request, response);
+
+        // then
+        assertAll(
+                () -> verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE),
+                () -> verify(response.getWriter()).write("{\"user1\":{\"name\":\"gooreum1\",\"age\":1},\"user2\":{\"name\":\"gooreum2\",\"age\":2}}")
+        );
+    }
+
+    record User(String name, int age) {
+    }
+}
