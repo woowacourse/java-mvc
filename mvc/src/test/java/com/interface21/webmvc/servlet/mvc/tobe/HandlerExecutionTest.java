8844e3e6671cf970ed73a86a5249f5c14ffc95ea
nayonsoso <panda0329@naver.com>
2024-09-23 01:22:18 +0900

    test: 핸들러 메서드를 저장할 객체 테스트

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionTest.java
@@ -0,0 +1,43 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JsonView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HandlerExecutionTest {
+
+    @Test
+    @DisplayName("주어진 메서드에 대해 HandlerExecution 객체를 생성한다.")
+    void create() throws NoSuchMethodException {
+        Method declaredMethod = TestClass.class.getDeclaredMethod(
+                "testMethod", HttpServletRequest.class, HttpServletResponse.class);
+
+        assertThatCode(() -> new HandlerExecution(declaredMethod))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("저장해둔 메서드를 실행한다.")
+    void handle() throws Exception {
+        Method declaredMethod = TestClass.class.getDeclaredMethod(
+                "testMethod", HttpServletRequest.class, HttpServletResponse.class);
+        HandlerExecution handlerExecution = new HandlerExecution(declaredMethod);
+
+        ModelAndView modelAndView = handlerExecution.handle(null, null);
+
+        assertThat(modelAndView.getView()).isInstanceOf(JsonView.class);
+    }
+
+    static class TestClass {
+        public ModelAndView testMethod(HttpServletRequest request, HttpServletResponse response) {
+            return new ModelAndView(new JsonView());
+        }
+    }
+}
