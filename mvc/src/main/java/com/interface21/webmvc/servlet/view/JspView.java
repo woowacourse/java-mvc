f100128a86eea4a6f5bdcde2d2614eceff4e4731
hoyeonyy <highoption@naver.com>
2024-09-22 15:51:37 +0900

    refactor: JspView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,31 +1,42 @@
 package com.interface21.webmvc.servlet.view;
 
-import com.interface21.webmvc.servlet.View;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
+import com.interface21.webmvc.servlet.View;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class JspView implements View {
 
-    private static final Logger log = LoggerFactory.getLogger(JspView.class);
+	private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
-    public static final String REDIRECT_PREFIX = "redirect:";
+	private static final String REDIRECT_PREFIX = "redirect:";
 
-    public JspView(final String viewName) {
-    }
+	public final String url;
 
-    @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+	public JspView(final String url) {
+		this.url = url;
+	}
 
-        model.keySet().forEach(key -> {
-            log.debug("attribute name : {}, value : {}", key, model.get(key));
-            request.setAttribute(key, model.get(key));
-        });
+	@Override
+	public void render(final Map<String, ?> model, final HttpServletRequest request,
+		final HttpServletResponse response) throws Exception {
+		if (url.startsWith(JspView.REDIRECT_PREFIX)) {
+			response.sendRedirect(url.substring(JspView.REDIRECT_PREFIX.length()));
+			return;
+		}
 
-        // todo
-    }
+		model.keySet().forEach(key -> {
+			log.debug("attribute name : {}, value : {}", key, model.get(key));
+			request.setAttribute(key, model.get(key));
+		});
+
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
+		requestDispatcher.forward(request, response);
+	}
 }
