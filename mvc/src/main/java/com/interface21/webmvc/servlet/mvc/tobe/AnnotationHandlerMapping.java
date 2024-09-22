549011f1ec85f3e2db2893a4b116d6ea64ce0db8
seunghye218 <seunghye@student.42seoul.kr>
2024-09-22 21:31:20 +0900

    feat: @MVC Framework 테스트 통과하기

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +28,55 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage, Scanners.TypesAnnotated);
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(controller -> {
+                    try {
+                        registerControllerHandlers(controller);
+                    } catch (Exception e) {
+                        log.error("Failed to register controller handlers", e);
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void registerControllerHandlers(Class<?> controllerClass) throws Exception {
+        for (Method method : controllerClass.getMethods()) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                Object controller = createControllerInstance(controllerClass);
+                registerHandlerExecutions(controller, method, requestMapping);
+            }
+        }
+    }
+
+    private Object createControllerInstance(Class<?> controllerClass) throws Exception {
+        Constructor<?> constructor = controllerClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return constructor.newInstance();
+    }
+
+    private void registerHandlerExecutions(Object controller, Method method, RequestMapping requestMapping) {
+        String url = requestMapping.value();
+
+        RequestMethod[] requestMethods = requestMapping.method();
+        if (requestMapping.method().length == 0) {
+            requestMethods = RequestMethod.values();
+        }
+
+        Arrays.stream(requestMethods)
+                    .forEach(requestMethod -> registerHandlerExecution(url, requestMethod, controller, method));
+    }
+
+    private void registerHandlerExecution(String url, RequestMethod requestMethod, Object controller, Method method) {
+        HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+        handlerExecutions.put(handlerKey, new HandlerExecution(controller, method));
+    }
+
+    public Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
