16de68d44c8db10d5e83f3b147e06e52880ad04b
kelly6bf <kelly6bf@gmail.com>
2024-09-22 16:24:57 +0900

    feat : JspView render() 메서드 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,11 +1,18 @@
 package com.techcourse;
 
+import java.util.Map;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
 import com.interface21.webmvc.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
@@ -14,6 +21,7 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private ManualHandlerMapping manualHandlerMapping;
+    private AnnotationHandlerMapping annotationHandlerMapping;
 
     public DispatcherServlet() {
     }
@@ -21,7 +29,9 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init() {
         manualHandlerMapping = new ManualHandlerMapping();
+        annotationHandlerMapping = new AnnotationHandlerMapping();
         manualHandlerMapping.initialize();
+        annotationHandlerMapping.initialize();
     }
 
     @Override
@@ -30,22 +40,34 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            /**
+             * 레거시 코드
+             */
+//            final var controller = manualHandlerMapping.getHandler(requestURI);
+//            final var viewName = controller.execute(request, response);
+//            move(viewName, request, response);
+
+            final HandlerExecution handler = (HandlerExecution) annotationHandlerMapping.getHandler(request);
+            final ModelAndView modelAndView = handler.handle(request, response);
+            final Map<String, Object> model = modelAndView.getModel();
+            final View view = modelAndView.getView();
+            view.render(model, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
-    }
+    /**
+     * 레거시 코드
+     */
+//    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+//        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
+//            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
+//            return;
+//        }
+//
+//        final var requestDispatcher = request.getRequestDispatcher(viewName);
+//        requestDispatcher.forward(request, response);
+//    }
 }
