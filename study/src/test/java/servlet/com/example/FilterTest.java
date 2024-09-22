5631af86fc3c23e885fb49ef9a87e576982dd582
fromitive <fromitive@gmail.com>
2024-09-22 17:24:11 +0900

    test: 2단계 - 필터 학습 테스트 완료

--- study/src/test/java/servlet/com/example/FilterTest.java
+++ study/src/test/java/servlet/com/example/FilterTest.java
@@ -1,10 +1,9 @@
 package servlet.com.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import support.HttpUtils;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static servlet.com.example.KoreanServlet.인코딩;
 
 class FilterTest {
 
@@ -13,7 +12,7 @@ class FilterTest {
         // 톰캣 서버 시작
         final var tomcatStarter = new TomcatStarter("src/main/webapp/");
         tomcatStarter.start();
-
+        // init () -> doFilter() -> service() -> destroy() 호출
         final var response = HttpUtils.send("/korean");
 
         // 톰캣 서버 종료
@@ -22,6 +21,6 @@ class FilterTest {
         assertThat(response.statusCode()).isEqualTo(200);
 
         // 테스트가 통과하도록 CharacterEncodingFilter 클래스를 수정해보자.
-        assertThat(response.body()).isEqualTo(인코딩);
+        assertThat(response.body()).isEqualTo("인코딩");
     }
 }
