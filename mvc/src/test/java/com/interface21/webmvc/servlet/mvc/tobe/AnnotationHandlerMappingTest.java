46ec66c023cf0cb8957fdaacbc5fd221d3eafd8a
devhoya97 <diajbh123@gmail.com>
2024-09-22 22:41:39 +0900

    test: @EnumSource 활용

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,17 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 
 class AnnotationHandlerMappingTest {
@@ -54,12 +56,12 @@ class AnnotationHandlerMappingTest {
 
     @DisplayName("@RequestMapping()에 method 설정이 되어 있지 않으면 모든 HTTP method를 지원한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE"})
-    void supplyAllMethods(String methodName) throws Exception {
+    @EnumSource(RequestMethod.class)
+    void supplyAllMethods(RequestMethod requestMethod) throws Exception {
         // given
         when(request.getAttribute("id")).thenReturn("gugu");
         when(request.getRequestURI()).thenReturn("/all-method");
-        when(request.getMethod()).thenReturn(methodName);
+        when(request.getMethod()).thenReturn(requestMethod.name());
 
         // when
         HandlerExecution handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
