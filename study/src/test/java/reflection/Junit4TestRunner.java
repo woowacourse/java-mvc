2839bf150fc573f0290cd65eb66f87888aa09790
SungKyum Kim <sungkyum1@naver.com>
2024-09-23 13:48:43 +0900

    test: Junit4TestRunner

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +10,17 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test의 인스턴스를 생성
+        Junit4Test instance = new Junit4Test();
+
+        // 클래스의 모든 메서드를 조회
+        Method[] methods = clazz.getDeclaredMethods();
+
+        // @MyTest 애노테이션이 붙은 메서드만 실행
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(instance);  // 인스턴스에서 메서드 실행
+            }
+        }
     }
 }
