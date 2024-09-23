6400f95baed4902df34be23afbb5705991bd5bda
hyeonjilee <skylar12200@gmail.com>
2024-09-23 15:38:48 +0900

    refactor: redirect의 경우 attribute 설정하지 않도록 변경

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
