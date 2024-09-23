e10f78fc398a01694f00882abc57b1ddbb74f2f4
nhlee98 <e5slnh9876@gmail.com>
2024-09-24 00:04:49 +0900

    test: `getDeclaredFields()`를 `getFields()`로 변경

--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -90,9 +89,7 @@ class ReflectionTest {
     @Test
     void givenClass_whenGetsPublicFields_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Field[] fields = Arrays.stream(questionClass.getDeclaredFields())
-                .filter(field -> Modifier.isPublic(field.getModifiers()))
-                .toArray(Field[]::new);
+        final Field[] fields = questionClass.getFields();
 
         assertThat(fields).hasSize(0);
     }
