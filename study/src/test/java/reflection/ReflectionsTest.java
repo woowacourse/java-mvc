0377a3183b1058393cd70f725a73afe1e0af5eb6
SungKyum Kim <sungkyum1@naver.com>
2024-09-23 14:31:31 +0900

    test: ReflectionsTest

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,57 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
+import reflection.examples.JdbcQuestionRepository;
+import reflection.examples.JdbcUserRepository;
+import reflection.examples.MyQnaService;
+import reflection.examples.QnaController;
+
 class ReflectionsTest {
 
-    private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
+	private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
-    @Test
-    void showAnnotationClass() throws Exception {
-        Reflections reflections = new Reflections("reflection.examples");
+	@Test
+	void showAnnotationClass() throws Exception {
+		// Reflections로 reflection.examples 패키지를 탐색
+		Reflections reflections = new Reflections("reflection.examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
-    }
+		// @Controller 애노테이션이 붙은 클래스 찾기
+		Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+		log.info("Found Controllers:");
+		for (Class<?> clazz : controllers) {
+			log.info(clazz.getSimpleName());
+		}
+
+		// @Service 애노테이션이 붙은 클래스 찾기
+		Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+		log.info("Found Services:");
+		for (Class<?> clazz : services) {
+			log.info(clazz.getSimpleName());
+		}
+
+		// @Repository 애노테이션이 붙은 클래스 찾기
+		Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+		log.info("Found Repositories:");
+		for (Class<?> clazz : repositories) {
+			log.info(clazz.getSimpleName());
+		}
+
+		Assertions.assertAll(
+			() -> assertThat(controllers).containsExactly(QnaController.class),
+			() -> assertThat(services).containsExactly(MyQnaService.class),
+			() -> assertThat(repositories).containsExactly(JdbcQuestionRepository.class, JdbcUserRepository.class)
+		);
+	}
 }
