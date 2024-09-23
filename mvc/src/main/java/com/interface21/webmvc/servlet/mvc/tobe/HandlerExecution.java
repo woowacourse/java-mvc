f6dfcf076cf4daca905f8d710af37d1276c172e5
zangsu <zangsu_@naver.com>
2024-09-23 22:29:01 +0900

    refactor: 필드 명 명확하게 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -9,21 +9,21 @@ import com.interface21.webmvc.servlet.ModelAndView;
 
 public class HandlerExecution {
 
-    private final Object clazz;
+    private final Object runnerInstance;
     private final Method method;
 
-    public HandlerExecution(Class<?> clazz, Method method) {
+    public HandlerExecution(Class<?> runnerInstanceClazz, Method method) {
         try {
-            this.clazz = clazz.getConstructor().newInstance();
+            this.runnerInstance = runnerInstanceClazz.getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(String.format(
-                    "컨트롤러 %s 를 생성할 수 있는 기본 생성자가 존재하지 않습니다.", clazz.getName())
+                    "컨트롤러 %s 를 생성할 수 있는 기본 생성자가 존재하지 않습니다.", runnerInstanceClazz.getName())
             );
         }
         this.method = method;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(clazz, request, response);
+        return (ModelAndView) method.invoke(runnerInstance, request, response);
     }
 }
