83b7c07028e3dc96bc919e3bea0bb611285beba1
Eugene Jang <kyunellroll@gmail.com>
2024-09-23 23:08:26 +0900

    refactor(HandlerKeyMakerMapping): KeyMaker를 싱글톤 벙글톤으로 관리하도록 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/keymaker/HandlerKeyMakerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/keymaker/HandlerKeyMakerMapping.java
@@ -1,20 +1,19 @@
-package com.interface21.webmvc.servlet.mvc.tobe.keyMaker;
+package com.interface21.webmvc.servlet.mvc.tobe.keymaker;
 
 import java.lang.reflect.Method;
 import java.util.List;
 
 public class HandlerKeyMakerMapping {
 
-    private final List<KeyMaker> keyMakers;
+    private static final List<KeyMaker> keyMakers = List.of(new RequestMappingKeyMaker());
 
     public HandlerKeyMakerMapping() {
-        this.keyMakers = List.of(new RequestMappingKeyMaker());
     }
 
     public KeyMaker match(Method method) {
         return keyMakers.stream()
                 .filter(keyMaker -> keyMaker.hasAnnotation(method))
                 .findAny()
-                .orElseThrow(() -> new IllegalStateException("해당하는 Method에 매핑할 수 있는 어노테이션이 존재하지 않습니다."));
+                .orElse(null);
     }
 }
