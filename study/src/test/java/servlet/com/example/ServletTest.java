1a1c20a1a7cb2a833fdc0ec83f3f6c29b4d74ad2
SungKyum Kim <sungkyum1@naver.com>
2024-09-23 13:30:25 +0900

    test: 서블릿 학습 테스트

--- study/src/test/java/servlet/com/example/ServletTest.java
+++ study/src/test/java/servlet/com/example/ServletTest.java
@@ -28,7 +28,9 @@ class ServletTest {
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
         // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        // 서블릿은 요청마다 한개만 존재한다.
+        // 하나의 서블릿을 재사용하는 과정에서 3번의 counter 변수가 변경되는 결과가 발생한다.
+        assertThat(Integer.parseInt(response.body())).isEqualTo(3);
     }
 
     @Test
@@ -50,6 +52,9 @@ class ServletTest {
 
         // expected를 0이 아닌 올바른 값으로 바꿔보자.
         // 예상한 결과가 나왔는가? 왜 이런 결과가 나왔을까?
-        assertThat(Integer.parseInt(response.body())).isEqualTo(0);
+        // 마찬가지로 서블릿은 요청마다 한개만 존재한다.
+        // localCounter는 로컬 변수로 선언되어 있다. 즉, 이 변수는 service() 메서드가 호출될 때마다 새로 초기화된다.
+        // 따라서, 매번 localCounter가 0으로 초기화되고 localCounter++로 인해 1이 되어 응답
+        assertThat(Integer.parseInt(response.body())).isEqualTo(1);
     }
 }
