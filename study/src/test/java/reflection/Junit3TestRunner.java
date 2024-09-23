ce7f808819739cec674c31fdd8ba6cff2277d204
ashsty <qkralsdk991@naver.com>
2024-09-23 18:07:21 +0900

    test: 학습 테스트 보강

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,20 +1,41 @@
 package reflection;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class Junit3TestRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(Junit3TestRunner.class);
+
     @Test
+    @DisplayName("테스트 이름이 test로 시작하는 테스트들을 실행한다.")
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test instance = new Junit3Test();
 
-        Reflections reflections = new Reflections(clazz);
+        Method[] methods = clazz.getDeclaredMethods();
+        List<String> executedMethods = new ArrayList<>();
 
-        for (Method method : reflections.getMethodsAnnotatedWith(Test.class)) {
-            method.invoke(clazz);
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                log.info("메서드 실행: " + method.getName());
+                method.invoke(instance);
+                executedMethods.add(method.getName());
+            }
         }
+
+        assertAll(
+                () -> assertThat(executedMethods).hasSize(2),
+                () -> assertThat(executedMethods).doesNotContain("three")
+        );
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,21 +1,39 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class Junit4TestRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Junit4TestRunner.class);
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test instance = new Junit4Test();
 
-        Reflections reflections = new Reflections(clazz);
+        Method[] methods = clazz.getDeclaredMethods();
+        List<String> executedMethods = new ArrayList<>();
 
-        for (Method method : reflections.getMethodsAnnotatedWith(MyTest.class)) {
-            method.invoke(clazz);
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                log.info("메서드 실행: " + method.getName());
+                method.invoke(instance);
+                executedMethods.add(method.getName());
+            }
         }
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+
+        assertAll(
+                () -> assertThat(executedMethods).hasSize(2),
+                () -> assertThat(executedMethods).doesNotContain("testThree")
+        );
     }
 }
