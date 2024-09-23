4dfbbe1d8532386e3d00c1fa60ff472d2e6b9fae
HyungUk Ryu <hw0603@naver.com>
2024-09-23 13:30:09 +0900

    feat(HandlerExecutionHandlerAdapter): HandlerExecution을 담당하는 HandlerAdapter 구현체 구현

---
+++ app/src/main/java/com/techcourse/HandlerExecutionHandlerAdapter.java
@@ -0,0 +1,21 @@
+package com.techcourse;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HandlerExecutionHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean canHandle(Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        HandlerExecution handlerExecution = (HandlerExecution) handler;
+        return handlerExecution.handle(request, response);
+    }
+}
