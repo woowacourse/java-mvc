cb16c79b6ff0fedbd2c0e73ab21d60074ac4c016
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-09-23 12:16:01 +0900

    test: HandlerAdapter 관련 테스트 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -4,6 +4,8 @@ import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,5 +35,29 @@ public class JspView implements View {
         });
         final var requestDispatcher = request.getRequestDispatcher(viewName);
         requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        JspView jspView = (JspView) o;
+        return Objects.equals(viewName, jspView.viewName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(viewName);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", JspView.class.getSimpleName() + "[", "]")
+                .add("viewName='" + viewName + "'")
+                .toString();
     }
 }
