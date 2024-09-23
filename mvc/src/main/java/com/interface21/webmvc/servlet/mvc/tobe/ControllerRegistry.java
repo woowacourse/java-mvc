78855ba4afa814fd452d9bdb660913521350c15a
Eugene Jang <kyunellroll@gmail.com>
2024-09-23 20:17:17 +0900

    fix(ControllerRegistry): 생성자를 가져오는 과정에서 발생할 수 있는 에러 핸들링 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerRegistry.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerRegistry.java
@@ -1,19 +1,41 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ControllerRegistry {
 
+    private static final Logger log = LoggerFactory.getLogger(ControllerRegistry.class);
     private static final Map<Class<?>, Object> controllers = new HashMap<>();
 
-    public static Object getOrCreateController(Class<?> controller) throws Exception {
+    public static Object getOrCreateController(Class<?> controller) {
         if (controllers.containsKey(controller)) {
             return controllers.get(controller);
         }
 
-        Object controllerInstance = controller.getConstructor().newInstance();
-        controllers.put(controller, controllerInstance);
-        return controllerInstance;
+        try {
+            Constructor<?> constructor = controller.getConstructor();
+            Object controllerInstance = constructor.newInstance();
+
+            controllers.put(controller, controllerInstance);
+            return controllerInstance;
+        } catch (NoSuchMethodException e) {
+            log.error("메서드를 찾지 못했습니다. {}", e.getStackTrace()[0]);
+            throw new IllegalArgumentException("메서드를 찾지 못했습니다.");
+        } catch (IllegalAccessException e) {
+            log.error("생성자에 접근할 수 없습니다. {}", e.getStackTrace()[0]);
+            throw new IllegalStateException("생성자에 접근할 수 없습니다.");
+        } catch (ExceptionInInitializerError e) {
+            log.error("메서드를 초기화 하던 도중 실패했습니다. {}", e.getStackTrace()[0]);
+            throw new IllegalStateException("메서드를 초기화 하던 도중 실패했습니다.");
+        } catch (InvocationTargetException | InstantiationException e) {
+            log.error("생성자를 실행하던 도중 실패했습니다. {}", e.getStackTrace()[0]);
+            throw new IllegalStateException("생성자를 실행하던 도중 실패했습니다.");
+        }
     }
 }
