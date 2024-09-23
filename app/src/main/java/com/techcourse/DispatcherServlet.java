19f5eec115964b70fe5cb6241ad2592a6ecfc5b0
임수빈 <robin980108@naver.com>
2024-09-24 01:13:23 +0900

    refactor: DispatcherServlet 에서 새로 통합된 프레임워크의 인터페이스들을 사용하도록 수정

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,10 +2,10 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
-import com.interface21.webmvc.servlet.view.JspView;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMappingAdaptor;
+import com.interface21.webmvc.servlet.mvc.tobe.CompositeHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdaptor;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,18 +19,22 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
-    private AnnotationHandlerMapping annotationHandlerMapping;
+    private CompositeHandlerMapping handlerMapping;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
-        annotationHandlerMapping = new AnnotationHandlerMapping(getPackageName());
-        annotationHandlerMapping.initialize();
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(getPackageName());
+        var controllerHandlerMappingAdaptor = new ControllerHandlerMappingAdaptor(manualHandlerMapping);
+        var annotationHandlerMappingAdaptor = new AnnotationHandlerMappingAdaptor(annotationHandlerMapping);
+
+        this.handlerMapping = new CompositeHandlerMapping(
+                controllerHandlerMappingAdaptor,
+                annotationHandlerMappingAdaptor
+        );
     }
 
     private String getPackageName() {
@@ -42,57 +46,20 @@ public class DispatcherServlet extends HttpServlet {
             throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
         try {
-            serviceByManual(request, response, requestURI);
-        } catch (NullPointerException e) {
-            serviceByAnnotation(request, response);
+            HandlerAdaptor handler = handlerMapping.getHandler(request);
+            ModelAndView modelAndView = handler.handle(request, response);
+            render(modelAndView, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void serviceByManual(HttpServletRequest request, HttpServletResponse response, String requestURI)
+    private void render(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        Controller controller = manualHandlerMapping.getHandler(requestURI);
-        final var viewName = controller.execute(request, response);
-        move(viewName, request, response);
-    }
-
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response)
-            throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
-    }
-
-    private void serviceByAnnotation(HttpServletRequest request, HttpServletResponse response) {
-        HandlerExecution handlerExecution = (HandlerExecution) annotationHandlerMapping.getHandler(request);
-        ModelAndView modelAndView = executeHandlerExecution(handlerExecution, request, response);
-        move(modelAndView, request, response);
-    }
-
-    private ModelAndView executeHandlerExecution(HandlerExecution handlerExecution, HttpServletRequest request,
-                                                 HttpServletResponse response) {
-        try {
-            return handlerExecution.handle(request, response);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void move(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) {
         Map<String, Object> model = modelAndView.getModel();
         View view = modelAndView.getView();
-        try {
-            view.render(model, request, response);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        view.render(model, request, response);
     }
 }
