a5d79bbef658d181b60f61cfa55a43f9da480f02
HyungUk Ryu <hw0603@naver.com>
2024-09-23 15:55:59 +0900

    Test(ControllerScannerTest): 컨트롤러 클래스의 인스턴스화 실패 테스트 추가

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScannerTest.java
@@ -0,0 +1,24 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+
+class ControllerScannerTest {
+
+    @DisplayName("@Controller 어노테이션이 붙은 클래스를 인스턴스화할수 없는 경우 예외가 발생한다.")
+    @Test
+    void throwsWhenCannotInstantiateController() {
+        // given
+        Reflections reflections = new Reflections("com.interface21.webmvc.servlet.mvc.tobe");
+        ControllerScanner controllerScanner = new ControllerScanner(reflections);
+
+        // when & then
+        assertThatThrownBy(controllerScanner::getControllers)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(
+                        "컨트롤러를 인스턴스화 할 수 없습니다.");
+    }
+}
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/FakeController.java
@@ -0,0 +1,11 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.context.stereotype.Controller;
+
+@Controller
+
+public class FakeController {
+
+    private FakeController() {
+    }
+}
