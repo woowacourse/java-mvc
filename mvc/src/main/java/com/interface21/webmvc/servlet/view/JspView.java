c72ddd90a66362d03f99350f47338978e057a95c
임수빈 <robin980108@naver.com>
2024-09-24 00:56:58 +0900

    refactor: AbstractView 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,22 +1,20 @@
 package com.interface21.webmvc.servlet.view;
 
-import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.AbstractView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JspView implements View {
+public class JspView extends AbstractView {
 
     private static final Logger log = LoggerFactory.getLogger(JspView.class);
 
     public static final String REDIRECT_PREFIX = "redirect:";
 
-    private final String viewName;
-
     public JspView(String viewName) {
-        this.viewName = viewName;
+        super(viewName);
     }
 
     @Override
