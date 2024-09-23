9a35d7675335db9e2c720b02c2665e0fdb3dea7a
nhlee98 <e5slnh9876@gmail.com>
2024-09-23 22:32:29 +0900

    refactor: HandlerExecution의 필드명을 controller에서 handler로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,21 +1,21 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    private final Object controller;
+    private final Object handler;
     private final Method method;
 
-    public HandlerExecution(Object controller, Method method) {
-        this.controller = controller;
+    public HandlerExecution(Object handler, Method method) {
+        this.handler = handler;
         this.method = method;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(controller, request, response);
+        return (ModelAndView) method.invoke(handler, request, response);
     }
 }
