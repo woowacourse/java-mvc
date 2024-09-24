6b714d6189d79cf418ccfa47fbf77f0db4c62bd3
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-24 22:06:11 +0900

    feat(HandlerMappings): 핸들러 매핑을 찾아주는 일급 컬랙션 구현

---
+++ app/src/main/java/com/techcourse/HandlerMappings.java
@@ -0,0 +1,27 @@
+package com.techcourse;
+
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HandlerMappings {
+
+    private final Set<HandlerMapping> handlerMappings;
+
+    public HandlerMappings(HandlerMapping... handlerMappings) {
+        this.handlerMappings = Set.of(handlerMappings);
+    }
+
+    public void initialize() {
+        handlerMappings.forEach(HandlerMapping::initialize);
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
