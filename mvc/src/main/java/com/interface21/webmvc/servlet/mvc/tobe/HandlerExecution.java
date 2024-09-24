9244f370efe6e9725efa9bd005653fba6aca8755
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-09-24 20:52:33 +0900

    feat: 확인용 로그 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -2,17 +2,23 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerExecution.class);
+
     private final Object controller;
     private final Method method;
 
     public HandlerExecution(final Object controller, final Method method) {
         this.controller = controller;
         this.method = method;
+        log.info("Controller Instance : {}, Controller Method : {}",controller,method);
     }
 
     public Object handle(final HttpServletRequest request, final HttpServletResponse response) {
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerKey.java
@@ -2,10 +2,14 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class HandlerKey {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerKey.class);
 
     private final String url;
     private final RequestMethod requestMethod;
@@ -18,6 +22,7 @@ public class HandlerKey {
     public HandlerKey(final String url, final RequestMethod requestMethod) {
         this.url = url;
         this.requestMethod = requestMethod;
+        log.info("Url : {}, HTTP Method : {}",url,requestMethod);
     }
 
     public HandlerKey(final String url) {
