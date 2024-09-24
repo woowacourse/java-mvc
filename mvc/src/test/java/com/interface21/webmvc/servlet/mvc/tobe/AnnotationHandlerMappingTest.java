3ee7709c5c8db182a3f5e7f2b004d5887eea65f3
pricelees <priceelees@gmail.com>
2024-09-24 17:57:57 +0900

    refactor: 핸들러 등록 테스트에서의 하드코딩된 변수 수정

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,113 +1,135 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import samples.TestController;
 
 class AnnotationHandlerMappingTest {
 
-	private AnnotationHandlerMapping handlerMapping;
+    private AnnotationHandlerMapping handlerMapping;
 
-	@BeforeEach
-	void setUp() {
-		handlerMapping = new AnnotationHandlerMapping("samples");
-		handlerMapping.initialize();
-	}
+    @BeforeEach
+    void setUp() {
+        handlerMapping = new AnnotationHandlerMapping("samples");
+        handlerMapping.initialize();
+    }
 
-	@Test
-	void get() throws Exception {
-		final var request = mock(HttpServletRequest.class);
-		final var response = mock(HttpServletResponse.class);
+    @Test
+    void get() throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
 
-		when(request.getAttribute("id")).thenReturn("gugu");
-		when(request.getRequestURI()).thenReturn("/get-test");
-		when(request.getMethod()).thenReturn("GET");
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
 
-		final var handlerExecution = (HandlerExecution)handlerMapping.getHandler(request);
-		final var modelAndView = handlerExecution.handle(request, response);
+        final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        final var modelAndView = handlerExecution.handle(request, response);
 
-		assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
-	}
+        assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
 
-	@Test
-	void post() throws Exception {
-		final var request = mock(HttpServletRequest.class);
-		final var response = mock(HttpServletResponse.class);
+    @Test
+    void post() throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
 
-		when(request.getAttribute("id")).thenReturn("gugu");
-		when(request.getRequestURI()).thenReturn("/post-test");
-		when(request.getMethod()).thenReturn("POST");
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/post-test");
+        when(request.getMethod()).thenReturn("POST");
 
-		final var handlerExecution = (HandlerExecution)handlerMapping.getHandler(request);
-		final var modelAndView = handlerExecution.handle(request, response);
+        final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        final var modelAndView = handlerExecution.handle(request, response);
 
-		assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
-	}
+        assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
 
-	@DisplayName("요청 url에 대한 핸들러가 존재하지 않으면 null 핸들러를 반환한다.")
-	@Test
-	void handlerNotFound() {
-		final var request = mock(HttpServletRequest.class);
+    @DisplayName("요청 url에 대한 핸들러가 존재하지 않으면 null 핸들러를 반환한다.")
+    @Test
+    void handlerNotFound() {
+        final var request = mock(HttpServletRequest.class);
 
-		when(request.getRequestURI()).thenReturn("/non-existing-url");
-		when(request.getMethod()).thenReturn("GET");
+        when(request.getRequestURI()).thenReturn("/non-existing-url");
+        when(request.getMethod()).thenReturn("GET");
 
-		final var handlerExecution = handlerMapping.getHandler(request);
+        final var handlerExecution = handlerMapping.getHandler(request);
 
-		assertThat(handlerExecution).isNull();
-	}
+        assertThat(handlerExecution).isNull();
+    }
 
-	@DisplayName("요청 url은 같지만, 메서드가 다르면 null 핸들러를 반환한다.")
-	@Test
-	void methodNotAllowed() {
-		final var request = mock(HttpServletRequest.class);
+    @DisplayName("요청 url은 같지만, 메서드가 다르면 null 핸들러를 반환한다.")
+    @Test
+    void methodNotAllowed() {
+        final var request = mock(HttpServletRequest.class);
 
-		when(request.getRequestURI()).thenReturn("/post-test");
-		when(request.getMethod()).thenReturn("GET");
+        when(request.getRequestURI()).thenReturn("/post-test");
+        when(request.getMethod()).thenReturn("GET");
 
-		final var handlerExecution = handlerMapping.getHandler(request);
+        final var handlerExecution = handlerMapping.getHandler(request);
 
-		assertThat(handlerExecution).isNull();
-	}
+        assertThat(handlerExecution).isNull();
+    }
 
-	@DisplayName("RequestMapping에 메서드를 지정하지 않아도 처리할 수 있다.")
-	@Test
-	void noRequestMethod() throws Exception {
-		final var request = mock(HttpServletRequest.class);
-		final var response = mock(HttpServletResponse.class);
+    @DisplayName("RequestMapping에 메서드를 지정하지 않아도 처리할 수 있다.")
+    @Test
+    void noRequestMethod() throws Exception {
+        final var request = mock(HttpServletRequest.class);
 
-		when(request.getAttribute("id")).thenReturn("gugu");
-		when(request.getRequestURI()).thenReturn("/no-request-method");
-		when(request.getMethod()).thenReturn("GET");
+        when(request.getRequestURI()).thenReturn("/no-request-method");
+        when(request.getMethod()).thenReturn("GET");
 
-		final var handlerExecution = (HandlerExecution)handlerMapping.getHandler(request);
-		final var modelAndView = handlerExecution.handle(request, response);
+        final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        assertThat(handlerExecution).isNotNull();
+    }
 
-		assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
-	}
+    @DisplayName("핸들러의 정상 등록을 확인한다.")
+    @Test
+    void initialize() throws Exception {
+        Class<?> clazz = AnnotationHandlerMapping.class;
+        AnnotationHandlerMapping instance = (AnnotationHandlerMapping) clazz.getDeclaredConstructor(Object[].class)
+                .newInstance((Object) new Object[]{"samples"});
 
-	@DisplayName("핸들러의 정상 등록을 확인한다.")
-	@Test
-	void initialize() throws Exception {
-		Class<?> clazz = handlerMapping.getClass();
+        instance.initialize();
 
-		Field handlerExecutions = clazz.getDeclaredField("handlerExecutions");
-		handlerExecutions.setAccessible(true);
+        Field handlerExecutions = clazz.getDeclaredField("handlerExecutions");
+        handlerExecutions.setAccessible(true);
 
-		Map<?, ?> executions = (Map<?, ?>)handlerExecutions.get(handlerMapping);
+        Map<?, ?> executions = (Map<?, ?>) handlerExecutions.get(handlerMapping);
 
-		assertThat(executions.size()).isEqualTo(2);
-		assertThat(executions.keySet())
-			.extracting("url")
-			.containsExactlyInAnyOrder("/get-test", "/post-test");
-	}
+        List<String> requestUrl = Stream.of(TestController.class.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .map(method -> method.getAnnotation(RequestMapping.class).value())
+                .toList();
+
+        int expectedHandlerExecutionsSize = Stream.of(TestController.class.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .map(method -> {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    RequestMethod[] methods = annotation.method();
+                    int methodLength = methods.length;
+                    if (methodLength != 0) {
+                        return methodLength;
+                    }
+                    return RequestMethod.values().length;
+                })
+                .reduce(0, Integer::sum);
+
+        assertThat(executions.size()).isEqualTo(expectedHandlerExecutionsSize);
+        assertThat(executions.keySet())
+                .extracting("url")
+                .containsAll(requestUrl);
+    }
 }
