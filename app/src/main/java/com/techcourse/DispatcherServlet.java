d1bf7aad275353b8031fab39d17c5678ae653664
Nam Gi Beom <rlqja0523@naver.com>
2024-09-24 18:55:15 +0900

    feat: view render 추가

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.HandlerMapping;
 import com.interface21.webmvc.servlet.mvc.adepter.ControllerHandlerAdapter;
@@ -32,10 +33,17 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void init() {
+        initHandlerMappings();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerMappings() {
         handlerMappings.add(new ManualHandlerMapping());
         handlerMappings.add(new AnnotationHandlerMapping("com.techcourse.controller"));
         handlerMappings.forEach(HandlerMapping::initialize);
+    }
 
+    private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerHandlerAdapter());
         handlerAdapters.add(new HandlerExecutionHandlerAdapter());
     }
@@ -48,10 +56,11 @@ public class DispatcherServlet extends HttpServlet {
             Object handler = getHandler(request);
             if (handler == null) {
                 response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
             }
             HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
             ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
-            // todo
+            render(modelAndView, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
@@ -75,5 +84,19 @@ public class DispatcherServlet extends HttpServlet {
             }
         }
         throw new ServletException("There is no handler adapter for handler: %s".formatted(handler.getClass().getCanonicalName()));
+    }
+
+    private void render(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        View view = modelAndView.getView();
+        if (view == null) {
+            throw new ServletException("No view found in ModelAndView: %s, servlet with name: %s"
+                    .formatted(modelAndView, getServletName()));
+        }
+        try {
+            view.render(modelAndView.getModel(), request, response);
+        } catch (Exception e) {
+            log.debug("error rendering view: {}", view, e);
+            throw e;
+        }
     }
 }
