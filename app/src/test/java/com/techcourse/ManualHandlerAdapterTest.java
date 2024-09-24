b881ff24dbdb95f9c73454686a7e5154149e0b32
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-25 00:56:10 +0900

    test: ManualHandlerAdapter 테스트 구현

---
+++ app/src/test/java/com/techcourse/ManualHandlerAdapterTest.java
@@ -0,0 +1,61 @@
+package com.techcourse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.controller.RegisterController;
+
+class ManualHandlerAdapterTest {
+
+    @Nested
+    @DisplayName("지원하는 핸들러인지 판단")
+    class Supports {
+
+        @Test
+        @DisplayName("성공 : 지원하는 핸들러(Controller)는 true 반환")
+        void supportsTrue() {
+            ManualHandlerAdapter adapter = new ManualHandlerAdapter();
+
+            boolean actual = adapter.supports(new RegisterController());
+
+            assertThat(actual).isTrue();
+        }
+
+        @Test
+        @DisplayName("성공 : 지원하는 핸들러는 false 반환")
+        void supportsFalse() {
+            ManualHandlerAdapter adapter = new ManualHandlerAdapter();
+
+            boolean actual = adapter.supports("not exists");
+
+            assertThat(actual).isFalse();
+        }
+    }
+
+    @Test
+    @DisplayName("핸들러 실행")
+    void handle() throws Exception {
+        ManualHandlerAdapter adapter = new ManualHandlerAdapter();
+        Controller controller = mock(Controller.class);
+        adapter.supports(controller);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        when(controller.execute(request, response)).thenReturn("/");
+
+        ModelAndView actual = adapter.handle(request, response);
+
+        ModelAndView expected = new ModelAndView(new JspView("/"));
+        assertThat(actual.getView()).isEqualTo(expected.getView());
+    }
+}
