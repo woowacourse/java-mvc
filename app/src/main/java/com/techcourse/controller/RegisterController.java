ae5eb72968649b67ec8debf3bd4b59c669f213d2
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-24 21:17:17 +0900

    refactor: RegisterController 어노테이션 기반으로 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.techcourse.controller;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.Model;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
@@ -14,17 +15,24 @@ import jakarta.servlet.http.HttpServletResponse;
 public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
+    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) throws Exception {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserRepository.save(user);
-        return new ModelAndView(new JspView("redirect:/index.jsp"));
+        JspView view = new JspView("redirect:/index.jsp");
+        Model model = new Model();
+        model.addAttribute("user", user);
+        view.render(model.getModel(), req, res);
+        return new ModelAndView(model, view);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) {
-        return new ModelAndView(new JspView("/register.jsp"));
+    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        JspView view = new JspView("redirect:/register.jsp");
+        Model model = new Model();
+        view.render(model.getModel(), req, res);
+        return new ModelAndView(model, view);
     }
 }
