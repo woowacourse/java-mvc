65e69f05ccb9b044a5f2122f2c67183ddc67f621
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-25 02:37:06 +0900

    refactor(ControllerHandlerAdapterTest): View 테스트 로직 ArgumentCaptor 사용하여 변경

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/ControllerHandlerAdapterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/ControllerHandlerAdapterTest.java
@@ -5,20 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Method;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
-import com.interface21.webmvc.servlet.view.JspView;
 
 import samples.TestAnnotationController;
 import samples.TestExtendsController;
@@ -51,23 +53,27 @@ class ControllerHandlerAdapterTest {
 
     @Nested
     @DisplayName("Controller 핸들러 실행")
-    class Handler {
+    class Handle {
 
         @Test
         @DisplayName("Controller 핸들러 실행 성공: ModelAndView 반환")
-        void handle() {
+        void handle() throws Exception {
             // given
             final HttpServletRequest request = mock(HttpServletRequest.class);
             final HttpServletResponse response = mock(HttpServletResponse.class);
-            final Controller testController = new TestExtendsController();
+            final RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+            final ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
+            when(request.getRequestDispatcher(argumentCaptor.capture())).thenReturn(requestDispatcher);
 
             // when
             final ControllerHandlerAdapter controllerHandlerAdapter = ControllerHandlerAdapter.getInstance();
 
             // then
-            final ModelAndView modelAndView = controllerHandlerAdapter.handle(testController, request, response);
+            final ModelAndView modelAndView = controllerHandlerAdapter.handle(new TestExtendsController(), request, response);
+            modelAndView.getView().render(modelAndView.getModel(), request, response);
             assertAll(
-                    () -> assertThat(modelAndView.getView()).isEqualTo(new JspView("/test.jsp")),
+                    () -> assertThat(argumentCaptor.getValue()).isEqualTo("/test.jsp"),
                     () -> assertThat(modelAndView.getModel()).isEmpty()
             );
         }
