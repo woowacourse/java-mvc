d2efc1590b40cd133e2dea61062574a5cac29ad5
HyungHoKim00 <hkim1109@naver.com>
2024-09-24 19:59:21 +0900

    feat: JSONView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final int UNIQUE_SIZE = 1;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        PrintWriter writer = response.getWriter();
+        if (model.size() == UNIQUE_SIZE) {
+            model.forEach((key, value) -> writer.print(value.toString()));
+            return;
+        }
+        responseJson(model, response, writer);
+    }
+
+    private void responseJson(Map<String, ?> model, HttpServletResponse response, Writer writer)
+            throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        mapper.writeValue(writer, model);
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -0,0 +1,63 @@
+package com.interface21.webmvc.servlet.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JsonViewTest {
+
+    @DisplayName("model의 attribute가 1개이면, value를 응답한다.")
+    @Test
+    void render_unique() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(outputStream);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView view = new JsonView();
+        Map<String, Object> model = Map.of("key", "value");
+
+        // when
+        view.render(model, request, response);
+        writer.flush();
+        String expected = outputStream.toString();
+
+        // then
+        assertThat(expected).isEqualTo("value");
+    }
+
+    @DisplayName("model의 attribute가 2개 이상이면, json 형태로 응답한다.")
+    @Test
+    void render_json() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        OutputStream outputStream = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(outputStream);
+        when(response.getWriter()).thenReturn(writer);
+
+        JsonView view = new JsonView();
+        Map<String, Object> model = Map.of("key1", "value1", "key2", "value2");
+
+        // when
+        view.render(model, request, response);
+        writer.flush();
+        String expected = outputStream.toString();
+
+        // then
+        assertThat(expected)
+                .contains("\"key1\":\"value1\"")
+                .contains("\"key2\":\"value2\"");
+    }
+}
