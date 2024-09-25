1f980a690917c0d25b1828537b08d4295600c32a
zangsu <zangsu_@naver.com>
2024-09-25 13:13:34 +0900

    refactor: 미션에서 요구하는 회원가입 컨트롤러 생성

---
+++ app/src/main/java/com/techcourse/controller/RegisterControllerV2.java
@@ -0,0 +1,32 @@
+package com.techcourse.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+
+@Controller
+public class RegisterControllerV2 {
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
+        final var user = new User(2,
+                req.getParameter("account"),
+                req.getParameter("password"),
+                req.getParameter("email"));
+        InMemoryUserRepository.save(user);
+
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) {
+        return new ModelAndView(new JspView("/register.jsp"));
+    }
+}
