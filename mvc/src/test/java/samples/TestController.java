9b79b047fd3ed411867039cd13a5dd02134794fa
linirini <2001yerin@naver.com>
2024-09-25 17:37:44 +0900

    test: 테스트용 Controller 추가

--- mvc/src/test/java/samples/TestController.java
+++ mvc/src/test/java/samples/TestController.java
@@ -2,40 +2,12 @@ package samples;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class TestController implements Controller {
 
-import com.interface21.context.stereotype.Controller;
-import com.interface21.web.bind.annotation.RequestMapping;
-import com.interface21.web.bind.annotation.RequestMethod;
-import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.view.JspView;
-
-@Controller
-public class TestController {
-
-    private static final Logger log = LoggerFactory.getLogger(TestController.class);
-
-    @RequestMapping(value = "/get-test", method = RequestMethod.GET)
-    public ModelAndView findUserId(final HttpServletRequest request, final HttpServletResponse response) {
-        log.info("test controller get method");
-        final var modelAndView = new ModelAndView(new JspView(""));
-        modelAndView.addObject("id", request.getAttribute("id"));
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/post-test", method = RequestMethod.POST)
-    public ModelAndView save(final HttpServletRequest request, final HttpServletResponse response) {
-        log.info("test controller post method");
-        final var modelAndView = new ModelAndView(new JspView(""));
-        modelAndView.addObject("id", request.getAttribute("id"));
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/all-test")
-    public ModelAndView any(final HttpServletRequest request, final HttpServletResponse response) {
-        log.info("test controller all method");
-        return null;
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        return "/test.jsp";
     }
 }
