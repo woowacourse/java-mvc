65f2f01fb84d057b2a241486a003477940029241
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-26 08:34:57 +0900

    feat: DispatcherServlet 로직 개선
    수동 컨트롤러 처리 -> 핸들러 매핑 및 어댑터

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,10 +1,12 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.ControllerHandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutionHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
-import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,7 +23,8 @@ public class DispatcherServlet extends HttpServlet {
 
     private ManualHandlerMapping manualHandlerMapping;
     private AnnotationHandlerMapping annotationHandlerMapping;
-    private List<HandlerMapping> mappings;
+    private List<HandlerMapping> handlerMappings;
+    private List<HandlerAdapter> handlerAdapters;
 
     public DispatcherServlet() {
     }
@@ -33,9 +36,13 @@ public class DispatcherServlet extends HttpServlet {
         annotationHandlerMapping = new AnnotationHandlerMapping("com.techcourse.controller");
         annotationHandlerMapping.initialize();
 
-        mappings = new ArrayList<>();
-        mappings.add(manualHandlerMapping);
-        mappings.add(annotationHandlerMapping);
+        handlerMappings = new ArrayList<>();
+        handlerMappings.add(manualHandlerMapping);
+        handlerMappings.add(annotationHandlerMapping);
+
+        handlerAdapters = new ArrayList<>();
+        handlerAdapters.add(new ControllerHandlerAdapter());
+        handlerAdapters.add(new HandlerExecutionHandlerAdapter());
     }
 
     @Override
@@ -43,16 +50,38 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            final Controller controller = manualHandlerMapping.getHandler(request);
-            final String viewName = controller.execute(request, response);
-            final JspView jspView = new JspView(viewName);
-            final ModelAndView modelAndView = new ModelAndView(jspView);
-            jspView.render(modelAndView.getModel(), request, response);
+            Object handler = getHandler(request);
+            if (handler == null) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            }
+            ModelAndView modelAndView = execute(request, response, handler);
+            View view = modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
         } catch (IllegalArgumentException e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        for (HandlerMapping handlerMapping : handlerMappings) {
+            Object handler = handlerMapping.getHandler(request);
+            if (handler != null) {
+                return handler;
+            }
+        }
+        return null;
+    }
+
+    private ModelAndView execute(
+            HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        for (HandlerAdapter handlerAdapter : handlerAdapters) {
+            if (handlerAdapter.support(handler)) {
+                return handlerAdapter.handle(request, response, handler);
+            }
+        }
+        return null;
     }
 }
