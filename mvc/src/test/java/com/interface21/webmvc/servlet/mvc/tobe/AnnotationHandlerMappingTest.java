a9ed4c69c4a34fcefaccd3394911993ff70e5fce
ksk0605 <sungkyum1@naver.com>
2024-09-26 00:12:29 +0900

    test: DisplayName 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -22,6 +23,7 @@ class AnnotationHandlerMappingTest {
 	}
 
 	@Test
+	@DisplayName("GET 요청에 대해 핸들러를 반환한다")
 	void get() throws Exception {
 		final var request = mock(HttpServletRequest.class);
 		final var response = mock(HttpServletResponse.class);
@@ -37,6 +39,7 @@ class AnnotationHandlerMappingTest {
 	}
 
 	@Test
+	@DisplayName("POST 요청에 대해 핸들러를 반환한다")
 	void post() throws Exception {
 		final var request = mock(HttpServletRequest.class);
 		final var response = mock(HttpServletResponse.class);
@@ -52,6 +55,7 @@ class AnnotationHandlerMappingTest {
 	}
 
 	@Test
+	@DisplayName("매칭되는 핸들러가 없을 경우 null을 반환한다")
 	void get_withNoMatchingHandler() {
 		final var request = mock(HttpServletRequest.class);
 
@@ -64,6 +68,7 @@ class AnnotationHandlerMappingTest {
 	}
 
 	@Test
+	@DisplayName("지원되지 않는 메서드에 대해 null을 반환한다")
 	void unsupportedMethod() {
 		final var request = mock(HttpServletRequest.class);
 
@@ -77,6 +82,7 @@ class AnnotationHandlerMappingTest {
 
 	@ParameterizedTest
 	@ValueSource(strings = {"GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE"})
+	@DisplayName("메서드가 지정되지 않은 경우 모든 메소드르 지원한다.")
 	void noMethodSpecified(String method) throws Exception {
 		final var request = mock(HttpServletRequest.class);
 		final var response = mock(HttpServletResponse.class);
