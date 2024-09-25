4d58b05e953ee6df396392f9ce23fbfe0a6a5912
J-I-H-O <jeongjiho0731@gmail.com>
2024-09-25 20:19:39 +0900

    test: 컴포넌트 스캔 관련 테스트 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AnnotationHandlerMappingTest {
@@ -47,5 +48,31 @@ class AnnotationHandlerMappingTest {
         final var modelAndView = handlerExecution.handle(request, response);
 
         assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+
+    @DisplayName("지원하지 않는 URL로 요청 시 handlerExecution은 null이다.")
+    @Test
+    void invalidUrl() {
+        final var request = mock(HttpServletRequest.class);
+
+        when(request.getRequestURI()).thenReturn("/invalid-url");
+        when(request.getMethod()).thenReturn("POST");
+
+        final var handlerExecution = handlerMapping.getHandler(request);
+
+        assertThat(handlerExecution).isNull();
+    }
+
+    @DisplayName("유효한 URL이더라도, 지원하지 않는 메서드로 요청 시 hanlderExecution은 null이다.")
+    @Test
+    void invalidMethod() {
+        final var request = mock(HttpServletRequest.class);
+
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("POST");
+
+        final var handlerExecution = handlerMapping.getHandler(request);
+
+        assertThat(handlerExecution).isNull();
     }
 }
