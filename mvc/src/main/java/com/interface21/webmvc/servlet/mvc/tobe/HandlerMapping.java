3d9fa71a0be7ac1e6e3ff7638f293ab31875af8d
BurningFalls <sherlocky31415@gmail.com>
2024-09-25 17:34:53 +0900

    feature: HandlerAdapter 인터페이스 메서드 재정의

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerMapping.java
@@ -2,8 +2,6 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import com.interface21.webmvc.servlet.View;
-
 public interface HandlerMapping {
-    View handle(HttpServletRequest request);
+    Object getHandler(HttpServletRequest request);
 }
