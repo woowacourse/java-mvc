747a4ed02ad18166779592c8dcec5d55f61accc9
woojong <dwax1324@gmail.com>
2024-09-25 11:30:00 +0900

    refactor: 불필요한 테스트 어노테이션 제거

--- study/src/test/java/reflection/Junit3Test.java
+++ study/src/test/java/reflection/Junit3Test.java
@@ -4,17 +4,14 @@ import org.junit.jupiter.api.Test;
 
 
 public class Junit3Test{
-    @Test
     public void test1() throws Exception {
         System.out.println("Running Test1");
     }
 
-    @Test
     public void test2() throws Exception {
         System.out.println("Running Test2");
     }
 
-    @Test
     public void three() throws Exception {
         System.out.println("Running Test3");
     }
