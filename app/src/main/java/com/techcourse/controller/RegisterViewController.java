de76d25fc458c38b671ad5cd5dc50ea81b43d180
kelly6bf <kelly6bf@gmail.com>
2024-09-25 12:39:59 +0900

    test: 레거시 + @MVC 통합 테스트를 위한 @MVC RegisterController 구현

--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -1,13 +1,13 @@
-package com.techcourse.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
-
-public class RegisterViewController implements Controller {
-
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        return "/register.jsp";
-    }
-}
+//package com.techcourse.controller;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import com.interface21.webmvc.servlet.mvc.asis.Controller;
+//
+//public class RegisterViewController implements Controller {
+//
+//    @Override
+//    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+//        return "/register.jsp";
+//    }
+//}
