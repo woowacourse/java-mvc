34ba6bbc3aa5941d2c9cea5686897d88459997e2
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-26 00:07:13 +0900

    refactor: var을 타입으로 수정

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -5,6 +5,7 @@ import com.interface21.webmvc.servlet.mvc.DispatcherServlet;
 import com.interface21.webmvc.servlet.mvc.adapter.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.mapping.AnnotationHandlerMapping;
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +25,7 @@ public class DispatcherServletInitializer implements WebApplicationInitializer {
         dispatcherServlet.addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
         dispatcherServlet.addHandlerAdapter(new AnnotationHandlerAdapter());
 
-        var registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
         if (registration == null) {
             throw new IllegalStateException("Failed to register servlet with name '" + DEFAULT_SERVLET_NAME + "'. " +
                                             "Check if there is another servlet registered under the same name.");
