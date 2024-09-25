afa0e3c36fc5e2cdff1a366468727791011a10cd
takoyakimchi <c4nyou@gmail.com>
2024-09-25 17:11:27 +0900

    refactor: ControllerScanner 분리 및 리팩터링

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.el.MethodNotFoundException;
@@ -10,8 +9,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,11 +27,38 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        for (Class<?> controllerClass : findControllerClasses()) {
-            initializeHandlerExecutions(controllerClass);
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+
+        for (Class<?> clazz : controllers.keySet()) {
+            List<Method> requestMappingMethods = findRequestMappingMethods(clazz);
+            mapHandlers(requestMappingMethods, controllers.get(clazz));
         }
 
         log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    private List<Method> findRequestMappingMethods(Class<?> clazz) {
+        return Arrays.stream(clazz.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void mapHandlers(List<Method> mapperMethods, Object controller) {
+        for (Method mapperMethod : mapperMethods) {
+            HandlerExecution handlerExecution = new HandlerExecution(mapperMethod, controller);
+            makeHandlerKeys(mapperMethod)
+                    .forEach(handlerKey -> handlerExecutions.put(handlerKey, handlerExecution));
+        }
+    }
+
+    private List<HandlerKey> makeHandlerKeys(Method mapperMethod) {
+        RequestMapping annotation = mapperMethod.getAnnotation(RequestMapping.class);
+        String uri = annotation.value();
+
+        return Arrays.stream(annotation.method())
+                .map(method -> new HandlerKey(uri, method))
+                .toList();
     }
 
     public Object getHandler(final HttpServletRequest request) {
@@ -53,54 +77,15 @@ public class AnnotationHandlerMapping {
     private HandlerExecution createNotFoundHandlerExecution() {
         try {
             DefaultController defaultController = DefaultController.getInstance();
+
             Method notFoundMethod = Arrays.stream(defaultController.getClass().getDeclaredMethods())
                     .filter(method -> method.getName().equals(NOT_FOUND_HANDLER_NAME))
                     .findAny()
                     .orElseThrow(MethodNotFoundException::new);
+
             return new HandlerExecution(notFoundMethod, defaultController);
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
-        }
-    }
-
-    private Set<Class<?>> findControllerClasses() {
-        Reflections reflections = new Reflections(basePackage);
-        return reflections.getTypesAnnotatedWith(Controller.class);
-    }
-
-    private void initializeHandlerExecutions(Class<?> controllerClass) {
-        for (Method mapperMethod : findMapperMethods(controllerClass)) {
-            addMappings(mapperMethod);
-        }
-    }
-
-    private List<Method> findMapperMethods(Class<?> clazz) {
-        return Arrays.stream(clazz.getMethods())
-                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                .toList();
-    }
-
-    private void addMappings(Method mapperMethod) {
-        RequestMapping annotation = mapperMethod.getAnnotation(RequestMapping.class);
-        Object controllerInstance = findControllerInstance(mapperMethod);
-
-        String uri = annotation.value();
-        RequestMethod[] requestMethods = annotation.method();
-
-        for (RequestMethod requestMethod : requestMethods) {
-            HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
-            HandlerExecution handlerExecution = new HandlerExecution(mapperMethod, controllerInstance);
-            handlerExecutions.put(handlerKey, handlerExecution);
-        }
-    }
-
-    private Object findControllerInstance(Method method) {
-        try {
-            return method.getDeclaringClass()
-                    .getDeclaredConstructor()
-                    .newInstance();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("해당 method의 인스턴스를 생성할 수 없습니다.");
         }
     }
 }
