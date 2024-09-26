c4e712aeef46e258c540bfe945d2a6cc21caf485
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-26 11:31:02 +0900

    test: Filter 학습 테스트

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -11,6 +12,15 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        // 방법1)
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        // 방법2)
+        //response.setContentType("text/html; charset=UTF-8");
+
+        // 방법3)
+        //response.setCharacterEncoding("UTF-8");
+        //response.setLocale(Locale.KOREA);
         chain.doFilter(request, response);
     }
 }
