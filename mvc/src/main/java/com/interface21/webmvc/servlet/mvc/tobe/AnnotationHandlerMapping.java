ba1bc47240efbe4c4d9a4e72c43b7f15ca801f1b
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-09-26 12:13:19 +0900

    refactor: 중복 로직 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,12 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -21,6 +27,7 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        createController().forEach(this::checkMethodOfController);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
@@ -29,8 +36,36 @@ public class AnnotationHandlerMapping {
         if (handlerExecutions.containsKey(handlerKey)) {
             return handlerExecutions.get(handlerKey);
         }
-        HandlerExecution handlerExecution = new HandlerExecution(basePackage);
-        handlerExecutions.put(handlerKey, handlerExecution);
-        return handlerExecution;
+        throw new IllegalArgumentException("No handler found for " + request.getRequestURI() + request.getMethod());
+    }
+
+    private List<Object> createController() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        return annotatedClasses.stream()
+                .map(this::constructController)
+                .toList();
+    }
+
+    private Object constructController(Class<?> annotatedClass) {
+        try {
+            return annotatedClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create controller instance", e);
+        }
+    }
+
+    private void checkMethodOfController(Object controller) {
+        Arrays.stream(controller.getClass().getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(method -> addHandlerExecution(controller, method));
+    }
+
+    private void addHandlerExecution(Object controller, Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        Arrays.stream(requestMapping.method())
+                .map(requestMethod -> new HandlerKey(requestMapping.value(), requestMethod))
+                .filter(key -> !handlerExecutions.containsKey(key))
+                .forEach(key -> handlerExecutions.put(key, new HandlerExecution(controller, method)));
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,73 +1,21 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
-import com.interface21.web.bind.annotation.RequestMapping;
-import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.reflections.Reflections;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class HandlerExecution {
 
-    private final Object[] basePackage;
-    private final List<Object> controllers;
+    private final Object controller;
+    private final Method method;
 
-    public HandlerExecution(Object[] basePackage) {
-        this.basePackage = basePackage;
-        this.controllers = createController();
+    public HandlerExecution(Object controller, Method method) {
+        this.controller = controller;
+        this.method = method;
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return controllers.stream()
-                .flatMap(controller -> Arrays.stream(controller.getClass().getDeclaredMethods())
-                        .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                        .filter(method -> isMatchingRequest(request, method))
-                        .map(method -> invokeMethod(method, controller, request, response)))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Invalid request"));
-    }
-
-    private List<Object> createController() {
-        Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Controller.class);
-        return annotatedClasses.stream()
-                .map(this::constructController)
-                .toList();
-    }
-
-
-    private Object constructController(Class<?> annotatedClass) {
-        try {
-            return annotatedClass.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create controller instance", e);
-        }
-    }
-
-    private boolean isMatchingRequest(HttpServletRequest request, Method method) {
-        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-        boolean isMatchURI = requestMapping.value().startsWith(request.getRequestURI());
-        if (requestMapping.method() == null) {
-            return isMatchURI;
-        }
-        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
-        boolean isMatchMethod = Arrays.asList(requestMapping.method()).contains(requestMethod);
-        return isMatchURI && isMatchMethod;
-    }
-
-    private ModelAndView invokeMethod(Method method, Object controller, HttpServletRequest request, HttpServletResponse response) {
-        try {
-            return (ModelAndView) method.invoke(controller, request, response);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to invoke controller method", e);
-        }
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
