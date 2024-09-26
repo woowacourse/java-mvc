14216b52fe9ca5d20e1d1e3d9a2b5381b261a76b
3juhwan <13selfesteem91@naver.com>
2024-09-27 00:04:43 +0900

    test: DI 학습 테스트

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,20 +1,61 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
 class DIContainer {
 
+    private static final Logger log = LoggerFactory.getLogger(DIContainer.class);
+
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = new HashSet<>();
+        initializeBeans(classes);
+    }
+
+    private void initializeBeans(final Set<Class<?>> classes) {
+        constructBeans(classes);
+        beans.forEach(this::injectDependencies);
+    }
+
+    private void constructBeans(final Set<Class<?>> classes) {
+        classes.forEach(aClass -> {
+            try {
+                Constructor<?> constructor = aClass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                beans.add(constructor.newInstance());
+            } catch (Exception e) {
+                log.error("빈 생성에서 문제 발생: {}", e.getMessage());
+                throw new RuntimeException("빈 생성에서 문제 발생: " + e.getMessage(), e);
+            }
+        });
+    }
+
+    private void injectDependencies(final Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields()).forEach(field -> {
+            try {
+                field.setAccessible(true);
+                field.set(bean, getBean(field.getType()));
+            } catch (Exception e) {
+                log.warn("빈 의존성 주입에서 문제 발생: {}", e.getMessage());
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+            .filter(aClass::isInstance)
+            .findAny()
+            .orElseThrow(() -> new NoSuchElementException(aClass.getName() + " 클래스의 빈이 없음"));
     }
 }
--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,20 @@
 package di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
+    private static final List<Class<? extends Annotation>> ANNOTATIONS = List.of(
+        Inject.class, Service.class, Repository.class);
+
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        return ANNOTATIONS.stream()
+            .flatMap(annotation -> reflections.getTypesAnnotatedWith(annotation).stream())
+            .collect(Collectors.toSet());
     }
 }
