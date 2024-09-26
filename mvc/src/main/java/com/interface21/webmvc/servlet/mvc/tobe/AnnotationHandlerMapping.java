7ddff48d4b4f0d0ba808627a89e706cddf08f58d
Qtack <90441959+tackyu@users.noreply.github.com>
2024-09-26 19:58:40 +0900

    [MVC 구현하기 - 1단계] 폰드(권규택) 미션 제출합니다. (#712)
    * chore: reflection 학습 테스트 진행
    
    * chore: servlet 학습 테스트 진행
    
    * feat: HandlerExecution 구현
    
    * feat: AnnotationHandlerMapping 구현
    
    * refactor: render 책임 이동
    
    * refactor: Reflections 생성 반복문 제거
    
    * refactor: 불필요한 controller 인스턴스 생성 방지
    
    * test: 모든 HTTP 메서드를 지원받는 엔드포인트 테스트

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,15 +1,24 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    public static final int EMPTY = 0;
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -20,10 +29,55 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerClass : controllerClasses) {
+            initializeHandlerExecutions(controllerClass);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void initializeHandlerExecutions(Class<?> controllerClass) {
+        List<Method> requestMappingMethods = getRequestMappingMethods(controllerClass);
+        for (Method requestMappingMethod : requestMappingMethods) {
+            RequestMapping annotation = requestMappingMethod.getAnnotation(RequestMapping.class);
+            setHandlerExecutions(controllerClass, requestMappingMethod, annotation);
+        }
+    }
+
+    public List<Method> getRequestMappingMethods(Class<?> controllerClass) {
+        Method[] methods = controllerClass.getDeclaredMethods();
+        return Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void setHandlerExecutions(Class<?> controllerClass, Method method, RequestMapping annotation) {
+        String url = annotation.value();
+        RequestMethod[] requestMethods = annotation.method();
+        if (requestMethods.length == EMPTY) {
+            requestMethods = RequestMethod.values();
+        }
+
+        Object controller = getController(controllerClass);
+        HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private Object getController(Class<?> controllerClass) {
+        try {
+            return controllerClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMappingTest.java
@@ -1,13 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import com.interface21.web.bind.annotation.RequestMethod;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class AnnotationHandlerMappingTest {
 
@@ -42,6 +46,23 @@ class AnnotationHandlerMappingTest {
         when(request.getAttribute("id")).thenReturn("gugu");
         when(request.getRequestURI()).thenReturn("/post-test");
         when(request.getMethod()).thenReturn("POST");
+
+        final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
+        final var modelAndView = handlerExecution.handle(request, response);
+
+        assertThat(modelAndView.getObject("id")).isEqualTo("gugu");
+    }
+
+    @ParameterizedTest
+    @EnumSource(RequestMethod.class)
+    @DisplayName("@RequestMapping에 지정된 HTTP 메서드가 없으면 모든 메서드들을 지원한다.")
+    void allHttpMethod(RequestMethod requestMethod) throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/all-test");
+        when(request.getMethod()).thenReturn(requestMethod.name());
 
         final var handlerExecution = (HandlerExecution) handlerMapping.getHandler(request);
         final var modelAndView = handlerExecution.handle(request, response);
