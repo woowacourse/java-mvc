b2f75adf9195f78396682abce6bfe277e2a37f9c
HoeSeong123 <ghltjd3@gmail.com>
2024-09-26 13:40:55 +0900

    test: Adapter 테스트 코드 작성

---
+++ app/src/test/java/com/techcourse/ManualHandlerAdapterTest.java
@@ -0,0 +1,68 @@
+package com.techcourse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.controller.LoginController;
+import com.techcourse.domain.User;
+
+import samples.TestController;
+
+class ManualHandlerAdapterTest {
+
+    @Test
+    @DisplayName("컨트롤러 인터페이스 기반을 지원하는지 확인한다.")
+    void supoort() {
+        ManualHandlerAdapter sut = new ManualHandlerAdapter();
+        var controller = new LoginController();
+
+        assertThat(sut.support(controller)).isTrue();
+    }
+
+    @Test
+    @DisplayName("컨트롤러 인터페이스 기반이 아니면 지원하지 않는다.")
+    void notSupport() {
+        ManualHandlerAdapter sut = new ManualHandlerAdapter();
+        var controller = new TestController();
+
+        assertThat(sut.support(controller)).isFalse();
+    }
+
+    @Test
+    @DisplayName("request를 처리하여 ModelAndView를 반환한다.")
+    void handle() throws Exception {
+        // given
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+        final var session = mock(HttpSession.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("user")).thenReturn(new User(1, "gugu", "password", "hkkang@woowahan.com")); // 필요한 경우 사용자 정보 반환
+
+        ManualHandlerAdapter sut = new ManualHandlerAdapter();
+        var controller = new LoginController();
+
+        // when
+        var actual = sut.handle(request, response, controller);
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getView()).isInstanceOf(JspView.class)
+        );
+    }
+}
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerAdapterTest.java
@@ -0,0 +1,70 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
+
+import samples.TestController;
+
+class AnnotationHandlerAdapterTest {
+
+    @Test
+    @DisplayName("컨트롤러 어노테이션을 지원하는지 확인한다.")
+    void supoort() {
+        AnnotationHandlerAdapter sut = new AnnotationHandlerAdapter();
+        var controller = new TestController();
+
+        assertThat(sut.support(controller)).isTrue();
+    }
+
+    @Test
+    @DisplayName("컨트롤러 어노테이션이 아니면 지원하지 않는다.")
+    void notSupport() {
+        AnnotationHandlerAdapter sut = new AnnotationHandlerAdapter();
+        var controller = new Controller() {
+            @Override
+            public String execute(HttpServletRequest req, HttpServletResponse res) {
+                return "/test";
+            }
+        };
+
+        assertThat(sut.support(controller)).isFalse();
+    }
+
+    @Test
+    @DisplayName("request를 처리하여 ModelAndView를 반환한다.")
+    void handle() throws Exception {
+        // given
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/get-test");
+        when(request.getMethod()).thenReturn("GET");
+
+        AnnotationHandlerAdapter sut = new AnnotationHandlerAdapter();
+        var controller = new TestController();
+        var method = controller.getClass().getMethod("findUserId", HttpServletRequest.class, HttpServletResponse.class);
+        var handler = new HandlerExecution(controller, method);
+
+        // when
+        var actual = sut.handle(request, response, handler);
+
+        // then
+        assertAll(
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getView()).isInstanceOf(JspView.class)
+        );
+    }
+
+}
