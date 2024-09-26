18949a0ba915203b2e3e4f079bf4798b5334b5c7
le2sky <leehaneul990623@gmail.com>
2024-09-26 23:34:11 +0900

    feat: controller 인터페이스 기반 핸들러 매핑 구현

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerHandlerMappingTest.java
@@ -0,0 +1,52 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ControllerHandlerMappingTest {
+
+    private ControllerHandlerMapping handlerMapping;
+
+    @BeforeEach
+    void setUp() {
+        handlerMapping = new ControllerHandlerMapping("samples");
+        handlerMapping.initialize();
+    }
+
+    @Test
+    void get() throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/test/controller/interface/view");
+        when(request.getMethod()).thenReturn("GET");
+
+        final var controller = (Controller) handlerMapping.getHandler(request);
+        final var viewName = controller.execute(request, response);
+
+        assertThat(viewName).isEqualTo("controller based handler get");
+    }
+
+    @Test
+    void post() throws Exception {
+        final var request = mock(HttpServletRequest.class);
+        final var response = mock(HttpServletResponse.class);
+
+        when(request.getAttribute("id")).thenReturn("gugu");
+        when(request.getRequestURI()).thenReturn("/test/controller/interface");
+        when(request.getMethod()).thenReturn("POST");
+
+        final var controller = (Controller) handlerMapping.getHandler(request);
+        final var viewName = controller.execute(request, response);
+
+        assertThat(viewName).isEqualTo("controller based handler post");
+    }
+}
