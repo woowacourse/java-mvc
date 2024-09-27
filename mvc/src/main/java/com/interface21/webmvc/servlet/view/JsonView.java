526960d96be8e48d8b0f54c1868122cc82980901
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-09-27 09:49:50 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,8 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,8 +10,25 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        String json = getJson(model);
+        response.getWriter().write(json);
+    }
+
+    private String getJson(Map<String, ?> model) throws JsonProcessingException {
+        if (hasSingleSize(model)) {
+            Object singleValue = model.values().iterator().next();
+            return objectMapper.writeValueAsString(singleValue);
+        }
+        return objectMapper.writeValueAsString(model);
+    }
+
+    private boolean hasSingleSize(Map<String, ?> model) {
+        return model.size() == 1;
     }
 }
