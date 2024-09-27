716081b1ade93e9bb02243235d957272294dfcc7
ay-eonii <naayen@naver.com>
2024-09-27 14:31:47 +0900

    fix: 테스트 수정

--- app/src/test/java/com/samples/AnnotationController.java
+++ app/src/test/java/com/samples/AnnotationController.java
@@ -3,6 +3,8 @@ package com.samples;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -10,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AnnotationController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        return "/index.jsp";
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return new ModelAndView(new JspView("/index.jsp"));
     }
 }
--- app/src/test/java/com/techcourse/HandlerAdapterRegistryTest.java
+++ app/src/test/java/com/techcourse/HandlerAdapterRegistryTest.java
@@ -1,10 +1,11 @@
 package com.techcourse;
 
-import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.samples.AnnotationController;
+import com.techcourse.controller.LoginController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +13,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class HandlerAdapterRegistryTest {
 
@@ -25,17 +24,20 @@ class HandlerAdapterRegistryTest {
         handlerAdapterRegistry.addHandlerAdapter(new ManualHandlerAdapter());
     }
 
-    @DisplayName("URI에 해당하는 컨트롤러 기반 핸들러어댑터를 반환한다.")
+    @DisplayName("인터페이스 기반 핸들러에 해당하는 핸들러어댑터를 반환한다.")
     @Test
-    void getHandlerAdapter() throws NoSuchMethodException {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-
-        when(request.getMethod()).thenReturn(RequestMethod.GET.name());
-        when(request.getRequestURI()).thenReturn("/login");
-
-        HandlerExecution handlerExecution = new HandlerExecution(this.getClass(), this.getClass().getMethod("findUserId", HttpServletRequest.class, HttpServletResponse.class));
-        HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handlerExecution);
+    void getControllerHandlerAdapter() {
+        HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(new LoginController());
 
         assertThat(handlerAdapter).isInstanceOf(ManualHandlerAdapter.class);
+    }
+
+    @DisplayName("어노테이션 기반 핸들러에 해당하는 핸들러어댑터를 반환한다.")
+    @Test
+    void getHandlerExecutionHandlerAdapter() throws NoSuchMethodException {
+        HandlerExecution handlerExecution = new HandlerExecution(AnnotationController.class, AnnotationController.class.getMethod("execute", HttpServletRequest.class, HttpServletResponse.class));
+        HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handlerExecution);
+
+        assertThat(handlerAdapter).isInstanceOf(AnnotationHandlerAdapter.class);
     }
 }
