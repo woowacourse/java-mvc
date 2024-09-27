d8d314435939dfa73eacb4892b4ad441639b7dee
le2sky <leehaneul990623@gmail.com>
2024-09-27 16:40:25 +0900

    chore: 불필요한 todo 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/Controller.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/Controller.java
@@ -3,7 +3,6 @@ package com.interface21.webmvc.servlet.mvc.asis;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-// TODO : ModelAndView 어댑팅
 public interface Controller {
     String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception;
 }
