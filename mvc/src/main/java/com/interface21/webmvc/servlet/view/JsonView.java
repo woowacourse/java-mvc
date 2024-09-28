9d78748095d07474b35cc6a22676e0411824db28
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-09-28 16:21:24 +0900

    feat: JsonView 구현 및 관련 컨트롤러 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,55 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
+    private static final String JSON_CONTENT_TYPE = "Application/json";
+
+    private static final String EMPTY_STRING = "";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
+    static {
+        OBJECT_MAPPER.setVisibility(
+                OBJECT_MAPPER.getSerializationConfig()
+                        .getDefaultVisibilityChecker()
+                        .withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+    }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        String responseBody = createResponseBody(model);
+
+        response.setContentType(JSON_CONTENT_TYPE);
+        setResponseBody(response, responseBody);
+    }
+
+    private String createResponseBody(Map<String, ?> model) throws JsonProcessingException {
+        if (model.isEmpty()) {
+            return EMPTY_STRING;
+        }
+        if (model.size() == 1) {
+            Object object = model.values().iterator().next();
+            log.debug("object = {}", object);
+            return OBJECT_MAPPER.writeValueAsString(object);
+        }
+        return OBJECT_MAPPER.writeValueAsString(model);
+    }
+
+    private void setResponseBody(HttpServletResponse response, String body) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.write(body);
+        writer.flush();
     }
 }
