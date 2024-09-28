b8723759f62343e1a7fab32ce720b36605f7966d
libienz <leekh10195@gmail.com>
2024-09-28 18:45:58 +0900

    feat: HandlerAdapterRegistry 구현

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistry.java
@@ -0,0 +1,22 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class HandlerAdapterRegistry {
+
+    private final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public void addHandlerAdapter(HandlerAdapter handlerAdapter) {
+        handlerAdapters.add(handlerAdapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(Object handler) {
+        return handlerAdapters.stream()
+                .filter(handlerAdapter -> handlerAdapter.supports(handler))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("핸들러를 처리할 수 있는 Adapter를 찾지 못했습니다"));
+    }
+}
+
