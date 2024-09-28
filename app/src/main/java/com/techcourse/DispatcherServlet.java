2e65ddac8d7e2c2ba5a791a564cde297bf2a74ac
takoyakimchi <c4nyou@gmail.com>
2024-09-29 04:24:34 +0900

    refactor: handler를 찾지 못했을 때 404 반환하는 로직을 Optional로 개선

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,11 +1,11 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.mvc.asis.NotFoundController;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
 import com.interface21.webmvc.servlet.view.JspView;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +17,8 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    private static final String NOT_FOUND_FILE_PATH = "/404.jsp";
 
     private HandlerMappingRegistry handlerMappingRegistry;
     private HandlerAdapterRegistry handlerAdapterRegistry;
@@ -41,20 +43,35 @@ public class DispatcherServlet extends HttpServlet {
             final HttpServletResponse response) throws ServletException {
 
         try {
-            Object handler = handlerMappingRegistry.getHandler(request)
-                    .orElse(new NotFoundController());
-            HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
-            ModelAndView modelAndView = handlerAdapter.handle(handler, request, response);
-            render(modelAndView, request, response);
+            handlerMappingRegistry.getHandler(request)
+                    .ifPresentOrElse(
+                            handler -> handle(request, response, handler),
+                            () -> renderNotFoundView(request, response)
+                    );
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void render(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        JspView jspView = (JspView) modelAndView.getView();
-        jspView.render(modelAndView.getModel(), request, response);
+    private void handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        try {
+            HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
+            ModelAndView modelAndView = handlerAdapter.handle(handler, request, response);
+            JspView jspView = (JspView) modelAndView.getView();
+            jspView.render(modelAndView.getModel(), request, response);
+        } catch (Exception e) {
+            throw new UncheckedServletException(e);
+        }
+    }
+
+    private void renderNotFoundView(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(NOT_FOUND_FILE_PATH);
+            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            throw new UncheckedServletException(e);
+        }
     }
 }
