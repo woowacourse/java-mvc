894da3c150d12e0aacba6cc2b0913411a2a45e17
HyungUk Ryu <hw0603@naver.com>
2024-09-28 13:36:45 +0900

    test(ControllerScannerTest): 컨트롤러 클래스 인스턴스화 성공 테스트 추가

---
+++ mvc/src/test/java/samples/error/ErrorController.java
@@ -0,0 +1,12 @@
+package samples.error;
+
+import com.interface21.context.stereotype.Controller;
+
+@Controller
+
+@SuppressWarnings("unused")
+public class ErrorController {
+
+    private ErrorController() {
+    }
+}
