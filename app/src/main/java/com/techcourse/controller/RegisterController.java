b5b0e6a95a21ef96f9ad9ebf227c7208600d09f0
reddevilmidzy <midzy0228@gmail.com>
2024-09-29 00:39:54 +0900

    refactor: @Requestmapping 메서드의 리턴타입 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -6,14 +6,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 
 @Controller
 public class RegisterController {
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public String register(final HttpServletRequest request, final HttpServletResponse response) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView register(final HttpServletRequest request, final HttpServletResponse response) {
         final long id = InMemoryUserRepository.idCounter.incrementAndGet();
         final var user = new User(id,
                 request.getParameter("account"),
@@ -21,6 +24,7 @@ public class RegisterController {
                 request.getParameter("email"));
         InMemoryUserRepository.save(user);
 
-        return "redirect:/index.jsp";
+        final View view = new JspView("redirect:/index.jsp");
+        return new ModelAndView(view);
     }
 }
--- app/src/main/java/com/techcourse/controller/RegisterViewController.java
+++ app/src/main/java/com/techcourse/controller/RegisterViewController.java
@@ -6,12 +6,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.view.JspView;
 
 @Controller
 public class RegisterViewController {
 
     @RequestMapping(value = "/register/view", method = RequestMethod.GET)
-    public String getView(final HttpServletRequest request, final HttpServletResponse response) {
-        return "/register.jsp";
+    public ModelAndView getView(final HttpServletRequest request, final HttpServletResponse response) {
+        final View view = new JspView("/register.jsp");
+        return new ModelAndView(view);
     }
 }
