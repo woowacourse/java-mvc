ac7e5a59e8992b1f71356e536e6b9e7ea2efbffd
ashsty <qkralsdk991@naver.com>
2024-09-28 16:01:01 +0900

    refactor: RegisterController 구현 방식 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,10 +1,12 @@
 package com.techcourse;
 
-import com.techcourse.controller.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
+import com.techcourse.controller.LoginController;
+import com.techcourse.controller.LoginViewController;
+import com.techcourse.controller.LogoutController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,8 +22,6 @@ public class ManualHandlerMapping {
         controllers.put("/login", new LoginController());
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
