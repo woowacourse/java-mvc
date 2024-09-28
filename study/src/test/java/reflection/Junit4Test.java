11d6498517fef6c4c7c0b21167e3e2ece77840bc
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-28 23:56:55 +0900

    test: methodCallCount을 통한 test 자동화

--- study/src/test/java/reflection/Junit4Test.java
+++ study/src/test/java/reflection/Junit4Test.java
@@ -2,17 +2,30 @@ package reflection;
 
 public class Junit4Test {
 
+    private int methodCallCount;
+
+    public Junit4Test() {
+        methodCallCount = 0;
+    }
+
     @MyTest
-    public void one() throws Exception {
+    public void one() {
         System.out.println("Running Test1");
+        methodCallCount++;
     }
 
     @MyTest
-    public void two() throws Exception {
+    public void two() {
         System.out.println("Running Test2");
+        methodCallCount++;
     }
 
-    public void testThree() throws Exception {
+    public void testThree() {
         System.out.println("Running Test3");
+        methodCallCount++;
+    }
+
+    public int getMethodCallCount() {
+        return methodCallCount;
     }
 }
