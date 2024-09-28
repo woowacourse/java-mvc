b88810f85475dc4857136aab43a2debf5a9aba31
libienz <leekh10195@gmail.com>
2024-09-28 16:09:09 +0900

    feat: HandlerMapping 타입 인터페이스 정의 및 AnnotationHandlerMapping 클래스 타입 정의

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
@@ -24,7 +24,7 @@ public class AnnotationHandlerMapping {
         this.controllerScanner = controllerScanner;
     }
 
-
+    @Override
     public Object getHandler(final HttpServletRequest request) {
         String requestURI = request.getRequestURI();
         RequestMethod method = RequestMethod.valueOf(request.getMethod());
@@ -56,7 +56,7 @@ public class AnnotationHandlerMapping {
 
     private List<HandlerKey> createHandlerKeys(final Method method) {
         RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-        
+
         return Arrays.stream(getMappingMethods(requestMapping))
                 .map(requestMethod -> new HandlerKey(requestMapping.value(), requestMethod))
                 .toList();
