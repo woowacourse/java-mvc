d4be27fafb1a8ff0bbb2cea2d09dd2f25e613831
mzeong <mjeong@sookmyung.ac.kr>
2024-09-28 21:36:44 +0900

    feat: HandlerAdapter interface

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/HandlerExecutionHandlerAdapter.java
@@ -0,0 +1,20 @@
+package com.interface21.webmvc.servlet.mvc.tobe.adapter;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HandlerExecutionHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return ((HandlerExecution) handler).handle(request, response);
+    }
+}
