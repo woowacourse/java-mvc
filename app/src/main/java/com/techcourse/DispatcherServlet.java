43a314e285ac016f1041ebcf99576112e2ec99c3
KIMGYUTAE <kiimwf0819@gmail.com>
2024-09-29 01:53:26 +0900

    refactor: DispatcherServlet 내부 메서드 책임 분리

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -4,11 +4,14 @@ import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.tobe.adapter.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.adapter.HandlerAdapterRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.exception.HandlerAdapterNotFoundException;
+import com.interface21.webmvc.servlet.mvc.tobe.exception.HandlerMappingNotFoundException;
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.HandlerMappingRegistry;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,37 +31,40 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
 
         try {
-            Object handler = handlerMappingRegistry.getHandler(request);
-            if (handler == null) {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                log.info("No handler found for request");
-                return;
-            }
-
-            HandlerAdapter adapter = handlerAdapterRegistry.getHandlerAdapter(handler);
-            if (adapter == null) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                log.error("No adapter found for handler");
-                return;
-            }
-
-            ModelAndView modelAndView = adapter.handle(request, response, handler);
-            if (modelAndView == null || modelAndView.getView() == null) {
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                log.error("No modelAndView found for handler result");
-                return;
-            }
-
-            View view = modelAndView.getView();
-            view.render(modelAndView.getModel(), request, response);
-
+            handleRequest(request, response);
         } catch (Exception e) {
             log.error("Servlet Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage(), e);
         }
+    }
+
+    private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            Object handler = handlerMappingRegistry.getHandler(request);
+            HandlerAdapter adapter = handlerAdapterRegistry.getHandlerAdapter(handler);
+            ModelAndView modelAndView = adapter.handle(request, response, handler);
+            renderView(request, response, modelAndView);
+
+        } catch (HandlerMappingNotFoundException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        } catch (HandlerAdapterNotFoundException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void renderView(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView)
+            throws Exception {
+        if (modelAndView == null || modelAndView.getView() == null) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        View view = modelAndView.getView();
+        view.render(modelAndView.getModel(), request, response);
     }
 }
