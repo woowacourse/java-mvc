df91cf65ad1baa4bf54c880e7ddff872f1f6a6be
le2sky <leehaneul990623@gmail.com>
2024-09-28 19:02:05 +0900

    test: handler adapter 조회 테스트 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/HandlerAdaptersTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/HandlerAdaptersTest.java
@@ -1,12 +1,30 @@
 package com.interface21.webmvc.servlet;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HandlerAdaptersTest {
+
+    @Test
+    @DisplayName("주어진 핸들러의 처리를 위임할 수 있는 어댑터를 반환한다.")
+    void getHandler() {
+        Object handler = new Object();
+        HandlerAdapter handlerAdapter = mock(HandlerAdapter.class);
+        when(handlerAdapter.supports(any()))
+                .thenReturn(true);
+        HandlerAdapters handlerAdapters = new HandlerAdapters(handlerAdapter);
+
+        HandlerAdapter actual = handlerAdapters.getHandlerAdapter(handler);
+
+        assertThat(actual).isEqualTo(handlerAdapter);
+    }
 
     @Test
     @DisplayName("핸들러를 처리할 수 있는 어댑터가 존재하지 않는 경우 예외를 발생한다.")
