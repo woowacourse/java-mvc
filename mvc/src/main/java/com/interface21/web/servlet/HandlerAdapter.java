ac3605a23543c0439044560d71a95641bab38d93
ashsty <qkralsdk991@naver.com>
2024-09-28 18:41:01 +0900

    feat: HandlerAdapter 인터페이스 구현

---
+++ mvc/src/main/java/com/interface21/web/servlet/HandlerAdapter.java
@@ -0,0 +1,12 @@
+package com.interface21.web.servlet;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface HandlerAdapter {
+
+    ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
+
+    boolean supports(Object handler);
+}
