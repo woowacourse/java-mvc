68dff5ff6192f6765cb2b25203bee3dc9a23739e
Gyeongho Yang <chchch1213@naver.com>
2024-09-29 21:16:51 +0900

    feat: impl json view with jsonMapper

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,27 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final JsonMapper jsonMapper;
+
+    public JsonView() {
+        this.jsonMapper = new JsonMapper();
+    }
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String json = jsonMapper.writeValueAsString(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(json);
     }
 }
