850d19bd2c000f886022dd7a857bc6ac1b23891f
eun-byeol <joeunbyeol98@gmail.com>
2024-09-29 21:00:54 +0900

    refactor: base package 동적 생성하도록 수정

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -32,7 +32,7 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     public void init() {
         addHandlerMapping(new ManualHandlerMapping());
-        addHandlerMapping(new AnnotationHandlerMapping("com.techcourse.controller"));
+        addHandlerMapping(new AnnotationHandlerMapping(this.getClass().getPackage().getName()));
 
         addHandlerAdapter(new ControllerAdapter());
         addHandlerAdapter(new HandlerExecutionAdapter());
