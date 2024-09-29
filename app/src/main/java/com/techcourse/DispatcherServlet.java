f7572e1e3fa77a0e6467c017529ae13604d85818
H <99153215+hyeon0208@users.noreply.github.com>
2024-09-29 18:45:46 +0900

    [MVC 구현하기 - 2단계] 카키(서현준) 미션 제출합니다. (#761)
    * test: 매핑되는 핸들러가 없을 경우 예외 발생 테스트 추가
    
    * feat: 핸들러 매핑 인터페이스 추가
    
    * feat: 핸들러 매핑 인터페이스 구현체들을 관리하는 클래스 추가
    
    * refactor: CompositeHandlerMapping을 사용해 view를 반환하도록 수정
    
    * test: CompositeHandlerMapping 매핑 테스트 추가
    
    * refactor: AnnotationHandlerMapping 스캔 패키지 변경
    
    * refactor: CompositeHandlerMapping 핸들러 반환 로직 stream으로 개선
    
    * refactor: DispatcherServlet 핸들러 실행 로직 분리

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,28 +2,42 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.CompositeHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    public static final String DEFAULT_BASE_PACKAGE = "com.techcourse.controller";
+
+    private CompositeHandlerMapping compositeHandlerMapping;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(DEFAULT_BASE_PACKAGE);
+        annotationHandlerMapping.initialize();
+
+        this.compositeHandlerMapping = new CompositeHandlerMapping(
+                List.of(manualHandlerMapping, annotationHandlerMapping)
+        );
     }
 
     @Override
@@ -31,26 +45,33 @@ public class DispatcherServlet extends HttpServlet {
             final HttpServletRequest request,
             final HttpServletResponse response
     ) throws ServletException {
-        final String requestURI = request.getRequestURI();
-        log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
+        log.debug("Method : {}, Request URI : {}", request.getMethod(), request.getRequestURI());
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            resolveView(viewName, request, response);
-        } catch (Throwable e) {
-            log.error("Exception : {}", e.getMessage(), e);
-            throw new ServletException(e.getMessage());
+            Object handler = compositeHandlerMapping.getHandler(request);
+            ModelAndView modelAndView = executeHandler(handler, request, response);
+            resolveView(modelAndView, request, response);
+        } catch (Exception exception) {
+            log.error("Exception : {}", exception.getMessage(), exception);
+            throw new ServletException(exception);
         }
     }
 
+    private ModelAndView executeHandler(Object handler, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (handler instanceof Controller) {
+            String viewName = ((Controller) handler).execute(request, response);
+            return new ModelAndView(new JspView(viewName));
+        } else if (handler instanceof HandlerExecution) {
+            return ((HandlerExecution) handler).handle(request, response);
+        }
+        throw new IllegalArgumentException("실행할 핸들러가 존재하지 않습니다. : " + handler.getClass().getName());
+    }
+
     private void resolveView(
-            final String viewName,
+            final ModelAndView modelAndView,
             final HttpServletRequest request,
             final HttpServletResponse response
     ) throws Exception {
-        View view = new JspView(viewName);
-        ModelAndView modelAndView = new ModelAndView(view);
+        View view = modelAndView.getView();
         view.render(modelAndView.getModel(), request, response);
     }
 }
