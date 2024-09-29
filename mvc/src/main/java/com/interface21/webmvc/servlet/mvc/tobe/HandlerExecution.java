eaacf754021b1d40055073085e39aebd92e17c63
yoonjuho <dbswn990@naver.com>
2024-09-29 14:01:22 +0900

    refactor: Deprecated 메서드 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,10 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.core.util.ReflectionUtils;
 import com.interface21.webmvc.servlet.ModelAndView;
 
 public class HandlerExecution {
@@ -19,20 +17,6 @@ public class HandlerExecution {
     public HandlerExecution(Object instance, Method method) {
         this.instance = instance;
         this.method = method;
-    }
-
-    public HandlerExecution(Method method) {
-        this(createInstance(method), method);
-    }
-
-    private static Object createInstance(Method method) {
-        try {
-            return ReflectionUtils.accessibleConstructor(method.getDeclaringClass()).newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
-            log.error("Failed to create instance by method: {}", method.getName(), e);
-            throw new RuntimeException(e);
-        }
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
