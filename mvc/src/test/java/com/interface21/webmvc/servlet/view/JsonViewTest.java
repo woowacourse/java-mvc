088dbf2604067431acd6b222721b960052510124
linirini <2001yerin@naver.com>
2024-09-30 06:13:02 +0900

    refactor: 테스트 분리

--- mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -1,10 +1,12 @@
 package com.interface21.webmvc.servlet.view;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,20 +18,39 @@ import static org.mockito.Mockito.when;
 
 class JsonViewTest {
 
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private PrintWriter writer;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+    }
+
+    @DisplayName("JSON 응답 시 ContentType은 application/json;charset=UTF-8이다.")
     @Test
-    @DisplayName("JsonView가 모델을 JSON으로 직렬화하고 올바른 ContentType으로 응답한다.")
+    void contentTypeTest() throws Exception {
+        // given
+        JsonView jsonView = new JsonView();
+
+        // when
+        jsonView.render(new HashMap<>(), request, response);
+
+        // then
+        verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+    }
+
+    @Test
+    @DisplayName("Map 형태의 응답을 JSON 형태로 변환하여 응답한다..")
     void renderJson() throws Exception {
         // given
         JsonView jsonView = new JsonView();
         Map<String, Object> model = new HashMap<>();
         model.put("name", "John");
         model.put("age", 30);
-
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        PrintWriter writer = mock(PrintWriter.class);
-
-        when(response.getWriter()).thenReturn(writer);
 
         // when
         jsonView.render(model, request, response);
@@ -38,7 +59,6 @@ class JsonViewTest {
         ObjectMapper objectMapper = new ObjectMapper();
         String expectedJson = objectMapper.writeValueAsString(model);
 
-        verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         verify(writer).write(expectedJson);
     }
 }
