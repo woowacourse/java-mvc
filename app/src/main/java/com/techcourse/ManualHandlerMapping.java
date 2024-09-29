5b773eca5993a1435ac5921a12aa96d2e8b42e9f
SungKyum Kim <sungkyum1@naver.com>
2024-09-29 20:43:56 +0900

    refactor: 로그인 관련 로직을 어노테이션 기반 핸들러로 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -9,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
-import com.techcourse.controller.LoginController;
-import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
 import com.techcourse.controller.RegisterController;
 import com.techcourse.controller.RegisterViewController;
@@ -19,26 +17,24 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class ManualHandlerMapping implements HandlerMapping {
 
-	private static final Logger log = LoggerFactory.getLogger(ManualHandlerMapping.class);
+    private static final Logger log = LoggerFactory.getLogger(ManualHandlerMapping.class);
 
-	private static final Map<String, Controller> controllers = new HashMap<>();
+    private static final Map<String, Controller> controllers = new HashMap<>();
 
-	public void initialize() {
-		controllers.put("/", new ForwardController("/index.jsp"));
-		controllers.put("/login", new LoginController());
-		controllers.put("/login/view", new LoginViewController());
-		controllers.put("/logout", new LogoutController());
-		controllers.put("/register/view", new RegisterViewController());
-		controllers.put("/register", new RegisterController());
+    public void initialize() {
+        controllers.put("/", new ForwardController("/index.jsp"));
+        controllers.put("/logout", new LogoutController());
+        controllers.put("/register/view", new RegisterViewController());
+        controllers.put("/register", new RegisterController());
 
-		log.info("Initialized Handler Mapping!");
-		controllers.keySet()
-			.forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
-	}
+        log.info("Initialized Handler Mapping!");
+        controllers.keySet()
+            .forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
+    }
 
-	@Override
-	public Object getHandler(HttpServletRequest request) {
-		log.debug("Request Mapping Uri : {}", request.getRequestURI());
-		return controllers.get(request.getRequestURI());
-	}
+    @Override
+    public Object getHandler(HttpServletRequest request) {
+        log.debug("Request Mapping Uri : {}", request.getRequestURI());
+        return controllers.get(request.getRequestURI());
+    }
 }
--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,37 +1,51 @@
 package com.techcourse.controller;
 
-import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.domain.User;
+import com.techcourse.repository.InMemoryUserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(final HttpServletRequest request, final HttpServletResponse response) {
+        log.info("login controller get method");
+        if (UserSession.isLoggedIn(request.getSession())) {
+            return new ModelAndView(new JspView("redirect:/index.jsp"));
         }
-
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
-                .map(user -> {
-                    log.info("User : {}", user);
-                    return login(req, user);
-                })
-                .orElse("redirect:/401.jsp");
+        return new ModelAndView(new JspView("/login.jsp"));
     }
 
-    private String login(final HttpServletRequest request, final User user) {
-        if (user.checkPassword(request.getParameter("password"))) {
-            final var session = request.getSession();
-            session.setAttribute(UserSession.SESSION_KEY, user);
-            return "redirect:/index.jsp";
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(final HttpServletRequest request, final HttpServletResponse response) {
+        log.info("login controller post method");
+        final String account = request.getParameter("account");
+        final String password = request.getParameter("password");
+
+        return InMemoryUserRepository.findByAccount(account)
+            .map(user -> handleLogin(request, user, password))
+            .orElse(new ModelAndView(new JspView("redirect:/401.jsp")));
+    }
+
+    private ModelAndView handleLogin(final HttpServletRequest request, final User user, final String password) {
+        if (user.checkPassword(password)) {
+            request.getSession().setAttribute(UserSession.SESSION_KEY, user);
+            log.info("User logged in: {}", user);
+            return new ModelAndView(new JspView("redirect:/index.jsp"));
         }
-        return "redirect:/401.jsp";
+        log.info("Invalid login attempt for user: {}", user);
+        return new ModelAndView(new JspView("redirect:/401.jsp"));
     }
 }
