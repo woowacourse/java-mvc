f6f39ec17598cef0d4b459929bcb154bdcd38d3d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-29 16:38:38 +0900

    feat(HandlerAdapter, HandlerMapping): 파일 코드 블럭 내의 마지막 줄 개행 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerAdapter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerAdapter.java
@@ -10,5 +10,4 @@ public interface HandlerAdapter {
     boolean supports(final Object handler);
 
     ModelAndView handle(final Object handler, final HttpServletRequest request, final HttpServletResponse response);
-
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerMapping.java
@@ -7,5 +7,4 @@ public interface HandlerMapping {
     boolean hasHandler(HttpServletRequest request);
 
     Object getHandler(HttpServletRequest request);
-
 }
