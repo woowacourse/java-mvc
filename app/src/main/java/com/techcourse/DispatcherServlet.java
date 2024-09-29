7f7bcc9c1b0eb54f40d83c556c5b4fff2726b16c
hyeonjilee <skylar12200@gmail.com>
2024-09-29 16:45:22 +0900

    refactor: HandlerAdapter 인터페이스 적용

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,17 +2,13 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
-import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.slf4j.Logger;
@@ -24,15 +20,19 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private List<HandlerMapping> handlerMappings;
+    private List<HandlerAdapter> handlerAdapters;
 
     public DispatcherServlet() {
         handlerMappings = new ArrayList<>();
+        handlerAdapters = new ArrayList<>();
     }
 
     @Override
     public void init() {
         handlerMappings.add(new ManualHandlerMapping());
         handlerMappings.add(new AnnotationHandlerMapping("com"));
+        handlerAdapters.add(new ManualHandlerAdapter());
+        handlerAdapters.add(new AnnotationHandlerAdapter());
     }
 
     @Override
@@ -43,18 +43,10 @@ public class DispatcherServlet extends HttpServlet {
 
         try {
             Object handler = getHandler(request);
-
-            if (handler instanceof Controller controller) {
-                String viewName = controller.execute(request, response); // model을 request에 담음
-                JspView jspView = new JspView(viewName);
-                jspView.render(new HashMap<>(), request, response); // req에 있는 model 뽑아서 view에 넣음
-            }
-            if (handler instanceof HandlerExecution handlerExecution) {
-                ModelAndView modelAndView = handlerExecution.handle(request, response);
-                View view = modelAndView.getView();
-                view.render(modelAndView.getModel(), request, response);
-            }
-
+            HandlerAdapter handlerAdapter = getHandlerAdapter(handler);
+            ModelAndView modelAndView = handlerAdapter.handle(handler, request, response);
+            View view = modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
@@ -66,6 +58,15 @@ public class DispatcherServlet extends HttpServlet {
             Object handler = mapping.getHandler(request);
             if (handler != null) {
                 return handler;
+            }
+        }
+        throw new NoSuchElementException("Resource not found");
+    }
+
+    private HandlerAdapter getHandlerAdapter(Object handler) {
+        for (HandlerAdapter handlerAdapter : handlerAdapters) {
+            if (handlerAdapter.support(handler)) {
+                return handlerAdapter;
             }
         }
         throw new NoSuchElementException("Resource not found");
