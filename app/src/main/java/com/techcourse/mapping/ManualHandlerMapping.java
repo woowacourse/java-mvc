12441276e0086e21226c574d1a2281a0b6783b90
jinwoo22 <jenny010925@naver.com>
2024-09-30 01:19:55 +0900

    feat: RegisterController 어노테이션 기반 컨트롤러로 변경

--- app/src/main/java/com/techcourse/mapping/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/mapping/ManualHandlerMapping.java
@@ -6,7 +6,6 @@ import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import com.techcourse.controller.LoginController;
 import com.techcourse.controller.LoginViewController;
 import com.techcourse.controller.LogoutController;
-import com.techcourse.controller.RegisterController;
 import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
@@ -26,7 +25,6 @@ public class ManualHandlerMapping implements HandlerMapping {
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
         controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
