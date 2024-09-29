5d6e1d83f87a9ff38a781fcfe90942c4b8604b71
nhlee98 <e5slnh9876@gmail.com>
2024-09-29 21:27:08 +0900

    refactor: DispatcherServlet 패키지 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
@@ -1,4 +1,4 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet;
 
 import com.interface21.webmvc.servlet.mvc.handler.adapter.HandlerAdapters;
 import com.interface21.webmvc.servlet.mvc.handler.mapping.HandlerMappings;
