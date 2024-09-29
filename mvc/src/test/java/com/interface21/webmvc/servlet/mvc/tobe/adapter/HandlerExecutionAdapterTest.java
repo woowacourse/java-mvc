60d59238059e344a14e866cf0b1b872cf1b21787
eun-byeol <joeunbyeol98@gmail.com>
2024-09-29 22:30:17 +0900

    test: HandlerExecutionAdapterTest 패키지 이동 및 mock 사용

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/HandlerExecutionAdapterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/HandlerExecutionAdapterTest.java
@@ -1,29 +1,28 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet.mvc.tobe.adapter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
-import com.interface21.webmvc.servlet.mvc.tobe.adapter.HandlerExecutionAdapter;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.RegisterViewController;
 import org.junit.jupiter.api.Test;
 
 class HandlerExecutionAdapterTest {
 
-    private final HandlerExecutionAdapter handlerExecutionAdapter = new HandlerExecutionAdapter();
-
     @Test
     void HandlerExcution_타입_핸들러이면_true를_반환한다() {
-        HandlerExecution supportedHandler = new HandlerExecution(new RegisterController(),
-                RegisterController.class.getMethods()[0]);
+        HandlerExecutionAdapter handlerExecutionAdapter = new HandlerExecutionAdapter();
+
+        HandlerExecution supportedHandler = mock(HandlerExecution.class);
 
         assertThat(handlerExecutionAdapter.supports(supportedHandler)).isTrue();
     }
 
     @Test
     void HandlerExcution_타입_핸들러가_아니면_false를_반환한다() {
-        Controller unsupportedHandler = new RegisterViewController();
+        HandlerExecutionAdapter handlerExecutionAdapter = new HandlerExecutionAdapter();
+
+        Controller unsupportedHandler = mock(Controller.class);
 
         assertThat(handlerExecutionAdapter.supports(unsupportedHandler)).isFalse();
     }
