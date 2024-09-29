290d74bfaacd4c9802fc3e09b306f68198f00bfd
Eugene Jang <kyunellroll@gmail.com>
2024-09-29 22:41:13 +0900

    modify(ControllerRegistry): InstanceMaker를 사용하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/registry/ControllerRegistry.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/registry/ControllerRegistry.java
@@ -1,10 +1,9 @@
 package com.interface21.webmvc.servlet.mvc.tobe.registry;
 
+import com.interface21.webmvc.servlet.mvc.tobe.InstanceMaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,30 +19,10 @@ public class ControllerRegistry {
         if (controllers.containsKey(controller)) {
             return controllers.get(controller);
         }
-        Object controllerInstance = makeControllerInstance(controller);
+        Object controllerInstance = InstanceMaker.makeInstance(controller);
         controllers.put(controller, controllerInstance);
 
+        log.info("Controller Registered Success");
         return controllerInstance;
-    }
-
-    private static Object makeControllerInstance(Class<?> controller) {
-        try {
-            Constructor<?> constructor = controller.getConstructor();
-            Object controllerInstance = constructor.newInstance();
-
-            return controllerInstance;
-        } catch (NoSuchMethodException e) {
-            log.error("메서드를 찾지 못했습니다. {}", e.getStackTrace()[0]);
-            throw new IllegalArgumentException("메서드를 찾지 못했습니다.");
-        } catch (IllegalAccessException e) {
-            log.error("생성자에 접근할 수 없습니다. {}", e.getStackTrace()[0]);
-            throw new IllegalStateException("생성자에 접근할 수 없습니다.");
-        } catch (ExceptionInInitializerError e) {
-            log.error("메서드를 초기화 하던 도중 실패했습니다. {}", e.getStackTrace()[0]);
-            throw new IllegalStateException("메서드를 초기화 하던 도중 실패했습니다.");
-        } catch (InvocationTargetException | InstantiationException e) {
-            log.error("생성자를 실행하던 도중 실패했습니다. {}", e.getStackTrace()[0]);
-            throw new IllegalStateException("생성자를 실행하던 도중 실패했습니다.");
-        }
     }
 }
