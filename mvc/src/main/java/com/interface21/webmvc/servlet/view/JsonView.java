26165441c2d435fb77c493271db823bd59cdb7c1
tkdgur0906 <tkdgur0906@naver.com>
2024-09-29 23:00:42 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,8 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,7 +11,22 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonView() {
+    }
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String json = serialize(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(json);
+    }
+
+    private String serialize(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values().iterator().next());
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -0,0 +1,66 @@
+package com.interface21.webmvc.servlet.view;
+
+import com.interface21.web.http.MediaType;
+import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class JsonViewTest {
+
+    @DisplayName("Map에 저장된 데이터를 json으로 직렬화 성공")
+    @Test
+    void serialize() throws Exception {
+        //given
+        View jsonView = new JsonView();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("name", "capy");
+        model.put("age", 26);
+
+        //when
+        jsonView.render(model, request, response);
+        printWriter.flush();
+        String jsonOutput = stringWriter.toString();
+
+        //then
+        String expectedJson = "{\"name\":\"capy\",\"age\":26}";
+        assertThat(jsonOutput).isEqualTo(expectedJson);
+    }
+
+    @DisplayName("직렬화 시 response의 contentType을 APPLICATION_JSON_UTF8_VALUE으로 설정 성공")
+    @Test
+    void serialize_contentType() throws Exception {
+        //given
+        View jsonView = new JsonView();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+
+        Map<String, Object> model = new HashMap<>();
+
+        //when
+        jsonView.render(model, request, response);
+
+        //then
+        verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+    }
+}
