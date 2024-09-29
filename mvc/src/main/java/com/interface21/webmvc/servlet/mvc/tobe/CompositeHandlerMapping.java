0bdcf49ff061029f120bd25c7b660a848434aecb
H <dmsgktn0208@gmail.com>
2024-09-29 16:29:45 +0900

    refactor: CompositeHandlerMapping 핸들러 반환 로직 stream으로 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/CompositeHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/CompositeHandlerMapping.java
@@ -2,6 +2,7 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Objects;
 
 public class CompositeHandlerMapping implements HandlerMapping {
 
@@ -13,13 +14,13 @@ public class CompositeHandlerMapping implements HandlerMapping {
 
     @Override
     public Object getHandler(HttpServletRequest request) {
-        for (HandlerMapping handlerMapping : handlerMappings) {
-            Object handler = handlerMapping.getHandler(request);
-            if (handler != null) {
-                return handler;
-            }
-        }
-        String invalidRequestInfo = String.join(" ", request.getMethod(), request.getRequestURI());
-        throw new IllegalArgumentException("해당 요청을 처리할 수 있는 핸들러가 존재하지 않습니다. : " + invalidRequestInfo);
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(() -> {
+                    String invalidRequestInfo = String.join(" ", request.getMethod(), request.getRequestURI());
+                    return new IllegalArgumentException("해당 요청을 처리할 수 있는 핸들러가 존재하지 않습니다. : " + invalidRequestInfo);
+                });
     }
 }
