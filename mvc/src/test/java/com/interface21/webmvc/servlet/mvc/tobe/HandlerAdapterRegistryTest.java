7ba22b46114ffdeba75a34926af748498a893ee3
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-09-29 21:58:51 +0900

    feat(HandlerAdapterRegistryTest): 테스트 케이스 보충

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 
 class HandlerAdapterRegistryTest {
 
-    private final HandlerAdapter testHandlerAdapter = new HandlerAdapter() {
+    private final HandlerAdapter testHandlerAdapter1 = new HandlerAdapter() {
         @Override
         public boolean support(Object handler) {
             return true;
@@ -27,7 +27,22 @@ class HandlerAdapterRegistryTest {
         public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
                 throws Exception {
             ModelAndView modelAndView = mock(ModelAndView.class);
-            when(modelAndView.getObject("test")).thenReturn("test");
+            when(modelAndView.getObject("test")).thenReturn("test1");
+            return modelAndView;
+        }
+    };
+
+    private final HandlerAdapter testHandlerAdapter2 = new HandlerAdapter() {
+        @Override
+        public boolean support(Object handler) {
+            return true;
+        }
+
+        @Override
+        public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+                throws Exception {
+            ModelAndView modelAndView = mock(ModelAndView.class);
+            when(modelAndView.getObject("test")).thenReturn("test2");
             return modelAndView;
         }
     };
@@ -37,16 +52,17 @@ class HandlerAdapterRegistryTest {
     void getHandler() throws Exception {
         // given
         HandlerAdapterRegistry registry = new HandlerAdapterRegistry();
-        registry.addHandlerAdapter(testHandlerAdapter);
+        registry.addHandlerAdapter(testHandlerAdapter1);
+        registry.addHandlerAdapter(testHandlerAdapter2);
 
         // when
-        HandlerAdapter handlerAdapter = registry.getHandlerAdapter(testHandlerAdapter);
+        HandlerAdapter handlerAdapter = registry.getHandlerAdapter(testHandlerAdapter1);
 
         //then
         assertThat(
                 handlerAdapter.handle(null, null, null)
                         .getObject("test"))
-                .isEqualTo("test");
+                .isEqualTo("test1");
     }
 
     @Test
@@ -56,7 +72,7 @@ class HandlerAdapterRegistryTest {
         HandlerAdapterRegistry registry = new HandlerAdapterRegistry();
 
         // when & then
-        assertThatThrownBy(() -> registry.getHandlerAdapter(testHandlerAdapter))
+        assertThatThrownBy(() -> registry.getHandlerAdapter(testHandlerAdapter1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
