eb21879b89ba56c7e93361e471329a4c5e4d0dc3
SungKyum Kim <sungkyum1@naver.com>
2024-09-29 23:04:04 +0900

    refactor: 예외 발생 메소드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
@@ -9,28 +9,33 @@ import org.reflections.Reflections;
 import com.interface21.context.stereotype.Controller;
 
 public class ControllerScanner {
-	private final Reflections reflections;
+    private final Reflections reflections;
 
-	public ControllerScanner(Object... basePackage) {
-		this.reflections = new Reflections(basePackage);
-	}
+    public ControllerScanner(Object... basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
 
-	public Map<Class<?>, Object> getControllers() {
-		Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
 
-		return instantiateControllers(controllerClasses);
-	}
+        return instantiateControllers(controllerClasses);
+    }
 
-	private static Map<Class<?>, Object> instantiateControllers(Set<Class<?>> controllerClasses) {
-		Map<Class<?>, Object> controllers = new HashMap<>();
-		for (Class<?> controllerClass : controllerClasses) {
-			try {
-				Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
-				controllers.put(controllerClass, controllerInstance);
-			} catch (Exception e) {
-				throw new RuntimeException("Failed to instantiate controller: " + controllerClass.getName(), e);
-			}
-		}
-		return controllers;
-	}
+    private static Map<Class<?>, Object> instantiateControllers(Set<Class<?>> controllerClasses) {
+        Map<Class<?>, Object> controllers = new HashMap<>();
+        for (Class<?> controllerClass : controllerClasses) {
+            mapControllers(controllerClass, controllers);
+        }
+        return controllers;
+    }
+
+    private static void mapControllers(Class<?> controllerClass, Map<Class<?>, Object> controllers) {
+        try {
+            Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+            controllers.put(controllerClass, controllerInstance);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate controller: " + controllerClass.getName(), e);
+        }
+    }
 }
