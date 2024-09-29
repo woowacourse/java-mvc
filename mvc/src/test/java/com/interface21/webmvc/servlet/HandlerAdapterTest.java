4dc4e7f9b0892917b47d4d59998b0870042b5ccb
hangillee <skfcb10@naver.com>
2024-09-29 23:40:22 +0900

    refactor: 불필요한 Legacy MVC 코드 제거

--- mvc/src/test/java/com/interface21/webmvc/servlet/HandlerAdapterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/HandlerAdapterTest.java
@@ -1,39 +1,29 @@
 package com.interface21.webmvc.servlet;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.interface21.webmvc.servlet.mvc.annotation.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.annotation.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import samples.TestSimpleController;
 
 class HandlerAdapterTest {
 
-    private Map<String, Controller> controllers;
     private List<HandlerAdapter> adapters;
     private AnnotationHandlerMapping handlerMapping;
 
     @BeforeEach
     void setAdapters() {
-        controllers = new HashMap<>();
-        controllers.put("/test", new TestSimpleController());
-
         handlerMapping = new AnnotationHandlerMapping("samples");
         handlerMapping.initialize();
 
         adapters = new ArrayList<>();
-        adapters.add(new SimpleControllerHandlerAdapter());
         adapters.add(new AnnotationHandlerAdapter());
     }
 
@@ -42,18 +32,14 @@ class HandlerAdapterTest {
     void supports() {
         // given
         final var request = mock(HttpServletRequest.class);
-        
+
         when(request.getRequestURI()).thenReturn("/get-test");
         when(request.getMethod()).thenReturn("GET");
 
         // when
-        final var simpleController = controllers.get("/test");
         final var annotatedController = handlerMapping.getHandler(request);
 
         // then
-        assertAll(
-                () -> assertTrue(adapters.getFirst().supports(simpleController)),
-                () -> assertTrue(adapters.getLast().supports(annotatedController))
-        );
+        assertTrue(adapters.getLast().supports(annotatedController));
     }
 }
