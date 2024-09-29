58f37e07b6671f2fdb20944d8ec3ea2e0b4b1898
yoonjuho <dbswn990@naver.com>
2024-09-29 14:52:53 +0900

    refactor: 뷰 이름으로 ModelAndView 객체 생성

--- app/src/main/java/com/techcourse/ManualHandlerAdapter.java
+++ app/src/main/java/com/techcourse/ManualHandlerAdapter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
-import com.interface21.webmvc.servlet.view.JspView;
 
 public class ManualHandlerAdapter implements HandlerAdapter {
 
@@ -17,7 +16,6 @@ public class ManualHandlerAdapter implements HandlerAdapter {
     @Override
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String viewName = ((Controller) handler).execute(request, response);
-        JspView jspView = new JspView(viewName);
-        return new ModelAndView(jspView);
+        return new ModelAndView(viewName);
     }
 }
