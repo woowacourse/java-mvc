52d9e540869c22cea1d361c70f61514aaeaf3c45
Gyeongho Yang <chchch1213@naver.com>
2024-09-29 22:29:07 +0900

    refactor: combine exception on test

--- app/src/test/java/com/techcourse/controller/HomeControllerTest.java
+++ app/src/test/java/com/techcourse/controller/HomeControllerTest.java
@@ -3,7 +3,6 @@ package com.techcourse.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.techcourse.TomcatStarter;
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -33,7 +32,7 @@ class HomeControllerTest {
 
     @Test
     @DisplayName("홈 화면을 요청시 대시보드 페이지를 응답한다.")
-    void getHome() throws IOException, InterruptedException {
+    void getHome() throws Exception {
         // given
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
