82d76fce4a1547dc41f6bafc8ae11253c82ffb6b
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-09-29 14:39:08 +0900

    feat(HandlerMappingRegistry): handler를 관리하는 일급컬레션 생성

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerMappingRegistry.java
@@ -0,0 +1,27 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HandlerMappingRegistry {
+
+    private final List<HandlerMapping> handlerMappings;
+
+    public HandlerMappingRegistry() {
+        this.handlerMappings = new ArrayList<>();
+    }
+
+    public void add(HandlerMapping handlerMapping) {
+        handlerMappings.add(handlerMapping);
+    }
+
+    public Object findHandler(HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("요청에 알맞는 Handler를 찾을 수 없습니다."));
+    }
+}
