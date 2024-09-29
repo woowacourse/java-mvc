20434a2adcf062a52e63f06f1f3d2fff8a5e507c
Eugene Jang <kyunellroll@gmail.com>
2024-09-29 22:31:12 +0900

    feat(RootPathStrategy): RootPath를 가져오는 객체 생성
    - 루트 패스를 찾는 기본 위치는 FrameApplication 어노테이션 생성
    - 기본 Application 어노테이션이 없는 경우 에러를 발생시키는지 확인하는 테스트 작성

---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/pathfinder/TestRootPathStrategy.java
@@ -0,0 +1,9 @@
+package com.interface21.webmvc.servlet.mvc.tobe.pathfinder;
+
+public class TestRootPathStrategy implements RootPathStrategy {
+
+    @Override
+    public String search() {
+        return "samples";
+    }
+}
