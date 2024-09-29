4b235fbf5f7503b6877dea5bcc40f8ad117e2032
masonkimseoul <masonkimseoul@gmail.com>
2024-09-29 17:03:50 +0900

    refactor: DispatcherServlet에서 HandlerMappingRegistry, HandlerAdapterRegistry를 사용하도록 수정

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,15 +1,15 @@
 package com.techcourse;
 
-import ch.qos.logback.core.model.Model;
 import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMappingAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMappingRegistry;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.interface21.webmvc.servlet.view.JspView;
@@ -18,16 +18,20 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final String BASE_PACKAGE = "com.techcourse.controller";
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final HandlerMappingRegistry handlerMappingRegistry;
+    private final HandlerAdapterRegistry handlerAdapterRegistry;
 
     public DispatcherServlet() {
+        this.handlerMappingRegistry = new HandlerMappingRegistry();
+        this.handlerAdapterRegistry = new HandlerAdapterRegistry();
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        setHandlerMappingRegistry();
+        setHandlerAdapterRegistry();
     }
 
     @Override
@@ -36,15 +40,31 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = (Controller) manualHandlerMapping.getHandler(request);
-            final var viewName = controller.execute(request, response);
+            Object handler = handlerMappingRegistry.getHandlerMapping(request).get();
+            HandlerAdapter adapter = handlerAdapterRegistry.getHandlerAdapter(handler);
+            ModelAndView modelAndView = adapter.handle(handler, request, response);
 
-            Map<String, Object> model = new HashMap<>();
-            JspView jspView = new JspView(viewName);
-            jspView.render(model, request, response);
+            JspView view = (JspView) modelAndView.getView();
+            view.render(modelAndView.getModel(), request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
+    }
+
+    private void setHandlerMappingRegistry() {
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
+        manualHandlerMapping.initialize();
+
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping(BASE_PACKAGE);
+        annotationHandlerMapping.initialize();
+
+        handlerMappingRegistry.addHandlerMapping(manualHandlerMapping);
+        handlerMappingRegistry.addHandlerMapping(annotationHandlerMapping);
+    }
+
+    private void setHandlerAdapterRegistry() {
+        handlerAdapterRegistry.addHandlerAdapter(new ManualHandlerMappingAdapter());
+        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerMappingAdapter());
     }
 }
