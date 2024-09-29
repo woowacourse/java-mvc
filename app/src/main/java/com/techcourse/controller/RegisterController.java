ecf1a84c59facfadb0737c036e3580615f7a3999
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-09-29 22:31:50 +0900

    refactor: 모든 컨트롤러를 어노테이션 기반 MVC로 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -14,20 +14,18 @@ import jakarta.servlet.http.HttpServletResponse;
 public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView save(final HttpServletRequest request, final HttpServletResponse response) {
         User user = new User(2,
                 request.getParameter("account"),
                 request.getParameter("password"),
                 request.getParameter("email")
         );
         InMemoryUserRepository.save(user);
-        JspView jspView = new JspView("redirect:/index.jsp");
-        return new ModelAndView(jspView);
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
-        JspView jspView = new JspView("/register.jsp");
-        return new ModelAndView(jspView);
+    public ModelAndView show(final HttpServletRequest request, final HttpServletResponse response) {
+        return new ModelAndView(new JspView("/register.jsp"));
     }
 }
