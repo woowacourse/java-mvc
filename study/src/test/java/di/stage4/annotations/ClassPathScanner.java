b7a072931e99628efe1711815651f5a1b646d28e
tkdgur0906 <tkdgur0906@naver.com>
2024-09-30 17:17:52 +0900

    test: DI 학습 테스트 4단계

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,23 @@
 package di.stage4.annotations;
 
+import org.reflections.Reflections;
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> classes = new HashSet<>();
+
+        List<Class<? extends Annotation>> annotations = List.of(Service.class, Repository.class);
+
+        for (Class<? extends Annotation> annotation : annotations) {
+            classes.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+
+        return classes;
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,15 +14,64 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        final Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            try {
+                Constructor<?> constructor = aClass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                beans.add(constructor.newInstance());
+            } catch (NoSuchMethodException |
+                     InvocationTargetException |
+                     InstantiationException |
+                     IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return beans;
     }
 
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
+    // 각 필드에 빈을 대입(assign)한다.
+    private void setFields(final Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            setField(bean, field);
+        }
+    }
+
+    private void setField(Object bean, Field field) {
+        for (Object injectBean : beans) {
+            if (field.getType().isAssignableFrom(injectBean.getClass())
+                    && field.isAnnotationPresent(Inject.class)) {
+                try {
+                    field.setAccessible(true);
+                    field.set(bean, injectBean);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    // ClassPathScanner 클래스를 활용해보자.
+    public static DIContainer createContextForPackage(final String rootPackageName) {
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
+    }
+
+    // 빈 컨텍스트(DI)에서 관리하는 빈을 찾아서 반환한다.
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
+        for (Object bean : beans) {
+            if (aClass.isAssignableFrom(bean.getClass())) {
+                return (T) bean;
+            }
+        }
         return null;
     }
 }
