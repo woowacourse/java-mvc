ad8077feb00ff2635aa993b6c26b6089aa3e2a33
BurningFalls <sherlocky31415@gmail.com>
2024-09-30 15:26:43 +0900

    refactor: 테스트 설명 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.HashMap;
 import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -51,7 +50,7 @@ public class JsonViewTest {
         verify(response).setCharacterEncoding("UTF-8");
     }
 
-    @DisplayName("")
+    @DisplayName("모델에 데이터가 1개면 값을 그대로 반환한다.")
     @Test
     void test_Render_When_ModelSize_One() throws Exception {
         // given
@@ -67,7 +66,7 @@ public class JsonViewTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("")
+    @DisplayName("모델에 데이터가 2개 이상이면 Map 형태 그대로 JSON으로 변환해서 반환한다.")
     @Test
     void test_Render_When_ModelSize_Over_Two() throws Exception {
         // given
