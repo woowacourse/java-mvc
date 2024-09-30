4b9c2da2a657ee96e7bb78d1a936411800109a27
libienz <leekh10195@gmail.com>
2024-09-30 13:19:43 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,25 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (model.size() == 1) {
+            Object value = model.values().iterator().next();
+            objectMapper.writeValue(response.getWriter(), value);
+            return;
+        }
+        String body = objectMapper.writeValueAsString(model);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
     }
 }
