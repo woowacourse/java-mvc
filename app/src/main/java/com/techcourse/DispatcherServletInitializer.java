69007767de5f99e685e43df2e5a53663034c4c43
nhlee98 <e5slnh9876@gmail.com>
2024-09-30 17:22:18 +0900

    refactor: app 모듈에서 DispatcherServlet에 대한 구현 내용을 모르도록 팩토리 클래스로 분리

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -2,14 +2,8 @@ package com.techcourse;
 
 import com.interface21.web.WebApplicationInitializer;
 import com.interface21.webmvc.servlet.DispatcherServlet;
-import com.interface21.webmvc.servlet.mvc.handler.AnnotationHandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.handler.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.handler.adapter.HandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.handler.adapter.HandlerAdapters;
-import com.interface21.webmvc.servlet.mvc.handler.mapping.HandlerMapping;
-import com.interface21.webmvc.servlet.mvc.handler.mapping.HandlerMappings;
+import com.interface21.webmvc.servlet.DispatcherServletFactory;
 import jakarta.servlet.ServletContext;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,19 +16,10 @@ public class DispatcherServletInitializer implements WebApplicationInitializer {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServletInitializer.class);
 
     private static final String DEFAULT_SERVLET_NAME = "dispatcher";
-    private static final List<HandlerMapping> DEFAULT_MAPPINGS = List.of(
-            new AnnotationHandlerMapping("com.techcourse")
-    );
-    private static final List<HandlerAdapter> DEFAULT_ADAPTERS = List.of(
-            new AnnotationHandlerAdapter()
-    );
 
     @Override
     public void onStartup(final ServletContext servletContext) {
-        HandlerMappings handlerMappings = new HandlerMappings(DEFAULT_MAPPINGS);
-        HandlerAdapters handlerAdapters = new HandlerAdapters(DEFAULT_ADAPTERS);
-
-        final var dispatcherServlet = new DispatcherServlet(handlerMappings, handlerAdapters);
+        final var dispatcherServlet = DispatcherServletFactory.createWithBasePackages(Application.class);
 
         final var registration = servletContext.addServlet(DEFAULT_SERVLET_NAME, dispatcherServlet);
         if (registration == null) {
