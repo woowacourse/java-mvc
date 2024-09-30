4003a380014955e89f9c31692103db87b8c07191
libienz <leekh10195@gmail.com>
2024-09-30 13:34:01 +0900

    refactor: LoginController 어노테이션 기반 매핑으로 개선

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,37 +1,49 @@
 package com.techcourse.controller;
 
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@com.interface21.context.stereotype.Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+    private static final String LOGIN_SUCCESS_REDIRECT = "redirect:/index.jsp";
+    private static final String LOGIN_FAILED_REDIRECT = "redirect:/401.jsp";
+    private static final String ACCOUNT = "account";
+    private static final String PASSWORD = "password";
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView processLogin(final HttpServletRequest req, final HttpServletResponse resp) throws Exception {
         if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+            JspView view = new JspView(LOGIN_SUCCESS_REDIRECT);
+            return new ModelAndView(view);
         }
 
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+        String viewName = InMemoryUserRepository.findByAccount(req.getParameter(ACCOUNT))
                 .map(user -> {
                     log.info("User : {}", user);
                     return login(req, user);
                 })
-                .orElse("redirect:/401.jsp");
+                .orElse(LOGIN_FAILED_REDIRECT);
+
+        JspView view = new JspView(viewName);
+        return new ModelAndView(view);
     }
 
     private String login(final HttpServletRequest request, final User user) {
-        if (user.checkPassword(request.getParameter("password"))) {
+        if (user.checkPassword(request.getParameter(PASSWORD))) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return "redirect:/index.jsp";
+            return LOGIN_SUCCESS_REDIRECT;
         }
-        return "redirect:/401.jsp";
+        return LOGIN_FAILED_REDIRECT;
     }
 }
