5b53d2ee68eec8407181470263308721fda0f473
linirini <2001yerin@naver.com>
2024-09-30 17:50:48 +0900

    feat: DI Container로 빈 생성 구현

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,10 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
+import di.ConsumerWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,12 +13,41 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes) throws Exception {
+        this.beans = createBeans(classes);
+        beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) throws Exception {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            beans.add(constructor.newInstance());
+        }
+        return beans;
+    }
+
+    private void setFields(final Object bean) {
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            setFields(bean, field);
+        }
+    }
+
+    private void setFields(final Object bean, final Field field) {
+        final var fieldType = field.getType();
+        field.setAccessible(true);
+
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(target -> field.set(bean, target)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find Bean"));
     }
 }
