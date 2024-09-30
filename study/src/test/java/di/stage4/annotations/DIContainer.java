5f3b470936e2f52670f27fb6059704f82882b1ef
ehtjsv2 <ehtjsv2@naver.com>
2024-09-30 16:42:11 +0900

    study: di 학습테스트 통과

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,12 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +16,64 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            for (Class<?> anInterface : aClass.getInterfaces()) {
+                beans.add(anInterface);
+            }
+            beans.add(aClass);
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
+        Reflections reflections = new Reflections(rootPackageName);
+        Set<Class<?>> typesAnnotatedWithController = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> typesAnnotatedWithService = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> typesAnnotatedWithRepository = reflections.getTypesAnnotatedWith(Repository.class, true);
         return null;
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
+        try {
+            if (beans.contains(aClass)) {
+                // 인터페이스 일 경우 구현체를 찾아 인스턴스 반환
+                if (aClass.isInterface()) {
+                    System.out.println("인터페이스다:" + aClass.getName());
+                    for (Object bean : beans) {
+                        Class<?> bClass = (Class<?>) bean;
+                        Class<?>[] interfaces = bClass.getInterfaces();
+                        for (Class<?> anInterface : interfaces) {
+                            if (anInterface.getName() == aClass.getName()) {
+                                System.out.println("구현체 찾음");
+                                return (T) getBean(bClass);
+                            }
+                        }
+                    }
+                }
+
+                // 클래스 일 경우 인스턴스 반환
+                for (Constructor<?> constructor : aClass.getConstructors()) {
+                    int parameterCount = constructor.getParameterCount();
+                    System.out.println(aClass.getName() + ", paramCount = " + parameterCount);
+                    if (parameterCount == 0) {
+//                        return (T) constructor.newInstance();
+                        Object instance = constructor.newInstance();
+                    }
+
+                    // 파라미터타입들
+                    Object[] classes = new Object[parameterCount];
+                    Class<?>[] parameterTypes = constructor.getParameterTypes();
+                    for (int i = 0; i < parameterCount; i++) {
+                        System.out.println(parameterTypes[i]);
+                        classes[i] = getBean(parameterTypes[i]);
+                    }
+                    return (T) constructor.newInstance(classes);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 }
