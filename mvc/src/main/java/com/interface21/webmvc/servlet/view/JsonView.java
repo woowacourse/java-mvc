c36eff81392cae280bcb7ea1f4655986fd50a3de
SCY <helenason@naver.com>
2024-09-30 17:46:01 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,40 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonView() {
+    }
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        log.info("json view render");
+        model.forEach((k, v) -> log.info("attribute name : {}, value : {}", k, v));
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        PrintWriter writer = response.getWriter();
+        if (model.size() <= 1) {
+            model.forEach((k, v) -> writer.print(v));
+            writer.close();
+            return;
+        }
+        String json = objectMapper.writeValueAsString(model);
+        writer.print(json);
+        writer.close();
     }
 }
