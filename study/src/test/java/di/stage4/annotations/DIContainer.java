1075eff4aa44196f8f0dfd944f26be49d81c4ef8
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-30 17:52:40 +0900

    test: 학습 테스트 stage4 DIContainer 구현

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,75 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
- * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
+ * Equivalent to Spring's BeanFactory or ApplicationContext.
  */
 class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public static DIContainer createContainerForPackage(final String rootPackageName) throws Exception {
+        Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(allClassesInPackage);
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    public DIContainer(final Set<Class<?>> classes) throws Exception {
+        this.beans = new LinkedHashSet<>();
+        initializeBeans(classes);
+        injectFields();
+    }
+
+    private void initializeBeans(Set<Class<?>> classes) throws Exception {
+        for (Class<?> clazz : classes) {
+            Constructor<?> constructor = Arrays.stream(clazz.getDeclaredConstructors())
+                    .filter(this::isConstructorEligible)
+                    .findFirst()
+                    .orElseThrow(IllegalStateException::new);
+
+            constructor.setAccessible(true);
+            Object[] parameters = resolveConstructorParameters(constructor.getParameterTypes());
+            beans.add(constructor.newInstance(parameters));
+        }
+    }
+
+    private boolean isConstructorEligible(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        return countMatchedParameters(parameterTypes) == parameterTypes.length;
+    }
+
+    private int countMatchedParameters(Class<?>[] parameterTypes) {
+        return (int) Arrays.stream(parameterTypes)
+                .filter(parameterType -> getBean(parameterType) != null)
+                .count();
+    }
+
+    private Object[] resolveConstructorParameters(Class<?>[] parameterTypes) {
+        return Arrays.stream(parameterTypes)
+                .map(this::getBean)
+                .toArray();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private void injectFields() throws IllegalAccessException {
+        for (Object bean : beans) {
+            for (Field field : bean.getClass().getDeclaredFields()) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    field.setAccessible(true);
+                    field.set(bean, getBean(field.getType()));
+                }
+            }
+        }
     }
 }
