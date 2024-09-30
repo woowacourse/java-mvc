11f58a57f81f53d289821aaea6cac7ebc7776bc5
masonkimseoul <masonkimseoul@gmail.com>
2024-09-30 10:58:25 +0900

    test: AnnotationHandlerAdapterTest에 케이스 추가

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerAdapterTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerAdapterTest.java
@@ -3,6 +3,7 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
+import com.interface21.context.stereotype.Controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,16 @@ class AnnotationHandlerAdapterTest {
 
         // when & then
         assertThat(handlerAdapter.canHandle(mockHandlerExecution)).isTrue();
+    }
+
+    @Test
+    @DisplayName("Controller를 처리할 수 없다.")
+    void should_return_true_when_canhandle_HandlerExecution2() {
+        // given
+        Controller mockController = mock(Controller.class);
+        HandlerAdapter handlerAdapter = new AnnotationHandlerAdapter();
+
+        // when & then
+        assertThat(handlerAdapter.canHandle(mockController)).isFalse();
     }
 }
