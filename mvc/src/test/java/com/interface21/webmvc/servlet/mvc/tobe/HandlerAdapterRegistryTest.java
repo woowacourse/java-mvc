dc65c96b8e9908ebcf3859ae98103f54867d5200
masonkimseoul <masonkimseoul@gmail.com>
2024-09-30 10:50:12 +0900

    refactor: DisplayName 수정 및 BeforeEach 제거

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
@@ -5,24 +5,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class HandlerAdapterRegistryTest {
 
-    private HandlerAdapterRegistry handlerAdapterRegistry;
-
-    @BeforeEach
-    void setUp() {
-        handlerAdapterRegistry = new HandlerAdapterRegistry();
-        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter());
-    }
-
     @Test
-    @DisplayName("HandlerExecution으로 AnnotationHandlerAdapter를 반환할 수 있다.")
+    @DisplayName("HandlerAdapterRegistry에서 지원하는 핸들러이면 해당 HandlerAdapter를 반환한다.")
     void should_return_AnnotationHandlerAdapter_when_HandlerExecution() {
         // given
+        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
+        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter());
         HandlerExecution handlerExecution = mock(HandlerExecution.class);
 
         // when & then
@@ -34,6 +27,8 @@ class HandlerAdapterRegistryTest {
     @DisplayName("지원하는 타입의 HandlerAdapter가 존재하지 않으면 예외가 발생한다.")
     void should_throw_exception_when_handlerAdapter_that_can_handle_not_exist() {
         // given
+        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
+        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter());
         Controller controller = mock(Controller.class);
 
         // when & then
