cdd9339f83c3d97b145bbf5bf52b837bc35f09bd
hangillee <skfcb10@naver.com>
2024-09-30 17:22:42 +0900

    test: 3단계 학습 테스트 풀이

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,15 +14,62 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::initFields);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        final Set<Object> beans = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            beans.add(instantiateClass(clazz));
+        }
+        return beans;
+    }
+
+    private Object instantiateClass(final Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException |
+                 InvocationTargetException e) {
+            throw new IllegalArgumentException("인스턴스화 할 수 없습니다.");
+        }
+    }
+
+    private void initFields(final Object bean) {
+        final Field[] fields = bean.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    initField(bean, field);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            throw new IllegalArgumentException("초기화할 수 없는 필드입니다.");
+        }
+    }
+
+    private void initField(Object bean, Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        final Class<?> fieldType = field.getType();
+        for (Object o : beans) {
+            if (fieldType.isAssignableFrom(o.getClass())) {
+                field.set(bean, o);
+            }
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 빈입니다."));
     }
 }
