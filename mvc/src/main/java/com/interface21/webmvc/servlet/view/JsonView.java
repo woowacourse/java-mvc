3352f0c65fa288e195eb90b5549bd38d205604e4
Seokmyung Ham <gkatjraud1@gmail.com>
2024-09-30 13:22:38 +0900

    feat: model에 데이터가 1개면 값을 그대로 반환하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -2,6 +2,7 @@ package com.interface21.webmvc.servlet.view;
 
 import static com.interface21.web.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,16 +15,26 @@ public class JsonView implements View {
 
     public static final Logger log = LoggerFactory.getLogger(JsonView.class);
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String result = objectMapper.writeValueAsString(model);
-
-        log.debug("json data: {}", result);
+        String result = serializeToJson(model);
+        log.debug("data: {}", result);
 
         response.setContentType(APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(result);
+    }
+
+    private String serializeToJson(Map<String, ?> model) throws JsonProcessingException {
+        Object target = model;
+        if (model.size() == 1) {
+            target = model.values().stream()
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new);
+        }
+        return objectMapper.writeValueAsString(target);
     }
 }
