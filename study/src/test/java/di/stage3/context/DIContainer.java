44e291d26f022ab6a95713ea485d49bbd4b4ebe6
kunsanglee <gosmdochee2@gmail.com>
2024-09-30 23:40:27 +0900

    feat: 3단계 완료

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,20 +1,56 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
 class DIContainer {
 
+    private static final Logger log = LoggerFactory.getLogger(DIContainer.class);
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        this.beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            Constructor<?> declaredConstructor = aClass.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            Object newInstance = declaredConstructor.newInstance();
+            beans.add(newInstance);
+            injectDependencies(newInstance);
+        }
+    }
+
+    private void injectDependencies(Object instance) {
+        Field[] fields = instance.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            for (Object bean : beans) {
+                if (field.getType().isInstance(bean)) {
+                    field.setAccessible(true);
+                    try {
+                        field.set(instance, bean);
+                    } catch (IllegalAccessException e) {
+                        log.error("Failed to inject dependency for field: " + field.getName(), e);
+                    }
+                }
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
+        for (Object bean : beans) {
+            if (aClass.isInstance(bean)) {
+                return (T) bean;
+            }
+        }
         return null;
     }
 }
--- study/src/test/java/di/stage3/context/Stage3Test.java
+++ study/src/test/java/di/stage3/context/Stage3Test.java
@@ -1,11 +1,11 @@
 package di.stage3.context;
 
-import di.User;
-import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import di.User;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import org.junit.jupiter.api.Test;
 
 class Stage3Test {
 
@@ -16,7 +16,8 @@ class Stage3Test {
      * 테스트가 통과하도록 DIContainer 클래스를 구현하자.
      */
     @Test
-    void stage3() {
+    void stage3()
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         final var user = new User(1L, "gugu");
 
         final var diContainer = createDIContainer();
@@ -39,7 +40,8 @@ class Stage3Test {
     /**
      * DIContainer가 관리하는 객체는 빈(bean) 객체라고 부른다.
      */
-    private static DIContainer createDIContainer() {
+    private static DIContainer createDIContainer()
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         var classes = new HashSet<Class<?>>();
         classes.add(InMemoryUserDao.class);
         classes.add(UserService.class);
