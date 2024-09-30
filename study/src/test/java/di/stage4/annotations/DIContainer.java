e19b3ec8ed67b40a3d1f0e7def79602c9d6d0c63
tsulocalize <tsulocalizing@gmail.com>
2024-09-30 17:29:43 +0900

    test: 4단계 미션 통과

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,15 +14,59 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            addInstance(clazz, classes, beans);
+        }
+
+        this.beans = beans;
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
+    }
+
+    private Object addInstance(Class<?> clazz, Set<Class<?>> classes, Set<Object> beans) {
+        if (beans.stream().anyMatch(instance -> instance.getClass().equals(clazz))) {
+            return beans.stream()
+                    .filter(instance -> instance.getClass().equals(clazz))
+                    .findFirst()
+                    .get();
+        }
+
+        try {
+            Constructor<?> constructor= clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object instance = constructor.newInstance();
+            constructor.setAccessible(false);
+
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (!field.isAnnotationPresent(Inject.class)) continue;
+                System.out.println("field = " + field.getName());
+                Class<?> fieldClass = classes.stream()
+                        .filter(c -> field.getType().isAssignableFrom(c))
+                        .findFirst()
+                        .orElseThrow();
+
+                Object fieldInstance = addInstance(fieldClass, classes, beans);
+
+                field.setAccessible(true);
+                field.set(instance, fieldInstance);
+                field.setAccessible(false);
+            }
+            beans.add(instance);
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(instance -> instance.getClass().equals(aClass))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No beans exist for " + aClass.getName() +" in this container"));
     }
 }
