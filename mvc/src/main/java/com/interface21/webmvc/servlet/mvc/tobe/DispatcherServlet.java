d3a27fbf8a009194467b9cd006e2c9bc156a42b5
masonkimseoul <masonkimseoul@gmail.com>
2024-09-30 18:11:07 +0900

    refactor: DispatcherServlet의 패키지 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/DispatcherServlet.java
@@ -1,13 +1,7 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerMappingRegistry;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,9 +25,7 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     public void init() {
-        setHandlerMappingRegistry(new ManualHandlerMapping());
         setHandlerMappingRegistry(new AnnotationHandlerMapping(BASE_PACKAGE));
-        setHandlerAdapterRegistry(new ManualHandlerAdapter());
         setHandlerAdapterRegistry(new AnnotationHandlerAdapter());
     }
 
--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerAdapterRegistryTest.java
@@ -1,10 +1,8 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,20 +19,5 @@ class HandlerAdapterRegistryTest {
         // when & then
         assertThat(handlerAdapterRegistry.getHandlerAdapter(handlerExecution))
                 .isInstanceOf(AnnotationHandlerAdapter.class);
-    }
-
-    @Test
-    @DisplayName("지원하는 타입의 HandlerAdapter가 존재하지 않으면 예외가 발생한다.")
-    void should_throw_exception_when_handlerAdapter_that_can_handle_not_exist() {
-        // given
-        HandlerAdapterRegistry handlerAdapterRegistry = new HandlerAdapterRegistry();
-        handlerAdapterRegistry.addHandlerAdapter(new AnnotationHandlerAdapter());
-        Controller controller = mock(Controller.class);
-
-        // when & then
-        assertThatThrownBy(() -> handlerAdapterRegistry.getHandlerAdapter(controller))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("%s handler를 지원하는 HandlerAdapter가 존재하지 않습니다."
-                        .formatted(controller.getClass().getName()));
     }
 }
