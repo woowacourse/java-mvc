3b5357d7cf2fa120b0205f6a1a94420c17008119
hjk0761 <hjk0761@naver.com>
2024-10-01 00:47:02 +0900

    test: DI 학습 테스트 구현

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,20 +1,75 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
+// stage3
 class DIContainer {
 
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(clazz -> {
+                    try {
+                        Constructor<?> constructor = clazz.getDeclaredConstructor();
+                        constructor.setAccessible(true);
+                        return constructor.newInstance();
+                    } catch (NoSuchMethodException | InvocationTargetException | InstantiationException |
+                             IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toSet());
+
+    }
+
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
+    // 각 필드에 빈을 대입(assign)한다.
+    private void setFields(final Object bean) {
+        final Field[] fields = bean.getClass()
+                .getDeclaredFields();
+        Arrays.stream(fields)
+                .forEach(field -> {
+                    try {
+                        field.setAccessible(true);
+                        if (Modifier.isStatic(field.getModifiers()) || Objects.nonNull(field.get(bean))) {
+                            return;
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        field.set(bean, getBean(field.getType()));
+                    } catch (final IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    // 빈 컨텍스트(DI)에서 관리하는 빈을 찾아서 반환한다.
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .map(aClass::cast)
+                .findFirst()
+                .orElse(null);
     }
 }
+
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,89 @@
 package di.stage4.annotations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
+// stage3
 class DIContainer {
 
+    private static final Logger log = LoggerFactory.getLogger(DIContainer.class);
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map((final Class<?> aClass) -> {
+                    try {
+                        return aClass.getDeclaredConstructor();
+                    } catch (NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(constructor1 -> {
+                    try {
+                        final Object instance = constructor1.newInstance();
+                        constructor1.setAccessible(false);
+                        return instance;
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toSet());
+    }
+
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
+    // 각 필드에 빈을 대입(assign)한다.
+    private void setFields(final Object bean) {
+        final Field[] fields = bean.getClass().getDeclaredFields();
+        Arrays.stream(fields)
+                .forEach(field -> {
+                    try {
+                        field.setAccessible(true);
+                        if (Objects.nonNull(field.get(bean)) || !field.isAnnotationPresent(Inject.class)) {
+                            return;
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        field.set(bean, getBean(field.getType()));
+                    } catch (final IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    // 빈 컨텍스트(DI)에서 관리하는 빈을 찾아서 반환한다.
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(final Class<T> aClass) {
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .map(aClass::cast)
+                .findFirst()
+                .orElse(null);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
     }
 }
+
