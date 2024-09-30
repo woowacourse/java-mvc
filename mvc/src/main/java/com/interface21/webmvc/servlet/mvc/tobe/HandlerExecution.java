ec54f2af50db05e0572b37694b93b053e10ca9ec
unifolio0 <boho82ha@gmail.com>
2024-09-30 10:41:33 +0900

    refactor: SingletonManager 도입

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,9 +1,9 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.SingletonManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -13,16 +13,9 @@ public class HandlerExecution {
     private final Object instance;
 
     public HandlerExecution(Method method) {
-        try {
-            this.method = method;
-            Class<?> declaringClass = method.getDeclaringClass();
-            Constructor<?> constructor = declaringClass.getDeclaredConstructor();
-            this.instance = constructor.newInstance();
-        } catch (NoSuchMethodException e) {
-            throw new IllegalArgumentException("기본 생성자가 존재하지 않습니다");
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e.getMessage(), e);
-        }
+        this.method = method;
+        Class<?> declaringClass = method.getDeclaringClass();
+        this.instance = SingletonManager.getOrSaveObject(declaringClass);
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) {
