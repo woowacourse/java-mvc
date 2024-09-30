f864e5acd6c6c88c0135a982e04c6c3ca75fafe7
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-09-30 17:46:19 +0900

    test: DI 학습테스트

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,11 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +15,59 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = getBeans(classes);
+    }
+
+    private Set<Object> getBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(clazz -> {
+                    try {
+                        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+                        Constructor<?> constructor = Arrays.stream(constructors)
+                                .filter(constructor1 -> constructor1.getParameterTypes().length > 0)
+                                .findFirst()
+                                .orElse(clazz.getDeclaredConstructor());
+                        constructor.setAccessible(true);
+                        Object[] parameters = parameters(constructor, classes);
+                        return constructor.newInstance(parameters);
+                    } catch (InstantiationException |
+                             IllegalAccessException |
+                             InvocationTargetException |
+                             NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object[] parameters(Constructor<?> constructor, Set<Class<?>> classes) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        return classes.stream()
+                .filter(aClass -> {
+                    for (Class<?> parameterType : parameterTypes) {
+                        if (parameterType.isAssignableFrom(aClass)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .map(aClass -> {
+                    try {
+                        Constructor<?> declaredConstructor = aClass.getDeclaredConstructor();
+                        declaredConstructor.setAccessible(true);
+                        return declaredConstructor.newInstance();
+                    } catch (NoSuchMethodException | IllegalAccessException | InstantiationException |
+                             InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).toArray();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass() == aClass)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("빈 없음"));
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,13 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +17,64 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(clazz -> {
+                    try {
+                        Constructor<?> constructor = clazz.getDeclaredConstructor();
+                        constructor.setAccessible(true);
+                        Object instance = constructor.newInstance();
+                        Field[] fields = instance.getClass().getDeclaredFields();
+                        for (Field field : fields) {
+                            if (field.isAnnotationPresent(Inject.class)) {
+                                field.setAccessible(true);
+                                Object newField = findField(field, classes);
+
+                                field.set(instance, newField);
+                            }
+                        }
+                        return instance;
+                    } catch (InstantiationException |
+                             IllegalAccessException |
+                             InvocationTargetException |
+                             NoSuchMethodException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object findField(Field field, Set<Class<?>> classes) {
+        Class<?> aClass = classes.stream()
+                .filter(clazz -> field.getType().isAssignableFrom(clazz))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("주입 가능한 빈 없음"));
+        try {
+            return aClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Reflections reflections = new Reflections(rootPackageName);
+        Set<Class<?>> beanClasses = new HashSet<>();
+        beanClasses.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        beanClasses.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        beanClasses.addAll(reflections.getTypesAnnotatedWith(Inject.class));
+        return new DIContainer(beanClasses);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("빈 없음"));
     }
 }
