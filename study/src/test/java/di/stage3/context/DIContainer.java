ea54e8092fe3e417ee9fd0cbee3297b7ea6afa9f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-01 00:36:34 +0900

    refactor(DIContainer): @Service, @Repository Component 빈 등록 및 @Inject 빈 주입

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -67,6 +67,6 @@ class DIContainer {
                 .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
                 .findFirst()
                 .map(aClass::cast)
-                .orElseThrow(() -> new RuntimeException("Bean not found"));
+                .orElseThrow(() -> new RuntimeException("Beand을 찾을 수 없습니다."));
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,86 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
 class DIContainer {
 
+    private static final Class<Service> SERVICE_ANNOTATION = Service.class;
+    private static final Class<Repository> REPOSITORY_ANNOTATION = Repository.class;
+    private static final Class<Inject> INJECT_ANNOTATION = Inject.class;
+
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAnnotatedClassesInPackage(
+                rootPackageName,
+                SERVICE_ANNOTATION,
+                REPOSITORY_ANNOTATION);
+
+        return new DIContainer(classes);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toSet());
+    }
+
+    private Object createInstance(final Class<?> aClass) {
+        try {
+            Constructor<?> declaredConstructor = aClass.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            return declaredConstructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setFields(final Object bean) {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        Arrays.stream(declaredFields)
+                .filter(this::isInjectable)
+                .forEach(field -> injectField(field, bean));
+    }
+
+    private boolean isInjectable(final Field field) {
+        int modifiers = field.getModifiers();
+        return !Modifier.isStatic(modifiers) &&
+                !Modifier.isFinal(modifiers) &&
+                field.isAnnotationPresent(INJECT_ANNOTATION);
+    }
+
+    private void injectField(final Field field, final Object bean) {
+        field.setAccessible(true);
+        Object fieldInstance = getBean(field.getType());
+
+        try {
+            field.set(bean, fieldInstance);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .map(aClass::cast)
+                .orElseThrow(() -> new RuntimeException("Beand을 찾을 수 없습니다."));
     }
 }
