233f6d5b600e04ef40864d7922eccf295573d165
tkdgur0906 <tkdgur0906@naver.com>
2024-09-30 17:02:28 +0900

    test: DI 학습 테스트 3단계

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,11 +14,58 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        final Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            try {
+                Constructor<?> constructor = aClass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                beans.add(constructor.newInstance());
+            } catch (NoSuchMethodException |
+                     InvocationTargetException |
+                     InstantiationException |
+                     IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return beans;
+    }
+
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
+    // 각 필드에 빈을 대입(assign)한다.
+    private void setFields(final Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            setField(bean, field);
+        }
+    }
+
+    private void setField(Object bean, Field field) {
+        for (Object injectBean : beans) {
+            if (field.getType().isAssignableFrom(injectBean.getClass())) {
+                try {
+                    field.setAccessible(true);
+                    field.set(bean, injectBean);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    // 빈 컨텍스트(DI)에서 관리하는 빈을 찾아서 반환한다.
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
+        for (Object bean : beans) {
+            if (aClass.isAssignableFrom(bean.getClass())) {
+                return (T) bean;
+            }
+        }
         return null;
     }
 }
