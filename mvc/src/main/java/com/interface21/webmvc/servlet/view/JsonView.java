41c0f83c9b5b8a0772ac176d24390966b0599c2b
nayonsoso <panda0329@naver.com>
2024-09-30 12:04:10 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final String MEDIA_TYPE = MediaType.APPLICATION_JSON_UTF8_VALUE;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType(MEDIA_TYPE);
+            String json = getJson(model);
+            response.getWriter().write(json);
+        } catch (IOException ex) {
+            throw new JsonViewRenderFailedException(ex);
+        }
+    }
+
+    private String getJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Object singleValue = model.values().iterator().next();
+            return objectMapper.writeValueAsString(singleValue);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonViewRenderFailedException.java
@@ -0,0 +1,8 @@
+package com.interface21.webmvc.servlet.view;
+
+public class JsonViewRenderFailedException extends RuntimeException {
+
+    public JsonViewRenderFailedException(Throwable cause) {
+        super("JSON 뷰를 렌더링하는데에 실패했습니다.", cause);
+    }
+}
