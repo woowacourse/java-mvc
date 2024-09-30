953205748821bff01b42932418d47c74331de2ac
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-09-30 15:03:05 +0900

    feat: LogoutController 구현

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -1,15 +1,26 @@
 package com.techcourse.controller;
 
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var session = req.getSession();
+    private static final String REDIRECT_ROOT = "redirect:/";
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView getLogout(HttpServletRequest request, HttpServletResponse response) {
+        final var session = request.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        return "redirect:/";
+        return convertStringToMav(REDIRECT_ROOT);
+    }
+
+    private ModelAndView convertStringToMav(String viewName) {
+        return new ModelAndView(new JspView(viewName));
     }
 }
