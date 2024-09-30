e3723577eb200cfd8bcf940e3a8679779208e392
HoeSeong123 <ghltjd3@gmail.com>
2024-09-30 15:45:02 +0900

    feat: model에 데이터가 1개인 경우 그대로 반환한다

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -14,8 +14,12 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        Object target = model;
+        if (model.size() == 1) {
+            target = model.values().iterator().next();
+        }
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(model);
+        String json = mapper.writeValueAsString(target);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(json);
     }
