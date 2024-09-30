2c403254c3d503a21c2855ed0152211673b8a076
kunsanglee <gosmdochee2@gmail.com>
2024-10-01 00:11:31 +0900

    feat: 4단계 완료

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,58 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
- */
 class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        this.beans = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            Object instance = declaredConstructor.newInstance();
+            beans.add(instance);
+            injectDependencies(instance, beans);
+        }
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    public static DIContainer createContainerForPackage(final String rootPackageName)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(allClassesInPackage);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
+        for (Object bean : beans) {
+            if (aClass.isInstance(bean)) {
+                return (T) bean;
+            }
+        }
         return null;
+    }
+
+    private void injectDependencies(Object instance, Set<Object> beans) {
+        Field[] declaredFields = instance.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            if (declaredField.isAnnotationPresent(Inject.class)) {
+                declaredField.setAccessible(true);
+                for (Object bean : beans) {
+                    if (declaredField.getType().isInstance(bean)) {
+                        try {
+                            declaredField.set(instance, bean);
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException("Failed to inject dependency", e);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- study/src/test/java/di/stage4/annotations/Stage4Test.java
+++ study/src/test/java/di/stage4/annotations/Stage4Test.java
@@ -1,9 +1,10 @@
 package di.stage4.annotations;
 
-import di.User;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import di.User;
+import java.lang.reflect.InvocationTargetException;
+import org.junit.jupiter.api.Test;
 
 class Stage4Test {
 
@@ -13,7 +14,8 @@ class Stage4Test {
      * 해당 역할은 ClassPathScanner 클래스에게 맡기자.
      */
     @Test
-    void stage4() {
+    void stage4()
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         final var user = new User(1L, "gugu");
 
         final var diContext = createDIContainer();
@@ -24,7 +26,8 @@ class Stage4Test {
         assertThat(actual.getAccount()).isEqualTo("gugu");
     }
 
-    private static DIContainer createDIContainer() {
+    private static DIContainer createDIContainer()
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         final var rootPackageName = Stage4Test.class.getPackage().getName();
         return DIContainer.createContainerForPackage(rootPackageName);
     }
