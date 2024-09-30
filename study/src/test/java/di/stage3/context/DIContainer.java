bc1f5c992488d3ac559b50f9b72b2787daeb9328
seunghye218 <seunghye@student.42seoul.kr>
2024-09-30 17:38:07 +0900

    test: DI 학습 테스트

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,8 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -9,12 +12,39 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes) throws Exception {
+        beans = new LinkedHashSet<>();
+        for (Class<?> aClass : classes) {
+            Constructor<?>[] constructors = aClass.getDeclaredConstructors();
+            for (Constructor<?> constructor : constructors) {
+                Class<?>[] parameterTypes = constructor.getParameterTypes();
+                if (countMatchedParameter(parameterTypes) == parameterTypes.length) {
+                    constructor.setAccessible(true);
+                    Object[] beanOf = getBeanOf(parameterTypes);
+                    beans.add(constructor.newInstance(beanOf));
+                    break;
+                }
+            }
+        }
+    }
+
+    private int countMatchedParameter(Class<?>[] parameterTypes) {
+        return (int) Arrays.stream(parameterTypes)
+                .filter(parameter -> getBean(parameter) != null)
+                .count();
+    }
+
+    private Object[] getBeanOf(Class<?>[] parameterTypes) {
+        return Arrays.stream(parameterTypes)
+                .map(this::getBean)
+                .toArray();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
