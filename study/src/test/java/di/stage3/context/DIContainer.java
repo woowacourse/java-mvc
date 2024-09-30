121c474322a3b6ee907afb4303139f50e5b86477
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-01 00:08:43 +0900

    refactor(DIContainer): 필드 주입 로직 구현

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,12 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +16,57 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toSet());
+    }
+
+    private Object createInstance(final Class<?> aClass) {
+        try {
+            Constructor<?> declaredConstructor = aClass.getDeclaredConstructor();
+            declaredConstructor.setAccessible(true);
+            return declaredConstructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setFields(final Object bean) {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        Arrays.stream(declaredFields)
+                .filter(this::isInjectable)
+                .forEach(field -> injectField(field, bean));
+    }
+
+    private boolean isInjectable(final Field field) {
+        int modifiers = field.getModifiers();
+        return !Modifier.isStatic(modifiers) &&
+                !Modifier.isFinal(modifiers);
+    }
+
+    private void injectField(final Field field, final Object bean) {
+        field.setAccessible(true);
+        Object fieldInstance = getBean(field.getType());
+
+        try {
+            field.set(bean, fieldInstance);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .map(aClass::cast)
+                .orElseThrow(() -> new RuntimeException("Bean not found"));
     }
 }
