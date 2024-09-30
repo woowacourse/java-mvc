1134a5b3717af442cb13b4c481aaeae08b0441c0
HyungUk Ryu <hw0603@naver.com>
2024-09-30 17:56:30 +0900

    test: DI 학습 테스트 완료

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,26 @@
 package di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 public class ClassPathScanner {
 
-    public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+    private static final List<Class<? extends Annotation>> BEAN_ANNOTATIONS = List.of(Service.class, Repository.class);
+
+    public static Set<Class<?>> getAllClassesInPackage(String packageName) {
+        return getAllClassesInPackage(packageName, BEAN_ANNOTATIONS);
+    }
+
+    // @Service, @Repository 애노테이션이 붙은 클래스들을 모두 찾아 반환
+    public static Set<Class<?>> getAllClassesInPackage(String packageName, List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections(packageName, Scanners.TypesAnnotated);
+        return BEAN_ANNOTATIONS.stream()
+                .map(reflections::getTypesAnnotatedWith)
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
     }
 }
