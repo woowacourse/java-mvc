4e5accfb2d209a582639b4097ff2f7cf8ce870a7
linirini <2001yerin@naver.com>
2024-09-30 19:22:28 +0900

    feat: 어노테이션 기반 DI Container로 빈 생성 구현

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import di.ConsumerWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,16 +14,51 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes) throws Exception {
+        this.beans = createBeans(classes);
+        beans.forEach(this::setFields);
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    public static DIContainer createContainerForPackage(final String rootPackageName) throws Exception {
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName).stream()
+                .filter(aClass -> aClass.isAnnotationPresent(Service.class) || aClass.isAnnotationPresent(Repository.class))
+                .collect(Collectors.toSet());
+        return new DIContainer(classes);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) throws Exception {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            beans.add(constructor.newInstance());
+        }
+        return beans;
+    }
+
+    private void setFields(final Object bean) {
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Inject.class)) {
+                setFields(bean, field);
+            }
+        }
+    }
+
+    private void setFields(final Object bean, final Field field) {
+        Class<?> type = field.getType();
+        field.setAccessible(true);
+
+        beans.stream()
+                .filter(type::isInstance)
+                .forEach(ConsumerWrapper.accept(target -> field.set(bean, target)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find Bean"));
     }
 }
