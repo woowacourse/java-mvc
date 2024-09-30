46b82972f66fa3f5cd491cb4a6e38cc66cf6587b
Nam Gi Beom <rlqja0523@naver.com>
2024-09-30 19:17:33 +0900

    test: DI step3 test

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,10 @@
 package di.stage3.context;
 
+import static java.util.stream.Collectors.toSet;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -10,11 +15,44 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = instantiateBeans(classes);
+        beans.forEach(this::injectDependencies);
+    }
+
+    private Set<Object> instantiateBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(clazz -> {
+                    try {
+                        Constructor<?> constructor = clazz.getDeclaredConstructor();
+                        constructor.setAccessible(true);
+                        return constructor.newInstance();
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("빈 생성 불가");
+                    }
+                })
+                .collect(toSet());
+    }
+
+    private void injectDependencies(Object bean) {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        Arrays.stream(declaredFields).forEach(field -> {
+            Object dependency = getBean(field.getType());
+            if (dependency != null) {
+                field.setAccessible(true);
+                try {
+                    field.set(bean, dependency);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("의존성 주입 실패");
+                }
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findAny()
+                .orElse(null);
     }
 }
