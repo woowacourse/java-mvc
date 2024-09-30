9b189627a83e6777b1f2fc0ac0518b29af2f1085
masonkimseoul <masonkimseoul@gmail.com>
2024-09-30 17:26:35 +0900

    refactor: LoginViewController 리팩터링

--- app/src/main/java/com/techcourse/controller/LoginViewController.java
+++ app/src/main/java/com/techcourse/controller/LoginViewController.java
@@ -1,22 +1,33 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
-public class LoginViewController implements Controller {
+@Controller
+public class LoginViewController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginViewController.class);
+    private static final String INDEX_PATH = "redirect:/index.jsp";
+    private static final String LOGIN_PATH = "/login.jsp";
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         return UserSession.getUserFrom(req.getSession())
                 .map(user -> {
                     log.info("logged in {}", user.getAccount());
-                    return "redirect:/index.jsp";
+                    return redirect(INDEX_PATH);
                 })
-                .orElse("/login.jsp");
+                .orElse(redirect(LOGIN_PATH));
+    }
+
+    private ModelAndView redirect(String path) {
+        return new ModelAndView(new JspView(path));
     }
 }
