9eccb90b6d39792bcced1d7d462ec68368e6c17c
jongmee <ra123221@gmail.com>
2024-09-30 16:52:19 +0900

    DI 학습 테스트

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,17 @@
 package di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
-    public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+    public static Set<Class<?>> getAllClassesInPackage(String packageName) {
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        Set<Class<?>> allClasses = new HashSet<>(serviceClasses);
+        allClasses.addAll(repositoryClasses);
+        return allClasses;
     }
 }
