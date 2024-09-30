5c4e9fc081e75118950ab23c3bee4a50bb6a101e
jongmee <ra123221@gmail.com>
2024-09-30 15:40:34 +0900

    refactor: IndexController

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,13 +1,10 @@
 package com.techcourse;
 
-import com.techcourse.controller.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
-import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ManualHandlerMapping {
 
@@ -16,8 +13,6 @@ public class ManualHandlerMapping {
     private static final Map<String, Controller> controllers = new HashMap<>();
 
     public void initialize() {
-        controllers.put("/", new ForwardController("/index.jsp"));
-
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
                 .forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
