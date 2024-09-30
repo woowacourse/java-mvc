d983eb8d7d86cd2086140bdbad660251e6cbccc0
zangsu <zangsu_@naver.com>
2024-09-30 16:56:38 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,23 @@
 package com.interface21.webmvc.servlet.view;
 
-import com.interface21.webmvc.servlet.View;
+import java.io.PrintWriter;
+import java.util.Map;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.webmvc.servlet.View;
 
 public class JsonView implements View {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void render( Map<String, ?> model,  HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        PrintWriter writer = response.getWriter();
+        writer.write(objectMapper.writeValueAsString(model));
+        writer.flush();
+        response.setContentType("application/json");
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewStudyTest.java
@@ -0,0 +1,71 @@
+package com.interface21.webmvc.servlet.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+class JsonViewStudyTest {
+
+    @Test
+    @DisplayName("model 의 값을 JSON 으로 변환해 response 를 반환한다.")
+    void study() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        StringWriter stringWriter = new StringWriter();
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+        Map<String, Object> model = Map.of(
+                "user", new User("name", "password"),
+                "someModel", new SomeModel("attribute")
+        );
+
+        var jsonView = new JsonView();
+        jsonView.render(model, request, response);
+
+        assertThat(stringWriter.toString())
+                .isEqualTo(new ObjectMapper().writeValueAsString(model));
+    }
+
+    private static class User {
+        private final String name;
+        private final String password;
+
+        private User(String name, String password) {
+            this.name = name;
+            this.password = password;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    private static class SomeModel {
+        private final String attribute;
+
+        private SomeModel(String attribute) {
+            this.attribute = attribute;
+        }
+
+        public String getAttribute() {
+            return attribute;
+        }
+    }
+}
