7bafe309a2f9d87bad7bf5ce74d889b3b2f29beb
masonkimseoul <masonkimseoul@gmail.com>
2024-09-30 17:13:26 +0900

    refactor: LoginController 리팩터링

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,37 +1,48 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+    private static final String INDEX_PATH = "redirect:/index.jsp";
+    private static final String UNAUTHORIZED_PATH = "redirect:/401.jsp";
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (UserSession.isLoggedIn(request.getSession())) {
+            return redirect(INDEX_PATH);
         }
 
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+        return InMemoryUserRepository.findByAccount(request.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
-                    return login(req, user);
+                    return login(request, user);
                 })
-                .orElse("redirect:/401.jsp");
+                .orElse(redirect(UNAUTHORIZED_PATH));
     }
 
-    private String login(final HttpServletRequest request, final User user) {
+    private ModelAndView login(final HttpServletRequest request, final User user) {
         if (user.checkPassword(request.getParameter("password"))) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return "redirect:/index.jsp";
+            return redirect(INDEX_PATH);
         }
-        return "redirect:/401.jsp";
+        return redirect(UNAUTHORIZED_PATH);
+    }
+
+    private ModelAndView redirect(String path) {
+        return new ModelAndView(new JspView(path));
     }
 }
