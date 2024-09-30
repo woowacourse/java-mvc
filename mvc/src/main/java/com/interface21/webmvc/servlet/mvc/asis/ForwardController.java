144d7531464379e364ad21d8a69e7a0d424dac30
slimsha2dy <chy2581@naver.com>
2024-09-30 17:11:54 +0900

    refactor: ForwardController 어노테이션 기반 컨트롤러로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/ForwardController.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/ForwardController.java
@@ -1,11 +1,14 @@
 package com.interface21.webmvc.servlet.mvc.asis;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Objects;
 
-public class ForwardController implements Controller {
+@Controller
+public class ForwardController {
 
     private final String path;
 
@@ -13,7 +16,8 @@ public class ForwardController implements Controller {
         this.path = Objects.requireNonNull(path);
     }
 
-    @Override
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String execute(final HttpServletRequest request, final HttpServletResponse response) {
         return path;
     }
