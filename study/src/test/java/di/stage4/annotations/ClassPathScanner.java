7e310c046c5ae295c1081af5bbfeb27f58c7767f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-01 00:35:50 +0900

    refactor(ClassPathScanner): 패키지 경로로부터 해당하는 클래스를 반환하는 스캐너 구현

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,35 @@
 package di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        return reflections.getSubTypesOf(Object.class);
+    }
+
+    public static Set<Class<?>> getAnnotatedClassesInPackage(
+            final String packageName,
+            final Class<? extends Annotation> annotation
+    ) {
+        Reflections reflections = new Reflections(packageName, Scanners.TypesAnnotated);
+        return reflections.getTypesAnnotatedWith(annotation);
+    }
+
+    public static Set<Class<?>> getAnnotatedClassesInPackage(
+            final String packageName,
+            final Class<? extends Annotation>... annotations
+    ) {
+        Reflections reflections = new Reflections(packageName, Scanners.TypesAnnotated);
+        return Arrays.stream(annotations)
+                .flatMap(annotation -> reflections.getTypesAnnotatedWith(annotation).stream())
+                .collect(Collectors.toSet());
     }
 }
