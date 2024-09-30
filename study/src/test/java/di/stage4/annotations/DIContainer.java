8034e39deb2ed47c5a457476e3e277f3bf1fcac2
donghoony <aru0504@naver.com>
2024-09-30 18:58:56 +0900

    test: 학습 테스트 4단계 완료

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,12 @@
 package di.stage4.annotations;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +16,43 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toSet());
+        this.beans.forEach(this::setFields);
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    private void setFields(Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .forEach(field -> setField(bean, field));
+    }
+
+    private void setField(Object bean, Field field) {
+        Class<?> fieldType = field.getType();
+        field.setAccessible(true);
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(fieldBean -> field.set(bean, fieldBean)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().isAssignableFrom(aClass))
+                .map(bean -> (T) bean)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("No bean for [" + aClass + "] found"));
+    }
+
+    public static DIContainer createContainerForPackage(final String rootPackageName) {
+        Set<Class<?>> allClasses = ClassPathScanner.getAllClassesInPackage(rootPackageName)
+                .stream()
+                .filter(clazz -> clazz.isAnnotationPresent(Service.class) ||
+                                 clazz.isAnnotationPresent(Repository.class))
+                .collect(Collectors.toSet());
+        return new DIContainer(allClasses);
     }
 }
