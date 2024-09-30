ec9fc518a06a3ccf9abec6a08127ec5b9f9cf3e4
Nam Gi Beom <rlqja0523@naver.com>
2024-09-30 15:58:46 +0900

    feat: JsonView 구현 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,33 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final int SINGLE_MODEL_SIZE = 1;
+
+    private final ObjectMapper objectMapper;
+
+    public JsonView() {
+        this.objectMapper = new ObjectMapper();
+    }
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        String result = objectMapper.writeValueAsString(extractModel(model));
+        response.getWriter().write(result);
+    }
+
+    private Object extractModel(Map<String, ?> model) {
+        if (model.size() == SINGLE_MODEL_SIZE) {
+            return model.values().iterator().next();
+        }
+        return model;
     }
 }
---
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -0,0 +1,79 @@
+package com.interface21.webmvc.servlet.view;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class JsonViewTest {
+
+    @Test
+    void JSON_content_type을_설정한다() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        View jsonView = new JsonView();
+
+        // when
+        jsonView.render(Collections.emptyMap(), request, response);
+
+        // then
+        verify(response).setContentType("application/json;charset=UTF-8");
+    }
+
+    @Test
+    void model에_데이터가_한_개이면_값을_그대로_반환한다() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        View jsonView = new JsonView();
+
+        // when
+        Map<String, Object> model = Collections.singletonMap("user", new User("prin", "backend"));
+        jsonView.render(model, request, response);
+
+        // then
+        String result = """
+                {"name":"prin","group":"backend"}""";
+        verify(writer).write(result);
+    }
+
+    @Test
+    void model에_데이터가_2개_이상이면_전체를_JSON으로_변환한다() throws Exception {
+        // given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        PrintWriter writer = mock(PrintWriter.class);
+        when(response.getWriter()).thenReturn(writer);
+
+        View jsonView = new JsonView();
+
+        // when
+        Map<String, Object> model = Map.of(
+                "user1", new User("prin", "backend"),
+                "user2", new User("maru", "frontend")
+        );
+        jsonView.render(model, request, response);
+
+        // then
+        String result = """
+                {"user1":{"name":"prin","group":"backend"},"user2":{"name":"maru","group":"frontend"}}""";
+        verify(writer).write(result);
+    }
+
+    private record User(String name, String group) {
+    }
+}
