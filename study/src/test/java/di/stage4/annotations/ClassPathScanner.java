f94f5649941fafe4146ef06ea7649d8ff3951ef3
임수빈 <robin980108@naver.com>
2024-09-30 17:54:35 +0900

    DI 학습 테스트 4단계

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,17 @@
 package di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+        HashSet<Class<?>> classes = new HashSet<>(services);
+        classes.addAll(repositories);
+        return classes;
     }
 }
