0fa5d0f901822ba2884235e0817993bc288d7104
ashsty <qkralsdk991@naver.com>
2024-09-30 17:34:02 +0900

    feat: DI 학습 테스트

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,18 @@
 package di.stage4.annotations;
 
+import org.reflections.Reflections;
+
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        final Reflections reflections = new Reflections(packageName);
+        return Stream.of(Service.class, Repository.class)
+                .flatMap(annotation -> reflections.getTypesAnnotatedWith(annotation)
+                        .stream())
+                .collect(Collectors.toSet());
     }
 }
