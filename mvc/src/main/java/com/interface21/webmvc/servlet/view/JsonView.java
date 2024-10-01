ce53858201d7310d98df7c5efb55d15487c33ad2
재우 <Akaps6768@gmail.com>
2024-10-01 13:22:11 +0900

    test: JsonView 테스트 로직 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -11,13 +11,14 @@ import com.interface21.webmvc.servlet.View;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         Object value = model;
         if (model.size() == 1) {
             value = model.values().iterator().next();
         }
-        ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(value);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(json);
--- mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/view/JsonViewTest.java
@@ -17,20 +17,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.web.http.MediaType;
 
 class JsonViewTest {
 
-    @Test
-    @DisplayName("Render one json data.")
-    void render() throws Exception {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Render a model to json.")
+    void render(Map<String, Object> model, String expected) throws Exception {
         // given
         var request = mock(HttpServletRequest.class);
         var response = mock(HttpServletResponse.class);
         var printWriter = mock(PrintWriter.class);
         when(response.getWriter()).thenReturn(printWriter);
-        var model = Map.of("account", "Arachneee");
 
         var sut = new JsonView();
 
@@ -38,29 +41,52 @@ class JsonViewTest {
         sut.render(model, request, response);
 
         // then
-        var objectMapper = new ObjectMapper();
-        verify(printWriter).write(objectMapper.writeValueAsString("Arachneee"));
+        verify(printWriter).write(expected);
         verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
     }
 
+    static Stream<Arguments> render() throws JsonProcessingException {
+        var user = new User("Arachneee", "1q2w3e4r!");
+        var model1 = Map.of("account", user.account);
+        var model2 = Map.of("user", user);
+        return Stream.of(
+                Arguments.of(model1, objectMapper.writeValueAsString(user.account)),
+                Arguments.of(model2, objectMapper.writeValueAsString(user))
+        );
+    }
 
-    @Test
-    @DisplayName("Render mulitple json data.")
-    void render_multiple() throws Exception {
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("Render multiple model to json.")
+    void renderMultiple(Map<String, Object> model, String expected) throws Exception {
         // given
         var request = mock(HttpServletRequest.class);
         var response = mock(HttpServletResponse.class);
         var printWriter = mock(PrintWriter.class);
         when(response.getWriter()).thenReturn(printWriter);
-        var model = Map.of("account", "Arachneee", "company", "woowacourse");
+
         var sut = new JsonView();
 
-        //when
+        // when
         sut.render(model, request, response);
 
         // then
-        var objectMapper = new ObjectMapper();
-        verify(printWriter).write(objectMapper.writeValueAsString(model));
+        verify(printWriter).write(expected);
         verify(response).setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+    }
+
+    static Stream<Arguments> renderMultiple() throws JsonProcessingException {
+        var user1 = new User("Arachneee", "1q2w3e4r!");
+        var user2 = new User("gugu", "password");
+        var user3 = new User("zeus", "secret");
+        var model1 = Map.of("user1", user1, "user2", user2);
+        var model2 = Map.of("user1", user1, "user2", user2, "user3", user3);
+        return Stream.of(
+                Arguments.of(model1, objectMapper.writeValueAsString(model1)),
+                Arguments.of(model2, objectMapper.writeValueAsString(model2))
+        );
+    }
+
+    record User(String account, String password) {
     }
 }
