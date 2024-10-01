2db11f2aefe92ca0e6e81f94edac6b6fd095ce44
le2sky <leehaneul990623@gmail.com>
2024-10-01 10:50:54 +0900

    test: DI 4단계 학습 테스트 구현

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,10 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +14,48 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(this::createBean)
+                .collect(Collectors.toSet());
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
+    }
+
+    private Object createBean(Class<?> targetClass) {
+        try {
+            Constructor<?> defaultConstructor = targetClass.getDeclaredConstructor();
+            defaultConstructor.setAccessible(true);
+            return defaultConstructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("기본 생성자가 존재하지 않습니다.");
+        }
+    }
+
+    private void setFields(Object bean) {
+        Class<?> beanClass = bean.getClass();
+        for (Field declaredField : beanClass.getDeclaredFields()) {
+            declaredField.setAccessible(true);
+            injectDependency(bean, declaredField);
+        }
+    }
+
+    private void injectDependency(Object bean, Field dependencyField) {
+        try {
+            Class<?> dependencyFieldType = dependencyField.getType();
+            Object dependencyBean = getBean(dependencyFieldType);
+            dependencyField.set(bean, dependencyBean);
+        } catch (Exception ignored) {
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> targetBeanType) {
+        return (T) beans.stream()
+                .filter(bean -> targetBeanType.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 빈입니다."));
     }
 }
