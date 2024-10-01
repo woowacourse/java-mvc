62b94715a702b8c9b71b1bb26340fd089db3d599
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-10-01 23:34:39 +0900

    refactor: view 구현체 checkedException 시그니처 구체화

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -5,6 +5,7 @@ import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
@@ -13,7 +14,7 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response)
-            throws Exception {
+            throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         if (model.size() == 1) {
             response.getWriter().write(OBJECT_MAPPER.writeValueAsString(model.values().iterator().next()));
