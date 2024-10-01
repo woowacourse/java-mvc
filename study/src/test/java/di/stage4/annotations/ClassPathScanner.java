10d37a80210caa79146985362891f8ad00c07a21
Nam Gi Beom <rlqja0523@naver.com>
2024-10-01 16:01:10 +0900

    test: DI step4 test

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,16 @@
 package di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> allClasses = new HashSet<>();
+        allClasses.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        allClasses.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        return allClasses;
     }
 }
