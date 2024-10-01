6c715127cbd2e4bbfab8556d8772ce4ae285b0fa
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-01 17:13:55 +0900

    test: 학습테스트 완성

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,15 @@
 package di.stage4.annotations;
 
+import org.reflections.Reflections;
+
 import java.util.Set;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Service.class);
+        classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+        return classes;
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,65 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-/**
- * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
- */
-class DIContainer {
+public class DIContainer {
 
-    private final Set<Object> beans;
+    private final Map<Class<?>, Object> beans = new HashMap<>();
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    private DIContainer(final Set<Class<?>> classes) {
+        createBeans(classes);
+        beans.values()
+                .forEach(this::injectDependencies);
     }
 
-    public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+    private void createBeans(final Set<Class<?>> classes) {
+        for (Class<?> clazz : classes) {
+            if (clazz.isAnnotationPresent(Service.class) || clazz.isAnnotationPresent(Repository.class)) {
+                try {
+                    Constructor<?> constructor = clazz.getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    Object instance = constructor.newInstance();
+                    beans.put(clazz, instance);
+
+                    for (Class<?> iface : clazz.getInterfaces()) {
+                        beans.put(iface, instance);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("의존성 주입 실패: " + clazz.getName(), e);
+                }
+            }
+        }
     }
 
-    @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    private void injectDependencies(final Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Class<?> fieldType = field.getType();
+                Object dependency = beans.get(fieldType);
+                if (dependency == null) {
+                    throw new RuntimeException("그런 bean은 이 세상에 존재하지 않습니다: " + fieldType.getName());
+                }
+                field.setAccessible(true);
+                try {
+                    field.set(bean, dependency);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("의존성 주입 실패!: " + fieldType.getName(), e);
+                }
+            }
+        }
+    }
+
+    public <T> T getBean(Class<T> clazz) {
+        return clazz.cast(beans.get(clazz));
+    }
+
+    public static DIContainer createContainerForPackage(String packageName) {
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(packageName);
+        return new DIContainer(classes);
     }
 }
