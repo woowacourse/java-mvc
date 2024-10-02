d144217f30adc70370b053f51b91d0a1c4e2dced
PgmJun <84304802+PgmJun@users.noreply.github.com>
2024-10-02 14:58:57 +0900

    test: di 테스트 작성

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,24 @@
 package di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
+    private static final Set<Class> DI_TARGET_CLASSES = Set.of(
+            Inject.class,
+            Service.class,
+            Repository.class
+    );
+
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Set<Class<?>> classes = new HashSet<>();
+
+        Reflections reflections = new Reflections(packageName);
+        for (Class targetClass : DI_TARGET_CLASSES) {
+            classes.addAll(reflections.getTypesAnnotatedWith(targetClass));
+        }
+        return classes;
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,10 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,16 +13,72 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Object> beans) {
+        this.beans = beans;
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        Set<Object> beans = createBeans(allClassesInPackage);
+        beans.forEach(bean -> injectDependency(bean, beans));
+
+        return new DIContainer(beans);
+    }
+
+    private static Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(clazz -> {
+                    try {
+                        Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+                        declaredConstructor.setAccessible(true);
+                        return declaredConstructor.newInstance();
+                    } catch (Exception e) {
+                        throw new RuntimeException("빈 객체 생성 실패", e);
+                    }
+                })
+                .collect(Collectors.toSet());
+    }
+
+    private static void injectDependency(Object target, Set<Object> beans) {
+        try {
+            for (Field field : target.getClass().getDeclaredFields()) {
+                if (isStaticField(field)) {
+                    continue;
+                }
+                Object dependencyBean = getBean(field.getType(), beans);
+                if (!isBeanDependency(dependencyBean)) {
+                    continue;
+                }
+
+                field.setAccessible(true);
+                field.set(target, dependencyBean);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("의존성 주입 중 문제가 발생하였습니다.", e);
+        }
+    }
+
+    private static boolean isStaticField(Field field) {
+        return Modifier.isStatic(field.getModifiers());
+    }
+
+    private static boolean isBeanDependency(Object dependencyBean) {
+        return dependencyBean != null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T getBean(final Class<T> aClass, Set<Object> beans) {
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElse(null);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElse(null);
     }
 }
