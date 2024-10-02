b3a4d4d7059d2eb979b4f7a555cae3cb52f1af28
ay-eonii <naayen@naver.com>
2024-10-03 00:04:27 +0900

    refactor: 중복 로직 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,6 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,17 +11,20 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         ObjectMapper mapper = new ObjectMapper();
+        String json = mapToJson(model, mapper);
+        response.getWriter().write(json);
+    }
+
+    private static String mapToJson(Map<String, ?> model, ObjectMapper mapper) throws JsonProcessingException {
         if (model.size() == 1) {
             Object next = model.values().iterator().next();
-            String json = mapper.writeValueAsString(next);
-            response.getWriter().write(json);
-            return;
+            return mapper.writeValueAsString(next);
         }
-        String json = mapper.writeValueAsString(model);
-        response.getWriter().write(json);
+        return mapper.writeValueAsString(model);
     }
 }
