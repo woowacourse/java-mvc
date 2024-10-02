be1a5bbafb9984d1b93ad6bdf9d41eb3479f4860
HoeSeong123 <ghltjd3@gmail.com>
2024-10-02 15:53:09 +0900

    test: DI 학습 테스트

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,13 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +17,38 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(this::createInstance))
+                .collect(Collectors.toSet());
+    }
+
+    private Object createInstance(Class<?> clazz) throws Exception {
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return constructor.newInstance();
+    }
+
+    private void setFields(Object instance) {
+        Field[] fields = instance.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            var type = field.getType();
+            field.setAccessible(true);
+            beans.stream()
+                    .filter(type::isInstance)
+                    .forEach(ConsumerWrapper.accept(matchBean -> field.set(instance, matchBean)));
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("No bean of type " + aClass.getName()));
     }
 }
