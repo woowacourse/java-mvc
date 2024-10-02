b838e224697eaaa8871fb2aabad64e33730b8132
slimsha2dy <chy2581@naver.com>
2024-10-02 18:22:59 +0900

    feat: di 학습 테스트 코드 추가

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -7,14 +11,63 @@ import java.util.Set;
  */
 class DIContainer {
 
-    private final Set<Object> beans;
+    private final Map<Class<?>, Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = new HashMap<>();
+        classes.forEach(clazz -> registerBean(clazz, classes));
+    }
+
+    public void registerBean(Class<?> clazz, Set<Class<?>> classes) {
+        if (getBean(clazz) != null) {
+            return;
+        }
+        classes.stream().filter(clazz::isAssignableFrom)
+                .findFirst()
+                .ifPresentOrElse(
+                        actual -> beans.put(actual, createInstance(actual, classes)),
+                        () -> {
+                            throw new IllegalArgumentException("클래스에 해당하는 빈이 없습니다.");
+                        }
+                );
+    }
+
+    private Object createInstance(Class<?> clazz, Set<Class<?>> classes) {
+        try {
+            Constructor<?> constructor = clazz.getConstructors()[0];
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            Object[] args = Arrays.stream(parameterTypes)
+                    .map(type -> getOrRegister(type, classes))
+                    .toArray();
+            return constructor.newInstance(args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object getOrRegister(Class<?> clazz, Set<Class<?>> classes) {
+        Object bean = getBean(clazz);
+        if (bean == null) {
+            registerBean(clazz, classes);
+            bean = getBean(clazz);
+        }
+        return bean;
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        if (beans.containsKey(aClass)) {
+            return (T) beans.get(aClass);
+        }
+
+        Class<?> clazz = beans.keySet().stream()
+                .filter(aClass::isAssignableFrom)
+                .findFirst()
+                .orElse(null);
+
+        if (clazz == null) {
+            return null;
+        }
+        return (T) beans.get(clazz);
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,10 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -7,18 +12,81 @@ import java.util.Set;
  */
 class DIContainer {
 
-    private final Set<Object> beans;
+    private final Map<Class<?>, Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = new HashMap<>();
+        classes.forEach(clazz -> registerBean(clazz, classes));
+    }
+
+    public void registerBean(Class<?> clazz, Set<Class<?>> classes) {
+        if (getBean(clazz) != null) {
+            return;
+        }
+        classes.stream().filter(clazz::isAssignableFrom)
+                .findFirst()
+                .ifPresentOrElse(
+                        actual -> beans.put(actual, createInstance(actual, classes)),
+                        () -> {
+                            throw new IllegalArgumentException("클래스에 해당하는 빈이 없습니다.");
+                        }
+                );
+    }
+
+    private Object createInstance(Class<?> clazz, Set<Class<?>> classes) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object instance = constructor.newInstance();
+
+            Arrays.stream(clazz.getDeclaredFields())
+                    .filter(field -> field.isAnnotationPresent(Inject.class))
+                    .forEach(field -> setField(classes, field, instance));
+
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setField(Set<Class<?>> classes, Field field, Object instance) {
+        try {
+            Class<?> type = field.getType();
+            Object factor = getOrRegister(type, classes);
+            field.setAccessible(true);
+            field.set(instance, factor);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object getOrRegister(Class<?> clazz, Set<Class<?>> classes) {
+        Object bean = getBean(clazz);
+        if (bean == null) {
+            registerBean(clazz, classes);
+            bean = getBean(clazz);
+        }
+        return bean;
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        if (beans.containsKey(aClass)) {
+            return (T) beans.get(aClass);
+        }
+
+        Class<?> clazz = beans.keySet().stream()
+                .filter(aClass::isAssignableFrom)
+                .findFirst()
+                .orElse(null);
+
+        if (clazz == null) {
+            return null;
+        }
+        return (T) beans.get(clazz);
     }
 }
