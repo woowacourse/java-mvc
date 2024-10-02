6be031f94477a9956d72af52415b8b2b1fdc6f92
yoonjuho <dbswn990@naver.com>
2024-10-02 17:58:12 +0900

    test: DI 학습 테스트

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,9 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -10,15 +14,49 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(allClassesInPackage);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            try {
+                Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+                declaredConstructor.setAccessible(true);
+                beans.add(declaredConstructor.newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create bean for class: " + clazz.getName(), e);
+            }
+        }
+        return beans;
+    }
+
+    private void setFields(final Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            Object dependency = getBean(field.getType());
+            if (Objects.nonNull(dependency)) {
+                field.setAccessible(true);
+                try {
+                    field.set(bean, dependency);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Failed to inject dependency into field: " + field.getName(), e);
+                }
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(b -> aClass.isAssignableFrom(b.getClass()))
+                .findFirst()
+                .orElse(null);
     }
 }
