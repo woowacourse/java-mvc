71617a138d2d55912014e594a489237766c95759
fromitive <fromitive@gmail.com>
2024-10-02 15:38:53 +0900

    test: stag4 해결

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,14 @@
 package di.stage4.annotations;
 
+import static org.reflections.scanners.Scanners.SubTypes;
+
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        // Object.class의 모든 하위 타입의 클래스를 스캔하기 위해 SubTypes Scanner를 추가
+        return new Reflections(packageName, SubTypes.filterResultsBy(c -> true)).getSubTypesOf(Object.class);
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,10 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +14,61 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> targetClasses = ClassPathScanner.getAllClassesInPackage(rootPackageName).stream()
+                .filter(clazz -> clazz.isAnnotationPresent(Service.class) ||
+                        clazz.isAnnotationPresent(Repository.class))
+                .collect(Collectors.toSet());
+        return new DIContainer(targetClasses);
     }
 
-    @SuppressWarnings("unchecked")
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::createInstanceFromDefaultConstructor)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object createInstanceFromDefaultConstructor(Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("기본 생성자로 빈을 생성하는데 실패했습니다.");
+        }
+    }
+
+    private void setFields(final Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(filter -> filter.isAnnotationPresent(Inject.class))
+                .forEach(field -> setField(bean, field));
+    }
+
+    private void setField(Object bean, Field field) {
+        Class<?> fieldType = field.getType();
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(matchBean -> injectField(bean, field, matchBean));
+    }
+
+    private void injectField(Object bean, Field field, Object matchBean) {
+        try {
+            field.setAccessible(true);
+            field.set(bean, matchBean);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .map(bean -> (T) bean)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("bean을 찾을 수 없습니다."));
     }
 }
