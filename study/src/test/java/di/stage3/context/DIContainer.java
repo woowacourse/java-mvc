e9154e61a03a57dfccc6231473049ae842537b65
takoyakimchi <c4nyou@gmail.com>
2024-10-03 02:19:39 +0900

    study: DI 학습 테스트 3단계

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,7 +1,12 @@
 package di.stage3.context;
 
-import java.util.HashSet;
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -11,24 +16,49 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = new HashSet<>(classes);
+        this.beans = createInstances(classes);
+        this.beans.forEach(this::injectFields);
+    }
+
+    // 기본 생성자로 객체 생성
+    private Set<Object> createInstances(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(this::createNewInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object createNewInstance(Class<?> clazz) throws Exception {
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return constructor.newInstance();
+    }
+
+    // 필드 주입하기
+    private void injectFields(Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        Arrays.stream(fields)
+                .forEach(ConsumerWrapper.accept(field -> setField(field, bean)));
+    }
+
+    private void setField(Field field, Object bean) throws Exception {
+        field.setAccessible(true);
+        Class<?> fieldType = field.getType();
+
+        if (isBeanRegistered(fieldType)) {
+            field.set(bean, getBean(fieldType));
+        }
+    }
+
+    private boolean isBeanRegistered(Class<?> fieldType) {
+        return beans.stream()
+                .anyMatch(fieldType::isInstance);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        try {
-            if (beans.contains(aClass)) {
-                if (aClass == InMemoryUserDao.class) {
-                    return aClass.getConstructor().newInstance();
-                }
-                if (aClass == UserService.class) {
-                    return aClass.getConstructor(UserDao.class).newInstance(getBean(InMemoryUserDao.class));
-                }
-            }
-
-            return null;
-        } catch (Exception e) {
-            return null;
-        }
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("bean을 찾을 수 없습니다."));
     }
 }
