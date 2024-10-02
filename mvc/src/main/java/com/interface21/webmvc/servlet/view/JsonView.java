50584f24b987268f4d2260c504767726d5062442
libienz <leekh10195@gmail.com>
2024-10-02 10:52:19 +0900

    refactor: 메서드가 세부 예외를 던지도록 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -5,13 +5,14 @@ import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+            throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         if (model.size() == 1) {
             Object value = model.values().iterator().next();
