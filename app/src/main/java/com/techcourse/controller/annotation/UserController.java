422bc72f5b9fc100d2624d5a7f26e827b6ea03bf
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-02 10:42:20 +0900

    refactor: 불필요한 로거 삭제

--- app/src/main/java/com/techcourse/controller/annotation/UserController.java
+++ app/src/main/java/com/techcourse/controller/annotation/UserController.java
@@ -9,13 +9,9 @@ import com.techcourse.domain.User;
 import com.techcourse.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Controller
 public class UserController {
-
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService = new UserService();
 
