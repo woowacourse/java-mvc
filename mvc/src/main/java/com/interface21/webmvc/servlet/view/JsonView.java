55ce80ad83a4053cbd183fa34350d4ccbded15bf
takoyakimchi <c4nyou@gmail.com>
2024-10-02 19:52:33 +0900

    refactor: JsonView 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,11 +1,11 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
@@ -16,23 +16,23 @@ public class JsonView implements View {
 
     @Override
     public void render(
-            final Map<String, ?> model,
-            final HttpServletRequest request,
+            Map<String, ?> model,
+            HttpServletRequest request,
             HttpServletResponse response) throws Exception {
 
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        writeAsJson(model, response);
+        String json = convertToJson(model);
+        response.getWriter().write(json);
     }
 
-    private void writeAsJson(Map<String, ?> model, HttpServletResponse response) throws IOException {
+    private String convertToJson(Map<String, ?> model) throws JsonProcessingException {
         if (model.size() == SINGLE_DATA_SIZE) {
             Object object = model.values().stream()
                     .findFirst()
                     .get();
-            response.getWriter().write(objectMapper.writeValueAsString(object));
-            return;
+            return objectMapper.writeValueAsString(object);
         }
 
-        response.getWriter().write(objectMapper.writeValueAsString(model));
+        return objectMapper.writeValueAsString(model);
     }
 }
