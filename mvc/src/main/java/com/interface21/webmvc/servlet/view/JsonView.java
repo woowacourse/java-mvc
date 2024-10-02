457e1687d78104d42094fc03fbd06ab66bc7a0d9
yechop <channetattoo@gmail.com>
2024-10-02 16:40:56 +0900

    refactor: 던지는 예외 변경, 매직넘버 상수화

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -7,21 +7,24 @@ import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final int RETURN_ORIGIN_VALUE = 1;
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         String json = toJson(model);
         response.getWriter().write(json);
     }
 
     private String toJson(Map<String, ?> model) throws JsonProcessingException {
-        if (model.size() == 1) {
+        if (model.size() == RETURN_ORIGIN_VALUE) {
             Object object = model.values().iterator().next();
             return objectMapper.writeValueAsString(object);
         }
