b9b189c1c9a19d8c2b77c8acf15e313fc4434b58
libienz <leekh10195@gmail.com>
2024-10-02 17:36:02 +0900

    refactor: DI 컨테이너 학습 테스트 리팩터링

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,9 +1,13 @@
 package di.stage3.context;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -13,39 +17,36 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = new HashSet<>();
-        for (Class<?> clazz : classes) {
-            try {
-                Constructor<?> constructor = clazz.getDeclaredConstructor();
-                constructor.setAccessible(true);
-                beans.add(constructor.newInstance());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::injectDependency);
+    }
 
-        for (Object bean : beans) {
-            Field[] declaredFields = bean.getClass().getDeclaredFields();
-            for (Field declaredField : declaredFields) {
-                declaredField.setAccessible(true);
-                try {
-                    for (Object o : beans) {
-                        if (declaredField.getType().isAssignableFrom(o.getClass())) {
-                            declaredField.set(bean, o);
-                        }
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toSet());
+    }
+
+    private void injectDependency(Object bean) {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .forEach(field -> setFields(bean, field));
+    }
+
+    private void setFields(Object bean, Field field) {
+        Class<?> fieldType = field.getType();
+        field.setAccessible(true);
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
+    public <T> T getBean(Class<T> aClass) {
         return (T) beans.stream()
                 .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
                 .findAny()
-                .orElseThrow();
+                .orElseThrow(() -> new NoSuchElementException("bean을 찾을 수 없습니다"));
     }
 }
