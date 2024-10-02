b7add773bdd2a2d9de65edc6c95b00ed7126b055
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-03 00:24:46 +0900

    refactor(DIContainer): 빈을 찾을 수 없는 경우 IllegalArgumentException 발생

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -81,6 +81,6 @@ class DIContainer {
                 .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
                 .findFirst()
                 .map(aClass::cast)
-                .orElseThrow(() -> new RuntimeException("Bean을 찾을 수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("Bean을 찾을 수 없습니다."));
     }
 }
