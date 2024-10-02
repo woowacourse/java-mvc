d53c6a7a9122cc16b9cd65b1e322e38ca809e2d7
tackyu <tack970913@gmail.com>
2024-10-03 06:24:36 +0900

    test: di stage3 학습

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,11 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -9,12 +15,44 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    public DIContainer(final Set<Class<?>> classes)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        this.beans = new HashSet<>();
+        createBeans(classes);
+        for (Object bean : beans) {
+            setFields(bean);
+        }
+    }
+
+    private void createBeans(Set<Class<?>> classes)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        for (Class<?> aClass : classes) {
+            Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object bean = constructor.newInstance();
+            beans.add(bean);
+        }
+    }
+
+    private void setFields(Object bean) throws IllegalAccessException {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            Class<?> filedType = field.getType();
+            Optional<Object> beanField = beans.stream()
+                    .filter(filedType::isInstance)
+                    .findFirst();
+            if (beanField.isPresent()) {
+                field.set(bean, beanField.get());
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(object -> object.getClass().equals(aClass))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
