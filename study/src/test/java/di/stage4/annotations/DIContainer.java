a2556cd0f2c1e4e95f4d27ac6c7a0b53d440ae0b
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-02 22:20:26 +0900

    test: 학습테스트 진행

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,13 @@
 package di.stage4.annotations;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +17,47 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void setFields(Object bean) {
+        Field[] declaredFields = bean.getClass().getDeclaredFields();
+
+        Stream.of(declaredFields)
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .forEach(field -> setFields(bean, field));
+    }
+
+    private void setFields(Object bean, Field field) {
+        field.setAccessible(true);
+
+        beans.stream()
+                .filter(field.getType()::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
+
+        field.setAccessible(false);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classesInPackage);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .map(object -> (T) object)
+                .orElseThrow(() -> new NoSuchElementException(aClass.getSimpleName() + "클래스를 찾을 수 없습니다."));
     }
 }
