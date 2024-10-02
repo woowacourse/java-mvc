d22ce876c1ac2f1beeccbfcd1b5fd464efe4a13a
nayonsoso <panda0329@naver.com>
2024-10-03 02:08:11 +0900

    test: ControllerScanner 테스트

--- mvc/src/test/java/com/interface21/webmvc/servlet/mvc/mapping/impl/ControllerScannerTest.java
+++ mvc/src/test/java/com/interface21/webmvc/servlet/mvc/mapping/impl/ControllerScannerTest.java
@@ -1,19 +1,29 @@
 package com.interface21.webmvc.servlet.mvc.mapping.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.interface21.context.stereotype.Controller;
-import java.util.Set;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.reflections.Reflections;
 
 class ControllerScannerTest {
 
     @Test
     @DisplayName("특정 패키지에 있는 컨트롤러 어노테이션이 붙은 컨트롤러를 스캔한다")
-    void scan() {
+    void scanPackage() {
         // given
-        Reflections reflections = new Reflections();
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
-        System.out.println("스캔된 클래스들 = " + classes.size());
+        String packageName = TestController.class.getPackage().getName();
+
+        // when
+        ControllerScanner controllerScanner = new ControllerScanner(packageName);
+        Map<Class<?>, Object> controllerInstance = controllerScanner.getControllerInstance();
+
+        // then
+        assertThat(controllerInstance).containsKeys(TestController.class);
+    }
+
+    @Controller
+    static class TestController {
     }
 }
