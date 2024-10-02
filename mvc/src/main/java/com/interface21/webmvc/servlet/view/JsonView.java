e5adbdaa610fdc3d4bc703173b9812f1b8ef82e6
김민겸 <mingyum119@naver.com>
2024-10-02 21:58:20 +0900

    refactor: ObjectMapper을 static 변수로 생성

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -11,11 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JsonView implements View {
 
 	private static final int DOSE_NOT_USE_JSON_COUNT = 1;
-	private final ObjectMapper objectMapper;
-
-	public JsonView() {
-		this.objectMapper = new ObjectMapper();
-	}
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
