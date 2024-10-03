b76ab3471d8b2784186fa443171c52c004515148
nhlee98 <e5slnh9876@gmail.com>
2024-10-03 13:57:35 +0900

    feat: DI 학습테스트 구현

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,10 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -7,14 +12,60 @@ import java.util.Set;
  */
 class DIContainer {
 
-    private final Set<Object> beans;
+    private final Map<Class<?>, Object> beans = new HashMap<>();
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        classes.forEach(aClass -> registerBean(aClass, classes));
+    }
+
+    private Object registerBean(final Class<?> aClass, Set<Class<?>> classes) {
+        Class<?> subClass = findSubClass(aClass, classes);
+
+        if (beans.containsKey(subClass)) {
+            return beans.get(subClass);
+        }
+
+        List<Object> parameterObjects = Arrays.stream(findConstructorParameterTypes(subClass))
+                .map(parameterType -> registerBean(parameterType, classes))
+                .toList();
+
+        return registerBean(subClass, parameterObjects);
+    }
+
+    private Object registerBean(Class<?> aClass, List<Object> parameterObjects) {
+        try {
+            Object bean = aClass.getConstructor(findConstructorParameterTypes(aClass))
+                    .newInstance(parameterObjects.toArray());
+            beans.put(aClass, bean);
+            return bean;
+        } catch (Throwable e) {
+            throw new IllegalStateException("객체를 생성하는데 실패하였습니다.");
+        }
+    }
+
+    private Class<?> findSubClass(final Class<?> aClass, Set<Class<?>> classes) {
+        List<Class<?>> subClasses = classes.stream()
+                .filter(aClass::isAssignableFrom)
+                .toList();
+
+        if (subClasses.size() != 1) {
+            throw new IllegalStateException(aClass.getName() + " 타입의 빈이 여러개 등록되어 있습니다.");
+        }
+
+        return subClasses.get(0);
+    }
+
+    private Class<?>[] findConstructorParameterTypes(Class<?> aClass) {
+        Constructor<?>[] constructors = aClass.getConstructors();
+        if (constructors.length != 1) {
+            throw new IllegalArgumentException(aClass.getName() + "의 생성자가 하나 이상이라 빈 등록에 실패하였습니다.");
+        }
+
+        return constructors[0].getParameterTypes();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.get(findSubClass(aClass, beans.keySet()));
     }
 }
--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,13 @@
 package di.stage4.annotations;
 
 import java.util.Set;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        return new Reflections(packageName, Scanners.SubTypes.filterResultsBy(filter -> true))
+                .getSubTypesOf(Object.class);
     }
 }
