b8884bc43ac8246ddf3818f27600d97fc6ba62ad
nayonsoso <panda0329@naver.com>
2024-10-04 02:16:15 +0900

    feat: 루트 경로 컨트롤러 추가

---
+++ app/src/main/java/com/techcourse/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.techcourse.controller;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.impl.JspView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView view(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
+    }
+}
