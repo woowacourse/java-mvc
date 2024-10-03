28cb38733008c1c491a269bbacb6974412942435
libienz <leekh10195@gmail.com>
2024-10-03 21:26:10 +0900

    fix: 존재하지 않는 user정보 json 조회 분기 구현 (sc = 404)

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -5,10 +5,13 @@ import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JsonView;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,17 +19,20 @@ import org.slf4j.LoggerFactory;
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final String NOT_FOUND_PAGE = "404.jsp";
 
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
         final String account = request.getParameter("account");
         log.debug("user id : {}", account);
 
-        final ModelAndView modelAndView = new ModelAndView(new JsonView());
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow();
+        final Optional<User> user = InMemoryUserRepository.findByAccount(account);
+        if (user.isEmpty()) {
+            return new ModelAndView(new JspView(NOT_FOUND_PAGE));
+        }
 
-        modelAndView.addObject("user", user);
+        final ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", user.get());
         return modelAndView;
     }
 }
