b5a606051aa16bc3dcf9253918baad56dbfcbea7
hyeonjilee <skylar12200@gmail.com>
2024-10-04 08:02:12 +0900

    test: stage4 구현

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,19 @@
 package di.stage4.annotations;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        return Stream.of(
+                        reflections.getTypesAnnotatedWith(Inject.class).stream(),
+                        reflections.getTypesAnnotatedWith(Service.class).stream(),
+                        reflections.getTypesAnnotatedWith(Repository.class).stream())
+                .flatMap(clazz -> clazz)
+                .collect(Collectors.toSet());
     }
 }
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,8 @@
 package di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,15 +13,58 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
+    }
+
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            try {
+                Constructor<?> constructor = aClass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                beans.add(constructor.newInstance());
+            } catch (ReflectiveOperationException e) {
+                throw new IllegalArgumentException("bean을 생성할 수 없습니다.");
+            }
+        }
+        return beans;
+    }
+
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
+    private void setFields(final Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            setField(field, bean);
+        }
+    }
+
+    private void setField(Field field, Object bean) {
+        field.setAccessible(true);
+        beans.stream()
+                .filter(matchBean -> field.getType().isInstance(matchBean))
+                .forEach(matchBean -> {
+                    try {
+                        field.set(bean, matchBean);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        for (Object bean : beans) {
+            if (aClass.isInstance(bean)) {
+                return (T) bean;
+            }
+        }
+        throw new IllegalArgumentException("bean을 찾을 수 없습니다.");
     }
 }
