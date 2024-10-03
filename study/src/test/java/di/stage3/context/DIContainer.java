95630d0f72ad96c20003c1960d8744c3253f905a
seunghye218 <seunghye@student.42seoul.kr>
2024-10-03 11:37:42 +0900

    refactor(DIContainer): 로직 개선

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,9 +1,11 @@
 package di.stage3.context;
 
+import di.ConsumerWrapper;
 import java.lang.reflect.Constructor;
-import java.util.Arrays;
+import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -13,37 +15,44 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) throws Exception {
-        beans = new LinkedHashSet<>();
+        beans = generateBeans(classes);
+        injectFields(beans);
+    }
+
+    private Set<Object> generateBeans(Set<Class<?>> classes) throws Exception {
+        Set<Object> beans = new LinkedHashSet<>();
         for (Class<?> aClass : classes) {
-            Constructor<?>[] constructors = aClass.getDeclaredConstructors();
-            for (Constructor<?> constructor : constructors) {
-                Class<?>[] parameterTypes = constructor.getParameterTypes();
-                if (countMatchedParameter(parameterTypes) == parameterTypes.length) {
-                    constructor.setAccessible(true);
-                    Object[] beanOf = getBeanOf(parameterTypes);
-                    beans.add(constructor.newInstance(beanOf));
-                    break;
-                }
-            }
+            Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            Object bean = constructor.newInstance();
+            beans.add(bean);
+        }
+        return beans;
+    }
+
+    private void injectFields(Set<Object> beans) {
+        beans.stream()
+                .collect(Collectors.toMap(bean -> bean, bean -> bean.getClass().getDeclaredFields()))
+                .forEach(this::injectField);
+    }
+
+    private void injectField(Object bean, Field[] fields) {
+        for (Field field : fields) {
+            setField(bean, field);
         }
     }
 
-    private int countMatchedParameter(Class<?>[] parameterTypes) {
-        return (int) Arrays.stream(parameterTypes)
-                .filter(parameter -> getBean(parameter) != null)
-                .count();
-    }
-
-    private Object[] getBeanOf(Class<?>[] parameterTypes) {
-        return Arrays.stream(parameterTypes)
-                .map(this::getBean)
-                .toArray();
+    private void setField(Object bean, Field field) {
+        beans.stream()
+                .filter(matchBean -> field.getType().isInstance(matchBean))
+                .peek(matchBean -> field.setAccessible(true))
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
         return (T) beans.stream()
-                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .filter(aClass::isInstance)
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
     }
