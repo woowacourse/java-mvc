d401008432d2b087b40f50cafd48625c5e326eb9
devhoya97 <diajbh123@gmail.com>
2024-10-04 21:41:10 +0900

    test: DI 학습테스트 4단계

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,35 @@
 package di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 public class ClassPathScanner {
 
-    public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+    public static Set<Class<?>> getAnnotationClasses(String packageName, Class<? extends Annotation>... annotations) {
+        Set<Class<?>> annotationClasses = new HashSet<>();
+        for (Class<?> aClass : getAllClassesInPackage(packageName)) {
+            if (doesHaveAnyAnnotations(aClass, annotations)) {
+                annotationClasses.add(aClass);
+            }
+        }
+        return annotationClasses;
+    }
+
+    private static Set<Class<?>> getAllClassesInPackage(String packageName) {
+        Reflections reflections = new Reflections(packageName, Scanners.SubTypes.filterResultsBy(c -> true));
+        return reflections.getSubTypesOf(Object.class);
+    }
+
+    private static boolean doesHaveAnyAnnotations(Class<?> aClass, Class<? extends Annotation>... annotations) {
+        for (Class<? extends Annotation> annotation : annotations) {
+            if (aClass.isAnnotationPresent(annotation)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
