a5ea225d41978ff0d5149534f6fe62c2b9c74473
SungKyum Kim <sungkyum1@naver.com>
2024-10-05 02:17:28 +0900

    test: ClassPathScanner로 클래스 경로 기반 DIContainer 구현

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,21 @@
 package di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
+
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
+    private static Class<?>[] targetAnnotations = {Service.class, Repository.class};
+
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        HashSet<Class<?>> classes = new HashSet<>();
+        Reflections reflections = new Reflections(packageName);
+        for (Class<?> targetAnnotation : targetAnnotations) {
+            classes.addAll(reflections.getTypesAnnotatedWith((Class<? extends Annotation>)targetAnnotation));
+        }
+        return classes;
     }
 }
