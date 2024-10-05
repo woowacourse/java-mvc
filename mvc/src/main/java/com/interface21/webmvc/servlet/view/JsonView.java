e40a8c410c933080dd7df650747332211c8f7921
masonkimseoul <masonkimseoul@gmail.com>
2024-10-05 19:14:15 +0900

    refactor: model 내부 데이터 개수별로 다르게 변환하도록 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,6 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
@@ -14,8 +15,17 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        String jsonResult = objectMapper.writeValueAsString(model);
+        String jsonResult = getModelValue(model);
+
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.getWriter().write(jsonResult);
+    }
+
+    private String getModelValue(Map<String,?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            Object value = model.values().iterator().next();
+            return objectMapper.writeValueAsString(value);
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
