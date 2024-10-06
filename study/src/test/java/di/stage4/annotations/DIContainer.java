6d8a10bee676bb38da07d4f9f45292b4a76e8e5a
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-06 15:12:48 +0900

    test: DI stage4 학습테스트

--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package di.stage4.annotations;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+import java.lang.reflect.Field;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +15,38 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(bean -> setFields(bean, bean.getClass().getDeclaredFields()));
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(clazz -> clazz.getDeclaredConstructor()))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(constructor -> constructor.newInstance()))
+                .collect(Collectors.toSet());
+    }
+
+    private void setFields(final Object targetBean, final Field[] fields) {
+        for (Field field : fields) {
+            field.setAccessible(true);
+
+            beans.stream()
+                    .filter(bean -> field.getType().isInstance(bean))
+                    .forEach(ConsumerWrapper.accept(fieldBean -> field.set(targetBean, fieldBean)));
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> clazz) {
+        return (T) beans.stream()
+                .filter(bean -> clazz.isInstance(bean))
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("no such bean exists."));
     }
 }
