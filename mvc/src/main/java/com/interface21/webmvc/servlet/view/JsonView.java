6eaba78c9eb18d86631b561b7348c436653a7909
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-06 16:27:03 +0900

    fix: model 크기가 1인 경우 데이터를 그대로 반환하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -10,12 +10,19 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final int FIRST_VALUE_INDEX = 0;
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) {
         try {
             response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+            if (model.size() == 1) {
+                Object value = model.values().toArray()[FIRST_VALUE_INDEX];
+                response.getWriter().write(objectMapper.writeValueAsString(value));
+                return;
+            }
             response.getWriter().write(objectMapper.writeValueAsString(model));
         } catch (IOException e) {
             throw new RuntimeException("failed to write value into JSON", e);
