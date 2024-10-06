5fa09cba712f23253c8e303790bb439fca66fc43
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-06 16:04:05 +0900

    refactor: 예외 메시지와 함께 RuntimeException으로 예외 변환하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -5,6 +5,7 @@ import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
@@ -12,9 +13,12 @@ public class JsonView implements View {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request,
-                       final HttpServletResponse response) throws Exception {
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        response.getWriter().write(objectMapper.writeValueAsString(model));
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) {
+        try {
+            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            response.getWriter().write(objectMapper.writeValueAsString(model));
+        } catch (IOException e) {
+            throw new RuntimeException("failed to write value into JSON", e);
+        }
     }
 }
