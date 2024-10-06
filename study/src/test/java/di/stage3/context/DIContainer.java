a8f1c801431df0909f3a2e4ae2407fbd5087b1db
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-10-06 15:41:15 +0900

    refactor, study: 학습 테스트 코드 다시 구현
    - 생성자 주입에서 필드 주입으로 변경
      - 코드가 너무 읽기 힘들어져, 이미 제공되는 private 기본 생성자를 이용

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,9 +1,9 @@
 package di.stage3.context;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Set;
 import javax.annotation.Nullable;
 
@@ -16,49 +16,42 @@ class DIContainer {
 
     public DIContainer(final Set<Class<?>> classes) {
         this.beans = new HashSet<>();
-        initializeBean(classes);
+        initializeBeans(classes);
     }
 
-    private void initializeBean(Set<Class<?>> beanClasses) {
-        Queue<Class<?>> queue = new LinkedList<>(beanClasses);
-        while (!queue.isEmpty()) {
-            Class<?> beanClass = queue.poll();
-            Object bean = initializeBean(beanClass);
-            if (bean == null) {
-                queue.add(beanClass);
-                continue;
-            }
-            beans.add(bean);
+    private void initializeBeans(Set<Class<?>> beanClasses) {
+        for (Class<?> beanClass : beanClasses) {
+            beans.add(createBean(beanClass));
+        }
+        for (Object bean : beans) {
+            injectBeanAtField(bean);
         }
     }
 
-    private Object initializeBean(Class<?> beanClass) {
-        Object bean = getBean(beanClass);
-        if (bean != null) {
-            return bean;
-        }
-
-        Constructor<?>[] constructors = beanClass.getDeclaredConstructors();
-        if (constructors.length == 0) {
-            return null;
-        }
-
-        Constructor<?> constructor = constructors[0]; // 생성자는 1개만 있다고 가정
-        Class<?>[] parameterTypes = constructor.getParameterTypes();
-        Object[] parameters = new Object[parameterTypes.length];
+    private Object createBean(Class<?> beanClass) {
         try {
-            for (int i = 0; i < parameterTypes.length; i++) {
-                Object parameter = initializeBean(parameterTypes[i]);
-                if (parameter == null) {
-                    return null;
-                }
-                parameters[i] = parameter;
-            }
-            bean = constructor.newInstance(parameters);
+            Constructor<?> constructor = beanClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
         } catch (Exception e) {
             throw new RuntimeException("해당 빈을 만드는 데 실패했습니다." + beanClass.getName(), e);
         }
-        return bean;
+    }
+
+    private void injectBeanAtField(Object bean) {
+        Field[] fields = bean.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            Object fieldBean = getBean(field.getType());
+            if (Modifier.isStatic(field.getModifiers()) || fieldBean == null) {
+                continue;
+            }
+            try {
+                field.setAccessible(true);
+                field.set(bean, fieldBean);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("필드 주입에 실패했습니다: " + field.getName(), e);
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
--- study/src/test/java/di/stage4/annotations/DIContainer.java
+++ study/src/test/java/di/stage4/annotations/DIContainer.java
@@ -2,6 +2,7 @@ package di.stage4.annotations;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -18,9 +19,11 @@ class DIContainer {
 
     public DIContainer(final Set<Class<?>> classes) {
         this.beans = new HashSet<>();
-        initializeBean(classes);
+        for (Class<?> beanClass : classes) {
+            beans.add(createBean(beanClass));
+        }
         for (Object bean : beans) {
-            injectFieldBean(bean);
+            injectBeanAtField(bean);
         }
     }
 
@@ -35,64 +38,28 @@ class DIContainer {
         return new DIContainer(beanClasses);
     }
 
-    private void initializeBean(Set<Class<?>> beanClasses) {
-        Queue<Class<?>> queue = new LinkedList<>(beanClasses);
-        while (!queue.isEmpty()) {
-            Class<?> beanClass = queue.poll();
-            Object bean = initializeBean(beanClass);
-            if (bean == null) {
-                queue.add(beanClass);
-                continue;
-            }
-            beans.add(bean);
-        }
-    }
-
-    private Object initializeBean(Class<?> beanClass) {
-        Object bean = getBean(beanClass);
-        if (bean != null) {
-            return bean;
-        }
-
-        return createBean(beanClass);
-    }
-
     private Object createBean(Class<?> beanClass) {
         try {
-            Constructor<?>[] constructors = beanClass.getDeclaredConstructors();
-            if (constructors.length == 0) {
-                return null;
-            }
-
-            Constructor<?> constructor = constructors[0]; // 생성자는 1개만 있다고 가정
-            Class<?>[] parameterTypes = constructor.getParameterTypes();
-            Object[] parameters = new Object[parameterTypes.length];
-            for (int i = 0; i < parameterTypes.length; i++) {
-                Object parameter = initializeBean(parameterTypes[i]);
-                if (parameter == null) {
-                    return null;
-                }
-                parameters[i] = parameter;
-            }
+            Constructor<?> constructor = beanClass.getDeclaredConstructor();
             constructor.setAccessible(true);
-            return constructor.newInstance(parameters);
+            return constructor.newInstance();
         } catch (Exception e) {
             throw new RuntimeException("해당 빈을 만드는 데 실패했습니다." + beanClass.getName(), e);
         }
     }
 
-    private void injectFieldBean(Object bean) {
+    private void injectBeanAtField(Object bean) {
         Field[] fields = bean.getClass().getDeclaredFields();
-
         for (Field field : fields) {
-            if (field.isAnnotationPresent(Inject.class)) {
-                Object fieldBean = initializeBean(field.getType());
-                try {
-                    field.setAccessible(true);
-                    field.set(bean, fieldBean);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException("필드 주입에 실패했습니다: " + field.getName(), e);
-                }
+            if (!field.isAnnotationPresent(Inject.class)) {
+                continue;
+            }
+            Object fieldBean = getBean(field.getType());
+            try {
+                field.setAccessible(true);
+                field.set(bean, fieldBean);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("필드 주입에 실패했습니다: " + field.getName(), e);
             }
         }
     }
