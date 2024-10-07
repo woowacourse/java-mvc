09adbdeef0c489d6ebcc09ee4be0a91cecaecd9c
zangsu <zangsu_@naver.com>
2024-10-07 17:17:05 +0900

    study: stage4 완료

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,13 @@
 package di.stage4.annotations;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.platform.commons.support.ReflectionSupport;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        return ReflectionSupport.streamAllClassesInPackage(packageName, clazz -> true, name -> true)
+                .collect(Collectors.toSet());
     }
 }
