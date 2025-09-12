50957949fc84183ad45d33b310e72666fe026292
yeonnhuu <yeonnhuu@gmail.com>
2025-09-12 17:45:17 +0900

    feat: Servlet & Servlet Container 퀘스트 완료

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -11,6 +11,8 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        response.setContentType("text/html; charset=UTF-8");     // 브라우저 인코딩도 처리
+//        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 }
