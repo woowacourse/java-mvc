ce8da970f252344f1d74dd8cdf9940434b1cb79a
MinJun Choi <mj043000@naver.com>
2025-09-12 12:26:29 +0900

    chore: Junit3TestRunner, Junit4TestRunner 수정

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -8,6 +8,15 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Object testInstance = clazz.getDeclaredConstructor().newInstance();
+
+        var methods = clazz.getDeclaredMethods();
+        for (var method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                System.out.println("Executing: " + method.getName());
+                method.invoke(testInstance);
+            }
+        }
     }
 }
