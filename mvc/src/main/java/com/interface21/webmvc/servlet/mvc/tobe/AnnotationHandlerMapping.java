8b5226e2f9db19bc2d22afd06d3296fb904b3ec6
sung-june27 <kmymy27@gmail.com>
2025-09-12 21:19:09 +0900

    feat: handlerExecutions initialize() 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,14 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +28,24 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final Reflections reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            final Method[] methods = controller.getDeclaredMethods();
+            for (Method method : methods) {
+                final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                if (requestMapping == null) {
+                    continue;
+                }
+                final String url = requestMapping.value();
+                final RequestMethod[] requestMethods = requestMapping.method();
+                for (RequestMethod requestMethod : requestMethods) {
+                    final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                    final HandlerExecution handlerExecution = new HandlerExecution(handlerKey, controller);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+            }
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -3,10 +3,28 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Object handlerMethod;
+    private final Object handler;
+
+    public HandlerExecution(
+            final Object handlerMethod,
+            final Object handler
+    ) {
+        this.handlerMethod = handlerMethod;
+        this.handler = handler;
+    }
+
+    public ModelAndView handle(
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
+        final Method method = (Method) handlerMethod;
+        final Object returnValue = method.invoke(handler, request, response);
+
+        return (ModelAndView) returnValue;
     }
 }
