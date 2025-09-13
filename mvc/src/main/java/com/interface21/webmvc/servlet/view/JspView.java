ca3f49e8ff70684720d39152ec099002e1f8e0bc
ryan-dia <cjfdnjs158@naver.com>
2025-09-13 22:51:19 +0900

    feat: jspView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,6 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -14,18 +15,33 @@ public class JspView implements View {
 
     public static final String REDIRECT_PREFIX = "redirect:";
 
+    private final String viewName;
+
     public JspView(final String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
-
+    public void render(final Map<String, ?> model,
+                       final HttpServletRequest request,
+                       final HttpServletResponse response) throws Exception {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        /**
+         * redirect or forward
+         *
+         * redirect → 클라이언트에 302 응답 → 브라우저가 새 요청을 보냄 → URL 바뀜.
+         * forward → 서버 내부에서 JSP로 제어를 넘김 → URL 그대로 유지.
+         */
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            String redirectPath = viewName.substring(REDIRECT_PREFIX.length());
+            response.sendRedirect(redirectPath);
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(viewName);
+            dispatcher.forward(request, response);
+        }
     }
 }
