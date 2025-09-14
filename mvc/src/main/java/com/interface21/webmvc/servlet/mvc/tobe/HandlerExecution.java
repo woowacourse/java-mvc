60f5647e5a17b3d464ae14571f44bf1c1f1e69f1
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-15 00:19:57 +0900

    Revert "feat: AnnotationHandlerMapping 구현"
    This reverts commit a8be79ca461484242b03b0616c942c2ae9bf8d8b.

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,32 +1,12 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.web.bind.annotation.RequestMethod;
-import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class HandlerExecution {
 
-    private final RequestMethod[] methods;
-    private final Map<String,String> values;
-
-    public HandlerExecution(RequestMethod[] methods) {
-        this.methods = methods;
-        values = new HashMap<>();
-    }
-
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        JspView jspView = new JspView(request.getRequestURI());
-
-        ModelAndView modelAndView = new ModelAndView(jspView);
-
-        String id = (String) request.getAttribute("id");
-
-        modelAndView.addObject("id",id);
-        return modelAndView;
+        return null;
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,17 +1,9 @@
 package reflection;
 
-import jdk.jfr.ContentType;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
-import org.reflections.Store;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reflection.annotation.Controller;
-import reflection.annotation.Repository;
-import reflection.annotation.Service;
-
-import java.lang.reflect.Constructor;
-import java.util.Set;
 
 class ReflectionsTest {
 
@@ -20,20 +12,6 @@ class ReflectionsTest {
     @Test
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("reflection.examples");
-        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
-        for(Class<?> constructorsAnnotatedWith : typesAnnotatedWith){
-            System.out.println(constructorsAnnotatedWith.getName());
-        }
-
-        typesAnnotatedWith = reflections.getTypesAnnotatedWith(Service.class);
-        for(Class<?> constructorsAnnotatedWith : typesAnnotatedWith){
-            System.out.println(constructorsAnnotatedWith.getName());
-        }
-
-        typesAnnotatedWith = reflections.getTypesAnnotatedWith(Repository.class);
-        for(Class<?> constructorsAnnotatedWith : typesAnnotatedWith){
-            System.out.println(constructorsAnnotatedWith.getName());
-        }
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
     }
