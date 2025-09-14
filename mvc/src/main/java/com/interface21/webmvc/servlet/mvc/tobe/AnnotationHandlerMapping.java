809b6f63b69f34823f5e153cfebab0e8a440f906
Firedrago95 <yonghwa95@naver.com>
2025-09-14 17:48:08 +0900

    feat: mvc 프레임워크 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +24,43 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize() throws InstantiationException, IllegalAccessException {
         log.info("Initialized AnnotationHandlerMapping!");
+        for (Object base : basePackage) {
+            Reflections reflections = new Reflections((String) base);
+            Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+            addHandlerExecutions(typesAnnotatedWith);
+        }
+    }
+
+    private void addHandlerExecutions(Set<Class<?>> typesAnnotatedWith)
+            throws InstantiationException, IllegalAccessException {
+        for (Class<?> aClass : typesAnnotatedWith) {
+            Method[] methods = aClass.getMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    String url = requestMapping.value();
+                    RequestMethod[] httpMethod = requestMapping.method();
+                    for (RequestMethod requestMethod : httpMethod) {
+                        Object o = aClass.newInstance();
+                        HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                        HandlerExecution handlerExecution = new HandlerExecution(o, method);
+                        handlerExecutions.put(handlerKey, handlerExecution);
+                    }
+                }
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
+        HandlerExecution handlerExecution = handlerExecutions.getOrDefault(handlerKey, null);
+
+        if (handlerExecution == null) {
+            throw new IllegalArgumentException("");
+        }
+
+        return handlerExecution;
     }
 }
