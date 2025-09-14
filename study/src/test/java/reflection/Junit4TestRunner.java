2dac559e9c492d1a267e40f71508fd43fa3b9d92
Yehyeok Bang <qkddpgur318@gmail.com>
2025-09-14 20:07:45 +0900

    test: Reflection 학습 테스트

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -9,5 +9,11 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final var instance = clazz.getDeclaredConstructor().newInstance();
+        for (var method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(instance);
+            }
+        }
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,12 @@
 package reflection;
 
+import di.stage4.annotations.Repository;
+import di.stage4.annotations.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
 
 class ReflectionsTest {
 
@@ -14,5 +17,11 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        var controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        var serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        var repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        controllerClasses.forEach(c -> log.debug("Controller: {}", c.getName()));
+        serviceClasses.forEach(c -> log.debug("Service: {}", c.getName()));
+        repositoryClasses.forEach(c -> log.debug("Repository: {}", c.getName()));
     }
 }
