5bc5c6cb2a30a5493182ed5b11d66302011f2707
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-14 19:00:38 +0900

    feat: AnnotationHandlerMapping 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -3,12 +3,18 @@ package reflection;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Objects;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
         final var instance = clazz.newInstance();
+
+        Junit3Test instance = clazz.getDeclaredConstructor().newInstance();
+        instance.test1();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
         for (Method method : clazz.getDeclaredMethods()) {
