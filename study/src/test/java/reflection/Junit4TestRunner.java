138839f015d853dd5627bca4a83f86710e80262c
JO-eusan <goodsan0330@gmail.com>
2025-09-14 22:08:10 +0900

    docs: Reflection API 학습

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -8,6 +9,14 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // 리플렉션으로 Junit4Test 클래스의 인스턴스를 새로 만듬
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+
+        // Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : clazz.getMethods()) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(instance);
+            }
+        }
     }
 }
