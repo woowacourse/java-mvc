e469e265227d2309f2f9c76c7b548a8de162d2bd
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-09-15 03:33:52 +0900

    feat: JspView 리다이렉트 및 모델 처리 로직 추가
    - JspView 클래스에 리다이렉트 처리 및 JSP 포워드 로직 구현
    - 모델 데이터를 HttpServletRequest에 세팅하는 로직 추가
    - DispatcherServlet의 move 메서드에 JspView와 ModelAndView 활용하도록 수정
    - 기존 리다이렉트 및 포워드 처리 로직을 JspView를 통해 일원화

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -3,29 +3,40 @@ package com.interface21.webmvc.servlet.view;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
 public class JspView implements View {
 
-    private static final Logger log = LoggerFactory.getLogger(JspView.class);
-
     public static final String REDIRECT_PREFIX = "redirect:";
+    private static final Logger log = LoggerFactory.getLogger(JspView.class);
+    private final String viewName;
 
-    public JspView(final String viewName) {
+    public JspView(String viewName) {
+        this.viewName = viewName;
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
 
+        // 리다이렉트 처리
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            String redirectPath = viewName.substring(REDIRECT_PREFIX.length());
+            log.debug("Redirecting to: {}", redirectPath);
+            response.sendRedirect(redirectPath);
+            return;
+        }
+
+        // 모델 세팅
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        // JSP 포워드
+        log.debug("Forwarding to view: {}", viewName);
+        request.getRequestDispatcher(viewName).forward(request, response);
     }
 }
