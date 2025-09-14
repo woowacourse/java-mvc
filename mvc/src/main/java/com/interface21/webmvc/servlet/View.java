d0afc490aad2a7cd4ab947aaa0f40c37f4eef090
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-14 20:58:43 +0900

    feat: DispatchServlet AnnotationHandler 도입

--- mvc/src/main/java/com/interface21/webmvc/servlet/View.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/View.java
@@ -1,10 +1,11 @@
 package com.interface21.webmvc.servlet;
 
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Map;
 
-public interface View {
+public interface View extends Controller {
     void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
