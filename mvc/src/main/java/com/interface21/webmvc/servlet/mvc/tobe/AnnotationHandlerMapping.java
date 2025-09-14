8bbaefb80ca0f75e3e66823a6adb48f5c7cfcb3b
changui <122252160+changuii@users.noreply.github.com>
2025-09-14 19:56:10 +0900

    feat: MVC Framework 테스트 통과하기

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -19,11 +27,44 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize()
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controllerType : controllerTypes) {
+            Constructor<?> constructor = controllerType.getConstructor();
+            Object handler = constructor.newInstance();
+            Method[] controllerMethods = controllerType.getDeclaredMethods();
+
+            for (Method method : controllerMethods) {
+                if(!method.isAnnotationPresent(RequestMapping.class)){
+                    break;
+                }
+
+                RequestMapping handlerAnnotation = method.getAnnotation(RequestMapping.class);
+                String url = handlerAnnotation.value();
+                RequestMethod[] httpMethods = handlerAnnotation.method();
+
+                for (RequestMethod httpMethod : httpMethods) {
+                    HandlerKey handlerKey = new HandlerKey(url, httpMethod);
+
+                    HandlerExecution handlerExecution = new HandlerExecution((request, response) -> {
+                        return (ModelAndView) method.invoke(handler, request, response);
+                    });
+
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+            }
+        }
+
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String url = request.getRequestURI();
+        RequestMethod httpMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(url, httpMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -3,10 +3,17 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Handler handler;
+
+    public HandlerExecution(Handler handler) {
+        this.handler = handler;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return handler.handle(request, response);
     }
 }
