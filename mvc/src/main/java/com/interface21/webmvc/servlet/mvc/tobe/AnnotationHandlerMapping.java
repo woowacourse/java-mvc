501d0cf39cbb376914af4eca56a4c50eb27537e5
GiSeok Kang <ime.rokz@gmail.com>
2025-09-14 19:22:48 +0900

    feat: 리플렉션 API를 통해 Handler 매핑 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +26,44 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        try {
+            Set<Class<?>> controllerClasses = getControllersWithBasePackage();
+
+            for (Class<?> controller : controllerClasses) {
+                for (Method method : controller.getMethods()) {
+                    RequestMapping annotation = method.getAnnotation(RequestMapping.class);
+                    if (annotation == null) {
+                        continue;
+                    }
+
+                    String value = annotation.value();
+                    RequestMethod[] requestMethods = annotation.method();
+                    HandlerKey handlerKey = new HandlerKey(value, requestMethods[0]);
+                    Object handler = controller.getConstructor().newInstance();
+
+                    handlerExecutions.put(handlerKey, new HandlerExecution(handler, method));
+                }
+            }
+        } catch (InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
+            log.error("Handler 매핑 중 오류가 발생했습니다.");
+            throw new RuntimeException();
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private Set<Class<?>> getControllersWithBasePackage() {
+        Reflections reflections = new Reflections(this.basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        HandlerKey handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+
+        if (!handlerExecutions.containsKey(handlerKey)) {
+            return null;
+        }
+        return handlerExecutions.get(handlerKey);
     }
 }
