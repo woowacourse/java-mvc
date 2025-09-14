6dd06aba76cfa3a635b8bb138c26ff099c6a3df1
JO-eusan <goodsan0330@gmail.com>
2025-09-14 23:07:34 +0900

    feat: URI + HTTP 메서드를 Controller와 매핑

---
+++ mvc/README.md
@@ -0,0 +1,6 @@
+# 🪐@MVC 구현하기
+
+### 1단계 - @MVC 프레임워크 구현하기
+
+- [x] @RequestMapping의 URL + HTTP 메서드에 대한 실팽 Controller 클래스를 매핑할 수 있다.
+- [x] @RequestMapping에 메서드 설정이 되어 있지 않으면 모든 HTTP 메서드를 지원해야 한다.
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,62 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        for (Object basePkg : basePackage) {
+            Set<Class<?>> controllers = findControllers(basePkg);
+            controllers.forEach(controller -> {
+                Object instance = toInstance(controller);
+                Map<HandlerKey, HandlerExecution> handlerMapping = mapHandlerMethods(instance);
+                handlerExecutions.putAll(handlerMapping);
+
+                handlerMapping.forEach((key, execution) -> log.info("Mapped {}:{}", key, execution));
+            });
+        }
+    }
+
+    private Set<Class<?>> findControllers(Object basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private Object toInstance(Class<?> controller) {
+        try {
+            return controller.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate controller: " + controller, e);
+        }
+    }
+
+    private Map<HandlerKey, HandlerExecution> mapHandlerMethods(Object controller) {
+        Map<HandlerKey, HandlerExecution> result = new HashMap<>();
+        for (Method method : controller.getClass().getDeclaredMethods()) {
+            RequestMapping rm = method.getAnnotation(RequestMapping.class);
+            if (rm != null) {
+                String url = rm.value();
+                RequestMethod[] requestMethods = getAppliedRequestMethods(rm);
+
+                for (RequestMethod requestMethod : requestMethods) {
+                    HandlerKey key = new HandlerKey(url, requestMethod);
+                    HandlerExecution execution = new HandlerExecution(controller, method);
+                    result.put(key, execution);
+                }
+            }
+        }
+        return result;
+    }
+
+    private RequestMethod[] getAppliedRequestMethods(RequestMapping rm) {
+        RequestMethod[] requestMethods = rm.method();
+        if (requestMethods.length == 0) {
+            return RequestMethod.values();
+        }
+        return requestMethods;
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String url = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey key = new HandlerKey(url, requestMethod);
+
+        return handlerExecutions.get(key);
     }
 }
