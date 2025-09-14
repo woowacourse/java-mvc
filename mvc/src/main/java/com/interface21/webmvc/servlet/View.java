2b5207fd5ef452bb461f3dcb269afd04feaa104e
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-14 21:01:29 +0900

    refactor: 인터페이스 관계 리팩터링

--- mvc/src/main/java/com/interface21/webmvc/servlet/View.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/View.java
@@ -1,11 +1,11 @@
 package com.interface21.webmvc.servlet;
 
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Map;
 
-public interface View extends Controller {
+public interface View {
+
     void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
