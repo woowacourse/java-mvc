d46c9c601610c1249e81a53a2a367964d4f0b472
kjyyjk <rlawnsdud920@gmail.com>
2025-09-14 20:48:35 +0900

    feat: 1-1 @MVC Framework 테스트 통과하기

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,22 +1,47 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> types = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : types) {
+            Method[] handlers = clazz.getMethods();
+            for (Method handler : handlers) {
+                addHandlerExecutions(clazz, handler);
+            }
+        }
+    }
+
+    private void addHandlerExecutions(Class<?> clazz, Method handler) {
+        RequestMapping mappingAnnotation = handler.getAnnotation(RequestMapping.class);
+        if (mappingAnnotation == null) {
+            return;
+        }
+        for (RequestMethod requestMethod : mappingAnnotation.method()) {
+            HandlerKey handlerKey = new HandlerKey(mappingAnnotation.value(), requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution(clazz, handler));
+        }
     }
 
     public void initialize() {
@@ -24,6 +49,9 @@ public class AnnotationHandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        String requestURI = request.getRequestURI();
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import java.lang.reflect.Method;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
 
 public class HandlerExecution {
 
+    private Class<?> clazz;
+    private Method handler;
+
+    public HandlerExecution(Class<?> clazz, Method handler) {
+        this.clazz = clazz;
+        this.handler = handler;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) handler.invoke(clazz.getConstructor().newInstance(), request, response);
     }
 }
