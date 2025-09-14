873517cc51649d4fe0dbe81f03538582121fb798
GiSeok Kang <ime.rokz@gmail.com>
2025-09-14 14:42:43 +0900

    test: Junit3Test 테스트 구현

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -9,5 +10,16 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getConstructor()
+            .newInstance();
+
+        Method test1 = clazz.getMethod("test1");
+        test1.invoke(junit3Test);
+
+        Method test2 = clazz.getMethod("test2");
+        test2.invoke(junit3Test);
+
+        Method test3 = clazz.getMethod("three");
+        test3.invoke(junit3Test);
     }
 }
