31c78d74074e536f3c1b8491e849821e8e46b17e
seaniiio <cnc4750@naver.com>
2025-09-16 01:08:31 +0900

    feat: 애노테이션을 기반으로 AnnotationHandler의 handlerExecutions 정보를 초기화하는 기능 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE, NONE;
+
+    public static RequestMethod from(final String method) {
+        return Arrays.stream(values())
+                .filter(requestMethod -> requestMethod.name().equals(method))
+                .findFirst()
+                .orElse(NONE);
+    }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,29 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        try {
+            final Reflections reflections = new Reflections(basePackage);
+            final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+            for (Class<?> controller : controllers) {
+                final Method[] methods = controller.getDeclaredMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        final RequestMapping mapping = method.getDeclaredAnnotation(RequestMapping.class);
+                        final RequestMethod[] requestMethods = mapping.method();
+                        for (RequestMethod requestMethod : requestMethods) {
+                            handlerExecutions.put(new HandlerKey(mapping.value(), requestMethod),
+                                    new HandlerExecution(controller.getDeclaredConstructor().newInstance(), method));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
