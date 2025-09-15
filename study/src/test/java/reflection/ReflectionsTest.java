e08c8097b5f92fb6badf6ce8fc3ca9267c75cf43
mimi <110226866+hongkikii@users.noreply.github.com>
2025-09-15 20:12:49 +0900

    test: reflection 패키지 테스트 수행

--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -14,5 +14,18 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final var controllerClasses = reflections.getTypesAnnotatedWith(reflection.annotation.Controller.class);
+        final var serviceClasses = reflections.getTypesAnnotatedWith(reflection.annotation.Service.class);
+        final var repositoryClasses = reflections.getTypesAnnotatedWith(reflection.annotation.Repository.class);
+
+        for (final var clazz : controllerClasses) {
+            log.debug("Controller: {}", clazz.getName());
+        }
+        for (final var clazz : serviceClasses) {
+            log.debug("Service: {}", clazz.getName());
+        }
+        for (final var clazz : repositoryClasses) {
+            log.debug("Repository: {}", clazz.getName());
+        }
     }
 }
