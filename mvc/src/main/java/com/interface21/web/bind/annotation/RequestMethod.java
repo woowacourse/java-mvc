d70d30b7dff1d06614d99be1ef56890cc76ef53b
7i.w__k <tizmfns3@gmail.com>
2025-09-15 21:56:46 +0900

    feat: @MVC 프레임워크 구현하기

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,16 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Locale;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+
+    public static RequestMethod from(String method) {
+        if (method == null || method.isBlank()) {
+            throw new IllegalArgumentException("HTTP method must not be null or empty");
+        }
+
+        return RequestMethod.valueOf(method.toUpperCase(Locale.ROOT));
+    }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,65 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        for (final Object basePackage : basePackages) {
+            scanBasePackage(basePackage);
+        }
+    }
+
+    private void scanBasePackage(Object basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+
+        for (final Class<?> aClass : reflections.getTypesAnnotatedWith(Controller.class)) {
+            for (final Method method : aClass.getDeclaredMethods()) {
+                registerHandler(aClass, method);
+            }
+        }
+    }
+
+    private void registerHandler(Class<?> aClass, Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping == null){
+            return;
+        }
+
+        final String url = requestMapping.value();
+        for (final RequestMethod requestMethod : requestMapping.method()) {
+            final HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+            final HandlerExecution handlerExecution = new HandlerExecution(aClass, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final RequestMethod method = RequestMethod.from(request.getMethod());
+        final String requestURI = request.getRequestURI();
+        final HandlerKey handlerKey = new HandlerKey(requestURI, method);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
