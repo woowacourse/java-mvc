26baaeb194595fe3186715220fad721a4d720823
kjungw <sagakkb@gmail.com>
2025-09-15 13:41:19 +0900

    feat: 리플렉션 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,39 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
+    // TODO Junit3Test에서 test로 시작하는 메소드 실행
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test instance = new Junit3Test();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // 클래스객체.getMethods()
+        // - 클래스의 모든 메서드를 가져온다.
+        Method[] methods = clazz.getMethods();
+
+        // 메서드객체.getName()
+        // - 메서드의 이름을 가져온다.
+        List<Method> testMethod = Arrays.stream(methods)
+                .filter(method -> method.getName().startsWith("test"))
+                .toList();
+
+        for(Method method : testMethod) {
+            // 메서드객체.invoke(객체인스턴스, 파라미터...)
+            // - 메서드를 실행한다.
+            // - 첫번째 인자는 메서드를 실행할 객체 인스턴스라는 점 유의하자.
+            method.invoke(instance);
+        }
     }
+
+    /*
+    ** 추가 학습 포인트
+    - 클래스객체.getMethod(메서드명, 파라미터타입...)
+    - 클래스객체.getDeclaredMethod(메서드명, 파라미터타입...)
+     */
 }
--- study/src/test/java/reflection/ReflectionTest.java
+++ study/src/test/java/reflection/ReflectionTest.java
@@ -1,15 +1,16 @@
 package reflection;
 
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class ReflectionTest {
 
@@ -19,34 +20,49 @@ class ReflectionTest {
     void givenObject_whenGetsClassName_thenCorrect() {
         final Class<Question> clazz = Question.class;
 
-        assertThat(clazz.getSimpleName()).isEqualTo("");
-        assertThat(clazz.getName()).isEqualTo("");
-        assertThat(clazz.getCanonicalName()).isEqualTo("");
+        // 클래스객체.getSimpleName()
+        // - 클래스의 이름을 가져온다.
+        assertThat(clazz.getSimpleName()).isEqualTo("Question");
+        // 클래스객체.getSimpleName()
+        // - JVM 내부에서 사용하는 클래스 이름을 가져온다. (패키지명 포함)
+        assertThat(clazz.getName()).isEqualTo("reflection.Question");
+        // 클래스객체.getCanonicalName()
+        // - JVM 내부에서 사용하는 클래스 이름의 경우 일부 알아보기 어려운 경우도 존재한다.
+        // - 그렇기에 getCanonicalName()를 활용해 사람일 읽기 좋은 형태로 클래스 이름을 가져온다. (패키지명 포함)
+        assertThat(clazz.getCanonicalName()).isEqualTo("reflection.Question");
     }
 
     @Test
     void givenClassName_whenCreatesObject_thenCorrect() throws ClassNotFoundException {
+        // Class.forName(클래스명);
+        // - 클래스 이름을 통해 클래스 객체를 가져올 수 있다.
         final Class<?> clazz = Class.forName("reflection.Question");
 
-        assertThat(clazz.getSimpleName()).isEqualTo("");
-        assertThat(clazz.getName()).isEqualTo("");
-        assertThat(clazz.getCanonicalName()).isEqualTo("");
+        assertThat(clazz.getSimpleName()).isEqualTo("Question");
+        assertThat(clazz.getName()).isEqualTo("reflection.Question");
+        assertThat(clazz.getCanonicalName()).isEqualTo("reflection.Question");
     }
 
     @Test
     void givenObject_whenGetsFieldNamesAtRuntime_thenCorrect() {
+        // clazz.getDeclaredFields() : 클래스의 모든 필드를 가져옴
+        // clazz. getFields() :public 필드만을 가져옴
+
         final Object student = new Student();
-        final Field[] fields = null;
-        final List<String> actualFieldNames = null;
+        final Field[] fields = student.getClass().getDeclaredFields();
+        final List<String> actualFieldNames = Arrays.stream(fields).map(Field::getName).toList();
 
         assertThat(actualFieldNames).contains("name", "age");
     }
 
     @Test
     void givenClass_whenGetsMethods_thenCorrect() {
+        // clazz.getDeclaredMethods() : 클래스의 모든 메서드를 가져옴
+        // clazz. getMethods() : public 메서드만을 가져옴
+
         final Class<?> animalClass = Student.class;
-        final Method[] methods = null;
-        final List<String> actualMethods = null;
+        final Method[] methods = animalClass.getDeclaredMethods();
+        final List<String> actualMethods = Arrays.stream(methods).map(Method::getName).toList();
 
         assertThat(actualMethods)
                 .hasSize(3)
@@ -55,8 +71,11 @@ class ReflectionTest {
 
     @Test
     void givenClass_whenGetsAllConstructors_thenCorrect() {
+        // clazz.getDeclaredConstructors() : 클래스의 모든 생성자를 가져옴
+        // clazz. getConstructors() : public 생성자만을 가져옴
+
         final Class<?> questionClass = Question.class;
-        final Constructor<?>[] constructors = null;
+        final Constructor<?>[] constructors = questionClass.getDeclaredConstructors();
 
         assertThat(constructors).hasSize(2);
     }
@@ -65,11 +84,16 @@ class ReflectionTest {
     void givenClass_whenInstantiatesObjectsAtRuntime_thenCorrect() throws Exception {
         final Class<?> questionClass = Question.class;
 
-        final Constructor<?> firstConstructor = null;
-        final Constructor<?> secondConstructor = null;
+        // clazz.getDeclaredConstructor(파라미터 Clazz들) : 해당 파라미터 Clazz들을 가진 생성자 탐색
+        final Constructor<?> firstConstructor = questionClass.getDeclaredConstructor(String.class, String.class,
+                String.class);
+        final Constructor<?> secondConstructor = questionClass.getDeclaredConstructor(long.class, String.class,
+                String.class, String.class, Date.class, int.class);
 
-        final Question firstQuestion = null;
-        final Question secondQuestion = null;
+        // constructure.newInstance(생성자에 넣을 값들) : 생성자로 인스턴스 생성
+        final Question firstQuestion = (Question) firstConstructor.newInstance("gugu", "제목1", "내용1");
+        final Question secondQuestion = (Question) secondConstructor.newInstance(1L, "gugu", "제목2", "내용2", new Date(),
+                3);
 
         assertThat(firstQuestion.getWriter()).isEqualTo("gugu");
         assertThat(firstQuestion.getTitle()).isEqualTo("제목1");
@@ -82,7 +106,7 @@ class ReflectionTest {
     @Test
     void givenClass_whenGetsPublicFields_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Field[] fields = null;
+        final Field[] fields = questionClass.getFields();
 
         assertThat(fields).hasSize(0);
     }
@@ -90,7 +114,7 @@ class ReflectionTest {
     @Test
     void givenClass_whenGetsDeclaredFields_thenCorrect() {
         final Class<?> questionClass = Question.class;
-        final Field[] fields = null;
+        final Field[] fields = questionClass.getDeclaredFields();
 
         assertThat(fields).hasSize(6);
         assertThat(fields[0].getName()).isEqualTo("questionId");
@@ -98,16 +122,21 @@ class ReflectionTest {
 
     @Test
     void givenClass_whenGetsFieldsByName_thenCorrect() throws Exception {
+        // clazz.getField("필드명") : 필드명으로 클래스의 public 필드 찾기
+        // clazz.getDeclaredField("필드명") : 필드명으로 클래스의 public/private 필드 찾기
         final Class<?> questionClass = Question.class;
-        final Field field = null;
+        final Field field = questionClass.getDeclaredField("questionId");
 
         assertThat(field.getName()).isEqualTo("questionId");
     }
 
     @Test
     void givenClassField_whenGetsType_thenCorrect() throws Exception {
+        // field.getClass() : 필드 객체의 Field 클래스 타입
+        // field.getDeclaringClass() : 해당 필드가 선언된 클래스 타입 리턴
+        // field.getType() : 필드의 실제 타입 리턴
         final Field field = Question.class.getDeclaredField("questionId");
-        final Class<?> fieldClass = null;
+        final Class<?> fieldClass = field.getType();
 
         assertThat(fieldClass.getSimpleName()).isEqualTo("long");
     }
@@ -115,15 +144,17 @@ class ReflectionTest {
     @Test
     void givenClassField_whenSetsAndGetsValue_thenCorrect() throws Exception {
         final Class<?> studentClass = Student.class;
-        final Student student = null;
-        final Field field = null;
+        final Student student = (Student) studentClass.getDeclaredConstructor().newInstance();
+        final Field field = studentClass.getDeclaredField("age");
 
-        // todo field에 접근 할 수 있도록 만든다.
+        // private 필드에 접근이 가능하도록 한다.
+        field.setAccessible(true);
 
         assertThat(field.getInt(student)).isZero();
         assertThat(student.getAge()).isZero();
 
-        field.set(null, null);
+        // 특정 인스턴스의 필드값을 변경한다.
+        field.set(student, 99);
 
         assertThat(field.getInt(student)).isEqualTo(99);
         assertThat(student.getAge()).isEqualTo(99);
