d0e848e1a161cd1568456089bd120e44ae12c03b
ykmxxi <ykmxxi97@gmail.com>
2025-09-15 22:14:08 +0900

    test: Reflection API 학습 테스트

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -8,6 +10,13 @@ class Junit3TestRunner {
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        // Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                Junit3Test junit3Test = new Junit3Test();
+                method.invoke(junit3Test);
+            }
+        }
     }
 }
