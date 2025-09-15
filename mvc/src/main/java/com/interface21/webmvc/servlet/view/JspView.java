4d2c654142358d99fbc222aaec6a55d6a06d2e94
(minjae8563) <(minjae8563@naver.com)>
2025-09-15 21:53:50 +0900

    refactor: early return 하도록 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -25,6 +25,7 @@ public class JspView implements View {
         // todo
         if (viewName.startsWith(REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(REDIRECT_PREFIX.length()));
+            return;
         }
 
         model.keySet().forEach(key -> {
