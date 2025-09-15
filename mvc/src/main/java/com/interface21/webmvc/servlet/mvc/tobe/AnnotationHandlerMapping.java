39eb5df04e52ef75c37ff3579ef3f3d1c39d28b1
eueo8259 <162389416+eueo8259@users.noreply.github.com>
2025-09-15 21:53:30 +0900

    [1단계 - @MVC 프레임워크 구현하기] 다로(이정민) 미션 제출합니다.  (#900)
    * test: 리플렉션 퀘스트
    
    * feat: AnnotationHandlerMapping 기능 구현
    
    * feat: ModelAndView handle 기능 구현
    
    * feat: DispatcherServlet move()기능 JspView로 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,13 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,10 +27,44 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (Class<?> clazz : controllerClasses) {
+            Object controllerInstance = createControllerInstance(clazz);
+            registerController(clazz, controllerInstance);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private Object createControllerInstance(Class<?> clazz) {
+        Object controllerInstance;
+        try {
+            controllerInstance = clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        return controllerInstance;
+    }
+
+    private void registerController(Class<?> clazz, Object controllerInstance) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(RequestMapping.class)){
+                RequestMapping mapping = method.getAnnotation(RequestMapping.class);
+                for (RequestMethod requestMethod : mapping.method()) {
+                    handlerExecutions.put(
+                            new HandlerKey(mapping.value(), requestMethod),
+                            new HandlerExecution(controllerInstance, method)
+                    );
+                }
+            }
+        }
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestPath = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestPath, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- study/src/test/java/reflection/ReflectionsTest.java
+++ study/src/test/java/reflection/ReflectionsTest.java
@@ -1,9 +1,14 @@
 package reflection;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reflection.annotation.Controller;
+import reflection.annotation.Repository;
+import reflection.annotation.Service;
 
 class ReflectionsTest {
 
@@ -14,5 +19,15 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("reflection.examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> annotatedClasses = new HashSet<>();
+
+        // 각 애노테이션으로 클래스 검색 및 결과 합치기
+        annotatedClasses.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        annotatedClasses.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        annotatedClasses.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        // 찾은 클래스 목록 출력
+        annotatedClasses.forEach(clazz -> System.out.println(clazz.getName()));
+
     }
 }
