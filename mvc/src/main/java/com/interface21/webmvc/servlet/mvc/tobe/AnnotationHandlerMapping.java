0a3dd0ca71dcc898cd241e1e571f606631626b34
seaniiio <cnc4750@naver.com>
2025-09-16 01:31:41 +0900

    refactor: 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -26,29 +26,49 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        try {
-            final Reflections reflections = new Reflections(basePackage);
-            final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
-            for (Class<?> controller : controllers) {
-                final Method[] methods = controller.getDeclaredMethods();
-                for (Method method : methods) {
-                    if (method.isAnnotationPresent(RequestMapping.class)) {
-                        final RequestMapping mapping = method.getDeclaredAnnotation(RequestMapping.class);
-                        final RequestMethod[] requestMethods = mapping.method();
-                        for (RequestMethod requestMethod : requestMethods) {
-                            handlerExecutions.put(new HandlerKey(mapping.value(), requestMethod),
-                                    new HandlerExecution(controller.getDeclaredConstructor().newInstance(), method));
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-
-        }
+        final Reflections reflections = new Reflections(basePackage);
+        scanControllers(reflections);
     }
 
     public Object getHandler(final HttpServletRequest request) {
         final HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
         return handlerExecutions.get(handlerKey);
+    }
+
+    private void scanControllers(final Reflections reflections) {
+        final Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            scanRequestMappingMethods(controller);
+        }
+    }
+
+    private void scanRequestMappingMethods(final Class<?> controller) {
+        final Method[] methods = controller.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(RequestMapping.class)) {
+                final RequestMapping mapping = method.getDeclaredAnnotation(RequestMapping.class);
+                final RequestMethod[] requestMethods = mapping.method();
+                addHandlerExecutions(controller, method, requestMethods, mapping);
+            }
+        }
+    }
+
+    private void addHandlerExecutions(final Class<?> controller, final Method method,
+                                      final RequestMethod[] requestMethods, final RequestMapping mapping) {
+        for (RequestMethod requestMethod : requestMethods) {
+            addHandlerExecution(controller, method, mapping, requestMethod);
+        }
+    }
+
+    private void addHandlerExecution(final Class<?> controller, final Method method, final RequestMapping mapping,
+                                     final RequestMethod requestMethod) {
+        try {
+            final HandlerKey handlerKey = new HandlerKey(mapping.value(), requestMethod);
+            final Object controllerInstance = controller.getDeclaredConstructor().newInstance();
+            final HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        } catch (Exception e) {
+            log.warn("controller 인스턴스 생성 실패");
+        }
     }
 }
