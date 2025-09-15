a1426b6bf85de1329ea04f0a72a2e2b7110211fb
ppparkta <pppppark2@gmail.com>
2025-09-15 22:41:02 +0900

    test: 이해 돕기 위한 주석 추가

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -7,13 +7,16 @@ class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
+        // 클래스 객체 생성
         Class<Junit3Test> clazz = Junit3Test.class;
-        // Junit3Test에서 test로 시작하는 메소드 실행
+        // 인스턴스 생성
         Object testInstance = clazz.getDeclaredConstructor().newInstance();
 
+        // 클래스 객체로부터 메서드 정보 추출
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
             if (method.getName().startsWith("test")) {
+                // 메서드 실행 (invoke(instance))
                 method.invoke(testInstance);
             }
         }
