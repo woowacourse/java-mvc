39bcbd92239ab45ec1da069a45c35466d1378726
supernovaMK <minkikim2002@gmail.com>
2025-09-16 00:30:06 +0900

    feat: AnnotationHandlerMapping 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,47 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        for (var pack : basePackage) {
+            Reflections reflections = new Reflections(pack.toString());
+            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+            for (Class<?> controller : controllers) {
+                Object controllerInstance;
+                try {
+                    controllerInstance = controller.getConstructor().newInstance();
+                } catch (Exception e) {
+                    throw new IllegalArgumentException("excetion occured" + e.getMessage());
+                }
+
+                Method[] methods = controller.getDeclaredMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                        String url = requestMapping.value();
+                        RequestMethod[] requestMethods = requestMapping.method();
+
+                        for (var requestMethod : requestMethods) {
+                            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                            HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+
+                            handlerExecutions.put(handlerKey, handlerExecution);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String url = request.getRequestURI();
+        String method = request.getMethod();
+        RequestMethod requestMethod = RequestMethod.getRequestMethod(method);
+        HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+
+        HandlerExecution handlerExecution = handlerExecutions.get(handlerKey);
+        if (handlerExecution == null) {
+            throw new IllegalArgumentException("handler not found");
+        }
+        return handlerExecution;
     }
 }
