737eb6507537b8d53b1583a6fe3275b98391fb64
supernovaMK <minkikim2002@gmail.com>
2025-09-16 00:30:37 +0900

    feat: RequestMethod 메서드 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,14 @@
 package com.interface21.web.bind.annotation;
 
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod getRequestMethod(String method) {
+        for (var m : RequestMethod.values()) {
+            if (m.name().equals(method)) {
+                return m;
+            }
+        }
+        throw new IllegalArgumentException("method does no found");
+    }
 }
