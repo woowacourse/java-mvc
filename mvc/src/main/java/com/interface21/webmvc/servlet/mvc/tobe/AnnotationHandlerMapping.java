403395f1740bd3f66fa4956cd56c3fdefdbbb130
MinJun Choi <mj043000@naver.com>
2025-09-15 21:27:42 +0900

    feat: ControllerScanner 및 HandlerMappingRegistry 추가
    - `ControllerScanner` 클래스 추가: 지정된 패키지의 `@Controller` 클래스를 스캔하고 인스턴스 생성
    - `HandlerMappingRegistry` 클래스 추가: `@RequestMapping` 메서드 기반으로 핸들러 매핑 등록 및 관리
    - `AnnotationHandlerMapping` 클래스 리팩토링: `ControllerScanner`와 `HandlerMappingRegistry` 활용하도록 수정
    - `ReflectionUtils` 클래스 확장: 어노테이션 필터를 기반으로 메서드 찾기 지원

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,156 +1,71 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
-import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.mvc.HandlerMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.util.*;
+import java.util.Map;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
-    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final String basePackage;
+    private final ControllerScanner controllerScanner;
+    private final HandlerMappingRegistry handlerRegistry;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
+    public AnnotationHandlerMapping(final String basePackage) {
         this.basePackage = basePackage;
-        this.handlerExecutions = new HashMap<>();
+        this.controllerScanner = new ControllerScanner(basePackage);
+        this.handlerRegistry = new HandlerMappingRegistry();
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        log.info("Initializing AnnotationHandlerMapping for package: {}", basePackage);
         
-        for (final Object pkg : basePackage) {
-            final String packageName = pkg.toString();
-            log.info("Scanning package: {}", packageName);
-            scanPackage(packageName);
-        }
+        final Map<Class<?>, Object> controllers = controllerScanner.scanAndCreateControllers();
         
-        log.info("Total handlers registered: {}", handlerExecutions.size());
-        handlerExecutions.keySet().forEach(key -> 
-            log.info("Registered handler: {}", key));
-    }
-
-    private void scanPackage(final String packageName) {
-        try {
-            final String path = packageName.replace('.', '/');
-            final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            final Enumeration<URL> resources = classLoader.getResources(path);
-            
-            while (resources.hasMoreElements()) {
-                final URL resource = resources.nextElement();
-                final File file = new File(resource.getFile());
-                
-                if (file.exists() && file.isDirectory()) {
-                    scanDirectory(file, packageName);
-                }
-            }
-        } catch (final Exception e) {
-            log.error("Package scan failed: {}", packageName, e);
-        }
-    }
-
-    private void scanDirectory(final File directory, final String packageName) {
-        final File[] files = directory.listFiles();
-        if (files == null) return;
-        
-        for (final File file : files) {
-            processDirectoryFile(file, packageName);
-        }
-    }
-
-    private void processDirectoryFile(final File file, final String packageName) {
-        if (file.isDirectory()) {
-            scanDirectory(file, packageName + "." + file.getName());
+        if (controllers.isEmpty()) {
+            log.warn("No @Controller classes found in package: {}", basePackage);
             return;
         }
         
-        if (file.getName().endsWith(".class")) {
-            final String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
-            processClass(className);
-        }
-    }
-
-    private void processClass(final String className) {
-        try {
-            final Class<?> clazz = Class.forName(className);
-            
-            if (isController(clazz)) {
-                registerController(clazz);
-            }
-        } catch (final ClassNotFoundException e) {
-            log.debug("Class not found: {}", className);
-        } catch (final Exception e) {
-            log.warn("Failed to process class: {}", className, e);
-        }
-    }
-
-    private boolean isController(final Class<?> clazz) {
-        return clazz.isAnnotationPresent(Controller.class);
-    }
-
-    private void registerController(final Class<?> controllerClass) {
-        try {
-            final Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
-            
-            for (final Method method : controllerClass.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(RequestMapping.class)) {
-                    registerHandlerMethod(controllerInstance, method);
-                }
-            }
-        } catch (final Exception e) {
-            log.error("Failed to register controller: {}", controllerClass.getName(), e);
-        }
-    }
-
-    private void registerHandlerMethod(final Object controllerInstance, final Method method) {
-        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-        final String url = requestMapping.value();
-        final RequestMethod[] methods = requestMapping.method();
+        handlerRegistry.registerControllers(controllers);
         
-        final RequestMethod[] targetMethods = determineTargetMethods(methods);
-        registerHandlerForMethods(controllerInstance, method, url, targetMethods);
-        
-        log.debug("Registered handler: {} -> {}.{}", url, controllerInstance.getClass().getSimpleName(), method.getName());
+        log.info("AnnotationHandlerMapping initialization completed. Total handlers: {}", 
+            handlerRegistry.size());
     }
 
-    private RequestMethod[] determineTargetMethods(final RequestMethod[] methods) {
-        return methods.length == 0 ? RequestMethod.values() : methods;
-    }
-
-    private void registerHandlerForMethods(final Object controllerInstance, final Method method, final String url, final RequestMethod[] targetMethods) {
-        for (final RequestMethod requestMethod : targetMethods) {
-            final HandlerKey key = new HandlerKey(url, requestMethod);
-            final HandlerExecution execution = new HandlerExecution(controllerInstance, method);
-            handlerExecutions.put(key, execution);
-        }
-    }
-
+    @Override
     public Object getHandler(final HttpServletRequest request) {
-        final HandlerKey key = createHandlerKey(request);
-        if (key == null) {
+        final HandlerKey handlerKey = createHandlerKey(request);
+        
+        if (handlerKey == null) {
+            log.debug("Could not create HandlerKey for request: {} {}", 
+                request.getMethod(), request.getRequestURI());
             return null;
         }
         
-        return handlerExecutions.get(key);
+        final HandlerExecution handlerExecution = handlerRegistry.getHandlerExecution(handlerKey);
+        
+        if (handlerExecution == null) {
+            log.debug("No handler found for: {}", handlerKey);
+        }
+        
+        return handlerExecution;
     }
 
     private HandlerKey createHandlerKey(final HttpServletRequest request) {
         final String uri = request.getRequestURI();
-        final String httpMethod = request.getMethod();
+        final String httpMethodName = request.getMethod();
         
         try {
-            final RequestMethod requestMethod = RequestMethod.valueOf(httpMethod);
+            final RequestMethod requestMethod = RequestMethod.valueOf(httpMethodName);
             return new HandlerKey(uri, requestMethod);
         } catch (final IllegalArgumentException e) {
-            log.warn("Unsupported HTTP method: {}", httpMethod);
+            log.warn("Unsupported HTTP method: {}", httpMethodName);
             return null;
         }
     }
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerMappingRegistry.java
@@ -0,0 +1,86 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.core.util.ReflectionUtils;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HandlerMappingRegistry {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerMappingRegistry.class);
+
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public HandlerMappingRegistry() {
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void registerControllers(final Map<Class<?>, Object> controllers) {
+        for (final Entry<Class<?>, Object> entry : controllers.entrySet()) {
+            final Class<?> controllerClass = entry.getKey();
+            final Object controllerInstance = entry.getValue();
+
+            registerControllerMethods(controllerClass, controllerInstance);
+        }
+
+        logRegisteredHandlers();
+    }
+
+    private void registerControllerMethods(final Class<?> controllerClass, final Object controllerInstance) {
+        final Set<Method> requestMappingMethods = findRequestMappingMethods(controllerClass);
+
+        for (final Method method : requestMappingMethods) {
+            registerHandlerMethod(controllerInstance, method);
+        }
+    }
+
+    private Set<Method> findRequestMappingMethods(final Class<?> controllerClass) {
+        return ReflectionUtils.getAllMethods(controllerClass, ReflectionUtils.withAnnotation(RequestMapping.class));
+    }
+
+    private void registerHandlerMethod(final Object controllerInstance, final Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+
+        final String url = requestMapping.value();
+        final RequestMethod[] httpMethods = getHttpMethods(requestMapping);
+
+        for (final RequestMethod httpMethod : httpMethods) {
+            final HandlerKey handlerKey = new HandlerKey(url, httpMethod);
+            final HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
+
+            log.debug("Registered handler: {} {} -> {}.{}",
+                    httpMethod, url, controllerInstance.getClass().getSimpleName(), method.getName());
+        }
+    }
+
+    private RequestMethod[] getHttpMethods(final RequestMapping requestMapping) {
+        final RequestMethod[] methods = requestMapping.method();
+        return methods.length == 0 ? RequestMethod.values() : methods;
+    }
+
+    private void logRegisteredHandlers() {
+        log.info("Total handlers registered: {}", handlerExecutions.size());
+        handlerExecutions.keySet().forEach(key ->
+                log.debug("Handler registered: {}", key));
+    }
+
+    public HandlerExecution getHandlerExecution(final HandlerKey handlerKey) {
+        return handlerExecutions.get(handlerKey);
+    }
+
+    public boolean isEmpty() {
+        return handlerExecutions.isEmpty();
+    }
+
+    public int size() {
+        return handlerExecutions.size();
+    }
+}
