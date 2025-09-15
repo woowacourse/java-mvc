a57f493ce67722b5a64576fdb28235b9604813f2
byesol <bsj5967@naver.com>
2025-09-16 03:02:24 +0900

    feat: 컨트롤러 스캔 및 핸들러 정보 추출 기능 구현
    -  리플렉션 라이브러리를 사용해 컨트롤러 스캔해서 인스턴스 및 메소드 정보 조회
    - @RequestMapping 애노테이션에서 URL, HTTP Method 추출해 HandlerKey 생성

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,6 +26,27 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> controller : controllers) {
+            try {
+                Method[] methods = controller.getDeclaredMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                        String url = requestMapping.value();
+                        RequestMethod[] requestMethods = requestMapping.method();
+                        for (RequestMethod requestMethod : requestMethods) {
+                            HandlerKey handlerKey = new HandlerKey(url, requestMethod);
+                            //TODO handlerExecutions 에 등록하기
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                log.error("컨트롤러 시작 에러" + controller.getName(), e);
+            }
+
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
