cf59d37cfce8a59719fb5100de943deedb802baa
junhaa <wooha34567@gmail.com>
2025-09-15 21:41:20 +0900

    refactor: 리플렉션 예외 처리 로직 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -58,16 +58,26 @@ public class AnnotationHandlerMapping {
     private Object createControllerInstance(Class<?> controllerClass) {
         try {
             return controllerClass.getConstructor().newInstance();
-        } catch (InstantiationException | InvocationTargetException e) {
-            log.error("Controller 인스턴스 생성에 실패했습니다: {}", controllerClass.getName());
-            throw new IllegalStateException("Controller 인스턴스 생성에 실패했습니다: " + controllerClass.getName(), e);
-        } catch (IllegalAccessException e) {
-            log.error("메서드에 대한 접근 권한이 없습니다.");
-            throw new IllegalStateException("메서드에 대한 접근 권한이 없습니다.", e);
-        } catch (NoSuchMethodException e) {
-            log.error("기본 생성자를 찾을 수 없습니다: {}", controllerClass.getName());
-            throw new IllegalStateException("기본 생성자를 찾을 수 없습니다: " + controllerClass.getName(), e);
+        } catch (ReflectiveOperationException ex) {
+            handleReflectionException(ex, controllerClass);
+            return null;
         }
+    }
+
+    private void handleReflectionException(ReflectiveOperationException ex, Class<?> controllerClass) {
+        if(ex instanceof InstantiationException || ex instanceof InvocationTargetException){
+            log.error("Controller 인스턴스 생성에 실패했습니다: {}", controllerClass.getName());
+            throw new IllegalStateException("Controller 인스턴스 생성에 실패했습니다: " + controllerClass.getName(), ex);
+        }
+        if(ex instanceof IllegalAccessException) {
+            log.error("메서드에 대한 접근 권한이 없습니다.");
+            throw new IllegalStateException("메서드에 대한 접근 권한이 없습니다.", ex);
+        }
+        if(ex instanceof NoSuchMethodException) {
+            log.error("기본 생성자를 찾을 수 없습니다: {}", controllerClass.getName());
+            throw new IllegalStateException("기본 생성자를 찾을 수 없습니다: " + controllerClass.getName(), ex);
+        }
+        throw new IllegalStateException("알 수 없는 Reflection 예외가 발생했습니다.", ex);
     }
 
     private void registerHandlerExecution(Method handlerMethod, Object controller) {
