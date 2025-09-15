6d1e6000474d2b7eab29a0b578acea0e8faba9aa
AppleMint98 <applemint9821@gmail.com>
2025-09-15 15:36:42 +0900

    feat: AnnotationHandlerMapping, HandlerExecution 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,29 +1,57 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
+        for (Object basePackage :basePackages){
+            Reflections reflections = new Reflections(basePackage);
+            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+            for (Class<?> controller : controllers) {
+                Method[] declaredMethods = controller.getDeclaredMethods();
+                for (Method declaredMethod : declaredMethods) {
+                    if (declaredMethod.isAnnotationPresent(RequestMapping.class)) {
+                        RequestMapping annotation = declaredMethod.getAnnotation(RequestMapping.class);
+                        RequestMethod[] methods = annotation.method();
+                        String url = annotation.value();
+                        if (methods.length == 0) {
+                            methods = RequestMethod.values();
+                        }
+                        for (RequestMethod method : methods) {
+                            HandlerKey handlerKey = new HandlerKey(url, method);
+                            handlerExecutions.put(handlerKey, new HandlerExecution());
+                        }
+                    }
+                }
+            }
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        return handlerExecutions.get(new HandlerKey(requestURI, requestMethod));
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,24 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JsonView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.util.Enumeration;
 
 public class HandlerExecution {
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+
+        Enumeration<String> attributes = request.getAttributeNames();
+        while (attributes.hasMoreElements()) {
+            final String name = attributes.nextElement();
+            final Object value = request.getAttribute(name);
+            modelAndView.addObject(name, value);
+        }
+
+        return modelAndView;
     }
 }
