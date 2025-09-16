5ebfb5b182157c5c5bd1b926cda9c1bfa56e668f
yesjuhee <juhee0924@g.skku.edu>
2025-09-16 17:34:51 +0900

    refactor: 리팩토링 및 AnnotationHandlerMapping 초기화 로직 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -6,6 +6,7 @@ import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.reflections.Reflections;
@@ -20,50 +21,61 @@ public class AnnotationHandlerMapping {
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage; // 컨트롤러가 위치한 패키지 이름
+        this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!"); // 핸들러 싹 다 가져오기
-        try {
-            var reflections = new Reflections(basePackage);
-            var controllers = reflections.getTypesAnnotatedWith(Controller.class);
-            for (Class<?> controller : controllers) { // @Controller 붙어있는 클래스
-                for (Method method : controller.getMethods()) { // public 메서드
-                    if (method.isAnnotationPresent(RequestMapping.class)) { // @RequestMapping 붙어있는 메서드
-                        var annotation = method.getAnnotation(RequestMapping.class);
-                        var value = annotation.value();
-                        var httpMethods = annotation.method();
-                        // HandlerKey 생성
-                        // method 없으면 모든 메서드 지원
-                        if (httpMethods.length == 0) {
-                            for (RequestMethod requestMethod : RequestMethod.values()) {
-                                var handlerKey = new HandlerKey(value, requestMethod);
-                                Constructor<?> constructor = controller.getDeclaredConstructor();
-                                handlerExecutions.put(handlerKey,
-                                        new HandlerExecution(constructor.newInstance(), method));
-                            }
-                        } else {
-                            for (RequestMethod httpMethod : httpMethods) {
-                                var handlerKey = new HandlerKey(value, httpMethod);
-                                Constructor<?> constructor = controller.getDeclaredConstructor();
-                                handlerExecutions.put(handlerKey,
-                                        new HandlerExecution(constructor.newInstance(), method));
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
+        log.info("Initialized AnnotationHandlerMapping!");
+        final var controllers = new Reflections(basePackage).getTypesAnnotatedWith(Controller.class);
+        controllers.forEach(controller -> Arrays.stream(controller.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .forEach(requestMappingMethod -> {
+                    var handlerExecution = createHandlerExecution(controller, requestMappingMethod);
+                    var requestMappingAnnotation = requestMappingMethod.getAnnotation(RequestMapping.class);
+                    extractHandlerExecutions(handlerExecution, requestMappingAnnotation);
+                })
+        );
+    }
 
+    public Object getHandler(final HttpServletRequest request) {
+        final var requestURI = request.getRequestURI();
+        final var method = request.getMethod();
+        final var handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private HandlerExecution createHandlerExecution(final Class<?> controller, final Method method) {
+        try {
+            final Constructor<?> constructor = controller.getDeclaredConstructor();
+            return new HandlerExecution(constructor.newInstance(), method);
+        } catch (final Exception e) {
+            log.error("Exception : {}", e.getMessage(), e);
+            throw new RuntimeException(e);
         }
     }
 
-    public Object getHandler(final HttpServletRequest request) { // 여기서 원하는 컨트롤러 찾아주기
-        var requestURI = request.getRequestURI();
-        var method = request.getMethod();
-        var handlerKey = new HandlerKey(requestURI, RequestMethod.valueOf(method));
-        return handlerExecutions.get(handlerKey);
+    private void extractHandlerExecutions(
+            final HandlerExecution handlerExecution,
+            final RequestMapping requestMappingMethod
+    ) {
+        try {
+            final var value = requestMappingMethod.value();
+            final var httpMethods = requestMappingMethod.method();
+            if (httpMethods.length == 0) {
+                for (RequestMethod requestMethod : RequestMethod.values()) {
+                    final var handlerKey = new HandlerKey(value, requestMethod);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                }
+                return;
+            }
+            for (RequestMethod requestMethod : httpMethods) {
+                final var handlerKey = new HandlerKey(value, requestMethod);
+                handlerExecutions.put(handlerKey, handlerExecution);
+            }
+        } catch (final Exception e) {
+            log.error("Exception : {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
