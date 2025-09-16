a555b0e2607a06d3b6c1a0f76bb5e6a72ab59ae9
bowook <mbosx316@naver.com>
2025-09-16 23:28:26 +0900

    test: Junit4TestRunner 학습

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +8,13 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        for (Method method : junit4Test.getClass().getMethods()) {
+            //isAnnotationPresent의 파라미터 타입이 class네, 그러면 어노테이션의 클래스를 전달해ㅐ야함
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
