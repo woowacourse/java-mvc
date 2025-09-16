69945c456385db3ff5a1b2ec549f5fbbb9dda0eb
bowook <mbosx316@naver.com>
2025-09-17 01:35:41 +0900

    feat: RequestMethod 문자열 파싱 구현

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,22 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE,
+    ;
+
+    public static RequestMethod from(final String requestMethod) {
+        return Arrays.stream(RequestMethod.values())
+                .filter(value -> value.name().equals(requestMethod))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("지원하는 HTTP 메서드가 없습니다."));
+    }
 }
