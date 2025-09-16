b3bbd7b0b7dfb23477d5a927ce6624c3a67141c7
minSsan <msp1125@hanyang.ac.kr>
2025-09-16 17:59:21 +0900

    feat: `@Controller`, `@RequestMapping` 리플렉션을 통해 AnnotationHandlerMapping 필드 값 초기화

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,19 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +28,60 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        setHandlerExecutions();
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void setHandlerExecutions() {
+        handlerExecutions.clear();
+
+        final Set<Class<?>> controllers = findControllerTypes();
+        for (Class<?> controller : controllers) {
+            registerControllerMappings(controller);
+        }
+    }
+
+    private Set<Class<?>> findControllerTypes() {
+        final Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerControllerMappings(Class<?> controller) {
+        final List<Method> methods = findRequestMappingMethods(controller);
+        for (Method method : methods) {
+            registerMethodMapping(method);
+        }
+    }
+
+    private List<Method> findRequestMappingMethods(Class<?> controller) {
+        return Arrays.stream(controller.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void registerMethodMapping(Method method) {
+        final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        final String path = requestMapping.value();
+        final RequestMethod[] requestMethods = resolveRequestMethods(requestMapping);
+
+        for (RequestMethod requestMethod : requestMethods) {
+            final HandlerKey handlerKey = new HandlerKey(path, requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution());
+        }
+    }
+
+    private RequestMethod[] resolveRequestMethods(RequestMapping requestMapping) {
+        final RequestMethod[] requestMethods = requestMapping.method();
+        if (requestMethods.length == 0) {
+            return RequestMethod.values();
+        }
+        return requestMethods;
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final String requestURI = request.getRequestURI();
+        final String requestMethod = request.getMethod();
+        final HandlerKey key = new HandlerKey(requestURI, RequestMethod.valueOf(requestMethod.toUpperCase()));
+        return handlerExecutions.get(key);
     }
 }
