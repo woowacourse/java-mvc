862f26f3ba99984e5ab41419f9cf9633e95cf4d3
HyeonsuLee <leehyeonsu4888@naver.com>
2025-09-16 19:32:35 +0900

    feat: step1 작성

--- study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
+++ study/src/main/java/servlet/com/example/CharacterEncodingFilter.java
@@ -8,9 +8,15 @@ import java.io.IOException;
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
+    private static final String DEFAULT_ENCODING = "UTF-8";
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.getServletContext().log("doFilter() 호출");
+        request.setCharacterEncoding(DEFAULT_ENCODING);
+        response. setCharacterEncoding(DEFAULT_ENCODING);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain; charset=UTF-8");
         chain.doFilter(request, response);
     }
 }
