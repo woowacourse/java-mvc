db772e0459cf348dfe6124bfd6881982dd6a5129
kimdev <kim__dev@hufs.ac.kr>
2025-09-17 00:13:35 +0900

    refactor: AnnotationHandlerMapping 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -26,38 +26,59 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Set<Class<?>> controllers = scanControllers();
+        registerHandlers(controllers);
+    }
+
+    private Set<Class<?>> scanControllers() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlers(Set<Class<?>> controllers) {
         try {
-            Reflections reflections = new Reflections(basePackage);
-            Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
             for (Class<?> controller : controllers) {
                 Object controllerInstance = controller.getConstructor().newInstance();
                 Method[] controllerMethods = controller.getMethods();
-                for (Method controllerMethod : controllerMethods) {
-                    if (controllerMethod.isAnnotationPresent(RequestMapping.class)) {
-                        RequestMapping requestMappingAnnotation = controllerMethod.getAnnotation(
-                            RequestMapping.class);
-                        RequestMethod[] httpMethods = requestMappingAnnotation.method();
-                        String path = requestMappingAnnotation.value();
-                        if (httpMethods.length == 0) {
-                            for (RequestMethod httpMethod : RequestMethod.values()) {
-                                HandlerKey handlerKey = new HandlerKey(path, httpMethod);
-                                HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, controllerMethod);
-                                this.handlerExecutions.put(handlerKey, handlerExecution);
-                                System.out.println(path + " " + httpMethod);
-                            }
-                        }
-                        for (RequestMethod httpMethod : httpMethods) {
-                            HandlerKey handlerKey = new HandlerKey(path, httpMethod);
-                            HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, controllerMethod);
-                            this.handlerExecutions.put(handlerKey, handlerExecution);
-                            System.out.println(path + " " + httpMethod);
-                        }
-                    }
-                }
+                registerControllerMethods(controllerInstance, controllerMethods);
             }
         } catch (Exception exception) {
-            log.error("알 수 없는 오류가 발생했습니다.");
+            log.error("알 수 없는 오류가 발생했습니다: " + exception.getMessage());
         }
+    }
+
+    private void registerControllerMethods(Object controllerInstance, Method[] controllerMethods) {
+        for (Method controllerMethod : controllerMethods) {
+            if (controllerMethod.isAnnotationPresent(RequestMapping.class)) {
+                registerHandler(controllerInstance, controllerMethod);
+            }
+        }
+    }
+
+    private void registerHandler(Object controllerInstance, Method controllerMethod) {
+        RequestMapping requestMappingAnnotation = controllerMethod.getAnnotation(RequestMapping.class);
+        RequestMethod[] httpMethods = requestMappingAnnotation.method();
+        String path = requestMappingAnnotation.value();
+
+        if (httpMethods.length == 0) {
+            registerHandlerWithAllHttpMethods(controllerInstance, controllerMethod, path);
+            return;
+        }
+        for (RequestMethod httpMethod : httpMethods) {
+            putHandler(path, httpMethod, controllerInstance, controllerMethod);
+        }
+    }
+
+    private void registerHandlerWithAllHttpMethods(Object controllerInstance, Method controllerMethod, String path) {
+        for (RequestMethod httpMethod : RequestMethod.values()) {
+            putHandler(path, httpMethod, controllerInstance, controllerMethod);
+        }
+    }
+
+    private void putHandler(String path, RequestMethod httpMethod, Object controllerInstance, Method controllerMethod) {
+        HandlerKey handlerKey = new HandlerKey(path, httpMethod);
+        HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, controllerMethod);
+        this.handlerExecutions.put(handlerKey, handlerExecution);
     }
 
     public HandlerExecution getHandler(final HttpServletRequest request) {
