cd8a38b9a4ea4529e41222f14f10b179348bf1a8
eunseongu <rndmstjs75@gmail.com>
2025-09-16 14:50:31 +0900

    feat: @MVC Framework 테스트 통과하도록 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +25,64 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Set<Class<?>> controllers = findControllers();
+        registerHandlerExecutions(controllers);
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void registerHandlerExecutions(Set<Class<?>> controllers) {
+        for (Class<?> controller : controllers) {
+            try {
+                Object controllerInstance = controller.getDeclaredConstructor().newInstance();
+                addHandlers(controllerInstance);
+            } catch (Exception e) {
+                log.error("Failed to instantiate controller: {}", controller, e);
+            }
+        }
+    }
+
+    private Set<Class<?>> findControllers() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void addHandlers(Object controllerInstance) {
+        Method[] methods = controllerInstance.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            addHandler(controllerInstance, method);
+        }
+    }
+
+    private void addHandler(Object controllerInstance, Method method) {
+        if (!method.isAnnotationPresent(RequestMapping.class)) {
+            return;
+        }
+
+        RequestMapping mapping = method.getAnnotation(RequestMapping.class);
+        String uri = mapping.value();
+        RequestMethod[] requestMethods = getRequestMethods(mapping);
+
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+            handlerExecutions.put(handlerKey, handlerExecution);
+            log.info("Mapped [{}] {} -> {}", requestMethod, uri, method);
+        }
+    }
+
+    private RequestMethod[] getRequestMethods(final RequestMapping mapping) {
+        RequestMethod[] requestMethods = mapping.method();
+        if (requestMethods.length == 0) {
+            return RequestMethod.values();
+        }
+        return requestMethods;
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String method = request.getMethod();
+        RequestMethod requestMethod = RequestMethod.valueOf(method);
+        String uri = request.getRequestURI();
+        HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,27 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+    private final Object handler;
+    private final Method method;
+
+    public HandlerExecution(Object handler, Method method) {
+        this.handler = handler;
+        this.method = method;
+    }
+
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object result = method.invoke(handler, request, response);
+
+        if (result instanceof ModelAndView modelAndView) {
+            return modelAndView;
+        }
+        return new ModelAndView(new JspView(""));
     }
 }
