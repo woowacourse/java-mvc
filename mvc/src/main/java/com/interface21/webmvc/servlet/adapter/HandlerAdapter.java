06ff3c8a58a6b7105fbc0891f85b15a414353246
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-16 21:20:06 +0900

    feat: HandlerAdapter 인터페이스 구현 및 구현체 적용

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/adapter/HandlerAdapter.java
@@ -0,0 +1,10 @@
+package com.interface21.webmvc.servlet.adapter;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface HandlerAdapter {
+    boolean support(Object handler);
+    ModelAndView handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception;
+}
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/adapter/ManualHandlerAdapter.java
@@ -0,0 +1,19 @@
+package com.interface21.webmvc.servlet.adapter;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ManualHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean support(Object handler) {
+        return handler instanceof HandlerExecution;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+        return ((HandlerExecution) handler).handle(req, resp);
+    }
+}
