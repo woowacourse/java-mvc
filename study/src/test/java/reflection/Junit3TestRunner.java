11f89822c5300be883cc70cea7238384c713fad9
minSsan <msp1125@hanyang.ac.kr>
2025-09-16 22:13:33 +0900

    feat: 학습 테스트 진행

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -2,6 +2,10 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 class Junit3TestRunner {
 
     @Test
@@ -9,5 +13,12 @@ class Junit3TestRunner {
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .toList();
+
+        for (Method method : methods) {
+            method.invoke(new Junit3Test());
+        }
     }
 }
