75dde9bfd81ae1db72dc9a28ceb73a4d7d919eec
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-16 16:13:00 +0900

    feat: ControllerScanner 클래스 구현

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,38 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.context.stereotype.Controller;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+
+public class ControllerScanner {
+
+    private final Reflections reflections;
+
+    public ControllerScanner(Object[] basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+
+        return instantiateControllers(controllers);
+    }
+
+    private Map<Class<?>, Object> instantiateControllers(Set<Class<?>> controllers) {
+        final Map<Class<?>, Object> controllersMap = new HashMap<>();
+        for (Class<?> controllerType : controllers) {
+            try {
+                Constructor<?> constructor = controllerType.getConstructor();
+                Object controller = constructor.newInstance();
+                controllersMap.put(controllerType, controller);
+            }  catch (Exception e) {
+                throw new IllegalStateException("컨트롤러 인스턴스 생성 실패: " + controllerType, e);
+            }
+        }
+        return controllersMap;
+    }
+
+}
