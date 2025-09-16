e218d6f46d732393188ed2bb775b01a00eba084e
bowook <mbosx316@naver.com>
2025-09-17 01:35:27 +0900

    feat: HandlerExecution 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -3,10 +3,20 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    private final Object controllerClass;
+    private final Method method;
+
+    public HandlerExecution(final Object controllerClass, final Method method) {
+        this.controllerClass = controllerClass;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        //실행 해야함
+        return (ModelAndView) method.invoke(controllerClass, request, response);
     }
 }
