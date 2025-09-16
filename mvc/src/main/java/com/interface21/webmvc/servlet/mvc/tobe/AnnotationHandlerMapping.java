36eae39f7922feb35856911fabebea757f40ad61
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-16 21:25:36 +0900

    feat: mvc 프레임워크 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,17 +1,22 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackage; // package 마다 동명 controller 존재 가능성
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
@@ -19,11 +24,39 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize()
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+        for (Class<?> controllerClass : reflections.getTypesAnnotatedWith(Controller.class)) {
+            RequestMapping controllerRequestMapping = controllerClass.getAnnotation(RequestMapping.class);
+            String basePath = "";
+            if (controllerRequestMapping != null) {
+                basePath = controllerRequestMapping.value();
+            }
+
+            Method[] methods = controllerClass.getMethods();
+            for (Method method : methods) {
+                RequestMapping methodRequestMapping = method.getAnnotation(RequestMapping.class);
+
+                if (methodRequestMapping == null) {
+                    return ;
+                }
+                String methodPath = methodRequestMapping.value();
+                String fullPath = basePath + methodPath;
+
+                RequestMethod[] requestMethods = methodRequestMapping.method();
+                // TODO 2025. 9. 16. 21:07: requestMethods[0] tmp 디버깅 용
+                for (RequestMethod requestMethod : requestMethods) {
+                    handlerExecutions.put(new HandlerKey(fullPath, requestMethod),
+                            new HandlerExecution(controllerClass.getConstructor().newInstance(), method));
+                }
+            }
+        }
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.findByName(request.getMethod()));
+        return handlerExecutions.get(handlerKey);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,26 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
 
+    // private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    // HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.findByName(request.getMethod()));
+    // 이미 url, method로 handlerExecution을 찾은 상태
+    // 근데 그러면 handle 내에서 controller 동작하도록 처리를 해줘야 할 것 같은데 어떻게 하는 거지?
+
+    private final Object controller;
+    private final Method method;
+
+    public HandlerExecution(Object controller, Method method) {
+        this.controller = controller;
+        this.method = method;
+    }
+
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(controller, request, response);
     }
 }
