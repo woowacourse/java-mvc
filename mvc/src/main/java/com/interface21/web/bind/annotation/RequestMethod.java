6087165b4ea453f0226b0aea5e5888842069cecd
Gabin Choi <gazette9@naver.com>
2025-09-16 21:49:10 +0900

    [1단계 - @MVC 프레임워크 구현하기] 가콩(최가빈) 미션 제출합니다. (#892)
    * test: 구현 전 테스트 완료
    
    * feat: AnnotationHandlerMapping 구현
    
    - 어노테이션 기반으로 HandlerKey 추출하고 해당 클래스, 메서드를 HandlerExecution로 Mapping
    
    * feat: DispatcherServlet의 view 처리 로직 JspView로 책임 위임
    
    * feat: JspView의 model 매개변수 빈 Map으로 변경
    
    * feat: 네이밍 개선 및 생성자 reflection 변경

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,18 @@
 package com.interface21.web.bind.annotation;
 
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+    public static RequestMethod getByName(final String method) {
+        for (RequestMethod value : RequestMethod.values()) {
+            if (value.name().equalsIgnoreCase(method.trim())) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 Http Method입니다. : " + method);
+    }
+
+    public static RequestMethod[] allValues() {
+        return values();
+    }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +28,56 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        final var samples = new Reflections(basePackage);
+        try {
+            final Set<Class<?>> controllerClasses = samples.getTypesAnnotatedWith(Controller.class);
+            for (Class<?> controllerClass : controllerClasses) {
+                final List<Method> requestMappingMethods = getRequestMappingMethods(controllerClass);
+                final Object targetClass = controllerClass.getConstructor().newInstance();
+                for (Method requestMappingMethod : requestMappingMethods) {
+                    final var handlerExecution = new HandlerExecution(requestMappingMethod, targetClass);
+                    putHandlerExecutions(requestMappingMethod, handlerExecution);
+                }
+            }
+        } catch (Exception e) {
+            log.error("Handler 매핑 중 에러가 발생했습니다.", e.fillInStackTrace());
+        }
+    }
+
+    private List<Method> getRequestMappingMethods(Class<?> controllerClass) {
+        return Arrays.stream(controllerClass.getMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
+    }
+
+    private void putHandlerExecutions(final Method requestMappingMethod, final HandlerExecution handlerExecution) {
+        final RequestMapping annotation = requestMappingMethod.getAnnotation(RequestMapping.class);
+        final String url = annotation.value();
+        final RequestMethod[] httpMethods = getAnnotationHttpMethods(annotation.method());
+        for (RequestMethod httpMethod : httpMethods) {
+            final var handlerKey = new HandlerKey(url, httpMethod);
+            handlerExecutions.put(handlerKey, handlerExecution);
+        }
+    }
+
+    private RequestMethod[] getAnnotationHttpMethods(final RequestMethod[] methods) {
+        if (methods.length == 0) {
+            return RequestMethod.allValues();
+        }
+        return methods;
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final var handlerKey = convertRequestToHandlerKey(request);
+        if (!handlerExecutions.containsKey(handlerKey)) {
+            throw new IllegalArgumentException("잘못된 요청입니다. : " + handlerKey);
+        }
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private HandlerKey convertRequestToHandlerKey(HttpServletRequest request) {
+        final var requestMethod = RequestMethod.getByName(request.getMethod());
+        final var url = request.getRequestURI();
+        return new HandlerKey(url, requestMethod);
     }
 }
