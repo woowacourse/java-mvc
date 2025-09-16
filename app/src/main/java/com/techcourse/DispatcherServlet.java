ec33975b5d355223febd201756ab79590edbf790
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-09-16 23:11:35 +0900

    refactor: 2단계 초기 구현
    - 두 개의 handlerMapping을 함께 사용할 수 있도록 구현함

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,10 +1,15 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,15 +18,14 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private List<HandlerMapping> handlerMappings;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        this.handlerMappings = List.of(new ManualHandlerMapping(), new AnnotationHandlerMapping());
     }
 
     @Override
@@ -30,9 +34,23 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final ModelAndView modelAndView = controller.execute(request, response);
-            modelAndView.render(request, response);
+            for (HandlerMapping handlerMapping : handlerMappings) {
+                final Object handler = handlerMapping.getHandler(request);
+
+                if (handler instanceof Controller) {
+                    final Controller controller = (Controller) handler;
+                    ModelAndView modelAndView = controller.execute(request, response);
+                    modelAndView.render(request, response);
+                    break;
+                }
+
+                if (handler instanceof HandlerExecution) {
+                    final HandlerExecution controller = (HandlerExecution) handler;
+                    final ModelAndView modelAndView = controller.handle(request, response);
+                    modelAndView.render(request, response);
+                    break;
+                }
+            }
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
