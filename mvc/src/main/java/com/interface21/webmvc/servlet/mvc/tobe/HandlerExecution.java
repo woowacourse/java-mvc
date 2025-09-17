42c683873ec024309c0ac1eeb2526d325b155616
Yeongjae Kim <sees77770@gmail.com>
2025-09-17 19:31:38 +0900

    refactor: ModelAndView 방어 로직 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -3,6 +3,7 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HandlerExecution {
@@ -16,6 +17,19 @@ public class HandlerExecution {
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(handler, request, response);
+        try {
+            Object result = method.invoke(handler, request, response);
+
+            if (result instanceof ModelAndView) {
+                return (ModelAndView) result;
+            }
+
+            throw new IllegalStateException(
+                    "Handler method must return ModelAndView, but returned: " +
+                            (result != null ? result.getClass().getName() : "null")
+            );
+        } catch (InvocationTargetException e) {
+            throw new Exception("Handler execution failed", e.getCause());
+        }
     }
 }
