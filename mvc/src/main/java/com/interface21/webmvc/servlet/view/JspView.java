edaf2638a1e65e86af823c6547c1f11370bfc516
supernovaMK <minkikim2002@gmail.com>
2025-09-17 13:16:58 +0900

    chore: todo 삭제

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -22,7 +22,6 @@ public class JspView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
         if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
             response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
             return;
@@ -33,7 +32,6 @@ public class JspView implements View {
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
         final var requestDispatcher = request.getRequestDispatcher(viewName);
         requestDispatcher.forward(request, response);
     }
