ae5376f4e3b7cb9af1c3ca7858fb5a2ee55f8460
juno <wnsgh732@gmail.com>
2025-09-18 00:19:19 +0900

    feat: 1단계 요구사항

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,37 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+
+        for (Object pkg : basePackage) {
+            Reflections reflections = new Reflections(pkg);
+            Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+            try {
+                for (Class<?> clazz : classes) {
+                    Object controller = clazz.getDeclaredConstructor().newInstance();
+                    for (Method method : clazz.getMethods()) {
+                        registerHandler(method, controller);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Initialize AnnotationHandlerMapping failed", e);
+            }
+
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), requestMethod);
+        return handlerExecutions.get(handlerKey);
+    }
+
+    private void registerHandler(final Method method, final Object controller) {
+        RequestMapping mapping = method.getAnnotation(RequestMapping.class);
+        String path = mapping.value();
+        RequestMethod[] requestMethods = mapping.method();
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(path, requestMethod);
+            handlerExecutions.put(handlerKey, new HandlerExecution(method, controller));
+        }
     }
 }
