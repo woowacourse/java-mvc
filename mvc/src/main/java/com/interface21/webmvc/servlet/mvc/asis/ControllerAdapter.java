c40f906b50402e638e68adbfc10cffce84750a12
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-09-18 01:47:50 +0900

    feat: HandlerAdapter 인터페이스 및 구현 클래스 추가
    - HandlerAdapter 인터페이스 정의
    - 기존 Controller 기반 ControllerAdapter 클래스 구현
    - HandlerExecution 기반 HandlerExecutionAdapter 클래스 구현
    - 요청 처리 로직 확장을 위한 어댑터 패턴 적용

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/ControllerAdapter.java
@@ -0,0 +1,20 @@
+package com.interface21.webmvc.servlet.mvc.asis;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
+import com.interface21.webmvc.servlet.view.JspView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ControllerAdapter implements HandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String viewName = ((Controller) handler).execute(request, response);
+        return new ModelAndView(new JspView(viewName));
+    }
+}
