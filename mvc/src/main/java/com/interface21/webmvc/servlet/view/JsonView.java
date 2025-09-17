773edf4848f8d5a70e7235cae9f832ea14b85386
yeonnhuu <yeonnhuu@gmail.com>
2025-09-17 14:05:39 +0900

    feat: JsonView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,38 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+    private final ObjectMapper mapper;
+
+    public JsonView() {
+        this.mapper = new ObjectMapper();
+    }
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response)
+            throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        log.debug("Rendering model as JSON: {}", model);
+        final Object body = extractBodyFromModel(model);
+        final String json = mapper.writeValueAsString(body);
+        response.getWriter().write(json);
+    }
+
+    private Object extractBodyFromModel(final Map<String, ?> model) throws Exception {
+        if (model.size() == 1) {
+            return model.values().iterator().next();
+        }
+        return model;
     }
 }
