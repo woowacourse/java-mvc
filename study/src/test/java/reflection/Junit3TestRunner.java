9d258a8164b0ce3d2a4d0b8afd2b5754001fab74
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-17 15:45:32 +0900

    study: 기초 내용 학습

--- study/src/test/java/reflection/Junit3TestRunner.java
+++ study/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,6 @@
 package reflection;
 
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +8,16 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        final Junit3Test junit3Test = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            if (method.getName().contains("test")) {
+                method.invoke(junit3Test);
+            }
+        }
+
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +9,19 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            final Annotation[] annotations = method.getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (annotation.annotationType().equals(MyTest.class)) {
+                    method.invoke(junit4Test);
+                    break;
+                }
+            }
+        }
     }
 }
