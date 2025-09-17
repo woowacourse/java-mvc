19c95c3bc95b09d76c16e0024a82880048ebca27
Hyunwook Kim <129609928+kimdevv@users.noreply.github.com>
2025-09-17 11:47:54 +0900

    [1단계 - @MVC 프레임워크 구현하기] 프리 제출합니다. (#940)
    * feat: 요청에 맞게 컨트롤러 메서드 찾아서 처리하는 기능 구현
    
    * refactor: @RequestMapping에 메서드가 없는 경우 모든 메서드에 대해 지원하도록 수정
    
    * feat: JspView 기능 구현
    
    * refactor: JspView에서 모델에 값 추가하는 메서드 분리
    
    * refactor: AnnotationHandlerMapping 메서드 분리

--- mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
+++ mvc/src/main/java/com/interface21/web/bind/annotation/RequestMethod.java
@@ -1,5 +1,23 @@
 package com.interface21.web.bind.annotation;
 
+import java.util.Arrays;
+
 public enum RequestMethod {
-    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    TRACE,
+    ;
+
+    public static RequestMethod from(String requestMethodText) {
+        return Arrays.stream(values())
+            .filter(method -> method.name().equals(requestMethodText))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 요청 메서드입니다."));
+    }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -21,9 +26,63 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Set<Class<?>> controllers = scanControllers();
+        registerHandlers(controllers);
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private Set<Class<?>> scanControllers() {
+        Reflections reflections = new Reflections(basePackage);
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    private void registerHandlers(Set<Class<?>> controllers) {
+        try {
+            for (Class<?> controller : controllers) {
+                Object controllerInstance = controller.getConstructor().newInstance();
+                Method[] controllerMethods = controller.getMethods();
+                registerControllerMethods(controllerInstance, controllerMethods);
+            }
+        } catch (Exception exception) {
+            log.error("알 수 없는 오류가 발생했습니다: " + exception.getMessage());
+        }
+    }
+
+    private void registerControllerMethods(Object controllerInstance, Method[] controllerMethods) {
+        for (Method controllerMethod : controllerMethods) {
+            if (controllerMethod.isAnnotationPresent(RequestMapping.class)) {
+                registerHandler(controllerInstance, controllerMethod);
+            }
+        }
+    }
+
+    private void registerHandler(Object controllerInstance, Method controllerMethod) {
+        RequestMapping requestMappingAnnotation = controllerMethod.getAnnotation(RequestMapping.class);
+        RequestMethod[] httpMethods = requestMappingAnnotation.method();
+        String path = requestMappingAnnotation.value();
+
+        if (httpMethods.length == 0) {
+            registerHandlerWithAllHttpMethods(controllerInstance, controllerMethod, path);
+            return;
+        }
+        for (RequestMethod httpMethod : httpMethods) {
+            putHandler(path, httpMethod, controllerInstance, controllerMethod);
+        }
+    }
+
+    private void registerHandlerWithAllHttpMethods(Object controllerInstance, Method controllerMethod, String path) {
+        for (RequestMethod httpMethod : RequestMethod.values()) {
+            putHandler(path, httpMethod, controllerInstance, controllerMethod);
+        }
+    }
+
+    private void putHandler(String path, RequestMethod httpMethod, Object controllerInstance, Method controllerMethod) {
+        HandlerKey handlerKey = new HandlerKey(path, httpMethod);
+        HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, controllerMethod);
+        this.handlerExecutions.put(handlerKey, handlerExecution);
+    }
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        HandlerKey handlerKey = new HandlerKey(request.getRequestURI(), RequestMethod.from(request.getMethod()));
+        return this.handlerExecutions.get(handlerKey);
     }
 }
