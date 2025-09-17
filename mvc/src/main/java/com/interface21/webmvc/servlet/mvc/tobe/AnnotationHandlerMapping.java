ef300bb5d7dbeab398cbacac8cacd6200f28840c
byesol <bsj5967@naver.com>
2025-09-17 23:38:00 +0900

    refactor: 컨트롤러 인스턴스 책임을 ControllerScanner 로 분리
    - AnnotationHandlerMapping 으로부터 컨트롤러 인스턴스 생성 책임 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,15 +1,11 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,27 +23,19 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
-        for (Class<?> controller : controllers) {
-            try {
-                Object controllerInstance = createControllerInstance(controller);
-                registerRequestMappings(controllerInstance, controller);
-            } catch (Exception e) {
-                log.error("컨트롤러 시작 에러: {}", controller.getName(), e);
-            }
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        for (Map.Entry<Class<?>, Object> entry : controllers.entrySet()) {
+            final Class<?> controllerClass = entry.getKey();
+            final Object controllerInstance = entry.getValue();
+            registerRequestMappings(controllerInstance, controllerClass);
         }
+
     }
 
     public HandlerExecution getHandler(final HttpServletRequest request) {
         HandlerKey key = new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod()));
         return handlerExecutions.get(key);
-    }
-
-    private Object createControllerInstance(Class<?> controller) throws Exception {
-        Constructor<?> constructor = controller.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        return constructor.newInstance();
     }
 
     private void registerRequestMappings(Object controllerInstance, Class<?> controllerClass) {
