c671035b68ec8abcd53417e5c0a16c5e7c34a867
Gabin Choi <choegabin171@gmail.com>
2025-09-18 01:37:53 +0900

    refactor: Controller Scanner 로 Controller 스캔 및 생성 책임 위임

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,14 +8,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map.Entry;
 
 public class AnnotationHandlerMapping {
-
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -27,20 +21,16 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
-        final var samples = new Reflections(basePackage);
-        try {
-            final Set<Class<?>> controllerClasses = samples.getTypesAnnotatedWith(Controller.class);
-            for (Class<?> controllerClass : controllerClasses) {
-                final List<Method> requestMappingMethods = getRequestMappingMethods(controllerClass);
-                final Object targetClass = controllerClass.getConstructor().newInstance();
-                for (Method requestMappingMethod : requestMappingMethods) {
-                    final var handlerExecution = new HandlerExecution(requestMappingMethod, targetClass);
-                    putHandlerExecutions(requestMappingMethod, handlerExecution);
-                }
+        final var controllerScanner = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        for (Entry<Class<?>, Object> controllerMap : controllers.entrySet()) {
+            final Class<?> controllerClass = controllerMap.getKey();
+            final Object controllerInstance = controllerMap.getValue();
+            final List<Method> requestMappingMethods = getRequestMappingMethods(controllerClass);
+            for (Method requestMappingMethod : requestMappingMethods) {
+                final var handlerExecution = new HandlerExecution(requestMappingMethod, controllerInstance);
+                putHandlerExecutions(requestMappingMethod, handlerExecution);
             }
-        } catch (Exception e) {
-            log.error("Handler 매핑 중 에러가 발생했습니다.", e.fillInStackTrace());
         }
     }
 
@@ -67,7 +57,7 @@ public class AnnotationHandlerMapping {
         return methods;
     }
 
-    public Object getHandler(final HttpServletRequest request) {
+    public HandlerExecution getHandler(final HttpServletRequest request) {
         final var handlerKey = convertRequestToHandlerKey(request);
         if (!handlerExecutions.containsKey(handlerKey)) {
             throw new IllegalArgumentException("잘못된 요청입니다. : " + handlerKey);
