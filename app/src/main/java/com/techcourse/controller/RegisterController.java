89a747e9ec101046146f136a402bf5de583888fd
kjungw <sagakkb@gmail.com>
2025-09-18 17:29:58 +0900

    fix: 비즈니스 로직 발생한 예외 처리

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,15 +1,22 @@
 package com.techcourse.controller;
 
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
 public class RegisterController implements Controller {
 
     @Override
     public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        if (req.getParameter("account") == null ||
+                req.getParameter("password") == null ||
+                req.getParameter("email") == null
+        ) {
+            return "redirect:/404.jsp";
+        }
+
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
