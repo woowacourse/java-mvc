810c2dece7a4901fa1aafcaa9b35a6f2d5cd83ee
eueo8259 <eueo8259@gmail.com>
2025-09-18 14:13:42 +0900

    refactor: 컨트롤러 어노테이션 사용하도록 수정

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,34 +1,43 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
 import com.techcourse.domain.User;
+import com.techcourse.factory.ModelAndViewFactory;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse rep) throws Exception {
+        String viewName;
         if (UserSession.isLoggedIn(req.getSession())) {
-            return "redirect:/index.jsp";
+            viewName =  "redirect:/index.jsp";
+            return ModelAndViewFactory.createModelAndViewByViewName(req, viewName);
+
         }
 
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+        viewName = InMemoryUserRepository.findByAccount(req.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
                     return login(req, user);
                 })
                 .orElse("redirect:/401.jsp");
+        return ModelAndViewFactory.createModelAndViewByViewName(req, viewName);
     }
 
-    private String login(final HttpServletRequest request, final User user) {
-        if (user.checkPassword(request.getParameter("password"))) {
-            final var session = request.getSession();
+    private String login(final HttpServletRequest req, final User user) {
+        if (user.checkPassword(req.getParameter("password"))) {
+            final var session = req.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
             return "redirect:/index.jsp";
         }
