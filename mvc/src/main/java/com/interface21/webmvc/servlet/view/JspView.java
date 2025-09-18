09e1cbbd836be394dfeaf74ae50789c6abae3722
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-19 02:05:45 +0900

    feat: jsp view 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,6 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -10,22 +11,34 @@ import java.util.Map;
 
 public class JspView implements View {
 
-    private static final Logger log = LoggerFactory.getLogger(JspView.class);
-
     public static final String REDIRECT_PREFIX = "redirect:";
+    public static final String BASE_PATH = "/webapp/";
+    private static final Logger log = LoggerFactory.getLogger(JspView.class);
+    private final String viewName;
 
     public JspView(final String viewName) {
+        this.viewName = viewName + ".jsp";
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // todo
-
+    public void render(
+            final Map<String, ?> model,
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
         model.keySet().forEach(key -> {
             log.debug("attribute name : {}, value : {}", key, model.get(key));
             request.setAttribute(key, model.get(key));
         });
 
-        // todo
+        if (viewName.startsWith(REDIRECT_PREFIX)) {
+            String redirectUrl = viewName.substring(REDIRECT_PREFIX.length());
+            response.sendRedirect(redirectUrl);
+            return;
+        }
+
+        String actualPath = BASE_PATH + viewName;
+        RequestDispatcher dispatcher = request.getRequestDispatcher(actualPath);
+        dispatcher.forward(request, response);
     }
 }
