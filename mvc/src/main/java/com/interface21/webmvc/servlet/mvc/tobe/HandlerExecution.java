bb53d4fe2e794fba01a40a14394b71a568437a4a
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-18 21:44:29 +0900

    feat: HandlerExecution.java ModelAndView 반환 메서드 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,12 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.interface21.webmvc.servlet.ModelAndView;
+import java.util.Enumeration;
 
 public class HandlerExecution {
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return null;
+        String requestURI = request.getRequestURI();
+        ModelAndView modelAndView = new ModelAndView(new JspView(requestURI + ".jsp"));
+        modelAndView.addObject("id", request.getAttribute("id"));
+        return modelAndView;
     }
 }
