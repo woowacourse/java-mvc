e40748ed4fdc486893ef97b765d3cac7071ba245
byesol <bsj5967@naver.com>
2025-09-18 11:34:33 +0900

    refactor: HandlerAdapter 패키지 위치를 mvc로 이동
    - HandlerExecutionAdapter 구현

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionAdapter.java
@@ -0,0 +1,14 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HandlerExecutionAdapter implements HandlerAdapter {
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return ((HandlerExecution) handler).handle(request, response);
+    }
+}
