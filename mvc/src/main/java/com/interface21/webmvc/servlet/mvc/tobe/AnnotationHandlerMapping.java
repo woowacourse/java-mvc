4e53fa071e83ba40968308c06a638a996f8cc818
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-18 21:43:51 +0900

    feat: AnnotationHandlerMapping.java 초기화 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,7 +1,12 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +27,29 @@ public class AnnotationHandlerMapping {
 
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
-        handlerExecutions.clear();
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : typesAnnotatedWith) {
+            Method[] declaredMethods = aClass.getDeclaredMethods();
+            for (Method declaredMethod : declaredMethods) {
+                RequestMapping annotation = declaredMethod.getAnnotation(RequestMapping.class);
+                String value = annotation.value();
+                RequestMethod[] method = annotation.method();
+
+                if (method.length == 0) {
+                    RequestMethod[] values = RequestMethod.values();
+                    for (RequestMethod requestMethod : values) {
+                        HandlerKey handlerKey = new HandlerKey(value, requestMethod);
+                        handlerExecutions.put(handlerKey, new HandlerExecution());
+                    }
+                } else {
+                    for (RequestMethod requestMethod : method) {
+                        HandlerKey handlerKey = new HandlerKey(value, requestMethod);
+                        handlerExecutions.put(handlerKey, new HandlerExecution());
+                    }
+                }
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
