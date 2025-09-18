268c928ba53da42e0edff730feae925573db20e0
Park Minseon <63039855+minSsan@users.noreply.github.com>
2025-09-18 23:31:44 +0900

    [1단계 - @MVC 프레임워크 구현하기] 밍곰(박민선) 미션 제출합니다. (#936)
    * feat: 퀘스트1 - 서블릿의 HTTP 요청 처리
    
    * feat: `@Controller`, `@RequestMapping` 리플렉션을 통해 AnnotationHandlerMapping 필드 값 초기화
    
    * feat: `HandlerExecution` 정의
    
    * refactor: ManualHandlerMapping -> AnnotationHandlerMapping 변경 및 JspView 책임 옮기기
    
    * feat: 학습 테스트 진행

--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -2,6 +2,10 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
 class Junit4TestRunner {
 
     @Test
@@ -9,5 +13,12 @@ class Junit4TestRunner {
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final List<Method> methods = Arrays.stream(clazz.getMethods())
+                .filter(m -> m.isAnnotationPresent(MyTest.class))
+                .toList();
+
+        for (Method method : methods) {
+            method.invoke(new Junit4Test());
+        }
     }
 }
