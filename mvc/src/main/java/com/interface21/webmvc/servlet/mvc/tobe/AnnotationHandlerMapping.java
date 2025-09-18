72527fa7e15322e9a7320a845fc19e7b486f7a67
Julie Hahn <128875051+jbilee@users.noreply.github.com>
2025-09-18 23:21:44 +0900

    [2단계 - 점진적인 리팩터링] 줄리(한승아) 미션 제출합니다. (#964)
    * feat: ControllerScanner 클래스 구현
    
    * refactor: AnnotationHandlerMapping 및 ControllerScanner 로직 리팩터링
    
    * refactor: HandlerMapping 클래스 추상화
    
    * refactor: 레거시 Handler에서 반환하는 view 문자열을 ModelAndView로 변환하는 로직 추가
    
    * refactor: DispatcherServlet 예외 핸들링 로직 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,21 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
@@ -27,19 +23,25 @@ public class AnnotationHandlerMapping {
         this.handlerExecutions = new HashMap<>();
     }
 
+    @Override
     public void initialize() {
-        Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
 
-        for (Class<?> controller : controllers) {
-            mapController(controller);
-        }
+        mapControllers(controllerScanner.getControllers());
 
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void mapController(final Class<?> controller) {
-        List<Method> mappedMethods = findMappedMethods(controller);
+    private void mapControllers(final Map<Class<?>, Object> controllers) {
+        for (Map.Entry<Class<?>, Object> entrySet : controllers.entrySet()) {
+            mapControllerMethods(entrySet.getKey(), entrySet.getValue());
+        }
+    }
+
+    private void mapControllerMethods(final Class<?> controllerClass, final Object controller) {
+        List<Method> mappedMethods = Arrays.stream(controllerClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                .toList();
 
         for (Method method : mappedMethods) {
             try {
@@ -51,16 +53,7 @@ public class AnnotationHandlerMapping {
         }
     }
 
-    private List<Method> findMappedMethods(final Class<?> controller) {
-        Method[] declaredMethods = controller.getDeclaredMethods();
-        return Arrays.stream(declaredMethods)
-                .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                .toList();
-    }
-
-    private void addHandlerExecutions(final Class<?> controllerClass, final Method method) throws Exception {
-        Constructor<?> controllerConstructor = controllerClass.getDeclaredConstructor();
-        Object controller = controllerConstructor.newInstance();
+    private void addHandlerExecutions(final Object controller, final Method method) {
         RequestMapping annotation = method.getAnnotation(RequestMapping.class);
         String url = annotation.value();
         RequestMethod[] requestMethods = annotation.method();
@@ -79,6 +72,7 @@ public class AnnotationHandlerMapping {
         handlerExecutions.put(handlerKey, handlerExecution);
     }
 
+    @Override
     public Object getHandler(final HttpServletRequest request) {
         String requestUri = request.getRequestURI();
         String method = request.getMethod();
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
@@ -0,0 +1,40 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.context.stereotype.Controller;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ControllerScanner {
+
+    private static final Logger log = LoggerFactory.getLogger(ControllerScanner.class);
+
+    private final Reflections reflections;
+
+    public ControllerScanner(final Object[] basePackage) {
+        this.reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        try {
+            Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+            return instantiateControllers(controllerClasses);
+        } catch (Exception e) {
+            log.error("Failed to get controllers.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<Class<?>, Object> instantiateControllers(final Set<Class<?>> controllerClasses) throws Exception {
+        final Map<Class<?>, Object> controllers = new HashMap<>();
+        for (Class<?> controllerClass : controllerClasses) {
+            Constructor<?> controllerConstructor = controllerClass.getDeclaredConstructor();
+            controllers.put(controllerClass, controllerConstructor.newInstance());
+        }
+        return controllers;
+    }
+}
