04ccfe837c0abf8e5d531afe201a89a644b4a720
kjungw <sagakkb@gmail.com>
2025-09-18 17:54:26 +0900

    refactor: RegisterController만 애노테이션 형식으로 변경

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,20 +1,25 @@
 package com.techcourse.controller;
 
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class RegisterController implements Controller {
+@Controller
+public class RegisterController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
         if (req.getParameter("account") == null ||
                 req.getParameter("password") == null ||
                 req.getParameter("email") == null
         ) {
-            return "redirect:/404.jsp";
+            return new ModelAndView(new JspView("redirect:/404.jsp"));
         }
 
         final var user = new User(2,
@@ -22,7 +27,33 @@ public class RegisterController implements Controller {
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserRepository.save(user);
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
+    }
 
-        return "redirect:/index.jsp";
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) {
+        return new ModelAndView(new JspView("redirect:/register.jsp"));
     }
 }
+
+// 이전 버전 RegisterController
+//public class RegisterController implements Controller {
+//
+//    @Override
+//    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+//        if (req.getParameter("account") == null ||
+//                req.getParameter("password") == null ||
+//                req.getParameter("email") == null
+//        ) {
+//            return "redirect:/404.jsp";
+//        }
+//
+//        final var user = new User(2,
+//                req.getParameter("account"),
+//                req.getParameter("password"),
+//                req.getParameter("email"));
+//        InMemoryUserRepository.save(user);
+//
+//        return "redirect:/index.jsp";
+//    }
+//}
