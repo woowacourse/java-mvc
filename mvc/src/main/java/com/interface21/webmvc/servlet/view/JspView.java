603d6b30c85ad6d8ac1d48f55b9f0970330a3065
ljhee92 <weilainib7@gmail.com>
2025-09-18 19:13:38 +0900

    refactor: DispatcherServlet, JspView 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -22,14 +22,11 @@ public class JspView implements View {
     }
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(REDIRECT_PREFIX)) {
-            final String redirectPath = viewName.substring(REDIRECT_PREFIX.length());
-            log.debug("Redirecting to: {}", redirectPath);
-            response.sendRedirect(redirectPath);
-            return;
-        }
-
+    public void render(
+            final Map<String, ?> model,
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
         if (model != null) {
             model.keySet().forEach(key -> {
                 log.debug("attribute name : {}, value : {}", key, model.get(key));
