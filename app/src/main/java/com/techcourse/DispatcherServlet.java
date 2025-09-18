53da36b9a3d41402da547a4aefd5a2bd119950c2
supernovaMK <minkikim2002@gmail.com>
2025-09-18 22:08:36 +0900

    feat: HandlerMappingRegistry & HandlerAdapterRegistry 구현

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -2,16 +2,9 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.ControllerHandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
-import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,16 +14,16 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private final List<HandlerMapping> handlerMappings = new ArrayList<>();
-    private final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
+    private HandlerMappingRegistry handlerMappingRegistry;
+    private HandlerAdapterRegistry handlerAdapterRegistry;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        addHandlerMapping();
-        addHandlerAdapter();
+        handlerMappingRegistry = new HandlerMappingRegistry();
+        handlerAdapterRegistry = new HandlerAdapterRegistry();
     }
 
     @Override
@@ -40,26 +33,8 @@ public class DispatcherServlet extends HttpServlet {
         render(modelAndView, request, response);
     }
 
-    private void addHandlerMapping() {
-        final ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
-        final AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping("com.interface21",
-                "com.techcourse");
-        annotationHandlerMapping.initialize();
-
-        handlerMappings.add(manualHandlerMapping);
-        handlerMappings.add(annotationHandlerMapping);
-    }
-
-    private void addHandlerAdapter() {
-        final ControllerHandlerAdapter controllerHandlerAdapter = new ControllerHandlerAdapter();
-        final AnnotationHandlerAdapter annotationHandlerAdapter = new AnnotationHandlerAdapter();
-        handlerAdapters.add(controllerHandlerAdapter);
-        handlerAdapters.add(annotationHandlerAdapter);
-    }
-
     private Object getHandler(HttpServletRequest request) {
-        for (var handlerMapping : handlerMappings) {
+        for (var handlerMapping : handlerMappingRegistry.getHandlerMappings()) {
             Object handler;
             if ((handler = handlerMapping.getHandler(request)) != null) {
                 return handler;
@@ -72,7 +47,7 @@ public class DispatcherServlet extends HttpServlet {
                                         Object handler) {
         ModelAndView modelAndView;
         try {
-            for (var handlerAdapter : handlerAdapters) {
+            for (var handlerAdapter : handlerAdapterRegistry.getHandlerAdapters()) {
                 if (handlerAdapter.supports(handler)) {
                     modelAndView = handlerAdapter.handle(request, response, handler);
                     return modelAndView;
