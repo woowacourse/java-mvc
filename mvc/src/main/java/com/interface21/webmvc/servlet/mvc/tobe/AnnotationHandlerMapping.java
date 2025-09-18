e659c67942e72ebf0a44d3c635fdcddb2885f422
YuyoungRhee <bbclara@naver.com>
2025-09-18 11:13:53 +0900

    feat: RequestMapping 어노테이션 자동화 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,17 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +26,51 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        final var reflections = new Reflections(basePackage);
+        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+
+        for (final Class<?> controllerClass : controllerClasses) {
+            try {
+                final Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+                populateHandlerExecutions(controllerInstance, controllerClass.getDeclaredMethods());
+            } catch (Exception e) {
+                log.error("Error creating controller instance: {}", controllerClass.getName(), e);
+                throw new RuntimeException(e);
+            }
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
+    private void populateHandlerExecutions(final Object controllerInstance, final java.lang.reflect.Method[] methods) {
+        for (final java.lang.reflect.Method method : methods) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
+            }
+
+            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            final Stream<RequestMethod> requestMethods = getRequestMethods(requestMapping);
+
+            requestMethods.forEach(requestMethod -> {
+                final var handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
+                final var handlerExecution = new HandlerExecution(controllerInstance, method);
+                handlerExecutions.put(handlerKey, handlerExecution);
+                log.info("Mapped handler: {} -> {}", handlerKey, method);
+            });
+        }
+    }
+
+    private Stream<RequestMethod> getRequestMethods(final RequestMapping requestMapping) {
+        final RequestMethod[] methods = requestMapping.method();
+        if (methods.length == 0) {
+            return Arrays.stream(RequestMethod.values());
+        }
+        return Arrays.stream(methods);
+    }
+
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        final var requestUri = request.getRequestURI();
+        final var requestMethod = RequestMethod.valueOf(request.getMethod());
+        final var handlerKey = new HandlerKey(requestUri, requestMethod);
+        return handlerExecutions.get(handlerKey);
     }
 }
