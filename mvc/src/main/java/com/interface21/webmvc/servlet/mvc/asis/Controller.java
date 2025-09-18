ebbd6fc74af9d70dcbf270af69df1a2157a9069f
wook <mbosx316@naver.com>
2025-09-18 14:05:41 +0900

    [1단계 - @MVC 프레임워크 구현하기] 우가(민보욱) 미션 제출합니다. (#957)
    * test: Junit3TestRunner 학습
    
    * test: Junit4TestRunner 학습
    
    * test: ReflectionTest 학습
    
    * test: ReflectionsTest 학습
    
    * feat: HandlerMapping 구현
    
    * feat: HandlerExecution 구현
    
    * feat: RequestMethod 문자열 파싱 구현
    
    * feat: DispatcherServlet의 뷰 처리 JspView로 이동
    
    * refactor: 핸들러 중복 방지
    
    * refactor: 메서드 분리 및 ModelAndView 반환하도록 수정
    
    * refactor: 사용하지 않는 import 제거
    
    * test: sout 제거
    
    * style: final 키워드 추가
    
    * feat: public 메서드만 핸들러 메서드로 등록
    
    * feat: 중복된 핸들러 키 검사
    
    * refactor: public -> private 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/Controller.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/asis/Controller.java
@@ -1,8 +1,11 @@
 package com.interface21.webmvc.servlet.mvc.asis;
 
+import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface Controller {
-    String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception;
+    //String으로만 반환하면 화면에 전달할 데이터를 같이 넘길 방법이 부족한 것 같음
+    //그래서 Model을 사용한 것이고, Model과 View를 같이 묶은 객체를 사용한듯
+    ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception;
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,13 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Set;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +18,85 @@ public class AnnotationHandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
-    private final Object[] basePackage;
+    private final Object[] basePackages;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
         this.handlerExecutions = new HashMap<>();
     }
 
+    // 초기화 메서드
     public void initialize() {
         log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackages);
+
+        Set<Class<?>> controllerClasses = findControllerClasses(reflections);
+        for (Class<?> controllerClass : controllerClasses) {
+            registerController(controllerClass);
+        }
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    // 컨트롤러 클래스 조회
+    private Set<Class<?>> findControllerClasses(final Reflections reflections) {
+        return reflections.getTypesAnnotatedWith(Controller.class);
+    }
+
+    // 컨트롤러 등록
+    private void registerController(final Class<?> controllerClass) {
+        Object controller = createInstance(controllerClass);
+        for (Method method : controllerClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(RequestMapping.class) && Modifier.isPublic(method.getModifiers())) {
+                registerHandlerMethod(controller, method);
+            }
+        }
+    }
+
+    // 핸들러 메서드 등록
+    private void registerHandlerMethod(final Object controller, final Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        String uri = requestMapping.value();
+        RequestMethod[] requestMethods = requestMapping.method();
+
+        if (requestMethods.length == 0) {
+            requestMethods = RequestMethod.values(); // 모든 메서드 허용
+        }
+
+        for (RequestMethod requestMethod : requestMethods) {
+            HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(controller, method);
+
+            validateDuplicateHandlerKey(handlerKey, handlerExecution);
+            log.debug("Handler registered: {} {}", requestMethod, uri);
+        }
+    }
+
+    // 중복 핸들러 키 검사
+    private void validateDuplicateHandlerKey(final HandlerKey handlerKey, final HandlerExecution handlerExecution) {
+        if(handlerExecutions.containsKey(handlerKey)) {
+            throw new IllegalStateException("Duplicate mapping detected: " + handlerKey);
+        }
+
+        handlerExecutions.put(handlerKey, handlerExecution);
+    }
+
+    // 컨트롤러 인스턴스 생성
+    private Object createInstance(final Class<?> controllerClass) {
+        try {
+            return controllerClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            log.info("인스턴스 생성에 실패했습니다. {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 요청으로부터 핸들러 조회
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.from(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(uri, requestMethod);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
+
