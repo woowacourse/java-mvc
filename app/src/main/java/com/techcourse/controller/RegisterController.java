4956d2596b7a003d4b8be54461ab9d1021543ae2
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-09-19 17:33:30 +0900

    refactor: Controller 인터페이스 리팩토링 및 반환 타입 수정
    - Controller 인터페이스 execute 메서드 반환 타입 변경 (String → ModelAndView)
    - View 반환 로직에서 JspView 객체로 전환
    - 가독성 향상을 위한 코드 최적화 및 로깅 추가

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,21 +1,29 @@
 package com.techcourse.controller;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RegisterController implements Controller {
 
+    private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
+
     @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserRepository.save(user);
+        StringBuilder builder = new StringBuilder();
+        log.info(builder.append("회원가입 성공 : ").append(user).toString());
 
-        return "redirect:/index.jsp";
+        return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
 }
