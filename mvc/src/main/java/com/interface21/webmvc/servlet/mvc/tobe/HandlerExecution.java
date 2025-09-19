466a0576d3b1dd3596c9aaf29c65505ab8e1cc85
Dompoo <dlckdrms0517@gmail.com>
2025-09-19 10:01:05 +0900

    refactor: 여러 방식의 핸들러 파라미터 지원

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -4,6 +4,7 @@ import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class HandlerExecution {
 
@@ -16,8 +17,16 @@ public class HandlerExecution {
     }
 
     public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        // TODO : 여러 형태의 HandlerExecution을 지원하면 좋을 듯
-        return (ModelAndView) method.invoke(handler, request, response);
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        int parameterCount = method.getParameterCount();
+        if (parameterCount == 2 && parameterTypes[0] == HttpServletRequest.class && parameterTypes[1] == HttpServletResponse.class) {
+            return (ModelAndView) method.invoke(handler, request, response);
+        } else if (parameterCount == 1 && parameterTypes[0] == HttpServletRequest.class) {
+            return (ModelAndView) method.invoke(handler, request);
+        } else if (parameterCount == 0) {
+            return (ModelAndView) method.invoke(handler);
+        }
+        throw new IllegalStateException("Handler ParameterTypes Not Supported : " + Arrays.toString(parameterTypes));
     }
 
     public Object getHandler() {
