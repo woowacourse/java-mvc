7cea890e0a4476640820d9bfdfcf5893097a0db4
Dompoo <dlckdrms0517@gmail.com>
2025-09-19 10:42:21 +0900

    fix: 핸들러 어댑터가 ModelAndView를 항상 응답하도록 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.webmvc.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
@@ -16,15 +15,15 @@ public class HandlerExecution {
         this.method = method;
     }
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+    public Object handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         Class<?>[] parameterTypes = method.getParameterTypes();
         int parameterCount = method.getParameterCount();
         if (parameterCount == 2 && parameterTypes[0] == HttpServletRequest.class && parameterTypes[1] == HttpServletResponse.class) {
-            return (ModelAndView) method.invoke(handler, request, response);
+            return method.invoke(handler, request, response);
         } else if (parameterCount == 1 && parameterTypes[0] == HttpServletRequest.class) {
-            return (ModelAndView) method.invoke(handler, request);
+            return method.invoke(handler, request);
         } else if (parameterCount == 0) {
-            return (ModelAndView) method.invoke(handler);
+            return method.invoke(handler);
         }
         throw new IllegalStateException("Handler ParameterTypes Not Supported : " + Arrays.toString(parameterTypes));
     }
