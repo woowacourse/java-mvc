66b00cb156254a4125bd7becf8deb624c46dbb6f
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-19 21:15:05 +0900

    refactor: static import 방식 수정

--- app/src/main/java/com/techcourse/controller/AnnotatedRegisterController.java
+++ app/src/main/java/com/techcourse/controller/AnnotatedRegisterController.java
@@ -1,12 +1,13 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 
-@com.interface21.context.stereotype.Controller
+@Controller
 public class AnnotatedRegisterController {
 
     @RequestMapping(value = "/register2", method = RequestMethod.POST)
