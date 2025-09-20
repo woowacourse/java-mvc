24915a2abeba3b345b42fc1131cbc4ad3ad877ab
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-20 19:11:52 +0900

    feat: 다양한 핸들러를 지원하는 HandlerAdapter 구현체 추가

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ManualHandlerAdapter.java
@@ -0,0 +1,21 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.asis.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ManualHandlerAdapter implements HandlerAdapter {
+
+    @Override
+    public boolean supports(final Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler)
+            throws Exception {
+        return ((Controller) handler).execute(request, response);
+    }
+}
