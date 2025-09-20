955f3ba116e1edbffa12925ad0261d5f889c5962
kimdev <kim__dev@hufs.ac.kr>
2025-09-20 23:36:26 +0900

    refactor: 핸들러 종류에 따른 실행 분기를 담당하는 HandlerAdapter 클래스 생성

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.controller.Controller;
+import com.interface21.webmvc.servlet.handler.HandlerAdapter;
 import com.interface21.webmvc.servlet.handler.HandlerExecution;
 import com.interface21.webmvc.servlet.view.JspView;
 import com.interface21.webmvc.servlet.view.ModelAndView;
@@ -32,23 +33,7 @@ public class DispatcherServlet extends HttpServlet {
         log.debug("Method : {}, Request URI : {}", httpServletRequest.getMethod(), requestURI);
 
         Object handler = this.handlerMappings.getHandler(httpServletRequest);
-        ModelAndView modelAndView = executeHandler(handler, httpServletRequest, httpServletResponse);
+        ModelAndView modelAndView = HandlerAdapter.executeHandler(handler, httpServletRequest, httpServletResponse);
         modelAndView.render(httpServletRequest, httpServletResponse);
-    }
-
-    private ModelAndView executeHandler(Object handler, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException {
-        try {
-            if (handler instanceof Controller) {
-                String viewName = ((Controller) handler).execute(httpServletRequest, httpServletResponse);
-                return new ModelAndView(new JspView(viewName));
-            } else if (handler instanceof HandlerExecution) {
-                return ((HandlerExecution) handler).handle(httpServletRequest, httpServletResponse);
-            } else {
-                throw new ServletException("지원하지 않는 응답 방식입니다.");
-            }
-        } catch (Exception exception) {
-            log.error("Exception : {}", exception.getMessage(), exception);
-            throw new ServletException(exception.getMessage());
-        }
     }
 }
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/handler/HandlerAdapter.java
@@ -0,0 +1,31 @@
+package com.interface21.webmvc.servlet.handler;
+
+import com.interface21.webmvc.servlet.controller.Controller;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.interface21.webmvc.servlet.view.ModelAndView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HandlerAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(HandlerAdapter.class);
+
+    public static ModelAndView executeHandler(Object handler, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException {
+        try {
+            if (handler instanceof Controller) {
+                String viewName = ((Controller) handler).execute(httpServletRequest, httpServletResponse);
+                return new ModelAndView(new JspView(viewName));
+            } else if (handler instanceof HandlerExecution) {
+                return ((HandlerExecution) handler).handle(httpServletRequest, httpServletResponse);
+            } else {
+                throw new ServletException("지원하지 않는 응답 방식입니다.");
+            }
+        } catch (Exception exception) {
+            log.error("Exception : {}", exception.getMessage(), exception);
+            throw new ServletException(exception.getMessage());
+        }
+    }
+}
