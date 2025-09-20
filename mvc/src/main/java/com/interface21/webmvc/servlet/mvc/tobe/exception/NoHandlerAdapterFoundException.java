1d7b2559e2ad098e95dfc0ab4451699bc5464c6a
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-09-21 03:12:42 +0900

    refactor: 예외 클래스 리팩토링 및 패키지 이동
    - NoHandlerFoundException 등 예외 클래스 패키지를 exception으로 이동
    - NoHandlerAdapterFoundException 및 NoHandlerMappingFoundException 클래스 추가
    - 예외 처리 구분을 위한 클래스 분리 및 가독성 향상

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/exception/NoHandlerAdapterFoundException.java
@@ -0,0 +1,7 @@
+package com.interface21.webmvc.servlet.mvc.tobe.exception;
+
+public class NoHandlerAdapterFoundException extends RuntimeException {
+    public NoHandlerAdapterFoundException(String message) {
+        super(message);
+    }
+}
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/exception/NoHandlerMappingFoundException.java
@@ -0,0 +1,7 @@
+package com.interface21.webmvc.servlet.mvc.tobe.exception;
+
+public class NoHandlerMappingFoundException extends RuntimeException {
+    public NoHandlerMappingFoundException(String message) {
+        super(message);
+    }
+}
