1d1075bf2fd624baf6515247f5397971ae8003aa
jbilee <julishahn@gmail.com>
2025-09-20 20:00:17 +0900

    refactor: RegisterController 및 RegisterViewController를 애노테이션 기반으로 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -4,8 +4,6 @@ import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
 import com.techcourse.controller.LogoutController;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.RegisterViewController;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,8 +20,6 @@ public class ManualHandlerMapping implements HandlerMapping {
     public void initialize() {
         controllers.put("/", new ForwardController("/index.jsp"));
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
