38f0caa594be9d2cd7a050af30c7aa116e745054
Gabin Choi <choegabin171@gmail.com>
2025-09-21 01:50:44 +0900

    refactor: add objectWriter

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -2,6 +2,7 @@ package com.interface21.webmvc.servlet.view;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,22 +13,23 @@ public class JsonView implements View {
 
     // ObjectMapper는 생성비용이 비싸기 때문에 static 으로 단일 공유
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectWriter objectWriter = objectMapper.writer();
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        String json = convertModelToJson(model);
+        final String json = convertModelToJson(model);
         response.getWriter().write(json);
     }
 
     private String convertModelToJson(Map<String, ?> model) {
         try {
             if (model.size() == 1) {
-                Object singleValue = model.values().iterator().next();
-                return objectMapper.writeValueAsString(singleValue);
+                final Object singleValue = model.values().iterator().next();
+                return objectWriter.writeValueAsString(singleValue);
             }
-            return objectMapper.writeValueAsString(model);
+            return objectWriter.writeValueAsString(model);
         } catch (JsonProcessingException ex) {
             throw new IllegalArgumentException("json 응답 생성에 실패하였습니다.");
         }
