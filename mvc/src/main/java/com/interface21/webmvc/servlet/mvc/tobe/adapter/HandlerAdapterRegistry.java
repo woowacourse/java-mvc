f6d6cc7810c8b104b70bc3d6615beda8ee3f331f
ryan-dia <cjfdnjs158@naver.com>
2025-09-20 14:16:54 +0900

    feat: HandlerAdapterRegistry 구현

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/HandlerAdapterRegistry.java
@@ -0,0 +1,23 @@
+package com.interface21.webmvc.servlet.mvc.tobe.adapter;
+
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerAdapterRegistry {
+
+    private  final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public void addHandlerAdapter(HandlerAdapter adapter) {
+        handlerAdapters.add(adapter);
+    }
+
+    public HandlerAdapter getHandlerAdapter(Object handler) throws NoHandlerAdapterFoundException {
+        for (HandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new NoHandlerAdapterFoundException("핸들러 [" + handler.getClass().getName() + "] 를 실행할 수 있는 어댑터가 없습니다.");
+    }
+}
---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/NoHandlerAdapterFoundException.java
@@ -0,0 +1,6 @@
+package com.interface21.webmvc.servlet.mvc.tobe.adapter;
+
+public class NoHandlerAdapterFoundException extends Throwable {
+    public NoHandlerAdapterFoundException(String format) {
+    }
+}
