7d5812964a18ef6593d77806f5dfa02448465bb9
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-21 05:02:05 +0900

    refactor: AnnotationHandlerMapping의 컨트롤러 스캔 로직 분리
    - annotationHandlerMapping 패키지 이동, 관련 테스트 삭제

---
+++ app/src/main/java/com/techcourse/AnnotationHandlerMapping.java
@@ -0,0 +1,32 @@
+package com.techcourse;
+
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.mapping.HandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.ControllerScanner;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecution;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerKey;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class AnnotationHandlerMapping implements HandlerMapping {
+
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public static AnnotationHandlerMapping from(final Object... basePackages) {
+        final Map<HandlerKey, HandlerExecution> handlerExecutions = new ControllerScanner(basePackages).process();
+        return new AnnotationHandlerMapping(handlerExecutions);
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        final HandlerKey handlerKey = new HandlerKey(
+                request.getRequestURI(),
+                RequestMethod.valueOf(request.getMethod()));
+
+        return handlerExecutions.get(handlerKey);
+    }
+}
