fd7bf4180c975a7445797d690d35b161f581b561
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-21 05:33:53 +0900

    refactor: 회원가입 컨트롤러 어노테이션으로 변경

--- app/src/main/java/com/techcourse/ManualHandlerMapping.java
+++ app/src/main/java/com/techcourse/ManualHandlerMapping.java
@@ -1,34 +1,40 @@
 package com.techcourse;
 
-import com.techcourse.controller.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.interface21.webmvc.servlet.mapping.HandlerMapping;
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
-
+import com.techcourse.controller.LoginController;
+import com.techcourse.controller.LoginViewController;
+import com.techcourse.controller.LogoutController;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-public class ManualHandlerMapping {
+@Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ManualHandlerMapping implements HandlerMapping {
 
-    private static final Logger log = LoggerFactory.getLogger(ManualHandlerMapping.class);
+    private final Map<String, Controller> controllers;
 
-    private static final Map<String, Controller> controllers = new HashMap<>();
-
-    public void initialize() {
+    public static ManualHandlerMapping initialize() {
+        final Map<String, Controller> controllers = new HashMap<>();
         controllers.put("/", new ForwardController("/index.jsp"));
         controllers.put("/login", new LoginController());
         controllers.put("/login/view", new LoginViewController());
         controllers.put("/logout", new LogoutController());
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
 
         log.info("Initialized Handler Mapping!");
         controllers.keySet()
                 .forEach(path -> log.info("Path : {}, Controller : {}", path, controllers.get(path).getClass()));
+
+        return new ManualHandlerMapping(controllers);
     }
 
-    public Controller getHandler(final String requestURI) {
+    public Object getHandler(final HttpServletRequest request) {
+        final String requestURI = request.getRequestURI();
         log.debug("Request Mapping Uri : {}", requestURI);
         return controllers.get(requestURI);
     }
