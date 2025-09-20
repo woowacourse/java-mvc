b90cf88ecca388058eb9e6c8aed993ce4f189e39
jbilee <julishahn@gmail.com>
2025-09-20 20:06:24 +0900

    refactor: ForwardController를 애노테이션 기반으로 변경

---
+++ app/src/main/java/com/techcourse/controller/ForwardController.java
@@ -0,0 +1,18 @@
+package com.techcourse.controller;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspViewRenderer;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class ForwardController implements JspViewRenderer {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView indexView(final HttpServletRequest request, final HttpServletResponse response) {
+        return showPage("/index.jsp");
+    }
+}
