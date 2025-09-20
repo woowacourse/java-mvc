184989145830fc1e7a23b5e4a6fc94d1eff623dd
GiSeok Kang <ime.rokz@gmail.com>
2025-09-20 16:38:16 +0900

    refactor: JSON 응답 시 model에 데이터가 1개 이상이면 Map 형태로 전송하도록 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -6,21 +6,34 @@ import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JsonView implements View {
 
     @Override
     public void render(final HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<Object> attributes = Collections.list(request.getAttributeNames()).stream()
-            .map(request::getAttribute)
-            .toList();
-
-        ServletOutputStream outputStream = response.getOutputStream();
-        objectMapper.writeValue(outputStream, attributes);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ServletOutputStream outputStream = response.getOutputStream();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<String> attributeNames = Collections.list(request.getAttributeNames());
+        if (attributeNames.size() == 1) {
+            String name = attributeNames.getFirst();
+            Object attribute = request.getAttribute(name);
+
+            objectMapper.writeValue(outputStream, attribute);
+            return;
+        }
+
+        Map<String, Object> attributes = attributeNames.stream()
+            .collect(Collectors.toMap(
+                name -> name,
+                request::getAttribute
+            ));
+        objectMapper.writeValue(outputStream, attributes);
     }
 
     @Override
