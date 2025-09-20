52f3f5a4559446783e17059bcfb9eaaa3898d019
chaeyounglee <113325033+Chaeyoung714@users.noreply.github.com>
2025-09-20 23:49:34 +0900

    [1단계 - @MVC 프레임워크 구현하기] 모다(이채영) 미션 제출합니다. (#1011)
    * quest 1
    
    * 학습 테스트
    
    * chore: 의존성 추가
    
    * feat: HanlderExecution 및 AnnotationHandlerMapping 클래스 추가
    
    * test: AnnotationHandlerMappingTest 통과
    
    * feat: JspView 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.sql.Ref;
+import java.util.*;
 
 public class AnnotationHandlerMapping {
 
@@ -16,14 +23,44 @@ public class AnnotationHandlerMapping {
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackage = basePackage;
-        this.handlerExecutions = new HashMap<>();
+        handlerExecutions = new HashMap<>();
     }
 
-    public void initialize() {
+    public void initialize() throws Exception {
+        for (Object bp : basePackage) {
+            Reflections reflections = new Reflections(bp, Scanners.TypesAnnotated);
+            Set<Class<?>> controllerTypes = reflections.getTypesAnnotatedWith(Controller.class);
+
+            for (Class<?> controllerType : controllerTypes) {
+                Object controller = controllerType.getDeclaredConstructor().newInstance();
+
+                List<Method> methods = Arrays.stream(controllerType.getDeclaredMethods())
+                        .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                        .toList();
+
+                methods.forEach(
+                        method -> {
+                            if (method.isAnnotationPresent(RequestMapping.class)) {
+                                RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                                handlerExecutions.put(
+                                        new HandlerKey(requestMapping.value(), requestMapping.method()[0]),
+                                        new HandlerExecution(controller, method)
+                                );
+                            }
+                        }
+                );
+            }
+        }
+
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        String requestURI = request.getRequestURI();
+        RequestMethod method = RequestMethod.valueOf(request.getMethod());
+
+        HandlerKey handlerKey = new HandlerKey(requestURI, method);
+
+        return handlerExecutions.get(handlerKey);
     }
 }
--- study/src/test/java/reflection/Junit4TestRunner.java
+++ study/src/test/java/reflection/Junit4TestRunner.java
@@ -1,6 +1,11 @@
 package reflection;
 
 import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import java.lang.reflect.Method;
+import java.util.Set;
 
 class Junit4TestRunner {
 
@@ -8,6 +13,26 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // sol1
+        System.out.println("---With Reflection Library---");
+
+        Reflections reflection = new Reflections("reflection", Scanners.MethodsAnnotated);
+
+        Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+
+        Set<Method> methods = reflection.getMethodsAnnotatedWith(MyTest.class);
+        for (Method method : methods) {
+            method.invoke(junit4Test);
+        }
+
+        // sol2
+        System.out.println("\n---With Class Api---");
+
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(junit4Test);
+            }
+        }
     }
 }
