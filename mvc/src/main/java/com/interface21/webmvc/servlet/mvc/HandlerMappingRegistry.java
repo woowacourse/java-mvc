ffa362b04f8ca9a0b0866f21e16457ac1aa48e24
RaZel713 <razel7130@gmail.com>
2025-09-20 19:26:35 +0900

    feat: HandlerMappingRegistry 구현

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerMappingRegistry.java
@@ -0,0 +1,33 @@
+package com.interface21.webmvc.servlet.mvc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HandlerMappingRegistry {
+
+    private final List<HandlerMapping> handlerMappings;
+
+    public HandlerMappingRegistry(final List<HandlerMapping> handlerMappings) {
+        this.handlerMappings = handlerMappings;
+    }
+
+    public static HandlerMappingRegistry createEmpty() {
+        return new HandlerMappingRegistry(new ArrayList<>());
+    }
+
+    public void initialize() {
+        handlerMappings.forEach(HandlerMapping::initialize);
+    }
+
+    public void addHandlerMapping(final HandlerMapping handlerMapping) {
+        handlerMappings.add(handlerMapping);
+    }
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("요청을 처리할 수 없습니다."));
+    }
+}
