4590391145cb48e1ada939d640a184cf13e5af5f
yush <yush2202@gmail.com>
2025-09-20 20:26:29 +0900

    refactor: ControllerScanner 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,14 +1,13 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,24 +17,19 @@ public class AnnotationHandlerMapping {
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final ControllerScanner controllerScanner;
 
-    public AnnotationHandlerMapping(final Object... basePackage) {
+    public AnnotationHandlerMapping(final Object... basePackage)
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
+        this.controllerScanner = new ControllerScanner(basePackage);
     }
 
     // 여기에서 초기화하면서 handlerExceptions 채우기
     public void initialize() {
-        // @Controller 가 달린 클래스 찾기
-        Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> annotatedControllers = findAnnotatedControllers(reflections);
-
-        setHandlerExecutions(annotatedControllers);
+        setHandlerExecutions(controllerScanner.getControllerClass());
         log.info("Initialized AnnotationHandlerMapping!");
-    }
-
-    private Set<Class<?>> findAnnotatedControllers(Reflections reflections) {
-        return reflections.getTypesAnnotatedWith(Controller.class);
     }
 
     private void setHandlerExecutions(Set<Class<?>> annotatedControllers) {
@@ -47,13 +41,13 @@ public class AnnotationHandlerMapping {
             registerHandler(controller, method);
         }
     }
-    
+
     private void registerHandler(Class<?> controller, Method method) {
         if (!method.isAnnotationPresent(RequestMapping.class)) {
             return;
         }
         RequestMapping mapping = method.getAnnotation(RequestMapping.class);
-        HandlerExecution execution = new HandlerExecution(controller, method);
+        HandlerExecution execution = new HandlerExecution(controllerScanner.getControllerInstance(controller), method);
 
         for (RequestMethod httpMethod : mapping.method()) {
             HandlerKey key = new HandlerKey(mapping.value(), httpMethod);
@@ -62,9 +56,6 @@ public class AnnotationHandlerMapping {
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        // 여기서 HandlerExecution 이 나가야함.
-        // 하는 것 = request를 받아서 handlerExecution 찾아서 반환.
-        // key는 handler key
         RequestMethod requestMethod = RequestMethod.getRequestMethodBy(request.getMethod());
         HandlerKey key = new HandlerKey(request.getRequestURI(), requestMethod);
 
