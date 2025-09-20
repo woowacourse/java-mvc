35dd720fdd2dd54053ba00a52bcbe6cd55db8703
egaeng09 <egaeng09@gmail.com>
2025-09-21 03:36:22 +0900

    refactor: ControllerScanner 추가

---
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -0,0 +1,71 @@
+package com.interface21.webmvc.servlet.mvc.tobe;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import jakarta.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnnotationHandlerMapping {
+
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
+    private final Object[] basePackages;
+    private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+
+    public AnnotationHandlerMapping(final Object... basePackages) {
+        this.basePackages = basePackages;
+        this.handlerExecutions = new HashMap<>();
+    }
+
+    public void initialize() {
+        log.info("Initialized AnnotationHandlerMapping!");
+        final ControllerScanner controllerScanner = new ControllerScanner(basePackages);
+        final Map<Class<?>, Object> controllers = controllerScanner.scan();
+
+        for (Map.Entry<Class<?>, Object> entry : controllers.entrySet()) {
+            Class<?> clazz = entry.getKey();
+            Object controller = entry.getValue();
+
+            final Set<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                    .filter(m -> m.isAnnotationPresent(RequestMapping.class))
+                    .collect(Collectors.toSet());
+
+            for (Method method : methods) {
+                final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                RequestMethod[] requestMethods = requestMapping.method();
+
+                if (requestMethods.length == 0) {
+                    requestMethods = RequestMethod.values();
+                }
+
+                for (RequestMethod requestMethod : requestMethods) {
+                    final String url = requestMapping.value();
+                    final var handlerKey = new HandlerKey(url, requestMethod);
+                    if (handlerExecutions.containsKey(handlerKey)) {
+                        throw new IllegalStateException("Duplicate mapping found: " + handlerKey);
+                    }
+                    final var handlerExecution = new HandlerExecution(controller, method);
+                    handlerExecutions.put(handlerKey, handlerExecution);
+                    log.info("Mapping {} {}", requestMethod, url);
+                }
+            }
+        }
+    }
+
+
+    public Object getHandler(final HttpServletRequest request) {
+        return handlerExecutions.get(
+                new HandlerKey(request.getRequestURI(), RequestMethod.valueOf(request.getMethod())));
+    }
+}
