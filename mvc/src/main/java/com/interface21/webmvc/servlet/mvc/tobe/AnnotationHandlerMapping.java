599d7d1ca6642c19ca7c0fe5b405147fe0f380ad
nourzoo <tmddus4671@gmail.com>
2025-09-21 02:04:24 +0900

    feat: handlerAdapter 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,14 +8,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Map.Entry;
 import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
-
-    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
@@ -31,22 +26,37 @@ public class AnnotationHandlerMapping {
      * requestMethod)키로 HandlerExecution를 등록한다
      */
     public void initialize() {
+
         Reflections reflections = new Reflections(basePackage);
-        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        ControllerScanner controllerScanner = new ControllerScanner(reflections);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
 
         try {
-            for (Class<?> controllerClass : controllerClasses) {
-                // 컨트롤러 기본 생성자를 실행해서 인스턴스 생성
-                Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+            for (Entry<Class<?>, Object> entry : controllers.entrySet()) {
+                Class<?> clazz = entry.getKey();
+                Object instance = entry.getValue();
+
                 // @RequestMapping 붙은 메서드만 골라냄 ex) findUserId(), save()
-                List<Method> methods = getRequestMappingMethods(controllerClass);
+                List<Method> methods = getRequestMappingMethods(clazz);
                 // HandlerExecution 테이블에 등록
-                methods.forEach(method -> putHandlerExecutionByRequestMapping(controllerInstance, method));
+                methods.forEach(method -> putHandlerExecutionByRequestMapping(handlerExecutions, instance, method));
             }
         } catch (Exception e) {
             throw new IllegalStateException("HandlerMapping initialization failed", e);
         }
-        log.info("Initialized AnnotationHandlerMapping!");
+    }
+
+    /**
+     * HttpServletRequest를 통해 실행할 HandlerExecution을 찾는다 요청에서 uri, http method 추출해서 handlerKey 만들고 이 키로 handlerExecution
+     * 조회
+     */
+    @Override
+    public Object getHandler(final HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        RequestMethod method = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, method);
+
+        return handlerExecutions.get(handlerKey);
     }
 
     private List<Method> getRequestMappingMethods(final Class<?> controllerClass) {
@@ -56,7 +66,11 @@ public class AnnotationHandlerMapping {
                 .toList();
     }
 
-    private void putHandlerExecutionByRequestMapping(final Object instance, final Method method) {
+    private void putHandlerExecutionByRequestMapping(
+            final Map<HandlerKey, HandlerExecution> handlerExecutions,
+            final Object instance,
+            final Method method
+    ) {
         RequestMapping mapping = method.getAnnotation(RequestMapping.class);
         if (mapping == null) {
             return;
@@ -73,17 +87,5 @@ public class AnnotationHandlerMapping {
             HandlerExecution execution = new HandlerExecution(instance, method);
             handlerExecutions.put(handlerKey, execution);
         }
-    }
-
-    /**
-     * HttpServletRequest를 통해 실행할 HandlerExecution을 찾는다 요청에서 uri, http method 추출해서 handlerKey 만들고 이 키로 handlerExecution
-     * 조회
-     */
-    public Object getHandler(final HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        RequestMethod method = RequestMethod.valueOf(request.getMethod());
-        HandlerKey handlerKey = new HandlerKey(requestURI, method);
-
-        return handlerExecutions.get(handlerKey);
     }
 }
