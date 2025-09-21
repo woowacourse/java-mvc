55d8cfafea197b31cf62695c1110333dcae5aac4
changui <122252160+changuii@users.noreply.github.com>
2025-09-21 17:50:18 +0900

    [3단계 - JSON View 구현하기] 부기(이창의) 미션 제출합니다! (#990)
    * feat: JsonView 구현
    
    * feat: 레거시 MVC
    
    * refactor: 잘못된 요청 지점 제거
    
    * refactor: body의 길이를 헤더에 담는 작업을 서블릿 컨테이너에 위임

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -6,22 +6,27 @@ import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping implements HandlerMapping{
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+    private static final String DEFAULT_PACKAGE = "com.interface21.webmvc.servlet.mvc";
 
-    private final Object[] basePackage;
+    private final List<Object> basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
-        this.basePackage = basePackage;
+        this.basePackage = Arrays.stream(basePackage)
+                .collect(Collectors.toList());
+        this.basePackage.add(DEFAULT_PACKAGE);
         this.handlerExecutions = new HashMap<>();
         initialize();
     }
