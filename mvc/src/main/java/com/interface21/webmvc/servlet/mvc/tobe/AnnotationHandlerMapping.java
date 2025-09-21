40664961f3806a4c036556369de2bc61526b9f61
egaeng09 <egaeng09@gmail.com>
2025-09-21 22:02:44 +0900

    refactor: AnnotationHandlerMapping.initialize 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,15 +7,13 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.reflections.ReflectionUtils;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
@@ -34,34 +31,43 @@ public class AnnotationHandlerMapping {
         final ControllerScanner controllerScanner = new ControllerScanner(basePackages);
         final Map<Class<?>, Object> controllers = controllerScanner.scan();
 
-        for (Map.Entry<Class<?>, Object> entry : controllers.entrySet()) {
-            Class<?> clazz = entry.getKey();
-            Object controller = entry.getValue();
+        controllers.forEach(this::registerHandlerMethods);
+    }
 
-            final Set<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
-                    .filter(m -> m.isAnnotationPresent(RequestMapping.class))
-                    .collect(Collectors.toSet());
+    private void registerHandlerMethods(Class<?> clazz, Object controller) {
+        getRequestMappingMethods(clazz).forEach(method -> {
+            final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            final RequestMethod[] requestMethods = resolveRequestMethods(requestMapping);
 
-            for (Method method : methods) {
-                final RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                RequestMethod[] requestMethods = requestMapping.method();
-
-                if (requestMethods.length == 0) {
-                    requestMethods = RequestMethod.values();
-                }
-
-                for (RequestMethod requestMethod : requestMethods) {
-                    final String url = requestMapping.value();
-                    final var handlerKey = new HandlerKey(url, requestMethod);
-                    if (handlerExecutions.containsKey(handlerKey)) {
-                        throw new IllegalStateException("Duplicate mapping found: " + handlerKey);
-                    }
-                    final var handlerExecution = new HandlerExecution(controller, method);
-                    handlerExecutions.put(handlerKey, handlerExecution);
-                    log.info("Mapping {} {}", requestMethod, url);
-                }
+            for (RequestMethod requestMethod : requestMethods) {
+                registerHandler(controller, method, requestMapping.value(), requestMethod);
             }
+        });
+    }
+
+    private Set<Method> getRequestMappingMethods(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(RequestMapping.class))
+                .collect(Collectors.toSet());
+    }
+
+    private RequestMethod[] resolveRequestMethods(RequestMapping requestMapping) {
+        RequestMethod[] methods = requestMapping.method();
+        if (methods.length == 0) {
+            return RequestMethod.values();
         }
+        return methods;
+    }
+
+    private void registerHandler(Object controller, Method method, String url, RequestMethod requestMethod) {
+        final var handlerKey = new HandlerKey(url, requestMethod);
+
+        if (handlerExecutions.containsKey(handlerKey)) {
+            throw new IllegalStateException("Duplicate mapping found: " + handlerKey);
+        }
+
+        handlerExecutions.put(handlerKey, new HandlerExecution(controller, method));
+        log.info("Mapping {} {}", requestMethod, url);
     }
 
     public Object getHandler(final HttpServletRequest request) {
