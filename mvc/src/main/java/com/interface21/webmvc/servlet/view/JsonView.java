836a7df28566e69b45898d07684ad559afc9c102
Jin409 <sualng123@gmail.com>
2025-09-21 18:26:29 +0900

    feat: `JsonView` 에서 JSON 형태로 쓰도록 하는 기능 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,7 +10,16 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        if (model.isEmpty()) {
+            return;
+        }
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        OBJECT_MAPPER.writeValue(response.getWriter(), model);
     }
 }
