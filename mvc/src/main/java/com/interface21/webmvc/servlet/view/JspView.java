c99471b5adec335c5f094dd23095b22a693649bc
Chaeyoung714 <202102760@hufs.ac.kr>
2025-09-21 15:57:55 +0900

    feat: JspView에서 jsp 파일 읽기 기능 추가 및 viewName 반환 기능 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JspView.java
@@ -1,12 +1,15 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.interface21.webmvc.servlet.View;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.NoSuchFileException;
 import java.util.Map;
 
 public class JspView implements View {
@@ -35,7 +38,22 @@ public class JspView implements View {
         });
 
         String path = "/webapp/" + viewName + ".jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-        dispatcher.forward(request, response);
+        String fileContent = new String(readFile(path), StandardCharsets.UTF_8);
+
+        response.getWriter().write(fileContent);
+    }
+
+    @Override
+    public String getViewName() {
+        return viewName;
+    }
+
+    private byte[] readFile(String path) throws IOException {
+        try (final InputStream fileStream = getClass().getClassLoader().getResourceAsStream(path)) {
+            if (fileStream == null) {
+                throw new NoSuchFileException(String.format("No Such file : location = %s", path));
+            }
+            return fileStream.readAllBytes();
+        }
     }
 }
