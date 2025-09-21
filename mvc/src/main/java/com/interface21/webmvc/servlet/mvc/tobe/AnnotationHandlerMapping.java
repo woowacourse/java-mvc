4f950bd554ce89a4040e240bdb8c2b2230e658a9
taek2222 <oot309@daum.net>
2025-09-21 20:46:58 +0900

    feat: 미션 1단계 수행

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,16 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +25,43 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
-        log.info("Initialized AnnotationHandlerMapping!");
+        Reflections reflections = new Reflections(basePackage);
+
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> aClass : classes) {
+
+            try {
+                Object controllerInstance = aClass.getDeclaredConstructor().newInstance();
+                Method[] methods = aClass.getMethods();
+
+                for (Method method : methods) {
+                    if (!method.isAnnotationPresent(RequestMapping.class)) {
+                        continue;
+                    }
+
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    String uri = requestMapping.value();
+                    RequestMethod[] httpMethods = requestMapping.method();
+
+                    for (RequestMethod httpMethod : httpMethods) {
+                        HandlerKey handlerKey = new HandlerKey(uri, httpMethod);
+                        HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+
+                        handlerExecutions.put(handlerKey, handlerExecution);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Failed to initialize handler for class {}", aClass, e);
+            }
+        }
     }
 
     public Object getHandler(final HttpServletRequest request) {
-        return null;
+        return handlerExecutions.get(
+                new HandlerKey(
+                        request.getRequestURI(),
+                        RequestMethod.valueOf(request.getMethod())
+                )
+        );
     }
 }
