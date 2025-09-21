25fb301188ec5407a8f23ec80562ca94d6e381e3
taek2222 <oot309@daum.net>
2025-09-21 21:46:49 +0900

    refactor: 메서드 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -29,30 +29,41 @@ public class AnnotationHandlerMapping {
 
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
         for (Class<?> aClass : classes) {
+            registerController(aClass);
+        }
+    }
 
-            try {
-                Object controllerInstance = aClass.getDeclaredConstructor().newInstance();
-                Method[] methods = aClass.getMethods();
+    private void registerController(Class<?> aClass) {
+        try {
+            Object controllerInstance = aClass.getDeclaredConstructor().newInstance();
+            Method[] methods = aClass.getMethods();
 
-                for (Method method : methods) {
-                    if (!method.isAnnotationPresent(RequestMapping.class)) {
-                        continue;
-                    }
+            registerMethod(methods, controllerInstance);
+        } catch (Exception e) {
+            log.error("Failed to initialize handler for class {}", aClass, e);
+        }
+    }
 
-                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
-                    String uri = requestMapping.value();
-                    RequestMethod[] httpMethods = requestMapping.method();
-
-                    for (RequestMethod httpMethod : httpMethods) {
-                        HandlerKey handlerKey = new HandlerKey(uri, httpMethod);
-                        HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
-
-                        handlerExecutions.put(handlerKey, handlerExecution);
-                    }
-                }
-            } catch (Exception e) {
-                log.error("Failed to initialize handler for class {}", aClass, e);
+    private void registerMethod(Method[] methods, Object controllerInstance) {
+        for (Method method : methods) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
             }
+
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            String uri = requestMapping.value();
+            RequestMethod[] httpMethods = requestMapping.method();
+
+            registerHttpMethod(controllerInstance, method, httpMethods, uri);
+        }
+    }
+
+    private void registerHttpMethod(Object controllerInstance, Method method, RequestMethod[] httpMethods, String uri) {
+        for (RequestMethod httpMethod : httpMethods) {
+            HandlerKey handlerKey = new HandlerKey(uri, httpMethod);
+            HandlerExecution handlerExecution = new HandlerExecution(controllerInstance, method);
+
+            handlerExecutions.put(handlerKey, handlerExecution);
         }
     }
 
