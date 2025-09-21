11a86ab6f0e72eec02d7c446f414c9df64b2d7a0
yush <yush2202@gmail.com>
2025-09-21 18:04:37 +0900

    refactor: 핸들러의 종류에 따라 다른 어댑터를 관리하는 기능 추가 및 DispatcherServlet 수정

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,51 +1,74 @@
 package com.techcourse;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.View;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdaptor;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdaptor;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdaptorRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMappingRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.ManualHandlerAdaptor;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final HandlerMappingRegistry handlerMappingRegistry = new HandlerMappingRegistry();
+    private final HandlerAdaptorRegistry handlerAdaptorRegistry = new HandlerAdaptorRegistry();
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        try {
+            AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping();
+            ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
+
+            handlerMappingRegistry.addHandler(annotationHandlerMapping);
+            handlerMappingRegistry.addHandler(manualHandlerMapping);
+
+            handlerMappingRegistry.initialize();
+
+            AnnotationHandlerAdaptor annotationHandlerAdaptor = new AnnotationHandlerAdaptor();
+            ManualHandlerAdaptor manualHandlerAdaptor = new ManualHandlerAdaptor();
+
+            handlerAdaptorRegistry.addHandlerAdaptor(annotationHandlerAdaptor);
+            handlerAdaptorRegistry.addHandlerAdaptor(manualHandlerAdaptor);
+
+        } catch (InvocationTargetException | IllegalAccessException | InstantiationException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            render(request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
-        }
+    private void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Object handler = handlerMappingRegistry.getHandler(request);
+        HandlerAdaptor handlerAdaptor = handlerAdaptorRegistry.getHandlerAdaptor(handler);
 
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
+        ModelAndView modelAndView = handlerAdaptor.handle(request, response, handler);
+        View view = modelAndView.getView();
+        view.render(modelAndView.getModel(), request, response);
     }
 }
