35b2f6e45bd2c230fc01b151d8dcd0ce2db027cd
LEE SUNG WOO <108395670+CodingMasterLSW@users.noreply.github.com>
2025-09-21 13:03:43 +0900

    [2단계 - 점진적인 리팩터링] 젠슨(이성우) 미션 제출합니다. (#981)
    * refactor: 2단계 초기 구현
    
    - 두 개의 handlerMapping을 함께 사용할 수 있도록 구현함
    
    * refactor: handlerAdapter 도입
    
    * refactor: handlerMappings, handlerAdaters 일급컬렉션 도입

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,10 +1,12 @@
 package com.techcourse;
 
 import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,26 +15,34 @@ public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private HandlerMappings handlerMappings;
+    private HandlerAdapters handlerAdapters;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
-        manualHandlerMapping.initialize();
+        this.handlerMappings = new HandlerMappings(List.of(new ManualHandlerMapping(), new AnnotationHandlerMapping()));
+        this.handlerAdapters = new HandlerAdapters(List.of(new ControllerHandlerAdapter(), new HandlerExecutionAdapter()));
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final ModelAndView modelAndView = controller.execute(request, response);
+            final Object handler = handlerMappings.getHandler(request);
+            if (handler == null) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+            final HandlerAdapter adapter = handlerAdapters.getHandlerAdapter(handler);
+            final ModelAndView modelAndView = adapter.handle(request, response, handler);
             modelAndView.render(request, response);
+
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
---
+++ app/src/main/java/com/techcourse/HandlerAdapters.java
@@ -0,0 +1,21 @@
+package com.techcourse;
+
+import java.util.List;
+
+public class HandlerAdapters {
+
+    private final List<HandlerAdapter> handlerAdapters;
+
+    public HandlerAdapters(final List<HandlerAdapter> handlerAdapters) {
+        this.handlerAdapters = handlerAdapters;
+    }
+
+    public HandlerAdapter getHandlerAdapter(Object handler) {
+        for (HandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalStateException("No adapter found for handler: " + handler);
+    }
+}
