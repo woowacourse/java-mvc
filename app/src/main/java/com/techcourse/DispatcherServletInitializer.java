eeaf18c4d7acb3d7a9242564b0135aae56643509
supernovaMK <minkikim2002@gmail.com>
2025-09-21 17:25:15 +0900

    refactor: DispatcherServletInitializer 패키지 이동

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -1,6 +1,7 @@
-package com.interface21.webmvc.servlet.mvc.tobe;
+package com.techcourse;
 
 import com.interface21.web.WebApplicationInitializer;
+import com.interface21.webmvc.servlet.mvc.tobe.DispatcherServlet;
 import jakarta.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
