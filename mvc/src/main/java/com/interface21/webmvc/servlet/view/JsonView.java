506c67b1905c5d9da4675c926aaccab2b3447d77
jye326 <jye326@naver.com>
2025-09-21 23:14:04 +0900

    feat: JsonView에 JSON 직렬화 및 응답 처리 로직 추가
    - ObjectMapper를 활용한 JSON 직렬화 로직 추가
    - 응답 Content-Type을 "application/json;charset=UTF-8"로 설정
    - 모델 값이 1개일 경우 해당 값만 JSON으로 직렬화
    - PrintWriter를 사용해 JSON 데이터를 HTTP 응답에 작성 및 전송 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType("application/json;charset=UTF-8");
+
+        Object body;
+        if (model.size() == 1) {
+            // 값이 1개뿐이면, 그 value만 꺼내서 JSON 직렬화
+            body = model.values().iterator().next();
+        } else {
+            // 값이 2개 이상이면 Map 전체를 JSON 직렬화
+            body = model;
+        }
+
+        String json = objectMapper.writeValueAsString(body);
+
+        try(PrintWriter printWriter = response.getWriter()){
+            printWriter.write(json);
+            printWriter.flush();
+        }
     }
 }
