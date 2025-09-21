a6f8d288daaa7110dde213201f15f2eb204d97d2
kimdev <kim__dev@hufs.ac.kr>
2025-09-21 14:39:03 +0900

    fix: logout 컨트롤러 수정

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -1,12 +1,17 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.view.JspView;
 import com.interface21.webmvc.servlet.view.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Controller
 public class LogoutController {
 
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView logout(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
