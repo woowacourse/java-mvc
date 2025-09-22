63122b3b8c200184f6409eb1f70e4604c5f3a1ae
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-22 21:50:59 +0900

    refactor: 수동 예외 처리를 orElseThrow로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/DispatcherServlet.java
@@ -23,10 +23,8 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response) {
         try {
-            Optional<Object> handler = handlerMappingRegistry.getHandler(request);
-            if (handler.isEmpty()) {
-                throw new IllegalStateException("no handler ");
-            }
+            Object handler = handlerMappingRegistry.getHandler(request)
+                    .orElseThrow(() -> new IllegalStateException("no handler "));
             HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
             ModelAndView modelAndView = handlerAdapter.handle(request, response, handlerAdapter);
 
