bc69eddbf72e35945212bde02dd39a79803bfc6f
kjungw <sagakkb@gmail.com>
2025-09-22 21:24:34 +0900

    refactor: UserController 메서드 분리

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -4,11 +4,13 @@ import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.view.JsonView;
+import com.interface21.webmvc.servlet.view.JspView;
 import com.interface21.webmvc.servlet.view.ModelAndView;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,15 +21,27 @@ public class UserController {
 
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
-        final String account = request.getParameter("account");
-        //TODO: account가 존재하지 않은 경우  (2025-09-22, 월, 13:24)
-        log.debug("user id : {}", account);
+        if (isValidSaveRequest(request)) {
+            return new ModelAndView(new JspView("redirect:/401.jsp"));
+        }
+
+        Optional<User> user = findUser(request);
+        if (user.isEmpty()) {
+            return new ModelAndView(new JspView("redirect:/401.jsp"));
+        }
 
         final ModelAndView modelAndView = new ModelAndView(new JsonView());
-        final User user = InMemoryUserRepository.findByAccount(account)
-                .orElseThrow();
-
-        modelAndView.addObject("user", user);
+        modelAndView.addObject("user", user.get());
         return modelAndView;
+    }
+
+    private boolean isValidSaveRequest(HttpServletRequest req) {
+        return req.getParameter("account") != null;
+    }
+
+    private Optional<User> findUser(HttpServletRequest request) {
+        final String account = request.getParameter("account");
+        log.debug("user id : {}", account);
+        return InMemoryUserRepository.findByAccount(account);
     }
 }
