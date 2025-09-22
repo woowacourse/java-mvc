bcd09b03caaafcb503ab282d01a672cf6de816ba
byesol <bsj5967@naver.com>
2025-09-23 00:50:09 +0900

    feat: JsonView 클래스 추가 및 UserController 구현
    - JsonView 클래스 생성 및 JSON 응답 처리 구현.
    - UserController에 `/api/user` 엔드포인트 추가.
    - 조회된 사용자 정보를 JSON 형태로 응답하도록 설정.

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,27 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setContentType("application/json;charset=UTF-8");
+        Object objectToRender;
+        if (model.size() == 1) {
+            objectToRender = model.values().iterator().next();
+        } else {
+            objectToRender = model;
+        }
+        var writer = response.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        writer.write(objectMapper.writeValueAsString(objectToRender));
     }
 }
