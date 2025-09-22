d6e8e73653123991b9e521ca492e90a3e8faaa11
jeyong <joon6093@naver.com>
2025-09-22 20:40:53 +0900

    feat: JsonView 클래스를 구현한다.

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,40 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.PrintWriter;
 import java.util.Map;
+
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(
+            final Map<String, ?> model,
+            final HttpServletRequest request,
+            final HttpServletResponse response
+    ) throws Exception {
+        final var value = extractValue(model);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try (PrintWriter writer = response.getWriter()) {
+            objectMapper.writeValue(writer, value);
+        }
+    }
+
+    private Object extractValue(final Map<String, ?> model) {
+        if (model.size() == 1) {
+            return model.values()
+                    .iterator()
+                    .next();
+        }
+
+        return model;
     }
 }
