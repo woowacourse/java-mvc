1a60cdf2c993d307c6a8adbbd1487df1a6cc6bde
Yeongjae Kim <sees77770@gmail.com>
2025-09-22 22:14:26 +0900

    [2단계 - 점진적인 리팩터링] 폰트(김영재) 미션 제출합니다. (#1030)
    * style: 변수 타입 수정
    
    * refactor: ModelAndView 방어 로직 추가
    
    * refactor: requestMapping에 메서드 명시 안할 시 처리 추가
    
    * refactor: ControllerScanner 추가
    
    * refactor: HandlerMapping, HandlerAdapter 구현
    
    * refactor: RegisterController 어노테이션 방식으로 변경

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,48 +1,77 @@
 package com.techcourse;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.mvc.tobe.ControllerHandlerAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutionAdapter;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutor;
+import com.interface21.webmvc.servlet.mvc.tobe.HandlerMappingRegistry;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
-
-import java.util.Collections;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private HandlerMappingRegistry handlerMappingRegistry;
+    private HandlerAdapterRegistry handlerAdapterRegistry;
+    private HandlerExecutor handlerExecutor;
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
+        handlerMappingRegistry = new HandlerMappingRegistry();
+        handlerAdapterRegistry = new HandlerAdapterRegistry();
+
+        initHandlerMappings();
+        initHandlerAdapters();
+
+        handlerExecutor = new HandlerExecutor(handlerMappingRegistry, handlerAdapterRegistry);
+    }
+
+    private void initHandlerMappings() {
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+        handlerMappingRegistry.addHandlerMapping(manualHandlerMapping);
+
+        AnnotationHandlerMapping annotationHandlerMapping = new AnnotationHandlerMapping("com.techcourse");
+        annotationHandlerMapping.initialize();
+        handlerMappingRegistry.addHandlerMapping(annotationHandlerMapping);
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapterRegistry.addHandlerAdapter(new ControllerHandlerAdapter());
+        handlerAdapterRegistry.addHandlerAdapter(new HandlerExecutionAdapter());
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
+            ModelAndView mav = handlerExecutor.execute(request, response);
+            if (mav == null) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+            render(mav, request, response);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
         }
     }
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        final var jspView = new JspView(viewName);
-        jspView.render(Collections.emptyMap(), request, response);
+    private void render(ModelAndView mav, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        mav.getView().render(mav.getModel(), request, response);
     }
 }
--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,28 +1,28 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
-import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class AnnotationHandlerMapping {
+public class AnnotationHandlerMapping implements HandlerMapping {
 
     private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
     private final Object[] basePackage;
     private final Map<HandlerKey, HandlerExecution> handlerExecutions;
+    private final ControllerScanner controllerScanner;
 
     public AnnotationHandlerMapping(final Object... basePackage) {
         this.basePackage = basePackage;
         this.handlerExecutions = new HashMap<>();
+        this.controllerScanner = new ControllerScanner();
     }
 
     public void initialize() {
@@ -33,8 +33,7 @@ public class AnnotationHandlerMapping {
     }
 
     private void scanAndRegisterControllers(Object packageName) {
-        Reflections reflections = new Reflections(packageName);
-        Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        Set<Class<?>> controllerClasses = controllerScanner.scanControllers(packageName);
 
         for (Class<?> controllerClass : controllerClasses) {
             registerController(controllerClass);
@@ -42,19 +41,21 @@ public class AnnotationHandlerMapping {
     }
 
     private void registerController(Class<?> controllerClass) {
-        Object controllerInstance = createInstance(controllerClass);
-        Method[] methods = controllerClass.getDeclaredMethods();
-
+        Object controllerInstance = controllerScanner.getInstance(controllerClass);
+        Set<Method> methods = ReflectionUtils.getAllMethods(
+                controllerClass, ReflectionUtils.withAnnotation(RequestMapping.class));
         for (Method method : methods) {
-            if (method.isAnnotationPresent(RequestMapping.class)) {
-                registerHandlerMethod(controllerInstance, method);
-            }
+            registerHandlerMethod(controllerInstance, method);
         }
     }
 
     private void registerHandlerMethod(Object controllerInstance, Method method) {
         RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
         RequestMethod[] requestMethods = requestMapping.method();
+
+        if (requestMethods.length == 0) {
+            requestMethods = RequestMethod.values();
+        }
 
         for (RequestMethod requestMethod : requestMethods) {
             HandlerKey handlerKey = new HandlerKey(requestMapping.value(), requestMethod);
@@ -63,18 +64,10 @@ public class AnnotationHandlerMapping {
         }
     }
 
-    private Object createInstance(Class<?> clazz) {
-        try {
-            return clazz.getDeclaredConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public Object getHandler(final HttpServletRequest request) {
-        var requestURI = request.getRequestURI();
-        var requestMethod = RequestMethod.valueOf(request.getMethod());
-        var handlerKey = new HandlerKey(requestURI, requestMethod);
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey handlerKey = new HandlerKey(requestURI, requestMethod);
         return handlerExecutions.get(handlerKey);
     }
 }
