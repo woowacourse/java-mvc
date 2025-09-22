2d5e7d2fa256fba87506e01f42a0bce3eb176d9b
seaniiio <cnc4750@naver.com>
2025-09-23 02:25:47 +0900

    refactor: 인터페이스 기반 컨트롤러를 애노테이션 기반 컨트롤러로 수정

--- app/src/main/java/com/techcourse/ManualControllerConfigurator.java
+++ app/src/main/java/com/techcourse/ManualControllerConfigurator.java
@@ -2,8 +2,6 @@ package com.techcourse;
 
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import com.interface21.webmvc.servlet.mvc.asis.ForwardController;
-import com.techcourse.controller.RegisterController;
-import com.techcourse.controller.RegisterViewController;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,8 +10,6 @@ public class ManualControllerConfigurator {
     public static Map<String, Controller> getManualControllers() {
         final Map<String, Controller> controllers = new HashMap<>();
         controllers.put("/", new ForwardController("/index.jsp"));
-        controllers.put("/register/view", new RegisterViewController());
-        controllers.put("/register", new RegisterController());
         return controllers;
     }
 }
--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -17,7 +17,7 @@ public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "/login/view", method = RequestMethod.GET)
     public ModelAndView show(final HttpServletRequest req, final HttpServletResponse res) {
         return UserSession.getUserFrom(req.getSession())
                 .map(user -> {
