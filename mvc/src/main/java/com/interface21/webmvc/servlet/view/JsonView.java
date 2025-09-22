6dcff81ebdc6e18df8afc4eb3498df03aaf8f989
threepebbles <threepebbles@naver.com>
2025-09-22 13:40:53 +0900

    feat: JsonView 수정, UserController 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,5 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,8 +9,14 @@ import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
             throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        // writeValueAsString(): public getter가 있는 필드만 json으로 직렬화
+        response.getWriter()
+                .write(objectMapper.writeValueAsString(model));
     }
 }
