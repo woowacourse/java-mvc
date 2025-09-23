a0a018a0cae1c3f7071631260e9fec52df9dd7c6
minSsan <msp1125@hanyang.ac.kr>
2025-09-23 22:26:47 +0900

    feat: json view render 메서드 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -13,15 +13,17 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        final var writer = response.getWriter();
 
-        if (model.size() == 1) {
-            final var only = model.entrySet().iterator().next().getValue();
-            writer.print(only);
-            return;
+        try (final var writer = response.getWriter()) {
+            final var objectMapper = new ObjectMapper();
+            if (model.size() == 1) {
+                final var value = model.values().iterator().next();
+                final var json = objectMapper.writeValueAsString(value);
+                writer.print(json);
+                return;
+            }
+            final var json = objectMapper.writeValueAsString(model);
+            writer.print(json);
         }
-        final var objectMapper = new ObjectMapper();
-        final var json = objectMapper.writeValueAsString(model);
-        writer.print(json);
     }
 }
