4158b0f1752a357bb87726d4d9ad6bbf81809be1
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-23 17:23:56 +0900

    feat: 컨텐츠 타입, jsonView content 요구 사항 반영

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,6 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -22,13 +23,18 @@ public class JsonView implements View {
             final HttpServletRequest request,
             final HttpServletResponse response
     ) throws Exception {
-        response.setContentType("application/json");
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.setStatus(HttpServletResponse.SC_OK);
 
         final ObjectMapper mapper = new ObjectMapper();
-        final String json = mapper.writeValueAsString(model);
 
-        try (final PrintWriter writer = response.getWriter()){
+        final Object target = (model.size() == 1)
+                ? model.values().iterator().next() // 값 하나면 Value
+                : model;                           // 여러 개면 Json
+
+        final String json = mapper.writeValueAsString(target);
+
+        try (final PrintWriter writer = response.getWriter()) {
             writer.write(json);
             writer.flush();
         }
