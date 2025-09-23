1e744ab0d0f1c07acc5cd4594f91b5d8b7a43e04
junhaa <wooha34567@gmail.com>
2025-09-23 10:43:40 +0900

    [3단계 - JSON View 구현하기] 루키(김준하) 미션 제출합니다. (#1033)
    * feat: JsonView 클래스 구현
    
    * refactor: Legacy MVC 제거하기
    
    * refactor: JsonView render 메서드 로직 분리 및 가독성 개선

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final String body = createResponseBody(model);
+        writeJsonResponse(response, body);
+    }
+
+    private String createResponseBody(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            final Object value = model.values().iterator().next();
+            return objectMapper.writeValueAsString(value);
+        }
+        return objectMapper.writeValueAsString(model);
+    }
+
+    private void writeJsonResponse(HttpServletResponse response, String body) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.getWriter().write(body);
     }
 }
