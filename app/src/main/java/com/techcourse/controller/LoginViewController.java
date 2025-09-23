135ad95b7efa4669de211ab1807036fafcce61c2
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-23 19:35:15 +0900

    refactor: 어노테이션 import

--- app/src/main/java/com/techcourse/controller/LoginViewController.java
+++ app/src/main/java/com/techcourse/controller/LoginViewController.java
@@ -1,5 +1,6 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@com.interface21.context.stereotype.Controller
+@Controller
 public class LoginViewController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginViewController.class);
--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -1,10 +1,13 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
     @Override
     public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
