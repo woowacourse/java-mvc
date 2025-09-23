d68e312e084968a79433e3962f510eaae47faeed
ryan-dia <cjfdnjs158@naver.com>
2025-09-23 21:42:02 +0900

    style: AnnotationHandlerAdapter 포맷팅 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/AnnotationHandlerAdapter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/adapter/AnnotationHandlerAdapter.java
@@ -1,11 +1,11 @@
 package com.interface21.webmvc.servlet.mvc.tobe.adapter;
 
-import com.interface21.webmvc.servlet.view.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.HandlerExecution;
+import com.interface21.webmvc.servlet.view.ModelAndView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AnnotationHandlerAdapter implements HandlerAdapter{
+public class AnnotationHandlerAdapter implements HandlerAdapter {
 
     @Override
     public boolean supports(Object handler) {
