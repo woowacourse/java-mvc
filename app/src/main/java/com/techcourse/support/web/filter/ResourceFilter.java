c530a5f31ac98051b3e5874f7f687f1ca16731bd
Dompoo <dlckdrms0517@gmail.com>
2025-09-23 17:06:06 +0900

    fix: ResourceFilter가 url을 startsWith로 검사하던 문제 해결

--- app/src/main/java/com/techcourse/support/web/filter/ResourceFilter.java
+++ app/src/main/java/com/techcourse/support/web/filter/ResourceFilter.java
@@ -3,13 +3,12 @@ package com.techcourse.support.web.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebFilter("/*")
 public class ResourceFilter implements Filter {
@@ -51,7 +50,7 @@ public class ResourceFilter implements Filter {
 
     private boolean isResourceUrl(final String url) {
         for (final var prefix : resourcePrefixs) {
-            if (url.startsWith(prefix)) {
+            if (url.equals(prefix)) {
                 return true;
             }
         }
