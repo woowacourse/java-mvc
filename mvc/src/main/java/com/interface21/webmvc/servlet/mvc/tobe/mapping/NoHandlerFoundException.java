52ccb9101f76bc3f9be8b9052f60b08d530ee765
Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
2025-09-23 21:54:59 +0900

    Revert "[2단계 - 점진적인 리팩터링] 새로이(강철원) 미션 제출합니다. (#1012)"
    This reverts commit c3332c7c20352c6dfdc0f72a6aad5f793413eb88.

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/mapping/NoHandlerFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.interface21.webmvc.servlet.mvc.tobe.mapping;
-
-public class NoHandlerFoundException extends RuntimeException {
-    public NoHandlerFoundException(String message) {
-        super(message);
-    }
-}
