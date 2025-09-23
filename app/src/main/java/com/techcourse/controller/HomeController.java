fea9c02f8389da2227da8fec7a1c6a9fc896d2ed
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-23 19:34:45 +0900

    feat: 루트 경로를 어노테이션 기반으로 처리

---
+++ app/src/main/java/com/techcourse/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.techcourse.controller;
+
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return "/index.jsp";
+    }
+}
