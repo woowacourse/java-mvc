92958aeed9584ddcf44117a9e0506042c062b32a
JO-eusan <goodsan0330@gmail.com>
2025-09-23 22:23:08 +0900

    refactor: 제공 MediaType으로 Json 타입 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,6 +1,7 @@
 package com.interface21.webmvc.servlet.view;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,10 +14,8 @@ public class JsonView implements View {
 
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        try(PrintWriter writer = response.getWriter()) {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try (PrintWriter writer = response.getWriter()) {
             Object value = getSerializableValue(model);
             objectMapper.writeValue(writer, value);
         }
