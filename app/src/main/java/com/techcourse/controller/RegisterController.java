e76bbc81b24df05fe65aa244fade88d27e9b1743
DongchannN <ldc991104@gmail.com>
2025-09-24 13:18:10 +0900

    refactor: 정적인 view만 반환하는 경우 메서드에서 문자열 반환하도록 수정

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -14,18 +14,18 @@ import com.interface21.webmvc.servlet.ModelAndView;
 public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView save(HttpServletRequest req, HttpServletResponse res) {
+    public String save(HttpServletRequest req, HttpServletResponse res) {
         final var user = new User(2,
                 req.getParameter("account"),
                 req.getParameter("password"),
                 req.getParameter("email"));
         InMemoryUserRepository.save(user);
 
-        return new ModelAndView(new JspView("redirect:/index.jsp"));
+        return "redirect:/index.jsp";
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView show(HttpServletRequest req, HttpServletResponse res) {
-        return new ModelAndView(new JspView("/register.jsp"));
+    public String show(HttpServletRequest req, HttpServletResponse res) {
+        return "/register.jsp";
     }
 }
