1b3af6dcc5b767d7e115d32b98486502b56601bb
Minuring <mw9505@gmail.com>
2025-09-25 00:21:34 +0900

    model에 데이터가 1개면 값만 반환

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -15,6 +15,12 @@ public class JsonView implements View {
     @Override
     public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        if (model.size() == 1) {
+            final var value = model.entrySet().iterator().next().getValue();
+            mapper.writeValue(response.getOutputStream(), value);
+            return;
+        }
         mapper.writeValue(response.getOutputStream(), model);
     }
 }
