efb4a200ec2239e535b204933e6ad3063bbb80a9
jhpark1227 <junhyeok0246@naver.com>
2025-09-24 11:33:19 +0900

    refactor: Legacy MVC 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/ControllerScanner.java
@@ -16,6 +16,7 @@ public class ControllerScanner {
 
     public Map<Class<?>, Object> getControllers() {
         Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
+        System.out.println("size: "+controllerClasses.size());
         return instantiateController(controllerClasses);
     }
 
