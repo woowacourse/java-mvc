c770496a0eeb59fec554240c37015c16e8aa5752
geonwoo <kysub99@naver.com>
2025-09-24 19:04:16 +0900

    chore: DispatcherServlet 패키지 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/DispatcherServlet.java
@@ -1,8 +1,7 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet.mvc;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.View;
-import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutionAdapter;
