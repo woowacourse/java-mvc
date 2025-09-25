c4a80939bab15cec12afb0565e531b9b274858cc
Kim Eunsu <88280787+rladmstn@users.noreply.github.com>
2025-09-25 21:08:21 +0900

    [3단계 - JSON View 구현하기] 피글렛(김은수) 미션 제출합니다. (#1052)
    * chore: DispatcherServlet 이동
    
    * feat: JsonView 구현
    
    * fix: 레거시 MVC 컨트롤러 수정
    
    * fix: DispatcherServlet 의존성 추가
    
    * fix: asis 패키지의 레거시 코드 모두 삭제
    
    * fix: 불필요한 close 제거

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,33 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(mapToJson(model));
+        writer.flush();
+    }
+
+    private String mapToJson(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values().iterator().next());
+        }
+        return objectMapper.writeValueAsString(model);
     }
 }
