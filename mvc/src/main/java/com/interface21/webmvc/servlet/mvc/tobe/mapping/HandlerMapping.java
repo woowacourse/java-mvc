9a5acef53cbd0993e17a60f87d6b62954b0206dc
eunseongu <rndmstjs75@gmail.com>
2025-09-25 11:30:24 +0900

    feat: HandlerMapping 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/mapping/HandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/mapping/HandlerMapping.java
@@ -1,8 +1,8 @@
-package com.interface21.webmvc.servlet.mvc.tobe;
+package com.interface21.webmvc.servlet.mvc.tobe.mapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface HandlerMapping {
-    
+
     Object getHandler(HttpServletRequest request);
 }
