ef672668a1b6ff86d5bf607ea266c9efdae49540
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-09-27 01:08:28 +0900

    [3단계 - JSON View 구현하기] 코기(장재현) 미션 제출합니다. (#1039)
    * feat: Json 뷰 추가
    
    * refactor: 애노테이션 기반 컨트롤러로 변경
    
    * feat: json 기반 컨트롤러 추가
    
    * refactor: 애노테이션 기반 컨트롤러로 변경

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,23 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final ServletOutputStream outputStream = response.getOutputStream();
+
+        objectMapper.writeValue(outputStream, model);
     }
 }
