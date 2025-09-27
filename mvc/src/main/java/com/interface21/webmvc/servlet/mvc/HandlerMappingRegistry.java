c2a4b3923b930a9cab5db8044be5fb0bc50d86bc
라젤 <razel7130@gmail.com>
2025-09-28 02:44:57 +0900

    [3단계 - JSON View 구현하기] 라젤(문선영) 미션 제출합니다. (#1051)
    * docs: 3단계 기능 요구사항 작성
    
    * feat: JsonView 구현
    
    * docs: 진행사항 업데이트
    
    * feat: LoginViewController를 어노테이션 기반 MVC로 변경
    
    * feat: LogoutController 어노테이션 기반 MVC로 변경
    
    * feat: RegisterViewController 어노테이션 기반 MVC로 변경
    
    * feat: LoginController 어노테이션 기반 MVC로 변경
    
    * feat: RegisterController 어노테이션 기반 MVC로 변경
    
    * feat: UserController 구현
    
    * feat: ForwardController 구현
    
    * refactor: asis 패키지에 있는 레거시 코드 삭제
    
    * refactor: ManualHandlerMapping 삭제
    
    * refactor: DispatcherServletInitializer의 Legacy MVC 제거하기
    
    * fix: DispatcherServletInitializer에 누락된 basePackage 설정 추가
    
    * refactor: HandlerMappingRegistry의 getHandler 메서드에 null 필터링 추가
    
    * fix: ControllerScanner의 from 메서드에서 var args 모호성 오류 해결을 위해 basePackages를 Object[]로 캐스팅
    
    * refactor: DispatcherServlet를 app -> mvc 로 패키지 이동
    
    * docs: 진행 사항 업데이트

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerMappingRegistry.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/HandlerMappingRegistry.java
@@ -3,6 +3,7 @@ package com.interface21.webmvc.servlet.mvc;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HandlerMappingRegistry {
 
@@ -27,6 +28,7 @@ public class HandlerMappingRegistry {
     public Object getHandler(final HttpServletRequest request) {
         return handlerMappings.stream()
                 .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("요청을 처리할 수 없습니다."));
     }
