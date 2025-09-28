90d933a6efe4a35dd5f1089a89ef303d1fac4c5a
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-28 22:32:57 +0900

    feat: JSON view 구현

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -18,17 +18,29 @@ public class User {
         return this.password.equals(password);
     }
 
+    public long getId() {
+        return id;
+    }
+
     public String getAccount() {
         return account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
-                ", account='" + account + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+               "id=" + id +
+               ", account='" + account + '\'' +
+               ", email='" + email + '\'' +
+               ", password='" + password + '\'' +
+               '}';
     }
 }
