94518dd2f68e2e0e21d656f2f951676913710974
시원 <cnc4750@inha.edu>
2025-09-28 15:28:53 +0900

    feat: Json 응답을 렌더링하는 JsonView 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,35 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.addHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final PrintWriter responseWriter = response.getWriter();
+
+        if (model.size() == 1) {
+            final String result = model.values().stream()
+                    .findFirst()
+                    .get()
+                    .toString();
+
+            responseWriter.write(result);
+        }
+
+        if (model.size() > 1) {
+            final String result = objectMapper.writeValueAsString(model);
+            responseWriter.write(result);
+        }
     }
 }
