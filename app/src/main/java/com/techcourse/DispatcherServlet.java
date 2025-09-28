d7be5d535f7453607de644d107ad4ff47f1dc793
eunseongu <rndmstjs75@gmail.com>
2025-09-28 15:29:39 +0900

    refactor: Handler가 없을 경우 404를 응답하도록 변경

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -10,10 +10,12 @@ import com.interface21.webmvc.servlet.mvc.tobe.adapter.HandlerExecutionHandlerAd
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.HandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.HandlerMappingRegistry;
+import com.techcourse.exception.HandlerNotFoundException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,16 +50,19 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
-            throws ServletException {
+            throws ServletException, IOException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
 
         try {
             Object handler = handlerMappingRegistry.getHandler(request)
-                    .orElseThrow(() -> new IllegalStateException("handler를 찾을 수 없습니다."));
+                    .orElseThrow(() -> new HandlerNotFoundException("handler를 찾을 수 없습니다."));
             HandlerAdapter handlerAdapter = handlerAdapterRegistry.getHandlerAdapter(handler);
             ModelAndView modelAndView = handlerExecutor.execute(handlerAdapter, handler, request, response);
             render(modelAndView, request, response);
+        } catch (HandlerNotFoundException e) {
+            log.warn("Not found handler. message={}", e.getMessage());
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         } catch (Throwable e) {
             log.error("Exception : {}", e.getMessage(), e);
             throw new ServletException(e.getMessage());
