cad897b4ddf417122ba6fbfeb4392c5ba604b848
geonwoo kim <115832836+kysub99@users.noreply.github.com>
2025-09-28 17:15:10 +0900

    [3단계 - JSON View 구현하기] 조로(김건우) 미션 제출합니다. (#1078)
    * feat: AnnotationHandlerMapping 구현
    
    * feat: DispatchServlet AnnotationHandler 도입
    
    * refactor: 인터페이스 관계 리팩터링
    
    * refactor: 패키지 이동
    
    * Revert "refactor: 패키지 이동"
    
    This reverts commit 19e722428c2d0e675a3fe251c5f3240e8a2746f0.
    
    * Revert "refactor: 인터페이스 관계 리팩터링"
    
    This reverts commit 7d8b2f45fb2345651828aec8eb6e2431a8af5116.
    
    * Revert "feat: DispatchServlet AnnotationHandler 도입"
    
    This reverts commit 5764cae6e137b8939f50839461595d9fdf4788c7.
    
    * Revert "feat: AnnotationHandlerMapping 구현"
    
    This reverts commit a8be79ca461484242b03b0616c942c2ae9bf8d8b.
    
    * feat: JsonView 구현
    
    * refactor: JsonView 요구사항 반영
    
    * refactor: Controller 어노테이션 기반으로 변경
    
    * refactor: Legacy MVC 제거
    
    * feat: UserController 추가
    
    * chore: DispatcherServlet 패키지 이동
    
    ---------
    
    Co-authored-by: PraiseBak <60121346+PraiseBak@users.noreply.github.com>

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionAdapter.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecutionAdapter.java
@@ -2,9 +2,11 @@ package com.interface21.webmvc.servlet.mvc.tobe;
 
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.HandlerAdapter;
+import com.interface21.webmvc.servlet.view.JsonView;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class HandlerExecutionAdapter implements HandlerAdapter {
 
@@ -14,14 +16,20 @@ public class HandlerExecutionAdapter implements HandlerAdapter {
     }
 
     @Override
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response,
+                               final Object handler) throws Exception {
         final var result = ((HandlerExecution) handler).handle(request, response);
 
-        if (result instanceof ModelAndView) {
-            return (ModelAndView) result;
+        if (result instanceof Map<?, ?> map) {
+            var mav = new ModelAndView(new JsonView());
+            map.forEach((k, v) -> mav.addObject((String) k, v));
+            return mav;
         }
-        if (result instanceof String) {
-            return new ModelAndView(new JspView((String) result));
+        if (result instanceof ModelAndView mav) {
+            return mav;
+        }
+        if (result instanceof String viewName) {
+            return new ModelAndView(new JspView(viewName));
         }
         throw new IllegalStateException("Unsupported return type: " + result.getClass().getName());
     }
