78dfbe01ffd6659345566d480fcda490c3bcda16
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-09-29 10:57:15 +0900

    refactor: 불필요한 패키지 경로 제거

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.techcourse.domain.User;
@@ -7,7 +8,7 @@ import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@com.interface21.context.stereotype.Controller
+@Controller
 public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
