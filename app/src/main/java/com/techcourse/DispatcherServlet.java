7112c2793f562b4ea25b2c6e0fdd7658116784a6
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-29 16:15:34 +0900

    refactor: 두 가지 형식의 Handler를 지원하도록 변경

--- app/src/main/java/com/techcourse/DispatcherServlet.java
+++ app/src/main/java/com/techcourse/DispatcherServlet.java
@@ -1,51 +1,58 @@
 package com.techcourse;
 
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.mvc.HandlerSelector;
+import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
+import com.interface21.webmvc.servlet.processor.HandlerProcessorFacade;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.webmvc.servlet.view.JspView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class DispatcherServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private ManualHandlerMapping manualHandlerMapping;
+    private final List<HandlerSelector> handlerSelectors = new ArrayList<>();
+    private final HandlerProcessorFacade handlerProcessorFacade = new HandlerProcessorFacade();
 
     public DispatcherServlet() {
     }
 
     @Override
     public void init() {
-        manualHandlerMapping = new ManualHandlerMapping();
+        ManualHandlerMapping manualHandlerMapping = new ManualHandlerMapping();
         manualHandlerMapping.initialize();
+        handlerSelectors.add(manualHandlerMapping);
+        AnnotationHandlerMapping annotationHandlerMapping =
+                new AnnotationHandlerMapping(this.getClass().getPackageName());
+        annotationHandlerMapping.initialize();
+        handlerSelectors.add(annotationHandlerMapping);
     }
 
     @Override
-    protected void service(final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
+    protected void service(
+            final HttpServletRequest request, final HttpServletResponse response) throws ServletException {
         final String requestURI = request.getRequestURI();
         log.debug("Method : {}, Request URI : {}", request.getMethod(), requestURI);
-
         try {
-            final var controller = manualHandlerMapping.getHandler(requestURI);
-            final var viewName = controller.execute(request, response);
-            move(viewName, request, response);
-        } catch (Throwable e) {
-            log.error("Exception : {}", e.getMessage(), e);
-            throw new ServletException(e.getMessage());
-        }
-    }
+            Object handler = handlerSelectors.stream()
+                    .map(mapping -> mapping.getHandler(request))
+                    .filter(Objects::nonNull)
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("Handler Not Found!"));
 
-    private void move(final String viewName, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        if (viewName.startsWith(JspView.REDIRECT_PREFIX)) {
-            response.sendRedirect(viewName.substring(JspView.REDIRECT_PREFIX.length()));
-            return;
+            ModelAndView modelAndView = handlerProcessorFacade.process(handler, request, response);
+            modelAndView.getView().render(modelAndView.getModel(), request, response);
+        } catch (Exception e) {
+            throw new ServletException(e);
         }
-
-        final var requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
