d9b57f16c898ec129d5dfacbc30bad3bd6e5f1fc
Lee Dong Yeop <s01054830455@gmail.com>
2025-09-29 19:44:48 +0900

    [3단계 - JSON View 구현하기] 도기(이동엽) 미션 제출합니다. (#1093)
    * feat: JSON view 구현
    
    * feat: Legacy MVC 제거하기
    
    - 기존 ManualHandlerMapping 및 Controller 인터페이스 제거
    - @Controller와 @RequestMapping 기반 매핑 처리로 전환
    - ForwardController 제거 및 컨트롤러 별 HTTP 메서드 별도 매핑 추가
    - ModelAndView와 JspView 도입으로 뷰 렌더링 개선
    - login.jsp 및 index.jsp의 링크 수정 및 불필요 코드 정리
    - HomeController 추가로 기본 경로 요청 매핑 구현
    
    * feat: ManualHandlerAdapter 및 관련 레거시 코드 제거
    
    - ManualHandlerAdapter 클래스 삭제
    - com.techcourse.DispatcherServlet의 패키지 경로를 com.interface21.webmvc.servlet로 이동
    - DispatcherServletInitializer 내 코드 스타일 변경 및 미세한 포맷 수정
    - 불필요한 레거시 의존성 패키지 제거 및 구조 정리
    
    * fix: register.jsp 로그인 링크 경로 변경
    
    - 로그인 링크 경로를 `/login/view`에서 `/login`으로 수정

--- mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
@@ -1,12 +1,9 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet;
 
-import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerMapping;
-import com.interface21.webmvc.servlet.mvc.tobe.ManualHandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,8 +22,8 @@ public class DispatcherServlet extends HttpServlet {
     private final List<HandlerAdapter> handlerAdapter;
 
     public DispatcherServlet() {
-        this.handlerAdapter = List.of(new AnnotationHandlerAdapter(), new ManualHandlerAdapter());
-        this.handlerMapping = List.of(new AnnotationHandlerMapping(BASE_PACKAGE), new ManualHandlerMapping());
+        this.handlerAdapter = List.of(new AnnotationHandlerAdapter());
+        this.handlerMapping = List.of(new AnnotationHandlerMapping(BASE_PACKAGE));
     }
 
     @Override
--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,24 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.util.Map;
 
 public class JsonView implements View {
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        Object value = model;
+        if (model.size() == 1) {
+            value = model.get(model.keySet().iterator().next());
+        }
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getWriter(), value);
     }
 }
