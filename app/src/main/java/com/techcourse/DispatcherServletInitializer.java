60b609afd59456d585d516eef24e74b30473dd89
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-09-29 13:21:54 +0900

    [3단계 - JSON View 구현하기] 수양(양수빈) 미션 제출합니다. (#1091)
    * feat: jsonView 구현
    
    * refactor: legacy 패키지로 이동
    
    * refactor: legacy 패키지로 이동
    
    * fix: root 화면 연결되도록 수정
    
    * feat: UserController 추가
    
    * refactor: json 응답 처리
    
    * refactor: email, password 보이도록 수정

--- app/src/main/java/com/techcourse/DispatcherServletInitializer.java
+++ app/src/main/java/com/techcourse/DispatcherServletInitializer.java
@@ -1,9 +1,10 @@
 package com.techcourse;
 
+import com.interface21.web.WebApplicationInitializer;
+import com.interface21.webmvc.servlet.DispatcherServlet;
 import jakarta.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.interface21.web.WebApplicationInitializer;
 
 /**
  * Base class for {@link WebApplicationInitializer}
@@ -12,7 +13,6 @@ import com.interface21.web.WebApplicationInitializer;
 public class DispatcherServletInitializer implements WebApplicationInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServletInitializer.class);
-
     private static final String DEFAULT_SERVLET_NAME = "dispatcher";
 
     @Override
--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -1,14 +1,36 @@
 package com.interface21.webmvc.servlet.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interface21.web.http.MediaType;
 import com.interface21.webmvc.servlet.View;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final Logger log = LoggerFactory.getLogger(JsonView.class);
+
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        log.debug("Rendering JSON. Model size: {}", model.size());
+        model.forEach((key, value) -> log.debug("Model entry: key={}, value.class={}", key, value.getClass().getName()));
+
+        final PrintWriter writer = response.getWriter();
+        if (model.size() == 1) {
+            Object value = model.values().iterator().next();
+            log.debug("Model size is 1. Serializing value: {}", value);
+            writer.write(OBJECT_MAPPER.writeValueAsString(value));
+            return;
+        }
+
+        log.debug("Model size is > 1. Serializing entire model map.");
+        writer.write(OBJECT_MAPPER.writeValueAsString(model));
     }
 }
