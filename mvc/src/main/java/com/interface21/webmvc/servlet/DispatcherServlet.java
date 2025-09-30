b22694f48071aaf357cf849df2aba0b01d9f8436
egaeng09 <egaeng09@gmail.com>
2025-09-30 19:46:48 +0900

    chore: DispatcherServlet app -> mvc 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
@@ -1,7 +1,5 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet;
 
-import com.interface21.webmvc.servlet.ModelAndView;
-import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.mvc.tobe.exception.MethodNotAllowedException;
 import com.interface21.webmvc.servlet.mvc.tobe.exception.NoHandlerFoundException;
 import com.interface21.webmvc.servlet.mvc.tobe.mapping.AnnotationHandlerMapping;
