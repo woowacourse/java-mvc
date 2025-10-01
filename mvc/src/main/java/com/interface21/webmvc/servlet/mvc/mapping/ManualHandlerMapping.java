9561f6cd39baef68cbd810f19ca48ebab3bdb179
yush <yush2202@gmail.com>
2025-10-01 16:15:18 +0900

    refactor: 패키지 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/mapping/ManualHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/mapping/ManualHandlerMapping.java
@@ -1,7 +1,6 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet.mvc.mapping;
 
 import com.interface21.webmvc.servlet.mvc.asis.Controller;
-import com.interface21.webmvc.servlet.mvc.mapping.HandlerMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -9,13 +9,6 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
-    /***
-     *
-     * JSON을 자바 객체로 변환할 때 Jackson 라이브러리를 사용한다.
-     * Jackson 라이브러리 공식 문서를 읽어보고 사용법을 익힌다.
-     * JSON으로 응답할 때 ContentType은 MediaType.APPLICATION_JSON_UTF8_VALUE으로 반환해야 한다.
-     * model에 데이터가 1개면 값을 그대로 반환하고 2개 이상이면 Map 형태 그대로 JSON으로 변환해서 반환한다.
-     */
 
     private final ObjectMapper mapper = new ObjectMapper();
 
