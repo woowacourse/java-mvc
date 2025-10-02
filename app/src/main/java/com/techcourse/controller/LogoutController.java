1444a2a9a62fe0459d39c0331ca4ae42f222b16a
yush <yush2202@gmail.com>
2025-10-02 12:38:49 +0900

    fix: logout 메서드 수정

--- app/src/main/java/com/techcourse/controller/LogoutController.java
+++ app/src/main/java/com/techcourse/controller/LogoutController.java
@@ -8,15 +8,19 @@ import com.interface21.webmvc.servlet.View;
 import com.interface21.webmvc.servlet.view.JspView;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class LogoutController {
 
-    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ModelAndView post(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
         final var session = req.getSession();
         session.removeAttribute(UserSession.SESSION_KEY);
-        View view = new JspView("redirect:/");
+        View view = new JspView("redirect:/index.jsp");
         return new ModelAndView(view);
     }
 }
