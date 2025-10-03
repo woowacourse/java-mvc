b967f058a33d149250ecfd94c8a41b4cccc48c4e
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-03 19:43:34 +0900

    refactor: argumentResolver 로직 추가

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/HandlerExecution.java
@@ -1,10 +1,13 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.ModelAndView;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HandlerExecution {
 
@@ -16,7 +19,35 @@ public class HandlerExecution {
         this.method = method;
     }
 
-    public ModelAndView handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-        return (ModelAndView) method.invoke(controllerInstance, request, response);
+    public Object handle(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        Object[] args = resolveArguments(request, response);
+        return method.invoke(controllerInstance, args);
+    }
+
+    private Object[] resolveArguments(final HttpServletRequest request, final HttpServletResponse response) {
+        List<Object> args = new ArrayList<>();
+
+        for (Parameter parameter : method.getParameters()) {
+            args.add(resolveArgument(parameter, request, response));
+        }
+
+        return args.toArray();
+    }
+
+    private Object resolveArgument(final Parameter parameter, final HttpServletRequest request, final HttpServletResponse response) {
+        Class<?> type = parameter.getType();
+
+        if (type == HttpServletRequest.class) {
+            return request;
+        }
+        if (type == HttpServletResponse.class) {
+            return response;
+        }
+        if (parameter.isAnnotationPresent(RequestParam.class)) {
+            String paramName = parameter.getAnnotation(RequestParam.class).value();
+            return request.getParameter(paramName);
+        }
+
+        return null;
     }
 }
