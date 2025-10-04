fdbfc23e9ec505f5ce30d523d873029fe93f29c9
eunseongu <rndmstjs75@gmail.com>
2025-10-04 17:37:50 +0900

    refactor: 패키지 이동

--- app/src/main/java/DispatcherServletInitializer.java
+++ app/src/main/java/DispatcherServletInitializer.java
@@ -1,6 +1,5 @@
-package com.interface21.webmvc.servlet;
-
 import com.interface21.web.WebApplicationInitializer;
+import com.interface21.webmvc.servlet.DispatcherServlet;
 import jakarta.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
