1791d4ebbf909a5879d40080ef60910d0a6a54c1
PARK HYUNG GYUN <122284322+phk1128@users.noreply.github.com>
2025-10-04 11:57:21 +0900

    [3단계 - JSON View 구현하기] 윌슨(박형균) 미션 제출합니다. (#1096)
    * refactor: DispatcherServlet을 mvc 패키지로 이동
    
    * refactor: Legacy MVC를 어노테이션 기반 MVC로 변경
    
    * feat: JsonView 구현
    
    * feat: UserController 구현
    
    * fix: 컨트롤러에서 String을 반환할 경우 ModelAndView로 정상적으로 응답하도록 수정
    
    * refactor: argumentResolver 로직 추가
    
    * refactor: login 파라미터 매핑 수정

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,33 +1,36 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.web.bind.annotation.RequestParam;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        if (UserSession.isLoggedIn(req.getSession())) {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("account") String account, @RequestParam("password") String password, HttpServletRequest request) {
+        if (UserSession.isLoggedIn(request.getSession())) {
             return "redirect:/index.jsp";
         }
 
-        return InMemoryUserRepository.findByAccount(req.getParameter("account"))
+        return InMemoryUserRepository.findByAccount(account)
                 .map(user -> {
                     log.info("User : {}", user);
-                    return login(req, user);
+                    return authenticateUser(request, user, password);
                 })
                 .orElse("redirect:/401.jsp");
     }
 
-    private String login(final HttpServletRequest request, final User user) {
-        if (user.checkPassword(request.getParameter("password"))) {
+    private String authenticateUser(final HttpServletRequest request, final User user, final String password) {
+        if (user.checkPassword(password)) {
             final var session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
             return "redirect:/index.jsp";
--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,21 +1,19 @@
 package com.techcourse.controller;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.web.bind.annotation.RequestParam;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.mvc.asis.Controller;
 
-public class RegisterController implements Controller {
+@Controller
+public class RegisterController {
 
-    @Override
-    public String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var user = new User(2,
-                req.getParameter("account"),
-                req.getParameter("password"),
-                req.getParameter("email"));
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(@RequestParam String account, @RequestParam String password, @RequestParam String email) {
+        final var user = new User(2, account, password, email);
         InMemoryUserRepository.save(user);
-
         return "redirect:/index.jsp";
     }
 }
