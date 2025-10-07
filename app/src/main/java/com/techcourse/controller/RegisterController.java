f0a5ad39354643e48e3046e9c0518d7d5d4026a4
eoehd1ek <eoehd2ek@gmail.com>
2025-10-08 06:16:57 +0900

    refactor: RegisterController 리팩토링

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -5,6 +5,7 @@ import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.BadRequestException;
 import com.techcourse.domain.User;
 import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,14 +15,27 @@ import jakarta.servlet.http.HttpServletResponse;
 public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
-        final var user = new User(2,
-                req.getParameter("account"),
-                req.getParameter("password"),
-                req.getParameter("email"));
-        InMemoryUserRepository.save(user);
+    public ModelAndView register(final HttpServletRequest req, final HttpServletResponse res)
+            throws BadRequestException {
+        saveUser(req);
 
-        final String viewName = "redirect:/index.jsp";
+        return redirectToIndex();
+    }
+
+    private void saveUser(final HttpServletRequest req) throws BadRequestException {
+        try {
+            final User user = new User(2,
+                    req.getParameter("account"),
+                    req.getParameter("password"),
+                    req.getParameter("email"));
+            InMemoryUserRepository.save(user);
+        } catch (IllegalStateException e) {
+            throw new BadRequestException("회원가입 폼 데이터가 누락 되었습니다.");
+        }
+    }
+
+    private ModelAndView redirectToIndex() {
+        final String viewName = "redirect:/";
         final JspView jspView = new JspView(viewName);
         return new ModelAndView(jspView);
     }
