0446f4807b50927337e50a653da832da9f95b12a
taek2222 <oot309@daum.net>
2025-10-07 18:28:55 +0900

    refactor: json view 리팩토링

--- mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/view/JsonView.java
@@ -9,23 +9,15 @@ import java.util.Map;
 
 public class JsonView implements View {
 
-    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
-    private final ObjectMapper objectMapper;
-    private final Object data;
+    private static final String CONTENT_TYPE = "application/json";
 
-    public JsonView(Object data) {
-        this.data = data;
-        this.objectMapper = new ObjectMapper();
-    }
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void render(final Map<String, ?> model, final HttpServletRequest request, HttpServletResponse response)
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType(CONTENT_TYPE);
         response.setCharacterEncoding(StandardCharsets.UTF_8);
-
-        String json = objectMapper.writeValueAsString(data);
-        response.getWriter().write(json);
-        response.getWriter().flush();
+        response.getWriter().write(objectMapper.writeValueAsString(model));
     }
 }
