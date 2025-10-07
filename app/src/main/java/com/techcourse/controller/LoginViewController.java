125ea675ed846bfcb274c9ffbdddad29e3817347
eoehd1ek <eoehd2ek@gmail.com>
2025-10-08 06:06:40 +0900

    refactor: LoginViewController 리팩토링

--- app/src/main/java/com/techcourse/controller/LoginViewController.java
+++ app/src/main/java/com/techcourse/controller/LoginViewController.java
@@ -5,8 +5,10 @@ import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.domain.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,14 +18,32 @@ public class LoginViewController {
     private static final Logger log = LoggerFactory.getLogger(LoginViewController.class);
 
     @RequestMapping(value = "/login/view", method = RequestMethod.GET)
-    public ModelAndView loginView(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+    public ModelAndView loginView(final HttpServletRequest req, final HttpServletResponse res) {
+        if (isLoggedIn(req)) {
+            return redirectToIndex();
+        }
 
-        final String viewName = UserSession.getUserFrom(req.getSession())
-                .map(user -> {
-                    log.info("logged in {}", user.getAccount());
-                    return "redirect:/index.jsp";
-                })
-                .orElse("/login.jsp");
+        return redirectToLoginView();
+    }
+
+    private boolean isLoggedIn(final HttpServletRequest req) {
+        final Optional<User> user = UserSession.getUserFrom(req.getSession());
+        if (user.isEmpty()) {
+            return false;
+        }
+
+        log.info("이미 로그인 상태입니다. User: {}", user.get().getAccount());
+        return true;
+    }
+
+    private ModelAndView redirectToIndex() {
+        final String viewName = "redirect:/index.jsp";
+        final JspView jspView = new JspView(viewName);
+        return new ModelAndView(jspView);
+    }
+
+    private ModelAndView redirectToLoginView() {
+        final String viewName = "/login.jsp";
         final JspView jspView = new JspView(viewName);
         return new ModelAndView(jspView);
     }
