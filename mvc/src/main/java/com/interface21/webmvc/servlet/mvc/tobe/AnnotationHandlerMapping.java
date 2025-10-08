73ce535cecffd3ab06ce9be895e7cf3ffec032ac
YuyoungRhee <bbclara@naver.com>
2025-10-09 00:33:48 +0900

    refactor: ControllerScanner 클래스 분리

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,6 +1,5 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
-import com.interface21.context.stereotype.Controller;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,9 +7,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Stream;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,23 +24,16 @@ public class AnnotationHandlerMapping implements HandlerMapping {
     }
 
     public void initialize() {
-        final var reflections = new Reflections(basePackage);
-        final Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(Controller.class);
-
-        try {
-            for (final Class<?> controllerClass : controllerClasses) {
-                final Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
-                registerControllerMethods(controllerInstance, controllerClass.getDeclaredMethods());
-            }
-        } catch (Exception e) {
-            log.error("Error creating controller instance", e);
-            throw new RuntimeException(e);
-        }
+        final ControllerScanner scanner = new ControllerScanner(basePackage);
+        final Map<Class<?>, Object> controllers = scanner.scan();
+        controllers.forEach((controllerClass, controllerInstance) -> {
+            registerControllerMethods(controllerInstance, controllerClass.getDeclaredMethods());
+        });
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    private void registerControllerMethods(final Object controllerInstance, final java.lang.reflect.Method[] methods) {
-        for (final java.lang.reflect.Method method : methods) {
+    private void registerControllerMethods(final Object controllerInstance, final Method[] methods) {
+        for (final Method method : methods) {
             if (!method.isAnnotationPresent(RequestMapping.class)) {
                 continue;
             }
