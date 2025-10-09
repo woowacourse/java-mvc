e331dfa073624b561b576af1b2ae7e53083d8e97
YuyoungRhee <bbclara@naver.com>
2025-10-10 03:59:51 +0900

    refactor: DispatcherServlet를 mvc 모듈로 이동

--- mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/DispatcherServlet.java
@@ -1,6 +1,5 @@
-package com.techcourse;
+package com.interface21.webmvc.servlet;
 
-import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutionAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerAdapterRegistry;
