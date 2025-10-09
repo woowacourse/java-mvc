1d4f2c1105e281e7b440ae2c40a29a284cab6914
YuyoungRhee <bbclara@naver.com>
2025-10-10 04:14:32 +0900

    fix: HomeController 인자 오류 수정

--- app/src/main/java/com/techcourse/controller/HomeController.java
+++ app/src/main/java/com/techcourse/controller/HomeController.java
@@ -5,12 +5,14 @@ import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JspView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView home() {
+    public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
         return new ModelAndView(new JspView("index.jsp"));
     }
 }
